e1a34ad77620aa6b86158500331aa7535d000be9
Tony Leottaa <leotta@cshl.edu>
2013-07-08 15:21:10 -0400

    Added the stylesheets and java script.

--- unittests/run.sh
+++ unittests/run.sh
@@ -9,6 +9,7 @@ DEV_DB_DIR=/data/safe/leotta/send_to_natalia/data
 PRD_SRC_DIR=/mnt/wigclust8/data/safe/autism/SeqPipeline/python/DAE
 DEV_SRC_DIR=/data/safe/leotta/wigserv2_clone3/python/DAE
 
+PATH=${UT_DIR}/:$PATH
 
 $UT_DIR/runUT_production.sh $PRD_SRC_DIR $PRD_DB_DIR $UT_TEST_DEF production
 $UT_DIR/runUT_development.sh $DEV_SRC_DIR $DEV_DB_DIR $UT_TEST_DEF development
