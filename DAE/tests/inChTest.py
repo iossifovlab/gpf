8176af9f6dc4e8b4cd499527bbe23e8decdf7613
tony <leotta@cshl.edu>
2013-07-09 15:46:42 -0400

    added exit codes for inChTest.py

--- DAE/tests/inChTest.py
+++ DAE/tests/inChTest.py
@@ -1,18 +1,28 @@
 #!/bin/env python
 
 from DAE import *
+import sys
 
+exitCode=0
+
+if len(vDB.studies)==0:
+    exitCode=100
 
 for st in vDB.studies.values():
+    print st
     if not st.has_denovo:
         continue
     try:
         vs = [v for v in vDB.get_study(st.name).get_denovo_variants() if v.inChS!=v.atts['inChild'] ]
     except:
-        print st.name, "FAIL" 
+        print st.name, "FAIL"
+	exitCode=-1 
         continue
          
     if vs:
         print st.name, len(vs)
         for v in vs:
-            print "\t", v.familyId,v.inChS,v.atts['inChild'] 
+            print "\t", v.familyId,v.inChS,v.atts['inChild']
+	exitCode=-1
+
+sys.exit(exitCode)
