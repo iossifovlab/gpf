17b128ce88c22290b50a44c91cbd7634ad7f6e14
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2013-10-22 16:24:21 +0300

    download a file api initial setup...

--- wdae/api/views.py
+++ wdae/api/views.py
@@ -1,8 +1,15 @@
 # Create your views here.
+from django.http import HttpResponse
+
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
-from DAE import *
+from DAE import vDB, giDB, get_gene_sets_symNS,_safeVs
+
+from GetVariantsInterface import augmentAVar
+
+import itertools
 
 
 @api_view(['GET'])
@@ -44,7 +51,6 @@ def families_list(request,study_name=None):
     families=st.families
     
     query_params=request.QUERY_PARAMS
-    print query_params
     if query_params.has_key('filter'):
         start_string=query_params['filter']
         families=[f for f in families if f.startswith(start_string)]
@@ -52,4 +58,91 @@ def families_list(request,study_name=None):
     return Response({'study':study_name,
                      'families':families})
 
+@api_view(['GET'])
+def gene_set_list(request,set_name):
+    gs=get_gene_sets_symNS(set_name)
+    return Response(gs.tDesc)    
+
+@api_view(['GET'])
+def gene_set_denovo_list(request,denovo_study):
+    try:
+        #gts=vDB.get_denovo_sets('IossifovWE2012')
+        gts = vDB.get_denovo_sets(str(denovo_study))
+        return Response(gts.tDesc) 
     
+    except Exception as e:
+        print "Exception: ",e
+        return Response(status=status.HTTP_404_NOT_FOUND,data={"reason":"Denovo study <%s> not found..."%denovo_study})
+        
+
+def __gene_set_filter_response_dict(request,data):
+    query_params=request.QUERY_PARAMS
+    
+    if query_params.has_key('filter'):
+        filter_string=query_params['filter']
+        l=[(key,value) for (key,value) in data.items() if (filter_string in key) or (filter_string in value)]
+        data=dict(l)
+    return data
+       
+@api_view(['GET'])
+def gene_set_main_list(request):
+    gts = get_gene_sets_symNS('main')
+    res=__gene_set_filter_response_dict(request, gts.tDesc)
+    return Response(res) 
+    
+        
+@api_view(['GET'])
+def gene_set_go_list(request):
+    gts=giDB.getGeneTerms('GO')
+    res=__gene_set_filter_response_dict(request, gts.tDesc)
+    return Response(res)
+
+@api_view(['GET'])
+def gene_list(request,page_count=30):
+    gl=[g.sym for g in giDB.genes.values()]
+
+    query_params=request.QUERY_PARAMS
+    
+    if query_params.has_key('filter'):
+        start_string=query_params['filter']
+        gl=[g for g in gl if g.startswith(start_string)]
+        
+    if query_params.has_key('page_count'):
+        try:
+            page_count=int(query_params['page_count'])
+        except:
+            page_count=30
+
+    if not (page_count>0 and page_count<=100):
+        page_count=30
+        
+    return Response(gl[:page_count])
+
+
+@api_view(['POST'])
+def get_variants(request):
+    data=request.DATA
+    print data
+    
+    dvs = []
+    if data.has_key('denovoStudies'):
+        dl=data['denovoStudies']
+        try:
+            dst = [vDB.get_study(d) for d in dl]
+        except:
+            print "The denovo study: %s DOES NOT EXIST! ...exiting!" % ' '.join(dl) 
+            raise
+        dvs = vDB.get_denovo_variants(dst)
+    
+    #response=Response(status.HTTP_200_OK,content_type='application/bin',mimetype='text/csv')
+    response = HttpResponse(mimetype='text/csv')
+    response['Content-Disposition'] = 'attachment; filename=unruly.csv'
+    response['Expires'] = '0'
+
+    
+    _safeVs(response,itertools.imap(augmentAVar,itertools.chain(dvs,[])),
+                    ['effectType', 'effectDetails', 'all.altFreq','all.nAltAlls','all.nParCalled', '_par_races_', '_ch_prof_'],sep=",")
+    return response
+
+
+
