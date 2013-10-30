bae90bd4c898785affbbf1fafc1261945a02c373
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2013-10-30 16:45:18 +0200

    reformatting...

--- wdae/api/dae_query.py
+++ wdae/api/dae_query.py
@@ -1,67 +1,67 @@
 import itertools
 
 from django.conf import settings
-  
+
 from DAE import vDB
-    
+
 def prepare_inchild(data):
     if not data.has_key('inChild'):
         return None
 
-    inChild=data['inChild']
-    if inChild=='All' or inChild=='none' or inChild=='None':
+    inChild = data['inChild']
+    if inChild == 'All' or inChild == 'none' or inChild == 'None':
         return None
-    
+
     if inChild not in vDB.get_child_types():
         return None
-    
+
     return inChild
-    
-    
+
+
 def prepare_effect_types(data):
     if not data.has_key('effectTypes'):
         return None
-    
-    effect_type=data['effectTypes']
-    if effect_type=='none' or effect_type == 'None' or effect_type is None:
+
+    effect_type = data['effectTypes']
+    if effect_type == 'none' or effect_type == 'None' or effect_type is None:
         return None
 
     if effect_type not in vDB.get_effect_types():
-        effect_type='All'
-    
-    if effect_type=='All':
+        effect_type = 'All'
+
+    if effect_type == 'All':
         return ",".join(vDB.get_effect_types()[1:])
-    
+
     return effect_type
 
 
 def prepare_variant_types(data):
     if not data.has_key('variantTypes'):
         return None
-    
-    variant_types=data['variantTypes']
-    if variant_types=='none' or variant_types == 'None' or variant_types is None:
+
+    variant_types = data['variantTypes']
+    if variant_types == 'none' or variant_types == 'None' or variant_types is None:
         return None
-    
+
     if variant_types not in vDB.get_variant_types():
         return None
-    
+
     if variant_types == 'All':
         return None
-    
+
     return variant_types
 
 def prepare_family_ids(data):
     if not data.has_key('familyIds'):
         return None
-    
-    families=data['familyIds']
-    if isinstance(families,str):
+
+    families = data['familyIds']
+    if isinstance(families, str):
         if families.lower() == 'none' or families.lower() == 'all':
             return None
         else:
-            return [s.strip() for s in families.split(',') if len(s.strip())>0]
-    elif isinstance(families,list):
+            return [s.strip() for s in families.split(',') if len(s.strip()) > 0]
+    elif isinstance(families, list):
         return families
     else:
         return None
@@ -72,17 +72,17 @@ def __filter_gene_syms(gl):
 def prepare_gene_syms(data):
     if not data.has_key('geneSyms'):
         return None
-    
-    gene_sym=data['geneSyms']
+
+    gene_sym = data['geneSyms']
     if isinstance(gene_sym, list):
-        gl=__filter_gene_syms(gene_sym)
+        gl = __filter_gene_syms(gene_sym)
         if not gl:
             return None
         else:
             return set(gl)
-        
+
     elif isinstance(gene_sym, str):
-        gl = [s.strip() for s in gene_sym.split(',') if len(s.strip())>0]
+        gl = [s.strip() for s in gene_sym.split(',') if len(s.strip()) > 0]
         gl = __filter_gene_syms(gl)
         if not gl:
             return None
@@ -92,92 +92,93 @@ def prepare_gene_syms(data):
 
 from DAE import get_gene_sets_symNS
 
-def __filter_gene_set(gene_set,data):
-    gs_id=gene_set['gs_id']
-    gs_term=gene_set['gs_term']
-    
-    gs=None
-    if gs_id.lower().strip()=='main':
-        gs=settings.GENE_SETS_MAIN
-    elif gs_id.lower().strip()=='go':
-        gs=settings.GENE_SETS_GO
-    elif gs_id.lower().strip()=='disease':
-        gs=settings.GENE_SETS_DISEASE
-    elif gs_id.lower().strip()=='denovo':
-        dl=prepare_denovo_studies(data)
+def __filter_gene_set(gene_set, data):
+    gs_id = gene_set['gs_id']
+    gs_term = gene_set['gs_term']
+
+    gs = None
+    if gs_id.lower().strip() == 'main':
+        gs = settings.GENE_SETS_MAIN
+    elif gs_id.lower().strip() == 'go':
+        gs = settings.GENE_SETS_GO
+    elif gs_id.lower().strip() == 'disease':
+        gs = settings.GENE_SETS_DISEASE
+    elif gs_id.lower().strip() == 'denovo':
+        dl = prepare_denovo_studies(data)
         if not dl:
             return None
-        gs=get_gene_sets_symNS('denovo',dl)
+        gs = get_gene_sets_symNS('denovo', dl)
     else:
         return None
-    
+
     if gs_term not in gs.tDesc:
         return None
-    
-    gl=__filter_gene_syms(gs.t2G[gs_term].keys())
+
+    gl = __filter_gene_syms(gs.t2G[gs_term].keys())
 
     if not gl:
         return None
-        
+
     return set(gl)
 
-    
+
 def prepare_gene_sets(data):
     if not data.has_key('geneSet'):
         return None
-     
+
     gene_set = data['geneSet']
-    
-    if isinstance(gene_set,dict):
-        return __filter_gene_set(gene_set,data)
-    elif isinstance(gene_set,str):
-        gs_id=gene_set
+
+    if isinstance(gene_set, dict):
+        return __filter_gene_set(gene_set, data)
+    elif isinstance(gene_set, str):
+        gs_id = gene_set
         if not data.has_key('geneTerm'):
             return None
-        gs_term=data['geneTerm']
-        
-        return __filter_gene_set({'gs_id':gs_id,'gs_term':gs_term.split('|')[0].strip()},data)
+        gs_term = data['geneTerm']
+
+        return __filter_gene_set({'gs_id':gs_id, 'gs_term':gs_term.split('|')[0].strip()},
+                                 data)
     else:
         return None
 
 def prepare_denovo_studies(data):
     if not data.has_key('denovoStudies'):
         return None
-    
-    dl=data['denovoStudies']
-    if isinstance(dl,list):
+
+    dl = data['denovoStudies']
+    if isinstance(dl, list):
         dst = [vDB.get_studies(str(d)) for d in dl]
     else:
-        dst=[vDB.get_studies(str(dl))]
+        dst = [vDB.get_studies(str(dl))]
 
-    flatten=itertools.chain.from_iterable(dst)
-    res=[st for st in flatten if st is not None]
+    flatten = itertools.chain.from_iterable(dst)
+    res = [st for st in flatten if st is not None]
     if not res:
         return None
-     
+
     return res
 
 
 def prepare_transmitted_studies(data):
     if not data.has_key('transmittedStudies'):
         return None
-    
-    tl=data['transmittedStudies']
-    if isinstance(tl,list):
+
+    tl = data['transmittedStudies']
+    if isinstance(tl, list):
         tst = [vDB.get_studies(str(t)) for t in tl]
     else:
-        tst=[vDB.get_studies(str(tl))]
-    
-    flatten=itertools.chain.from_iterable(tst)
-    res=[st for st in flatten if st is not None]
+        tst = [vDB.get_studies(str(tl))]
+
+    flatten = itertools.chain.from_iterable(tst)
+    res = [st for st in flatten if st is not None]
     if not res:
         return None
-     
+
     return res
 
 def combine_gene_syms(data):
-    gene_syms=prepare_gene_syms(data)
-    gene_sets=prepare_gene_sets(data)
+    gene_syms = prepare_gene_syms(data)
+    gene_sets = prepare_gene_sets(data)
 
     if gene_syms is None:
         return gene_sets
@@ -186,46 +187,46 @@ def combine_gene_syms(data):
             return gene_syms
         else:
             return gene_sets.union(gene_syms)
-    
-#"minParentsCalled=600,maxAltFreqPrcnt=5.0,minAltFreqPrcnt=-1"
+
+# "minParentsCalled=600,maxAltFreqPrcnt=5.0,minAltFreqPrcnt=-1"
 
 def __prepare_min_alt_freq_prcnt(data):
-    minAltFreqPrcnt=-1.0
+    minAltFreqPrcnt = -1.0
     if data.has_key('minAltFreqPrcnt'):
         try:
-            minAltFreqPrcnt=float(str(data['minAltFreqPrcnt']))
+            minAltFreqPrcnt = float(str(data['minAltFreqPrcnt']))
         except:
-            minAltFreqPrcnt=-1.0
+            minAltFreqPrcnt = -1.0
     return minAltFreqPrcnt
 
 def __prepare_max_alt_freq_prcnt(data):
-    maxAltFreqPrcnt=100.0
+    maxAltFreqPrcnt = 100.0
     if data.has_key('maxAltFreqPrcnt'):
         try:
-            maxAltFreqPrcnt=float(str(data['maxAltFreqPrcnt']))
+            maxAltFreqPrcnt = float(str(data['maxAltFreqPrcnt']))
         except:
-            maxAltFreqPrcnt=5.0
+            maxAltFreqPrcnt = 5.0
     return maxAltFreqPrcnt
-        
+
 
 def __prepare_min_parents_called(data):
-    minParentsCalled=600
+    minParentsCalled = 600
     if data.has_key('minParentsCalled'):
         try:
-            minParentsCalled=float(str(data['minParentsCalled']))
+            minParentsCalled = float(str(data['minParentsCalled']))
         except:
-            minParentsCalled=600
+            minParentsCalled = 600
     return minParentsCalled
 
 
 
 def __prepare_ultra_rare(data):
-    ultraRareOnly=None
+    ultraRareOnly = None
     if data.has_key('ultraRareOnly'):
-        if ultraRareOnly=='True' or ultraRareOnly=='true':
-            ultraRareOnly=True
+        if ultraRareOnly == 'True' or ultraRareOnly == 'true':
+            ultraRareOnly = True
     elif data.has_key('rarity'):
-        if data['rarity'].strip()=='ultraRare':
+        if data['rarity'].strip() == 'ultraRare':
             return True
     return ultraRareOnly
 
@@ -236,7 +237,7 @@ def prepare_gene_region(data):
 
 
 def prepare_transmitted_filters(data):
-    filters={'variantTypes':prepare_variant_types(data),
+    filters = {'variantTypes':prepare_variant_types(data),
              'effectTypes':prepare_effect_types(data),
              'inChild':prepare_inchild(data),
              'familyIds':prepare_family_ids(data),
@@ -250,8 +251,8 @@ def prepare_transmitted_filters(data):
     return filters
 
 def prepare_denovo_filters(data):
-        
-    filters={'inChild':prepare_inchild(data),
+
+    filters = {'inChild':prepare_inchild(data),
              'variantTypes':prepare_variant_types(data),
              'effectTypes':prepare_effect_types(data),
              'familyIds':prepare_family_ids(data),
@@ -260,59 +261,59 @@ def prepare_denovo_filters(data):
     return filters
 
 def dae_query_variants(data):
-    print "dae_query_variants:",data
-    variants=[]
-    dstudies=prepare_denovo_studies(data)
-    print "denovo studies:",dstudies
-    
+    print "dae_query_variants:", data
+    variants = []
+    dstudies = prepare_denovo_studies(data)
+    print "denovo studies:", dstudies
+
     if dstudies is not None:
-        filters=prepare_denovo_filters(data)
-        print 'denovo filters:',filters
-        dvs=vDB.get_denovo_variants(dstudies,**filters)
+        filters = prepare_denovo_filters(data)
+        print 'denovo filters:', filters
+        dvs = vDB.get_denovo_variants(dstudies, **filters)
         variants.append(dvs)
-    
-    tstudies=prepare_transmitted_studies(data)
-    print "transmitted studies:",tstudies
+
+    tstudies = prepare_transmitted_studies(data)
+    print "transmitted studies:", tstudies
     if tstudies is not None:
-        filters=prepare_transmitted_filters(data)
-        print 'transmitted filters:',filters
+        filters = prepare_transmitted_filters(data)
+        print 'transmitted filters:', filters
         for study in tstudies:
-            tvs=study.get_transmitted_variants(**filters)
+            tvs = study.get_transmitted_variants(**filters)
             variants.append(tvs)
 
     return variants
-    
+
 
 from VariantsDB import mat2Str
 
 
-def save_vs(tf,vs,atts=[],sep="\t"):
-    for line in generate_response(vs,atts,sep):
+def save_vs(tf, vs, atts = [], sep = "\t"):
+    for line in generate_response(vs, atts, sep):
         tf.write(line)
 
 
-def generate_response(vs,atts=[],sep="\t"):
+def generate_response(vs, atts = [], sep = "\t"):
     def ge2Str(gs):
-        return "|".join( x['sym'] + ":" + x['eff'] for x in gs)
+        return "|".join(x['sym'] + ":" + x['eff'] for x in gs)
 
     mainAtts = "familyId studyName location variant bestSt fromParentS inChS counts geneEffect requestedGeneEffects popType".split()
     specialStrF = {"bestSt":mat2Str, "counts":mat2Str, "geneEffect":ge2Str, "requestedGeneEffects":ge2Str}
 
-    yield (sep.join(mainAtts+atts)+"\n") 
+    yield (sep.join(mainAtts + atts) + "\n")
 
     for v in vs:
         mavs = []
         for att in mainAtts:
             try:
                 if att in specialStrF:
-                    mavs.append(specialStrF[att](getattr(v,att)))
+                    mavs.append(specialStrF[att](getattr(v, att)))
                 else:
-                    mavs.append(str(getattr(v,att)))
+                    mavs.append(str(getattr(v, att)))
             except:
                 mavs.append("")
-                 
-        tmp = sep.join(mavs + [str(v.atts[a]).replace(sep, ';') if a in v.atts else "" for a in atts]) 
-        yield (tmp +"\n")
+
+        tmp = sep.join(mavs + [str(v.atts[a]).replace(sep, ';') if a in v.atts else "" for a in atts])
+        yield (tmp + "\n")
 
 
 
--- wdae/api/views.py
+++ wdae/api/views.py
@@ -1,11 +1,11 @@
 # Create your views here.
 from django.conf import settings
-  
+
 
 # from rest_framework.response import Response as RestResponse
 from rest_framework.response import Response
-from rest_framework.decorators import api_view,parser_classes
-from rest_framework.parsers import JSONParser,FormParser
+from rest_framework.decorators import api_view, parser_classes
+from rest_framework.parsers import JSONParser, FormParser
 
 
 from DAE import vDB, get_gene_sets_symNS
@@ -25,56 +25,56 @@ from dae_query import dae_query_variants, generate_response
 #         else:
 #             headers['Access-Control-Allow-Origin']='*'
 #         RestResponse.__init__(self,data,status,template_name,headers,exception,content_type)
-    
+
 @api_view(['GET'])
 def denovo_studies_list(request):
-    stds=vDB.getDenovoStudies()
-    stgs=vDB.getStudyGroups()
+    stds = vDB.getDenovoStudies()
+    stgs = vDB.getStudyGroups()
     stds.extend(stgs)
-    return Response({"denovo_studies" : [st for st in stds if st!='studyDir']})
+    return Response({"denovo_studies" : [st for st in stds if st != 'studyDir']})
 
 @api_view(['GET'])
 def study_groups_list(request):
-    stds=vDB.getStudyGroups()
+    stds = vDB.getStudyGroups()
     return Response({"study_groups" : stds})
 
 
 @api_view(['GET'])
 def transmitted_studies_list(request):
-    stds=vDB.getTransmittedStudies()
+    stds = vDB.getTransmittedStudies()
     return Response({"transmitted_studies" : stds})
 
 
 @api_view(['GET'])
 def effect_types_list(request):
-    eff=vDB.get_effect_types()
+    eff = vDB.get_effect_types()
     return Response({"effect_types" : eff})
 
 @api_view(['GET'])
 def variant_types_list(request):
-    var_types=vDB.get_variant_types()
+    var_types = vDB.get_variant_types()
     return Response({'variant_types': var_types})
 
 @api_view(['GET'])
 def child_type_list(request):
-    child_types=vDB.get_child_types()
+    child_types = vDB.get_child_types()
     return Response({'child_types':child_types})
 
 @api_view(['GET'])
-def families_list(request,study_name=None):
+def families_list(request, study_name = None):
 
-    st=vDB.get_study(study_name)
-    families=st.families.keys()
-    
-    query_params=request.QUERY_PARAMS
+    st = vDB.get_study(study_name)
+    families = st.families.keys()
+
+    query_params = request.QUERY_PARAMS
     if query_params.has_key('filter'):
-        start_string=query_params['filter']
-        families=[f for f in families if f.startswith(start_string)]
-        
+        start_string = query_params['filter']
+        families = [f for f in families if f.startswith(start_string)]
+
     return Response({'study':study_name,
                      'families':families})
 
-def __get_page_count(query_params, page_count=30):
+def __get_page_count(query_params, page_count = 30):
     if query_params.has_key('page_count'):
         try:
             page_count = int(query_params['page_count'])
@@ -84,84 +84,84 @@ def __get_page_count(query_params, page_count=30):
         page_count = 30
     return page_count
 
-def __gene_set_filter_response_dict(request,data):
-    query_params=request.QUERY_PARAMS
-    
-    page_count=__get_page_count(query_params,page_count=100)
+def __gene_set_filter_response_dict(request, data):
+    query_params = request.QUERY_PARAMS
+
+    page_count = __get_page_count(query_params, page_count = 100)
     print "page_count:", page_count
-    
+
     if query_params.has_key('filter'):
-        filter_string=query_params['filter'].lower().strip()
-        l=[(key,value) for (key,value) in data.items() if (filter_string in key.lower()) or (filter_string in value.lower())]
+        filter_string = query_params['filter'].lower().strip()
+        l = [(key, value) for (key, value) in data.items() if (filter_string in key.lower()) or (filter_string in value.lower())]
         return dict(l[0:page_count])
     else:
         return dict(data.items()[0:page_count])
 
 def __gene_set_response(request, gts, gt):
     if gt is None:
-        res=__gene_set_filter_response_dict(request, gts.tDesc)
-        return Response(res) 
-        
+        res = __gene_set_filter_response_dict(request, gts.tDesc)
+        return Response(res)
+
     if str(gt) not in gts.tDesc.keys():
         return Response({})
-    gl=gts.t2G[gt].keys()
+    gl = gts.t2G[gt].keys()
     if not gl:
         return Response({})
     return Response({"gene_count":len(gl)})
 
 
 @api_view(['GET'])
-def gene_set_main_list(request,gene_set=None):
-    return __gene_set_response(request,settings.GENE_SETS_MAIN,gene_set)
-        
+def gene_set_main_list(request, gene_set = None):
+    return __gene_set_response(request, settings.GENE_SETS_MAIN, gene_set)
+
 
 @api_view(['GET'])
-def gene_set_go_list(request,gene_set=None):
-    return __gene_set_response(request,settings.GENE_SETS_GO,gene_set)
+def gene_set_go_list(request, gene_set = None):
+    return __gene_set_response(request, settings.GENE_SETS_GO, gene_set)
 
 @api_view(['GET'])
-def gene_set_disease_list(request,gene_set=None):
-    return __gene_set_response(request,settings.GENE_SETS_DISEASE,gene_set)
+def gene_set_disease_list(request, gene_set = None):
+    return __gene_set_response(request, settings.GENE_SETS_DISEASE, gene_set)
 
 @api_view(['GET'])
-def gene_set_denovo_list(request,denovo_study,gene_set=None):
-    gts = get_gene_sets_symNS('denovo',str(denovo_study))
+def gene_set_denovo_list(request, denovo_study, gene_set = None):
+    gts = get_gene_sets_symNS('denovo', str(denovo_study))
     return __gene_set_response(request, gts, gene_set)
 
 @api_view(['GET'])
-def gene_list(request,page_count=30):
-    gl=settings.GENE_SYMS_LIST
+def gene_list(request, page_count = 30):
+    gl = settings.GENE_SYMS_LIST
 
-    query_params=request.QUERY_PARAMS
-    
+    query_params = request.QUERY_PARAMS
+
     if query_params.has_key('filter'):
-        start_string=query_params['filter']
-        gl=[g for g in gl if g.startswith(start_string)]
-        
+        start_string = query_params['filter']
+        gl = [g for g in gl if g.startswith(start_string)]
+
     page_count = __get_page_count(query_params, page_count)
-        
+
     return Response(gl[:page_count])
 
 
-from django.http import StreamingHttpResponse,QueryDict
+from django.http import StreamingHttpResponse, QueryDict
 
 def prepare_query_dict(data):
-    res=[]
-    for (key,value) in data.items():
-        key=str(key)
-        if isinstance(value,list):
-            value=[str(s) for s in value]
+    res = []
+    for (key, value) in data.items():
+        key = str(key)
+        if isinstance(value, list):
+            value = [str(s) for s in value]
         else:
-            value=str(value)
-        res.append((key,value))
+            value = str(value)
+        res.append((key, value))
     return dict(res)
 
 @api_view(['POST'])
-@parser_classes([JSONParser,FormParser])
+@parser_classes([JSONParser, FormParser])
 def query_variants(request):
     """
-Performs a query to DAE. The body of the request should be JSON formatted object containing
-all the parameters for the query. 
+Performs a query to DAE. The body of the request should be JSON formatted object
+containing all the parameters for the query.
 
 Example JSON object describing the query is following:
 
@@ -183,7 +183,8 @@ The expected fields are:
 
 
 * "geneSet" --- contains gene set name (one of 'main','GO' or 'disease')
-* "geneTerm" --- contains gene set term. Example:  'GO:2001293' (from GO), 'mPFC_maternal' (from main).
+* "geneTerm" --- contains gene set term. Example:  'GO:2001293' (from GO),
+'mPFC_maternal' (from main).
 
 * "geneRegion" --- gene region to filter (just a text field).
 
@@ -195,31 +196,39 @@ The expected fields are:
 * "ultraRareOnly" --- True of False
 * "minAltFreqPrcnt" --- minimal frequency
 * "maxAltFreqPrcnt" --- maximum frequency
- 
+
     """
-    
+
     if request.method == 'OPTIONS':
         print "get_variants_csv: OPTIONS"
-        
-        
+
+
         return Response()
 
-    
-    data=request.DATA
-    if isinstance(data,QueryDict):
-        data=prepare_query_dict(data)
-        
-    vsl=dae_query_variants(data)
+
+    data = request.DATA
+    if isinstance(data, QueryDict):
+        data = prepare_query_dict(data)
+
+    vsl = dae_query_variants(data)
     print "query_variants: result ready; sending..."
-    
-    generator=generate_response(itertools.imap(augmentAVar,itertools.chain(*vsl)),
-                                      ['effectType', 'effectDetails', 'all.altFreq','all.nAltAlls','all.nParCalled', '_par_races_', '_ch_prof_'],sep=',')
-    response = StreamingHttpResponse(generator, mimetype='text/csv')
+
+    generator = generate_response(itertools.imap(augmentAVar, itertools.chain(*vsl)),
+                                ['effectType',
+                                 'effectDetails',
+                                 'all.altFreq',
+                                 'all.nAltAlls',
+                                 'all.nParCalled',
+                                 '_par_races_',
+                                 '_ch_prof_'],
+                                sep = ',')
+
+    response = StreamingHttpResponse(generator, mimetype = 'text/csv')
     response['Content-Disposition'] = 'attachment; filename=unruly.csv'
     response['Expires'] = '0'
-    
+
     #     response['Access-Control-Allow-Origin'] = '*'
-    
+
     #     _safeVs(response,itertools.imap(augmentAVar,itertools.chain(*vsl)),
     #                     ['effectType', 'effectDetails', 'all.altFreq','all.nAltAlls','all.nParCalled', '_par_races_', '_ch_prof_'],sep=",")
     return response
