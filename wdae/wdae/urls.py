e62187ae08cac3b3e91d923ba8aba2ed7f368c96
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2013-11-04 14:27:27 +0200

    static files moved into separete django application to map them to root URL...

--- wdae/wdae/urls.py
+++ wdae/wdae/urls.py
@@ -1,19 +1,18 @@
 from django.conf.urls import patterns, url, include
 
 
-#import api.views
+# import api.views
 # router = routers.DefaultRouter()
 # router.register(r'users', quickstart.views.UserViewSet)
 # router.register(r'groups', quickstart.views.GroupViewSet)
 
 
 # API v1
-#router.register(r'studies', api.views.hello_world)
+# router.register(r'studies', api.views.hello_world)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browseable API.
 urlpatterns = patterns('',
-#     url(r'^', include(router.urls)),
-    url(r'^$','variants.views.index'),
-    url(r'^api/',include('api.urls')),
+    url(r'^$', 'variants.views.index'),
+    url(r'^api/', include('api.urls')),
 )
