8d795408d76372ae4331be4245c2558ba302380c
Ivan Iossifov <iossifov@cshl.edu>
2013-11-14 23:54:13 -0500

    Removed wvdb, renamed manage.py to manageWDAE.py and made few final fixes before deploying to autims.

--- DAE/tests/inChTest.py
+++ DAE/tests/inChTest.py
@@ -1,27 +1,19 @@
 #!/bin/env python
 
 from DAE import *
-import sys
 
-exitCode=0
 
-if len(vDB.studies)==0:
-    exitCode=1
-
-for st in vDB.studies.values():
+for stN in vDB.get_study_names():
+    st = vDB.get_study(stN)
     if not st.has_denovo:
         continue
     try:
         vs = [v for v in vDB.get_study(st.name).get_denovo_variants() if v.inChS!=v.atts['inChild'] ]
     except:
-        print st.name, "FAIL"
-	exitCode=2 
+        print st.name, "FAIL" 
         continue
          
     if vs:
         print st.name, len(vs)
         for v in vs:
-            print "\t", v.familyId,v.inChS,v.atts['inChild']
-	exitCode=3
-
-sys.exit(exitCode)
+            print "\t", v.familyId,v.inChS,v.atts['inChild'] 
--- wvdb/wvdb/StringBuilder.py
+++
@@ -1,6 +0,0 @@
-# A string builder class
-# features:
-# keeps trakc of indent level
-
-class StringBuilder:
-    pass
