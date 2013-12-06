c7fd8dd9b5ca53aef73f424387ed3c8698efd413
Evgeniy Yanev <eyanev@setelis.com>
2013-12-06 14:14:58 +0200

    html files location change

--- wdae/wdae/urls.py
+++ wdae/wdae/urls.py
@@ -15,6 +15,5 @@ from django.conf.urls import patterns, url, include
 urlpatterns = patterns('',
     url(r'^$', 'variants.views.index'),
     url(r'^variant_reports$', 'variants.views.variant_reports'),
-    url(r'^app$', 'variants.views.app'),
     url(r'^api/', include('api.urls')),
 )
