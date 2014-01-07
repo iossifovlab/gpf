0dcdc2635f27f5bf815010e436e7c746f6a123a2
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2014-01-07 12:24:25 +0200

    several new integration tests for get_variants added...

--- DAE/tests/get_variants_tests.py
+++ DAE/tests/get_variants_tests.py
@@ -13,17 +13,19 @@ class GetVariantsTests(unittest.TestCase):
         return do_query_variants(args_dict)
 
     def compare_variants_to_data_file(self, generator, data_file):
-        fail = False
         count = 0
+        mismatch = 0
         for v in generator:
             vline = join_line(v, '\t')
             dline = data_file.readline()
             if vline != dline:
-                fail = True
-                logger.error('lines %d mismatched\ndl:<|%s|>\nvl:<|%s|>', count, dline, vline)
+                mismatch += 1
+                if mismatch < 10:
+                    logger.error('lines %d mismatched\ndl:<|%s|>\nvl:<|%s|>', count, dline, vline)
             count += 1
-
-        self.assertFalse(fail)
+        if mismatch > 0:
+            logger.error('number of mismatched lines: %d', mismatch)
+        self.assertEqual(0, mismatch)
 
     def assert_variants_to_data_file(self, args, filename):
         generator = self.perform_query(args)
@@ -101,3 +103,81 @@ class GetVariantsTests(unittest.TestCase):
                 '--transmittedStudy=none']
         filename = 'data/denovo_defaults_wigState333.test'
         self.assert_variants_to_data_file(args, filename)
+
+    def test_denovo_defaults_StateWE2012(self):
+        args = ['--denovoStudies=StateWE2012',
+                '--effectTypes=none',
+                '--transmittedStudy=none']
+        filename = 'data/denovo_defaults_StateWE2012.test'
+        self.assert_variants_to_data_file(args, filename)
+
+    def test_denovo_defaults_wigStateWE2012(self):
+        args = ['--denovoStudies=wigStateWE2012',
+                '--effectTypes=none',
+                '--transmittedStudy=none']
+        filename = 'data/denovo_defaults_wigStateWE2012.test'
+        self.assert_variants_to_data_file(args, filename)
+
+    def test_denovo_defaults_EichlerWE2012(self):
+        args = ['--denovoStudies=EichlerWE2012',
+                '--effectTypes=none',
+                '--transmittedStudy=none']
+        filename = 'data/denovo_defaults_EichlerWE2012.test'
+        self.assert_variants_to_data_file(args, filename)
+
+    def test_denovo_defaults_wigEichlerWE2012(self):
+        args = ['--denovoStudies=wigEichlerWE2012',
+                '--effectTypes=none',
+                '--transmittedStudy=none']
+        filename = 'data/denovo_defaults_wigEichlerWE2012.test'
+        self.assert_variants_to_data_file(args, filename)
+
+    def test_denovo_defaults_EichlerTG2012(self):
+        args = ['--denovoStudies=EichlerTG2012',
+                '--effectTypes=none',
+                '--transmittedStudy=none']
+        filename = 'data/denovo_defaults_EichlerTG2012.test'
+        self.assert_variants_to_data_file(args, filename)
+
+    def test_denovo_defaults_DalyWE2012(self):
+        args = ['--denovoStudies=DalyWE2012',
+                '--effectTypes=none',
+                '--transmittedStudy=none']
+        filename = 'data/denovo_defaults_DalyWE2012.test'
+        self.assert_variants_to_data_file(args, filename)
+
+    def test_denovo_defaults_IossifovWE2012(self):
+        args = ['--denovoStudies=IossifovWE2012',
+                '--effectTypes=none',
+                '--transmittedStudy=none']
+        filename = 'data/denovo_defaults_IossifovWE2012.test'
+        self.assert_variants_to_data_file(args, filename)
+
+    def test_denovo_defaults_KarayiorgouWE2012(self):
+        args = ['--denovoStudies=KarayiorgouWE2012',
+                '--effectTypes=none',
+                '--transmittedStudy=none']
+        filename = 'data/denovo_defaults_KarayiorgouWE2012.test'
+        self.assert_variants_to_data_file(args, filename)
+
+    def test_denovo_defaults_combSSCWE(self):
+        args = ['--denovoStudies=combSSCWE',
+                '--effectTypes=none',
+                '--transmittedStudy=none']
+        filename = 'data/denovo_defaults_combSSCWE.test'
+        self.assert_variants_to_data_file(args, filename)
+
+    def test_transmitted_with_region_w873e374s322(self):
+        args = ['--effectTypes=none',
+                '--transmittedStudy=w873e374s322',
+                '--regionS', '1:1000000-2000000']
+        filename = 'data/transmitted_with_region_w873e374s322.test'
+        self.assert_variants_to_data_file(args, filename)
+
+    def test_transmitted_with_gene_sym_list_w873e374s322(self):
+        args = ['--effectTypes=none', 
+                '--transmittedStudy=w873e374s322', 
+                '--geneSym', '"OSBPL8,DIP2C,FAM49A,AGPAT3"']
+        filename = 'data/transmitted_with_gene_sym_list_w873e374s322.test' 
+        self.assert_variants_to_data_file(args, filename)
+
