adc965f0ae81e7371206bd391e69970c9ecd77cb
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2014-09-16 11:16:58 +0300

    studies summary changed to contain additional columns...

--- wdae/api/studies.py
+++ wdae/api/studies.py
@@ -1,5 +1,5 @@
 from DAE import vDB
-
+from report_variants import build_header_summary
 
 def get_transmitted_studies_names():
     r = []
@@ -38,19 +38,67 @@ def get_denovo_studies_names():
     r = [(stN, dsc) for ord, stN, dsc in sorted(r)]
     return r
 
+def __build_studies_family_stats(fams):
+    
+    fam_buff = defaultdict(dict)
+    for study in studies:
+        for f in study.families.values():
+            for p in [f.memberInOrder[c] for c in xrange(2, len(f.memberInOrder))]:
+                if p.personId not in fam_buff[f.familyId]:
+                    fam_buff[f.familyId][p.personId] = p
+    return fam_buff
 
 
 def __build_studies_summaries(studies):
-    fams = set()
+    phenotype = set()
+    study_type = set()
+    study_year = set()
+    pmids = list()
+
+    # fams = set()
+    # prbs = set()
+    # sibs = set()
+
     has_denovo = False
     has_transmitted = False
-
+    
     for study in studies:
-        fams = fams.union(study.families.keys())
+        if study.get_attr('study.phenotype'):
+            phenotype.add(study.get_attr('study.phenotype'))
+        if study.get_attr('study.type'):
+            study_type.add(study.get_attr('study.type'))
+        if study.get_attr('study.year'):
+            study_year.add(study.get_attr('study.year'))
+        
+        # fams = fams.union(study.families.keys())
         has_denovo = has_denovo or study.has_denovo
         has_transmitted = has_transmitted or study.has_transmitted
 
-    return [len(fams), has_denovo, has_transmitted]
+    phenotype = list(phenotype)
+    phenotype.sort()
+    study_type = list(study_type)
+    study_type.sort()
+    study_year = list(study_year)
+    study_year.sort()
+
+    pub_med = ""
+    if len(studies) == 1:
+        study = studies[0]
+        if study.get_attr('study.pmid'):
+            pub_med = study.get_attr('study.pmid')
+        
+    fams_stat = build_header_summary(studies)
+
+
+    return [','.join(phenotype),
+            ','.join(study_type),
+            ','.join(study_year),
+            pub_med,
+            fams_stat[0],
+            fams_stat[2]['prb'],
+            fams_stat[2]['sib'],
+            has_denovo,
+            has_transmitted]
 
 def __build_denovo_studies_summaries():
     r = []
@@ -60,8 +108,10 @@ def __build_denovo_studies_summaries():
         ord = group.get_attr('wdae.production.order')
         if not ord:
             continue
-        studies = vDB.get_studies(','.join(group.studyNames))
-        summary = [study_group_name, group.description]
+        studies_names = ','.join(group.studyNames)
+        studies = vDB.get_studies(studies_names)
+        summary = [study_group_name,
+                   "%s (%s)" % (group.description, studies_names)]
         summary.extend(__build_studies_summaries(studies))
         
         r.append((int(ord), summary))
@@ -106,10 +156,16 @@ def get_studies_summaries():
     summaries = __build_denovo_studies_summaries()
     summaries.extend(__build_transmitted_studies_summaries())
 
-    return {"columns": ["Study Name",
-                        "Description",
-                        "Families",
-                        "Denovo",
-                        "Transmitted"],
+    return {"columns": ["study name",
+                        "description",
+                        "phenotype",
+                        "study type",
+                        "study year",
+                        "PubMed",
+                        "families",
+                        "number of probands",
+                        "number of siblings",
+                        "denovo",
+                        "transmitted"],
             "summaries": summaries}
 
