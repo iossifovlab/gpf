a0c1a01b6f97c6b87e4460643a97317359d93d91
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2014-09-25 15:19:26 +0300

    first simple selenium tests implemented...

--- wdae/functional_tests/tests/test_simple_check.py
+++ wdae/functional_tests/tests/test_simple_check.py
@@ -3,12 +3,16 @@ from .base import FunctionalTest
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 import unittest
 
 
 class NewVisitorTest(FunctionalTest):
 
-    def test_can_start_a_list_and_retrive_it_later(self):
+    def test_landing_page(self):
 
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -17,59 +21,20 @@ class NewVisitorTest(FunctionalTest):
         # She notices the page title and header mention to-do lists
         self.assertIn('Seqpip', self.browser.title)
 
-        # # She is invited to enter a to-do item straight away
-        # inputbox = self.get_item_input_box()
-        # self.assertEqual(inputbox.get_attribute('placeholder'),
-        #                  'Enter a to-do item')
+        title_elem = self.browser.find_element_by_css_selector(
+            "#pageContent > div.container.container-form > h1")
+        self.assertIn("Get Variants", title_elem.text.decode())
 
-        # # She types 'Buy peacock feathers' into a text box (Edith's hobby
-        # # is tying fly-fishing lures)
-        # inputbox.send_keys('Buy peacock feathers')
-
-        # # When she hits enter, the page updates, and now the page lists
-        # # '1: Buy peacock feathers' as an item in a to-do list
-        # inputbox.send_keys(Keys.ENTER)
-        # edith_list_url = self.browser.current_url
-        # self.assertRegexpMatches(edith_list_url, '/lists/.+')
-        # self.check_for_row_in_list_table('1: Buy peacock feathers')
-
-        # # There is still a text box inviting her to add another item. She
-        # # enters 'Use peacock feathers to make a fly' (Edith is very methodical)
-        # inputbox = self.get_item_input_box()
-        # inputbox.send_keys('Use peacock feathers to make a fly')
-        # inputbox.send_keys(Keys.ENTER)
-
-        # # The page updates again, and now shows both items on her list
-        # self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-        # self.check_for_row_in_list_table('1: Buy peacock feathers')
-
-        # # Now a new user, Francis, comes along to the site
-        # self.browser.quit()
-
-        # ## We use a new browser session to make sure that no information
-        # ## of Edith's is comming through from cookies etc...
-        # self.browser = webdriver.Firefox()
-
-        # # Francis visits the home page. There is no sign of Edith's list
-        # self.browser.get(self.server_url)
-        # page_text = self.browser.find_element_by_tag_name('body').text
-        # self.assertNotIn('Buy peacock feathers', page_text)
-        # self.assertNotIn('make a fly',page_text)
-
-        # # Francis starts a new list by entering a new item. He
-        # # is less interesting than Edith
-        # inputbox = self.get_item_input_box()
-        # inputbox.send_keys('Buy milk')
-        # inputbox.send_keys(Keys.ENTER)
-
-        # # Francis gets his own unique URL
-        # francis_list_url = self.browser.current_url
-        # self.assertRegexpMatches(francis_list_url, '/lists/.+')
-        # self.assertNotEqual(francis_list_url, edith_list_url)
-
-        # # Again there is no trace of Edith's list
-        # page_text = self.browser.find_element_by_tag_name('body').text
-        # self.assertNotIn('Buy peacock feathers', page_text)
-        # self.assertIn('Buy milk', page_text)
-
-        # # Satisfied, she goes back to sleep
+    def test_study_summaries_page(self):
+        self.browser.get(self.server_url)
+        summaries_button_elem = self.browser.find_element_by_css_selector(
+            "#summaries > a")
+        summaries_button_elem.click()
+        
+        try:
+            element = WebDriverWait(self.browser, 10).until(
+                EC.presence_of_element_located((By.ID, "summariesTable"))
+            )
+        finally:
+            pass
+        
