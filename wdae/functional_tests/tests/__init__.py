771cb5c14c549bde4a40b67dc134c15e2fcd1b64
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2014-09-25 14:50:06 +0300

    first functional test started...

---
+++ wdae/functional_tests/tests/__init__.py
@@ -0,0 +1,3 @@
+from .test_simple_check import NewVisitorTest
+# from .test_layout_and_styling import LayoutAndStylingTest
+# from .test_list_item_validation import ItemValidationTest
---
+++ wdae/functional_tests/tests/base.py
@@ -0,0 +1,35 @@
+from django.test import LiveServerTestCase
+
+from selenium import webdriver
+import sys
+
+
+class FunctionalTest(LiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        LiveServerTestCase.setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            LiveServerTestCase.tearDownClass()
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text.decode() for row in rows])
+
+    def get_item_input_box(self):
+        return self.browser.find_element_by_id('id_text')
