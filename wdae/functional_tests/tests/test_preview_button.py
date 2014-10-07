c75b69eeaaeb02a9f8b9463618d06187a86daf88
Alexander Popov <apopov@setelis.com>
2014-10-07 14:22:10 +0300

    fixed some bugs with the tests

--- wdae/functional_tests/tests/test_preview_button.py
+++ wdae/functional_tests/tests/test_preview_button.py
@@ -32,8 +32,11 @@ class CheckPreviewTest(FunctionalTest):
 	
 	def random_families(self):
 		
+		random_choice = random.choice(["allFamiliesRadio","familyAdvancedRadio"])
+		print "Random Families : " , random_choice
+		
 		families_radio_button = self.browser.find_element_by_id(
-			random.choice(["allFamiliesRadio","familyAdvancedRadio"]))
+			random_choice)
 		families_radio_button.click()
 		
 		if families_radio_button.get_attribute("value") == "advanced" :
@@ -47,6 +50,8 @@ class CheckPreviewTest(FunctionalTest):
 			self.browser.find_element_by_id("effectType"))
 		effect_type_option.select_by_visible_text(
 			random.choice(effect_type_option.options).text)
+		
+		print "Random effect type option : " , effect_type_option.first_selected_option.text
 		time.sleep(2)
 	
 	def random_variant_types_option(self):
@@ -55,6 +60,7 @@ class CheckPreviewTest(FunctionalTest):
 			self.browser.find_element_by_id("variants"))
 		variant_types_option.select_by_visible_text(
 			random.choice(variant_types_option.options).text)
+		print "Random variant type option : " , variant_types_option.first_selected_option.text
 		time.sleep(2)
 	
 	def random_in_child_option(self):
@@ -63,17 +69,24 @@ class CheckPreviewTest(FunctionalTest):
 			self.browser.find_element_by_id("inChild"))
 		inChildOption.select_by_visible_text(
 			random.choice(inChildOption.options).text)
+		print "Random in child option : " , inChildOption.first_selected_option.text
 		time.sleep(2)
 	
 	def random_rare_radio_button_max(self):
 		
-		max_inputbox = lambda: self.browser.find_element_by_id("max")
-		if max_inputbox().is_displayed():
+		try:
+			element = WebDriverWait(self.browser, 10).until(
+				EC.presence_of_element_located(
+					(By.NAME,"popFrequencyMax")))
+		finally: 
+			pass
+		max_inputbox = self.browser.find_element_by_name("popFrequencyMax")
+		if max_inputbox.is_displayed():
 			random_max_percentages = str(
 				round(random.uniform(0,100),2))
 			print "Random max percentages " + random_max_percentages
-			max_inputbox().clear()
-			max_inputbox().send_keys(random_max_percentages)
+			max_inputbox.clear()
+			max_inputbox.send_keys(random_max_percentages)
 			time.sleep(2)
 				
 	def random_interval_max_min(self):
@@ -102,14 +115,18 @@ class CheckPreviewTest(FunctionalTest):
 		select_random_rarity_option = self.browser.find_element_by_xpath(
 			"//div[@id='rarity']/div/input[" + random_integer + "]")
 		
+		print "Random rarity radio buttons : " , select_random_rarity_option.get_attribute("value")
+		
 		if select_random_rarity_option.is_displayed():
 			select_random_rarity_option.click()
 		time.sleep(2)
 		
 		if random_integer == "3":
+			time.sleep(3)
 			self.random_rare_radio_button_max()
 				
 		if random_integer == "4":
+			time.sleep(3)
 			self.random_interval_max_min()
 
 		time.sleep(2)
@@ -120,6 +137,7 @@ class CheckPreviewTest(FunctionalTest):
 			self.browser.find_element_by_id("transmittedStudies"))
 		transmitted_studies.select_by_visible_text(
 			random.choice(transmitted_studies.options).text)
+		print "Random transmitted studies : " , transmitted_studies.first_selected_option.text
 		
 		rarity_div = self.browser.find_element_by_id("rarity")
 	
@@ -134,6 +152,9 @@ class CheckPreviewTest(FunctionalTest):
 			self.browser.find_element_by_id("denovoStudies"))
 		denovo_studies.select_by_visible_text(
 			random.choice(denovo_studies.options).text)
+		
+		print "Random Denovo Studies : " , denovo_studies.first_selected_option.text
+		
 		time.sleep(2)
 	
 	def random_denovo_gene_set(self):
@@ -142,6 +163,7 @@ class CheckPreviewTest(FunctionalTest):
 			self.browser.find_element_by_id("denovoStudiesInGeneSet"))
 		denovo_gene_set_option.select_by_visible_text(
 			random.choice(denovo_gene_set_option.options).text)
+		print "Random denovo gene set : " , denovo_gene_set_option.first_selected_option.text
 		time.sleep(2)
 	
 	def random_gene_set_main(self):
@@ -150,7 +172,7 @@ class CheckPreviewTest(FunctionalTest):
 			self.browser.find_element_by_xpath("//select[@id='geneSet']"))
 		gene_set_main_option.select_by_visible_text(
 			random.choice(gene_set_main_option.options).text)
-		print gene_set_main_option.first_selected_option.text
+		print "Random gene set main : " , gene_set_main_option.first_selected_option.text
 		if gene_set_main_option.first_selected_option.text == "Denovo":
 			self.random_denovo_gene_set()
 		time.sleep(2)
@@ -160,7 +182,9 @@ class CheckPreviewTest(FunctionalTest):
 		gene_set_value_option =self.browser.find_element_by_xpath(
 			"//div[@id='preloadedBtn']/button")
 		gene_set_value_option.click()
+		
 		time.sleep(5)
+		"""
 		dropdown_menu = self.browser.find_elements_by_xpath(
 			"//ul[@id='ui-id-1']/li")
 		
@@ -172,9 +196,16 @@ class CheckPreviewTest(FunctionalTest):
 			"//ul[@id='ui-id-1']/li[" + 
 			random_value_from_dropdown_menu + "]")
 		
-		#wait = WebDriverWait(self.browser, 10)
-		#element = wait.until(lambda s: CheckPreviewTest.is_element_stale(self,random_element))
 		
+		try:
+			element = WebDriverWait(self.browser, 10).until(
+				EC.presence_of_element_located((By.XPATH, 
+					"//ul[@id='ui-id-1']/li[" + 
+			random_value_from_dropdown_menu + "]" + "/a"))
+			)
+		finally:
+			pass
+		"""
 		"""try:
 			element = WebDriverWait(self.browser, 30).until(
 				        EC.element_to_be_clickable(
@@ -186,7 +217,40 @@ class CheckPreviewTest(FunctionalTest):
 			pass
 		"""
 		 
+		#random_element().click()
+		
+		try:
+			element = WebDriverWait(self.browser, 10).until(
+				EC.presence_of_element_located(
+					(By.CSS_SELECTOR,".ui-autocomplete")))
+		finally:
+			pass
+		
+		dropdown_menu = self.browser.find_elements_by_css_selector(
+			"ul.ui-autocomplete > li")
+		print "dropdown_menu len" , len(dropdown_menu)
+		
+		random_value_from_dropdown_menu = str(random.randint(
+			1,len(dropdown_menu)))
+		
+		"""try:
+			random_element = WebDriverWait(self.browser, 10).until(
+				EC.element_to_be_clickable((
+					By.CSS_SELECTOR,
+					"ul.ui-autocomplete > li:nth-child(" + random_value_from_dropdown_menu + ")"))
+				)
+		finally:
+			pass
+		"""
+		
+		print "random_value_from_dropdown_menu " , random_value_from_dropdown_menu
+		random_element =lambda: self.browser.find_element_by_css_selector(
+			"ul.ui-autocomplete > li:nth-child(" + random_value_from_dropdown_menu + ")")
+		
+		print "Random gene set value : " , random_element().text
+		
 		random_element().click()
+		
 		time.sleep(2)
 		
 	def random_gene_sets_radio_buttons(self):
@@ -198,8 +262,11 @@ class CheckPreviewTest(FunctionalTest):
 		
 		time.sleep(5)
 		
+		random_choice = random.choice(["allGenesRadio","geneSetsRadio"])
+		print "Random genes : " , random_choice
+		
 		genes_all_option = self.browser.find_element_by_id(
-			random.choice(["allGenesRadio","geneSetsRadio"]))
+			random_choice)
 			#random.choice(["geneSetsRadio"]))
 		genes_all_option.click()
 		
@@ -214,6 +281,8 @@ class CheckPreviewTest(FunctionalTest):
 		race_option = Select(self.browser.find_element_by_id("race"))
 		race_option.select_by_visible_text(
 			random.choice(race_option.options).text)
+		
+		print "Random race option : " ,race_option.first_selected_option.text
 		time.sleep(2)
 		
 	def random_verbal_iq_low(self):
@@ -238,6 +307,7 @@ class CheckPreviewTest(FunctionalTest):
 		quad_trio_option = Select(self.browser.find_element_by_id("quad-trio"))
 		quad_trio_option.select_by_visible_text(
 			random.choice(quad_trio_option.options).text)
+		print "Random quad trio : " , quad_trio_option.first_selected_option.text
 		time.sleep(2)
 		
 	def random_proband_gender(self):
@@ -246,6 +316,7 @@ class CheckPreviewTest(FunctionalTest):
 			self.browser.find_element_by_id("probandGender"))
 		proband_gender_option.select_by_visible_text(
 			random.choice(proband_gender_option.options).text)
+		print "Random proband gender : ", proband_gender_option.first_selected_option.text
 		time.sleep(2)
 		
 	def random_sibling_gender(self):
@@ -254,6 +325,7 @@ class CheckPreviewTest(FunctionalTest):
 			self.browser.find_element_by_id("siblingGender"))
 		sibling_gender_option.select_by_visible_text(
 			random.choice(sibling_gender_option.options).text)
+		print "Random sibling gender : " , sibling_gender_option.first_selected_option.text
 		time.sleep(2)
 	
 	def random_family_advanced_options(self):
@@ -322,7 +394,7 @@ class CheckPreviewTest(FunctionalTest):
 		]
 		
 		for i in range(0, number_of_repetitions):
-			
+			print "Test " ,i+1 , " ---------------------------------------"
 			random_integer = random.randint(1,len(key_array))
 			print "Random Integer = " , random_integer
 			variations_of_functions = random.sample(key_array, random_integer)
@@ -331,6 +403,7 @@ class CheckPreviewTest(FunctionalTest):
 				functions_map[variations_of_functions[j]](self)
 			print "The function is = click_the_preview_button"
 			self.click_the_preview_button()
+			
 	
 			
 		
@@ -338,12 +411,12 @@ class CheckPreviewTest(FunctionalTest):
 	def test_preview_button_multiple_times(self):
 		
 		self.browser.get(self.server_url)
-		self.multiple_tests_not_random(5)
+		self.multiple_tests_not_random(0)
 		
 	def test_preview_button_random_clicks(self):
 		
 		self.browser.get(self.server_url)
-		self.multiple_tests_random(5)
+		self.multiple_tests_random(1000)
 		
 
 			
