e9917202ade87778b4e7a0f38a1f953a212466f2
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2014-10-09 08:38:19 +0300

    test case for bad region query added...

--- wdae/api/tests/query_variants_tests.py
+++ wdae/api/tests/query_variants_tests.py
@@ -10,7 +10,7 @@ import itertools
 
 logger = logging.getLogger(__name__)
 
-class IvanchoSubmittedQueryTests(unittest.TestCase):
+class IvanchoSubmittedDoubleGenesQueryTests(unittest.TestCase):
     TEST_DATA = {'geneRegionType': 'on',
                  'familyIds': '',
                  'genes': 'Gene Symbols',
@@ -40,3 +40,36 @@ class IvanchoSubmittedQueryTests(unittest.TestCase):
             print(v[8], gl, gl.count('FMR1'))
             self.assertEqual(1, gl.count('FMR1'))
 
+
+class AlexPopovSubmittedBadRegionQueryTests(unittest.TestCase):
+    TEST_DATA = {'geneRegionType': 'on',
+                 'familyIds': '',
+                 'genes': 'Gene Sets',
+                 'families': 'all',
+                 'geneSyms': '',
+                 'familyVerbalIqLo': '',
+                 'geneTerm': 'ANATO',
+                 'geneStudy': 'allWEAndTG',
+                 'inChild': 'All',
+                 'rarity': 'ultraRare',
+                 'geneRegion': '',
+                 'effectTypes': 'LGDs',
+                 'familySibGender': 'All',
+                 'familyPrbGender': 'All',
+                 'geneSet': 'domain',
+                 'variantTypes': 'All',
+                 'familyQuadTrio': 'All',
+                 'transmittedStudies': 'w1202s766e611',
+                 'familyVerbalIqHi': '',
+                 'familyRace': 'All',
+                 'denovoStudies': 'allWEandTG'}
+
+    def test_bad_region_exception(self):
+        
+        vs = do_query_variants(self.TEST_DATA)
+        
+        vs.next()
+
+        for v in vs:
+            print v
+            
