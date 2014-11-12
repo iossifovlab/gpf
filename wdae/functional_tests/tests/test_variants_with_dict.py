ee8666f9dc95e5b938d50716db5b97a7f243ee39
Alexander Popov <apopov@setelis.com>
2014-11-12 19:58:34 +0200

    pep8 styling and download button

--- wdae/functional_tests/tests/test_variants_with_dict.py
+++ wdae/functional_tests/tests/test_variants_with_dict.py
@@ -23,13 +23,35 @@ import filecmp
 class VariantsTest(FunctionalTest,base_select):
 	
 	def get_dictionary(self):
-		return {'geneRegionType': 'on', 'familyVerbalIqLo': '45', 'geneTerm': 'BIOCARTA_BCELLSURVIVAL_PATHWAY', 'inChild': 'prb', 'popFrequencyMax': '14.31', 'familyPrbGender': 'All', 'transmittedStudies': 'w1202s766e611', 'popFrequencyMin': '12.34', 'geneSet': 'MSigDB.curated', 'families': 'advanced', 'familyIds': '', 'familyQuadTrio': 'All', 'genes': 'Gene Sets', 'effectTypes': 'tRNA:ANTICODON', 'familySibGender': 'All', 'variantTypes': 'del', 'familyRace': 'All', 'denovoStudies': 'sscPublishedWE', 'geneSyms': '', 'geneStudy': 'allWEAndTG', 'rarity': 'interval', 'familyVerbalIqHi': '145', 'geneRegion': ''}
+		return {'geneRegionType': 'on',
+			'familyVerbalIqLo': '45',
+			'geneTerm': 'BIOCARTA_BCELLSURVIVAL_PATHWAY',
+			'inChild': 'prb', 'popFrequencyMax': '14.31', 
+			'familyPrbGender': 'All', 
+			'transmittedStudies': 'w1202s766e611',
+			'popFrequencyMin': '12.34',
+			'geneSet': 'MSigDB.curated', 
+			'families': 'advanced', 
+			'familyIds': '',
+			'familyQuadTrio': 'All',
+			'genes': 'Gene Sets',
+			'effectTypes': 'tRNA:ANTICODON', 
+			'familySibGender': 'All', 
+			'variantTypes': 'del',
+			'familyRace': 'All',
+			'denovoStudies': 'sscPublishedWE', 
+			'geneSyms': '', 
+			'geneStudy': 'allWEAndTG',
+			'rarity': 'interval', 
+			'familyVerbalIqHi': '145', 
+			'geneRegion': ''}
 
 
 
 	def select_gene_set_main(self, data):
 		
-		base_select(self).select_method_by_value("geneSet",data['geneSet'])
+		base_select(self).select_method_by_value(
+			"geneSet",data['geneSet'])
 		
 	def select_gene_set_value(self,data):
 		
@@ -45,7 +67,10 @@ class VariantsTest(FunctionalTest,base_select):
 		finally:
 			pass
 		selected_element =lambda: self.browser.find_element_by_xpath(
-				"//ul[@class='ui-autocomplete ui-front ui-menu ui-widget ui-widget-content ui-corner-all']/li/a[contains(text(),'" + data['geneTerm'] +"')]"
+				"//ul[@class='ui-autocomplete " + 
+				"ui-front ui-menu ui-widget ui-widget-content "+
+				"ui-corner-all']/li/a[contains(text(),'"+
+				data['geneTerm'] +"')]"
 			)
 		
 		print "Random gene set value : " , selected_element().text
@@ -56,7 +81,8 @@ class VariantsTest(FunctionalTest,base_select):
 	
 	def select_denovo_studies(self, data):
 		
-		base_select(self).select_method("denovoStudies",data['denovoStudies'])
+		base_select(self).select_method(
+			"denovoStudies",data['denovoStudies'])
 	
 	def select_in_child(self, data):
 		
@@ -64,11 +90,13 @@ class VariantsTest(FunctionalTest,base_select):
 		
 	def select_effect_type(self,data):
 		
-		base_select(self).select_method("effectType",data['effectTypes'])
+		base_select(self).select_method(
+			"effectType",data['effectTypes'])
 		
 	def select_variant_type(self,data):
 		
-		base_select(self).select_method("variants",data['variantTypes'])
+		base_select(self).select_method(
+			"variants",data['variantTypes'])
 		
 	def genes_radio_buttons(self,data):
 		
@@ -95,16 +123,19 @@ class VariantsTest(FunctionalTest,base_select):
 	
 	def select_transmitted_studies(self, data):
 		
-		base_select(self).select_method("transmittedStudies",data['transmittedStudies'])
+		base_select(self).select_method("transmittedStudies",
+			data['transmittedStudies'])
 		rarity_div = self.browser.find_element_by_id("rarity")
 		if rarity_div.is_displayed():
 			self.select_rarity_radio_buttons(data)
 	
 	def select_gene_symbols(self,gene_list):
 		
-		gene_syms_radio = self.browser.find_element_by_id("geneSymsRadio")
+		gene_syms_radio=self.browser.find_element_by_id(
+			"geneSymsRadio")
 		gene_syms_radio.click()
-		gene_syms_text_area = self.browser.find_element_by_id("geneSyms")
+		gene_syms_text_area =self.browser.find_element_by_id(
+			"geneSyms")
 		gene_syms_text_area.send_keys(gene_list)
 		time.sleep(2)
 		
@@ -113,7 +144,8 @@ class VariantsTest(FunctionalTest,base_select):
 		family_ids_radio = self.browser.find_element_by_id(
 			"familyIdsRadio")
 		family_ids_radio.click()
-		family_ids_text_area = self.browser.find_element_by_id("familyIds")
+		family_ids_text_area = self.browser.find_element_by_id(
+			"familyIds")
 		family_ids_text_area.send_keys(family_list)
 		time.sleep(2)
 		
@@ -123,23 +155,28 @@ class VariantsTest(FunctionalTest,base_select):
 	
 	def select_verbal_iq_low(self,data):
 		
-		base_select(self).type_method("verbalIQLow",data['familyVerbalIqLo'])
+		base_select(self).type_method("verbalIQLow",
+			data['familyVerbalIqLo'])
 		
 	def select_verbal_iq_high(self,data):
 		
-		base_select(self).type_method("verbalIQHigh",data['familyVerbalIqHi'])
+		base_select(self).type_method("verbalIQHigh",
+			data['familyVerbalIqHi'])
 		
 	def select_quad_trio(self,data):
 		
-		base_select(self).select_method("quad-trio",data['familyQuadTrio']);
+		base_select(self).select_method("quad-trio",
+			data['familyQuadTrio']);
 		
 	def select_proband_gender(self,data):
 		
-		base_select(self).select_method("probandGender",data['familyPrbGender']);
+		base_select(self).select_method("probandGender",
+			data['familyPrbGender']);
 		
 	def select_sibling_gender(self,data):
 		
-		base_select(self).select_method("siblingGender",data['familySibGender']);
+		base_select(self).select_method("siblingGender",
+			data['familySibGender']);
 		
 	def select_family_advanced_options(self,data):
 		
@@ -153,7 +190,8 @@ class VariantsTest(FunctionalTest,base_select):
 	def select_families(self,data):
 		
 		self.browser.find_element_by_xpath(
-			"//div[@class='controls form-inline']//input[@value='" + data['families'] + "']").click()
+			"//div[@class='controls form-inline']//input[@value='"+
+			data['families'] + "']").click()
 		if data['families'] == "advanced":
 			self.select_family_advanced_options(data)
 		if data['families'] == 'familyIds':
@@ -200,7 +238,10 @@ class VariantsTest(FunctionalTest,base_select):
 		table_content = []
 		temp = self.browser.find_elements_by_css_selector(
 			"table#previewTable > tbody > tr")
-		f = open('/home/alexanderpopov/Projects/SeqPipelineTesting/python/wdae/functional_tests/tests/table_results_' + str(idx) + '.txt', 'w+')
+		location = os.path.realpath(
+			os.path.join(os.getcwd(), os.path.dirname(__file__)))
+		f = open(os.path.join(
+			location, 'table_results_' + str(idx) + '.txt'),'w+')
 		for tr in temp:
 			tds=tr.find_elements_by_css_selector('td')
 			for td in tds:
@@ -215,19 +256,22 @@ class VariantsTest(FunctionalTest,base_select):
 			"div#preview > svg > svg > g")
 		temp_rect = temp.find_elements_by_css_selector("rect")
 		temp_path = temp.find_elements_by_css_selector("path")
-		f = open('/home/alexanderpopov/Projects/SeqPipelineTesting/python/wdae/functional_tests/tests/chroms_results_' + str(idx) + '.txt', 'w+')
+		location = os.path.realpath(
+			os.path.join(os.getcwd(), os.path.dirname(__file__)))
+		f = open(os.path.join(
+			location, 'chroms_results_' + str(idx) + '.txt'),'w+')
 		for elem in temp_rect:
-			f.write(str(elem.get_attribute("x")) + " / ")
-			f.write(str(elem.get_attribute("y")) +" / ")
-			f.write(str(elem.get_attribute("width"))+" / ")
-			f.write(str(elem.get_attribute("height"))+ " / ")
-			f.write(str(elem.get_attribute("style"))+ " / ")
-			f.write("\n")
+			chroms_content.append(elem.get_attribute("x"))
+			chroms_content.append(elem.get_attribute("y"))
+			chroms_content.append(elem.get_attribute("width"))
+			chroms_content.append(elem.get_attribute("height"))
+			chroms_content.append(elem.get_attribute("style"))
 		for elem in temp_path:
-			f.write(str(elem.get_attribute("d"))+" / ")
-			f.write(str(elem.get_attribute("transform"))+ " / ")
-			f.write(str(elem.get_attribute("style"))+ " / ")
-			f.write("\n")		
+			chroms_content.append(elem.get_attribute("d"))
+			chroms_content.append(elem.get_attribute("transform"))
+			chroms_content.append(elem.get_attribute("style"))
+		
+		f.write(str([str(x) for x in chroms_content]))
 		f.close()
 		
 	def wait_for_page_to_load(self):
@@ -248,13 +292,45 @@ class VariantsTest(FunctionalTest,base_select):
 		time.sleep(2)
 		self.wait_for_page_to_load()
 		
+	def compare_table_files(self):
+		
+		location = os.path.realpath(
+			os.path.join(os.getcwd(), os.path.dirname(__file__)))
+		print filecmp.cmp(
+			os.path.join(
+			location, 'table_results_0.txt') ,
+			os.path.join(
+			location, 'table_results_1.txt'))
+
+		
 	def compare_chroms_files(self):
 		
-		print filecmp.cmp('/home/alexanderpopov/Projects/SeqPipelineTesting/python/wdae/functional_tests/tests/chroms_results_0.txt' , '/home/alexanderpopov/Projects/SeqPipelineTesting/python/wdae/functional_tests/tests/chroms_results_0.txt')
+		location = os.path.realpath(
+			os.path.join(os.getcwd(), os.path.dirname(__file__)))
+		print filecmp.cmp(
+			os.path.join(
+			location, 'chroms_results_0.txt') ,
+			os.path.join(
+			location, 'chroms_results_0.txt'))	
+	
+	def download_file(self):
+		
+		"""profile = webdriver.FirefoxProfile()
+		profile.set_preference('browser.download.folderList', 2) # custom location
+		profile.set_preference('browser.download.manager.showWhenStarting', False)
+		profile.set_preference('browser.download.dir', '/tmp')
+		profile.set_preference('browser.helperApps.neverAsk.saveToDisk', 'text/csv')
+		
+		browser = webdriver.Firefox(profile)
+		browser.get(self.server_url)"""
+		
+		download_button = self.browser.find_element_by_id(
+			"submitBtn")
+		download_button.click()
+		self.assertTrue(os.path.exists("unruly.csv"))
 		
 	def dictionary_test(self,data,idx):
 		
-		self.compare_chroms_files()
 		self.genes_radio_buttons(data)
 		if data['genes'] == 'Gene Sets':
 			self.select_gene_set_main(data)
@@ -267,25 +343,30 @@ class VariantsTest(FunctionalTest,base_select):
 		self.select_variant_type(data)
 		self.select_effect_type(data)
 		self.select_families(data)
+		self.download_file()
 		self.click_the_preview_button()
 		self.get_table_content(idx)
 		self.click_the_chroms_button()
 		self.get_chroms_content(idx)
 		self.click_the_reset_button()
+		self.compare_table_files()
+		self.compare_chroms_files()
 		
 	def test_variants_with_predefined_values(self):
 		
 		self.browser.get(self.server_url)
 		data = self.get_dictionary()
-		text_file = open(
-			r'/home/alexanderpopov/Projects/SeqPipelineTesting/python/wdae/functional_tests/tests/data_dict.txt')
+		
+		location = os.path.realpath(
+			os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+		text_file = open(os.path.join(
+			location, 'data_dict.txt'),'r')
 		lines = text_file.readlines()
-		print len(lines)
+		#print len(lines)
 		for idx,line in enumerate(lines):
 			print "idx",idx
 			temp_data = ast.literal_eval(line)
 			self.dictionary_test(temp_data,idx)
 		text_file.close()
-		#self.dictionary_test(data)
-		#self.select_gene_symbols(["a","b","c"])
 		
