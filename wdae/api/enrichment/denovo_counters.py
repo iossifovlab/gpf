d8e9d69b6b6d80970e8363143f65616a58e75452
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2015-06-16 12:09:37 +0300

    small fix in filtering denovo studies for enrichment

--- wdae/api/enrichment/denovo_counters.py
+++ wdae/api/enrichment/denovo_counters.py
@@ -61,7 +61,8 @@ def collect_denovo_variants(dsts, inchild, effect, **kwargs):
 
 
 def filter_denovo_studies_by_phenotype(dsts, phenotype):
-    return [st for st in dsts if st.get_attr('study.phenotype') == phenotype]
+    return [st for st in dsts 
+            if st and st.get_attr('study.phenotype') == phenotype]
 
 
 def count_denovo_variant_events(affected_gene_syms, gene_syms):
