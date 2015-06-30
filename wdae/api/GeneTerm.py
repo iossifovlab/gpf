77c1dfcd1fc36e5a56af25b7e07d3101244e76a7
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2015-06-30 12:37:25 +0300

    84: Denovo Sets: UI for phenotypes to become 'checkboxes' an to allow combinations of gene sets formatting fixes... Task-Url: https://seqpipe.setelis.com/trac/ticket/84

--- wdae/api/GeneTerm.py
+++ wdae/api/GeneTerm.py
@@ -1,18 +1,18 @@
 
 class GeneTerm:
-        def __init__(self, geneTerms, geneSetName=None):
-                if geneSetName is None:
-                        self.tDesc = geneTerms.tDesc
-                        self.geneNS = geneTerms.geneNS
-                        self.t2G = dict([(key, dict(val))
-                                         for key, val in geneTerms.t2G.items()])
-                        self.g2T = dict([(key, dict(val))
-                                         for key, val in geneTerms.g2T.items()])
-                else:
-                        self.tDesc = dict([(geneSetName, geneTerms.tDesc[geneSetName])])
-                        self.geneNS = geneTerms.geneNS
-                        self.t2G = dict([(geneSetName, geneTerms.t2G[geneSetName])])
-                        self.g2T = dict([(key, dict([(gt, d) for (gt, d) in val.items() 
-                                                     if gt == geneSetName]))
-                                         for key, val in geneTerms.g2T.items()
-                                         if geneSetName in val])
+    def __init__(self, geneTerms, geneSetName=None):
+        if geneSetName is None:
+            self.tDesc = geneTerms.tDesc
+            self.geneNS = geneTerms.geneNS
+            self.t2G = dict([(key, dict(val))
+                             for key, val in geneTerms.t2G.items()])
+            self.g2T = dict([(key, dict(val))
+                             for key, val in geneTerms.g2T.items()])
+        else:
+            self.tDesc = dict([(geneSetName, geneTerms.tDesc[geneSetName])])
+            self.geneNS = geneTerms.geneNS
+            self.t2G = dict([(geneSetName, geneTerms.t2G[geneSetName])])
+            self.g2T = dict([(key, dict([(gt, d) for (gt, d) in val.items() 
+                                         if gt == geneSetName]))
+                             for key, val in geneTerms.g2T.items()
+                             if geneSetName in val])
