946af47f5eee9b02fdb19ec52dcfe5c9e138fee9
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2015-07-09 13:12:56 +0300

    97: Create example researchers on the stage to test with
    Task-Url: https://seqpipe.setelis.com/trac/ticket/97

---
+++ wdae/api/management/commands/exresearchers.py
@@ -0,0 +1,31 @@
+'''
+Created on Jun 3, 2015
+
+@author: lubo
+'''
+from django.core.management.base import BaseCommand, CommandError
+
+class Command(BaseCommand):
+    args = ''
+    help = 'Creates example researchers.'
+
+    def handle(self, *args, **options):
+        if(len(args) != 0):
+            raise CommandError('Unexpected argument passed!')
+        from django.contrib.auth import get_user_model
+
+        User = get_user_model()
+        u, _ = User.objects.get_or_create(email='lchorbadjiev@elsys-bg.org')
+        u.is_staff = False
+        u.is_active = True
+        u.researcher_id = '10020'
+        u.set_password('paslubo')
+        u.save()
+        
+        
+        u, _ = User.objects.get_or_create(email='ivan.iossifov@gmail.com')
+        u.is_staff = False
+        u.is_active = True
+        u.researcher_id = '10021'
+        u.set_password('pasivan')
+        u.save()
