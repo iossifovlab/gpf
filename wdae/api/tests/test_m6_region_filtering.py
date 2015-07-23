a9726a9c22c45342b181638a3e9141ef0ee8351c
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2015-07-23 11:33:46 +0300

    106: SSC: filtering by region with De Novo variants does not work workaround for variants with interval locations added... Task-Url: https://seqpipe.setelis.com/trac/ticket/106

---
+++ wdae/api/tests/test_m6_region_filtering.py
@@ -0,0 +1,67 @@
+'''
+Created on Jul 23, 2015
+
+@author: lubo
+'''
+import unittest
+from rest_framework.test import APITestCase
+from django.contrib.auth import get_user_model
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+
+
+class Test(APITestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(Test, cls).setUpClass()
+        
+        
+        User = get_user_model()
+        u = User.objects.create(email="admin@example.com",
+                                first_name="First",
+                                last_name="Last",
+                                is_staff=True,
+                                is_active=True,
+                                researcher_id="0001000")
+        u.set_password("secret")
+        u.save()
+
+        cls.user = u
+        _token = Token.objects.get_or_create(user=u)
+        cls.user.save()
+        
+    def setUp(self):
+        super(Test, self).setUpClass()
+
+        self.client.login(email='admin@example.com', password='secret')
+        token = Token.objects.get(user__email='admin@example.com')
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
+
+
+    def tearDown(self):
+        pass
+
+
+    def test_query_by_region(self):
+        region = "1:151377900-151378500"
+        data = {
+            "effectTypes": "Nonsense,Frame-shift,Splice-site",
+            "denovoStudies": "ALL SSC",
+            "transmittedStudies": "None",
+            "presentInChild": "autism only",
+            "gender": "male,female",
+            "geneRegion": region,
+        }
+
+        
+        url = '/api/ssc_query_variants_preview'
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual('2', response.data['count'])
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
