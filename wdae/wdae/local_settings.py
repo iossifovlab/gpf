b874ce0574e5afd02a790080c9f223a4083ff414
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2015-08-31 14:26:26 +0300

    disable nose testing

--- wdae/wdae/local_settings.py
+++ wdae/wdae/local_settings.py
@@ -3,11 +3,11 @@ import os
 
 settings.INSTALLED_APPS += [
     'transmitted',
-    'django_nose',
+    # 'django_nose',
 ]
 
 # Use nose to run all tests
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+# TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 # Tell nose to measure coverage on the 'foo' and 'bar' apps
 NOSE_ARGS = [
