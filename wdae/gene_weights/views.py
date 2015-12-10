be76a21eab57c3a5ce25177b9176daf9e80cda3e
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2015-12-10 14:29:53 +0200

    293: Gene Weights: experiment with implementation of weights range functions query pep8 fix... Task-Url: https://seqpipe.setelis.com/trac/ticket/293

--- wdae/gene_weights/views.py
+++ wdae/gene_weights/views.py
@@ -3,7 +3,7 @@ Created on Dec 10, 2015
 
 @author: lubo
 '''
-from rest_framework import views, status
+from rest_framework import views
 from rest_framework.response import Response
 from api.preloaded.register import get_register
 
