6ec5a6a93757de83a1acf7df8aa9a8a9282eaecb
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2016-01-04 16:53:08 +0200

    222: The region filter doesn't work for CNV
    Task-Url: https://seqpipe.setelis.com/trac/ticket/222

--- wdae/api/tests/test_cnv_region_filter.py
+++ wdae/api/tests/test_cnv_region_filter.py
@@ -57,7 +57,6 @@ class Test(APITestCase):
 
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        print(response.data)
         self.assertEqual('2', response.data['count'])
 
     def test_all_region(self):
@@ -73,8 +72,24 @@ class Test(APITestCase):
 
         response = self.client.post(url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        print(response.data)
         self.assertEqual('4', response.data['count'])
+
+    def test_all_effects_region_and_gene(self):
+        data = {
+            "geneSyms": "WDR64",
+            'geneRegion': '1:240530000-242300000',
+            "denovoStudies": "ALL SSC",
+            "transmittedStudies": "None",
+            "presentInChild": "autism only",
+            "gender": "male,female",
+        }
+
+        url = '/api/ssc_query_variants_preview'
+
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual('2', response.data['count'])
+
 
 if __name__ == "__main__":
     unittest.main()
