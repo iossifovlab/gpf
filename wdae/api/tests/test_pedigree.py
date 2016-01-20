f6713d533c152e23ce966b6c050fa40cc231ea1c
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2016-01-20 15:01:50 +0200

    find broken bestSt in denovo variants

--- wdae/api/tests/test_pedigree.py
+++ wdae/api/tests/test_pedigree.py
@@ -7,6 +7,9 @@ import unittest
 from rest_framework.test import APITestCase
 from rest_framework import status
 import json
+from query_variants import prepare_denovo_filters, get_denovo_variants
+from DAE import vDB
+from Variant import normalRefCopyNumber
 
 
 class Test(APITestCase):
@@ -76,6 +79,52 @@ class Test(APITestCase):
         self.assertEqual(2, m[2][-1])
         self.assertEqual(1, m[3][-1])
 
+    def test_all_pedigree(self):
+        data = {
+            'denovoStudies': 'ALL WHOLE EXOME',
+            'geneSyms': None,
+            'gender': 'female,male',
+            'genes': 'All',
+            'effectTypes': "frame-shift,intergenic,intron,missense,non-coding,"
+            "no-frame-shift,nonsense,splice-site,synonymous,noEnd,noStart,"
+            "3'UTR,3'UTR-intron,5'UTR,5'UTR-intron",
+            'phenoType': 'autism,congenital heart disease,'
+            'epilepsy,intelectual disability,schizophrenia,unaffected',
+            'variantTypes': 'del,ins,sub',
+            'transmittedStudies': None,
+            'studyType': 'TG,WE'}
+
+        filters = prepare_denovo_filters(data)
+        study_names = vDB.get_study_names()
+        studies = []
+        for sn in study_names:
+            st = vDB.get_study(sn)
+            if st.has_denovo:
+                studies.append((st, None))
+                print("adding study: {}".format(sn))
+
+        vs = get_denovo_variants(studies, family_filters=None, **filters)
+        for v in vs:
+            location = v.location
+            bs = v.bestSt
+            for (c, m) in enumerate(v.memberInOrder):
+                gender = m.gender
+                normal = normalRefCopyNumber(location, gender)
+                # print("variantCount: {}, {}, {}".format(
+                # location, gender, normalRefCN))
+                ref = bs[0, c]
+                # print("count: {}".format(count))
+                if bs.shape[0] == 2:
+                    alles = bs[1, c]
+                    if alles != 0:
+                        if ref == normal:
+                            print("------------------------------------------")
+                            print("Study: {}".format(v.studyName))
+                            print(v.atts)
+                            print("location: {}, gender: {}, c: {}, normal: {}"
+                                  .format(location, gender, c, normal))
+                            print("bs: {}".format(bs))
+                            print("------------------------------------------")
 
 if __name__ == "__main__":
     unittest.main()
