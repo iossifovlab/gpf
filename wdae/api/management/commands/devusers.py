fc6dbd58f8bd938dd9b117a677c6f23afcc15fb9
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2016-05-09 13:23:52 +0300

    391: Import researchers from csv file
    Task-Url: https://seqpipe.setelis.com/trac/ticket/391

--- wdae/api/management/commands/devusers.py
+++ wdae/api/management/commands/devusers.py
@@ -4,13 +4,10 @@ Created on Jun 3, 2015
 @author: lubo
 '''
 from django.core.management.base import BaseCommand, CommandError
-from django.db.utils import IntegrityError
 
-from api.models import Researcher
 
 class Command(BaseCommand):
     args = '<file> <file> ...'
-    help = 'Creates researchers from csv. Column names for the csv file - first name, last name, email and researcher_id.'
 
     def handle(self, *args, **options):
         if(len(args) != 0):
@@ -21,11 +18,10 @@ class Command(BaseCommand):
         u, _ = User.objects.get_or_create(email='admin@iossifovlab.com')
         u.is_staff = True
         u.is_active = True
-        
+
         u.set_password('secret')
         u.save()
-        
-        
+
         u, _ = User.objects.get_or_create(email='research@iossifovlab.com')
         u.is_staff = False
         u.is_active = True
---
+++ wdae/api/management/commands/import_researchers.py
@@ -0,0 +1,51 @@
+import csv
+
+from django.core.management.base import BaseCommand, CommandError
+
+from api.models import Researcher, ResearcherId
+from pprint import pprint
+
+
+class Command(BaseCommand):
+    args = '<file> <file> ...'
+    help = 'Creates researchers from csv.' \
+        'Required column names for the csv file - LastName, Email and Id.'
+
+    def handle(self, *args, **options):
+        if(len(args) < 1):
+            raise CommandError('At least one argument is required')
+        for csv_file in args:
+            try:
+                with open(csv_file, 'rb') as csvfile:
+                    resreader = csv.DictReader(csvfile)
+
+                    for res in resreader:
+                        pprint(res)
+
+                        rid = res['Id']
+                        email = res['Email']
+
+                        res_instances = Researcher.objects.filter(email=email)
+                        if len(res_instances) == 1:
+                            res_instance = res_instances[0]
+                            res_id = ResearcherId()
+                            res_id.researcher_id = rid
+                            res_id.owner = res_instance
+                            res_id.save()
+                        else:
+
+                            res_instance = Researcher()
+                            res_instance.email = res['Email']
+                            res_instance.last_name = res['LastName']
+                            res_instance.save()
+
+                            res_id = ResearcherId()
+                            res_id.researcher_id = rid
+                            res_id.owner = res_instance
+                            res_id.save()
+
+            except csv.Error:
+                raise CommandError(
+                    'There was a problem while reading "%s"' % csv_file)
+            except IOError:
+                raise CommandError('File "%s" not found' % csv_file)
