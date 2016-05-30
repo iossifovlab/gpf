7732e94253f4e2623388393b2243c9c450ad1e45
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2016-05-30 13:42:07 +0300

    409: Ensure that the sum of 'with hit' + 'without hit' matches the result from family stats
    Task-Url: https://seqpipe.setelis.com/trac/ticket/409

--- wdae/pheno/tests/test_pheno_report_with_family_stats.py
+++ wdae/pheno/tests/test_pheno_report_with_family_stats.py
@@ -20,31 +20,67 @@ class Test(APITestCase):
     def test_all_with_lgds_non_verbal_iq(self):
         url = "/api/v2/pheno_reports"
 
-        data = {u'phenoMeasure': u'non_verbal_iq',
-                u'denovoStudies': u'ALL SSC',
-                u'families': u'Advanced',
-                u'gene_set_phenotype': u'autism',
-                u'normalizedBy': u'',
-                u'familyQuadTrio': u'All',
-                u'genes': u'All',
-                u'familyPhenoMeasureMin': 8,
-                u'familyPhenoMeasureMax': 162,
-                u'familySibGender': u'All',
-                u'effectTypeGroups': u'LGDs',
-                u'familyPrbGender': u'All',
-                u'presentInParent': u'neither',
-                u'familyStudyType': u'All',
-                u'familyRace': u'All',
-                u'familyPhenoMeasure': u'non_verbal_iq'}
+        data = {
+            u'phenoMeasure': u'non_verbal_iq',
+            u'denovoStudies': u'ALL SSC',
+            u'effectTypeGroups': u'LGDs',
+            u'presentInParent': u'neither',
+        }
         response = self.client.post(url, data, format='json')
         self.assertEqual(200, response.status_code)
         result = response.data['data']
         pprint(result)
 
+        url = "/api/v2/families/counter"
+        data = {
+            'phenoMeasure': 'non_verbal_iq',
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(200, response.status_code)
+
+        family_stats = response.data
+        pprint(family_stats)
+
         with_hit_male = self.with_hit(result[0])
         without_hit_male = self.without_hit(result[0])
-        self.assertEquals(2388, 7 + with_hit_male + without_hit_male)  # 2477
+        self.assertEquals(family_stats['autism']['male'],
+                          with_hit_male + without_hit_male)
 
         with_hit_female = self.with_hit(result[1])
         without_hit_female = self.without_hit(result[1])
-        self.assertEquals(375, 1 + with_hit_female + without_hit_female)  # 390
+        self.assertEquals(family_stats['autism']['female'],
+                          with_hit_female + without_hit_female)  # 390
+
+    def test_all_with_lgds_non_head_circumference(self):
+        url = "/api/v2/pheno_reports"
+
+        data = {
+            u'phenoMeasure': u'head_circumference',
+            u'denovoStudies': u'ALL SSC',
+            u'effectTypeGroups': u'LGDs',
+            u'presentInParent': u'neither',
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(200, response.status_code)
+        result = response.data['data']
+        pprint(result)
+
+        url = "/api/v2/families/counter"
+        data = {
+            'phenoMeasure': 'head_circumference',
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(200, response.status_code)
+
+        family_stats = response.data
+        pprint(family_stats)
+
+        with_hit_male = self.with_hit(result[0])
+        without_hit_male = self.without_hit(result[0])
+        self.assertEquals(family_stats['autism']['male'],
+                          with_hit_male + without_hit_male)
+
+        with_hit_female = self.with_hit(result[1])
+        without_hit_female = self.without_hit(result[1])
+        self.assertEquals(family_stats['autism']['female'],
+                          with_hit_female + without_hit_female)  # 390
