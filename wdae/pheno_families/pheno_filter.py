ae1d212d80529f6d403a3c190b494ee01354418f
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2016-06-29 13:44:20 +0300

    Find probands that match study type filter
    Task-Url: https://app.asana.com/0/149572395865854/149572395865857

--- wdae/pheno_families/pheno_filter.py
+++ wdae/pheno_families/pheno_filter.py
@@ -4,6 +4,8 @@ Created on Jun 29, 2016
 @author: lubo
 '''
 import preloaded
+from api.default_ssc_study import get_ssc_denovo_studies, get_ssc_denovo
+from DAE import vDB
 
 
 class PhenoMeasureFilters(object):
@@ -40,11 +42,42 @@ class PhenoStudyFilter(object):
     that are not included in any of the studies.
     '''
 
+    STUDY_TYPES = set(['all', 'we', 'tg', 'cnv'])
+    STUDIES = get_ssc_denovo_studies()
+    STUDY_NAMES = set(get_ssc_denovo().split(','))
+
     def __init__(self):
         pass
 
     def get_matching_probands_by_study(self, study_name):
-        pass
+        assert study_name in self.STUDY_NAMES
+        probands = set([])
+        study = vDB.get_study(study_name)
+        for _fid, fam in study.families.items():
+            for p in fam.memberInOrder:
+                if p.role == 'prb':
+                    probands.add(p.personId)
+        return probands
+
+    def get_matching_probands_by_study_type(self, study_type):
+        assert study_type.lower() in self.STUDY_TYPES
+        probands = set([])
+        for st in self.STUDIES:
+            if st.get_attr('study.type').lower() != study_type.lower():
+                continue
+            for _fid, fam in st.families.items():
+                for p in fam.memberInOrder:
+                    if p.role == 'prb':
+                        probands.add(p.personId)
+        return probands
+
+    def _build_study_types(self):
+        stypes = set()
+        for st in self.STUDIES:
+            stypes.add(st.get_attr('study.type'))
+        self._study_types = list(stypes)
+        self._study_types.sort()
+        return self._study_types
 
 
 class PhenoRaceFilter(object):
--- wdae/pheno_families/tests/test_pheno_filter.py
+++ wdae/pheno_families/tests/test_pheno_filter.py
@@ -4,10 +4,10 @@ Created on Jun 29, 2016
 @author: lubo
 '''
 import unittest
-from pheno_families.pheno_filter import PhenoMeasureFilters
+from pheno_families.pheno_filter import PhenoMeasureFilters, PhenoStudyFilter
 
 
-class Test(unittest.TestCase):
+class PhenoMeasureFiltersTest(unittest.TestCase):
 
     def setUp(self):
         unittest.TestCase.setUp(self)
@@ -16,7 +16,6 @@ class Test(unittest.TestCase):
     def test_get_matching_probands(self):
         probands = self.pheno_filters.get_matching_probands(
             "non_verbal_iq", 9, 10)
-        print(probands)
 
         self.assertEquals(1, len(probands))
 
@@ -24,3 +23,31 @@ class Test(unittest.TestCase):
         siblings = self.pheno_filters.get_matching_siblings(
             "non_verbal_iq")
         self.assertEquals(0, len(siblings))
+
+
+class PhenoStudyFilterTest(unittest.TestCase):
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+        self.study_filters = PhenoStudyFilter()
+
+    def test_study_types(self):
+        types = self.study_filters._build_study_types()
+        self.assertEquals(set(["WE", "TG", "CNV"]),
+                          set(types))
+
+    def test_get_matching_probands_by_study_type(self):
+        probands = self.study_filters.get_matching_probands_by_study_type(
+            "CNV")
+        self.assertEquals(858, len(probands))
+
+    def test_get_matching_probands_by_bad_study_name(self):
+        self.assertRaises(
+            AssertionError,
+            self.study_filters.get_matching_probands_by_study, "ala bala")
+
+    def test_get_matching_probands_by_study_name(self):
+        probands = self.study_filters.get_matching_probands_by_study(
+            "LevyCNV2011")
+
+        self.assertEquals(858, len(probands))
