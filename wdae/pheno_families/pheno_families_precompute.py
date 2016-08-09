1fee4630307a30619e16e709a1ef921c5dc61aa0
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2016-08-09 12:07:24 +0300

    Refactor SSC Pheno Report Tool to use proband ids instead of family ids
    Task-Url: https://app.asana.com/0/149572395865854/164730697587411

--- wdae/pheno_families/pheno_families_precompute.py
+++ wdae/pheno_families/pheno_families_precompute.py
@@ -8,6 +8,7 @@ import zlib
 import precompute
 from api.default_ssc_study import get_ssc_denovo_studies
 from DAE import phDB
+import itertools
 
 
 class FamiliesPrecompute(precompute.register.Precompute):
@@ -21,11 +22,15 @@ class FamiliesPrecompute(precompute.register.Precompute):
         result = {}
         result['probands'] = zlib.compress(cPickle.dumps(self._probands))
         result['siblings'] = zlib.compress(cPickle.dumps(self._siblings))
+        result['probands_gender'] = \
+            zlib.compress(cPickle.dumps(self._probands_gender))
         result['races'] = zlib.compress(cPickle.dumps(self._races))
         return result
 
     def deserialize(self, data):
         self._probands = cPickle.loads(zlib.decompress(data['probands']))
+        self._probands_gender = \
+            cPickle.loads(zlib.decompress(data['probands_gender']))
         self._siblings = cPickle.loads(zlib.decompress(data['siblings']))
         self._races = cPickle.loads(zlib.decompress(data['races']))
 
@@ -48,12 +53,13 @@ class FamiliesPrecompute(precompute.register.Precompute):
                          phDB.get_variable('mocuv.race_parents'),
                          phDB.get_variable('focuv.race_parents'))])
 
-    def precompute(self):
+    def _precompute_races(self):
         self._races = dict([(r, set()) for r in self.get_races()])
         parent_races = self._parents_race()
         for fid in parent_races.keys():
             self._races[parent_races[fid]].add(fid)
 
+    def _precompute_children_gender(self):
         self._siblings = {'M': set(),
                           'F': set()}
         self._probands = {'M': set(),
@@ -73,11 +79,27 @@ class FamiliesPrecompute(precompute.register.Precompute):
         print("siblings with both genders: {}".format(
             self._siblings['M'] & self._siblings['F']))
 
+    def _precompute_probands_gender(self):
+        self._probands_gender = \
+            zip(itertools.cycle(['M']),
+                self.probands('M'))
+        self._probands_gender.extend(
+            zip(itertools.cycle(['F']),
+                self.probands('F')))
+
+    def precompute(self):
+        self._precompute_races()
+        self._precompute_children_gender()
+        self._precompute_probands_gender()
+
     def probands(self, gender):
         return self._probands[gender]
 
     def siblings(self, gender):
         return self._siblings[gender]
+
+    def probands_gender(self):
+        return self._probands_gender
 
     def race(self, race):
         assert self._races is not None
