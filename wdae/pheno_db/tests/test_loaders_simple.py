26654e980ed19041e8b01280df7038dac7cba991
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2016-08-12 11:28:16 +0300

    Experiment to find pheno measure data in v14 EVERYTHING.csv
    Task-Url: https://app.asana.com/0/149572395865854/156310682165267

--- wdae/pheno_db/tests/test_loaders_simple.py
+++ wdae/pheno_db/tests/test_loaders_simple.py
@@ -60,6 +60,93 @@ class V14LoaderTest(unittest.TestCase):
         self.assertEquals(len(df1), len(df))
         self.assertTrue((df.variableCategory == df1.variableCategory).all())
 
+    def test_check_cdv_measures_in_everything(self):
+        cdv = self.loader.load_cdv()
+        df = self.loader.load_everything()
+
+        print(cdv.columns)
+
+        print(len(df.columns))
+        print(df.columns)
+        print(len(df.columns))
+
+        for _index, md in cdv.iterrows():
+            found = False
+            for col in df.columns:
+                if md['columnId'] in col:
+                    print("col: {}; tableName: {}, name: {}; columnId: {}"
+                          .format(
+                              col,
+                              md['tableName'],
+                              md['name'],
+                              md['columnId']))
+                    found = True
+            if not found:
+                print("NOT FOUND: tableName: {}, name: {}; columnId: {}"
+                      .format(
+                          md['tableName'],
+                          md['name'],
+                          md['columnId']))
+
+    def test_match_cdv_to_main(self):
+        cdv = self.loader.load_cdv()
+        main = self.loader.load_main()
+
+        for _cdvindex, md in cdv.iterrows():
+            found = False
+            for _mainindex, mmd in main.iterrows():
+                if mmd['uniqueVariableId'] in md['columnId']:
+                    found = True
+                    print("cdv columnId: {} = main uniqueVariableId: {}"
+                          .format(
+                              md['columnId'], mmd['uniqueVariableId']
+                          ))
+            if not found:
+                print("cdv columnId: {} not found"
+                      .format(
+                          md['columnId']
+                      ))
+
+    def test_check_main_measures_in_everything(self):
+        main = self.loader.load_main()
+        df = self.loader.load_everything()
+
+        found_count = 0
+        not_found_count = 0
+        not_found_list = []
+
+        for _index, md in main.iterrows():
+            found = []
+            variable_name = md['uniqueVariableId']
+            table_name = md['tableName']
+
+            for col_name in df.columns:
+                if variable_name == col_name[-len(variable_name):]:
+                    #                     print("col: {}; main name: {}"
+                    #                           .format(
+                    #                               col,
+                    #                               main_name,
+                    #                           ))
+                    found.append(col_name)
+            if found:
+                found_count += 1
+                if len(found) > 1:
+                    print("table: {}; var: {}; found: {}"
+                          .format(
+                              table_name,
+                              variable_name,
+                              found
+                          ))
+            else:
+                not_found_count += 1
+                not_found_list.append(md)
+#                 print("NOT FOUND: main name: {}"
+#                       .format(
+#                           main_name,
+#                       ))
+
+        print("found: {}; not found: {}".format(found_count, not_found_count))
+        # print("not found list: {}".format(not_found_list))
 
 if __name__ == "__main__":
     unittest.main()
