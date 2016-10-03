2cef8a0f9c07d68dd812a6dcf77b7cd8507bc13c
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2016-10-03 11:33:40 +0300

    Fix counting in enrichment test
    Task-Url: https://app.asana.com/0/184312595500139/187968838321004

--- wdae/enrichment/tests/test_m6_enrichment_children_stats.py
+++ wdae/enrichment/tests/test_m6_enrichment_children_stats.py
@@ -26,3 +26,6 @@ class Test(unittest.TestCase):
 
         self.assertEquals(1502, stats['unaffected']['F'])
         self.assertEquals(5069, stats['unaffected']['M'])
+
+        self.assertEquals(1111, stats['unaffected']['M'])
+        self.assertEquals(1192, stats['unaffected']['F'])
