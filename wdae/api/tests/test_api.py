fb6a867d3ecf20bd96f869b3b28960f31b76c4e9
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2016-10-24 15:43:59 +0300

    Review and fix problem with pheno measure filter in advanced family filters
    Task-Url: https://app.asana.com/0/198129762902403/200364662186761

--- wdae/api/tests/test_api.py
+++ wdae/api/tests/test_api.py
@@ -81,11 +81,11 @@ class ApiTest(APITestCase):
         # Test page count with wrong attr
         url_page_count = '/api/gene_set_list2?gene_set=main&page_count=foo'
         response = self.client.get(url_page_count)
-        self.assertEqual(len(response.data), 8)
+        self.assertEqual(len(response.data), 14)
 
         url_page_count = '/api/gene_set_list2?gene_set=main&page_count=-10'
         response = self.client.get(url_page_count)
-        self.assertEqual(len(response.data), 8)
+        self.assertEqual(len(response.data), 14)
 
     def test_child_type_list(self):
         response = self.client.get('/api/child_types')
