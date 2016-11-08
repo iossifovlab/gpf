2176e195c119895cc8640016e4b777c07e411551
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2016-11-08 10:43:45 +0200

    Move enrichment tools from wdae to DAE
    Task-Url: https://app.asana.com/0/207923896306386/207924209205151

--- DAE/enrichment_tool/background.py
+++ DAE/enrichment_tool/background.py
@@ -17,49 +17,6 @@ import numpy as np
 import pandas as pd
 
 
-# from DAE import *
-def _collect_affected_gene_syms(vs):
-    return [set([ge['sym'].upper() for ge in v.requestedGeneEffects])
-            for v in vs]
-
-
-def _collect_unique_gene_syms(affected_gene_sets):
-    gene_set = set()
-    for gs in affected_gene_sets:
-        gene_set |= set(gs)
-    return gene_set
-
-
-def _count_gene_syms(affected_gene_sets):
-    background = Counter()
-    for gene_set in affected_gene_sets:
-        for gene_sym in gene_set:
-            background[gene_sym] += 1
-    return background
-
-
-def _build_synonymous_background(transmitted_study_name):
-    transmitted_study = vDB.get_study(transmitted_study_name)
-    vs = transmitted_study.get_transmitted_summary_variants(
-        ultraRareOnly=True,
-        minParentsCalled=600,
-        effectTypes=["synonymous"])
-    affected_gene_syms = _collect_affected_gene_syms(vs)
-
-    base = [gs for gs in affected_gene_syms if len(gs) == 1]
-    foreground = [gs for gs in affected_gene_syms if len(gs) > 1]
-
-    base_counts = _count_gene_syms(base)
-
-    base_sorted = sorted(zip(base_counts.keys(),
-                             base_counts.values()))
-
-    b = np.array(base_sorted,
-                 dtype=[('sym', '|S32'), ('raw', '>i4')])
-
-    return (b, foreground)
-
-
 class StatsResults(EnrichmentConfig):
 
     def __init__(self, config):
@@ -153,14 +110,74 @@ class BackgroundCommon(BackgroundBase):
 class SynonymousBackground(BackgroundCommon):
     TRANSMITTED_STUDY_NAME = 'w1202s766e611'
 
+    @staticmethod
+    def _collect_affected_gene_syms(vs):
+        return [set([ge['sym'].upper() for ge in v.requestedGeneEffects])
+                for v in vs]
+
+    @staticmethod
+    def _collect_unique_gene_syms(affected_gene_sets):
+        gene_set = set()
+        for gs in affected_gene_sets:
+            gene_set |= set(gs)
+        return gene_set
+
+    @staticmethod
+    def _count_gene_syms(affected_gene_sets):
+        background = Counter()
+        for gene_set in affected_gene_sets:
+            for gene_sym in gene_set:
+                background[gene_sym] += 1
+        return background
+
+    @staticmethod
+    def _build_synonymous_background(transmitted_study_name):
+        transmitted_study = vDB.get_study(transmitted_study_name)
+        vs = transmitted_study.get_transmitted_summary_variants(
+            ultraRareOnly=True,
+            minParentsCalled=600,
+            effectTypes=["synonymous"])
+        affected_gene_syms = \
+            SynonymousBackground._collect_affected_gene_syms(vs)
+
+        base = [gs for gs in affected_gene_syms if len(gs) == 1]
+        foreground = [gs for gs in affected_gene_syms if len(gs) > 1]
+
+        base_counts = SynonymousBackground._count_gene_syms(base)
+
+        base_sorted = sorted(zip(base_counts.keys(),
+                                 base_counts.values()))
+
+        background = np.array(base_sorted,
+                              dtype=[('sym', '|S32'), ('raw', '>i4')])
+
+        return (background, foreground)
+
     def __init__(self):
         super(SynonymousBackground, self).__init__()
         self.name = 'synonymousBackgroundModel'
 
     def precompute(self):
         self.background, self.foreground = \
-            _build_synonymous_background(self.TRANSMITTED_STUDY_NAME)
+            self._build_synonymous_background(self.TRANSMITTED_STUDY_NAME)
         return self.background
+
+    def serialize(self):
+        fout = cStringIO.StringIO()
+        np.save(fout, self.background)
+
+        b = zlib.compress(fout.getvalue())
+        f = zlib.compress(cPickle.dumps(self.foreground))
+        return {'background': b,
+                'foreground': f}
+
+    def deserialize(self, data):
+        b = data['background']
+        fin = cStringIO.StringIO(zlib.decompress(b))
+        self.background = np.load(fin)
+
+        f = data['foreground']
+        self.foreground = cPickle.loads(zlib.decompress(f))
 
     def _count_foreground_events(self, gene_syms):
         count = 0
@@ -186,28 +203,15 @@ class SynonymousBackground(BackgroundCommon):
     def _total(self):
         return np.sum(self.background['raw']) + len(self.foreground)
 
-    def serialize(self):
-        fout = cStringIO.StringIO()
-        np.save(fout, self.background)
-
-        b = zlib.compress(fout.getvalue())
-        f = zlib.compress(cPickle.dumps(self.foreground))
-        return {'background': b,
-                'foreground': f}
-
-    def deserialize(self, data):
-        b = data['background']
-        fin = cStringIO.StringIO(zlib.decompress(b))
-        self.background = np.load(fin)
-
-        f = data['foreground']
-        self.foreground = cPickle.loads(zlib.decompress(f))
-
 
 class CodingLenBackground(BackgroundCommon):
 
+    @property
+    def filename(self):
+        return self[self.name, 'file']
+
     def _load_and_prepare_build(self):
-        filename = self[self.name, 'file']
+        filename = self.filename
         assert filename is not None
         back = []
         with open(filename, 'r') as f:
@@ -266,6 +270,7 @@ def poisson_test(observed, expected):
 
 
 class SamochaBackground(BackgroundBase):
+
     def _load_and_prepare_gender_count(self, df):
         GM = genomesDB.get_gene_models()  # @UndefinedVariable
 
