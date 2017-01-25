19b2ad2b4460fd34ecbbe6c7b70bafaa8e971849
Nikolay Dimitrov <nikidimi@gmail.com>
2017-01-25 02:09:12 +0200

    Most data is now displayer in genotype preview table using the layout from previous version

--- gpfjs/src/app/genotype-preview-table/genotype-preview-table.component.ts
+++ gpfjs/src/app/genotype-preview-table/genotype-preview-table.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { QueryService } from '../query/query.service';
+import { GenotypePreview, GenotypePreviewsArray } from '../genotype-preview-table/genotype-preview';
+
 
 @Component({
   selector: 'gpf-genotype-preview-table',
@@ -7,6 +9,37 @@ import { QueryService } from '../query/query.service';
   styleUrls: ['./genotype-preview-table.component.css']
 })
 export class GenotypePreviewTableComponent implements OnInit {
+  private genotypePreviewsArray: GenotypePreviewsArray
+
+  COLUMN_HEADINGS = {
+    FAMILY: "family",
+    VARIANT: "variant",
+    GENOTYPE: "genotype",
+    EFFECT: "effect",
+    ALLELE: "allele freq",
+    RACES: "races",
+    IQ: "Proband IQ"
+
+  };
+  COLUMN_SUBHEADINGS = {
+    FAMILY_ID: "id",
+    STDY: "stdy",
+    LOC: "loc",
+    VAR: "var",
+    CH: "ch",
+    PAR: "par",
+    TYPE: "type",
+    GENE: "gene",
+    SSC: "SSC",
+    EVS: "EVS",
+    E65: "E65",
+    MOM: "mom",
+    DAD: "dad",
+    VIQ: "vIQ",
+    NVIQ: "NvIQ"
+  };
+
+
   constructor(
     private queryService: QueryService
   ) { }
@@ -14,7 +47,7 @@ export class GenotypePreviewTableComponent implements OnInit {
   ngOnInit() {
     this.queryService.getGenotypePreviewByFilter()
     .then(genotypePreviewsArray => {
-
+      this.genotypePreviewsArray = genotypePreviewsArray
     });
   }
 }
