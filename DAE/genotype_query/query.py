5e04cbbe025920540b368752f9ce599709956c51
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2017-02-06 18:05:23 +0200

    add query preview to v3 API
    Task-Url: https://app.asana.com/0/236219705950418/262725370526709

--- DAE/genotype_query/query.py
+++ DAE/genotype_query/query.py
@@ -8,6 +8,13 @@ from common.query_base import QueryBase
 
 class QueryDataset(QueryBase):
 
+    @staticmethod
+    def get_named(named_list, name):
+        for el in named_list:
+            if name == el['id']:
+                return el
+        return None
+
     def __init__(self, datasets):
         self.datasets = {}
         for dataset in datasets:
@@ -16,3 +23,33 @@ class QueryDataset(QueryBase):
     def get_variants(self, dataset_id, **kwargs):
         assert dataset_id is not None
         assert dataset_id in self.datasets
+
+    def get_pedigree_selector(self, dataset, **kwargs):
+        pedigrees = dataset['pedigreeSelectors']
+        pedigree = pedigrees[0]
+        if 'pedigree_selector' in kwargs:
+            pedigree = self.get_named(pedigrees, kwargs['pedigree_selector'])
+        assert pedigree is not None
+        return pedigree
+
+    def get_dataset(self, dataset_id):
+        assert dataset_id is not None
+        assert dataset_id in self.datasets
+        dataset = self.datasets[dataset_id]
+        assert dataset is not None
+
+        return dataset
+
+    def get_legend(self, dataset_id, **kwargs):
+        dataset = self.get_dataset(dataset_id)
+        pedigree = self.get_pedigree_selector(dataset, **kwargs)
+
+        values = pedigree['domain'][:]
+        default_value = pedigree['default']
+        if self.get_named(values, default_value['id']) is None:
+            values.append(default_value)
+
+        return {
+            'name': pedigree['name'],
+            'values': values
+        }
--- DAE/genotype_query/tests/test_dataset_query.py
+++ DAE/genotype_query/tests/test_dataset_query.py
@@ -4,6 +4,40 @@ Created on Feb 6, 2017
 @author: lubo
 '''
 import pytest
+import copy
+from pprint import pprint
+
+
+EXAMPLE_QUERY_SSC = {
+    "effect_types": "Frame-shift,Nonsense,Splice-site",
+    "gender": "female,male",
+    "present_in_child": "autism and unaffected,autism only",
+    "present_in_parent": "neither",
+    "variant_types": "CNV,del,ins,sub",
+    "genes": "All",
+    "family_race": "All",
+    "family_quad_trio": "All",
+    "family_prb_gender": "All",
+    "family_sib_gender": "All",
+    "family_study_type": "All",
+    "family_studies": "All",
+    "family_pheno_measure_min": 1.08,
+    "family_pheno_measure_max": 40,
+    "family_pheno_measure": "abc.subscale_ii_lethargy",
+    "dataset_id": "SSC",
+    "pedigree_selector": "phenotypes"
+}
+
+EXAMPLE_QUERY_VIP = {
+    "effect_types": "Frame-shift,Nonsense,Splice-site",
+    "gender": "female,male",
+    "present_in_child": "autism and unaffected,autism only",
+    "present_in_parent": "neither",
+    "variant_types": "CNV,del,ins,sub",
+    "genes": "All",
+    "dataset_id": "VIP",
+    "pedigree_selector": "16pstatus"
+}
 
 
 def test_none_dataset_id(query):
@@ -18,3 +52,35 @@ def test_bad_dataset_id(query):
 
 def test_good_dataset_id(query):
     query.get_variants(dataset_id='SSC')
+
+
+def test_example_query(query):
+    query.get_variants(**EXAMPLE_QUERY_SSC)
+    query.get_legend(**EXAMPLE_QUERY_SSC)
+
+
+def test_get_legend_ssc(query):
+    legend = query.get_legend(**EXAMPLE_QUERY_SSC)
+    assert legend is not None
+    pprint(legend)
+    assert 'name' in legend
+    assert 'values' in legend
+
+    assert 3 == len(legend['values'])
+
+
+def test_get_legend_vip(query):
+    legend = query.get_legend(**EXAMPLE_QUERY_VIP)
+    assert legend is not None
+    pprint(legend)
+    assert 'name' in legend
+    assert 'values' in legend
+
+    assert 5 == len(legend['values'])
+
+
+def test_get_legend_bad_pedigree(query):
+    kwargs = copy.deepcopy(EXAMPLE_QUERY_SSC)
+    kwargs['pedigree_selector'] = 'ala bala'
+    with pytest.raises(AssertionError):
+        query.get_legend(**kwargs)
