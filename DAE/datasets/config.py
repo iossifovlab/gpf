da568d270261fdee366f580accaf5f90ac6772de
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2017-02-06 15:28:17 +0200

    add query preview to v3 API
    Task-Url: https://app.asana.com/0/236219705950418/262725370526709

---
+++ DAE/datasets/config.py
@@ -0,0 +1,162 @@
+'''
+Created on Jan 20, 2017
+
+@author: lubo
+'''
+import ConfigParser
+
+from Config import Config
+import collections
+
+
+class PedigreeSelector(dict):
+
+    def __init__(self, pedigree_id, **kwargs):
+        self['id'] = pedigree_id
+        self['domain'] = self._parse_domain(kwargs['domain'])
+        self['default'] = self._parse_domain_value(kwargs['default'])
+        self['source'] = kwargs['source']
+        self['name'] = kwargs['name']
+
+    def _parse_domain_value(self, value):
+        (selector_id, selector_name, selector_color) = value.split(':')
+        return {
+            'id': selector_id,
+            'name': selector_name,
+            'color': selector_color,
+        }
+
+    def _parse_domain(self, domain):
+        values = domain.split(',')
+        result = [self._parse_domain_value(v) for v in values]
+        return result
+
+
+class DatasetConfig(object):
+
+    @staticmethod
+    def split_section(section):
+        index = section.find('.')
+        if index == -1:
+            return (section, None)
+        section_type = section[:index]
+        section_name = section[index + 1:]
+        return (section_type, section_name)
+
+    def __init__(self, **kwargs):
+        super(DatasetConfig, self).__init__()
+        config = kwargs.get('config', None)
+
+        if config:
+            assert isinstance(config, ConfigParser.SafeConfigParser)
+            self.config = config
+        else:
+            dae_config = Config()
+            wd = dae_config.daeDir
+            self.config = ConfigParser.SafeConfigParser({'wd': wd})
+            self.config.read(dae_config.variantsDBconfFile)
+
+    def get_datasets(self):
+        res = []
+        for section in self.config.sections():
+            (section_type, section_id) = self.split_section(section)
+            if section_id is None:
+                continue
+            if section_type == 'dataset':
+                dataset = self.get_dataset(section_id)
+                res.append(dataset)
+        return res
+
+    def _get_boolean(self, section, option):
+        res = False
+        if self.config.has_option(section, option):
+            res = self.config.getboolean(section, option)
+        return res
+
+    def _get_string(self, section, option):
+        res = None
+        if self.config.has_option(section, option):
+            res = self.config.get(section, option)
+        return res
+
+    def _get_genotype_browser(self, section):
+        if not self._get_boolean(section, 'genotypeBrowser'):
+            return None
+        main_form = self._get_string(section, 'genotypeBrowser.mainForm')
+        has_denovo = self._get_boolean(section, 'genotypeBrowser.hasDenovo')
+        has_transmitted = \
+            self._get_boolean(section, 'genotypeBrowser.hasTransmitted')
+        has_cnv = self._get_boolean(section, 'genotypeBrowser.hasCNV')
+        study_types = self._get_boolean(
+            section, 'genotypeBrowser.hasStudyTypes')
+
+        advanced_family_filters = \
+            self._get_boolean(
+                section, 'genotypeBrowser.hasAdvancedFamilyFilter')
+        pedigree_selector = \
+            self._get_boolean(
+                section, 'genotypeBrowser.hasPedigreeSelector')
+        return {
+            'mainForm': main_form,
+            'hasDenovo': has_denovo,
+            'hasTransmitted': has_transmitted,
+            'hasCNV': has_cnv,
+            'hasStudyTypes': study_types,
+            'hasAdvancedFamilyFilters': advanced_family_filters,
+            'hasPedigreeSelector': pedigree_selector,
+        }
+
+    def _get_pedigree_selectors(self, section):
+        params = collections.defaultdict(dict)
+
+        options = self.config.options(section)
+        for option in options:
+            option_type, option_fullname = self.split_section(option)
+            if option_type != 'pedigree':
+                continue
+            pedigree_type, pedigree_option = \
+                self.split_section(option_fullname)
+            pedigree = params[pedigree_type]
+            value = self.config.get(section, option)
+            pedigree[pedigree_option] = value
+
+        if not params:
+            return None
+        result = []
+        for key, value in params.items():
+            pedigree = PedigreeSelector(key, **value)
+            result.append(pedigree)
+        return result
+
+    def get_dataset(self, dataset_id):
+        section = 'dataset.{}'.format(dataset_id)
+        if not self.config.has_section(section):
+            return None
+
+        name = self.config.get(section, 'name')
+
+        studies = self.config.get(section, 'studies')
+        study_types = self._get_string(section, 'studyTypes')
+        if study_types:
+            study_types = [st for st in study_types.split(',')]
+
+        pheno_db = self._get_string(section, 'phenoDB')
+        enrichment_tool = self._get_boolean(section, 'enrichmentTool')
+        pheno_geno_tool = self._get_boolean(section, 'phenoGenoTool')
+        phenotype_browser = self._get_boolean(section, 'phenotypeBrowser')
+        genotype_browser = self._get_genotype_browser(section)
+
+        pedigree_selectors = self._get_pedigree_selectors(section)
+
+        return {
+            'id': dataset_id,
+            'name': name,
+            'studies': studies,
+            'studyTypes': study_types,
+            'phenoDB': pheno_db,
+            'enrichmentTool': enrichment_tool,
+            'phenotypeGenotypeTool': pheno_geno_tool,
+            'phenotypeBrowser': phenotype_browser,
+            'genotypeBrowser': genotype_browser,
+            'pedigreeSelectors': pedigree_selectors,
+        }
---
+++ DAE/datasets/tests/test_datasets_config.py
@@ -0,0 +1,57 @@
+'''
+Created on Jan 20, 2017
+
+@author: lubo
+'''
+
+import unittest
+from datasets.config import DatasetConfig
+from pprint import pprint
+
+
+class DatasetsConfigTest(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(DatasetsConfigTest, cls).setUpClass()
+        cls.dataset_config = DatasetConfig()
+
+    def test_datasets_config_simple(self):
+        datasets = self.dataset_config.get_datasets()
+
+        self.assertIsNotNone(datasets)
+        self.assertEquals(3, len(datasets))
+
+    def test_dataset_sd(self):
+        ds = self.dataset_config.get_dataset('SD')
+        self.assertIsNotNone(ds)
+
+        self.assertIsNone(ds['phenoDB'])
+        self.assertEquals(1, len(ds['pedigreeSelectors']))
+
+    def test_dataset_ssc(self):
+        ds = self.dataset_config.get_dataset('SSC')
+        self.assertIsNotNone(ds)
+
+        self.assertEquals('ssc', ds['phenoDB'])
+        self.assertIsNotNone(ds['pedigreeSelectors'])
+        pedigree = ds['pedigreeSelectors'][0]
+
+        self.assertEquals('Phenotypes', pedigree['name'])
+
+    def test_dataset_not_found(self):
+        ds = self.dataset_config.get_dataset('ala bala')
+        self.assertIsNone(ds)
+
+    def test_dataset_vip(self):
+        ds = self.dataset_config.get_dataset('VIP')
+        self.assertIsNotNone(ds)
+
+        self.assertEquals('vip', ds['phenoDB'])
+        self.assertEquals(2, len(ds['pedigreeSelectors']))
+        pprint(ds['pedigreeSelectors'])
+        pedigrees = ds['pedigreeSelectors']
+
+        p16p = pedigrees[0]
+        self.assertIn('16pstatus', p16p['id'])
+        self.assertIn('name', p16p)
