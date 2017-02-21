b598058e8a99117a0bc1cc55af66a1672a280cfd
Nikolay Dimitrov <nikidimi@gmail.com>
2017-02-21 14:06:57 +0200

    Multiline support added to regions filter

--- gpfjs/src/app/regions-filter/regions-filter.validator.ts
+++ gpfjs/src/app/regions-filter/regions-filter.validator.ts
@@ -8,6 +8,16 @@ import { NG_VALIDATORS, Validator, Validators, AbstractControl } from '@angular/
 })
 export class RegionsFilterValidatorDirective implements Validator {
   validate(control: AbstractControl): {[key: string]: any} {
-    return /^(chr)?([0-9xX]|[0-1][0-9]|2[0-2]):[0-9]+(\-[0-9]+)?$/ig.test(control.value) ? null : {'regionsFilter': true};
+    if (!control.value) {
+      return null;
+    }
+
+    let valid = true;
+    for (var line of control.value.split(/\s+/)) {
+      if (! /^(chr)?([0-9xX]|[0-1][0-9]|2[0-2]):[0-9]+(\-[0-9]+)?$/ig.test(line)) {
+        valid = false;
+      }
+    }
+    return valid ? null : {'regionsFilter': true};
   }
 }
