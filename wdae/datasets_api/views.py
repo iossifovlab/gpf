3d5273b2c9584977fbaead5ef1d946ce54eea9f3
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2017-02-23 17:28:28 +0200

    implement logged user check in dataset v3 API
    Task-Url: https://app.asana.com/0/236219705950418/279177651016818

--- wdae/datasets_api/views.py
+++ wdae/datasets_api/views.py
@@ -8,6 +8,7 @@ from rest_framework.response import Response
 from rest_framework import status
 
 import preloaded
+from astropy.units import ds
 
 
 class DatasetView(APIView):
@@ -20,12 +21,32 @@ class DatasetView(APIView):
         self.datasets_config = self.datasets.get_config()
         self.datasets_factory = self.datasets.get_factory()
 
+    def augment_accessibility(self, dataset, user):
+        access_rights = False
+        if dataset['visibility'] == 'ALL':
+            access_rights = True
+        if dataset['visibility'] == 'AUTHENTICATED' \
+                and user.is_authenticated():
+            access_rights = True
+        if dataset['visibility'] == 'STAFF' \
+                and user.is_staff:
+            access_rights = True
+        dataset['accessRights'] = access_rights
+        return dataset
+
     def get(self, request, dataset_id=None):
+        user = request.user
+        print(user)
+        print(user.is_anonymous())
+        print(user.is_authenticated())
+
         if dataset_id is None:
             res = self.datasets_config.get_datasets()
+            res = [self.augment_accessibility(ds, user) for ds in res]
             return Response({'data': res})
         res = self.datasets_config.get_dataset_desc(dataset_id)
         if res:
+            res = self.augment_accessibility(res, user)
             return Response({'data': res})
         return Response({'error': 'Dataset {} not found'.format(dataset_id)},
                         status=status.HTTP_404_NOT_FOUND)
