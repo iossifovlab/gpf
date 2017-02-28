6a7acabbcb27e8a2e537b8bf17985e2340225c45
Nikolay Dimitrov <nikidimi@gmail.com>
2017-02-28 13:07:20 +0200

    Added v3 login API url

--- wdae/wdae/urls.py
+++ wdae/wdae/urls.py
@@ -17,5 +17,6 @@ urlpatterns = patterns(
     url(r'^api/v3/gene_sets', include('gene_sets.urls')),
     url(r'^api/v3/genotype_browser', include('genotype_browser.urls')),
     url(r'^api/v3/enrichment', include('enrichment_api.urls')),
+    url(r'^api/v3/users/', include('users.urls_session')),
 
 )
