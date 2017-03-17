169542c71a3cbac87febaf17da01e6d9a2d48d28
Nikolay Dimitrov <nikidimi@gmail.com>
2017-03-17 20:28:52 +0200

    Workaround for angular bug #14919

--- gpfjs/src/app/tabset/tabset.component.ts
+++ gpfjs/src/app/tabset/tabset.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { NgbTabset } from  '@ng-bootstrap/ng-bootstrap';
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { NgbTabset, NgbTab } from  '@ng-bootstrap/ng-bootstrap';
 
 
 @Component({
@@ -8,4 +8,30 @@ import { NgbTabset } from  '@ng-bootstrap/ng-bootstrap';
   templateUrl: './tabset.component.html',
 })
 export class GpfTabsetComponent extends NgbTabset {
+  @ViewChild('ngTemplateOutletContainer', { read: ViewContainerRef })
+  private ngTemplateOutletContainer: ViewContainerRef;
+
+  getTabById(id: string): NgbTab {
+    let tabsWithId: NgbTab[] = this.tabs.filter(tab => tab.id === id);
+    return tabsWithId.length ? tabsWithId[0] : null;
+  }
+
+  select(tabId: string) {
+    let selectedTab = this.getTabById(tabId);
+    if (selectedTab && !selectedTab.disabled && this.activeId !== selectedTab.id) {
+      let defaultPrevented = false;
+
+      if(this.ngTemplateOutletContainer) {
+        this.ngTemplateOutletContainer.clear();
+      }
+
+      this.tabChange.emit(
+          {activeId: this.activeId, nextId: selectedTab.id, preventDefault: () => { defaultPrevented = true; }});
+
+      if (!defaultPrevented) {
+        this.activeId = selectedTab.id;
+      }
+    }
+  }
+
 }
