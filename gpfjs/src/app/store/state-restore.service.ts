a0742f9c26e9f172d166e90df4e1feac3f51af9e
Nikolay Dimitrov <nikidimi@gmail.com>
2017-03-23 14:35:24 +0200

    State is now restored from URL. Currently only gene symbols are properly handled

---
+++ gpfjs/src/app/store/state-restore.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http, Response, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs';
+import { ReplaySubject } from 'rxjs/ReplaySubject';
+
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class StateRestoreService {
+  public state = new ReplaySubject<any>(1);
+
+
+  constructor(
+  ) {
+  }
+
+  onParamsUpdate(jsonEncodedState: string) {
+    if (jsonEncodedState && jsonEncodedState.length > 0) {
+      let currentState = JSON.parse(jsonEncodedState);
+      console.log("url state", currentState);
+      this.state.next(currentState);
+    }
+  }
+
+
+}
