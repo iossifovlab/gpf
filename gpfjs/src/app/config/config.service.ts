a76a6663513187cfc10327181ec8064c97c48ce1
Nikolay Dimitrov <nikidimi@gmail.com>
2017-03-29 19:41:41 +0300

    API path is now changed base on dev/production env

--- gpfjs/src/app/config/config.service.ts
+++ gpfjs/src/app/config/config.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class ConfigService {
 
-  readonly baseUrl: string = 'http://localhost:8000/api/v3/';
+  readonly baseUrl: string = environment.apiPath;
   // Drakov default url
   // readonly baseUrl: string = 'http://localhost:3000';
 
