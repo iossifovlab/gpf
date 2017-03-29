f1c4dfced966914959e1aa87ce59752062d5d612
Nikolay Dimitrov <nikidimi@gmail.com>
2017-03-29 14:37:13 +0300

    Initial state is no longer restored after tool/dataset change

--- gpfjs/src/app/store/state-restore.service.ts
+++ gpfjs/src/app/store/state-restore.service.ts
@@ -2,16 +2,33 @@ import { Injectable } from '@angular/core';
 import { Headers, Http, Response, RequestOptions } from '@angular/http';
 import { Observable } from 'rxjs';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
+import { Router } from '@angular/router';
 
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class StateRestoreService {
   public state = new ReplaySubject<any>(1);
-
+  private firstUrl: string;
+  private emptyObjectSend = false;
 
   constructor(
+    private router: Router
   ) {
+    //Reset state when tool/dataset is changed
+    //This prevents restoring the initial state constantly
+    router.events.subscribe(
+      (params) => {
+        let newUrl = this.router.url.split(';')[0];
+        if (this.firstUrl == null) {
+          this.firstUrl = newUrl;
+        }
+        else if (!this.emptyObjectSend && this.firstUrl != newUrl) {
+          this.state.next({});
+          this.emptyObjectSend = true;
+        }
+      }
+    )
   }
 
   onParamsUpdate(jsonEncodedState: string) {
