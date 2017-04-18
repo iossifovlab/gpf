ede6d5004786041932c7e928ddf14bea7afd9138
Nikolay Dimitrov <nikidimi@gmail.com>
2017-04-18 16:32:01 +0300

    Fixed NumberWithExpPipe when value is not a number

--- gpfjs/src/app/utils/number-with-exp.pipe.ts
+++ gpfjs/src/app/utils/number-with-exp.pipe.ts
@@ -5,6 +5,10 @@ import { DecimalPipe } from '@angular/common';
 export class NumberWithExpPipe extends DecimalPipe {
 
   transform(value: any, digits: string = null): string {
+    if (typeof value != 'number') {
+      return value;
+    }
+
     if (value >= 0.0001 || value == 0.0) {
       return super.transform(value, digits);
     }
