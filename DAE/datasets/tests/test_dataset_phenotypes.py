82b2938b5ce7f7e507b83212136ac8b48d09fd83
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2017-04-25 09:58:08 +0300

    datasets pedigree selector (person groups) support in enrichment tool
    Task-Url: https://github.com/seqpipe/gpf/issues/19

--- DAE/datasets/tests/test_dataset_phenotypes.py
+++ DAE/datasets/tests/test_dataset_phenotypes.py
@@ -28,26 +28,3 @@ def test_phenotypes_vip(vip):
     phenotypes = vip.get_phenotypes()
     print(phenotypes)
     assert ['autism', 'unaffected'] == phenotypes
-
-
-def test_children_stats_sd(sd):
-    children_stats = sd.children_stats
-    print(children_stats)
-
-    assert children_stats['intellectual disability']['F'] == 85
-    assert children_stats['intellectual disability']['M'] == 66
-
-    # assert children_stats['autism']['M'] == 3367
-    # assert children_stats['autism']['F'] == 596
-
-    # assert children_stats['congenital heart disease']['F'] == 467
-    # assert children_stats['congenital heart disease']['M'] == 762
-
-    assert children_stats['epilepsy']['F'] == 106
-    assert children_stats['epilepsy']['M'] == 158
-
-    assert children_stats['schizophrenia']['F'] == 248
-    assert children_stats['schizophrenia']['M'] == 716
-
-    # assert children_stats['unaffected']['F'] == 1192
-    # assert children_stats['unaffected']['M'] == 1111
--- DAE/enrichment_tool/tests/test_children_stats.py
+++ DAE/enrichment_tool/tests/test_children_stats.py
@@ -58,3 +58,51 @@ def test_children_stats_dataset(sd):
 
     assert 1111 == children_stats['M']
     assert 1192 == children_stats['F']
+
+
+def test_children_stats_intellectual_disability_sd(sd):
+    gh = GH.from_dataset(sd, 'phenotype', 'intellectual disability')
+    children_stats = gh.get_children_stats()
+
+    assert children_stats['F'] == 85
+    assert children_stats['M'] == 66
+
+
+def test_children_stats_autism_sd(sd):
+    gh = GH.from_dataset(sd, 'phenotype', 'autism')
+    children_stats = gh.get_children_stats()
+
+    assert children_stats['M'] == 3367  # 3367
+    assert children_stats['F'] == 596
+
+
+def test_children_stats_congenital_heart_disease_sd(sd):
+    gh = GH.from_dataset(sd, 'phenotype', 'congenital heart disease')
+    children_stats = gh.get_children_stats()
+
+    assert children_stats['F'] == 467  # 467
+    assert children_stats['M'] == 762  # 762
+
+
+def test_children_stats_epilepsy_sd(sd):
+    gh = GH.from_dataset(sd, 'phenotype', 'epilepsy')
+    children_stats = gh.get_children_stats()
+
+    assert children_stats['F'] == 106
+    assert children_stats['M'] == 158
+
+
+def test_children_stats_schizophrenia_sd(sd):
+    gh = GH.from_dataset(sd, 'phenotype', 'schizophrenia')
+    children_stats = gh.get_children_stats()
+
+    assert children_stats['F'] == 248
+    assert children_stats['M'] == 716
+
+
+def test_children_stats_unaffected_sd(sd):
+    gh = GH.from_dataset(sd, 'phenotype', 'unaffected')
+    children_stats = gh.get_children_stats()
+
+    assert children_stats['F'] == 1192  # 1192
+    assert children_stats['M'] == 1111  # 1111
