76dc923f81a366f9e343d73e49c305424ba9feea
Nikolay Dimitrov <nikidimi@gmail.com>
2017-04-26 14:45:26 +0300

    Import researchers command cleanup and it now supports groups

--- wdae/users/management/commands/import_researchers.py
+++ wdae/users/management/commands/import_researchers.py
@@ -2,65 +2,62 @@ import csv
 
 from django.core.management.base import BaseCommand, CommandError
 
-from users.models import Researcher, ResearcherId
-from django.contrib.auth.models import BaseUserManager
+from users.models import WdaeUser, ResearcherId
+from django.contrib.auth.models import BaseUserManager, Group
 
 
 class Command(BaseCommand):
     args = '<file> <file> ...'
-    help = 'Creates researchers from csv.' \
+    help = 'Creates researchers from csv. ' \
         'Required column names for the csv file - LastName, Email and Id.'
 
-    def load_or_create_researcher_id(self, rid):
-        res_ids = ResearcherId.objects.filter(researcher_id=rid)
-        if len(res_ids) == 1:
-            res_id = res_ids[0]
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '--additional-group', '-g',
+            action='append',
+            dest='groups',
+            default=[],
+            help='Add users to this group(s) in addition to the ones \
+                  in the file. Can be specified multiple times\
+                  e.g. -g GROUP1 -g GROUP2',
+        )
+
+    def handle_researcher(self, res, additional_groups):
+        rid = res['Id']
+        email = BaseUserManager.normalize_email(res['Email'])
+
+        user, created = WdaeUser.objects.get_or_create(email=email,
+            defaults = {'last_name': res['LastName']})
+
+        if created:
+            print("created researcher:{}".format(res))
         else:
-            res_id = ResearcherId()
-            res_id.researcher_id = rid
-            res_id.save()
-        return res_id
+            print("Updating researcher id/groups for:{}".format(res))
+
+        user.groups.clear()
+        groups = additional_groups + res['Groups'].split(':')
+
+        for group_name in set(groups):
+            if group_name == "":
+                continue
+            group, _ = Group.objects.get_or_create(name=group_name)
+            group.user_set.add(user)
+
+        res_id, _ = ResearcherId.objects.get_or_create(researcher_id=rid)
+        res_id.researcher.add(user)
 
     def handle(self, *args, **options):
         if(len(args) < 1):
             raise CommandError('At least one argument is required')
+
+        print(args, options)
         for csv_file in args:
             try:
                 with open(csv_file, 'rb') as csvfile:
                     resreader = csv.DictReader(csvfile)
 
                     for res in resreader:
-                        print("creating researcher:{}".format(res))
-
-                        rid = res['Id']
-                        email = BaseUserManager.normalize_email(res['Email'])
-
-                        res_instances = Researcher.objects.filter(email=email)
-                        if len(res_instances) == 1:
-                            res_instance = res_instances[0]
-                            researcher_ids = \
-                                res_instance.researcherid_set.all()
-                            researcher_id_set = set(
-                                [r.researcher_id for r in researcher_ids])
-                            if rid in researcher_id_set:
-                                print("researcher already exists: {}".format(
-                                    email))
-                                continue
-                            res_id = self.load_or_create_researcher_id(rid)
-
-                            res_id.researcher.add(res_instance)
-                            res_id.save()
-                        else:
-
-                            res_instance = Researcher()
-                            res_instance.email = email
-                            res_instance.last_name = res['LastName']
-                            res_instance.save()
-
-                            res_id = self.load_or_create_researcher_id(rid)
-
-                            res_id.researcher.add(res_instance)
-                            res_id.save()
+                        self.handle_researcher(res, options['groups'])
 
             except csv.Error:
                 raise CommandError(
