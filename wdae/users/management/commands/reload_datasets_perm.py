be7fb1e3fa0c34c24b3727c15a6633c0cc53a377
Nikolay Dimitrov <nikidimi@gmail.com>
2017-04-27 13:06:42 +0300

    User groups are no longer recreated when updating datasets perm

--- wdae/users/management/commands/reload_datasets_perm.py
+++ wdae/users/management/commands/reload_datasets_perm.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 from datasets.config import DatasetsConfig
 from datasets_api.models import Dataset
 from django.contrib.auth.models import Group
-from guardian.shortcuts import assign_perm
+from guardian.shortcuts import assign_perm, remove_perm
 from guardian.utils import get_anonymous_user
 
 class Command(BaseCommand):
@@ -12,12 +12,14 @@ class Command(BaseCommand):
         if(len(args) != 0):
             raise CommandError('Unexpected argument passed!')
 
-        Group.objects.all().delete()
-
         dataset_config = DatasetsConfig()
         for ds in dataset_config.get_datasets():
             dataset_id = ds['id']
-            datasetObject, created = Dataset.objects.get_or_create(dataset_id=dataset_id)
+            datasetObject, _ =\
+                Dataset.objects.get_or_create(dataset_id=dataset_id)
+
+            for group in Group.objects.all():
+                remove_perm('view', group, datasetObject)
 
             for group_name in ds['authorizedGroups']:
                 group, created = Group.objects.get_or_create(name=group_name)
