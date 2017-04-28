86a552087d238495a089e7ecff70bc11666a7ec0
Nikolay Dimitrov <nikidimi@gmail.com>
2017-04-28 21:34:03 +0300

    Issue #37: First tests added

---
+++ wdae/users/tests/users_session.py
@@ -0,0 +1,61 @@
+'''
+Created on May 25, 2015
+
+@author: lubo
+'''
+from users.models import WdaeUser, VerificationPath, ResearcherId
+from django.contrib.auth import authenticate, get_user_model
+from rest_framework.test import APITestCase
+from rest_framework import status
+from rest_framework.authtoken.models import Token
+from pprint import pprint
+
+
+class ResearcherRegistrationTest(APITestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(ResearcherRegistrationTest, cls).setUpClass()
+
+        cls.res = WdaeUser()
+        cls.res.first_name = 'fname'
+        cls.res.last_name = 'lname'
+        cls.res.email = 'fake@fake.com'
+        cls.res.save()
+
+        cls.research_id = ResearcherId()
+        cls.research_id.researcher_id = '11aa--bb'
+        cls.research_id.save()
+
+        cls.research_id.researcher.add(cls.res)
+
+    @classmethod
+    def tearDownClass(cls):
+        super(ResearcherRegistrationTest, cls).tearDownClass()
+        cls.res.delete()
+
+    def test_fail_register(self):
+        data = {
+            'email': 'faulthymail@faulthy.com',
+            'firstName': 'bad_first_name',
+            'lastName': 'bad_last_name'
+        }
+
+        response = self.client.post('/api/v3/users/register', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_successful_register(self):
+        [id1] = self.res.researcherid_set.all()
+        data = {
+            'firstName': self.res.first_name,
+            'lastName': self.res.last_name,
+            'researcherId': id1.researcher_id,
+            'email': self.res.email
+        }
+        pprint(data)
+
+        response = self.client.post('/api/v3/users/register', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(
+            response.data['researcherId'], id1.researcher_id)
+        self.assertEqual(response.data['email'], self.res.email)
