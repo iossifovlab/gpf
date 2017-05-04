643b532dad71291b966ece81f56466052b57b3e5
Nikolay Dimitrov <nikidimi@gmail.com>
2017-05-05 00:56:28 +0300

    Issue #50: Added tests for family filters by study

--- wdae/datasets_api/tests/test_pheno_family_filters_ssc.py
+++ wdae/datasets_api/tests/test_pheno_family_filters_ssc.py
@@ -50,3 +50,67 @@ class Test(BaseAuthenticatedUserTest):
         data = response.data
 
         self.assertEquals('5', data['count'])
+
+    def test_pheno_family_filters_by_study(self):
+        url = '/api/v3/genotype_browser/preview'
+        data = {
+            'effectTypes': ['Frame-shift', 'Nonsense', 'Splice-site'],
+            "gender": ["female", "male"],
+            "presentInChild": [
+                "affected and unaffected",
+                "affected only",
+            ],
+            "presentInParent": [
+                "neither"
+            ],
+            "variantTypes": [
+                "CNV", "del", "ins", "sub",
+            ],
+            "datasetId": "SSC",
+            "phenoFilters": [
+                {
+                    "id": "Study",
+                    "measureType": "studies",
+                    "role": "study",
+                    "measure": "studyFilter",
+                    "selection": ["LevyCNV2011"]
+                }
+            ]
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.data
+
+        self.assertEquals('122', data['count'])
+
+    def test_pheno_family_filters_by_study_type(self):
+        url = '/api/v3/genotype_browser/preview'
+        data = {
+            'effectTypes': ['Frame-shift', 'Nonsense', 'Splice-site'],
+            "gender": ["female", "male"],
+            "presentInChild": [
+                "affected and unaffected",
+                "affected only",
+            ],
+            "presentInParent": [
+                "neither"
+            ],
+            "variantTypes": [
+                "CNV", "del", "ins", "sub",
+            ],
+            "datasetId": "SSC",
+            "phenoFilters": [
+                {
+                    "id": "Study Type",
+                    "measureType": "studies",
+                    "role": "study",
+                    "measure": "studyTypeFilter",
+                    "selection": ["TG"]
+                }
+            ]
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.data
+
+        self.assertEquals('412', data['count'])
