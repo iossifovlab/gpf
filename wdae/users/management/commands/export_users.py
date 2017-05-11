3e88340b60e000313d12d0293217c61a6a860094
Nikolay Dimitrov <nikidimi@gmail.com>
2017-05-11 12:47:41 +0300

    Issue #63: devusers/export_users/reload_datasets_perm command migrated to new model

--- wdae/users/management/commands/export_users.py
+++ wdae/users/management/commands/export_users.py
@@ -7,40 +7,40 @@ from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth import get_user_model
 import csv
 
+
 class Command(BaseCommand):
     args = '<file>'
 
     def handle_user(self, user, writer):
+        groups = list(user.groups.values_list('name', flat=True).all())
+        if user.is_superuser:
+            groups.append("superuser")
         groups_str = ":".join(
-            user.groups.values_list('name', flat=True).all()
+            groups
         )
-        researcher_ids_str = ":".join(
-            user.researcherid_set.values_list('researcher_id', flat=True).all()
-        )
+
+        if user.is_active:
+            password = user.password
+        else:
+            password = ""
 
         writer.writerow({
             "Email": user.email,
             "FirstName": user.first_name,
             "LastName": user.last_name,
             "Groups": groups_str,
-            "Superuser": user.is_superuser,
-            "Staff": user.is_staff,
-            "Active": user.is_active,
-            "Id": researcher_ids_str,
-            "Password": user.password
+            "Password": password
         })
 
     def handle(self, *args, **options):
         if(len(args) != 1):
             raise CommandError('Filename to export users missing')
 
-
         User = get_user_model()
         users = User.objects.all()
 
         with open(args[0], "w") as csvfile:
             fieldnames = ["Email", "FirstName", "LastName", "Groups",
-                          "Superuser", "Staff", "Active", "Id",
                           "Password"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
