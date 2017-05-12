e580fe9065e57db8d51a1dcdb4b5f994d4e51d77
Nikolay Dimitrov <nikidimi@gmail.com>
2017-05-12 17:31:36 +0300

    Fixes #69: user_create can now handle missing name/group param

--- wdae/users_api/management/commands/user_create.py
+++ wdae/users_api/management/commands/user_create.py
@@ -5,22 +5,40 @@ from django.core.management import call_command
 
 
 class Command(BaseCommand, ImportUsersBase):
-    args = '<email> [<name>] [<groups>]'
+    args = '[-n <name>] [-g <groups>] <email>'
     help = 'Creates new user'
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            '-g',
+            action='store',
+            dest='groups',
+            default='',
+            help='Add users to this group(s). Seperated by a colon e.g.'
+            'group1:group2:group3',
+        )
+
+        parser.add_argument(
+            '-n',
+            action='store',
+            dest='name',
+            default='',
+            help='Sets the name of the user',
+        )
+
     def handle(self, *args, **options):
-        if(len(args) < 1 or len(args) > 3):
-            raise CommandError('1-3 arguments required')
+        if(len(args) != 1):
+            raise CommandError('Exactly one argument required')
 
         UserModel = get_user_model()
         if UserModel.objects.filter(email=args[0]).exists():
             raise CommandError('User exists')
 
-        res = {'Email': args[0]}
-        if len(args) >= 2:
-            res['Name'] = args[1]
-        if len(args) >= 3:
-            res['Groups'] = args[2]
+        res = {
+            'Email': args[0],
+            'Name': options['name'],
+            'Groups': options['groups']
+        }
         self.handle_user(res)
 
         call_command('changepassword', username=args[0], stdout=self.stdout)
