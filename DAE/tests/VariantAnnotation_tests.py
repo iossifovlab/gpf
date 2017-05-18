49a6638bec25b3eb5bf1a29e5da16cec89647ef5
Nikolay Dimitrov <nikidimi@gmail.com>
2017-05-18 20:36:39 +0300

    Issue #77: Deletion test added

--- DAE/tests/VariantAnnotation_tests.py
+++ DAE/tests/VariantAnnotation_tests.py
@@ -1,16 +1,18 @@
 import unittest
 import VariantAnnotation
 from DAE import genomesDB
+import pytest
 
 
+@pytest.fixture(scope="class")
+def genomes_DB(request):
+    request.cls.GA = genomesDB.get_genome()
+    request.cls.gmDB = genomesDB.get_gene_models()
+
+
+@pytest.mark.usefixtures("genomes_DB")
 class VariantAnnotationTest(unittest.TestCase):
-
-    def test_chr1_897349_sub(self):
-        GA = genomesDB.get_genome()
-        gmDB = genomesDB.get_gene_models()
-        [effect] = VariantAnnotation.annotate_variant(gmDB, GA,
-                                                      loc="1:897349",
-                                                      var="sub(G->A)")
+    def assert_chr1_897349_sub(self, effect):
         self.assertEqual(effect.gene, "KLHL17")
         self.assertEqual(effect.transcript_id, "NM_198317_1")
         self.assertEqual(effect.strand, "+")
@@ -18,6 +20,42 @@ class VariantAnnotationTest(unittest.TestCase):
         self.assertEqual(effect.prot_pos, 211)
         self.assertEqual(effect.prot_length, 642)
         self.assertEqual(effect.aa_change, "Lys->Lys")
+
+    def test_chr1_897349_sub_var(self):
+        [effect] = VariantAnnotation.annotate_variant(self.gmDB, self.GA,
+                                                      loc="1:897349",
+                                                      var="sub(G->A)")
+        self.assert_chr1_897349_sub(effect)
+
+    def test_chr1_897349_sub_ref_alt(self):
+        [effect] = VariantAnnotation.annotate_variant(self.gmDB, self.GA,
+                                                      loc="1:897349",
+                                                      ref="G",
+                                                      alt="A")
+        self.assert_chr1_897349_sub(effect)
+
+    def test_chr1_897349_sub_ref_alt_pos(self):
+        [effect] = VariantAnnotation.annotate_variant(self.gmDB, self.GA,
+                                                      chr="1",
+                                                      position="897349",
+                                                      ref="G",
+                                                      alt="A")
+        self.assert_chr1_897349_sub(effect)
+
+    def assert_chr1_3519050_del(self, effect):
+        self.assertEqual(effect.gene, "MEGF6")
+        self.assertEqual(effect.transcript_id, "NM_001409_1")
+        self.assertEqual(effect.strand, "-")
+        self.assertEqual(effect.effect, "frame-shift")
+        self.assertEqual(effect.prot_pos, 82)
+        self.assertEqual(effect.prot_length, 1541)
+        self.assertEqual(effect.aa_change, None)
+
+    def test_chr1_3519050_del_var(self):
+        [effect] = VariantAnnotation.annotate_variant(self.gmDB, self.GA,
+                                                      loc="1:3519050",
+                                                      var="del(1)")
+        self.assert_chr1_3519050_del(effect)
 
 
 if __name__ == "__main__":
