7726861264431f6b15d29e5e73c104ab18efb4ca
Nikolay Dimitrov <nikidimi@gmail.com>
2017-05-31 22:54:12 +0300

    Issue #77: More variant annotations tests added

--- DAE/tests/VariantAnnotation_tests.py
+++ DAE/tests/VariantAnnotation_tests.py
@@ -112,6 +112,74 @@ class VariantAnnotationTest(unittest.TestCase):
         self.assertEqual(effects[4].prot_length, 394)
         self.assertEqual(effects[4].aa_change, None)
 
+    def test_chr1_24507340_del_3_var(self):
+        effects = VariantAnnotation.annotate_variant(self.gmDB, self.GA,
+                                                     loc="1:24507340",
+                                                     var="del(3)")
+        self.assertEqual(len(effects), 3)
+
+        self.assertEqual(effects[0].gene, "IFNLR1")
+        self.assertEqual(effects[0].transcript_id, "NM_173065_1")
+        self.assertEqual(effects[0].strand, "-")
+        self.assertEqual(effects[0].effect, "no-frame-shift")
+        self.assertEqual(effects[0].prot_pos, 21)
+        self.assertEqual(effects[0].prot_length, 244)
+        self.assertEqual(effects[0].aa_change, None)
+
+        self.assertEqual(effects[1].gene, "IFNLR1")
+        self.assertEqual(effects[1].transcript_id, "NM_173064_1")
+        self.assertEqual(effects[1].strand, "-")
+        self.assertEqual(effects[1].effect, "no-frame-shift")
+        self.assertEqual(effects[1].prot_pos, 21)
+        self.assertEqual(effects[1].prot_length, 491)
+        self.assertEqual(effects[1].aa_change, None)
+
+        self.assertEqual(effects[2].gene, "IFNLR1")
+        self.assertEqual(effects[2].transcript_id, "NM_170743_1")
+        self.assertEqual(effects[2].strand, "-")
+        self.assertEqual(effects[2].effect, "no-frame-shift")
+        self.assertEqual(effects[2].prot_pos, 21)
+        self.assertEqual(effects[2].prot_length, 520)
+        self.assertEqual(effects[2].aa_change, None)
+
+    def test_chr1_61553905_sub_var(self):
+        effects = VariantAnnotation.annotate_variant(self.gmDB, self.GA,
+                                                     loc="1:61553905",
+                                                     var="sub(C->T)")
+        self.assertEqual(len(effects), 4)
+
+        self.assertEqual(effects[0].gene, "NFIA")
+        self.assertEqual(effects[0].transcript_id, "NM_001145512_1")
+        self.assertEqual(effects[0].strand, "+")
+        self.assertEqual(effects[0].effect, "nonsense")
+        self.assertEqual(effects[0].prot_pos, 83)
+        self.assertEqual(effects[0].prot_length, 554)
+        self.assertEqual(effects[0].aa_change, "Arg->End")
+
+        self.assertEqual(effects[1].gene, "NFIA")
+        self.assertEqual(effects[1].transcript_id, "NM_001145511_1")
+        self.assertEqual(effects[1].strand, "+")
+        self.assertEqual(effects[1].effect, "nonsense")
+        self.assertEqual(effects[1].prot_pos, 30)
+        self.assertEqual(effects[1].prot_length, 501)
+        self.assertEqual(effects[1].aa_change, "Arg->End")
+
+        self.assertEqual(effects[2].gene, "NFIA")
+        self.assertEqual(effects[2].transcript_id, "NM_001134673_1")
+        self.assertEqual(effects[2].strand, "+")
+        self.assertEqual(effects[2].effect, "nonsense")
+        self.assertEqual(effects[2].prot_pos, 38)
+        self.assertEqual(effects[2].prot_length, 509)
+        self.assertEqual(effects[2].aa_change, "Arg->End")
+
+        self.assertEqual(effects[3].gene, "NFIA")
+        self.assertEqual(effects[3].transcript_id, "NM_005595_1")
+        self.assertEqual(effects[3].strand, "+")
+        self.assertEqual(effects[3].effect, "nonsense")
+        self.assertEqual(effects[3].prot_pos, 38)
+        self.assertEqual(effects[3].prot_length, 498)
+        self.assertEqual(effects[3].aa_change, "Arg->End")
+
 
 if __name__ == "__main__":
     unittest.main()
