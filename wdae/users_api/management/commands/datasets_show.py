8e144b332ce62d7aef9b8a721311d7d3a7cf76df
Nikolay Dimitrov <nikidimi@gmail.com>
2017-05-31 13:33:27 +0300

    Issue #82: Command for showing datasets with authoried groups added

---
+++ wdae/users_api/management/commands/datasets_show.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from datasets_api.models import Dataset
+from django.contrib.auth.models import Group
+from guardian.shortcuts import get_perms
+
+
+class Command(BaseCommand):
+    args = '<email>'
+    help = 'Shows all information about user'
+
+    def handle(self, *args, **options):
+        groups = Group.objects.all()
+
+        datasets = Dataset.objects.all()
+        for ds in datasets:
+            authorized = [group.name for group in groups
+                          if 'view' in get_perms(group, ds)]
+            print("{} Authorized groups: {}".format(ds.dataset_id,
+                                                    ','.join(authorized)))
