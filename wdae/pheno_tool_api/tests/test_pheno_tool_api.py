0453a3bbf554296cc01cf110fc9c2500a487d831
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2017-07-07 09:58:25 +0300

    #12 Investigate different results in new Pheno Tool compared to the old one
    Task-Url: https://github.com/seqpipe/gpf/issues/12

--- wdae/pheno_tool_api/tests/test_pheno_tool_api.py
+++ wdae/pheno_tool_api/tests/test_pheno_tool_api.py
@@ -6,38 +6,40 @@ Created on Jul 7, 2017
 from __future__ import print_function
 from users_api.tests.base_tests import BaseAuthenticatedUserTest
 from rest_framework import status
-from pprint import pprint
+import copy
 
 
 class Test(BaseAuthenticatedUserTest):
 
     TOOL_URL = "/api/v3/pheno_tool"
+    QUERY = {
+        "datasetId": "SSC",
+        "measureId": "ssc_hwhc.head_circumference",
+        "normalizeBy": [
+            "age",
+            "non-verbal iq"
+        ],
+        "geneSet": {
+            "geneSetsCollection": "main",
+            "geneSet": "autism candidates from Sanders Neuron 2015",
+            "geneSetsTypes": []
+        },
+        "presentInParent": {
+            "presentInParent": [
+                "neither"
+            ],
+        },
+        "effectTypes": [
+
+        ]
+    }
 
     def test_simple_query_lgds(self):
-        query = {
-            "datasetId": "SSC",
-            "measureId": "ssc_hwhc.head_circumference",
-            "normalizeBy": [
-                "age",
-                "non-verbal iq"
-            ],
-            "geneSet": {
-                "geneSetsCollection": "main",
-                "geneSet": "autism candidates from Sanders Neuron 2015",
-                "geneSetsTypes": []
-            },
-            "presentInParent": {
-                "presentInParent": [
-                    "neither"
-                ],
-            },
-            "effectTypes": [
-                "LGDs",
-            ]
-        }
+        query = copy.deepcopy(self.QUERY)
+        query['effectTypes'] = ['LGDs']
+
         response = self.client.post(self.TOOL_URL, query, format="json")
         self.assertEquals(status.HTTP_200_OK, response.status_code)
-        pprint(response.data)
         result = response.data['results'][0]
         self.assertEqual(result['effect'], 'LGDs')
         self.assertEqual(
@@ -58,27 +60,9 @@ class Test(BaseAuthenticatedUserTest):
         )
 
     def test_simple_query_missense(self):
-        query = {
-            "datasetId": "SSC",
-            "measureId": "ssc_hwhc.head_circumference",
-            "normalizeBy": [
-                "age",
-                "non-verbal iq"
-            ],
-            "geneSet": {
-                "geneSetsCollection": "main",
-                "geneSet": "autism candidates from Sanders Neuron 2015",
-                "geneSetsTypes": []
-            },
-            "presentInParent": {
-                "presentInParent": [
-                    "neither"
-                ],
-            },
-            "effectTypes": [
-                "Missense",
-            ]
-        }
+        query = copy.deepcopy(self.QUERY)
+        query['effectTypes'] = ['Missense']
+
         response = self.client.post(self.TOOL_URL, query, format="json")
         self.assertEquals(status.HTTP_200_OK, response.status_code)
         result = response.data['results'][0]
@@ -101,28 +85,9 @@ class Test(BaseAuthenticatedUserTest):
         )
 
     def test_simple_query_synonymous(self):
-        query = {
-            "datasetId": "SSC",
-            "measureId": "ssc_hwhc.head_circumference",
-            "normalizeBy": [
-                "age",
-                "non-verbal iq"
-            ],
-            "geneSet": {
-                "geneSetsCollection": "main",
-                "geneSet": "autism candidates from Sanders Neuron 2015",
-                "geneSetsTypes": []
-            },
-            "presentInParent": {
-                "presentInParent": [
-                    "neither"
-                ],
-            },
-            "effectTypes": [
-                "Synonymous",
-                #                 "CNV"
-            ]
-        }
+        query = copy.deepcopy(self.QUERY)
+        query['effectTypes'] = ['Synonymous']
+
         response = self.client.post(self.TOOL_URL, query, format="json")
         self.assertEquals(status.HTTP_200_OK, response.status_code)
         result = response.data['results'][0]
@@ -145,27 +110,9 @@ class Test(BaseAuthenticatedUserTest):
         )
 
     def test_simple_query_cnvs(self):
-        query = {
-            "datasetId": "SSC",
-            "measureId": "ssc_hwhc.head_circumference",
-            "normalizeBy": [
-                "age",
-                "non-verbal iq"
-            ],
-            "geneSet": {
-                "geneSetsCollection": "main",
-                "geneSet": "autism candidates from Sanders Neuron 2015",
-                "geneSetsTypes": []
-            },
-            "presentInParent": {
-                "presentInParent": [
-                    "neither"
-                ],
-            },
-            "effectTypes": [
-                "CNV"
-            ]
-        }
+        query = copy.deepcopy(self.QUERY)
+        query['effectTypes'] = ['CNV']
+
         response = self.client.post(self.TOOL_URL, query, format="json")
         self.assertEquals(status.HTTP_200_OK, response.status_code)
         result = response.data['results'][0]
