04ab52286da990027a13ccd97c4bd4bdfd47e9d5
Nikolay Dimitrov <nikidimi@gmail.com>
2017-07-15 18:18:52 +0300

    Issue #147: Splice-site fixed when no new splice site appears

--- DAE/variant_annotation/effect_checkers/splice_site.py
+++ DAE/variant_annotation/effect_checkers/splice_site.py
@@ -4,6 +4,9 @@ import logging
 
 
 class SpliceSiteEffectChecker:
+    def __init__(self, splice_site_length=2):
+        self.splice_site_length = splice_site_length
+
     def mutation_type(self, aaref, aaalt):
         if len(aaref) != len(aaalt):
             if "End" in aaalt:
@@ -26,18 +29,22 @@ class SpliceSiteEffectChecker:
         coding_regions = transcript_model.CDS_regions()
         prev = coding_regions[0].stop
 
-        for j in coding_regions:
-            logger.debug("pos: %d checking intronic region %d-%d",
-                         variant.position, prev, j.start)
-            if (prev <= variant.position <= j.start):
-                if (variant.position - prev < 3
-                        or j.start - variant.position < 3):
-                    # splice
-                    worstEffect = "splice-site"
-                else:
-                    # intron not splice
-                    worstEffect = "intron"
+        length = abs(len(variant.reference) - len(variant.alternate))
+        last_position = variant.position + length
 
+        for j in coding_regions:
+            logger.debug("pos: %d-%d checking intronic region %d-%d",
+                         variant.position, last_position, prev, j.start)
+
+            if (variant.position <= prev + self.splice_site_length
+                    and prev < last_position):
+                worstEffect = "splice-site"
+                ef = Effect(worstEffect, transcript_model)
+                return ef
+
+            if (variant.position < j.start
+                    and j.start - self.splice_site_length <= last_position):
+                worstEffect = "splice-site"
                 ef = Effect(worstEffect, transcript_model)
                 return ef
             prev = j.stop
