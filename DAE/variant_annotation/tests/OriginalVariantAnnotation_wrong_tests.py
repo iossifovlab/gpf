910cf79ac4d05acd741f1cf6688ea279fad8ccc3
Nikolay Dimitrov <nikidimi@gmail.com>
2017-07-15 21:38:26 +0300

    Issue #145: Test for the original code copied

---
+++ DAE/variant_annotation/tests/OriginalVariantAnnotation_wrong_tests.py
@@ -0,0 +1,28 @@
+import unittest
+from variant_annotation.annotator import VariantAnnotator as VariantAnnotation
+from DAE import genomesDB
+import pytest
+
+
+@pytest.fixture(scope="class")
+def genomes_DB(request):
+    request.cls.GA = genomesDB.get_genome()
+    request.cls.gmDB = genomesDB.get_gene_models()
+
+
+@pytest.mark.usefixtures("genomes_DB")
+class VariantAnnotationTest(unittest.TestCase):
+    def test_last_codon_ins_intergenic_var(self):
+        [effect] = VariantAnnotation.annotate_variant(self.gmDB, self.GA,
+                                                      loc="7:24727232",
+                                                      var="ins(A)")
+        self.assertEqual(effect.gene, None)
+        self.assertEqual(effect.transcript_id, None)
+        self.assertEqual(effect.strand, None)
+        self.assertEqual(effect.effect, "intergenic")
+        # self.assertEqual(effect.prot_pos, None)
+        # self.assertEqual(effect.prot_length, None)
+        self.assertEqual(effect.aa_change, None)
+
+if __name__ == "__main__":
+    unittest.main()
