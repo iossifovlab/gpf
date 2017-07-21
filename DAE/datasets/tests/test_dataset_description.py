aa58c704ab8dd5e1388f366b7c749c547ac306b6
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2017-07-21 12:20:09 +0300

    #155 support for dataset description page
    Task-Url: https://github.com/seqpipe/gpf/issues/155

---
+++ DAE/datasets/tests/test_dataset_description.py
@@ -0,0 +1,26 @@
+'''
+Created on Jul 21, 2017
+
+@author: lubo
+'''
+import pprint
+
+
+def test_datasets_description_simple(datasets_config):
+    pprint.pprint(datasets_config)
+
+    test = datasets_config.get_dataset_desc("TEST")
+    assert test is not None
+
+    print(test["description"])
+    assert test['description'] is not None
+
+
+def test_datasets_missing_description_simple(datasets_config):
+    pprint.pprint(datasets_config)
+
+    test = datasets_config.get_dataset_desc("SPARK")
+    assert test is not None
+
+    print(test["description"])
+    assert test['description'] is None
