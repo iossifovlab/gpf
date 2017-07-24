cf6b733ca6d516abab6f70d284afdfb97f998dda
Nikolay Dimitrov <nikidimi@gmail.com>
2017-07-24 22:13:01 +0300

    Issue #150: Fixed failing test due to splice-site definition change

--- DAE/variant_annotation/tests/OriginalVariantAnnotation_tests.py
+++ DAE/variant_annotation/tests/OriginalVariantAnnotation_tests.py
@@ -2214,31 +2214,6 @@ class VariantAnnotationTest(unittest.TestCase):
         # self.assertEqual(effects_sorted[1].prot_length, 412)
         self.assertEqual(effects_sorted[1].aa_change, None)
 
-    def test_chr13_21729291_ins_var(self):
-        var = "ins(AGTTTTCTTTGTTGCTGACATCTC" \
-            "GGATGTTCTGTCCATGTTTAAGGAACCTTTTACTGGGTGGCACTGCTTTAAT)"
-        effects = VariantAnnotation.annotate_variant(self.gmDB, self.GA,
-                                                     loc="13:21729291",
-                                                     var=var)
-        self.assertEqual(len(effects), 2)
-        effects_sorted = sorted(effects, key=lambda k: k.transcript_id)
-
-        self.assertEqual(effects_sorted[0].gene, "SKA3")
-        self.assertEqual(effects_sorted[0].transcript_id, "NM_001166017_1")
-        self.assertEqual(effects_sorted[0].strand, "-")
-        self.assertEqual(effects_sorted[0].effect, "splice-site")
-        # self.assertEqual(effects_sorted[0].prot_pos, 374)
-        # self.assertEqual(effects_sorted[0].prot_length, 389)
-        self.assertEqual(effects_sorted[0].aa_change, None)
-
-        self.assertEqual(effects_sorted[1].gene, "SKA3")
-        self.assertEqual(effects_sorted[1].transcript_id, "NM_145061_1")
-        self.assertEqual(effects_sorted[1].strand, "-")
-        self.assertEqual(effects_sorted[1].effect, "frame-shift")
-        # self.assertEqual(effects_sorted[1].prot_pos, 414)
-        # self.assertEqual(effects_sorted[1].prot_length, 412)
-        self.assertEqual(effects_sorted[1].aa_change, None)
-
     def test_chr20_5295014_ins_var(self):
         [effect] = VariantAnnotation.annotate_variant(self.gmDB, self.GA,
                                                       loc="20:5295014",
