8f201c5c5f04a2404aeb0c83270f6a3dafabbc74
Nikolay Dimitrov <nikidimi@gmail.com>
2017-07-25 19:16:10 +0300

    Issue #156: Unit tests for frame-shift mutations added

---
+++ DAE/variant_annotation/tests/frame_shift_tests.py
@@ -0,0 +1,58 @@
+import unittest
+import pytest
+from variant_annotation.annotator import Variant
+from mocks import ExonMock
+from mocks import TranscriptModelMock
+from mocks import ReferenceGenomeMock
+from mocks import AnnotatorMock
+from variant_annotation.effect_checkers.frame_shift \
+    import FrameShiftEffectChecker
+from variant_annotation.annotation_request import AnnotationRequestFactory
+
+
+@pytest.fixture(scope="class")
+def positive_strand(request):
+    request.cls.annotator = AnnotatorMock(ReferenceGenomeMock())
+    exons = [ExonMock(60, 70, 0),
+             ExonMock(80, 90, 1),
+             ExonMock(100, 110, 2)]
+    coding = [ExonMock(65, 70, 0),
+              ExonMock(80, 90, 1),
+              ExonMock(100, 110, 2)]
+    request.cls.tm = TranscriptModelMock("+", 65, 2000, exons, coding)
+    request.cls.effect_checker = FrameShiftEffectChecker()
+
+
+@pytest.mark.usefixtures("positive_strand")
+class FrameShiftPositiveStrandTest(unittest.TestCase):
+    def test_insertion_before_start_codon(self):
+        variant = Variant(loc="1:66", ref="", alt="A")
+        request = AnnotationRequestFactory.create_annotation_request(
+            self.annotator, variant, self.tm
+        )
+        effect = self.effect_checker.get_effect(request)
+        self.assertEqual(effect, None)
+
+    def test_insertion_after_start_codon(self):
+        variant = Variant(loc="1:65", ref="", alt="ABC")
+        request = AnnotationRequestFactory.create_annotation_request(
+            self.annotator, variant, self.tm
+        )
+        effect = self.effect_checker.get_effect(request)
+        self.assertEqual(effect.effect, "no-frame-shift")
+
+    def test_insertion_after_start_codon_2(self):
+        variant = Variant(loc="1:66", ref="", alt="BC")
+        request = AnnotationRequestFactory.create_annotation_request(
+            self.annotator, variant, self.tm
+        )
+        effect = self.effect_checker.get_effect(request)
+        self.assertEqual(effect.effect, "frame-shift")
+
+    def test_deletion_start_codon(self):
+        variant = Variant(loc="1:65", ref="ABC", alt="")
+        request = AnnotationRequestFactory.create_annotation_request(
+            self.annotator, variant, self.tm
+        )
+        effect = self.effect_checker.get_effect(request)
+        self.assertEqual(effect.effect, "no-frame-shift")
--- DAE/variant_annotation/tests/mocks.py
+++ DAE/variant_annotation/tests/mocks.py
@@ -6,16 +6,20 @@ class ExonMock:
 
 
 class TranscriptModelMock:
-    def __init__(self, strand, cds_start, cds_end, exons):
+    def __init__(self, strand, cds_start, cds_end, exons, coding=None):
         self.strand = strand
         self.cds = [cds_start, cds_end]
         self.exons = exons
         self.chr = "1"
         self.gene = "B"
         self.trID = "123"
+        if coding is None:
+            self.coding = self.exons
+        else:
+            self.coding = coding
 
     def CDS_regions(self):
-        return self.exons
+        return self.coding
 
 
 class ReferenceGenomeMock:
@@ -24,6 +28,11 @@ class ReferenceGenomeMock:
         return "".join([chr(i) for i in range(pos, pos_last + 1)])
 
 
+class CodeMock:
+    startCodons = ["ABC", "DEF"]
+
+
 class AnnotatorMock:
     def __init__(self, reference_genome):
         self.reference_genome = reference_genome
+        self.code = CodeMock()
