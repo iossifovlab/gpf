4f982c732f1e797ef4ecd7a79bbd59d26baec450
Nikolay Dimitrov <nikidimi@gmail.com>
2017-08-07 15:13:21 +0300

    Issue #191: Splice-site effect now has intron information and distance from donor/acceptor side

--- DAE/variant_annotation/effect_checkers/splice_site.py
+++ DAE/variant_annotation/effect_checkers/splice_site.py
@@ -7,13 +7,36 @@ class SpliceSiteEffectChecker:
         self.splice_site_length = splice_site_length
         self.logger = logging.getLogger(__name__)
 
+    def create_effect(self, request, position, start, end, index):
+        ef = Effect("splice-site", request.transcript_model)
+        ef.prot_pos = request.get_protein_position_for_pos(position)
+        ef.prot_length = request.get_protein_length()
+
+        dist_left = request.variant.position - start - 1
+        dist_right = end - request.variant.ref_position_last
+        ef.dist_from_coding = min(dist_left, dist_right)
+
+        ef.how_many_introns = len(request.transcript_model.exons) - 1
+        ef.intron_length = end - start - 1
+        if request.transcript_model.strand == "+":
+            ef.dist_from_acceptor = dist_right
+            ef.dist_from_donor = dist_left
+            ef.which_intron = index
+        else:
+            ef.dist_from_acceptor = dist_left
+            ef.dist_from_donor = dist_right
+            ef.which_intron = ef.how_many_introns - \
+                index + 1
+
+        return ef
+
     def get_effect(self, request):
         coding_regions = request.transcript_model.exons
         last_position = request.variant.position + \
             len(request.variant.reference)
         prev = None
 
-        for j in coding_regions:
+        for i, j in enumerate(coding_regions):
             if prev is None:
                 prev = j.stop
                 continue
@@ -24,17 +47,9 @@ class SpliceSiteEffectChecker:
 
             if (request.variant.position < prev + self.splice_site_length + 1
                     and prev + 1 < last_position):
-                worstEffect = "splice-site"
-                ef = Effect(worstEffect, request.transcript_model)
-                ef.prot_pos = request.get_protein_position_for_pos(prev)
-                ef.prot_length = request.get_protein_length()
-                return ef
+                return self.create_effect(request, prev, prev, j.start, i)
 
             if (request.variant.position < j.start
                     and j.start - self.splice_site_length < last_position):
-                worstEffect = "splice-site"
-                ef = Effect(worstEffect, request.transcript_model)
-                ef.prot_pos = request.get_protein_position_for_pos(j.start)
-                ef.prot_length = request.get_protein_length()
-                return ef
+                return self.create_effect(request, j.start, prev, j.start, i)
             prev = j.stop
