0753fc2139514b1913ff299df1e02740b98e42a2
Nikolay Dimitrov <nikidimi@gmail.com>
2017-08-07 16:49:57 +0300

    Fixes #192: prot_pos in splice_site now shows the first protein after the mutation

--- DAE/variant_annotation/effect_checkers/splice_site.py
+++ DAE/variant_annotation/effect_checkers/splice_site.py
@@ -7,9 +7,8 @@ class SpliceSiteEffectChecker:
         self.splice_site_length = splice_site_length
         self.logger = logging.getLogger(__name__)
 
-    def create_effect(self, request, position, start, end, index):
+    def create_effect(self, request, start, end, index):
         ef = Effect("splice-site", request.transcript_model)
-        ef.prot_pos = request.get_protein_position_for_pos(position)
         ef.prot_length = request.get_protein_length()
 
         dist_left = request.variant.position - start - 1
@@ -19,10 +18,12 @@ class SpliceSiteEffectChecker:
         ef.how_many_introns = len(request.transcript_model.exons) - 1
         ef.intron_length = end - start - 1
         if request.transcript_model.strand == "+":
+            ef.prot_pos = request.get_protein_position_for_pos(end)
             ef.dist_from_acceptor = dist_right
             ef.dist_from_donor = dist_left
             ef.which_intron = index
         else:
+            ef.prot_pos = request.get_protein_position_for_pos(start)
             ef.dist_from_acceptor = dist_left
             ef.dist_from_donor = dist_right
             ef.which_intron = ef.how_many_introns - \
@@ -47,9 +48,9 @@ class SpliceSiteEffectChecker:
 
             if (request.variant.position < prev + self.splice_site_length + 1
                     and prev + 1 < last_position):
-                return self.create_effect(request, prev, prev, j.start, i)
+                return self.create_effect(request, prev, j.start, i)
 
             if (request.variant.position < j.start
                     and j.start - self.splice_site_length < last_position):
-                return self.create_effect(request, j.start, prev, j.start, i)
+                return self.create_effect(request, prev, j.start, i)
             prev = j.stop
