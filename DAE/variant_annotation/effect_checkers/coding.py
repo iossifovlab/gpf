6cd1df0153447aad59405f0ab654085a20bb8b74
Nikolay Dimitrov <nikidimi@gmail.com>
2017-08-16 16:20:13 +0300

    Issue #205: Tests for non-coding transcripts added. Non-coding/non-coding introns detection improved

--- DAE/variant_annotation/effect_checkers/coding.py
+++ DAE/variant_annotation/effect_checkers/coding.py
@@ -8,11 +8,10 @@ class CodingEffectChecker:
         logger.debug("is coding=%s", request.transcript_model.is_coding())
         if not request.transcript_model.is_coding():
             all_regs = request.transcript_model.all_regions()
-            last_pos = request.variant.position + \
-                len(request.variant.reference)
+            last_pos = request.variant.corrected_ref_position_last
             for r in all_regs:
                 if (request.variant.position <= r.stop
-                        and r.start < last_pos):
+                        and r.start <= last_pos):
                     return EffectFactory.create_effect_with_request(
                         "non-coding", request
                     )
