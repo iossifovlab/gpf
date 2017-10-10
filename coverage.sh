40aba56a8c399e8c23e99d9f21c2ae2aa66678b6
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2017-10-10 18:40:14 +0300

    #43 experimental setup of jenkins on nemo
    Task-Url: https://github.com/seqpipe/deploy_tools/issues/43

---
+++ coverage.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+py.test -v --cov-config coveragerc \
+    --junitxml=wdae/junit.xml \
+    --cov common_reports_api \
+    --cov datasets_api \
+    --cov enrichment_api \
+    --cov family_counters_api \
+    --cov gene_sets \
+    --cov gene_weights \
+    --cov genotype_browser \
+    --cov groups_api \
+    --cov helpers \
+    --cov measures_api \
+    --cov pheno_browser_api \
+    --cov pheno_tool_api \
+    --cov precompute \
+    --cov preloaded \
+    --cov tools \
+    --cov users_api \
+    --cov common \
+    --cov datasets \
+    --cov enrichment_tool \
+    --cov gene \
+    --cov pheno \
+    --cov pheno_browser \
+    --cov pheno_tool \
+    --cov transmitted \
+    --cov utils \
+    wdae/
+
+
+py.test -v --cov-config coveragerc \
+    --junitxml=DAE/junit.xml \
+    --cov-append \
+    --cov-report html \
+    --cov-report xml \
+    --cov common_reports_api \
+    --cov datasets_api \
+    --cov enrichment_api \
+    --cov family_counters_api \
+    --cov gene_sets \
+    --cov gene_weights \
+    --cov genotype_browser \
+    --cov groups_api \
+    --cov helpers \
+    --cov measures_api \
+    --cov pheno_browser_api \
+    --cov pheno_tool_api \
+    --cov precompute \
+    --cov preloaded \
+    --cov tools \
+    --cov users_api \
+    --cov common \
+    --cov datasets \
+    --cov enrichment_tool \
+    --cov gene \
+    --cov pheno \
+    --cov pheno_browser \
+    --cov pheno_tool \
+    --cov transmitted \
+    --cov utils \
+    DAE/
