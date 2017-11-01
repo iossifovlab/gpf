09083d0dd4ca59c0c9cc0752ef1db01d213c4532
Nikolay Dimitrov <nikidimi@gmail.com>
2017-11-01 16:57:29 +0200

    Issue #139: Temporary workraround for newest RxJS version

--- gpfjs/src/main.ts
+++ gpfjs/src/main.ts
@@ -5,6 +5,9 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
 
+import 'rxjs/add/operator/takeUntil';
+import 'rxjs/add/operator/take';
+
 if (environment.production) {
   enableProdMode();
 }
