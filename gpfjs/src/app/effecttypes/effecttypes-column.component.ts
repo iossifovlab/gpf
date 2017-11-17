4ba12da458c8503366752c35d4a575d6cbbd48ed
Alexander Chernaev <alchernaev@gmail.com>
2017-11-17 12:04:42 +0200

    #40 Remove store from front-end
    Fixed effect types not displaying selected values

--- gpfjs/src/app/effecttypes/effecttypes-column.component.ts
+++ gpfjs/src/app/effecttypes/effecttypes-column.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, Output, EventEmitter, OnChanges } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { EffectTypes } from './effecttypes';
 
 
@@ -6,41 +6,21 @@ import { EffectTypes } from './effecttypes';
   selector: 'gpf-effecttypes-column',
   templateUrl: './effecttypes-column.component.html'
 })
-export class EffecttypesColumnComponent implements OnInit, OnChanges {
+export class EffecttypesColumnComponent implements OnInit {
   @Input() effectTypes: EffectTypes;
   @Input() columnName: string;
   @Input() effectTypesLabels: string[];
 
   @Output('effectTypeEvent') effectTypeEvent = new EventEmitter<any>();
 
-  effectTypesValues: boolean[];
-
-  ngOnChanges() {
-    this.effectTypesValues = new Array<boolean>(this.effectTypesLabels.length);
-    for (let i = 0; i < this.effectTypesValues.length; ++i) {
-      this.effectTypesValues[i] = false;
-    }
-
-    for (let value of this.effectTypes.selected) {
-      for (let i = 0; i < this.effectTypesLabels.length; ++i) {
-        if (value.indexOf(this.effectTypesLabels[i]) !== -1) {
-          this.effectTypesValues[i] = true;
-        } else {
-          this.effectTypesValues[i] = false;
-        }
-      }
-    }
-  }
-
   ngOnInit() {
   }
 
   checkEffectType(index: number, value: any) {
-    if (index < 0 || index > this.effectTypesValues.length) {
+    if (index < 0 || index > this.effectTypesLabels.length) {
       return;
     }
 
-    this.effectTypesValues[index] = value;
     this.effectTypeEvent.emit({
       'effectType': this.effectTypesLabels[index],
       'checked': value
