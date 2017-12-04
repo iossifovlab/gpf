f9120fca87d0fb248e2a20e7cb1f24c104a1b2c4
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2017-12-04 14:42:00 +0200

    review preloading and recreation of dataset's permissions...

--- wdae/datasets_api/datasets_preload.py
+++ wdae/datasets_api/datasets_preload.py
@@ -9,15 +9,17 @@ from datasets.datasets_factory import DatasetsFactory
 from datasets.config import DatasetsConfig
 from models import Dataset
 from django.db.utils import OperationalError, ProgrammingError
+from precompute.register import Precompute
 
 
-class DatasetsPreload(Preload):
+class DatasetsPreload(Preload, Precompute):
 
     def __init__(self):
         super(DatasetsPreload, self).__init__()
         self.dataset_config = DatasetsConfig()
         self.factory = DatasetsFactory(self.dataset_config)
 
+    def precompute(self):
         try:
             for ds in self.dataset_config.get_datasets(True):
                 Dataset.recreate_dataset_perm(ds['id'], ds['authorizedGroups'])
@@ -27,6 +29,16 @@ class DatasetsPreload(Preload):
         except ProgrammingError:
             # Database migrations are probably not run yet, ignore exception
             pass
+
+    def serialize(self):
+        return {}
+
+    def deserialize(self):
+        self.load()
+        return {}
+
+    def is_precomputed(self):
+        return self.is_loaded()
 
     def is_loaded(self):
         return True
