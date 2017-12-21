1faa7b948bbc371582e4d3daab46b526d001fb76
Alexander Chernaev <alchernaev@gmail.com>
2017-12-21 11:55:58 +0200

    Closes #374 Add data for vcf samples

--- DAE/converters/dae2vcf.py
+++ DAE/converters/dae2vcf.py
@@ -5,6 +5,7 @@ from datasets.datasets_factory import DatasetsFactory
 from datasets.datasets_factory import DatasetsConfig
 import pysam
 import re
+import itertools
 
 
 GA = genomesDB.get_genome()
@@ -49,27 +50,12 @@ class DaeToVcf(object):
         total = 0
         counts = 0
 
-        for variant in variants:
+        for variant in itertools.islice(variants, 1000): # FIXME: remove this
             total += 1
             try:
-                # variant.countsAtt = 'Count'
-                variant.counts
-                # print("dir one:", dir(variant))
-                # print('location', variant.location)
-                # print('family_id', variant.familyId)
-                # print('variant', variant.variant)
-                # print('bestSt', variant.bestSt, variant.bestStAtt)
-                # print('member', variant.memberInOrder)
-                # print('counts', variant.counts)
-                cohort |= set(variant.memberInOrder)
 
-                try:
-                    chromosome, position, reference, alternative = \
-                        vcfVarFormat(variant.location, variant.variant)
-                except Exception as e:
-                    print(e, variant.memberInOrder, variant.location, variant.familyId)
-                    continue
-                # print(chromosome, position, reference, alternative)
+                chromosome, position, reference, alternative = \
+                    vcfVarFormat(variant.location, variant.variant)
 
                 variant_records.append(VcfVariant(
                     chromosome=chromosome,
@@ -77,25 +63,30 @@ class DaeToVcf(object):
                     reference=reference,
                     alternative=alternative,
                     quality=100,
-                    info={'END': position + len(alternative) - 1},
+                    info={'END': position + len(alternative)},
                     format_={},
                     samples=[],
                     metadata=self._get_metadata_for_variant(variant)
                 ))
-                # break
-            except KeyError:
+                cohort |= set(variant.memberInOrder)
+            except (AssertionError, KeyError, NotImplementedError) as e:
+                print(e, variant)
                 continue
             counts += 1
 
-        cohort_set = set([x.personId for x in cohort])
-        assert len(cohort_set) == len(cohort)
+        cohort_set = set(['FAMILY_' + str(x.personId) for x in cohort])
+        assert len(cohort_set) == len(cohort), "cohort with different count"
+
+        print("Cohort length: {}".format(len(cohort)))
+        print("Variants count: {}", len(variant_records))
 
         print("{} variants with counts, {} total ({}%)".format(
-            counts, total, (counts / float(total)) * 100
+            counts, total,
+            (counts / float(total)) * 100 if total != 0 else float("inf")
         ))
 
         ordered_cohort = list(cohort)
-        samples_names = [x.personId for x in ordered_cohort]
+        samples_names = [str(x.personId) for x in ordered_cohort]
 
         for variant in variant_records:
             variant.samples = self._generate_samples(variant, ordered_cohort)
@@ -113,12 +104,10 @@ class DaeToVcf(object):
 
     @staticmethod
     def _generate_samples(variant, cohort):
+        empty = {}
         samples = []
         for member in cohort:
-            if member.personId in variant.metadata:
-                samples.append(variant.metadata[member.personId])
-            else:
-                samples.append({})
+            samples.append(variant.metadata.get(member.personId, empty))
 
         return samples
 
@@ -134,14 +123,16 @@ class DaeToVcf(object):
         elif ref == 1 and alt == 1:
             return ('0', '1')
         raise NotImplementedError(
-            'Unknown genotype - ref={}, alt={}'.format(ref, alt))
+            'Unknown genotype - ref={}, alt={}'.format(ref, alt)
+        )
 
     @staticmethod
     def _get_alleles_coverage_info(index, variant):
         assert len(variant.counts[0]) > index, 'Ref index out of bounds'
         assert len(variant.counts[1]) > index, 'Alt index out of bounds'
 
-        return (variant.counts[0][index], variant.counts[1][index])
+        return (str(variant.counts[0][index]), str(variant.counts[1][index]))
+
 
 class VcfWriter(object):
 
@@ -150,30 +141,28 @@ class VcfWriter(object):
         self.samples_labels = samples_labels
 
     def write_variants(self, variants_array):
-        chromosomes = [v.chromosome for v in variants_array]
-        header = self._prepare_header(set(chromosomes))
+        chromosomes = set(str(v.chromosome) for v in variants_array)
+        header = self._prepare_header(chromosomes)
         variants_array = sorted(
             variants_array, key=lambda v: (v.chromosome, v.position))
 
-        # FIXME: add samples
-
-        vcf_file = pysam.VariantFile(self.filename, 'w', header=header)
-        rows = []
+        vcf_file = pysam.VariantFile(self.filename, 'wb', header=header)
+        print(header.info.header)
 
         for variant in variants_array:
-            rows.append(header.new_record(
+
+            row = vcf_file.new_record(
                 contig=variant.chromosome,
                 alleles=(variant.reference, variant.alternative),
                 filter=variant.filter,
                 id=variant.id,
-                start=variant.position - 1,
-                stop=variant.position + 1,
+                start=variant.position,
+                stop=variant.info['END'],
                 qual=variant.quality,
                 info=variant.info,
                 samples=variant.samples
-            ))
+            )
 
-        for row in rows:
             vcf_file.write(row)
 
         vcf_file.close()
@@ -181,14 +170,15 @@ class VcfWriter(object):
     def _prepare_header(self, additional_chromosomes=set()):
         header = pysam.VariantHeader()
         header.add_meta('fileformat', value='VCFv4.1')
-        header.formats.add('GT', 1, 'String', 'Genotype')
+        header.formats.add('GT', 2, 'String', 'Genotype')
         header.formats.add(
-            'AD', '.', 'Integer',
+            'AD', '.', 'String',
             'Allelic depths for the ref and alt alleles in the order listed')
-        header.info.add('END', 1, 'Integer', 'Stop position of the interval')
 
         for sample in self.samples_labels:
             header.add_sample(sample)
+
+        header.info.add('END', 1, 'Integer', 'Stop position of the interval')
 
         chromosomes = set([str(num) for num in range(1, 23)] + ['X', 'Y'])
         chromosomes |= additional_chromosomes
@@ -222,6 +212,11 @@ class VcfVariant(object):
         self.samples = samples
         self.metadata = metadata
 
+    def __repr__(self):
+        return '{}:{} {}->{} ({}){}'.format(
+            self.chromosome, self.position, self.reference, self.alternative,
+            len(self.samples), self.samples)
+
 
 class VcfVariantSample(object):
 
@@ -252,7 +247,7 @@ def vcfVarFormat(loc, var):
         reference = GA.getSequence(chr, pos-1, pos+l-1)
         return chr, pos-1, reference, reference[0]
 
-    raise Exception('weird variant:' + var)
+    raise NotImplementedError('weird variant: ' + var)
 
 
 def main():
