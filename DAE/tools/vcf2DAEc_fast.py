96c0df1ee9e32f0143e2dd0276dc129f00d4d827
Nikolay Dimitrov <nikidimi@gmail.com>
2018-01-05 18:14:10 +0200

    Issue #384: Refactored vcf2DAEc to use functions and classes

--- DAE/tools/vcf2DAEc_fast.py
+++ DAE/tools/vcf2DAEc_fast.py
@@ -18,6 +18,148 @@ import vrtIOutil as vIO
 
 # add more data on fam Info
 
+class Family:
+    def __init__(self, familyInfo):
+        
+        self.familyInfo = familyInfo
+
+    def is_mendelian(self, arr):
+        if ((arr[2] == arr[0] or arr[3] == arr[0]) and
+                (arr[4] == arr[1] or arr[5] == arr[1])):
+            return True
+        if ((arr[2] == arr[1] or arr[3] == arr[1]) and
+                (arr[4] == arr[0] or arr[5] == arr[0])):
+            return True
+        if arr[0] != arr[1]:
+            if ((arr[2] == arr[0] and arr[3] == arr[1]) or
+                    (arr[2] == arr[1] and arr[3] == arr[0])):
+                if arr[4] != arr[5]:
+                    return True
+            if ((arr[4] == arr[0] and arr[5] == arr[1]) or
+                    (arr[4] == arr[1] and arr[5] == arr[0])):
+                if arr[2] != arr[3]:
+                    return True
+        return False
+
+    def set_indicies(self, individualToIndex):
+        indicies = []
+        simple_indicies = []
+        for personId in self.familyInfo['ids']:
+            index = individualToIndex[personId] * 2
+            indicies.append(index)
+            indicies.append(index + 1)
+
+            simple_indicies.append(individualToIndex[personId])
+
+        self.indicies = np.array(indicies)
+        self.simple_indicies = np.array(simple_indicies)
+
+        self.families = []
+        for iFM in self.familyInfo['iFM']:
+            indicies = []
+            for familyIndex in iFM:
+                personId = self.familyInfo['ids'][familyIndex]
+                index = individualToIndex[personId] * 2
+                indicies.append(index)
+                indicies.append(index + 1)
+            self.families.append(indicies)
+
+        parent_indicies = []
+        print(self.familyInfo['notChild'])
+        for personId in self.familyInfo['notChild']:
+            index = individualToIndex[self.familyInfo['ids'][personId]] * 2
+            parent_indicies.append(index)
+            parent_indicies.append(index + 1)
+
+        self.parent_indicies = np.array(parent_indicies)
+
+    def is_denovo(self, variant):
+        for family in self.families:
+            arr = variant[family]
+            if not self.is_mendelian(arr):
+                #print(self.familyInfo['newFid'])
+                return True
+        return False
+
+    def variant_present(self, variant, index):
+        data = variant.gt_idxs[self.indicies]
+        return any(data == index)
+
+    def variant_present_in_parent(self, variant, index):
+        data = variant.gt_idxs[self.parent_indicies]
+        return np.sum(data == index)
+
+    def generate_gt(self, variant, index):
+        refStr = []
+        altStr = []
+        addStr = []
+        additional = False
+
+        data = variant.gt_idxs[self.indicies]
+
+        for i in range(0, len(data), 2):
+            if data[i] == 0 and data[i + 1] == 0:
+                refStr.append('2')
+                altStr.append('0')
+                addStr.append('0')
+            elif data[i] == 0 or data[i + 1] == 0:
+                if data[i] == index or data[i + 1] == index:
+                    refStr.append('1')
+                    altStr.append('1')
+                    addStr.append('0')
+                else:
+                    refStr.append('1')
+                    altStr.append('0')
+                    addStr.append('1')
+                    additional = True
+            else:
+                if data[i] == index and data[i + 1] == index:
+                    refStr.append('0')
+                    altStr.append('2')
+                    addStr.append('0')
+                elif data[i] == index or data[i + 1] == index:
+                    refStr.append('0')
+                    altStr.append('1')
+                    addStr.append('1')
+                    additional = True
+                else:
+                    refStr.append('0')
+                    altStr.append('0')
+                    addStr.append('2')
+                    additional = True
+
+        if additional:
+            return "{}/{}/{}".format(''.join(refStr),
+                                     ''.join(altStr),
+                                     ''.join(addStr))
+        else:
+            return "{}/{}".format(''.join(refStr), ''.join(altStr))
+
+    def generate_cnt(self, variant, index, altsCount):
+        refCnt = ' '.join([str(i)
+                          for i in
+                          variant.gt_ref_depths[self.simple_indicies]])
+        altCnt = ' '.join([str(variant.gt_alt_depths[i + index])
+                          for i in self.simple_indicies * (altsCount + 1)])
+
+        addCnt = []
+        additional = False
+
+        if altsCount > 1:
+            for i in self.simple_indicies * (altsCount + 1):
+                r = [c for c in range(i + 1, i + altsCount + 1)
+                     if c != i + index]
+                addSum = np.sum(variant.gt_alt_depths[r])
+                addCnt.append(str(addSum))
+
+                if addSum > 0:
+                    additional = True
+        
+        if additional:
+            addCnt = ' '.join(addCnt)
+            return "{}/{}/{}".format(refCnt, altCnt, addCnt)
+        return "{}/{}".format(refCnt, altCnt)
+
 
 def makeFamInfoConv(fInfo, pInfo):
     for k, v in fInfo.items():
@@ -39,109 +181,6 @@ def makeFamInfoConv(fInfo, pInfo):
         v['notChild'] = numpy.array(nC)
 
     # print fInfo
-
-
-def array2str(mx, ix, delim=''):
-    n0, n1, n2 = mx[:, 0], mx[:, ix], mx.sum(1)
-    n2 = n2 - n0 - n1
-
-    s0 = map(str, n0)
-    s1 = map(str, n1)
-    s2 = map(str, n2)
-
-    strx = delim.join(s0) + '/' + delim.join(s1)
-    if sum(n2) > 0:
-        strx += ('/' + delim.join(s2))
-
-    return strx
-
-
-def fixNonAutoX(familyVariant):  # fam, pInfo ):
-    # Sex (1=male; 2=female; other=unknown)
-    # assume [numFam]x[genotype count] and genotype are 0,1,2
-
-    GT = familyVariant.GT
-    isM = familyVariant.familyInfo['isMale']
-
-    for n, im in enumerate(isM):
-        if im == 0:
-            continue  # female
-
-        if sum(GT[n, :]) == 2 and sum(GT[n, :] == 1) > 0:
-            return False
-        else:
-            GT[n, GT[n, :] == 2] = 1
-
-    return True
-
-# possible hap state
-
-
-def hapState(cn):
-    if cn == 0:
-        return [0]
-    if cn == 1:
-        return [0, 1]
-
-    return [1]
-
-
-# mendel State
-#
-#(fa, ma, child) copy number
-#       (ma,fa):(child) -- number of alleles
-mStat = {  \
-    # regular autosome 2 copies for all
-    (2, 2, 2): {\
-        (2, 2): [2],  (2, 1): [1, 2],  (2, 0): [1],  \
-        (1, 2): [1, 2], (1, 1): [0, 1, 2], (1, 0): [0, 1],\
-        (0, 2): [1],  (0, 1): [0, 1],  (0, 0): [0]},\
-    # X and male child, only from ma
-    (1, 2, 1): {\
-        (1, 2): [1], (1, 1): [0, 1], (1, 0): [0],   \
-        (0, 2): [1], (0, 1): [0, 1], (0, 0): [0]}, \
-    # X and female child
-    (1, 2, 2): {\
-        (1, 2): [2], (1, 1): [1, 2], (1, 0): [1],  \
-        (0, 2): [1], (0, 1): [0, 1], (0, 0): [0]}}
-
-# primitive version of checking de-novo
-# autosome
-
-
-def isDenovo(st, fm):  # fm : fa and ma index
-    mdl = mStat[(2, 2, 2)]
-
-    for c, f, m in fm:  # fa ma child index
-        for n, s in enumerate(st[c, :]):
-            if s not in mdl[(st[f, n], st[m, n])]:
-                # print st
-                return True
-            # print f,m,c,n,s, (st[f,n],st[m,n])
-
-    return False
-
-
-def printSome(output, pos=None, wsize=1000):
-    if pos == None:
-        for k in sorted(output.keys()):
-            v = output[k]
-            print >> v[0], v[1]
-
-        output.clear()
-        return
-
-    dk = []
-    for k in sorted(output.keys()):
-        if k[0] > pos - wsize:
-            break
-
-        dk.append(k)
-        v = output[k]
-        print >> v[0], v[1]
-
-    for k in dk:
-        del output[k]
 
 
 def famInVCF(fInfo, vcf):
@@ -222,182 +261,126 @@ def main():
     # setup to print transmission files
     OUT = ox.outputPrefix + '.txt'
     TOOMANY = ox.outputPrefix + '-TOOMANY.txt'
-    out = open(OUT, 'w')
-    outTOOMANY = open(TOOMANY, 'w')
+    with open(OUT, 'w') as out, open(TOOMANY, 'w') as outTOOMANY:
+        print >> out, '\t'.join(
+            'chr,position,variant,familyData,all.nParCalled,all.prcntParCalled,all.nAltAlls,all.altFreq'.split(','))
+        print >> outTOOMANY, '\t'.join(
+            'chr,position,variant,familyData'.split(','))
 
-    print >> out, '\t'.join(
-        'chr,position,variant,familyData,all.nParCalled,all.prcntParCalled,all.nAltAlls,all.altFreq'.split(','))
-    print >> outTOOMANY, '\t'.join(
-        'chr,position,variant,familyData'.split(','))
+        #fam = [x for x in sorted(fInfo.keys())]
+        #vf = pysam.VariantFile( dfile )
+        vf = VCF(dfile)
+        fam = famInVCF(fInfo, vf)
+        fam_count = len(fam)
 
-    #fam = [x for x in sorted(fInfo.keys())]
-    #vf = pysam.VariantFile( dfile )
-    vf = VCF(dfile)
-    fam = famInVCF(fInfo, vf)
-    fam_count = len(fam)
+        # print family Info in a format
+        FAMOUT = ox.outputPrefix + '-families.txt'
+        printFamData(fInfo, pInfo, proj=ox.project, lab=ox.lab,
+                    listFam=fam, out=open(FAMOUT, 'w'))
 
-    # print family Info in a format
-    FAMOUT = ox.outputPrefix + '-families.txt'
-    printFamData(fInfo, pInfo, proj=ox.project, lab=ox.lab,
-                 listFam=fam, out=open(FAMOUT, 'w'))
+        individualToFamily = dict()
+        individualToIndex = dict()
 
-    individualToFamily = dict()
-    individualToIndex = dict()
+        samples_arr = np.asarray(vf.samples)
+        for i, sampleId in enumerate(vf.samples):
+            individualToIndex[sampleId] = i
 
-    samples_arr = np.asarray(vf.samples)
-    for i, sampleId in enumerate(vf.samples):
-        individualToIndex[sampleId] = i
-
-    for familyId in fam:
-        familyInfo = fInfo[familyId]
-
-        indicies = []
-        for personId in familyInfo['ids']:
-            individualToFamily[personId] = familyId
-            index = individualToIndex[personId] * 2
-            indicies.append(index)
-            indicies.append(index + 1)
-
-        familyInfo['indicies'] = np.array(indicies)
-
-    output = {}
-    for variant in vf:
-        missingIndexes = np.where(variant.gt_types == 2)
-        missingIds = samples_arr[missingIndexes]
-
-        pgt = (1 - missingIndexes[0].shape[0] / len(samples_arr)) * 100
-        if pgt < ox.minPercentOfGenotypeSamples:
-            continue
-
-        chrom = variant.CHROM
-        px, vx = vrtF.vcf2cshlFormat2(variant.POS, variant.REF, variant.ALT)
-
-        families = []
-
-        altsCount = len(variant.ALT)
-        interestingIndexes = np.where(np.logical_or(variant.gt_types == 1,
-                                                    variant.gt_types == 3))
-        interestingIds = samples_arr[interestingIndexes]
-        interestingFamilies = {individualToFamily[individual]
-                               for individual in interestingIds
-                               if individual in individualToFamily}
-
-        missingFamilies = {individualToFamily[individual]
-                           for individual in missingIds
-                           if individual in individualToFamily}
-
-        dx = []
-
-        count = fam_count - len(missingFamilies)
-        #print(interestingFamilies)
-        #print(missingFamilies)
-
-        for familyId in sorted(interestingFamilies - missingFamilies):
+        for familyId in fam:
             familyInfo = fInfo[familyId]
-            fam = familyInfo['ids']
-            GT = np.zeros((len(fam), altsCount + 1,), dtype=numpy.int)
+            family = Family(familyInfo)
+
+            indicies = []
+            for personId in familyInfo['ids']:
+                individualToFamily[personId] = family
+
+            family.set_indicies(individualToIndex)
+
+        output = {}
+        for variant in vf:
+            missingIndexes = np.where(variant.gt_types == 2)
+            missingIds = samples_arr[missingIndexes]
+
+            pgt = (1 - missingIndexes[0].shape[0] / len(samples_arr)) * 100
+            if pgt < ox.minPercentOfGenotypeSamples:
+                continue
+
+            chrom = variant.CHROM
+            px, vx = vrtF.vcf2cshlFormat2(variant.POS, variant.REF, variant.ALT)
+
+            altsCount = len(variant.ALT)
             cnt = np.zeros((len(fam), altsCount + 1,), dtype=numpy.int)
-            for i, personId in enumerate(fam):
-                index = individualToIndex[personId]
-                gt_type = variant.gt_types[index]
+            interestingIndexes = np.where(np.logical_or(variant.gt_types == 1,
+                                                        variant.gt_types == 3))
+            interestingIds = samples_arr[interestingIndexes]
+            interestingFamilies = {individualToFamily[individual]
+                                for individual in interestingIds
+                                if individual in individualToFamily}
 
-                #print(fam, personId, variant.gt_types[index])
+            missingFamilies = {individualToFamily[individual]
+                            for individual in missingIds
+                            if individual in individualToFamily}
 
-                p = 1
-                if gt_type == 0:
-                    GT[i][0] = 2
+            count = fam_count - len(missingFamilies)
+            #print(interestingFamilies)
+            #print(missingFamilies)
+
+            families = [family
+                        for family in sorted(interestingFamilies - missingFamilies,
+                                            key=lambda(x): x.familyInfo['newFid'])
+                        if not family.is_denovo(variant.gt_idxs)]
+
+            dnv_count = len(interestingFamilies - missingFamilies) - len(families)
+
+            l = [(n, p, v) for n, (p, v) in enumerate(izip(px, vx))]
+
+            for (n, p, v) in sorted(l, key=lambda x: (x[1], x[2])):
+                if v.find('*') >= 0:
+                        continue
+                strx = []
+
+                cnt_in_parent = 0
+
+                for family in families:
+                    familyInfo = family.familyInfo
+
+                    if not family.variant_present(variant, n + 1):
+                        continue
+
+                    GT = family.generate_gt(variant, n + 1)
+                    cnt = family.generate_cnt(variant, n + 1, altsCount)
+
+                    cnt_in_parent += family.variant_present_in_parent(
+                        variant, n + 1)
+                    strx.append("{}:{}:{}".format(familyInfo['newFid'], GT, cnt))
+                if len(strx) < 1:
+                    continue
+
+                l = len(strx)
+                strx = ';'.join(strx)
+                
+                # print strx
+
+                tAll = 4 * len(families)
+                tAll += 4 * (fam_count - len(missingFamilies) - len(interestingFamilies - missingFamilies))
+                nPC = (count - dnv_count) * 2
+                #print(count, fam_count, len(missingFamilies), len(interestingFamilies), len(families), dnv_count)
+                nPcntC = (count - dnv_count) / fam_count * 100
+                cAlt = cnt_in_parent
+                freqAlt = (1. * cAlt) / tAll * 100.
+
+                if v.startswith('complex') or (p, v) in output:
+                    print >> sys.stdout, '\t'.join([chrom, str(p), v, strx, str(
+                        nPC), digitP(nPcntC), str(cAlt), digitP(freqAlt)])
+                    continue
+
+                if l >= ox.tooManyThresholdFamilies:
+                    print >> out, '\t'.join([chrom, str(p), v, 'TOOMANY', str(
+                        nPC), digitP(nPcntC), str(cAlt), digitP(freqAlt)])
+                    print >> outTOOMANY, '\t'.join([chrom, str(p), v, strx])
                 else:
-                    p = variant.gt_idxs[index * 2]
-                    GT[i][p] += 1
-                    p = variant.gt_idxs[index * 2 + 1]
-                    GT[i][p] += 1
-          
-                cnt[i][0] = variant.gt_ref_depths[index]
-
-                for k in range(1, altsCount + 1):
-                    cnt[i][k] = variant.gt_alt_depths[index * (altsCount + 1) + k]
-
-            flag = isDenovo(GT, familyInfo['iFM'])  # isDenovo( GT ):
-            if flag:
-                count -= 1
-                continue  # denovo
-
-            # NOTE: main data set
-            #dx.append( [fIx['newFid'], GT, cnt, qual] )
-            dx.append([familyInfo['newFid'], GT, cnt, familyInfo['notChild']])
-
-        if len(dx) < 1:
-            continue
-        # skip if failed families are more than certain threshold
-        # if len(dx)/(1.*len(fam))*100. < ox.minPercentOfGenotypeFamilies: continue
-        #print("bef", count)
-
-        #print(len(interestingFamilies), len(missingFamilies), len(dx), count)
-
-        nPC = 2 * count
-        nPcntC = (1. * nPC) / (2 * fam_count) * 100.
-        for n, (p, v) in enumerate(izip(px, vx)):
-            ix = n + 1
-            # ref is index 0 and vx (variants) has only alternatives
-
-            # some occasion '*' as one of alternatives
-            if v.find('*') >= 0:
-                continue
-
-            strx = []
-            cAlt = 0
-            tAll = 4 * len(dx)
-            # dx: fid, GT, cnt, notChild(index who is not child of any in this family)
-            for (fid, GT, cnt, nCi) in dx:
-                # assume that all the ill-regualr genotype for X Y are correct
-                # assume that all of auto have 2 copy, X non-Autosomal 1, and Y male(1) and female(0)
-                #tAll += sum(GT[nCi, :].sum(1))
-                #print(GT)
-                #print(nCi)
-                #print(GT[nCi, :])
-                #print(sum(GT[nCi, :].sum(1)))
-                if sum(GT[:, ix]) < 1:
-                    continue              
-
-                cAlt += sum(GT[nCi, ix])
-                # only concern about non children
-
-                strx.append(fid + ':' + array2str(GT, ix, delim='') +
-                            ':' + array2str(cnt, ix, delim=' '))
-
-            fC = len(strx)
-            if fC < 1:
-                continue
-
-            strx = ';'.join(strx)
-            # print strx
-
-            tAll += 4 * (fam_count - len(missingFamilies) - len(interestingFamilies - missingFamilies))
-
-            # chr,position,variant,familyData,all.nParCalled,all.prcntParCalled,all.nAltAlls,all.altFreq
-            # fid:bestState:counts:qualityScore
-            # 11948:2112/0110:40 20 20 40/0 20 20 0:0:0;... writing format
-            freqAlt = (1. * cAlt) / tAll * 100.
-
-            #pix = 0
-            #while (p,pix) in output: pix += 1
-
-            if v.startswith('complex') or (p, v) in output:
-                print >> sys.stdout, '\t'.join([chrom, str(p), v, strx, str(
-                    nPC), digitP(nPcntC), str(cAlt), digitP(freqAlt)])
-                continue
-
-            if fC >= ox.tooManyThresholdFamilies:
-                output[(p, v)] = (out, '\t'.join([chrom, str(p), v, 'TOOMANY', str(
-                    nPC), digitP(nPcntC), str(cAlt), digitP(freqAlt)]))
-                output[(p, v + '*')] = (outTOOMANY,
-                                        '\t'.join([chrom, str(p), v, strx]))
-            else:
-                output[(p, v)] = (out, '\t'.join([chrom, str(p), v, strx, str(
-                    nPC), digitP(nPcntC), str(cAlt), digitP(freqAlt)]))
-    
-        printSome(output)
-
+                    print >> out, '\t'.join([chrom, str(p), v, strx, str(
+                        nPC), digitP(nPcntC), str(cAlt), digitP(freqAlt)])
 
 if __name__ == "__main__":
     main()
+
