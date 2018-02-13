1e9c14ed51789c7458d89f40426b8e520eb62fd1
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-02-13 19:55:12 +0200

    #403 experiment with new variants query interface
    Task-Url: https://github.com/seqpipe/gpf/issues/403

--- DAE/variants/default_settings.py
+++ DAE/variants/default_settings.py
@@ -3,8 +3,14 @@ Created on Feb 7, 2018
 
 @author: lubo
 '''
+import os
 
+DATA_DIR = os.environ.get(
+    "DAE_DATA_DIR",
+    "/home/lubo/Work/seq-pipeline/data-variants/"
+)
 
-DATA_DIR = "/home/lubo/Work/seq-pipeline/data-variants/"
-
-CONFIG_FILE = "variants.conf"
+CONFIG_FILE = os.environ.get(
+    "DAE_CONFIG_FILE",
+    "variants.conf"
+)
