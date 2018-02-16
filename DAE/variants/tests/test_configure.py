5307afb382af594e89e34ed9760c11734e2705c2
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-02-16 12:23:33 +0200

    #403 experiment with new variants query interface
    Task-Url: https://github.com/seqpipe/gpf/issues/403

--- DAE/variants/tests/test_configure.py
+++ DAE/variants/tests/test_configure.py
@@ -9,8 +9,3 @@ from variants.configure import Configure
 def test_default_configure():
     conf = Configure.from_file()
     assert conf is not None
-
-
-def test_default_study():
-    conf = Configure.from_file()
-    print(conf['study.uagre'].to_dict())
