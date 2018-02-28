98ad0bfa5182501f377792a195c133a16f45181e
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-02-28 08:01:51 +0200

    #433 get_gene_sets_symNS name error in DAE
    Task-Url: https://github.com/seqpipe/gpf/issues/433

--- DAE/tests/test_gene_sets_syms.py
+++ DAE/tests/test_gene_sets_syms.py
@@ -9,3 +9,5 @@ from DAE import get_gene_sets_symNS
 def test_gene_sets_sym():
     main_sets = get_gene_sets_symNS('main')
     assert main_sets is not None
+    print(main_sets.t2G.keys())
+    assert "autism candidates from Iossifov PNAS 2015" in main_sets.t2G
