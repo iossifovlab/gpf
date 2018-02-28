978b43dd4ded0eb92cce19f8ed5505812fc15547
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-02-28 08:32:02 +0200

    #433 get_gene_sets_symNS name error in DAE
    Task-Url: https://github.com/seqpipe/gpf/issues/433

--- DAE/DAE.py
+++ DAE/DAE.py
@@ -29,24 +29,24 @@ vDB = VariantsDB(config.daeDir,
                  giDB=giDB, phDB=None, genomesDB=genomesDB)
 
 
-# def get_gene_sets_symNS(geneSetsDef, denovoStudies=None):
-#     if geneSetsDef == 'denovo':
-#         geneTerms = vDB.get_denovo_sets(denovoStudies)
-#     else:
-#         try:
-#             geneTerms = giDB.getGeneTerms(geneSetsDef)
-#         except:
-#             geneTerms = loadGeneTerm(geneSetsDef)
-# 
-#         if geneTerms.geneNS == 'id':
-#             def rF(x):
-#                 if x in giDB.genes:
-#                     return giDB.genes[x].sym
-#             geneTerms.renameGenes("sym", rF)
-# 
-#         if geneTerms.geneNS != 'sym':
-#             raise Exception('Only work with id or sym namespace')
-#     return geneTerms
+def get_gene_sets_symNS(geneSetsDef, denovoStudies=None):
+    if geneSetsDef == 'denovo':
+        geneTerms = vDB.get_denovo_sets(denovoStudies)
+    else:
+        try:
+            geneTerms = giDB.getGeneTerms(geneSetsDef)
+        except:
+            geneTerms = loadGeneTerm(geneSetsDef)
+ 
+        if geneTerms.geneNS == 'id':
+            def rF(x):
+                if x in giDB.genes:
+                    return giDB.genes[x].sym
+            geneTerms.renameGenes("sym", rF)
+ 
+        if geneTerms.geneNS != 'sym':
+            raise Exception('Only work with id or sym namespace')
+    return geneTerms
 
 pheno = PhenoFactory()
 
