89fc8fd97c2f749060f3f554fee44aabaadf76f4
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-03-19 10:38:19 +0200

    #472 review and test frequency calculation problems with spark
    Task-Url: https://github.com/seqpipe/gpf/issues/472

--- DAE/variants/tests/test_with_vcf_fixture.py
+++ DAE/variants/tests/test_with_vcf_fixture.py
@@ -40,3 +40,25 @@ def test_members_in_order2_genotype(sample_vcf):
         print(v, mat2str(v.best_st), mat2str(v.gt), v.inheritance)
         assert 'gpa' in v.variant_in_members
         assert Role.paternal_grandfather in v.variant_in_roles
+
+
+def test_freq_trios_2(sample_vcf):
+    fvars = sample_vcf("fixtures/trios2")
+    vs = list(fvars.query_variants(family_ids=['f1']))
+    assert len(vs) == 2
+
+    v1 = vs[0]
+    v2 = vs[1]
+
+    print(v1, mat2str(v1.best_st), mat2str(v1.gt), v1.inheritance)
+    assert v1.get_attr('all.nAltAlls') == 2
+    assert v1.get_attr('all.altFreq') == 25
+
+    assert Role.mom in v1.variant_in_roles
+    assert 'mom1' in v1.variant_in_members
+
+    print(v2, mat2str(v2.best_st), mat2str(v2.gt), v2.inheritance)
+    assert v1.get_attr('all.nAltAlls') == 2
+    assert v1.get_attr('all.altFreq') == 25
+    assert Role.dad in v2.variant_in_roles
+    assert 'dad1' in v2.variant_in_members
