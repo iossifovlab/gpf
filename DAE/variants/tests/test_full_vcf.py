7bf0b7df0c52e5286045504d6660f0e213e1ff5a
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-03-19 10:21:27 +0200

    #472 review and test frequency calculation problems with spark
    Task-Url: https://github.com/seqpipe/gpf/issues/472

--- DAE/variants/tests/test_full_vcf.py
+++ DAE/variants/tests/test_full_vcf.py
@@ -8,25 +8,29 @@ from variants.vcf_utils import mat2str
 
 
 def test_open_raw_vcf_with_region(vcf19r):
-    region = "7:120008905-121036422"
+    region = "7:121000000-121010000"
     fvars = vcf19r(region)
     assert fvars is not None
 
     vs = fvars.query_variants(
-        role='prb',
-        effect_types=['frame-shift', 'nonsense', 'splice-site'])
+        inheritance='mendelian')
     for c, v in enumerate(vs):
         print(c, v, v.family_id, mat2str(v.best_st), v.inheritance,
-              v.effect_type, v.effect_gene)
+              v.effect_type, v.effect_gene,
+              v.get_attr('all.nAltAlls'), v.get_attr('all.altFreq'))
+        assert v.get_attr('all.nAltAlls') > 0
 
 
 def test_raw_vcf_bad_frequency(vcf19r):
     fvars = vcf19r("1:897008-897010")
     vs = fvars.query_variants(
-        role='prb',
         effect_types=['frame-shift', 'nonsense', 'splice-site'])
     for c, v in enumerate(vs):
         print(c, v, v.family_id, mat2str(v.best_st), v.inheritance,
               v.effect_type, v.effect_gene,
               v.get_attr('all.nAltAlls'), v.get_attr('all.altFreq'))
         print(v.members_in_order)
+        print(v.variant_in_roles)
+        print(v.variant_in_members)
+
+        assert v.get_attr('all.nAltAlls') > 0
