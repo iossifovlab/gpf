eb3548e01b3aee61c0f1e719b25d19daacca1305
Emil Gotsev <egotsev@gmail.com>
2018-03-23 12:03:07 +0200

    Ref #422 adding dbSNP annotator to annotation pipeline

--- DAE/tools/annotation_pipeline.py
+++ DAE/tools/annotation_pipeline.py
@@ -11,6 +11,7 @@ import pysam
 from annotate_variants import EffectAnnotator
 from add_missense_scores import MissenseScoresAnnotator
 from annotate_with_genomic_scores import GenomicScoresAnnotator
+from annotate_dbSNP import DbSNPAnnotator
 
 def str_to_class(val):
     return reduce(getattr, val.split("."), sys.modules[__name__])
