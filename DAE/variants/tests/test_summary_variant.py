be6b9b74062e8f59948a85aee148367e9284e1eb
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-04-02 08:50:25 +0300

    #460 annotate test vcf variants and distribute annotations
    Task-Url: https://github.com/seqpipe/gpf/issues/460

--- DAE/variants/tests/test_summary_variant.py
+++ DAE/variants/tests/test_summary_variant.py
@@ -4,6 +4,7 @@ Created on Mar 23, 2018
 @author: lubo
 '''
 from variants.variant import EffectGene, SummaryVariant
+import pytest
 
 
 def test_effect_gene_serialization():
@@ -12,6 +13,7 @@ def test_effect_gene_serialization():
     assert str(eg) == 'a:b'
 
 
+@pytest.mark.slow
 def test_summary_variants_full(nvcf19f):
     for _index, row in nvcf19f.annot_df.iterrows():
         sv = SummaryVariant.from_dict(row)
