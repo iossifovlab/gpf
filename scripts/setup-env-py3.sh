0655d0bed477be3ac3a27b7c0231fa62a606d6dc
Alexander Chernaev <alchernaev@gmail.com>
2018-05-23 14:42:23 +0300

    Closes #594 Create docker containers to run tests on python 3 and python 2
    Added scripts to init python2 and python3 environment

---
+++ scripts/setup-env-py3.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+conda install \
+	pytest \
+	future \
+	numpy \
+	pandas \
+	sqlalchemy \
+	scipy \
+	statsmodels \
+	matplotlib \
+	seaborn \
+	pytest-mock
+
+pip install \
+	pysam \
+	mysqlclient \
+	reusables \
+	python-box \
+	Django==1.10 \
+	djangorestframework \
+	django-guardian \
+	django-cors-headers \
+	pytest-django
