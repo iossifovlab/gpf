5dc56893f03531ceebfe65ed4dbc5b6f9f1fbf4b
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-07-03 14:23:06 +0300

    fixture tests for query by sex #648
    Task-Url: https://github.com/seqpipe/gpf/issues/648

--- DAE/variants/tests/test_fixtures_query_by_sexes.py
+++ DAE/variants/tests/test_fixtures_query_by_sexes.py
@@ -28,3 +28,22 @@ def test_fixture_query_by_sex(
     vs = list(vs)
     print([(v, v.variant_in_members, v.variant_in_sexes) for v in vs])
     assert len(vs) == count
+
+
+@pytest.mark.skip("needs fixing")
+@pytest.mark.parametrize("fixture_name,sexes,count", [
+    ("fixtures/effects_trio_dad", 'male', 1),
+    ("fixtures/effects_trio_dad", 'female', 1),
+    ("fixtures/effects_trio_dad", 'male or female', 2),
+    ("fixtures/trios2", 'female and not male', 5),
+])
+def test_fixture_raw_df_query_by_sex_needs_fixing(
+        variants_df, fixture_name, sexes, count):
+    vvars = variants_df(fixture_name)
+    assert vvars is not None
+
+    vs = vvars.query_variants(
+        sexes=sexes)
+    vs = list(vs)
+    print([(v, v.variant_in_members, v.variant_in_sexes) for v in vs])
+    assert len(vs) == count
