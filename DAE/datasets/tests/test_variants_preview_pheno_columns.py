b3f06a52f68b19a4487b6211fc88abf6702c1ab0
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-10-03 14:14:42 +0300

    test dependency on the number of returned  columns removed

--- DAE/datasets/tests/test_variants_preview_pheno_columns.py
+++ DAE/datasets/tests/test_variants_preview_pheno_columns.py
@@ -15,14 +15,12 @@ def test_get_denovo_variants_ssc_11563(ssc):
 
     vs = ssc.get_variants_preview(**query)
     v = vs.next()
-    assert len(v) == 89
     assert 'Proband IQs NvIQ' in v
     assert 'Proband IQs vIQ' in v
     assert 'Races Mom' in v
     assert 'Races Dad' in v
 
     v = vs.next()
-    assert len(v) == 89
     assert '11563' == v[0]
 
 
@@ -33,14 +31,12 @@ def test_get_denovo_variants_ssc_11825(ssc):
     vs = ssc.get_variants_preview(**query)
     v = vs.next()
 
-    assert len(v) == 89
     assert 'Proband IQs NvIQ' in v
     assert 'Proband IQs vIQ' in v
     assert 'Races Mom' in v
     assert 'Races Dad' in v
 
     v = vs.next()
-    assert len(v) == 89
 
     assert '11825' == v[0]
 
@@ -57,11 +53,9 @@ def test_get_denovo_variants_vip(vip):
 
     vs = vip.get_variants_preview(**query)
     v = vs.next()
-    assert len(v) == 89
     assert 'Proband IQs NvIQ' in v
     assert 'Proband IQs vIQ' in v
 
-    assert len(v) == 89
     _families = vip.pheno_db.families
     count = 0
     for v in vs:
