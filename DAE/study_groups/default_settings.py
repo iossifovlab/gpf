5c1eb7944425ef510bfc7bff2b9efdfc8b00febd
Nikolay Stanishev <stanishev.nikolay@gmail.com>
2018-11-14 14:33:25 +0200

    Ref #954 Change path for study_groups config

--- DAE/study_groups/default_settings.py
+++ DAE/study_groups/default_settings.py
@@ -1,5 +1,10 @@
 import os
-from studies.default_settings import DATA_DIR
+from datasets.default_settings import DATA_DIR
+
+DATA_DIR = os.environ.get(
+    "DATA_STUDIES_DIR",
+    os.path.join(DATA_DIR, "study_groups")
+)
 
 CONFIG_FILE = os.environ.get(
     "DATA_STUDIES_CONF",
