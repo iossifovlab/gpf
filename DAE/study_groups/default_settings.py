454d347d373705fcb0324723f3580722599f61c8
Nikolay Stanishev <stanishev.nikolay@gmail.com>
2018-11-14 15:12:04 +0200

    Ref #946 Change COMMON_REPORTS_DIR path

--- DAE/study_groups/default_settings.py
+++ DAE/study_groups/default_settings.py
@@ -2,11 +2,16 @@ import os
 from datasets.default_settings import DATA_DIR
 
 DATA_DIR = os.environ.get(
-    "DATA_STUDIES_DIR",
+    "DATA_STUDY_GROUPS_DIR",
     os.path.join(DATA_DIR, "study_groups")
 )
 
 CONFIG_FILE = os.environ.get(
-    "DATA_STUDIES_CONF",
+    "DATA_STUDY_GROUP_CONF",
     "study_groups.conf"
+)
+
+COMMON_REPORTS_DIR = os.environ.get(
+    "DATA_STUDY_GROUPS_COMMON_REPORTS_DIR",
+    os.path.join(DATA_DIR, "commonReports")
 )
