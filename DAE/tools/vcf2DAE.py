fe394672cc3e9d0b4b05d992db0c4684e88be967
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2018-11-22 10:11:52 +0200

    command line arguments handling for vcf2dae import tool...

--- DAE/tools/vcf2DAE.py
+++ DAE/tools/vcf2DAE.py
@@ -4,106 +4,133 @@ from __future__ import print_function
 from __future__ import unicode_literals
 from builtins import str
 
+import os
 import subprocess
-import optparse
+import argparse
+import tempfile
+
+
+def run_command(cmd):
+    print("executing", cmd)
+    try:
+        subprocess.check_output(cmd, shell=True)
+    except subprocess.CalledProcessError as ex:
+        status = ex.returncode
+        output = ex.output
+
+        print(status, output)
+        raise Exception("FAILURE AT: " + cmd)
 
 
 def main():
-    # copy of options for vcf2DAEc.py
-    usage = "usage: %prog [options] "\
-        "<families pedigree file> <list of vcf files>"
-    parser = optparse.OptionParser(usage=usage)
+    parser = argparse.ArgumentParser(
+        description="converts list of VCF files to DAE transmitted varaints")
 
-    parser.add_option(
-        "-x", "--project", dest="project", default="VIP",
+    parser.add_argument(
+        'pedigree', type=str,
+        metavar='<pedigree filename>',
+        help='families file in pedigree format'
+    )
+    parser.add_argument(
+        'vcf', type=str,
+        metavar='<VCF filename>',
+        help='VCF file to import'
+    )
+
+    parser.add_argument(
+        "-x", "--project", dest="project",
         metavar="project", help="project name [defualt:VIP")
-    parser.add_option(
-        "-l", "--lab", dest="lab", default="SF",
-        metavar="lab", help="lab name [defualt:SF")
+    parser.add_argument(
+        "-l", "--lab", dest="lab",
+        default="cshl",
+        metavar="lab", help="lab name")
 
-    parser.add_option(
-        "-o", "--outputPrefix", dest="outputPrefix", default="transmission",
-        metavar="outputPrefix", help="prefix of output transmission file")
+    parser.add_argument(
+        "-o", "--output-prefix", dest="output_prefix", 
+        default="transmission",
+        help="prefix of output transmission file")
 
-    parser.add_option(
+    parser.add_argument(
+        "-w", "--work-dir", dest="work_dir", default=".",
+        help="work directory where to store result and temporary files")
+
+    parser.add_argument(
         "-m", "--minPercentOfGenotypeSamples",
         dest="minPercentOfGenotypeSamples", type=float, default=25.,
-        metavar="minPercentOfGenotypeSamples",
         help="threshold percentage of gentyped samples to printout "
         "[default: 25]")
 
-    parser.add_option(
+    parser.add_argument(
         "-t", "--tooManyThresholdFamilies",
         dest="tooManyThresholdFamilies", type=int, default=10,
-        metavar="tooManyThresholdFamilies",
         help="threshold for TOOMANY to printout [defaylt: 10]")
 
-    parser.add_option(
+    parser.add_argument(
         "-s", "--missingInfoAsNone",
         action="store_true",
         dest="missingInfoAsNone",
         default=False,
-        metavar="missingInfoAsNone",
         help="missing sample Genotype will be filled with 'None' for many VCF "
         "files input")
 
-    ox, args = parser.parse_args()
+    args = parser.parse_args()
+    print(args)
 
-    def tExt(xxx): return '.zZqZz{:d}'.format(xxx) if isinstance(
-        xxx, int) else '.zZqZz{:s}'.format(xxx)
-    # tmp files' extension
+    tempdir = tempfile.mkdtemp(dir=args.work_dir)
+    dae_prefix = os.path.join(args.work_dir, args.output_prefix)
+
+    dae_name = "{}.txt".format(args.output_prefix)
+    dae_too_name = "{}-TOOMANY.txt".format(args.output_prefix)
+
+    dae_fullname = os.path.join(args.work_dir, dae_name)
+    dae_too_fullname = os.path.join(args.work_dir, dae_too_name)
+
+    temp_dae_name = os.path.join(tempdir, dae_name)
+    temp_dae_too_name = os.path.join(tempdir, dae_too_name)
+    temp_dae_prefix = os.path.join(tempdir, args.output_prefix)
 
     # main procedure
     # cmd = ' '.join( ['vcf2DAEc.py', '-p', ox.pedFile, '-d', ox.dataFile,
     # '-x', ox.project, '-l', ox.lab, \
     cmd = ' '.join(
-        ['vcf2DAEc_fast.py', '-p', args[0], '-d "' + args[1] + '" -x',
-         ox.project, '-l', ox.lab,
-         '-o', ox.outputPrefix +
-         tExt(0), '-m', str(ox.minPercentOfGenotypeSamples),
-         '-t', str(ox.tooManyThresholdFamilies)])
-    if ox.missingInfoAsNone:
-        cmd += ' '.join([' --missingInfoAsNone', ' > ' +
-                         ox.outputPrefix + '-complex.txt'])
-    else:
-        cmd += ' > ' + ox.outputPrefix + '-complex.txt'
+        [
+            'vcf2DAEc_fast.py',
+            '-p', args.pedigree,
+            '-d ', '"' + args.vcf + '"',
+            '-x', args.project,
+            '-l', args.lab,
+            '-o', temp_dae_prefix,
+            '-m', str(args.minPercentOfGenotypeSamples),
+            '-t', str(args.tooManyThresholdFamilies),
+        ])
 
-    print("executing", cmd)
-    try:
-        subprocess.check_output(cmd, shell=True)
-    except subprocess.CalledProcessError as ex:
-        status = ex.returncode
-        output = ex.output
+    if args.missingInfoAsNone:
+        cmd += ' --missingInfoAsNone'
+    run_command(cmd)
 
-        print(status, output)
-        raise Exception("FAILURE AT: " + cmd)
+    # HW
+    cmd = ' '.join([
+        'hw.py',
+        '-c', temp_dae_name,
+        dae_fullname
+    ])
+    run_command(cmd)
 
     # family file
-    cmd = ' '.join(['\\mv', ox.outputPrefix + tExt(0) +
-                    '-families.txt', ox.outputPrefix + '-families.txt'])
-    print("executing", cmd)
+    cmd = ' '.join([
+        'mv',
+        temp_dae_prefix + '-families.txt',
+        dae_prefix + '-families.txt'
+    ])
+    run_command(cmd)
 
-    try:
-        subprocess.check_output(cmd, shell=True)
-    except subprocess.CalledProcessError as ex:
-        status = ex.returncode
-        output = ex.output
-
-        print(status, output)
-        raise Exception("FAILURE AT: " + cmd)
-    # HW
-    cmd = ' '.join(['hw.py -c', ox.outputPrefix + tExt(0) +
-                    '.txt', ox.outputPrefix + tExt(1) + '.txt'])
-    print("executing", cmd)
-
-    try:
-        subprocess.check_output(cmd, shell=True)
-    except subprocess.CalledProcessError as ex:
-        status = ex.returncode
-        output = ex.output
-
-        print(status, output)
-        raise Exception("FAILURE AT: " + cmd)
+    # toomany file
+    cmd = ' '.join([
+        'mv',
+        temp_dae_too_name,
+        dae_too_fullname
+    ])
+    run_command(cmd)
 
 
 if __name__ == "__main__":
