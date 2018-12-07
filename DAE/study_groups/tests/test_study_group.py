9e7c8aa4852c7b4aced128faf280ccb17fc7d088
Alexander Chernaev <alchernaev@gmail.com>
2018-12-07 15:37:37 +0200

    Fixed tests

--- DAE/study_groups/tests/test_study_group.py
+++ DAE/study_groups/tests/test_study_group.py
@@ -1,12 +1,12 @@
-def test_can_get_study_group_from_config(basic_groups_definition):
-    configs = basic_groups_definition.get_all_study_group_configs()
+def test_can_get_study_group_from_config(basic_study_groups_definition):
+    configs = basic_study_groups_definition.get_all_study_group_configs()
 
     assert configs is not None
     assert len(configs) != 0
 
 
-def test_skips_unknown_study_names(basic_groups_definition):
-    configs = basic_groups_definition.get_all_study_group_configs()
+def test_skips_unknown_study_names(basic_study_groups_definition):
+    configs = basic_study_groups_definition.get_all_study_group_configs()
 
     assert configs is not None
     assert len(configs) == 2
@@ -14,8 +14,8 @@ def test_skips_unknown_study_names(basic_groups_definition):
 
 
 def test_phenotypes_is_loaded_from_studies(
-        basic_groups_definition, study_groups_factory):
-    study_group_config = basic_groups_definition \
+        basic_study_groups_definition, study_groups_factory):
+    study_group_config = basic_study_groups_definition \
         .get_study_group_config('phenotypes')
 
     print(study_group_config)
