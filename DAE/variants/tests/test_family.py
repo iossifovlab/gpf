60e061a78580b49e88d819ed708f7a3a7da8ccb3
Nikolay Stanishev <stanishev.nikolay@gmail.com>
2019-01-17 13:52:47 +0200

    Ref #1034 Add tests for Status enum

--- DAE/variants/tests/test_family.py
+++ DAE/variants/tests/test_family.py
@@ -7,7 +7,7 @@ from io import StringIO
 import pandas as pd
 
 from variants.family import FamiliesBase
-from variants.attributes import Role, Sex
+from variants.attributes import Role, Sex, Status
 
 
 @pytest.mark.parametrize("infile,pedigree", [
@@ -22,7 +22,7 @@ familyId\tpersonId\tdadId\tmomId\tsex\tstatus\trole\tlayout
         'dadId': ['0', '0', '1.x2'],
         'momId': ['0', '0', '1.x1'],
         'sex': [Sex.female, Sex.male, Sex.female],
-        'status': [1, 1, 2],
+        'status': [Status.unaffected, Status.unaffected, Status.affected],
         'role': [Role.mom, Role.dad, Role.prb],
         'layout': ['53.5,50.0', '10.0,50.0', '31.75,80.0'],
         'sampleId': ['1.x1', '1.x2', '1.x3']
@@ -38,7 +38,7 @@ familyId\tpersonId\tdadId\tmomId\tgender\tstatus\trole\tlayout\tsampleId
         'dadId': ['0', '0', '1.x2'],
         'momId': ['0', '0', '1.x1'],
         'sex': [Sex.female, Sex.male, Sex.female],
-        'status': [1, 1, 2],
+        'status': [Status.unaffected, Status.unaffected, Status.affected],
         'role': [Role.mom, Role.dad, Role.prb],
         'layout': ['53.5,50.0', '10.0,50.0', '31.75,80.0'],
         'sampleId': ['1.x1', '1.x2', '1.x3']
@@ -54,7 +54,7 @@ familyId\tpersonId\tdadId\tmomId\tsex\tstatus\trole\tlayout\tsampleId
         'dadId': ['0', '0', '1.x2'],
         'momId': ['0', '0', '1.x1'],
         'sex': [Sex.female, Sex.male, Sex.female],
-        'status': [1, 1, 2],
+        'status': [Status.unaffected, Status.unaffected, Status.affected],
         'role': [Role.mom, Role.dad, Role.prb],
         'layout': ['53.5,50.0', '10.0,50.0', '31.75,80.0'],
         'sampleId': ['1.x1', '1.x2', '1.x3']
@@ -65,4 +65,5 @@ def test_load_pedigree_file(infile, pedigree):
     columns = ['familyId', 'personId', 'dadId', 'momId', 'sex', 'status',
                'role', 'layout', 'sampleId']
     for column in columns:
-        assert (loaded_pedigree[column].values == pedigree[column].values).all()
+        assert (loaded_pedigree[column].values ==
+                pedigree[column].values).all()
