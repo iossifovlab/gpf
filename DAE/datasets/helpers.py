6459d093321728a8e932af5032cf3e53e2a0fa2b
Nikolay Stanishev <stanishev.nikolay@gmail.com>
2019-01-17 15:06:22 +0200

    Ref #1032 Remove unnessesary else

--- DAE/datasets/helpers.py
+++ DAE/datasets/helpers.py
@@ -222,25 +222,24 @@ def transform_variants_to_lists(
 def get_person_color(member, pedigree_selectors, selected_pedigree_selector):
     if member.generated:
         return '#E0E0E0'
+    if len(pedigree_selectors) == 0:
+        return '#FFFFFF'
+    pedigree_selector_id = selected_pedigree_selector.get('id', None)
+    if pedigree_selector_id:
+        selected_pedigree_selectors = list(filter(
+            lambda ps: ps.id == pedigree_selector_id,
+            pedigree_selectors))[0]
     else:
-        if len(pedigree_selectors) == 0:
-            return '#FFFFFF'
-        pedigree_selector_id = selected_pedigree_selector.get('id', None)
-        if pedigree_selector_id:
-            selected_pedigree_selectors = list(filter(
-                lambda ps: ps.id == pedigree_selector_id,
-                pedigree_selectors))[0]
-        else:
-            selected_pedigree_selectors = pedigree_selectors[0]
+        selected_pedigree_selectors = pedigree_selectors[0]
 
-        people_group_attribute =\
-            member.get_attr(selected_pedigree_selectors['source'])
-        domain = list(filter(lambda d: d['name'] == people_group_attribute,
-                             selected_pedigree_selectors['domain']))
-        if domain and people_group_attribute:
-            return domain[0]['color']
-        else:
-            return selected_pedigree_selectors['default']['color']
+    people_group_attribute =\
+        member.get_attr(selected_pedigree_selectors['source'])
+    domain = list(filter(lambda d: d['name'] == people_group_attribute,
+                         selected_pedigree_selectors['domain']))
+    if domain and people_group_attribute:
+        return domain[0]['color']
+    else:
+        return selected_pedigree_selectors['default']['color']
 
 
 def generate_pedigree(variant, pedigree_selectors, selected_pedigree_selector):
