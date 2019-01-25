d62e9e3d9bfdc98dd30088d11a43e662b06d4813
iordanivanov <iiordan.ivanov@gmail.com>
2019-01-25 15:28:30 +0200

    Replace regex-based dbNSFP file search with glob.

--- DAE/annotation/tools/dbnsfp_annotator.py
+++ DAE/annotation/tools/dbnsfp_annotator.py
@@ -1,6 +1,5 @@
-from os import listdir
+import glob
 import os.path
-import re
 from annotation.tools.score_annotator import NPScoreAnnotator
 
 
@@ -8,9 +7,9 @@ class dbNSFPAnnotator(NPScoreAnnotator):
 
     def __init__(self, config):
         self.current_chr = None
-        self.dbNSFP_files = self._get_dbNSFP_files(
-                config.options.dbNSFP_path,
-                config.options.dbNSFP_filename)
+        self.dbNSFP_files = glob.glob(
+                os.path.join(config.options.dbNSFP_path,
+                             config.options.dbNSFP_filename))
         assert self.dbNSFP_files
 
         config.options.dbNSFP_filename = \
@@ -30,32 +29,6 @@ class dbNSFPAnnotator(NPScoreAnnotator):
                 self.config.options.dbNSFP_filename.format(self.current_chr))
             assert self.config.options.scores_file in self.dbNSFP_files
         super(dbNSFPAnnotator, self)._init_score_file()
-
-    @staticmethod
-    def _wildcard_to_regex(wildcard):
-        assert wildcard
-        if wildcard[0] != '*':
-            wildcard = r'^{}'.format(wildcard)
-        if wildcard[-1] != '*':
-            wildcard = r'{}\Z'.format(wildcard)
-        return wildcard.split('*')
-
-    @staticmethod
-    def _search_wildcard(inp, wildcard):
-        wildcards = dbNSFPAnnotator._wildcard_to_regex(wildcard)
-        for regex_token in wildcards:
-            if re.compile(regex_token).search(inp) is None:
-                return False
-        return True
-
-    def _get_dbNSFP_files(self, path, wildcard):
-        files = []
-        for file_ in listdir(path):
-            if not os.path.isfile(os.path.join(path, file_)):
-                continue
-            if dbNSFPAnnotator._search_wildcard(file_, wildcard):
-                files.append(os.path.join(path, file_))
-        return files
 
     def do_annotate(self, aline, variant):
         if variant.chromosome != self.current_chr:
