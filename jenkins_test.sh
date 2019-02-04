3c5cd0fe6e417b9da262a463158c19aa6cd4d52f
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2019-02-04 09:55:55 +0200

    jenkins wdae tests run probably fixed...

--- jenkins_test.sh
+++ jenkins_test.sh
@@ -5,7 +5,7 @@ export PATH=${DAE_SOURCE_DIR}/tests:$PATH
 export PYTHONPATH=${DAE_SOURCE_DIR}:$PYTHONPATH
 export PYTHONPATH=${DAE_SOURCE_DIR}/tools:$PYTHONPATH
 
-py.test -s --runslow --withspark -v --cov-config coveragerc \
+py.test --traceconfig -v --cov-config coveragerc \
     --junitxml=coverage/dae-junit.xml \
     --cov-report html:coverage/coverage.html \
     --cov-report xml:coverage/coverage.xml \
