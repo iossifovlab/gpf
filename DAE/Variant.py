7f52c783a50cfeba9dce11cd32b386a3bfd117d5
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2019-02-06 04:22:33 -0500

    handling of new intergenic annotation...

--- DAE/Variant.py
+++ DAE/Variant.py
@@ -165,7 +165,7 @@ def splitGeneEffect(effStr, geneEffect=[]):
 
 def parseGeneEffect(effStr):
     geneEffect = []
-    if effStr == "intergenic":
+    if effStr == "intergenic" or effStr == ':intergenic':
         return [{'eff': 'intergenic', 'sym': '', 'symu': ''}]
 
     # HACK!!! To rethink
