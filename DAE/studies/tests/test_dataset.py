202261fe594344b464d8a64b071f7f5e46f4b695
iordanivanov <iiordan.ivanov@gmail.com>
2019-02-14 16:37:57 +0200

    Add simple unit test

--- DAE/studies/tests/test_dataset.py
+++ DAE/studies/tests/test_dataset.py
@@ -2,3 +2,8 @@
 
 def test_inheritance_trio_can_init(inheritance_trio_dataset):
     assert inheritance_trio_dataset is not None
+
+
+def test_inheritance_trio_description(inheritance_trio_dataset):
+    assert inheritance_trio_dataset.description == \
+           "Sample description in markdown format.\n"
