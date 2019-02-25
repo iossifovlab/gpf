4d25d6fcd9972dc0dcfce4d882784ebddc195091
Alexander Chernaev <alchernaev@gmail.com>
2019-02-25 16:23:45 +0200

    #1080 Fix denovo gene sets generation script
    Fixed environment for tools tests

--- DAE/tools/generate_denovo_gene_sets.py
+++ DAE/tools/generate_denovo_gene_sets.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+from configurable_entities.configuration import DAEConfig
 from gene.gene_set_collections import GeneSetsCollections
-
-from DAE import variants_db
+from studies.factory import VariantsDb
 
 
 def main():
+    config = DAEConfig()
+    variants_db = VariantsDb(config)
     gsc = GeneSetsCollections(variants_db.dataset_facade)
     denovo = gsc.get_gene_sets_collection('denovo', load=False)
 
