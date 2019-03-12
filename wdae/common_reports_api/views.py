867f1bd6b5ea23748ebf76655e417ecc8b0b39ef
Nikolay Stanishev <stanishev.nikolay@gmail.com>
2019-03-12 16:58:06 +0200

    Ref #1114 Add support for download common report

--- wdae/common_reports_api/views.py
+++ wdae/common_reports_api/views.py
@@ -4,12 +4,15 @@ Created on Aug 3, 2015
 @author: lubo
 '''
 from __future__ import unicode_literals
+from django.http.response import StreamingHttpResponse
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from datasets_api.studies_manager import get_studies_manager
 from datasets_api.permissions import IsDatasetAllowed
+
+from helpers.dae_query import join_line
 
 
 class StudiesSummariesView(APIView):
@@ -100,3 +103,58 @@ class VariantReportsView(APIView):
                         common_report_id)
                 },
                 status=status.HTTP_404_NOT_FOUND)
+
+
+class FamiliesDataDownloadView(APIView):
+    def __init__(self):
+        self.common_reports_facade =\
+            get_studies_manager().get_common_report_facade()
+
+    def to_tsv(self, common_report_id):
+        study = get_studies_manager().get_variants_db().get_wdae_wrapper(
+            common_report_id)
+
+        data = []
+        data.append(['familyId', 'personId', 'dadId', 'momId', 'gender',
+                     'status', 'role', 'study'])
+
+        families = list(study.families.values())
+        families.sort(key=lambda f: f.family_id)
+        for f in families:
+            for (o, p) in enumerate(f.members_in_order):
+                if p.generated:
+                    continue
+
+                row = [
+                    p.family_id,
+                    p.person_id,
+                    p.dad_id,
+                    p.mom_id,
+                    p.sex,
+                    p.status,
+                    p.role,
+                    study.name,
+                ]
+
+                data.append(row)
+
+        return list(map(join_line, data))
+
+    def get(self, request, common_report_id):
+        report = self.common_reports_facade.get_common_report(common_report_id)
+        if not report:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        if not IsDatasetAllowed.user_has_permission(
+                request.user, common_report_id):
+            return Response(status=status.HTTP_403_FORBIDDEN)
+
+        families_data = self.to_tsv(common_report_id)
+
+        response = StreamingHttpResponse(
+            families_data, content_type='text/tsv')
+
+        response['Content-Disposition'] = 'attachment; filename=families.ped'
+        response['Expires'] = '0'
+
+        return response
