6db5ee30dba697b4df8587b4fc76fed8c398f85e
Alexander Chernaev <alchernaev@gmail.com>
2019-03-19 16:54:05 +0200

    #1110 Add tests for pheno filter queries in wdae
    Added tests for pheno filter
    Added fixtures for testing

--- wdae/genotype_browser/tests/conftest.py
+++ wdae/genotype_browser/tests/conftest.py
@@ -4,7 +4,7 @@ import os
 import pytest
 
 from configurable_entities.configuration import DAEConfig
-from studies.factory import VariantsDb
+from datasets_api.studies_manager import StudiesManager
 
 
 def fixtures_dir():
@@ -12,13 +12,20 @@ def fixtures_dir():
         os.path.join(os.path.dirname(__file__), 'fixtures'))
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture()
 def dae_config_fixture():
     dae_config = DAEConfig(fixtures_dir())
     return dae_config
 
 
-@pytest.fixture(scope='session')
-def variants_db(dae_config_fixture):
-    vdb = VariantsDb(dae_config_fixture)
-    return vdb
+@pytest.fixture()
+def studies_manager(dae_config_fixture):
+    return StudiesManager(dae_config_fixture)
+
+
+@pytest.fixture()
+def mock_studies_manager(db, mocker, studies_manager):
+    studies_manager.reload_dataset()
+    mocker.patch(
+        'datasets_api.studies_manager.get_studies_manager',
+        return_value=studies_manager)
--- wdae/genotype_browser/tests/test_query_api.py
+++ wdae/genotype_browser/tests/test_query_api.py
@@ -5,37 +5,59 @@ Created on Feb 6, 2017
 '''
 import copy
 import json
+import pytest
 
 from rest_framework import status
 
+pytestmark = pytest.mark.usefixtures("mock_studies_manager")
 
-EXAMPLE_REQUEST_SSC = {
-    "effectTypes": ["Frame-shift", "Nonsense", "Splice-site"],
-    "gender": ["female", "male"],
-    "presentInChild": [
-        "affected and unaffected",
-        "affected only",
-    ],
-    "presentInParent": [
-        "neither",
-    ],
-    "variantTypes": [
-        "CNV", "del", "ins", "sub",
-    ],
-    "genes": "All",
-    "datasetId": "SSC",
-    "pedigreeSelector": {
-        "id": "phenotype",
-        "checkedValues": ["autism", "unaffected"]
-    }
+
+EXAMPLE_REQUEST_F1 = {
+    "datasetId": "inheritance_trio",
+    # "effectTypes": ["Frame-shift", "Nonsense", "Splice-site"],
+    # "gender": ["female", "male"],
+    # "presentInChild": [
+    #     "affected and unaffected",
+    #     "affected only",
+    # ],
+    # "presentInParent": [
+    #     "neither",
+    # ],
+    # "variantTypes": [
+    #     "CNV", "del", "ins", "sub",
+    # ],
+    # "pedigreeSelector": {
+    #     "id": "phenotype",
+    #     "checkedValues": ["autism", "unaffected"]
+    # }
 }
 
 
 URL = "/api/v3/genotype_browser/preview"
 
 
+def test_simple_query_preview(db, admin_client):
+    data = copy.deepcopy(EXAMPLE_REQUEST_F1)
+
+    response = admin_client.post(
+        URL, json.dumps(data), content_type='application/json')
+    assert status.HTTP_200_OK == response.status_code
+    res = response.data
+
+    assert 'cols' in res
+    assert 'rows' in res
+    assert 'count' in res
+    assert 'legend' in res
+
+    assert 8 == len(res['legend'])
+    # self.assertEquals(422, int(res['count']))
+    assert 28 == int(res['count'])
+    # self.assertEquals(422, len(res['rows']))
+    assert 28 == len(res['rows'])
+
+
 def test_missing_dataset(db, client):
-    data = copy.deepcopy(EXAMPLE_REQUEST_SSC)
+    data = copy.deepcopy(EXAMPLE_REQUEST_F1)
     del data['datasetId']
 
     response = client.post(URL, json.dumps(data), content_type='application/json')
@@ -43,7 +65,7 @@ def test_missing_dataset(db, client):
 
 
 def test_bad_dataset(db, client):
-    data = copy.deepcopy(EXAMPLE_REQUEST_SSC)
+    data = copy.deepcopy(EXAMPLE_REQUEST_F1)
     data['datasetId'] = 'ala bala portokala'
 
     response = client.post(URL, json.dumps(data), content_type='application/json')
