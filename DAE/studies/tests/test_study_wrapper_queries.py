b1c1abe9e4d07beae7cd06db479df726f4b82711
Alexander Chernaev <alchernaev@gmail.com>
2019-03-26 17:07:21 +0200

    #1145 Add inRole columns to configuration and preview
    Removed unneeded fixture for mocking environment

--- DAE/studies/tests/test_study_wrapper_queries.py
+++ DAE/studies/tests/test_study_wrapper_queries.py
@@ -2,8 +2,6 @@ from __future__ import unicode_literals
 
 import pytest
 
-pytestmark = pytest.mark.usefixtures("pheno_conf_path")
-
 
 def test_query_all_variants(inheritance_trio_wrapper):
     variants = list(inheritance_trio_wrapper.query_variants())
