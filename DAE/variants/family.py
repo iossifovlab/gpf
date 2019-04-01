42d97fde4084b13ae165e169773670e5c7103bd9
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2019-04-01 16:16:47 +0300

    fix person_samples method in raw_vcf.py

--- DAE/variants/family.py
+++ DAE/variants/family.py
@@ -23,6 +23,7 @@ class Person(object):
         self.family_id = atts['family_id']
         self.person_id = atts['person_id']
         self.sample_id = atts.get('sample_id', None)
+        self.sample_index = atts.get('sampleIndex', None)
         self.index = atts.get('index', None)
         self.sex = atts['sex']
         self.role = atts['role']
