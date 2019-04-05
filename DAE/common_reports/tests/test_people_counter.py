888208a58d039607a0b1b59bad2e6a3e6e6c1337
Nikolay Stanishev <stanishev.nikolay@gmail.com>
2019-04-05 16:01:45 +0300

    Ref #1162 Add families to Study1

--- DAE/common_reports/tests/test_people_counter.py
+++ DAE/common_reports/tests/test_people_counter.py
@@ -1,8 +1,8 @@
 def test_people_counter(pc_s1_mom_and_phenotype1):
     assert pc_s1_mom_and_phenotype1.people_male == 0
-    assert pc_s1_mom_and_phenotype1.people_female == 2
+    assert pc_s1_mom_and_phenotype1.people_female == 4
     assert pc_s1_mom_and_phenotype1.people_unspecified == 0
-    assert pc_s1_mom_and_phenotype1.people_total == 2
+    assert pc_s1_mom_and_phenotype1.people_total == 4
 
     assert pc_s1_mom_and_phenotype1.column == 'mom and phenotype1'
 
@@ -29,12 +29,13 @@ def test_people_counter_empty(pc_s1_dad_and_phenotype1):
 
 
 def test_people_counters(people_counters):
-    assert len(people_counters.counters) == 6
+    assert len(people_counters.counters) == 7
     assert people_counters.group_name == 'Role and Diagnosis'
     assert people_counters.rows == \
         ['people_male', 'people_female', 'people_total']
     assert sorted(people_counters.columns) == sorted(
-        ['sib and phenotype2', 'prb and phenotype1', 'mom and unaffected',
-         'mom and phenotype1', 'dad and pheno', 'dad and unaffected'])
+        ['sib and phenotype2', 'prb and phenotype1', 'prb and phenotype2',
+         'mom and unaffected', 'mom and phenotype1', 'dad and pheno',
+         'dad and unaffected'])
 
     assert len(people_counters.to_dict().keys()) == 4
