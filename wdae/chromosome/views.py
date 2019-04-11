f265c1b5619eeeed32f4ae7cc4b9249e2eb51342
Nikolay Stanishev <stanishev.nikolay@gmail.com>
2019-04-11 12:27:36 +0300

    Ref #1176 Change open csv file mode

--- wdae/chromosome/views.py
+++ wdae/chromosome/views.py
@@ -1,23 +1,28 @@
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
 from builtins import next
 import csv
-import json
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from DAE import giDB
 
+
 class ChromosomeView(APIView):
     def __init__(self):
         self.chromosomes = []
-        with open(giDB.getChromosomesFile(), 'rb') as csvfile:
+        with open(giDB.getChromosomesFile(), 'r') as csvfile:
             reader = csv.reader(csvfile, delimiter='\t')
             next(reader)
-            self.chromosomes.append({ 'name': '1', 'bands': [] })
+            self.chromosomes.append({'name': '1', 'bands': []})
             for row in reader:
                 if self.chromosomes[-1]['name'] != row[0][3:]:
-                    self.chromosomes.append({ 'name': row[0][3:], 'bands': [] })
-                self.chromosomes[-1]['bands'].append({ 'start': int(row[1]), 'end': int(row[2]), 'name': row[3], 'gieStain': row[4] })
+                    self.chromosomes.append({'name': row[0][3:], 'bands': []})
+                self.chromosomes[-1]['bands'].append({
+                    'start': int(row[1]),
+                    'end': int(row[2]),
+                    'name': row[3],
+                    'gieStain': row[4]
+                })
 
     def get(self, request):
         return Response(self.chromosomes)
