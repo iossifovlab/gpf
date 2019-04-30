90538f0919bcc3b2d429d69a9ab7aec2c28ee7a4
Nikolay Stanishev <stanishev.nikolay@gmail.com>
2019-04-30 13:57:14 +0300

    Ref #457 Add tests for pedigree chart

---
+++ gpfjs/src/app/pedigree-chart/pedigree-chart-member.component.spec.ts
@@ -0,0 +1,38 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PedigreeChartMemberComponent } from './pedigree-chart-member.component';
+
+describe('PedigreeChartMemberComponent', () => {
+  let component: PedigreeChartMemberComponent;
+  let fixture: ComponentFixture<PedigreeChartMemberComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ PedigreeChartMemberComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PedigreeChartMemberComponent);
+    component = fixture.componentInstance;
+    component.pedigreeData = {
+      pedigreeIdentifier: 'pi',
+      id: 'id',
+      father: 'dad',
+      mother: 'mom',
+      gender: 'M',
+      role: 'prb',
+      color: 'F0F0F0',
+      position: [5, 10],
+      generated: true,
+      label: 'label',
+      smallLabel: 'sl'
+    };
+    fixture.detectChanges();
+  });
+
+  it('should be created', () => {
+    expect(component).toBeTruthy();
+  });
+});
