aaa0c7dc187d9e29dddc927e4b1f7772e99433ac
iordanivanov <iiordan.ivanov@gmail.com>
2019-07-05 19:41:08 +0300

    Add unit tests for PhenoTool and PhenoToolHelper

---
+++ DAE/pheno_tool/tests/conftest.py
@@ -0,0 +1,16 @@
+import pytest
+from pheno.pheno_db import PhenoDB
+from pheno_tool.pheno_common import PhenoFilterBuilder
+
+
+@pytest.fixture
+def fake_pheno_db():
+    pheno_db = PhenoDB('../../tests/fixtures/fake_pheno_db/fake.db')
+    pheno_db.load()
+    return pheno_db
+
+
+@pytest.fixture
+def filter_builder(fake_pheno_db):
+    builder = PhenoFilterBuilder(fake_pheno_db)
+    return builder
