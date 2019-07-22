82ab45732f8e09544cb5de2591f2a1220919bc5f
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2019-07-22 11:11:35 +0300

    django settings restored...

---
+++ wdae/wdae/settings.py
@@ -0,0 +1,31 @@
+'''
+Created on Sep 2, 2015
+
+@author: lubo
+'''
+from __future__ import unicode_literals
+
+from .default_settings import *  # @UnusedWildImport
+
+ALLOWED_HOSTS += ['localhost']
+
+
+INSTALLED_APPS += [
+    'corsheaders',
+]
+
+
+MIDDLEWARE += [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    'localhost:8000',
+    '127.0.0.1:9000',
+    'localhost:4200',
+    '127.0.0.1:4200',
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
