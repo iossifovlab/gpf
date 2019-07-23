5721f2f33bb02c3e77f990902cb3d92524d71b9e
Nikolay Stanishev <stanishev.nikolay@gmail.com>
2019-07-23 11:06:11 +0300

    #1 Add checking for existing directory

--- jenkins_data.sh
+++ jenkins_data.sh
@@ -5,7 +5,7 @@ wget -c https://iossifovlab.com/distribution/public/data-hg19-startup.tar.gz
 
 data_tar_time=`stat -c %Y data-hg19-startup.tar.gz`
 data_dir_time=`stat -c %Y data-hg19-startup`
-if [ $data_tar_time -nt $data_dir_time ];
+if [ ! -d 'data-hg19-startup' ] || [ $data_tar_time -nt $data_dir_time ];
 then
     tar zxf data-hg19-startup.tar.gz
 fi
