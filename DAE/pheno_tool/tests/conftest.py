047e53f9dafb61e215c34b87cb20a48568efa0e5
Nikolay Stanishev <stanishev.nikolay@gmail.com>
2019-07-24 19:06:38 +0300

    #13 Fix pheno db path

--- DAE/pheno_tool/tests/conftest.py
+++ DAE/pheno_tool/tests/conftest.py
@@ -1,11 +1,12 @@
 import pytest
+
 from pheno.pheno_db import PhenoDB
 from pheno_tool.pheno_common import PhenoFilterBuilder
 
 
 @pytest.fixture
-def fake_pheno_db():
-    pheno_db = PhenoDB('../../tests/fixtures/fake_pheno_db/fake.db')
+def fake_pheno_db(fixture_dirname):
+    pheno_db = PhenoDB(fixture_dirname('fake_pheno_db/fake.db'))
     pheno_db.load()
     return pheno_db
 
