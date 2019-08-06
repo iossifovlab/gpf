0faf6a068c539f8aec9552835498e1e261ebb8e4
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2019-08-06 18:12:05 +0300

    update coverage and testing report

--- jenkins_test.sh
+++ jenkins_test.sh
@@ -7,26 +7,10 @@ export PYTHONPATH=${DAE_SOURCE_DIR}/dae/tools:$PYTHONPATH
 
 rm -rf coverage/ && mkdir coverage && \
 
-py.test --traceconfig -v --cov-config coveragerc --reimport \
+py.test -v --cov-config coveragerc \
     --junitxml=coverage/dae-junit.xml \
     --cov-report html:coverage/coverage.html \
     --cov-report xml:coverage/coverage.xml \
-    --cov annotation \
-    --cov backends \
-    --cov common \
-    --cov common_reports \
-    --cov configurable_entities \
-    --cov enrichment_tool \
-    --cov gene \
-    --cov pedigrees \
-    --cov pheno \
-    --cov pheno_browser \
-    --cov studies \
-    --cov tests \
-    --cov tools \
-    --cov utils \
-    --cov variant_annotation \
-    --cov variants \
     dae/
 
 py.test -v --cov-config coveragerc \
@@ -34,25 +18,6 @@ py.test -v --cov-config coveragerc \
     --cov-append \
     --cov-report html:coverage/coverage.html \
     --cov-report xml:coverage/coverage.xml \
-    --cov chromosome \
-    --cov common_reports_api \
-    --cov datasets_api \
-    --cov enrichment_api \
-    --cov family_counters_api \
-    --cov gene_sets \
-    --cov gene_weights \
-    --cov genomic_scores_api \
-    --cov genotype_browser \
-    --cov groups_api \
-    --cov helpers \
-    --cov measures_api \
-    --cov pheno_browser_api \
-    --cov pheno_tool_api \
-    --cov query_state_save \
-    --cov tests \
-    --cov tools \
-    --cov users_api \
-    --cov wdae \
     wdae/
 
 
