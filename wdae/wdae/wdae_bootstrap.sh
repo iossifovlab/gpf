684c1a1d541e02c5bf7c84aa7972a38ba0a67156
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2019-09-03 14:29:54 +0300

    fix in study import tool...

--- wdae/wdae/wdae_bootstrap.sh
+++ wdae/wdae/wdae_bootstrap.sh
@@ -1,20 +1,26 @@
 #!/bin/bash
 
-DATA_ARCHIVE=data-hg19-startup-3.0.0dev22.tar.gz
+DATA_ARCHIVE=data-hg19-startup-latest.tar.gz
 INSTALL_DIRNAME=$1
 
 if [ -z $INSTALL_DIRNAME ]; then
     export INSTALL_DIRNAME=data-hg19-startup
 fi
 
+if [ ! -d '$INSTALL_DIRNAME' ];
+then
+    echo "Directory $INSTALL_DIRNAME alreay exists. Exiting..."
+    exit 1
+fi
+
 echo "Bootstrapping GPF into '$INSTALL_DIRNAME'"
 
-wget -c -q https://iossifovlab.com/distribution/public/$DATA_ARCHIVE
+wget -c https://iossifovlab.com/distribution/public/$DATA_ARCHIVE
 
 mkdir $INSTALL_DIRNAME && \
     tar xzf $DATA_ARCHIVE -C $INSTALL_DIRNAME --strip-components 1
 
-rm $DATA_ARCHIVE
+# rm $DATA_ARCHIVE
 
 cd $INSTALL_DIRNAME
 DAE_DB_DIR=`pwd`
@@ -27,11 +33,11 @@ wdaemanage.py migrate
 wdaemanage.py user_create admin@iossifovlab.com -p secret -g any_dataset:admin
 wdaemanage.py user_create research@iossifovlab.com -p secret
 
-cd -
-
 cat <<EOF > setenv.sh
 #!/bin/bash
 
 export DAE_DB_DIR=$DAE_DB_DIR
 
 EOF
+
+cd -
