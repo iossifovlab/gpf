148a91d9defe12efb4a90e7350d01e97cc3d3dae
Nikolay Stanishev <stanishev.nikolay@gmail.com>
2019-09-12 12:00:22 +0300

    #38 Remove unused url and view

--- wdae/wdae/common_reports_api/urls.py
+++ wdae/wdae/common_reports_api/urls.py
@@ -8,9 +8,6 @@ from django.conf.urls import url
 from common_reports_api import views
 
 urlpatterns = [
-    url(r'^$',
-        views.StudiesSummariesView.as_view(),
-        name="common_reports_all"),
     url(r'^/studies/(?P<common_report_id>.+)$',
         views.VariantReportsView.as_view(),
         name="common_report"),
