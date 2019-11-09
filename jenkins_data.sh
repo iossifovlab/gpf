1975bc9f7d27091e25801d9d572a646c01055e0f
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2019-11-09 18:37:02 +0200

    data startup downloade updated to get genotype storage data...

--- jenkins_data.sh
+++ jenkins_data.sh
@@ -3,25 +3,26 @@
 
 
 dir_setup() {
-    data_tar_time=`stat -c %Y data-hg19-startup-3.0.0dev-genotype-storage.tar.gz`
+    data_tar_time=`stat -c %Y data-hg19-startup-latest.tar.gz`
     data_dir_time=`stat -c %Y data-hg19-startup`
     if [ ! -d 'data-hg19-startup' ] || [ $data_tar_time -nt $data_dir_time ];
     then
-        tar zxf data-hg19-startup-3.0.0dev-genotype-storage.tar.gz
+        tar zxf data-hg19-startup-latest.tar.gz
     fi
 }
 
 get_file() {
     wget -c https://iossifovlab.com/distribution/public/data-hg19-startup-3.0.0dev-genotype-storage.tar.gz
+    git mv data-hg19-startup-3.0.0dev-genotype-storage.tar.gz data-hg19-startup-latest.tar.gz
 }
 
-if [ -f 'data-hg19-startup-3.0.0dev-genotype-storage.tar.gz' ];
+if [ -f 'data-hg19-startup-latest.tar.gz' ];
 then
     new_time=`curl -I https://iossifovlab.com/distribution/public/data-hg19-startup-3.0.0dev-genotype-storage.tar.gz | grep Last-Modified | sed "s/^Last-Modified: \(.*\)$/\1/"`
     python ${SOURCE_DIR}/jenkins_data_check_timestamp.py "$new_time"
     if [ $? != 0 ];
     then
-        rm data-hg19-startup-3.0.0dev-genotype-storage.tar.gz
+        rm data-hg19-startup-latest.tar.gz
 	get_file
     fi
 else
