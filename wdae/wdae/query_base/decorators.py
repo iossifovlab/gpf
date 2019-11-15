dc2bbaf148992102c3b828370cea29d9275ec390
IvoTod <ivo.tod.todorov@gmail.com>
2019-11-15 19:21:57 +0200

    Added decorator for getting dataset

---
+++ wdae/wdae/query_base/decorators.py
@@ -0,0 +1,12 @@
+from utils.datasets import find_dataset_id_in_request
+
+
+def inject_dataset(func):
+    def wrapper(self, request, dataset, *args, **kwargs):
+        dataset_id = find_dataset_id_in_request()
+        if dataset_id is None:
+            dataset = None
+        else:
+            dataset = self.variants_db.get_wdae_wrapper(dataset_id)
+
+        return func(self, request, dataset, *args, **kwargs)
