0ff96477d22b6d41160bd1a1b9dbf49100b2ab57
IvoTod <ivo.tod.todorov@gmail.com>
2019-11-18 10:15:35 +0200

    Add small documentation for the dataset injection decorator

--- wdae/wdae/query_base/decorators.py
+++ wdae/wdae/query_base/decorators.py
@@ -2,6 +2,7 @@ from utils.datasets import find_dataset_id_in_request
 
 
 def inject_dataset(func):
+    """Injects a dataset property into the request of a view method"""
     def wrapper(self, request, dataset, *args, **kwargs):
         dataset_id = find_dataset_id_in_request()
         if dataset_id is None:
