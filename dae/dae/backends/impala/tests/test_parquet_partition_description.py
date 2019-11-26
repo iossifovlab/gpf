63b1031317fd26c19f5080363e954d43a8fe5d4e
IvoTod <ivo.tod.todorov@gmail.com>
2019-11-26 10:02:41 +0200

    Add partition coding bin unit tests

--- dae/dae/backends/impala/tests/test_parquet_partition_description.py
+++ dae/dae/backends/impala/tests/test_parquet_partition_description.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 from dae.backends.impala.parquet_io import ParquetPartitionDescription
 from dae.variants.family_variant import FamilyVariant
 from dae.variants.variant import SummaryAllele, SummaryVariant
@@ -77,3 +78,63 @@ def test_parquet_frequency_bin(fam1, gt, attributes, rare_boundary, expected):
     assert pd.evaluate_variant_filename(fv) == \
         f'1_11/{expected}/' + \
         f'variants_region_bin_1_11_frequency_bin_{expected}.parquet'
+
+
+@pytest.mark.parametrize('eff1, eff2, eff3, coding_effect_types, expected', [
+        (
+            'synonymous!SAMD11:synonymous!NM_152486_1:40/68',
+            'synonymous!SAMD11:synonymous!NM_152486_1:40/68',
+            'synonymous!SAMD11:synonymous!NM_152486_1:40/68',
+            ['synonymous'],
+            1
+        ),
+        (
+            'synonymous!SAMD11:synonymous!NM_152486_1:40/68',
+            'synonymous!SAMD11:synonymous!NM_152486_1:40/68',
+            'synonymous!SAMD11:synonymous!NM_152486_1:40/68',
+            ['missense'],
+            0
+        ),
+        (
+            'synonymous!SAMD11:synonymous!NM_152486_1:40/68',
+            'synonymous!SAMD11:synonymous!NM_152486_1:40/68',
+            'synonymous!SAMD11:synonymous!NM_152486_1:40/68',
+            ['missense', 'synonymous'],
+            1
+        ),
+        (
+            'synonymous!SAMD11:synonymous!NM_152486_1:40/68',
+            'missense!SAMD11:missense!NM_152486_1:54/681(Ser->Arg)',
+            'intergenic!intergenic:intergenic!intergenic:intergenic',
+            ['synonymous'],
+            1
+        ),
+        (
+            'synonymous!SAMD11:synonymous!NM_152486_1:40/68',
+            'missense!SAMD11:missense!NM_152486_1:54/681(Ser->Arg)',
+            'missense!SAMD11:missense!NM_152486_1:54/681(Ser->Arg)',
+            ['nonsense', 'intergenic'],
+            0
+        ),
+    ])
+def test_parquet_coding_bin(fam1, gt, eff1, eff2, eff3,
+                            coding_effect_types, expected):
+    summary_alleles = [
+            SummaryAllele('1', 11539, 'T', None, 0, 0),
+            SummaryAllele('1', 11539, 'T', 'G', 0, 1,
+                          attributes={'effects': eff1}),
+            SummaryAllele('1', 11539, 'T', 'C', 0, 2,
+                          attributes={'effects': eff2}),
+            SummaryAllele('1', 11539, 'T', 'A', 0, 3,
+                          attributes={'effects': eff3}),
+    ]
+    gt = np.array([[0, 1, 0], [2, 0, 3]], dtype='int8')
+    sv = SummaryVariant(summary_alleles)
+    fv = FamilyVariant.from_summary_variant(sv, fam1, gt)
+    pd = ParquetPartitionDescription(
+        ['1'], 1000, coding_effect_types=coding_effect_types
+    )
+    assert pd._evaluate_coding_bin(fv) == expected
+    assert pd.evaluate_variant_filename(fv) == \
+        f'1_11/{expected}/' + \
+        f'variants_region_bin_1_11_coding_bin_{expected}.parquet'
