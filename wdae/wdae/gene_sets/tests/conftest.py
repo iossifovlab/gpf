1d077070c7edc0501348455018b00a489eb4f796
iordanivanov <iiordan.ivanov@gmail.com>
2019-11-27 17:50:14 +0200

    Ref #81 Update wdae gene sets conftest

--- wdae/wdae/gene_sets/tests/conftest.py
+++ wdae/wdae/gene_sets/tests/conftest.py
@@ -5,7 +5,8 @@ from dae.gpf_instance.gpf_instance import GPFInstance
 from gpf_instance.gpf_instance import reload_datasets
 
 from dae.gene.denovo_gene_set_config import DenovoGeneSetConfigParser
-from dae.gene.denovo_gene_set import DenovoGeneSetCollection
+from dae.gene.denovo_gene_set_collection_factory import \
+    DenovoGeneSetCollectionFactory
 
 
 def fixtures_dir():
@@ -37,33 +38,35 @@ def mock_gpf_instance(db, mocker, gpf_instance):
 
 
 @pytest.fixture(scope='session')
-def calc_gene_sets(request, denovo_gene_sets):
-    for dgs in denovo_gene_sets:
-        dgs.load(build_cache=True)
+def genotype_data_names():
+    return ['f1_group', 'f2_group', 'f3_group']
+
+
+@pytest.fixture(scope='session')
+def calc_gene_sets(request, variants_db_fixture, genotype_data_names):
+    for dgs in genotype_data_names:
+        genotype_data = variants_db_fixture.get(dgs)
+        DenovoGeneSetCollectionFactory.build_collection(genotype_data)
 
     print("PRECALCULATION COMPLETE")
 
     def remove_gene_sets():
-        for dgs in denovo_gene_sets:
+        for dgs in genotype_data_names:
+            genotype_data = variants_db_fixture.get(dgs)
+            config = DenovoGeneSetConfigParser.parse(genotype_data.config)
             os.remove(DenovoGeneSetConfigParser.denovo_gene_set_cache_file(
-                dgs.config, 'phenotype'))
+                config, 'phenotype')
+            )
     request.addfinalizer(remove_gene_sets)
-
-
-def get_denovo_gene_set_by_id(variants_db_fixture, dgs_id):
-    denovo_gene_set_config = DenovoGeneSetConfigParser.parse(
-        variants_db_fixture.get_config(dgs_id)
-    )
-
-    return DenovoGeneSetCollection(
-        variants_db_fixture.get(dgs_id), denovo_gene_set_config
-    )
 
 
 @pytest.fixture(scope='session')
 def denovo_gene_sets(variants_db_fixture):
     return [
-        get_denovo_gene_set_by_id(variants_db_fixture, 'f1_group'),
-        get_denovo_gene_set_by_id(variants_db_fixture, 'f2_group'),
-        get_denovo_gene_set_by_id(variants_db_fixture, 'f3_group')
+        DenovoGeneSetCollectionFactory.load_collection(
+            variants_db_fixture.get('f1_group')),
+        DenovoGeneSetCollectionFactory.load_collection(
+            variants_db_fixture.get('f2_group')),
+        DenovoGeneSetCollectionFactory.load_collection(
+            variants_db_fixture.get('f3_group')),
     ]
