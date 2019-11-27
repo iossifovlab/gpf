a1d94ec313d553a970b999be3930feceba8d2419
IvoTod <ivo.tod.todorov@gmail.com>
2019-11-27 14:59:30 +0200

    Restore variants_to_parquet functionality so old tests don't fail

--- dae/dae/backends/impala/parquet_io.py
+++ dae/dae/backends/impala/parquet_io.py
@@ -201,8 +201,8 @@ class VariantsParquetWriter():
 
     def __init__(
             self, fvars,
-            partition_description,
-            root_folder, bucket_index=1,
+            partition_description=None,
+            root_folder='', bucket_index=1,
             rows=100000, include_reference=True,
             include_unknown=True, filesystem=None):
 
@@ -320,8 +320,6 @@ class VariantsParquetWriter():
                     [summary], [frequency], [genomic_scores],
                     effect_genes, [family], member):
 
-                bin_writer = self._get_bin_writer(
-                    summary_variant, family_variant)
                 writer_data = []
                 writer_data.append(('bucket_index', self.bucket_index))
 
@@ -329,14 +327,18 @@ class VariantsParquetWriter():
                     for key, val in d._asdict().items():
                         writer_data.append((key, val))
 
-                bin_writer.data_append(writer_data)
+                return writer_data
 
     def _get_full_filepath(self, filename):
         filepath = os.path.join(self.root_folder, filename)
         return filepath
 
-    def _get_bin_writer(self, summary_variant, family_variant):
-        filename = self.partition_description.evaluate_variant_filename(
+    def _get_bin_writer(self, summary_variant, family_variant,
+                        force_filename=None):
+        if force_filename:
+            filename = force_filename
+        else:
+            filename = self.partition_description.evaluate_variant_filename(
                 family_variant)
 
         if filename not in self.data_writers:
@@ -348,31 +350,32 @@ class VariantsParquetWriter():
                     rows=self.rows)
         return self.data_writers[filename]
 
-    def write_partition(self):
+    def _write_internal(self, force_filename=None):
         family_variant_index = 0
         for summary_variant_index, (summary_variant, family_variants) in \
                 enumerate(self.full_variants_iterator):
             for family_variant in family_variants:
                 family_variant_index += 1
 
+                fv = family_variant
                 if family_variant.is_unknown():
                     if not self.include_unknown:
                         continue
                     # handle all unknown variants
                     unknown_variant = self._setup_all_unknown_variant(
                         summary_variant, family_variant.family_id)
-                    self._process_family_variant(
-                        summary_variant_index,
-                        summary_variant,
-                        family_variant_index,
-                        unknown_variant
-                    )
-                else:
-                    self._process_family_variant(
-                        summary_variant_index,
-                        summary_variant,
-                        family_variant_index,
-                        family_variant)
+                    fv = unknown_variant
+
+                data = self._process_family_variant(
+                    summary_variant_index,
+                    summary_variant,
+                    family_variant_index,
+                    fv)
+
+                bin_writer = self._get_bin_writer(
+                        summary_variant, family_variant, force_filename)
+
+                bin_writer.data_append(data)
 
             if family_variant_index % 1000 == 0:
                 elapsed = time.time() - self.start
@@ -396,40 +399,18 @@ class VariantsParquetWriter():
                 family_variant_index, elapsed),
             file=sys.stderr)
 
+    def write_partition(self):
+        self._write_internal()
+
         self.partition_description.write_partition_configuration_to_file(
                 self.root_folder)
-
 
 #    def variants_table(self):
 #        for key, value in self.data_writers.items():
 #            yield (key, value.build_table())
 #
-#    def save_variants_to_parquet(
-#            self, root_path):
-#
-#        try:
-#            self.fill_bins()
-#            print(self.data_writers.keys())
-#            for (bins, table) in self.variants_table():
-#                filename = root_path
-#                filename = os.path.join(filename, *tuple(map(str, bins)))
-#
-#                if not os.path.exists(filename):
-#                    os.makedirs(filename)
-#
-#                filename = os.path.join(filename,
-#                                        f'variants_region_bin_{bins[0]}'
-#                                        + f'_family_bin_{bins[1]}.parquet')
-#                assert table.schema == self.data_writers[bins].schema
-#                with pq.ParquetWriter(
-#                        filename, table.schema,
-#                        compression='snappy',
-#                        filesystem=self.filesystem) as writer:
-#                    writer.write_table(table)
-#
-#        except Exception as ex:
-#            print('unexpected error:', ex)
-#            traceback.print_exc(file=sys.stdout)
+    def save_variants_to_parquet(self, filename):
+        self._write_internal(filename)
 
 
 class ParquetManager:
