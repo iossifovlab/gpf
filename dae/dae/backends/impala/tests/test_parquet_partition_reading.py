5710861b25b63ed904a844403f0c049313b76a74
IvoTod <ivo.tod.todorov@gmail.com>
2019-11-28 11:13:33 +0200

    Finish basic parquet partition reading test

--- dae/dae/backends/impala/tests/test_parquet_partition_reading.py
+++ dae/dae/backends/impala/tests/test_parquet_partition_reading.py
@@ -3,14 +3,26 @@ from dae.pedigrees.family import PedigreeReader
 from dae.pedigrees.family import FamiliesData
 
 
-def test_reading_partition():
-    ped_file = '/home/ivo/gpf/dae/dae/tests/fixtures/backends/partition.ped'
+def test_partition_read(fixture_dirname):
+    # ped_file = '/home/ivo/gpf/dae/dae/tests/fixtures/backends/partition.ped'
+    ped_file = fixture_dirname('backends/partition.ped')
 
     ped_df = PedigreeReader.flexible_pedigree_read(ped_file)
     families = FamiliesData.from_pedigree_df(ped_df)
 
     loader = ParquetLoader(
             families,
-            '/tmp/dataset-partition-test/_PARTITION_DESCRIPTION')
+            fixture_dirname('backends/test_partition/_PARTITION_DESCRIPTION'))
+    summaries = []
+    genotypes = []
     for summary, genotype in loader.summary_genotypes_iterator():
-        print(summary, genotype)
+        summaries.append(summary)
+        genotypes.append(genotype)
+
+    assert len(summaries) == len(genotypes)
+    assert len(summaries) == 54
+    assert any(s.chrom == '1' for s in summaries)
+    assert any(s.chrom == '2' for s in summaries)
+    assert not any(s.chrom == '3' for s in summaries)
+    assert any(s.reference == 'G' for s in summaries)
+    assert any(s.reference == 'CGGCTCGGAAGG' for s in summaries)
