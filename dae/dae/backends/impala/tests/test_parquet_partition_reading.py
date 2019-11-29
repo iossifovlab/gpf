05a1efb835fd1b5f234dd314ed80da0ca9be4810
IvoTod <ivo.tod.todorov@gmail.com>
2019-11-29 09:12:28 +0200

    Add unit tests for parquet reading globbing

--- dae/dae/backends/impala/tests/test_parquet_partition_reading.py
+++ dae/dae/backends/impala/tests/test_parquet_partition_reading.py
@@ -26,3 +26,51 @@ def test_partition_read(fixture_dirname):
     assert not any(s.chrom == '3' for s in summaries)
     assert any(s.reference == 'G' for s in summaries)
     assert any(s.reference == 'CGGCTCGGAAGG' for s in summaries)
+
+
+def test_partition_read_glob_region_1(fixture_dirname):
+    # ped_file = '/home/ivo/gpf/dae/dae/tests/fixtures/backends/partition.ped'
+    ped_file = fixture_dirname('backends/partition.ped')
+
+    ped_df = PedigreeReader.flexible_pedigree_read(ped_file)
+    families = FamiliesData.from_pedigree_df(ped_df)
+
+    loader = ParquetLoader(
+            families,
+            fixture_dirname('backends/test_partition/_PARTITION_DESCRIPTION'),
+            ['region_bin=1_8/*/*/*/*'])
+    summaries = []
+    genotypes = []
+    for summary, genotype in loader.summary_genotypes_iterator():
+        summaries.append(summary)
+        genotypes.append(genotype)
+
+    assert len(summaries) == len(genotypes)
+    assert len(summaries) == 42
+    assert any(s.chrom == '1' for s in summaries)
+    assert not any(s.chrom == '2' for s in summaries)
+    assert not any(s.chrom == '3' for s in summaries)
+
+
+def test_partition_read_glob_region_2(fixture_dirname):
+    # ped_file = '/home/ivo/gpf/dae/dae/tests/fixtures/backends/partition.ped'
+    ped_file = fixture_dirname('backends/partition.ped')
+
+    ped_df = PedigreeReader.flexible_pedigree_read(ped_file)
+    families = FamiliesData.from_pedigree_df(ped_df)
+
+    loader = ParquetLoader(
+            families,
+            fixture_dirname('backends/test_partition/_PARTITION_DESCRIPTION'),
+            ['region_bin=1_8/*/*/*/*'])
+    summaries = []
+    genotypes = []
+    for summary, genotype in loader.summary_genotypes_iterator():
+        summaries.append(summary)
+        genotypes.append(genotype)
+
+    assert len(summaries) == len(genotypes)
+    assert len(summaries) == 12
+    assert not any(s.chrom == '1' for s in summaries)
+    assert any(s.chrom == '2' for s in summaries)
+    assert not any(s.chrom == '3' for s in summaries)
