ffa44215795271781b0b04f27fab4c384f66fc8c
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2019-12-09 13:31:23 +0200

    fix for handling of gene sets in wdae views...

--- wdae/wdae/gene_sets/expand_gene_set_decorator.py
+++ wdae/wdae/gene_sets/expand_gene_set_decorator.py
@@ -20,8 +20,7 @@ def expand_gene_set(request_function):
             else:
                 gene_sets_db = get_gpf_instance().gene_sets_db
                 gene_set = gene_sets_db.get_gene_set(
-                    gene_sets_collection_id, gene_set_id, gene_sets_types,
-                    IsDatasetAllowed.permitted_datasets(request.user))
+                    gene_sets_collection_id, gene_set_id)
             # del request.data['geneSet']
             request.data['geneSymbols'] = gene_set['syms']
             request.data['geneSet'] = gene_set['desc']
