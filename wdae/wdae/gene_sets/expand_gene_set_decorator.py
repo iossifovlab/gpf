ee5346911f17d640c2d8bf61349e801b58630939
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2019-12-10 12:38:24 +0200

    parameters renamed in call to denovo gene sets db...

--- wdae/wdae/gene_sets/expand_gene_set_decorator.py
+++ wdae/wdae/gene_sets/expand_gene_set_decorator.py
@@ -8,13 +8,13 @@ from dae.utils.gene_utils import GeneSymsMixin
 def expand_gene_set(request_function):
     def decorated(self, request):
         if 'geneSet' in request.data:
-            gene_sets_collection_id, gene_set_id, gene_sets_types = \
+            gene_sets_collection_id, gene_set_id, denovo_gene_set_spec = \
                 GeneSymsMixin.get_gene_set_query(**request.data)
             if gene_sets_collection_id == 'denovo':
                 denovo_gene_sets_db = get_gpf_instance().denovo_gene_sets_db
                 gene_set = denovo_gene_sets_db.get_gene_set(
                     gene_set_id,
-                    gene_sets_types,
+                    denovo_gene_set_spec,
                     IsDatasetAllowed.permitted_datasets(request.user)
                 )
             else:
