e7ee6b37de389e6dfb226262cf5a4e7252640c56
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2019-12-17 11:55:42 +0200

    jenkins build clean up...

--- jenkins_flake8.sh
+++ jenkins_flake8.sh
@@ -6,4 +6,4 @@
 
 cd /code && flake8 --format=pylint \
     --exclude "--exclude \"*old*,*tmp*,*temp*,data-hg19*,gpf*\"" . > \
-    ./pyflakes.report || echo "pylint exited with $?"
+    ./test_results/pyflakes.report || echo "pylint exited with $?"
--- jenkins_test.sh
+++ jenkins_test.sh
@@ -1,36 +1,37 @@
 #!/usr/bin/env bash
 
-export PATH=${DAE_SOURCE_DIR}/dae/tools:$PATH
-export PATH=${DAE_SOURCE_DIR}/dae/tests:$PATH
-export PYTHONPATH=${DAE_SOURCE_DIR}/dae:$PYTHONPATH
-export PYTHONPATH=${DAE_SOURCE_DIR}/dae/tools:$PYTHONPATH
+# export PATH=${DAE_SOURCE_DIR}/dae/tools:$PATH
+# export PATH=${DAE_SOURCE_DIR}/dae/tests:$PATH
+# export PYTHONPATH=${DAE_SOURCE_DIR}/dae:$PYTHONPATH
+# export PYTHONPATH=${DAE_SOURCE_DIR}/dae/tools:$PYTHONPATH
+cd /code
 
-rm -rf coverage/ && mkdir coverage
+find . -name "*.pyc" -delete
+rm -rf \
+	wdae-api.log wdae-debug.log \
+	wdae_django_default.cache wdae_django_pre.cache
+
+# rm -rf test_results/ && mkdir test_results
 
 cd dae && pip install -e . && cd -
 cd wdae && pip install -e . && cd -
 
 py.test -v --cov-config coveragerc \
     --reimport \
-    --junitxml=coverage/dae-junit.xml \
-    --cov-report=html:./coverage/coverage.html \
-    --cov-report=xml:./coverage/coverage.xml \
+    --junitxml=./test_results/dae-junit.xml \
+    --cov-report=html:./test_results/coverage.html \
+    --cov-report=xml:./test_results/coverage.xml \
     --cov dae/ \
     dae/dae/
 
 py.test -v --cov-config coveragerc \
-    --junitxml=coverage/wdae-junit.xml \
+    --junitxml=./test_results/wdae-junit.xml \
     --cov-append \
-    --cov-report=html:./coverage/coverage.html \
-    --cov-report=xml:./coverage/coverage.xml \
+    --cov-report=html:./test_results/coverage.html \
+    --cov-report=xml:./test_results/coverage.xml \
     --cov wdae/ \
     wdae/wdae
 
 
-chmod a+rwx -R coverage
+chmod a+rwx -R /code/test_results
 
-rm -rf \
-	wdae-api.log wdae-debug.log \
-	wdae_django_default.cache wdae_django_pre.cache
-
-find . -name "*.pyc" -delete
