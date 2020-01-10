434a29ae763f02330b4c1df64116ff33aa4d66ae
IvoTod <ivo.tod.todorov@gmail.com>
2020-01-10 09:52:01 +0200

    Fix multi vcf loader unit tests

--- dae/dae/backends/vcf/tests/test_multi_vcf_loader.py
+++ dae/dae/backends/vcf/tests/test_multi_vcf_loader.py
@@ -2,8 +2,7 @@ import pytest
 import os
 import numpy as np
 from dae.backends.vcf.loader import VcfLoader, MultiVcfLoader
-from dae.pedigrees.family import PedigreeReader
-from dae.pedigrees.family import FamiliesData
+from dae.pedigrees.loader import FamiliesLoader
 
 
 @pytest.mark.parametrize('multivcf_files', [
@@ -23,9 +22,8 @@ def test_multivcf_loader(fixture_dirname, multivcf_files):
         lambda x: os.path.join(fixture_dirname('backends'), x), multivcf_files
     ))
 
-    ped_df = PedigreeReader.flexible_pedigree_read(ped_file)
-    families = FamiliesData.from_pedigree_df(ped_df)
-    families_multi = FamiliesData.from_pedigree_df(ped_df)
+    families = FamiliesLoader(ped_file).load()
+    families_multi = FamiliesLoader(ped_file).load()
 
     single_loader = VcfLoader(families, single_vcf)
     assert single_loader is not None
@@ -36,57 +34,53 @@ def test_multivcf_loader(fixture_dirname, multivcf_files):
     for s, m in zip(single_it, multi_it):
         assert s[0] == m[0]
 
-        s_gt_f1 = s[1].get_family_genotype(families.get_family('f1'))
-        m_gt_f1 = m[1].get_family_genotype(families_multi.get_family('f1'))
+        s_gt_f1 = s[1].get_family_genotype(families.get('f1'))
+        m_gt_f1 = m[1].get_family_genotype(families_multi.get('f1'))
         assert all((s_gt_f1 == m_gt_f1).flatten())
 
-        s_gt_f2 = s[1].get_family_genotype(families.get_family('f2'))
-        m_gt_f2 = m[1].get_family_genotype(families_multi.get_family('f2'))
+        s_gt_f2 = s[1].get_family_genotype(families.get('f2'))
+        m_gt_f2 = m[1].get_family_genotype(families_multi.get('f2'))
         assert all((s_gt_f2 == m_gt_f2).flatten())
 
-        s_gt_f3 = s[1].get_family_genotype(families.get_family('f3'))
-        m_gt_f3 = m[1].get_family_genotype(families_multi.get_family('f3'))
+        s_gt_f3 = s[1].get_family_genotype(families.get('f3'))
+        m_gt_f3 = m[1].get_family_genotype(families_multi.get('f3'))
         assert all((s_gt_f3 == m_gt_f3).flatten())
 
-        s_gt_f4 = s[1].get_family_genotype(families.get_family('f4'))
-        m_gt_f4 = m[1].get_family_genotype(families_multi.get_family('f4'))
+        s_gt_f4 = s[1].get_family_genotype(families.get('f4'))
+        m_gt_f4 = m[1].get_family_genotype(families_multi.get('f4'))
         assert all((s_gt_f4 == m_gt_f4).flatten())
 
-        s_gt_f5 = s[1].get_family_genotype(families.get_family('f5'))
-        m_gt_f5 = m[1].get_family_genotype(families_multi.get_family('f5'))
+        s_gt_f5 = s[1].get_family_genotype(families.get('f5'))
+        m_gt_f5 = m[1].get_family_genotype(families_multi.get('f5'))
         assert all((s_gt_f5 == m_gt_f5).flatten())
 
 
-def test_multivcf_loader_fill_missing_ref(fixture_dirname):
+@pytest.mark.parametrize('fill_flag, fill_value', [[True, 0], [False, -1]])
+def test_multivcf_loader_fill_missing(fixture_dirname, fill_flag, fill_value):
     ped_file = fixture_dirname('backends/multivcf.ped')
 
     multivcf_files = [
         fixture_dirname('backends/multivcf_missing1.vcf'),
         fixture_dirname('backends/multivcf_missing2.vcf'),
     ]
-    ped_df = PedigreeReader.flexible_pedigree_read(ped_file)
-    families = FamiliesData.from_pedigree_df(ped_df)
+    families = FamiliesLoader(ped_file).load()
 
-    multi_vcf_loader = MultiVcfLoader(families, multivcf_files, True)
+    multi_vcf_loader = MultiVcfLoader(families, multivcf_files, fill_flag)
     assert multi_vcf_loader is not None
     multi_it = multi_vcf_loader.summary_genotypes_iterator()
-    first_present = next(multi_it)
-    second_missing = next(multi_it)
-    # print(second_missing[1].full_families_genotypes())
-    next(multi_it)
-    next(multi_it)
-    next(multi_it)
-    next(multi_it)
+    sum_gts = [sum_gt for sum_gt in multi_it]
+    first_present = sum_gts[0]
+    second_missing = sum_gts[1]
     assert next(multi_it, None) is None
 
     gt1_f1 = first_present[1].get_family_genotype(
-        multi_vcf_loader.families.get_family('f1'))
+        multi_vcf_loader.families.get('f1'))
     gt1_f1_expected = np.array([
         [1, 0, 0, 0],
         [1, 0, 1, 1]
     ], dtype=np.int8)
     gt1_f5 = first_present[1].get_family_genotype(
-        multi_vcf_loader.families.get_family('f5'))
+        multi_vcf_loader.families.get('f5'))
     gt1_f5_expected = np.array([
         [1, 0, 1, 0],
         [1, 0, 0, 1]
@@ -95,12 +89,12 @@ def test_multivcf_loader_fill_missing_ref(fixture_dirname):
     assert all((gt1_f1 == gt1_f1_expected).flatten())
     assert all((gt1_f5 == gt1_f5_expected).flatten())
 
-    gt2_f1 = second_missing[1].get_family_genotype(families.get_family('f1'))
-    gt2_f2 = second_missing[1].get_family_genotype(families.get_family('f2'))
-    gt2_f3 = second_missing[1].get_family_genotype(families.get_family('f3'))
-    gt2_f5 = second_missing[1].get_family_genotype(families.get_family('f5'))
+    gt2_f1 = second_missing[1].get_family_genotype(families.get('f1'))
+    gt2_f2 = second_missing[1].get_family_genotype(families.get('f2'))
+    gt2_f3 = second_missing[1].get_family_genotype(families.get('f3'))
+    gt2_f5 = second_missing[1].get_family_genotype(families.get('f5'))
 
-    gt2_f1_f2_f3_expected = np.array([[0]*4]*2, dtype=np.int8)
+    gt2_f1_f2_f3_expected = np.array([[fill_value]*4]*2, dtype=np.int8)
     gt2_f5_expected = np.array([
         [0, 1, 1, 0],
         [0, 1, 0, 1]
@@ -110,58 +104,9 @@ def test_multivcf_loader_fill_missing_ref(fixture_dirname):
     assert all((gt2_f2 == gt2_f1_f2_f3_expected).flatten())
     assert all((gt2_f3 == gt2_f1_f2_f3_expected).flatten())
     assert all((gt2_f5 == gt2_f5_expected).flatten())
-
-
-def test_multivcf_loader_fill_missing_unknown(fixture_dirname):
-    ped_file = fixture_dirname('backends/multivcf.ped')
-
-    multivcf_files = [
-        fixture_dirname('backends/multivcf_missing1.vcf'),
-        fixture_dirname('backends/multivcf_missing2.vcf'),
-    ]
-    ped_df = PedigreeReader.flexible_pedigree_read(ped_file)
-    families = FamiliesData.from_pedigree_df(ped_df)
-
-    multi_vcf_loader = MultiVcfLoader(families, multivcf_files, False)
-    assert multi_vcf_loader is not None
-    multi_it = multi_vcf_loader.summary_genotypes_iterator()
-    first_present = next(multi_it)
-    second_missing = next(multi_it)
-    # print(second_missing[1].full_families_genotypes())
-    next(multi_it)
-    next(multi_it)
-    next(multi_it)
-    next(multi_it)
-    assert next(multi_it, None) is None
-
-    gt1_f1 = first_present[1].get_family_genotype(
-        multi_vcf_loader.families.get_family('f1'))
-    gt1_f1_expected = np.array([
-        [1, 0, 0, 0],
-        [1, 0, 1, 1]
-    ], dtype=np.int8)
-    gt1_f5 = first_present[1].get_family_genotype(
-        multi_vcf_loader.families.get_family('f5'))
-    gt1_f5_expected = np.array([
-        [1, 0, 1, 0],
-        [1, 0, 0, 1]
-    ], dtype=np.int8)
-
-    assert all((gt1_f1 == gt1_f1_expected).flatten())
-    assert all((gt1_f5 == gt1_f5_expected).flatten())
-
-    gt2_f1 = second_missing[1].get_family_genotype(families.get_family('f1'))
-    gt2_f2 = second_missing[1].get_family_genotype(families.get_family('f2'))
-    gt2_f3 = second_missing[1].get_family_genotype(families.get_family('f3'))
-    gt2_f5 = second_missing[1].get_family_genotype(families.get_family('f5'))
-
-    gt2_f1_f2_f3_expected = np.array([[-1]*4]*2, dtype=np.int8)
-    gt2_f5_expected = np.array([
-        [0, 1, 1, 0],
-        [0, 1, 0, 1]
-    ], dtype=np.int8)
-
-    assert all((gt2_f1 == gt2_f1_f2_f3_expected).flatten())
-    assert all((gt2_f2 == gt2_f1_f2_f3_expected).flatten())
-    assert all((gt2_f3 == gt2_f1_f2_f3_expected).flatten())
-    assert all((gt2_f5 == gt2_f5_expected).flatten())
+    assert(sum_gts[0][0].ref_allele.position == 865582)
+    assert(sum_gts[1][0].ref_allele.position == 865583)
+    assert(sum_gts[2][0].ref_allele.position == 865624)
+    assert(sum_gts[3][0].ref_allele.position == 865627)
+    assert(sum_gts[4][0].ref_allele.position == 865664)
+    assert(sum_gts[5][0].ref_allele.position == 865691)
