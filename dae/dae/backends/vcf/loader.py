d514d6d1134cda931ca219cd148e81eb4224f021
IvoTod <ivo.tod.todorov@gmail.com>
2020-01-10 16:03:15 +0200

    Fix VcfLoader missing fill and remove MultiVcfLoader

--- dae/dae/backends/vcf/loader.py
+++ dae/dae/backends/vcf/loader.py
@@ -1,12 +1,9 @@
 import os
-from copy import deepcopy
 
 from cyvcf2 import VCF
 
 import numpy as np
 
-from typing import List, Dict
-from dae.pedigrees.family import FamiliesData
 
 from dae.utils.variant_utils import is_all_reference_genotype, \
     is_all_unknown_genotype, is_unknown_genotype
@@ -192,7 +189,7 @@ class VcfLoader(VariantsLoader):
 
         gt = np.array([[0] * 3] * sample_count, dtype=np.int8)
 
-        gt[0:2] = self.fill_missing_value
+        gt[0:sample_count] = self.fill_missing_value
 
         return gt
 
@@ -282,175 +279,3 @@ class VcfLoader(VariantsLoader):
             'include_unknown_person_genotypes': argv.vcf_include_unknown
         }
         return params
-
-
-class MultiVcfLoader(VariantsLoader):
-
-    def __init__(
-            self,
-            families: FamiliesData,
-            vcf_files: List[str],
-            reference_on_missing: bool = True,
-            regions=None,
-            params: Dict[str, bool] = {}):
-
-        super(MultiVcfLoader, self).__init__(
-            families=families,
-            filename=vcf_files[0],
-            source_type='vcf',
-            transmission_type=TransmissionType.transmitted,
-            params=params
-        )
-
-        self.families = families
-        self.vcf_files = vcf_files
-        self.reference_on_missing = reference_on_missing
-        self.fill_missing_value = 0 if reference_on_missing else -1
-        self.regions = regions
-        self.vcf_loaders = list()
-        self._init_loaders()
-        self.seqnames = self._init_chromosome_order()
-
-        # TODO: Find a better place for VcfLoader's _match_pedigree_to_samples
-        # method
-        VcfLoader._match_pedigree_to_samples(
-                self.families,
-                self._get_all_samples())
-
-    def _init_loaders(self):
-        for vcf_file in self.vcf_files:
-            loader_families = deepcopy(self.families)
-
-            loader = VcfLoader(
-                loader_families,
-                vcf_file,
-                regions=self.regions,
-                params=self.params)
-
-            self.vcf_loaders.append(loader)
-
-    def _init_chromosome_order(self) -> List[str]:
-        seqnames = list()
-
-        seqnames = self.vcf_loaders[0].vcf.seqnames
-        assert \
-            all([l.vcf.seqnames == seqnames for l in self.vcf_loaders])
-
-        return seqnames
-
-    def _get_all_samples(self):
-        samples = list()
-        for loader in self.vcf_loaders:
-            samples += loader.vcf.samples
-        return samples
-
-    def _generate_missing_vcf_families_genotype(
-            self, loader: VcfLoader):
-        sample_count = len(loader.vcf.samples)
-        gt = np.array(
-            [[0] * 3] * sample_count, dtype=np.int8).T
-        gt[0:2] = self.fill_missing_value
-        return VcfFamiliesGenotypes(
-            loader.families,
-            gt,
-            params=loader.params
-        )
-
-    def _merge_summary_genotypes(self, summary_variant, genotypes):
-        sv = summary_variant
-        gt_values = tuple()
-        for gt in genotypes:
-            gt_values += tuple(gt.T)
-        gt = np.stack(gt_values).T
-        gt = VcfFamiliesGenotypes(self.families, gt, self.params)
-        return sv, gt
-
-    def _fill_missing_family_genotypes(
-            self, missing_variant_indexes, summary_genotypes, iterators):
-        genotypes = list()
-        for idx, summary_gt in enumerate(summary_genotypes):
-            if idx not in missing_variant_indexes:
-                genotypes.append(
-                    summary_gt[1].full_families_genotypes()
-                )
-                summary_genotypes[idx] = \
-                    next(iterators[idx], None)
-            else:
-                missing_raw_gt = \
-                    self._generate_missing_vcf_families_genotype(
-                        self.vcf_loaders[idx])
-
-                missing_gt = \
-                    missing_raw_gt.full_families_genotypes()
-
-                genotypes.append(missing_gt)
-
-        return genotypes
-
-    def _compare_summaries_g(self, lhs, rhs):
-        """
-        Returns true if left summary variant position in file is
-        larger than right summary variant position in file
-        """
-        l_chrom_idx = self.seqnames.index(lhs.ref_allele.chromosome)
-        r_chrom_idx = self.seqnames.index(rhs.ref_allele.chromosome)
-
-        if l_chrom_idx > r_chrom_idx:
-            return True
-        elif lhs.ref_allele.position > rhs.ref_allele.position:
-            return True
-        else:
-            return False
-
-    def _find_min_summary_index(self, summary_genotypes) -> int:
-        assert len(summary_genotypes)
-        min = 0
-        for i in range(1, len(summary_genotypes)):
-            if self._compare_summaries_g(
-                    summary_genotypes[min][0],
-                    summary_genotypes[i][0]):
-                min = i
-        return min
-
-    def summary_genotypes_iterator(self):
-        sum_gt_iterators = \
-            [l.summary_genotypes_iterator() for l in self.vcf_loaders]
-
-        current_summary_genotypes = [next(it, None) for it in sum_gt_iterators]
-        while True:
-            if all(sum_gt is None for sum_gt in current_summary_genotypes):
-                break
-
-            sum_gt_missing_stack = list()
-
-            min_summary_variant_idx = \
-                self._find_min_summary_index(current_summary_genotypes)
-            min_summary_variant = \
-                current_summary_genotypes[min_summary_variant_idx][0]
-
-            for idx, sum_gt in enumerate(current_summary_genotypes):
-                if(sum_gt[0] != min_summary_variant):
-                    sum_gt_missing_stack.append(idx)
-
-            if len(sum_gt_missing_stack):
-
-                genotypes = self._fill_missing_family_genotypes(
-                    sum_gt_missing_stack,
-                    current_summary_genotypes,
-                    sum_gt_iterators)
-
-                yield self._merge_summary_genotypes(
-                    min_summary_variant, genotypes)
-
-                continue
-
-            summaries, genotypes = zip(*current_summary_genotypes)
-
-            genotypes = \
-                list(map(
-                    lambda x: x.full_families_genotypes(), genotypes))
-
-            yield self._merge_summary_genotypes(summaries[0], genotypes)
-
-            current_summary_genotypes = \
-                [next(it, None) for it in sum_gt_iterators]
