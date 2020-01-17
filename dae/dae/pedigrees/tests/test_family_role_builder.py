0d38ae2858204738f7d0fa1bf2d9f73b13449494
IvoTod <ivo.tod.todorov@gmail.com>
2020-01-17 11:48:31 +0200

    Make family role builder tests more precise
    Add new test for large family with only prb role given

--- dae/dae/pedigrees/tests/test_family_role_builder.py
+++ dae/dae/pedigrees/tests/test_family_role_builder.py
@@ -33,6 +33,10 @@ def test_paternal_and_maternal_grandparents(fixture_dirname):
     assert members[1].role == Role.maternal_grandmother
     assert members[2].role == Role.paternal_grandfather
     assert members[3].role == Role.paternal_grandmother
+    assert members[4].role == Role.dad
+    assert members[5].role == Role.mom
+    assert members[6].role == Role.prb
+    assert members[7].role == Role.sib
 
 
 def test_child_and_spouse(fixture_dirname):
@@ -43,6 +47,10 @@ def test_child_and_spouse(fixture_dirname):
     role_builder.build_roles()
     members = family.full_members
 
+    assert members[0].role == Role.dad
+    assert members[1].role == Role.mom
+    assert members[2].role == Role.prb
+    assert members[3].role == Role.sib
     assert members[4].role == Role.spouse
     assert members[5].role == Role.child
 
@@ -55,10 +63,18 @@ def test_maternal_and_paternal_aunts_and_uncles(fixture_dirname):
     role_builder.build_roles()
     members = family.full_members
 
+    assert members[0].role == Role.maternal_grandfather
+    assert members[1].role == Role.maternal_grandmother
+    assert members[2].role == Role.paternal_grandfather
+    assert members[3].role == Role.paternal_grandmother
+    assert members[4].role == Role.dad
+    assert members[5].role == Role.mom
     assert members[6].role == Role.maternal_aunt
     assert members[7].role == Role.maternal_uncle
     assert members[8].role == Role.paternal_aunt
     assert members[9].role == Role.paternal_uncle
+    assert members[10].role == Role.prb
+    assert members[11].role == Role.sib
 
 
 def test_maternal_and_paternal_cousins(fixture_dirname):
@@ -69,6 +85,18 @@ def test_maternal_and_paternal_cousins(fixture_dirname):
     role_builder.build_roles()
     members = family.full_members
 
+    assert members[0].role == Role.maternal_grandfather
+    assert members[1].role == Role.maternal_grandmother
+    assert members[2].role == Role.paternal_grandfather
+    assert members[3].role == Role.paternal_grandmother
+    assert members[4].role == Role.dad
+    assert members[5].role == Role.mom
+    assert members[6].role == Role.maternal_aunt
+    assert members[7].role == Role.unknown
+    assert members[8].role == Role.unknown
+    assert members[9].role == Role.paternal_uncle
+    assert members[10].role == Role.prb
+    assert members[11].role == Role.sib
     assert members[12].role == Role.maternal_cousin
     assert members[13].role == Role.paternal_cousin
 
@@ -81,9 +109,51 @@ def test_stepmom_and_stepdad(fixture_dirname):
     role_builder.build_roles()
     members = family.full_members
 
+    assert members[0].role == Role.dad
+    assert members[1].role == Role.mom
+
     assert members[2].role == Role.step_dad
     assert members[3].role == Role.step_mom
 
+    assert members[4].role == Role.maternal_half_sibling
+    assert members[5].role == Role.paternal_half_sibling
+    assert members[6].role == Role.prb
+    assert members[7].role == Role.sib
+
 
 def test_handling_of_family_with_only_prb_role(fixture_dirname):
-    pass
+    ped_file = fixture_dirname('pedigrees/pedigree_prb_only.ped')
+    families = FamiliesLoader.load_pedigree_file(ped_file)
+    family = families.get('f1')
+    role_builder = FamilyRoleBuilder(family)
+    role_builder.build_roles()
+    members = family.full_members
+
+    assert members[0].role == Role.dad
+    assert members[1].role == Role.mom
+    assert members[2].role == Role.prb
+    assert members[3].role == Role.sib
+
+
+def test_handling_of_large_family_with_only_prb_role(fixture_dirname):
+    ped_file = fixture_dirname('pedigrees/pedigree_prb_only_large.ped')
+    families = FamiliesLoader.load_pedigree_file(ped_file)
+    family = families.get('f1')
+    role_builder = FamilyRoleBuilder(family)
+    role_builder.build_roles()
+    members = family.full_members
+
+    assert members[0].role == Role.maternal_grandfather
+    assert members[1].role == Role.maternal_grandmother
+    assert members[2].role == Role.paternal_grandfather
+    assert members[3].role == Role.paternal_grandmother
+    assert members[4].role == Role.dad
+    assert members[5].role == Role.mom
+    assert members[6].role == Role.maternal_aunt
+    assert members[7].role == Role.unknown
+    assert members[8].role == Role.unknown
+    assert members[9].role == Role.paternal_uncle
+    assert members[10].role == Role.prb
+    assert members[11].role == Role.sib
+    assert members[12].role == Role.maternal_cousin
+    assert members[13].role == Role.paternal_cousin
