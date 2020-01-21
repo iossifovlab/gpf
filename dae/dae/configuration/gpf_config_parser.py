2872255f82b3a3d71916294ad215822e97c66ee2
iordanivanov <iiordan.ivanov@gmail.com>
2020-01-21 16:36:11 +0200

    Implement environment overrides and interpolations

--- dae/dae/configuration/gpf_config_parser.py
+++ dae/dae/configuration/gpf_config_parser.py
@@ -1,17 +1,26 @@
 import os
 import glob
+import re
+
 import yaml
 import json
 import toml
 
-from cerberus import Validator
+from copy import deepcopy
 from collections import namedtuple
-from Typing import List, Tuple, Any
+from Typing import List, Tuple, Any, Callable
+from cerberus import Validator
 
 from dae.utils.dict_utils import recursive_dict_update
 
 
+def environ_override(field: str) -> Callable:
+    return lambda value: os.environ.get(field, None) or value
+
+
 class GPFConfigParser:
+
+    interpolation_var_regex = r"%\(([A-Za-z0-9_]+)\)s"
 
     filetype_parsers: dict = {
         ".yaml": yaml.safe_load,
@@ -32,10 +41,36 @@ class GPFConfigParser:
         return tup_ctor(*input_dict.values())
 
     @classmethod
+    def _interpolate(cls, input_dict, **interpolation_vars):
+        result_dict = deepcopy(input_dict)
+
+        for key, val in result_dict.items():
+            if isinstance(val, str):
+                matched_interpolations = re.findall(
+                    cls.interpolation_vars, val
+                )
+                for interpolation in matched_interpolations:
+                    assert (
+                        interpolation in interpolation_vars
+                    ), (f"Undefined var '{interpolation}'!"
+                        " Defined vars: {interpolation_vars.keys()}")
+                    result_dict[key] = val.replace(
+                        f"%({interpolation})s",
+                        interpolation_vars[interpolation],
+                    )
+            elif isinstance(val, dict):
+                result_dict[key] = cls._interpolate(val, interpolation_vars)
+
+        return result_dict
+
+    @classmethod
     def _read_config(cls, filename: str) -> dict:
         ext = os.path.splitext(filename)[1]
         assert ext in cls.filetype_parsers, f"Unsupported filetype {filename}!"
         conf_dict = cls.filetype_parsers[ext](filename)
+        if "vars" in conf_dict:
+            conf_dict = cls._interpolate(conf_dict, **conf_dict["vars"])
+            del(conf_dict["vars"])
         return conf_dict
 
     @classmethod
