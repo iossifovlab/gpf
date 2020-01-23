fixed handling of best state calculation on X...

dae/dae/backends/raw/loader.py
@@ -489,12 +489,12 @@ class VariantsGenotypesLoader(VariantsLoader):
                 if best_state[0, idx] in (1, 2):
                     best_state[0, idx] -= 1
                 elif np.any(best_state[:, idx] == 2):
-                    best_state[best_state[:, idx] == 2] -= 1
-                else:
-                    print(
-                        "WARN: can't handle broken X best state:",
-                        family_variant, mat2str(family_variant.gt),
-                        file=sys.stderr)
+                    best_state[best_state[:, idx] == 2, idx] -= 1
+                # else:
+                #     print(
+                #         "WARN: can't handle broken X best state:",
+                #         family_variant, mat2str(family_variant.gt),
+                #         file=sys.stderr)
 
         return best_state
 

dae/dae/backends/raw/tests/test_best_state_calc.py
@@ -153,21 +153,27 @@ def test_family_variant_X_best2gt(bs, gt, gm1, gm2, fvX1, fvX2, genome_2013):
     assert genetic_model == gm2
 
 
-@pytest.mark.xfail(reason="handling of best state for X is broken")
 @pytest.mark.parametrize("gt,bs1,gm1,bs2,gm2", [
     (
         np.array([[0, 0, 1], [0, 0, 0]], dtype='int8'),
-        "221/001", GeneticModel.pseudo_autosomal,
-        "210/001", GeneticModel.X_broken
+        "221/001/000", GeneticModel.pseudo_autosomal,
+        "210/001/000", GeneticModel.X_broken
     ),
     (
         np.array([[1, 1, 1], [1, 1, 1]], dtype='int8'),
-        "000/222", GeneticModel.pseudo_autosomal,
-        "000/211", GeneticModel.X_broken
+        "000/222/000", GeneticModel.pseudo_autosomal,
+        "000/211/000", GeneticModel.X_broken
     ),
-    # (np.array([[0, 0, 1], [0, 0, 0]], dtype='int8'), "221/001/000"),
-    # (np.array([[0, 0, 0], [0, 0, 2]], dtype='int8'), "221/000/001"),
-    # (np.array([[0, 0, 0], [0, 0, 0]], dtype='int8'), "222/000/000"),
+    (
+        np.array([[0, 1, 0], [1, 2, 1]], dtype='int8'),
+        "101/111/010", GeneticModel.pseudo_autosomal,
+        "100/111/010", GeneticModel.X_broken
+    ),
+    (
+        np.array([[1, 1, 0], [2, 2, 1]], dtype='int8'),
+        "001/111/110", GeneticModel.pseudo_autosomal,
+        "000/111/110", GeneticModel.X_broken
+    ),
 ])
 def test_family_variant_X_gt2best_st(
         fvX1, fvX2, gt, bs1, gm1, bs2, gm2, genome_2013):