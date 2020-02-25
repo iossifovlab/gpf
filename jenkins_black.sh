ae6ead0dec6ff661c8f9a7e926b2c205ca52f91e
iordanivanov <iiordan.ivanov@gmail.com>
2020-02-25 17:36:33 +0200

    Update jenkins_black script to fail on error

--- jenkins_black.sh
+++ jenkins_black.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /code && black --line-length 79 --check . || echo "black exited with $?"
+cd /code && black --line-length 79 --check . || { echo "Black failed to verify formatting, exited with $?"; exit 1 }
