fbcccc205d070af4603e127064e25598522ad834
iordanivanov <iiordan.ivanov@gmail.com>
2020-02-26 11:08:51 +0200

    Fix jenkins_mypy script

--- jenkins_mypy.sh
+++ jenkins_mypy.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 cd /code
-mkdir mypy_report
-mypy dae/dae wdae/wdae --pretty \
-     --ignore-missing-imports \
-     --warn-return-any \
-     --warn-redundant-casts \
-     --html-report mypy_report \
-     || ( echo "Mypy failed to check typing, exited with $?"; exit 1 )
-tar czf mypy_report.tar.gz mypy_report
+
+if mypy dae/dae wdae/wdae --pretty --ignore-missing-imports --warn-return-any --warn-redundant-casts --html-report mypy_report
+then
+	tar czf mypy_report.tar.gz mypy_report
+else
+	echo "Mypy failed to check typing, exited with $?"; exit 1
+fi
