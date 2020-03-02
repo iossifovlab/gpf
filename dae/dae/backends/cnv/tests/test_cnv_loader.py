a4aaf4da6532179e0d6a9496f5e7fd2b5fb43426
IvoTod <ivo.tod.todorov@gmail.com>
2020-03-02 11:09:23 +0200

    Add cnv loader unit test

---
+++ dae/dae/backends/cnv/tests/test_cnv_loader.py
@@ -0,0 +1,19 @@
+from dae.backends.cnv.loader import CNVLoader
+from dae.pedigrees.loader import FamiliesLoader
+
+
+def test_cnv_loader(fixture_dirname, genomes_db_2013):
+    families_file = fixture_dirname("backends/cnv_ped.txt")
+    families = FamiliesLoader.load_simple_families_file(families_file)
+    assert families is not None
+
+    variants_file = fixture_dirname("backends/cnv_variants.txt")
+
+    loader = CNVLoader(families, variants_file, genomes_db_2013.get_genome())
+    assert loader is not None
+
+    svs = []
+    for sv, fvs in loader.full_variants_iterator():
+        svs.append(sv)
+
+    assert len(svs) == 53
