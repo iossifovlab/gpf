9324c5e9a12cf923a2a455899dac46cf48689c4d
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2020-03-09 14:04:56 +0200

    simple family file loading performance improvements...

--- dae/dae/pedigrees/tests/test_load_family_simple.py
+++ dae/dae/pedigrees/tests/test_load_family_simple.py
@@ -5,10 +5,8 @@ Created on Jul 23, 2018
 """
 import os
 import pytest
-import pandas as pd
 
 from dae.pedigrees.loader import FamiliesLoader
-from dae.pedigrees.family import FamiliesData
 
 
 @pytest.mark.parametrize("fixture_name", ["pedigrees/family_simple.txt"])
@@ -16,24 +14,11 @@ def test_load_family_simple(fixture_name, temp_filename, fixture_dirname):
     family_filename = fixture_dirname(fixture_name)
     assert os.path.exists(family_filename)
 
-    fam_df = FamiliesLoader.load_simple_family_file(family_filename)
-    assert fam_df is not None
-    print("-------------------------")
-    print("-------------------------")
-    print(fam_df)
-    print("-------------------------")
-
-    families = FamiliesData.from_pedigree_df(fam_df)
-
+    families = FamiliesLoader.load_simple_families_file(family_filename)
     assert families is not None
 
     FamiliesLoader.save_pedigree(families, temp_filename)
-    assert fam_df is not None
 
-    ped_df = FamiliesLoader.flexible_pedigree_read(temp_filename)
-    print("-------------------------")
-    print("-------------------------")
-    print(ped_df)
-    print("-------------------------")
+    families1 = FamiliesLoader.load_pedigree_file(temp_filename)
 
-    pd.testing.assert_frame_equal(fam_df, ped_df, check_like=True)
+    assert set(families.keys()) == set(families1.keys())
