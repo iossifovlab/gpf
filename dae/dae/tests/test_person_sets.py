78050e7992dcd27d41f5f86dd3812acc2c07c209
iordanivanov <iiordan.ivanov@gmail.com>
2020-03-18 15:48:01 +0200

    Replace some redundant fixtures with function
    Replaced person set collection config fixtures with a function

--- dae/dae/tests/test_person_sets.py
+++ dae/dae/tests/test_person_sets.py
@@ -6,45 +6,24 @@ from dae.pedigrees.loader import FamiliesLoader
 from dae.person_sets import produce_sets, from_pedigree, PersonSet
 
 
-@pytest.fixture(scope="module")
-def sample_person_sets_config(fixture_dirname):
+def get_person_set_collections_config(config_path):
     return GPFConfigParser.load_config(
-        fixture_dirname("sample_person_sets.toml"),
-        {"person_set_collections": person_set_collections_schema},
+        config_path, {"person_set_collections": person_set_collections_schema},
     ).person_set_collections
 
 
-@pytest.fixture(scope="module")
-def quads_f1_person_sets_config(fixture_dirname):
-    return GPFConfigParser.load_config(
-        fixture_dirname("quads_f1_person_sets.toml"),
-        {"person_set_collections": person_set_collections_schema},
-    ).person_set_collections
-
-
-@pytest.fixture(scope="module")
-def quads_f1_person_sets_config_incomplete_domain(fixture_dirname):
-    return GPFConfigParser.load_config(
-        fixture_dirname("quads_f1_person_sets_incomplete_domain.toml"),
-        {"person_set_collections": person_set_collections_schema},
-    ).person_set_collections
-
-
-@pytest.fixture(scope="module")
-def quads_f1_person_sets_config_nonexistent_domain(fixture_dirname):
-    return GPFConfigParser.load_config(
-        fixture_dirname("quads_f1_person_sets_nonexistent_domain.toml"),
-        {"person_set_collections": person_set_collections_schema},
-    ).person_set_collections
-
-
-def test_load_config(sample_person_sets_config):
-    config = sample_person_sets_config
+def test_load_config(fixture_dirname):
+    config = get_person_set_collections_config(
+        fixture_dirname("sample_person_sets.toml")
+    )
     assert hasattr(config, "status") and hasattr(config, "phenotype")
 
 
-def test_produce_sets(sample_person_sets_config):
-    people_sets = produce_sets(sample_person_sets_config.status)
+def test_produce_sets(fixture_dirname):
+    config = get_person_set_collections_config(
+        fixture_dirname("sample_person_sets.toml")
+    )
+    people_sets = produce_sets(config.status)
     assert people_sets == {
         "affected": PersonSet(
             "affected", "Affected", "affected_val", "#aabbcc", list()
@@ -55,13 +34,14 @@ def test_produce_sets(sample_person_sets_config):
     }
 
 
-def test_from_pedigree(fixture_dirname, quads_f1_person_sets_config):
+def test_from_pedigree(fixture_dirname):
+    config = get_person_set_collections_config(
+        fixture_dirname("quads_f1_person_sets.toml")
+    )
     quads_f1_families = FamiliesLoader(
         fixture_dirname("studies/quads_f1/data/quads_f1.ped")
     ).load()
-    status_collection = from_pedigree(
-        quads_f1_person_sets_config.status, quads_f1_families
-    )
+    status_collection = from_pedigree(config.status, quads_f1_families)
 
     result_person_ids = set(
         (
@@ -76,34 +56,34 @@ def test_from_pedigree(fixture_dirname, quads_f1_person_sets_config):
     }
 
 
-def test_from_pedigree_missing_value_in_domain(
-    fixture_dirname, quads_f1_person_sets_config_incomplete_domain
-):
+def test_from_pedigree_missing_value_in_domain(fixture_dirname):
+    config = get_person_set_collections_config(
+        fixture_dirname("quads_f1_person_sets_incomplete_domain.toml")
+    )
     quads_f1_families = FamiliesLoader(
         fixture_dirname("studies/quads_f1/data/quads_f1.ped")
     ).load()
 
     with pytest.raises(AssertionError) as excinfo:
         from_pedigree(
-            quads_f1_person_sets_config_incomplete_domain.status,
-            quads_f1_families,
+            config.status, quads_f1_families,
         )
     assert "Domain for 'status' does not have the value 'affected'!" in str(
         excinfo.value
     )
 
 
-def test_from_pedigree_nonexistent_domain(
-    fixture_dirname, quads_f1_person_sets_config_nonexistent_domain
-):
+def test_from_pedigree_nonexistent_domain(fixture_dirname):
+    config = get_person_set_collections_config(
+        fixture_dirname("quads_f1_person_sets_nonexistent_domain.toml")
+    )
     quads_f1_families = FamiliesLoader(
         fixture_dirname("studies/quads_f1/data/quads_f1.ped")
     ).load()
 
     with pytest.raises(AssertionError) as excinfo:
         from_pedigree(
-            quads_f1_person_sets_config_nonexistent_domain.invalid,
-            quads_f1_families,
+            config.invalid, quads_f1_families,
         )
 
     assert "Missing domain value for 'invalid' in person" in str(excinfo.value)
