aa4a1d7bea847c8dd4c17f2618c0031d710cd493
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2020-03-31 18:26:42 +0300

    trying to fix jenkins build...

--- jenkins_flake8.sh
+++ jenkins_flake8.sh
@@ -8,6 +8,8 @@
 #     --exclude "--exclude \"*old*,*tmp*,*temp*,data-hg19*,gpf*\"" . > \
 #     ./test_results/pyflakes.report || echo "pylint exited with $?"
 
-# cd /code && flake8 --format=pylint \
-#     --exclude "--exclude \"*old*,*tmp*,*temp*,data-hg19*,gpf*\"" . > \
-#     ./test_results/pyflakes.report
+cd /code && flake8 --format=pylint \
+    --exclude "--exclude \"*old*,*tmp*,*temp*,data-hg19*,gpf*\"" . > \
+    ./test_results/pyflakes.report || echo "pylint exited with $?"
+
+exit 0
