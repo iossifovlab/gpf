342cd9e19b7573812c647233d2695cc9e66bcea0
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2020-03-31 17:51:56 +0300

    trying to fix jenkins build...

--- jenkins_flake8.sh
+++ jenkins_flake8.sh
@@ -4,6 +4,10 @@
 # conda activate gpf
 
 
+# cd /code && flake8 --format=pylint \
+#     --exclude "--exclude \"*old*,*tmp*,*temp*,data-hg19*,gpf*\"" . > \
+#     ./test_results/pyflakes.report || echo "pylint exited with $?"
+
 cd /code && flake8 --format=pylint \
     --exclude "--exclude \"*old*,*tmp*,*temp*,data-hg19*,gpf*\"" . > \
-    ./test_results/pyflakes.report || echo "pylint exited with $?"
+    ./test_results/pyflakes.report
