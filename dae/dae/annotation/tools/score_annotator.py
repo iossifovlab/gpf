c77dbed642b8cbd92cd6b7ab59c9d795b48d20f9
iordanivanov <iiordan.ivanov@gmail.com>
2020-04-15 15:21:49 +0300

    Change annotation method for PositionScoreAnnotator

--- dae/dae/annotation/tools/score_annotator.py
+++ dae/dae/annotation/tools/score_annotator.py
@@ -89,6 +89,12 @@ class PositionScoreAnnotator(VariantScoreAnnotatorBase):
     def __init__(self, config, genomes_db):
         super(PositionScoreAnnotator, self).__init__(config, genomes_db)
 
+    def _convert_score(self, score):
+        if score == "NA":
+            return self.score_file.no_score_value
+        else:
+            return float(score)
+
     def do_annotate(self, aline, variant):
         if variant is None:
             self._scores_not_found(aline)
@@ -100,23 +106,30 @@ class PositionScoreAnnotator(VariantScoreAnnotatorBase):
             self._scores_not_found(aline)
             return
 
-        scores_df = self.score_file.scores_to_dataframe(scores)
         counts = scores["COUNT"]
         total_count = sum(counts)
 
         for score_name in self.score_names:
             column_name = getattr(self.config.columns, score_name)
-            values = scores_df[score_name]
+            values = list(
+                map(lambda x: self._convert_score(x), scores[score_name])
+            )
             assert len(values) > 0
             if len(values) == 1:
                 aline[column_name] = values[0]
-            elif VariantType.is_cnv(variant.variant_type):
-                aline[column_name] = values.max()
             else:
-                total_sum = sum(
-                    [c * v for (c, v) in zip(counts, values)]
-                )
-                aline[column_name] = total_sum / total_count
+                values = list(filter(None, values))
+                if VariantType.is_cnv(variant.variant_type):
+                    aline[column_name] = \
+                        max(values) if values \
+                        else self.score_file.no_score_value
+                else:
+                    total_sum = sum(
+                        [c * v for (c, v) in zip(counts, values)]
+                    )
+                    aline[column_name] = \
+                        (total_sum / total_count) if total_sum \
+                        else self.score_file.no_score_value
 
 
 class NPScoreAnnotator(VariantScoreAnnotatorBase):
@@ -128,8 +141,6 @@ class NPScoreAnnotator(VariantScoreAnnotatorBase):
         self.pos_begin_name = self.score_file.pos_begin_name
 
     def _aggregate_substitution(self, variant, scores_df):
-        assert self.score_file.ref_name is not None
-        assert self.score_file.alt_name is not None
         assert variant.variant_type == VariantType.substitution
 
         res = {}
