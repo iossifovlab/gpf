a7d63da9105261e9bd3c64371e9949183865a752
iordanivanov <iiordan.ivanov@gmail.com>
2020-05-07 15:46:28 +0300

    Rework handling of study wrapper phenotype filters

--- dae/dae/studies/tests/test_pheno_filters.py
+++ dae/dae/studies/tests/test_pheno_filters.py
@@ -36,9 +36,9 @@ FILTER_QUERY_ORDINAL = {
 @pytest.mark.parametrize(
     "pheno_query,variants_count",
     [
-        ([FILTER_QUERY_CATEGORICAL], 2),
-        ([FILTER_QUERY_CONTINUOUS], 2),
-        ([FILTER_QUERY_CATEGORICAL, FILTER_QUERY_CONTINUOUS], 2),
+        ([FILTER_QUERY_CATEGORICAL], 3),
+        ([FILTER_QUERY_CONTINUOUS], 3),
+        ([FILTER_QUERY_CATEGORICAL, FILTER_QUERY_CONTINUOUS], 3),
     ],
 )
 def test_query_with_pheno_filters_work(
@@ -63,9 +63,9 @@ def test_query_with_pheno_filters_and_people_ids_filter(
     variants = quads_f1_genotype_data_group_wrapper.query_variants(
         phenoFilters=pheno_query, person_ids=["mom1"]
     )
-    variants = list(variants)
 
-    assert len(variants) == 0
+    variants = list(variants)
+    assert len(variants) == 1
 
 
 def test_query_with_bogus_pheno_filters_is_ignored(
@@ -89,7 +89,7 @@ def test_query_with_query_not_in_config_passes(
     )
     variants = list(variants)
 
-    assert len(variants) == 2
+    assert len(variants) == 3
 
 
 def test_query_with_categorical_filter_opposing_roles(fake_study_wrapper):
