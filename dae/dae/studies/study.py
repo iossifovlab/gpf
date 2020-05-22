10d243a8a47188cc9cffdcbcab553a8e631fa2ff
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2020-05-22 17:08:27 +0300

    changed handling of missmapched persons in merging families...

--- dae/dae/studies/study.py
+++ dae/dae/studies/study.py
@@ -202,8 +202,17 @@ class GenotypeDataGroup(GenotypeData):
         combined_dict = {}
         combined_dict.update(first)
         combined_dict.update(second)
+        mismatched_families = []
         for sf in same_families:
-            combined_dict[sf] = Family.merge(first[sf], second[sf])
+            try:
+                combined_dict[sf] = Family.merge(first[sf], second[sf])
+            except AssertionError:
+                import traceback
+                traceback.print_exc()
+
+                mismatched_families.append(sf)
+        assert len(mismatched_families) == 0, mismatched_families
+
         return combined_dict
 
     def _build_person_set_collection(self, person_set_collection_id):
