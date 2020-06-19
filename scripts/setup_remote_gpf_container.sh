dd7e7f8661776bcb9487ec31f63fe60659fa6fd2
IvoTod <ivo.tod.todorov@gmail.com>
2020-06-19 11:00:56 +0300

    Jenkins attempt

--- scripts/setup_remote_gpf_container.sh
+++ scripts/setup_remote_gpf_container.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+if [[ -z $WD ]]; then
+    SCRIPT_LOCATION=$(readlink -f "$0")
+    SCRIPT_DIR=$(dirname "${SCRIPT_LOCATION}")
+    export WD=$(dirname "${SCRIPT_DIR}")
+fi
+
 docker pull seqpipe/seqpipe-gpf-conda
 
-wdae_bootstrap.sh hg19 gpf_remote
+$WD/wdae/wdae/wdae_bootstrap.sh hg19 gpf_remote
 
 wget -c https://iossifovlab.com/distribution/public/studies/genotype-iossifov_2014-latest.tar.gz
 tar -zxvf genotype-iossifov_2014-latest.tar.gz
 
 (. ./gpf_remote/setenv.sh; cd ./iossifov_2014; \
-    simple_study_import.py IossifovWE2014.ped --denovo-file IossifovWE2014.tsv --id iossifov_2014)
+    $WD/dae/dae/tools/simple_study_import.py IossifovWE2014.ped --denovo-file IossifovWE2014.tsv --id iossifov_2014)
 
-LOCAL_DATA_DIR="$PWD/gpf_remote"
+LOCAL_DATA_DIR="$WD/scripts/gpf_remote"
 
 sed -i -e "s@$LOCAL_DATA_DIR@/data@g" ./gpf_remote/studies/iossifov_2014/iossifov_2014.conf
