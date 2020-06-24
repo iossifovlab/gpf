67cb8588d794f66c373707b5b6684f3b1d1cf261
IvoTod <ivo.tod.todorov@gmail.com>
2020-06-24 18:09:13 +0300

    Add local remote settings

---
+++ wdae/wdae/wdae/remote_settings.py
@@ -0,0 +1,38 @@
+# flake8: noqa
+from .default_settings import *
+
+ALLOWED_HOSTS += ["localhost"]
+
+INSTALLED_APPS += [
+    "corsheaders",
+    "django_extensions",
+]
+
+
+MIDDLEWARE += [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+]
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:8000",
+    "http://127.0.0.1:9000",
+    "http://localhost:4200",
+    "http://127.0.0.1:4200",
+    "http://10.0.1.5:4200"
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+STUDIES_EAGER_LOADING = False
+
+REMOTES = [
+    {
+        "id": "REMOTE1",
+        "host": "localhost",
+        "base_url": "api/v3",
+        "port": "21010",
+        "user": "admin@iossifovlab.com",
+        "password": "secret",
+    }
+]
