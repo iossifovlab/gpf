e9ce3d2a30208bd8e845774e3dccdef94f841f9d
Joan <joankosev@gmail.com>
2020-07-03 12:28:07 +0300

    Update email validation regex

--- gpfjs/src/app/users/users.service.ts
+++ gpfjs/src/app/users/users.service.ts
@@ -85,7 +85,11 @@ export class UsersService {
   }
 
   isEmailValid(email: string): boolean {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const re = new RegExp(
+      "[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{" +
+      "|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?" +
+      ":[a-z0-9-]*[a-z0-9])?"
+    );
     return re.test(String(email).toLowerCase());
   }
 
