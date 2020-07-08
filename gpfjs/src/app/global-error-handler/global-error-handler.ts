1111311ce5ea1fdff7c14a18c04ee08bd5819b3c
iordanivanov <iiordan.ivanov@gmail.com>
2020-07-08 16:31:23 +0300

    Fix ng-bootstrap and NgbModal problems

--- gpfjs/src/app/global-error-handler/global-error-handler.ts
+++ gpfjs/src/app/global-error-handler/global-error-handler.ts
@@ -1,20 +1,19 @@
 import { ErrorHandler, Injectable, Injector } from '@angular/core';
-// import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { GlobalErrorDisplayComponent } from 'app/global-error-display/global-error-display.component';
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
   constructor(
     private injector: Injector,
-    // private modalService: NgbModal
   ) { }
 
   handleError(error: any): void {
     console.error(error);
 
-    // this.modalService = this.injector.get(NgbModal);
-    // if (!this.modalService.hasOpenModals()) {
-    //   this.modalService.open(GlobalErrorDisplayComponent, {centered: true, size: 'sm', windowClass: 'global-error-modal'});
-    // }
+    this.modalService = this.injector.get(NgbModal);
+    if (!this.modalService.hasOpenModals()) {
+      this.modalService.open(GlobalErrorDisplayComponent, {centered: true, size: 'sm', windowClass: 'global-error-modal'});
+    }
   }
 }
