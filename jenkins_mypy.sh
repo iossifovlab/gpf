4e7b746d899b65c654ec49ac5484a56d69e360e0
iordanivanov <iiordan.ivanov@gmail.com>
2020-07-31 17:03:30 +0300

    Don't fail build if type checking finds problems

--- jenkins_mypy.sh
+++ jenkins_mypy.sh
@@ -2,10 +2,6 @@
 
 cd /code
 
-if mypy dae/dae wdae/wdae --pretty --ignore-missing-imports --warn-return-any --warn-redundant-casts --html-report mypy_report
-then
-	tar czf mypy_report.tar.gz mypy_report
-	rm -rf mypy_report
-else
-	echo "Mypy failed to check typing, exited with $?"; exit 1
-fi
+mypy dae/dae wdae/wdae --pretty --ignore-missing-imports --warn-return-any --warn-redundant-casts --html-report mypy_report
+tar czf mypy_report.tar.gz mypy_report
+rm -rf mypy_report
