e37d0577c218e55bfc42bf9ca4b3dcf176f3b12a
iordanivanov <iiordan.ivanov@gmail.com>
2020-08-24 18:08:23 +0300

    Draw denovos and subdomain frequencies

--- gpfjs/src/app/variant-frequency-view/variant-frequency-view.component.ts
+++ gpfjs/src/app/variant-frequency-view/variant-frequency-view.component.ts
@@ -16,15 +16,24 @@ export class VariantFrequencyViewComponent implements OnInit, OnChanges {
   @Input() variantsArray: GenotypePreviewVariantsArray;
   @Input() streamingFinished$: Subject<boolean>;
 
-	margin = {top: 10, right: 30, left: 30, bottom: 60}
+	margin = {top: 10, right: 30, left: 70, bottom: 60}
+  y_axes_proportions = {domain: 0.75, subdomain: 0.20, denovo: 0.05}
 
   svgElement;
-  svgWidth = 1000 - this.margin.left - this.margin.right;
+  svgWidth = 1200 - this.margin.left - this.margin.right;
   svgHeight = 400 - this.margin.top - this.margin.bottom;
+
+  a = Math.round(this.svgHeight * 0.75);
+  b = this.a + Math.round(this.svgHeight * 0.20);
+
   x;
   y;
+  y_subdomain;
+  y_denovo;
   x_axis;
   y_axis;
+  y_axis_subdomain;
+  y_axis_denovo;
 	variantsDataRepr = [];
 
   constructor() { }
@@ -42,8 +51,16 @@ export class VariantFrequencyViewComponent implements OnInit, OnChanges {
     .range([0, this.svgWidth])
 
     this.y = d3.scaleLog()
-    .domain([0.001, 1])
-    .range([this.svgHeight, 0]);
+    .domain([0.1, 1])
+    .range([this.a, 0]);
+
+    this.y_subdomain = d3.scaleLinear()
+    .domain([0, 0.1])
+    .range([this.b, this.a]);
+
+    this.y_denovo = d3.scalePoint()
+    .domain(["Denovo"])
+    .range([this.svgHeight, this.b]);
 
 		this.streamingFinished$.subscribe(() => this.drawPlot());
   }
@@ -77,15 +94,13 @@ export class VariantFrequencyViewComponent implements OnInit, OnChanges {
 	hydrateVariantsData(variantsArray) {
 		this.variantsDataRepr = [];
 		for(let v of variantsArray.genotypePreviews) {
-			if(v.get("freq.genome gnomad") !== "-") {
-				this.variantsDataRepr.push(
-					{
-						position: this.extractPosition(v.get("variant.location")),
-						frequency: v.get("freq.genome gnomad"),
-						color: this.getVariantColor(v.get("effect.worst effect"))
-					}
-				)
-			}
+      this.variantsDataRepr.push(
+        {
+          position: this.extractPosition(v.get("variant.location")),
+          frequency: v.get("freq.genome gnomad"),
+          color: this.getVariantColor(v.get("effect.worst effect"))
+        }
+      )
 		}
 	}
 
@@ -97,8 +112,23 @@ export class VariantFrequencyViewComponent implements OnInit, OnChanges {
       this.setDefaultScale();
       this.x_axis = d3.axisBottom(this.x);
       this.y_axis = d3.axisLeft(this.y);
+      this.y_axis_subdomain = d3.axisLeft(this.y_subdomain).tickValues([0, 0.05]);
+      this.y_axis_denovo = d3.axisLeft(this.y_denovo);
       this.svgElement.append('g').attr('transform', `translate(0, ${this.svgHeight})`).call(this.x_axis);
 			this.svgElement.append('g').call(this.y_axis);
+			this.svgElement.append('g').call(this.y_axis_subdomain);
+			this.svgElement.append('g').call(this.y_axis_denovo);
+
+
+      this.svgElement.append('rect')
+      .attr("x", 0)
+      .attr("y", this.b)
+      .attr("width", this.svgWidth)
+      .attr("height", this.svgHeight - this.b)
+      .style("fill", "orange")
+      .style("stroke", "black")
+      .style("opacity", 0.3);
+
 
 			this.svgElement.append('g')
 			.selectAll("dot")
@@ -106,18 +136,30 @@ export class VariantFrequencyViewComponent implements OnInit, OnChanges {
 			.enter()
 			.append("circle")
 			.attr("cx", d => { return this.x(d.position)} )
-			.attr("cy", d => { return this.y(d.frequency)} )
+			.attr("cy", d => { return d.frequency !== "-" ? d.frequency < 0.1 ? this.y_subdomain(d.frequency) : this.y(d.frequency) : this.y_denovo("Denovo")} )
 			.attr("r", 5)
 			.style("fill", d => { return d.color })
     }
 	}
 
-  setDefaultScale(): void {
-    const a = this.gene.transcripts[0].exons;
-    const b = this.gene.transcripts[1].exons;
+  setDefaultScale() {
+    let domainBeginning = this.gene.transcripts[0].exons[0].start;
+    let domainEnding = this.gene.transcripts[0].exons[this.gene.transcripts[0].exons.length - 1].stop;
 
-    const domainBeginning = Math.min(a[0].start, b[0].start);
-    const domainEnding = Math.max(a[a.length - 1].stop, b[b.length - 1].stop);
+    let transcriptStart;
+    let transcriptEnd;
+
+    for (let i = 1; i < this.gene.transcripts.length; i++) {
+      transcriptStart = this.gene.transcripts[i].exons[0].start;
+      if (transcriptStart < domainBeginning) {
+        domainBeginning = transcriptStart;
+      }
+
+      transcriptEnd = this.gene.transcripts[i].exons[this.gene.transcripts[i].exons.length - 1].stop;
+      if (transcriptEnd > domainEnding) {
+        domainEnding = transcriptEnd;
+      }
+    }
 
     this.x.domain([domainBeginning, domainEnding]);
   }
