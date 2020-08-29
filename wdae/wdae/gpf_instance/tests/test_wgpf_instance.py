e8814a8882815de232c89a4503d7fb165d871e54
IvoTod <ivo.tod.todorov@gmail.com>
2020-08-30 00:39:50 +0300

    Commit temp work

---
+++ wdae/wdae/gpf_instance/tests/test_wgpf_instance.py
@@ -0,0 +1,22 @@
+def test_make_wdae_wrapper(wgpf_instance, remote_settings):
+    pass
+
+
+def test_get_wdae_wrapper(wgpf_instance, remote_settings):
+    pass
+
+
+def test_get_genotype_data_ids(wgpf_instance, remote_settings):
+    pass
+
+
+def test_get_genotype_data(wgpf_instance, remote_settings):
+    pass
+
+
+def test_get_genotype_data_config(wgpf_instance, remote_settings):
+    pass
+
+
+def test_get_common_report(wgpf_instance, remote_settings):
+    pass
