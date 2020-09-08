ae59366f32b03ea143dae66d31b8113925666ebc
iordanivanov <iiordan.ivanov@gmail.com>
2020-09-08 17:15:13 +0300

    Some changes to make showing of allowed datasets

--- wdae/wdae/users_api/serializers.py
+++ wdae/wdae/users_api/serializers.py
@@ -33,9 +33,14 @@ class UserSerializer(serializers.ModelSerializer):
 
     hasPassword = serializers.BooleanField(source="is_active", read_only=True)
 
+    allowedDatasets = serializers.ListSerializer(
+        source="allowed_datasets",
+        child=serializers.CharField()
+    )
+
     class Meta(object):
         model = get_user_model()
-        fields = ("id", "email", "name", "hasPassword", "groups")
+        fields = ("id", "email", "name", "hasPassword", "groups", "allowedDatasets")
 
     def validate(self, data):
         unknown_keys = set(self.initial_data.keys()) - set(self.fields.keys())
