e117a69e03c812c3184b115fbaf25b6fe17ef354
Joan <joankosev@gmail.com>
2020-09-09 15:59:20 +0300

    User management - add group creation functionality

--- gpfjs/src/app/user-groups-selector/user-groups-selector.component.ts
+++ gpfjs/src/app/user-groups-selector/user-groups-selector.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ChangeDetectorRef, ElementRef } from '@angular/core';
 import { UserGroup } from '../users-groups/users-groups';
 import { IDropdownSettings } from 'ng-multiselect-dropdown';
+import { NgbDropdownMenu } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'gpf-user-groups-selector',
@@ -11,11 +12,16 @@ export class UserGroupsSelectorComponent implements OnInit {
   @Input() allInputtedGroups: UserGroup[];
   @Input() defaultGroups: string[] = [];
   @Input() _selectedGroups;
+  @Output() createGroupEvent = new EventEmitter<string>();
+  @ViewChild(NgbDropdownMenu) ngbDropdownMenu: NgbDropdownMenu;
+  @ViewChild('groupInput') groupInputRef: ElementRef;
 
   data: object;
   dropdownSettings: IDropdownSettings = {};
 
-  constructor() { }
+  constructor(
+    private changeDetectorRef: ChangeDetectorRef
+  ) { }
 
   ngOnInit() {
     this.dropdownSettings = {
@@ -25,7 +31,6 @@ export class UserGroupsSelectorComponent implements OnInit {
     };
 
     this.data = this.groupsToOptions(this.allInputtedGroups);
-
     if (this.defaultGroups.length !== 0) {
       this._selectedGroups = this.filterOutDefaultGroups(this._selectedGroups);
     }
@@ -55,7 +60,21 @@ export class UserGroupsSelectorComponent implements OnInit {
       this.defaultGroups.indexOf(group) === -1);
   }
 
-  // Returns the .text values of the selectedGroups object
+  createGroup(group: string) {
+    if (!group) {
+      return;
+    }
+
+    this.createGroupEvent.emit(group);
+    this.ngbDropdownMenu.dropdown.close();
+    this.groupInputRef.nativeElement.value = '';
+  }
+
+  focusGroupNameInput() {
+    this.changeDetectorRef.detectChanges();
+    this.groupInputRef.nativeElement.focus();
+  }
+
   get selectedGroups() {
     const groupsArray = [];
 
