1aa8c81eb216704a22f85031744af3079455edaa
iordanivanov <iiordan.ivanov@gmail.com>
2020-09-15 14:24:02 +0300

    WIP! Fixing variant reports table dimensions

--- gpfjs/src/app/variant-reports/common-reports-row/common-reports-row.component.ts
+++ gpfjs/src/app/variant-reports/common-reports-row/common-reports-row.component.ts
@@ -1,37 +1,51 @@
-import { Component, OnInit, Input, ComponentFactoryResolver, ViewChild, ComponentFactory, ViewContainerRef } from '@angular/core';
+import { Component, OnInit, AfterContentInit, Input, ComponentFactoryResolver, ViewChild, ContentChildren, ComponentFactory, ViewContainerRef, QueryList } from '@angular/core';
 import { PedigreeCounter } from '../variant-reports';
 import { CommonReportsPedigreeCellDirective } from '../common-reports-pedigree-cell.directive';
 import { CommonReportsPedigreeCellComponent } from '../common-reports-pedigree-cell/common-reports-pedigree-cell.component';
 import { PedigreeData } from 'app/genotype-preview-model/genotype-preview';
 
 @Component({
-  selector: 'gpf-common-reports-row',
+  selector: '[gpf-common-reports-row]',
   templateUrl: './common-reports-row.component.html',
   styleUrls: ['./common-reports-row.component.css']
 })
-export class CommonReportsRowComponent implements OnInit {
+export class CommonReportsRowComponent implements OnInit, AfterContentInit {
   @Input() pedigreeGroup: [PedigreeData];
 
-  @ViewChild(CommonReportsPedigreeCellDirective, {static: true}) gpfPedigreeHost: CommonReportsPedigreeCellDirective;
+  @ContentChildren(CommonReportsPedigreeCellDirective, { descendants: true }) gpfPedigreeHost: QueryList<CommonReportsPedigreeCellDirective>;
 
   private componentFactory: ComponentFactory<CommonReportsPedigreeCellComponent>;
-  private rowViewContainer: ViewContainerRef;
+  // private rowViewContainer: ViewContainerRef;
 
   constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
 
   ngOnInit(): void {
     this.componentFactory = this.componentFactoryResolver.resolveComponentFactory(CommonReportsPedigreeCellComponent);
 
-    this.rowViewContainer = this.gpfPedigreeHost.viewContainerRef;
-    for (const pedigree of this.pedigreeGroup) {
-      setTimeout(() => {
-        this.createPedigree(pedigree);
-      }, 1);
+    // this.rowViewContainer = this.gpfPedigreeHost.viewContainerRef;
+
+    for(let i = 0; i < this.pedigreeGroup.length; i++) {
+      // console.log(i);
+      // setTimeout(() => {
+      //   this.createPedigree(this.rowViewContainer, pedigree);
+      // }, 1);
     }
+
+    // for (const pedigree of this.pedigreeGroup) {
+    //   setTimeout(() => {
+    //     console.log(pedigree);
+    //     this.createPedigree(this.rowViewContainer, pedigree);
+    //   }, 1);
+    // }
   }
 
-  async createPedigree(pedigree: PedigreeData): Promise<any> {
-    const component = this.rowViewContainer.createComponent<CommonReportsPedigreeCellComponent>(this.componentFactory);
+  ngAfterContentInit() {
+    console.log(this.gpfPedigreeHost);
+    this.gpfPedigreeHost.changes.subscribe(c => console.log(this.gpfPedigreeHost.toArray()));
+  }
+
+  async createPedigree(viewContainer: ViewContainerRef, pedigree: PedigreeData): Promise<any> {
+    const component = viewContainer.createComponent<CommonReportsPedigreeCellComponent>(this.componentFactory);
     component.instance.pedigree = pedigree;
   }
 }
