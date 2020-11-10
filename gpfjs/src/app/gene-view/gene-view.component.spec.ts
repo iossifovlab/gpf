bbf982204514ea3f68a7f65ccdcdaae13d2b3048
IvoStefanov <ivostefanovtues@gmail.com>
2020-11-10 16:15:06 +0200

    Add more tests to gene-view component

--- gpfjs/src/app/gene-view/gene-view.component.spec.ts
+++ gpfjs/src/app/gene-view/gene-view.component.spec.ts
@@ -5,9 +5,11 @@ import { ConfigService } from 'app/config/config.service';
 import { DatasetsService } from 'app/datasets/datasets.service';
 import { FullscreenLoadingService } from 'app/fullscreen-loading/fullscreen-loading.service';
 import { UsersService } from 'app/users/users.service';
+import * as svgDrawing from 'app/utils/svg-drawing';
 // tslint:disable-next-line:import-blacklist
-import { Subject } from 'rxjs';
+import { Subject, Observable } from 'rxjs';
 import { Gene } from './gene';
+import { GeneViewModel } from './gene-view';
 
 import { GeneViewComponent, GeneViewZoomHistory, GeneViewScaleState } from './gene-view.component';
 
@@ -92,6 +94,7 @@ describe('GeneViewComponent', () => {
         'exons': [{'start': 23, 'stop': 33}, {'start': 34, 'stop': 44}]
       }]
   });
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [GeneViewComponent],
@@ -112,8 +115,36 @@ describe('GeneViewComponent', () => {
     fixture.detectChanges();
   });
 
+  beforeEach(() => {
+    component.gene = testGene;
+    component.geneViewModel = new GeneViewModel(component.gene, component.svgWidth);
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get current state', (done) => {
+    component.zoomHistory.resetToDefaultState(new GeneViewScaleState([1, 10], 1, 10, false));
+    component.x = {'_domain': [1, 10], domain(){return this._domain; }};
+    const resolveRegionChromosomesSpy = spyOn(component.geneViewModel.collapsedGeneViewTranscript, 'resolveRegionChromosomes')
+      .and.returnValue(['regions']);
+
+    const state = component.getState();
+    expect(resolveRegionChromosomesSpy).toHaveBeenCalled();
+    const expectedState = {
+      'affectedStatus': Array.from(component.selectedAffectedStatus),
+      'selectedEffectTypes': Array.from(component.selectedEffectTypes),
+      'zoomState': component.zoomHistory.currentState,
+      'showDenovo': component.showDenovo,
+      'showTransmitted': component.showTransmitted,
+      'regions': ['regions']
+    };
+
+    state.subscribe(result => {
+      expect(result).toEqual(expectedState);
+      done();
+    });
   });
 
   it('should enable and disable intro condensing', () => {
@@ -123,16 +154,86 @@ describe('GeneViewComponent', () => {
     expect(component.condenseIntrons).toBeFalse();
   });
 
+  it('should draw transmitted icons with correct shapes and titles', () => {
+    const drawTypes: any = ['drawStar', 'drawTriangle', 'drawCircle', 'drawDot'];
+    const iconTitles = ['LGDs', 'Missense', 'Synonymous', 'Other'];
+
+    const drawSpies = [];
+    drawTypes.forEach((drawType, index) => {
+      drawSpies[index] = spyOn(svgDrawing, drawType).and.callFake((element, x, y, color, title) => {
+        expect(element).toEqual(component.svgElement);
+        expect(title).toEqual(iconTitles[index]);
+      });
+    });
+
+    component.drawTransmittedIcons();
+
+    for (const drawSpy of drawSpies) {
+      expect(drawSpy).toHaveBeenCalled();
+    }
+  });
+
+  it('should draw denovo icons with correct shapes and titles', () => {
+    const drawTypes: any = ['drawStar', 'drawTriangle', 'drawCircle', 'drawDot'];
+    const iconTitles = ['Denovo LGDs', 'Denovo Missense', 'Denovo Synonymous', 'Denovo Other'];
+
+    const drawSurroundingSquareSpy = spyOn(svgDrawing, 'drawSurroundingSquare');
+    const drawSpies = [];
+    drawTypes.forEach((drawType, index) => {
+      drawSpies[index] = spyOn(svgDrawing, drawType).and.callFake((element, x, y, color, title) => {
+        expect(element).toEqual(component.svgElement);
+        expect(title).toEqual(iconTitles[index]);
+      });
+    });
+
+    component.drawDenovoIcons();
+
+    for (const drawSpy of drawSpies) {
+      expect(drawSpy).toHaveBeenCalled();
+    }
+    expect(drawSurroundingSquareSpy).toHaveBeenCalledTimes(4);
+  });
+
+  it('should draw effect types icons with correct shapes and titles', () => {
+    const drawTypes: any = ['drawStar', 'drawTriangle', 'drawCircle', 'drawDot'];
+    const iconTitles = ['#LGDs', '#Missense', '#Synonymous', '#Other'];
+
+    const drawSpies = [];
+    drawTypes.forEach((drawType, index) => {
+      drawSpies[index] = spyOn(svgDrawing, drawType).and.callFake((element, x, y, color, title) => {
+        expect(element).toEqual(component.svgElement);
+        expect(title).toEqual(iconTitles[index]);
+      });
+    });
+
+    component.drawEffectTypesIcons();
+
+    for (const drawSpy of drawSpies) {
+      expect(drawSpy).toHaveBeenCalled();
+    }
+  });
+
+  it('should set svg scale', () => {
+    const calculateTextFontSizeSpy = spyOn(component, 'calculateTextFontSize').and.returnValue(100);
+    component.fontSize = 0;
+    component.svgWidth = 0;
+
+    component.setSvgScale(1920);
+    expect(calculateTextFontSizeSpy).toHaveBeenCalled();
+    expect(component.fontSize).toBe(100);
+    expect(component.svgWidth).toBe(1920 - component.options.margin.left - component.options.margin.right);
+  });
+
   it('should redraw', () => {
     const drawGeneSpy = spyOn(component, 'drawGene');
     const drawPlotSpy = spyOn(component, 'drawPlot');
 
+    component.gene = undefined;
     component.redraw();
     expect(drawGeneSpy).not.toHaveBeenCalled();
     expect(drawPlotSpy).not.toHaveBeenCalled();
 
     component.gene = testGene;
-
     component.redraw();
     expect(drawGeneSpy).toHaveBeenCalled();
     expect(drawPlotSpy).toHaveBeenCalled();
@@ -145,6 +246,26 @@ describe('GeneViewComponent', () => {
     component.redrawAndUpdateTable();
     expect(redrawSpy).toHaveBeenCalled();
     expect(updateFamilyVariantsTableSpy).toHaveBeenCalled();
+  });
+
+  it('should recalculate X range', () => {
+    component.svgWidth = 0;
+    const buildNormalIntronsRangeSpy = spyOn(component.geneViewModel, 'buildNormalIntronsRange')
+      .withArgs(1, 100, 0)
+      .and.returnValue([0, 10]);
+    const buildCondensedIntronsRangeSpy = spyOn(component.geneViewModel, 'buildCondensedIntronsRange')
+      .withArgs(1, 100, 0)
+      .and.returnValue([10, 20]);
+
+    component.condenseIntrons = false;
+    let newRange = component.recalculateXRange(1, 100);
+    expect(buildNormalIntronsRangeSpy).toHaveBeenCalled();
+    expect(newRange).toEqual([0, 10]);
+
+    component.condenseIntrons = true;
+    newRange = component.recalculateXRange(1, 100);
+    expect(buildCondensedIntronsRangeSpy).toHaveBeenCalled();
+    expect(newRange).toEqual([10, 20]);
   });
 
   it('should check Show Denovo and redraw', () => {
