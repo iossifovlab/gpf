62e37a27c3edbf4cf45e1f795e46840712b8736e
IvoStefanov <ivostefanovtues@gmail.com>
2021-01-11 10:33:59 +0200

    Enable global error handler for non production environment

--- gpfjs/src/app/global-error-handler/global-error-handler.ts
+++ gpfjs/src/app/global-error-handler/global-error-handler.ts
@@ -1,6 +1,7 @@
 import { ErrorHandler, Injectable, Injector } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { GlobalErrorDisplayComponent } from 'app/global-error-display/global-error-display.component';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
@@ -13,9 +14,11 @@ export class GlobalErrorHandler implements ErrorHandler {
   handleError(error: any): void {
     console.error(error);
 
-    // this.modalService = this.injector.get(NgbModal);
-    // if (!this.modalService.hasOpenModals()) {
-    //   this.modalService.open(GlobalErrorDisplayComponent, {centered: true, size: 'sm', windowClass: 'global-error-modal'});
-    // }
+    if (!environment.production) {
+      this.modalService = this.injector.get(NgbModal);
+      if (!this.modalService.hasOpenModals()) {
+        this.modalService.open(GlobalErrorDisplayComponent, {centered: true, size: 'sm', windowClass: 'global-error-modal'});
+      }
+    }
   }
 }
