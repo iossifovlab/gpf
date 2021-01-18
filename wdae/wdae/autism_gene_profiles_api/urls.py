92cc8406b51ac721794757985db3e4623dbf47b3
IvoTod <ivo.tod.todorov@gmail.com>
2021-01-18 17:54:08 +0200

    Fix urls on agp api

--- wdae/wdae/autism_gene_profiles_api/urls.py
+++ wdae/wdae/autism_gene_profiles_api/urls.py
@@ -8,12 +8,12 @@ urlpatterns = [
         name="agp_configuration",
     ),
     url(
-        r"^/query_summary_variants/?$",
+        r"^/genes/?$",
         views.QueryProfilesView.as_view(),
         name="agp_profiles_query",
     ),
     url(
-        r"^/download_summary_variants/?$",
+        r"^/genes/(?P<gene_symbol>.+)/?$",
         views.ProfileView.as_view(),
         name="agp_profile"
     )
