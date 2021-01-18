e7acea6730e925fbcf719ba32a330a93c64dc918
joankosev <joankosev@gmail.com>
2021-01-18 12:10:51 +0200

    Start prototyping AGP table

--- gpfjs/src/app/autism-gene-profiles/autism-gene-profiles.component.ts
+++ gpfjs/src/app/autism-gene-profiles/autism-gene-profiles.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./autism-gene-profiles.component.css']
 })
 export class AutismGeneProfilesComponent implements OnInit {
+  tableHeaders: string[] = [];
+  geneLists: string[];
+  autismScores: string[];
+  protectionScores: string[];
+  affectedStatus: string[];
+  effectTypes: string[];
+
   ngOnInit(): void {
+    this.tableHeaders = ["Gene", "Gene lists", "Autism score", "Protection score", "SSC", "SPARK"];
+    this.geneLists = ["gene list 1", "gene list 2", "gene list 3", "gene list 4"];
+    this.autismScores = ["autism score 1", "autism score 2", "autism score 3", "autism score 4"];
+    this.protectionScores = ["protection score 1", "protection score 2", "protection score 3", "protection score 4"];
+    this.affectedStatus = ["affected", "unaffected"];
+    this.effectTypes = ["LGD", "MS", "SYN"];
   }
 }
