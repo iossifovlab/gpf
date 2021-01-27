62e6a5fa8cc49d9ea7b718388ca02f4c2182ce04
joankosev <joankosev@gmail.com>
2021-01-27 15:20:50 +0200

    AGP - add gene lists columns filtering button

--- gpfjs/src/app/autism-gene-profiles/autism-gene-profiles.component.ts
+++ gpfjs/src/app/autism-gene-profiles/autism-gene-profiles.component.ts
@@ -1,7 +1,9 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { Observable } from 'rxjs';
 import { AutismGeneToolConfig, AutismGeneToolGene } from './autism-gene-profile';
 import { AutismGeneProfilesService } from './autism-gene-profiles.service';
+import { IDropdownSettings } from 'ng-multiselect-dropdown';
+import { NgbDropdownMenu } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'gpf-autism-gene-profiles',
@@ -9,21 +11,35 @@ import { AutismGeneProfilesService } from './autism-gene-profiles.service';
   styleUrls: ['./autism-gene-profiles.component.css']
 })
 export class AutismGeneProfilesComponent implements OnInit {
+  @ViewChild(NgbDropdownMenu) ngbDropdownMenu: NgbDropdownMenu;
   private config$: Observable<AutismGeneToolConfig>;
   private genes$: Observable<AutismGeneToolGene[]>;
-  private shownGeneListsCount: Number;
-  private geneListsCount: Number;
-  private collapseSymbol: String = String.fromCharCode(0x022B2);
+
+  private geneLists: string[];
+  // private shownGeneListsCount: Number;
+  // private geneListsCount: Number;
+  // private collapseSymbol: String = String.fromCharCode(0x022B2);
+
+  dropdownSettings: IDropdownSettings = {};
 
   constructor(
     private autismGeneProfilesService: AutismGeneProfilesService,
   ) { }
 
   ngOnInit(): void {
+    this.dropdownSettings = {
+      idField: 'id',
+      textField: 'text',
+      allowSearchFilter: true,
+      defaultOpen: true,
+      // itemsShowLimit: 3
+    };
+
     this.config$ = this.autismGeneProfilesService.getConfig();
     this.config$.take(1).subscribe(res => {
-      this.geneListsCount = res['geneLists'].length;
-      this.shownGeneListsCount = this.geneListsCount;
+      this.geneLists = res['geneLists'];
+      // this.geneListsCount = res['geneLists'].length;
+      // this.shownGeneListsCount = this.geneListsCount;
     });
 
     this.genes$ = this.autismGeneProfilesService.getGenes();
@@ -36,17 +52,25 @@ export class AutismGeneProfilesComponent implements OnInit {
     return datasetConfig.effects.length * datasetConfig.personSets.length;
   }
 
-  getShownGeneLists(geneLists) {
-    return geneLists.slice(0, this.shownGeneListsCount);
-  }
+  // getShownGeneLists(geneLists) {
+  //   return geneLists.slice(0, this.shownGeneListsCount);
+  // }
 
-  showOrHideGeneLists() {
-    if (this.shownGeneListsCount === this.geneListsCount) {
-      this.shownGeneListsCount = 5;
-      this.collapseSymbol = String.fromCharCode(0x022B3);
-    } else {
-      this.shownGeneListsCount = this.geneListsCount;
-      this.collapseSymbol = String.fromCharCode(0x022B2);
-    }
+  // showOrHideGeneLists() {
+  //   if (this.shownGeneListsCount === this.geneListsCount) {
+  //     this.shownGeneListsCount = 5;
+  //     this.collapseSymbol = String.fromCharCode(0x022B3);
+  //   } else {
+  //     this.shownGeneListsCount = this.geneListsCount;
+  //     this.collapseSymbol = String.fromCharCode(0x022B2);
+  //   }
+  // }
+
+  sortAlphabetically(array: string[]): string[] {
+    return array.sort((a, b) => {
+      if (a < b) { return -1; }
+      if (a > b) { return 1; }
+      return 0;
+   });
   }
 }
