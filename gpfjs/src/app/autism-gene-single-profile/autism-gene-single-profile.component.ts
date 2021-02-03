db80f0957a1bba456172190f4eea0b1645c4fd29
IvoStefanov <ivostefanovtues@gmail.com>
2021-02-03 14:08:29 +0200

    Add in AGP single profiles WIP code for getting gene

--- gpfjs/src/app/autism-gene-single-profile/autism-gene-single-profile.component.ts
+++ gpfjs/src/app/autism-gene-single-profile/autism-gene-single-profile.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { AutismGeneToolGene } from 'app/autism-gene-profiles/autism-gene-profile';
+import { AutismGeneSingleProfileService } from './autism-gene-single-profile.service';
 
 @Component({
   selector: 'gpf-autism-gene-single-profile',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./autism-gene-single-profile.component.css']
 })
 export class AutismGeneSingleProfileComponent implements OnInit {
+  @Input() readonly geneSymbol: string;
+  private gene: AutismGeneToolGene;
 
-  constructor() { }
+  constructor(
+    private autismGeneSingleProfileService: AutismGeneSingleProfileService,
+  ) { }
 
   ngOnInit(): void {
+    this.autismGeneSingleProfileService.getGene(this.geneSymbol).take(1).subscribe(res => {
+      this.gene = res;
+      console.log(this.gene)
+    });
   }
 
 }
