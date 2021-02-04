1569d4f0973864eadbe7805773060d6b35cb4e17
joankosev <joankosev@gmail.com>
2021-02-04 16:03:48 +0200

    AGP - add tab navigation keybinds
    w - close current tab
    1,2,3..9 - open tab by numeric index
    0 - open last tab

--- gpfjs/src/app/autism-gene-profiles-block/autism-gene-profiles-block.component.ts
+++ gpfjs/src/app/autism-gene-profiles-block/autism-gene-profiles-block.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit, ViewChild } from '@angular/core';
+import { NgbNav } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'gpf-autism-gene-profiles-block',
@@ -6,7 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./autism-gene-profiles-block.component.css']
 })
 export class AutismGeneProfilesBlockComponent implements OnInit {
+  @ViewChild('nav') nav: NgbNav;
   private geneTabs = new Set<string>();
+
+  @HostListener('window:keydown', ['$event'])
+  keyEvent($event: KeyboardEvent) {
+    if ($event.key === 'w') {
+      this.closeCurrentTab();
+    }
+
+    if (Number($event.key) || Number($event.key) === 0) {
+      this.openTabByNumericIndex(Number($event.key));
+    }
+  }
 
   constructor() { }
 
@@ -21,5 +34,37 @@ export class AutismGeneProfilesBlockComponent implements OnInit {
     this.geneTabs.delete(toRemove);
     event.preventDefault();
     event.stopImmediatePropagation();
+  }
+
+  closeCurrentTab() {
+    if (this.nav.activeId === 'autismGenesTool') {
+      return;
+    }
+
+    const index = [...this.geneTabs].indexOf(this.nav.activeId);
+    this.geneTabs.delete(this.nav.activeId);
+
+    if (index !== 0) {
+      this.nav.select([...this.geneTabs][index - 1]);
+    } else if (this.geneTabs.size !== 0) {
+      this.nav.select([...this.geneTabs][index]);
+
+    } else {
+      this.nav.select('autismGenesTool');
+    }
+  }
+
+  openTabByNumericIndex(index: number) {
+    if (this.geneTabs.size === 0) {
+      return;
+    }
+
+    if (index === 0) {
+      this.nav.select([...this.geneTabs][this.geneTabs.size - 1]);
+    } else if (index === 1) {
+      this.nav.select('autismGenesTool');
+    } else if (index - 1 <= this.geneTabs.size) {
+      this.nav.select([...this.geneTabs][index - 2]);
+    }
   }
 }
