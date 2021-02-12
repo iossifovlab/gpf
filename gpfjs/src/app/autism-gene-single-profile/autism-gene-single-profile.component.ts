97b3c2c52d136b3f9f36bfac9dafcf78b756c766
joankosev <joankosev@gmail.com>
2021-02-12 13:28:49 +0200

    AGP - update weights to be gpf-histograms

--- gpfjs/src/app/autism-gene-single-profile/autism-gene-single-profile.component.ts
+++ gpfjs/src/app/autism-gene-single-profile/autism-gene-single-profile.component.ts
@@ -2,7 +2,8 @@ import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { AutismGeneToolConfig, AutismGeneToolGene } from 'app/autism-gene-profiles/autism-gene-profile';
 import { Observable } from 'rxjs';
 import { AutismGeneSingleProfileService } from './autism-gene-single-profile.service';
-import { GeneWeightsState } from '../gene-weights/gene-weights-store';
+import { GeneWeightsService } from '../gene-weights/gene-weights.service';
+import { GeneWeights } from 'app/gene-weights/gene-weights';
 
 @Component({
   selector: 'gpf-autism-gene-single-profile',
@@ -14,11 +15,15 @@ export class AutismGeneSingleProfileComponent implements OnInit, OnChanges {
   @Input() config: AutismGeneToolConfig;
 
   private gene$: Observable<AutismGeneToolGene>;
+  private autismScores: String[];
+  private protectionScores: String[];
+  private autismScoreGeneWeights: GeneWeights[];
+  private protectionScoreGeneWeights: GeneWeights[];
   private geneLists: string[];
-  private geneWeightsState = new GeneWeightsState();
 
   constructor(
     private autismGeneSingleProfileService: AutismGeneSingleProfileService,
+    private geneWeightsService: GeneWeightsService,
   ) { }
 
   ngOnChanges(): void {
@@ -29,6 +34,27 @@ export class AutismGeneSingleProfileComponent implements OnInit, OnChanges {
 
   ngOnInit(): void {
     this.gene$ = this.autismGeneSingleProfileService.getGene(this.geneSymbol);
+
+    this.gene$.subscribe(res => {
+      this.protectionScores = [...res['protectionScores'].keys()];
+      this.autismScores = [...res['autismScores'].keys()];
+
+      this.geneWeightsService.getGeneWeights().subscribe(
+        geneWeights => {
+          this.autismScoreGeneWeights = geneWeights.filter(
+            geneWeight => {
+              return this.autismScores.includes(geneWeight['weight']);
+            }
+          );
+
+          this.protectionScoreGeneWeights = geneWeights.filter(
+            geneWeight => {
+              return this.protectionScores.includes(geneWeight['weight']);
+            }
+          );
+        }
+      );
+    });
   }
 
   formatScoreName(score: string) {
