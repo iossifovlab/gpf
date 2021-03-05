0e733a55be6b334db41581cc4078f833c6e67a50
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2021-03-05 16:05:18 +0200

    GPF-484 work on proper calculation of family status for SV

--- dae/dae/backends/impala/tests/test_summary_stats.py
+++ dae/dae/backends/impala/tests/test_summary_stats.py
@@ -19,11 +19,11 @@ def test_summary_stats_simple(
 
 @pytest.mark.parametrize("variants", ["variants_impala", ])  # "variants_vcf"])
 @pytest.mark.parametrize("regions,inheritance,count", [
+    ([Region("1", 865581, 865581)], None, 5),
     ([Region("1", 865582, 865582)], None, 5),
-    ([Region("1", 865583, 865583)], None, 5),
-    ([Region("1", 865583, 865583)], "denovo", 1),
-    ([Region("1", 865582, 865583)], None, 10),
-    ([Region("1", 865582, 865583)], "denovo", 1),
+    ([Region("1", 865581, 865582)], None, 10),
+    ([Region("1", 865582, 865582)], "denovo", 1),
+    ([Region("1", 865581, 865582)], "denovo", 1),
 ])
 def test_summary_stats_summary(
         variants_impl, variants, regions, inheritance, count):
@@ -40,6 +40,8 @@ def test_summary_stats_summary(
     # assert len(vs) == 1
     result = 0
     for v in vs:
+        for aa in v.alt_alleles:
+            print(aa, aa.get_attribute("family_variants_count"))
         result += max([
             aa.get_attribute("family_variants_count")
             for aa in v.matched_alleles])
--- scripts/experimental_impala_queries.sql
+++ scripts/experimental_impala_queries.sql
@@ -1,3 +1,20 @@
+SELECT variants.bucket_index, variants.summary_index, allele_index, COUNT(DISTINCT variants.family_id), gpf_bit_or(pedigree.status), gpf_or(BITAND(inheritance_in_members, 4))
+FROM impala_test_db.summary_stats_variants as variants JOIN impala_test_db.summary_stats_pedigree as pedigree 
+WHERE
+  ( (`chromosome` = '1' AND ((`position` >= 865581 AND `position` <= 865581) OR (COALESCE(end_position, -1) >= 865581 AND COALESCE(end_position, -1) <= 865581) OR (865581 >= `position` AND 865581 <= COALESCE(end_position, -1)))) ) AND 
+  ( variants.allele_index > 0 ) AND variants.variant_in_members = pedigree.person_id 
+GROUP BY bucket_index, summary_index, allele_index, variant_type
+
+
+SELECT variants.bucket_index, variants.summary_index, allele_index, COUNT(DISTINCT variants.family_id), gpf_bit_or(pedigree.status), gpf_or(BITAND(inheritance_in_members, 4))
+FROM gpf_variant_db.summary_stats_variants as variants JOIN gpf_variant_db.summary_stats_pedigree as pedigree 
+WHERE
+  ( (  variants.effect_gene_symbols in (  'SAMD11'  )  ) ) AND ( (`chromosome` = '1' AND ((`position` >= 841121 AND `position` <= 899961) OR (COALESCE(end_position, -1) >= 841121 AND COALESCE(end_position, -1) <= 899961) OR (841121 >= `position` AND 899961 <= COALESCE(end_position, -1)))) ) AND 
+  ( (  variants.effect_types in (  'frame-shift' , 'nonsense' , 'splice-site' , 'no-frame-shift-newStop' , 'nonsense' , 'frame-shift' , 'splice-site' , 'no-frame-shift-newStop' , 'missense' , 'synonymous' , 'noStart' , 'noEnd' , 'no-frame-shift' , 'CDS' , 'CNV+' , 'CNV-'  )  ) ) AND 
+  ( BITAND(8, variants.inheritance_in_members) = 0 AND BITAND(32, variants.inheritance_in_members) = 0 ) AND ( BITAND(150, variants.inheritance_in_members) != 0 ) AND ( variants.allele_index > 0 ) AND 
+  variants.variant_in_members = pedigree.person_id 
+GROUP BY bucket_index, summary_index, allele_index, variant_type, transmission_type
+
 SELECT variants.bucket_index, variants.summary_index, COUNT(DISTINCT variants.family_id), gpf_bit_or(pedigree.status), gpf_or(BITAND(inheritance_in_members, 4))
 FROM gpf_variant_db.summary_stats_variants as variants JOIN gpf_variant_db.summary_stats_pedigree as pedigree 
 WHERE
