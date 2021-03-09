203c35351f19f56b69584fee0e5e499efd7b17e1
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2021-03-09 18:39:58 +0200

    GPF-516 fix column ordering in common reports pedigree data

--- dae/dae/common_reports/common_report_facade.py
+++ dae/dae/common_reports/common_report_facade.py
@@ -100,8 +100,8 @@ class CommonReportFacade(object):
                 row = [
                     p.family_id,
                     p.person_id,
-                    p.mom_id if p.mom_id else "0",
                     p.dad_id if p.dad_id else "0",
+                    p.mom_id if p.mom_id else "0",
                     p.sex,
                     p.status,
                     p.role,
