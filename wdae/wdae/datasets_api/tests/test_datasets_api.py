15dde0279e44763ebf3c95c0497f76e2943a17c9
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2021-03-25 18:52:03 +0200

    trying to fix Jenkins build

--- wdae/wdae/datasets_api/tests/test_datasets_api.py
+++ wdae/wdae/datasets_api/tests/test_datasets_api.py
@@ -9,7 +9,7 @@ def test_datasets_api_get_all(admin_client):
 
     assert response
     assert response.status_code == 200
-    assert len(response.data["data"]) == 41
+    assert len(response.data["data"]) == 42
 
 
 def test_datasets_api_get_one(admin_client):
