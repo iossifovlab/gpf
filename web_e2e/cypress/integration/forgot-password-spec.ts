c04af5aad287b2b34648edbd44cbe379c527f22e
joankosev <joankosev@gmail.com>
2021-03-31 17:26:26 +0300

    GPF-589 add forgot-password tests

---
+++ web_e2e/cypress/integration/forgot-password-spec.ts
@@ -0,0 +1,16 @@
+import { ForgotPasswordPage } from "cypress/elements/forgot-password-page";
+import { UsersPage } from '../elements/users-page';
+
+describe('Forgotten password tests', () => {
+  const forgotPasswordPage = new ForgotPasswordPage();
+  const usersPage = new UsersPage();
+
+  it('should open forgotten password window', () => {
+    forgotPasswordPage.navigateToHome();
+    usersPage.loginDropdownToggleButton.click();
+    forgotPasswordPage.window.should('not.exist');
+
+    usersPage.forgottenPasswordButton.click();
+    forgotPasswordPage.window.should('be.visible');
+  });
+});
