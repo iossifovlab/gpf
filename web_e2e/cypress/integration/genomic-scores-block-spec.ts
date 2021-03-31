23ab51741a87da87c83b64c6508b24bd48994076
joankosev <joankosev@gmail.com>
2021-03-31 17:36:50 +0300

    GPF-589 add genomic-scores-block tests

---
+++ web_e2e/cypress/integration/genomic-scores-block-spec.ts
@@ -0,0 +1,32 @@
+import { GenomicScoresBlockPage } from "cypress/elements/genomic-scores-block-page";
+import { datasetIds, toolPageNames } from "cypress/elements/utils";
+
+describe('Genomic scores panel tests', () => {
+  const genomicScoresBlockPage = new GenomicScoresBlockPage();
+
+  before(() => {
+    genomicScoresBlockPage.navigateToHome();
+    genomicScoresBlockPage.loginAdmin();
+  });
+
+  after(() => {
+    genomicScoresBlockPage.logout();
+  });
+
+  beforeEach(() => {
+    genomicScoresBlockPage.navigateToHome();
+  });
+
+  it('should display genomic scores panel after \'add filter\' button click ' +
+     'and remove it after \'remove filter\' button click', () => {
+    genomicScoresBlockPage.navigateToDatasetPage(datasetIds.compAll, toolPageNames.genotypeBrowser);
+    genomicScoresBlockPage.panel.should('not.exist');
+
+    genomicScoresBlockPage.addFilterButton.click();
+    genomicScoresBlockPage.panel.should('be.visible');
+
+    genomicScoresBlockPage.removeFilterButton.click();
+    genomicScoresBlockPage.panel.should('not.exist');
+  });
+});
+
