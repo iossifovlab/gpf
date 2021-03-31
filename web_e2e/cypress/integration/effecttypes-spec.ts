0c8879704bce06bdd3c12a82c38126725e2ff661
joankosev <joankosev@gmail.com>
2021-03-31 16:57:28 +0300

    GPF-589 add effecttypes tests

---
+++ web_e2e/cypress/integration/effecttypes-spec.ts
@@ -0,0 +1,47 @@
+import { EffecttypesPage } from 'cypress/elements/effectypes-page';
+import { ErrorsAlertPage } from 'cypress/elements/errors-alert-page';
+import { GenotypeBlockPage } from 'cypress/elements/genotype-block-page';
+import { datasetIds, toolPageNames } from 'cypress/elements/utils';
+
+describe('Effect types tests', () => {
+  const effecttypesPage = new EffecttypesPage();
+  const genotypeBlockPage = new GenotypeBlockPage();
+
+  before(() => {
+    Cypress.Cookies.preserveOnce('sessionid');
+    effecttypesPage.navigateToHome();
+    effecttypesPage.loginAdmin();
+  });
+
+  after(() => {
+    effecttypesPage.logout();
+  });
+
+  beforeEach(() => {
+    effecttypesPage.navigateToHome();
+  });
+
+  it('should display error alert when none of the checkboxes are selected', () => {
+    const errorsAlertPage = new ErrorsAlertPage();
+
+    effecttypesPage.navigateToDatasetPage(datasetIds.compAll, toolPageNames.genotypeBrowser);
+    genotypeBlockPage.findButtonInComponentContainingText('gpf-effecttypes', 'None').click();
+    errorsAlertPage.findAlertWindowInComponent('gpf-effecttypes').should('be.visible');
+
+    genotypeBlockPage.findButtonInComponentContainingText('gpf-effecttypes', 'All').click();
+    errorsAlertPage.findAlertWindowInComponent('gpf-effecttypes').should('not.exist');
+  });
+
+  it('should check/uncheck effect types checkboxes using \'All\' and \'None\' buttons', () => {
+    effecttypesPage.navigateToDatasetPage(datasetIds.compAll, toolPageNames.genotypeBrowser);
+    genotypeBlockPage.findButtonInComponentContainingText('gpf-effecttypes', 'None').click();
+    genotypeBlockPage.findAllCheckboxesInComponent('gpf-effecttypes').each((element) => {
+      cy.wrap(element).should('not.be.checked');
+    });
+
+    genotypeBlockPage.findButtonInComponentContainingText('gpf-effecttypes', 'All').click();
+    genotypeBlockPage.findAllCheckboxesInComponent('gpf-effecttypes').each((element) => {
+      cy.wrap(element).should('be.checked');
+    });
+  });
+});
