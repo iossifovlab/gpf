70ca806ba4d47ad9646d443f7465214a9958512f
joankosev <joankosev@gmail.com>
2021-03-31 17:31:48 +0300

    GPF-589 add gender tests

---
+++ web_e2e/cypress/integration/gender-spec.ts
@@ -0,0 +1,52 @@
+import { ErrorsAlertPage } from "cypress/elements/errors-alert-page";
+import { GenderPage } from "cypress/elements/gender-page";
+import { GenotypeBlockPage } from "cypress/elements/genotype-block-page";
+import { datasetIds, toolPageNames } from "cypress/elements/utils";
+
+describe('Gender tests', () => {
+  const genderPage = new GenderPage();
+  const genotypeBlockPage = new GenotypeBlockPage();
+
+  before(() => {
+    genderPage.navigateToHome();
+    genderPage.loginAdmin();
+  });
+
+  after(() => {
+    genderPage.logout();
+  });
+
+  beforeEach(() => {
+    genderPage.navigateToHome();
+    Cypress.Cookies.preserveOnce('sessionid');
+  });
+
+  // review
+  // see if u can add error alert window as a var before everything
+  it('should display error alert when none of the checkboxes are selected', () => {
+    const errorsAlertPage = new ErrorsAlertPage();
+
+    genderPage.navigateToDatasetPage(datasetIds.compAll, toolPageNames.genotypeBrowser);
+    errorsAlertPage.findAlertWindowInComponent('gpf-gender').should('not.exist');
+
+    genotypeBlockPage.findButtonInComponentContainingText('gpf-gender', 'None').click();
+    errorsAlertPage.findAlertWindowInComponent('gpf-gender').should('be.visible');
+
+    genotypeBlockPage.findButtonInComponentContainingText('gpf-gender', 'All').click();
+    errorsAlertPage.findAlertWindowInComponent('gpf-gender').should('not.exist');
+  });
+
+  it('should check/uncheck child gender checkboxes using \'All\' and \'None\' buttons', () => {
+    genderPage.navigateToDatasetPage(datasetIds.compAll, toolPageNames.genotypeBrowser);
+
+    genotypeBlockPage.findButtonInComponentContainingText('gpf-gender', 'None').click();
+    genotypeBlockPage.findAllCheckboxesInComponent('gpf-gender').each((element) => {
+      cy.wrap(element).should('not.be.checked');
+    });
+
+    genotypeBlockPage.findButtonInComponentContainingText('gpf-gender', 'All').click();
+    genotypeBlockPage.findAllCheckboxesInComponent('gpf-gender').each((element) => {
+      cy.wrap(element).should('be.checked');
+    });
+  });
+});
