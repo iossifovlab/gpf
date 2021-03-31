12d8947f8488a2ac5a8b77f2f87618cdb7a92e45
joankosev <joankosev@gmail.com>
2021-03-31 17:58:32 +0300

    GPF-589 add registration tests

---
+++ web_e2e/cypress/integration/registration-spec.ts
@@ -0,0 +1,16 @@
+import { RegistrationPage } from "cypress/elements/registration-page";
+import { UsersPage } from "cypress/elements/users-page";
+
+describe('Register tests', () => {
+  const registrationPage = new RegistrationPage();
+  const usersPage = new UsersPage();
+
+  it('should open register window', () => {
+    registrationPage.navigateToHome();
+    usersPage.loginDropdownToggleButton.click();
+    registrationPage.window.should('not.exist');
+
+    usersPage.registerButton.click();
+    registrationPage.window.should('be.visible');
+  });
+});
