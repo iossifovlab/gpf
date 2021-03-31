b1fdfae250a8e59479affe5a8e7ff920833339b3
joankosev <joankosev@gmail.com>
2021-03-31 18:11:12 +0300

    GPF-589 add varianttypes tests

---
+++ web_e2e/cypress/integration/varianttypes-spec.ts
@@ -0,0 +1,48 @@
+import { ErrorsAlertPage } from 'cypress/elements/errors-alert-page';
+import { GenotypeBlockPage } from 'cypress/elements/genotype-block-page';
+import { datasetIds, toolPageNames } from 'cypress/elements/utils';
+import { VarianttypesPage } from 'cypress/elements/varianttypes-page';
+
+describe('Variant types tests', () => {
+  const varianttypesPage = new VarianttypesPage();
+  const genotypeBlockPage = new GenotypeBlockPage();
+
+  before(() => {
+    varianttypesPage.navigateToHome();
+    varianttypesPage.loginAdmin();
+  });
+
+  after(() => {
+    varianttypesPage.logout();
+  });
+
+  beforeEach(() => {
+    varianttypesPage.navigateToHome();
+    Cypress.Cookies.preserveOnce('sessionid');
+  });
+
+  it('should display error alert when none of the checkboxes are selected', () => {
+    const errorsAlertPage = new ErrorsAlertPage();
+    varianttypesPage.navigateToDatasetPage(datasetIds.compAll, toolPageNames.genotypeBrowser);
+    errorsAlertPage.findAlertWindowInComponent('gpf-varianttypes').should('not.exist');
+
+    genotypeBlockPage.findButtonInComponentContainingText('gpf-varianttypes', 'None').click();
+    errorsAlertPage.findAlertWindowInComponent('gpf-varianttypes').should('be.visible');
+
+    genotypeBlockPage.findButtonInComponentContainingText('gpf-varianttypes', 'All').click();
+    errorsAlertPage.findAlertWindowInComponent('gpf-varianttypes').should('not.exist');
+  });
+
+  it('should check/uncheck variant types checkboxes using \'All\' and \'None\' buttons', () => {
+    varianttypesPage.navigateToDatasetPage(datasetIds.compAll, toolPageNames.genotypeBrowser);
+    genotypeBlockPage.findButtonInComponentContainingText('gpf-varianttypes', 'None').click();
+    genotypeBlockPage.findAllCheckboxesInComponent('gpf-varianttypes').each((element) => {
+      cy.wrap(element).should('not.be.checked');
+    });
+
+    genotypeBlockPage.findButtonInComponentContainingText('gpf-varianttypes', 'All').click();
+    genotypeBlockPage.findAllCheckboxesInComponent('gpf-varianttypes').each((element) => {
+      cy.wrap(element).should('be.checked');
+    });
+  });
+});
