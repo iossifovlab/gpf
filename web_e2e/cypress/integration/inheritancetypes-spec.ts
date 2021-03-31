b15989b5c792ee17b85385427e7e9803800d89b1
joankosev <joankosev@gmail.com>
2021-03-31 17:41:49 +0300

    GPF-589 add inheritancetypes tests

---
+++ web_e2e/cypress/integration/inheritancetypes-spec.ts
@@ -0,0 +1,51 @@
+import { ErrorsAlertPage } from "cypress/elements/errors-alert-page";
+import { GenotypeBlockPage } from "cypress/elements/genotype-block-page";
+import { InheritancetypesPage } from "cypress/elements/inheritancetypes-page";
+import { datasetIds, toolPageNames } from "cypress/elements/utils";
+
+describe('Inheritance tests', () => {
+  const inheritancetypesPage = new InheritancetypesPage();
+  const genotypeBlockPage = new GenotypeBlockPage();
+
+  before(() => {
+    inheritancetypesPage.navigateToHome();
+    inheritancetypesPage.loginAdmin();
+  });
+
+  after(() => {
+    inheritancetypesPage.logout();
+  });
+
+  beforeEach(() => {
+    inheritancetypesPage.navigateToHome();
+    Cypress.Cookies.preserveOnce('sessionid');
+  });
+
+  // review
+  // add error alert to var?
+  it('should display error alert when none of the checkboxes are selected', () => {
+    const errorsAlertPage = new ErrorsAlertPage();
+
+    inheritancetypesPage.navigateToDatasetPage(datasetIds.compAll, toolPageNames.genotypeBrowser);
+    errorsAlertPage.findAlertWindowInComponent('gpf-inheritancetypes').should('not.exist');
+
+    genotypeBlockPage.findButtonInComponentContainingText('gpf-inheritancetypes', 'None').click();
+    errorsAlertPage.findAlertWindowInComponent('gpf-inheritancetypes').should('be.visible');
+
+    genotypeBlockPage.findButtonInComponentContainingText('gpf-inheritancetypes', 'All').click();
+    errorsAlertPage.findAlertWindowInComponent('gpf-inheritancetypes').should('not.exist');
+  });
+
+  it('should check/uncheck effect types checkboxes using \'All\' and \'None\' buttons', () => {
+    inheritancetypesPage.navigateToDatasetPage(datasetIds.compAll, toolPageNames.genotypeBrowser);
+    genotypeBlockPage.findButtonInComponentContainingText('gpf-inheritancetypes', 'None').click();
+    genotypeBlockPage.findAllCheckboxesInComponent('gpf-inheritancetypes').each((element) => {
+      cy.wrap(element).should('not.be.checked');
+    });
+
+    genotypeBlockPage.findButtonInComponentContainingText('gpf-inheritancetypes', 'All').click();
+    genotypeBlockPage.findAllCheckboxesInComponent('gpf-inheritancetypes').each((element) => {
+      cy.wrap(element).should('be.checked');
+    });
+  });
+});
