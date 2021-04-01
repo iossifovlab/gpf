ada12a044f58b05e0051ae213d16696e4e315de2
joankosev <joankosev@gmail.com>
2021-04-01 12:01:11 +0300

    GPF-589 add share-query tests

---
+++ web_e2e/cypress/integration/share-query-spec.ts
@@ -0,0 +1,60 @@
+import { DatasetsPage } from "cypress/elements/datasets-page";
+import { GenotypeBlockPage } from "cypress/elements/genotype-block-page";
+import { ShareQueryPage } from "cypress/elements/share-query-page";
+import { datasetIds, toolPageNames } from "cypress/elements/utils";
+
+describe('Share query tests', () => {
+  const shareQueryPage = new ShareQueryPage();
+
+  before(() => {
+    shareQueryPage.navigateToHome();
+    shareQueryPage.loginAdmin();
+  });
+
+  after(() => {
+    shareQueryPage.logout();
+  });
+
+  beforeEach(() => {
+    shareQueryPage.navigateToHome();
+    Cypress.Cookies.preserveOnce('sessionid');
+  });
+
+  it('should open share query dropdown menu after share query button click', () => {
+    shareQueryPage.navigateToDatasetPage(datasetIds.compAll, toolPageNames.genotypeBrowser);
+    shareQueryPage.dropdownMenu.invoke('attr', 'class').should('contain', 'dropdown-menu');
+
+    shareQueryPage.button.click();
+    shareQueryPage.dropdownMenu.invoke('attr', 'class').should('contain', 'dropdown-menu');
+  });
+
+  it('should share a query, load it and open all tools tabs', () => {
+    const datasetsPage = new DatasetsPage();
+
+    shareQueryPage.navigateToDatasetPage(datasetIds.compAll, toolPageNames.genotypeBrowser);
+    shareQueryPage.button.click();
+    shareQueryPage.input.invoke('val').then((url) => {
+      cy.visit(String(url));
+      datasetsPage.datasetStatisticsButton.click();
+      datasetsPage.genotypeBrowserButton.click();
+      datasetsPage.phenotypeBrowserButton.click();
+      datasetsPage.phenotypeToolButton.click();
+    })
+  });
+
+  it('should navigate to genotype browser, check all effect types checkboxes, generate a share query, ' +
+     'load it and validate that all effect types checkboxes are checked', () => {
+    const genotypeBlockPage = new GenotypeBlockPage();
+
+    shareQueryPage.navigateToDatasetPage(datasetIds.compAll, toolPageNames.genotypeBrowser);
+    genotypeBlockPage.findButtonInComponentContainingText('gpf-effecttypes', 'All').click();
+
+    shareQueryPage.button.click();
+    shareQueryPage.input.invoke('val').then((url) => {
+      cy.visit(String(url));
+      genotypeBlockPage.findAllCheckboxesInComponent('gpf-effecttypes').each((element) => {
+        cy.wrap(element).should('be.checked');
+      });
+    })
+  });
+});
