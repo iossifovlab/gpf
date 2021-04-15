001137a6c25429dc63c5f80e99368dbd9d0ff889
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2021-04-15 18:46:07 +0300

    GPF-666 fix federation support for eager loading

--- wdae/wdae/gpf_instance/gpf_instance.py
+++ wdae/wdae/gpf_instance/gpf_instance.py
@@ -32,10 +32,11 @@ _gpf_instance_lock = Lock()
 class WGPFInstance(GPFInstance):
     def __init__(self, *args, **kwargs):
         self._remote_clients = None
-        super(WGPFInstance, self).__init__(*args, **kwargs)
         self._remote_study_clients = dict()
         self._remote_study_ids = dict()
         self._study_wrappers = dict()
+
+        super(WGPFInstance, self).__init__(*args, **kwargs)
         self._load_remotes()
 
     def _load_remotes(self):
