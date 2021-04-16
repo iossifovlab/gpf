54a467ebf0fe8db224ecae0210148b23a679ef21
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2021-04-16 10:36:01 +0300

    GPF-666 filter out remote studies without permissions

--- wdae/wdae/gpf_instance/gpf_instance.py
+++ wdae/wdae/gpf_instance/gpf_instance.py
@@ -89,6 +89,14 @@ class WGPFInstance(GPFInstance):
     def _fetch_remote_studies(self, rest_client):
         studies = rest_client.get_datasets()
         for study in studies["data"]:
+            logger.debug(
+                f"study {study['id']} access rights are: "
+                f"{study.get('access_rights', False)}")
+            if not study.get("access_rights", None):
+                logger.info(
+                    f"skipping creation of remote study {study['id']}")
+                continue
+
             logger.info(f"creating remote genotype data: {study['id']}")
             study_wrapper = RemoteStudyWrapper(study["id"], rest_client)
             study_id = study_wrapper.study_id
