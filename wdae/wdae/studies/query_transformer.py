2c109f99111c45c1ccad7e6e0a2735773486867c
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2021-04-22 11:38:04 +0300

    GPF-683 explicitly filter out possible denovos and omissions

--- wdae/wdae/studies/query_transformer.py
+++ wdae/wdae/studies/query_transformer.py
@@ -310,10 +310,10 @@ class QueryTransformer:
     # TMM_ALL
     def transform_kwargs(self, **kwargs):
         logger.debug(f"kwargs in study wrapper: {kwargs}")
-        # self._add_inheritance_to_query(
-        #     "not possible_denovo and not possible_omission",
-        #     kwargs
-        # )
+        self._add_inheritance_to_query(
+            "not possible_denovo and not possible_omission",
+            kwargs
+        )
 
         kwargs = self._add_people_with_people_group(kwargs)
 
