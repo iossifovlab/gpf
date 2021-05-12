30d2cc7a2988ff029504b7a9d8463f2fd2ec42d2
Hristo Stefanov <hrstefanov@gmail.com>
2021-05-12 15:16:05 +0300

    build.sh: formatting

--- gpfjs/build.sh
+++ gpfjs/build.sh
@@ -17,33 +17,33 @@ include libdefer.sh
 
 function main() {
   libmain_init
-  libmain_init_build_env seqpipe-containers
+  libmain_init_build_env seqpipe-containers data-hg38
   libmain_save_build_env_on_exit gpfjs
 
-  libbuild_init gpfjs
+  libbuild_init gpfjs registry.seqpipe.org
 
   local image_ref
   image_ref="$(e docker_img_seqpipe_node_base)"
 
-
   local stage="$1"
   build_run_init "container" "$image_ref" "$stage"
-
-  defer_ret build_run_reset ctx
+  defer_ret build_run_reset
 
   build_stage "Clean and fetch fresh dependencies"
   build_run rm -rf node_modules package-lock.json
   build_run npm install
+
   build_stage "Lint"
-  build_run ng lint --format checkstyle > ts-lint-report.xml || echo "tslint exited with $?"
+  build_run ng lint --format checkstyle >ts-lint-report.xml || echo "tslint exited with $?"
   build_run sed -i '$ d' ts-lint-report.xml
+
   build_stage "Tests"
   build_run ng test -- --no-watch --no-progress --code-coverage --browsers=ChromeHeadlessCI || true
+
   build_stage "Clean and package"
   build_run rm -rf dist/
   build_run ng build --prod --aot --configuration 'default' --base-href '/gpf_prefix/' --deploy-url '/gpf_prefix/'
-  build_stage "ppindex"
-  build_run echo ppindex.py
+  build_run echo ppindex.py ??
 
 }
 
