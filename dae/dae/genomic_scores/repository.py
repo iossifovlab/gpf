b365ca12a8b3ee71cf2348998c8056a731777ed3
iordanivanov <iiordan.ivanov@gmail.com>
2021-06-02 13:34:12 +0300

    GPF-756 Implement HTTP GSD traversal

--- dae/dae/genomic_scores/repository.py
+++ dae/dae/genomic_scores/repository.py
@@ -1,6 +1,11 @@
 import os
+import yaml
 from glob import glob
 from typing import List
+
+import urllib.request
+from bs4 import BeautifulSoup
+
 from dae.genomic_scores.scores import GenomicScore, GenomicScoreGroup
 from dae.configuration.gpf_config_parser import GPFConfigParser
 from dae.configuration.schemas.genomic_score_database import \
@@ -40,7 +45,7 @@ class FilesystemGenomicScoreRepository(BaseGenomicScoreRepository):
     """
 
     def __init__(self, path: str):
-        super().__init__(path.split('/')[-1])
+        super().__init__(path.rstrip('/').split('/')[-1])
         self.path = path
         self._traverse_gsd_dir(self.path)
 
@@ -69,10 +74,59 @@ class HTTPGenomicScoreRepository(BaseGenomicScoreRepository):
     A genomic score repository on a local or remote filesystem.
     """
 
+    IGNORE_LIST = [
+        "Name",
+        "Last modified",
+        "Size",
+        "Description",
+        "Parent Directory",
+    ]
+
     def __init__(self, url: str):
-        super().__init__()
+        super().__init__(url.rstrip('/').split('/')[-1])
         self.url = url
-        # TODO Implement discovery
+        self.top_level_group.children = self._collect_gsd_children(self.url)
+
+    @staticmethod
+    def _collect_gsd_children(root_url: str):
+        opener = urllib.request.FancyURLopener({})
+        content = opener.open(root_url).read()
+        soup = BeautifulSoup(content, 'html.parser')
+
+        links = list(filter(
+            lambda el: el.text not in HTTPGenomicScoreRepository.IGNORE_LIST,
+            soup.find_all("a")
+        ))
+
+        conf_file_tag = next(
+            filter(lambda link: ".yaml" in link.text, links), None
+        )
+
+        children = dict()
+        if conf_file_tag is not None:
+            conf_url = conf_file_tag.attrs["href"]
+            raw_conf = yaml.safe_load(opener.open(
+                os.path.join(root_url, conf_url)
+            ).read().decode("utf-8"))
+            # FIXME temporarily disabled until remote has proper confs
+            # children[root_url] = GenomicScore(
+            #     GPFConfigParser.process_config(raw_conf, genomic_score_schema)
+            # )
+            children[root_url] = raw_conf
+        else:
+            subdirectories = filter(
+                lambda entry: entry[-1] == '/',
+                map(lambda link: link.attrs["href"], links)
+            )
+            for subdir in subdirectories:
+                # FIXME genomic score group's id is saved with a trailing '/'
+                full_url = os.path.join(root_url, subdir)
+                genomic_score_group = GenomicScoreGroup(subdir)
+                genomic_score_group.children = \
+                    HTTPGenomicScoreRepository._collect_gsd_children(full_url)
+                children[subdir] = genomic_score_group
+
+        return children
 
     def cache(self, genomic_score_id: str):
         raise NotImplementedError()
