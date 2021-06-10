01e0ac61657020f48c4cec892513a73aac4115df
Hristo Stefanov <hrstefanov@yahoo.co.uk>
2021-06-10 16:32:44 +0300

    build.sh: disable default mounts for impala container

--- build.sh
+++ build.sh
@@ -293,7 +293,7 @@ EOT'
       # setup impala
       {
         local -A ctx_impala
-        build_run_ctx_init ctx:ctx_impala "persistent" "container" "seqpipe/seqpipe-docker-impala:latest" "cmd-from-image" --hostname impala --network "${ctx_network["network_id"]}"
+        build_run_ctx_init ctx:ctx_impala "persistent" "container" "seqpipe/seqpipe-docker-impala:latest" "cmd-from-image" "no-def-mounts" --hostname impala --network "${ctx_network["network_id"]}"
         defer_ret build_run_ctx_reset ctx:ctx_impala
 
         build_run_container ctx:ctx_impala /wd/scripts/wait-for-it.sh -h localhost -p 21050 -t 300
