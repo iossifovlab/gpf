f2e2b7a6a49f453a79815b81f53803011c9e7650
Hristo Stefanov <hrstefanov@yahoo.co.uk>
2021-06-13 17:09:57 +0300

    build.sh: updates for latest version of build-scripts

--- gpfjs/build.sh
+++ gpfjs/build.sh
@@ -16,34 +16,44 @@ include libbuild.sh
 include libdefer.sh
 
 function main() {
-  libmain_init
+  local stage="${1-all}"
+
+  libmain_init gpfjs gpfjs
   libmain_init_build_env seqpipe-containers
-  libmain_save_build_env_on_exit gpfjs
+  libmain_save_build_env_on_exit
+  libbuild_init "$stage" registry.seqpipe.org
 
-  libbuild_init gpfjs registry.seqpipe.org
+  defer_ret build_run_ctx_reset_all_persistent
+  defer_err build_run_ctx_reset_all_persistent
+  defer_err build_run_ctx_reset
 
-  local image_ref
-  image_ref="$(e docker_img_seqpipe_node_base)"
-
-  local stage="$1"
-  build_run_init "container" "$image_ref" "$stage"
+  local node_base_image_ref
+  node_base_image_ref="$(e docker_img_seqpipe_node_base)"
+  build_run_init "container" "$node_base_image_ref"
   defer_ret build_run_reset
 
   build_stage "Clean and fetch fresh dependencies"
-  build_run rm -rf node_modules package-lock.json
-  build_run npm install
+  {
+    build_run rm -rf node_modules package-lock.json
+    build_run npm install
+  }
 
   build_stage "Lint"
-  build_run ng lint --format checkstyle >ts-lint-report.xml || echo "tslint exited with $?"
-  build_run sed -i '$ d' ts-lint-report.xml
+  {
+    build_run ng lint --format checkstyle >ts-lint-report.xml || echo "tslint exited with $?"
+    build_run sed -i '$ d' ts-lint-report.xml
+  }
 
   build_stage "Tests"
-  build_run ng test -- --no-watch --no-progress --code-coverage --browsers=ChromeHeadlessCI || true
+  {
+    build_run ng test -- --no-watch --no-progress --code-coverage --browsers=ChromeHeadlessCI || true
+  }
 
   build_stage "Clean and package"
-  build_run rm -rf dist/
-  build_run ng build --prod --aot --configuration 'default' --base-href '/gpf_prefix/' --deploy-url '/gpf_prefix/'
-  build_run echo ppindex.py ??
+  {
+    build_run rm -rf dist/
+    build_run ng build --prod --aot --configuration 'default' --base-href '/gpf_prefix/' --deploy-url '/gpf_prefix/'
+  }
 
 }
 
