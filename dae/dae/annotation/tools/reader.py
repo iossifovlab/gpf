8e24bb39a84f9e15db9d10c24de0bebcf9d2e0b2
IvoTod <ivo.tod.todorov@gmail.com>
2021-06-15 12:05:52 +0300

    GPF-807 Change TabixReader to ScoreFile

--- dae/dae/annotation/tools/reader.py
+++ dae/dae/annotation/tools/reader.py
@@ -2,10 +2,15 @@ import sys
 import os
 import logging
 import gzip
+
 import pysam
+import numpy as np
+import pandas as pd
+from collections import defaultdict
 
 from dae.annotation.tools.schema import Schema
-from dae.annotation.tools.utils import handle_header, handle_chrom_prefix
+from dae.annotation.tools.utils import handle_header, handle_chrom_prefix, \
+    AnnotatorFactory
 
 logger = logging.getLogger(__name__)
 
@@ -122,35 +127,47 @@ class TSVReader(FileReader):
             line = self.line_read()
 
 
-class TabixReader(FileReader):
+class ScoreFile:
 
     LONG_JUMP_THRESHOLD = 5000
     ACCESS_SWITCH_THRESHOLD = 1500
 
-    def __init__(
-        self, filename, tabix_filename,
-        id_names=None, id_indexes=None,
-        col_order=None, separator=None,
-        region=None
-    ):
+    def __init__(self, config):
 
-        assert id_names is not None or id_indexes is not None
-        super.__init__()
-
-        self.filename = filename
-        self.tabix_filename = tabix_filename
-        self.col_order = col_order
-        self.separator = "\t" if separator is None else separator
-        self.region = region
+        self.config = config
+        self.filename = config.filename
+        self.tabix_filename = config.index_file.filename
+        self.separator = "\t" if config.separator is None else config.separator
         self.buffer = []
-        self.id_names = id_names
-        self.id_indexes = id_indexes
         self.last_pos = 0
 
         assert os.path.exists(self.filename)
         assert os.path.exists(self.tabix_filename)
         assert is_gzip(self.filename)
         assert is_tabix(self.tabix_filename)
+
+        required_columns = AnnotatorFactory.name_to_class(
+            self.config.score_type
+        ).required_columns
+
+        file_columns = {rc: self.config[rc] for rc in required_columns}
+        file_columns = file_columns.update({
+            sc.id: sc for sc in self.config.scores
+        })
+
+        self._setup()
+
+        if self.config.has_header:
+            header = self.header
+            col_indexes = dict()
+            for col_name, desc in file_columns:
+                col_indexes[col_name] = header.index(desc.name)
+        else:
+            col_indexes = {
+                k: file_columns[k].index for k in file_columns.keys()
+            }
+
+        self.col_indexes = col_indexes
 
     @property
     def _buffer_chrom(self):
@@ -173,22 +190,37 @@ class TabixReader(FileReader):
 
         return self.buffer[0][self._pos_end_idx]
 
-    def _header_read(self):
-        if self.schema:
-            return self.schema.col_names
-
-        if self.col_order is not None:
-            return self.col_order
-
-        line = list(self.infile.header)
-        if not line:
-            with TSVReader(self.options, self.filename) as tempreader:
-                return tempreader.schema.col_names
+    def fetch_lines(self, chrom, pos_begin, pos_end):
+        if abs(pos_begin - self.last_pos) > self.ACCESS_SWITCH_THRESHOLD:
+            self.last_pos = pos_end
+            return self._fetch_direct(chrom, pos_begin, pos_end)
         else:
-            header_str = line[-1]
-            if header_str.startswith("#"):
-                header_str = header_str[1:]
-            return handle_header(header_str.split(self.separator))
+            self.last_pos = pos_end
+            return self._fetch_sequential(chrom, pos_begin, pos_end)
+
+    def close(self):
+        self.infile.close()
+        self.direct_infile.close()
+
+    def _parse_line(self, line):
+        return {
+            col: line[idx] for col, idx in self.col_indexes
+        }
+
+    def _setup(self):
+        self.infile = pysam.TabixFile(self.filename, index=self.tabix_filename)
+        self.direct_infile = pysam.TabixFile(
+            self.filename,
+            index=self.tabix_filename
+        )
+        contig_name = self.infile.contigs[-1]
+        self._has_chrom_prefix = contig_name.startswith("chr")
+
+        self._region_reset(self.region)
+
+    @property
+    def header(self):
+        return self.infile.header
 
     def _region_reset(self, region):
         region = handle_chrom_prefix(self._has_chrom_prefix, region)
@@ -201,38 +233,6 @@ class TabixReader(FileReader):
                 f"could not find region {region} in {self.filename}:",
                 ex, file=sys.stderr)
             self._lines_iterator = None
-
-    def _setup(self):
-        self.infile = pysam.TabixFile(self.filename, index=self.tabix_filename)
-        self.direct_infile = pysam.TabixFile(
-            self.filename,
-            index=self.tabix_filename
-        )
-        contig_name = self.infile.contigs[-1]
-        self._has_chrom_prefix = contig_name.startswith("chr")
-
-        self._region_reset(self.region)
-        self.schema = Schema.from_dict({"str": self._header_read()})
-        if self.id_names is not None:
-            col_names = self.schema.col_names
-            self._chrom_idx = col_names.index(self.id_names["chrom"])
-            self._pos_begin_idx = col_names.index(self.id_names["pos_begin"])
-            self._pos_end_idx = col_names.index(self.id_names["pos_end"])
-        else:
-            self._chrom_idx = self.id_indexes["chrom"]
-            self._pos_begin_idx = self.id_indexes["pos_begin"]
-            self._pos_end_idx = self.id_indexes["pos_end"]
-
-    def _cleanup(self):
-        self.infile.close()
-
-    def lines_read_iterator(self):
-        if self._lines_iterator is None:
-            return None
-
-        for line in self._lines_iterator:
-            self._progress_step()
-            yield line
 
     def _purge_buffer(self, chrom, pos_begin, pos_end):
         # purge start of line buffer
@@ -268,26 +268,6 @@ class TabixReader(FileReader):
             if line[self._pos_end_idx] > pos_end:
                 break
 
-    def _select_lines(self, chrom, pos_begin, pos_end):
-        result = []
-        for line in self.buffer:
-            if line[self._chrom_idx] != chrom:
-                continue
-            if regions_intersect(
-                pos_begin, pos_end,
-                line[self._pos_begin_idx], line[self._pos_end_idx]
-            ):
-                result.append(line)
-        return result
-
-    def fetch(self, chrom, pos_begin, pos_end):
-        if abs(pos_begin - self.last_pos) > self.ACCESS_SWITCH_THRESHOLD:
-            self.last_pos = pos_end
-            return self._fetch_direct(chrom, pos_begin, pos_end)
-        else:
-            self.last_pos = pos_end
-            return self._fetch_sequential(chrom, pos_begin, pos_end)
-
     def _fetch_sequential(self, chrom, pos_begin, pos_end):
         if (
             chrom != self._buffer_chrom
@@ -303,13 +283,25 @@ class TabixReader(FileReader):
         self._fill_buffer(chrom, pos_begin, pos_end)
         return self._select_lines(chrom, pos_begin, pos_end)
 
+    def _select_lines(self, chrom, pos_begin, pos_end):
+        result = []
+        for line in self.buffer:
+            if line[self._chrom_idx] != chrom:
+                continue
+            if regions_intersect(
+                pos_begin, pos_end,
+                line[self._pos_begin_idx], line[self._pos_end_idx]
+            ):
+                result.append(self._parse_line(line))
+        return result
+
     def _fetch_direct(self, chrom, pos_begin, pos_end):
         try:
             result = []
             for line in self.direct_infile.fetch(
                 str(chrom), pos_begin - 1, pos_end, parser=pysam.asTuple()
             ):
-                result.append(line)
+                result.append(self._parse_line(line))
             return result
         except ValueError as ex:
             print(
@@ -319,3 +311,63 @@ class TabixReader(FileReader):
                 file=sys.stderr,
             )
             return []
+
+    def fetch_scores_df(self, chrom, pos_begin, pos_end):
+        scores = self.fetch_scores(chrom, pos_begin, pos_end)
+        return self.scores_to_dataframe(scores)
+
+    def scores_to_dataframe(self, scores):
+        df = pd.DataFrame(scores)
+        for score_name in self.score_names:
+            df[score_name] = df[score_name].replace(["NA"], np.nan)
+            df[score_name] = df[score_name].astype("float32")
+        return df
+
+    def fetch_scores_iterator(self, chrom, pos_begin, pos_end):
+        stripped_chrom = handle_chrom_prefix(self.chr_prefix, chrom)
+
+        for line in self.fetch_lines(stripped_chrom, pos_begin, pos_end):
+            yield line
+
+    def fetch_scores(self, chrom, pos_begin, pos_end):
+        stripped_chrom = handle_chrom_prefix(self.chr_prefix, chrom)
+
+        score_lines = self.fetch_lines(stripped_chrom, pos_begin, pos_end)
+        logger.debug(f"score lines found: {score_lines}")
+        result = defaultdict(list)
+
+        for line in score_lines:
+            logger.debug(
+                f"pos_end: {pos_end}; line.pos_end: {line.get(pos_end)}; "
+                f"pos_begin: {pos_begin}; "
+                f"line.pos_begin: {line.get(pos_begin)}"
+            )
+            count = (
+                min(pos_end, line.get(pos_end)) -
+                max(line.get(pos_begin), pos_begin) + 1
+            )
+            if count <= 0:
+                continue
+
+            assert count >= 1, count
+            result["COUNT"].append(count)
+            for col, val in line:
+                result[col].append(val)
+        logger.debug(f"fetch scores: {result}")
+        return result
+
+    def fetch_highest_scores(self, chrom, pos_begin, pos_end):
+        result = dict()
+
+        stripped_chrom = handle_chrom_prefix(self.chr_prefix, chrom)
+
+        for line in self._fetch_direct(
+                stripped_chrom, pos_begin - 1, pos_end,
+                parser=pysam.asTuple()):
+            for score in self.config.scores:
+                score_id = score.id
+                score_value = float(line[score_id]) \
+                    if line.get(score_id) not in (None, "") else None
+                result[score_id] = max(
+                    score_value, result.get(score_id, np.nan))
+        return result
