2c50fab873cd797f384120567096359780263e72
joankosev <joankosev@gmail.com>
2021-06-16 15:08:31 +0300

    Lint genomic-scores-store.ts

--- gpfjs/src/app/genomic-scores/genomic-scores-store.ts
+++ gpfjs/src/app/genomic-scores/genomic-scores-store.ts
@@ -5,52 +5,52 @@ import { IsMoreThanOrEqual } from '../utils/is-more-than-validator';
 import { GenomicScores } from '../genomic-scores-block/genomic-scores-block';
 
 export class GenomicScoreState {
-    @IsNotEmpty()
-    score: GenomicScores;
+  @IsNotEmpty()
+  score: GenomicScores;
 
-    @ValidateIf(o => o.rangeStart !== null)
-    @IsNumber()
-    @IsLessThanOrEqual('rangeEnd')
-    @IsMoreThanOrEqual('domainMin')
-    @IsLessThanOrEqual('domainMax')
-    rangeStart: number;
+  @ValidateIf(o => o.rangeStart !== null)
+  @IsNumber()
+  @IsLessThanOrEqual('rangeEnd')
+  @IsMoreThanOrEqual('domainMin')
+  @IsLessThanOrEqual('domainMax')
+  rangeStart: number;
 
 
-    @ValidateIf(o => o.rangeEnd !== null)
-    @IsNumber()
-    @IsMoreThanOrEqual('rangeStart')
-    @IsMoreThanOrEqual('domainMin')
-    @IsLessThanOrEqual('domainMax')
-    rangeEnd: number;
+  @ValidateIf(o => o.rangeEnd !== null)
+  @IsNumber()
+  @IsMoreThanOrEqual('rangeStart')
+  @IsMoreThanOrEqual('domainMin')
+  @IsLessThanOrEqual('domainMax')
+  rangeEnd: number;
 
-    domainMin: any;
-    domainMax: any;
+  domainMin: any;
+  domainMax: any;
 
-    changeDomain(score: GenomicScores) {
-        if (this.score.domain != null) {
-            this.domainMin = score.domain[0];
-            this.domainMax = score.domain[1];
-        } else {
-            this.domainMin = score.bins[0];
-            this.domainMax = score.bins[score.bins.length - 1];
-        }
+  changeDomain(score: GenomicScores) {
+    if (this.score.domain != null) {
+      this.domainMin = score.domain[0];
+      this.domainMax = score.domain[1];
+    } else {
+      this.domainMin = score.bins[0];
+      this.domainMax = score.bins[score.bins.length - 1];
     }
+  }
 
-    constructor(score?: GenomicScores) {
-        if (score) {
-            this.score = score;
-            this.changeDomain(score);
-        } else {
-            this.score = null;
-        }
-        this.rangeStart = null;
-        this.rangeEnd = null;
+  constructor(score?: GenomicScores) {
+    if (score) {
+      this.score = score;
+      this.changeDomain(score);
+    } else {
+      this.score = null;
     }
+    this.rangeStart = null;
+    this.rangeEnd = null;
+  }
 }
 
 export class GenomicScoresState {
-    @ValidateNested({
-        each: true
-    })
-    genomicScoresState: GenomicScoreState[] = [];
+  @ValidateNested({
+    each: true
+  })
+  genomicScoresState: GenomicScoreState[] = [];
 }
