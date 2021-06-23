40d281f1b6877be5c39d6d8cd7ec9b3f2c36be5a
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2021-06-23 11:15:52 +0300

    GPF-800 revert setting sessionid and csrftoken secure flags

--- wdae/wdae/wdae/default_settings.py
+++ wdae/wdae/wdae/default_settings.py
@@ -191,8 +191,9 @@ REST_FRAMEWORK = {
 
 SESSION_SERIALIZER = "django.contrib.sessions.serializers.JSONSerializer"
 
-SESSION_COOKIE_SECURE = True
-CSRF_COOKIE_SECURE = True
+# SESSION_COOKIE_SECURE = True
+# CSRF_COOKIE_SECURE = True
+
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
