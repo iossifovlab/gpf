13f236994892ca6e4eb95e9815704cba01955889
Hristo Stefanov <hrstefanov@yahoo.co.uk>
2021-06-25 16:19:57 +0300

    build.sh: updates for latest version of build-scripts

--- gpfjs/build.sh
+++ gpfjs/build.sh
@@ -19,14 +19,15 @@ include libopt.sh
 
 function main() {
   local -A options
-  libopt_parse options stage:all preset:fast clobber:allow_if_matching_values -- "$@"
+  libopt_parse options stage:all preset:fast clobber:allow_if_matching_values build_no:0 -- "$@"
 
   local preset="${options["preset"]}"
   local stage="${options["stage"]}"
   local clobber="${options["clobber"]}"
+  local build_no="${options["build_no"]}"
 
   libmain_init gpfjs gpfjs
-  libmain_init_build_env clobber:"$clobber" preset:"$preset" seqpipe-containers
+  libmain_init_build_env clobber:"$clobber" preset:"$preset" build_no:"$build_no" seqpipe-containers
   libmain_save_build_env_on_exit
   libbuild_init stage:"$stage" registry.seqpipe.org
 
