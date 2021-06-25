77f9024202bb13f76db6a0920e38817da3f653ff
joankosev <joankosev@gmail.com>
2021-06-25 13:32:11 +0300

    GPF-863 update varianttypes state logic

--- gpfjs/src/app/varianttypes/varianttypes.component.ts
+++ gpfjs/src/app/varianttypes/varianttypes.component.ts
@@ -1,75 +1,76 @@
-import {
-  Input, Component, OnInit, OnChanges, SimpleChanges, forwardRef
-} from '@angular/core';
-
-import { QueryStateProvider, QueryStateWithErrorsProvider } from '../query/query-state-provider';
-import { StateRestoreService } from '../store/state-restore.service';
-import { DatasetsService } from '../datasets/datasets.service';
-
-import { Validate } from 'class-validator';
+import { Input, Component, OnInit, OnChanges } from '@angular/core';
+import { validate, Validate } from 'class-validator';
 import { SetNotEmpty } from '../utils/set.validators';
+import { Select, Store } from '@ngxs/store';
+import { VarianttypesState, VarianttypeModel, AddVariantType, RemoveVariantType } from './varianttypes.state';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'gpf-varianttypes',
   templateUrl: './varianttypes.component.html',
   styleUrls: ['./varianttypes.component.css'],
-  providers: [{
-    provide: QueryStateProvider,
-    useExisting: forwardRef(() => VarianttypesComponent)
-  }]
 })
-export class VarianttypesComponent extends QueryStateWithErrorsProvider
-    implements OnInit, OnChanges {
+export class VarianttypesComponent implements OnInit, OnChanges {
 
-  @Input()
-  variantTypes: Set<string> = new Set([]);
+  @Input() variantTypes: Set<string> = new Set([]);
+  @Input() @Validate(SetNotEmpty, {message: 'select at least one'}) selectedVariantTypes: Set<string> = new Set([]);
+  @Select(VarianttypesState) state$: Observable<VarianttypeModel>;
+  errors: string[] = [];
 
-  @Input()
-  @Validate(SetNotEmpty, {message: 'select at least one'})
-  selectedVariantTypes: Set<string> = new Set([]);
+  constructor(private store: Store) { }
 
-  constructor(
-    private stateRestoreService: StateRestoreService,
-    private datasetsService: DatasetsService
-  ) {
-    super();
+  ngOnChanges(): void {
+    if (this.selectedVariantTypes) {
+      this.selectedVariantTypes.forEach(variantType => {
+        this.variantTypesCheckValue(variantType, false);
+      });
+    }
   }
 
   ngOnInit() {
     this.selectedVariantTypes = new Set();
+    this.store.selectOnce(state => state.varianttypesState).subscribe(state => {
+      for (const variantType of state.variantTypes) {
+        this.variantTypesCheckValue(variantType, false);
+      }
+    });
+
+    this.state$.subscribe(() => {
+      validate(this).then(errors => this.errors = errors.map(err => String(err)));
+    });
   }
 
-  ngOnChanges(changes: SimpleChanges) {
-    this.stateRestoreService.getState(this.constructor.name)
-      .take(1)
-      .subscribe(state => {
-        if (state['variantTypes']) {
-          this.selectedVariantTypes = new Set(state['variantTypes'] as string[]);
-        }
-      });
+  addVariantType(variantType: string): void {
+    this.selectedVariantTypes.add(variantType);
+    this.store.dispatch(new AddVariantType(variantType));
+  }
+
+  removeVariantType(variantType: string): void {
+    this.selectedVariantTypes.delete(variantType);
+    this.store.dispatch(new RemoveVariantType(variantType));
   }
 
   selectAll(): void {
-    this.selectedVariantTypes = new Set(this.variantTypes);
-  }
-
-  selectNone(): void {
-    this.selectedVariantTypes = new Set();
-  }
-
-  variantTypesCheckValue(variantType: string, value: boolean): void {
-    if (variantType === 'sub' || variantType === 'ins' || variantType === 'del' ||
-        variantType === 'CNV' || variantType === 'complex' || variantType === 'TR') {
-      if (!value) {
-        this.selectedVariantTypes.add(variantType);
-      } else {
-        this.selectedVariantTypes.delete(variantType);
-      }
+    for (const variantType of this.variantTypes) {
+      this.addVariantType(variantType);
     }
   }
 
-  getState() {
-    return this.validateAndGetState(this)
-      .map(_ => ({ 'variantTypes': Array.from(this.selectedVariantTypes) }));
+  selectNone(): void {
+    for (const variantType of this.variantTypes) {
+      this.removeVariantType(variantType);
+    }
+  }
+
+  variantTypesCheckValue(variantType: string, value: boolean): void {
+    if (!this.variantTypes.has(variantType)) {
+      return;
+    }
+
+    if (!value) {
+      this.addVariantType(variantType);
+    } else {
+      this.removeVariantType(variantType);
+    }
   }
 }
