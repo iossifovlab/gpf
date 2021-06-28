e21b1d22b7ab48b4dc397a6dbef4e9841478f898
iordanivanov <iiordan.ivanov@gmail.com>
2021-06-28 10:29:49 +0300

    GPF-863 Begin work on adapting genotype browser to NGXS state

--- gpfjs/src/app/genotype-block/genotype-block.component.ts
+++ gpfjs/src/app/genotype-block/genotype-block.component.ts
@@ -1,107 +1,14 @@
-import { Component, AfterViewInit, forwardRef } from '@angular/core';
-
-import { Observable } from 'rxjs';
-
-import { QueryStateCollector } from '../query/query-state-provider';
-import { Dataset, PedigreeSelector, PresentInRole } from '../datasets/datasets';
-import { DatasetsService } from '../datasets/datasets.service';
+import { Component, Input } from '@angular/core';
+import { Dataset } from '../datasets/datasets';
 
 @Component({
   selector: 'gpf-genotype-block',
   templateUrl: './genotype-block.component.html',
   styleUrls: ['./genotype-block.component.css'],
-  providers: [{provide: QueryStateCollector, useExisting: forwardRef(() => GenotypeBlockComponent) }]
 })
-export class GenotypeBlockComponent extends QueryStateCollector implements AfterViewInit {
-  hasPedigreeSelector: Observable<boolean>;
-  hasPresentInChild: Observable<boolean>;
-  hasPresentInParent: Observable<boolean>;
-  hasPresentInRole: Observable<boolean>;
-  hasStudyTypes: Observable<boolean>;
-  pedigrees: Observable<Array<PedigreeSelector>>;
-  presentInRole: Observable<Array<PresentInRole>>;
-  selectedDataset$: Observable<Dataset>;
-  inheritanceTypeFilter: Observable<Array<string>>;
-  selectedInheritanceTypeFilterValues: Observable<Array<string>>;
-  variantTypes: Observable<Set<string>>;
-  selectedVariantTypes: Observable<Set<string>>;
+export class GenotypeBlockComponent {
+  @Input()
+  dataset: Dataset;
 
-  constructor(
-    private datasetsService: DatasetsService
-  ) {
-    super();
-  }
-
-  ngAfterViewInit() {
-    this.selectedDataset$ = this.datasetsService.getSelectedDataset();
-    const selectedDataset$ = this.selectedDataset$;
-
-    this.hasPedigreeSelector = selectedDataset$.map(dataset => {
-      if (!dataset || !dataset.genotypeBrowserConfig) {
-        return false;
-      }
-      return dataset.genotypeBrowserConfig.hasPedigreeSelector;
-    });
-    this.hasPresentInChild = selectedDataset$.map(dataset => {
-      if (!dataset || !dataset.genotypeBrowserConfig) {
-        return false;
-      }
-      return dataset.genotypeBrowserConfig.hasPresentInChild;
-    });
-    this.hasPresentInParent = selectedDataset$.map(dataset => {
-      if (!dataset || !dataset.genotypeBrowserConfig) {
-        return false;
-      }
-      return dataset.genotypeBrowserConfig.hasPresentInParent;
-    });
-    this.hasPresentInRole = selectedDataset$.map(dataset => {
-      if (!dataset || !dataset.genotypeBrowserConfig) {
-        return false;
-      }
-      return dataset.genotypeBrowserConfig.hasPresentInRole;
-    });
-    this.hasStudyTypes = selectedDataset$.map(dataset => {
-      if (!dataset || !dataset.genotypeBrowserConfig) {
-        return false;
-      }
-      return dataset.genotypeBrowserConfig.hasStudyTypes;
-    });
-    this.pedigrees = selectedDataset$.map(dataset => {
-      if (!dataset || !dataset.peopleGroupConfig) {
-        return [];
-      }
-      return dataset.peopleGroupConfig.pedigreeSelectors;
-    });
-    this.presentInRole = selectedDataset$.map(dataset => {
-      if (!dataset || !dataset.genotypeBrowserConfig) {
-        return [];
-      }
-      return dataset.genotypeBrowserConfig.presentInRole;
-    });
-    this.inheritanceTypeFilter = selectedDataset$.map(dataset => {
-      if (!dataset || !dataset.genotypeBrowserConfig) {
-        return [];
-      }
-      return dataset.genotypeBrowserConfig.inheritanceTypeFilter;
-    });
-    this.selectedInheritanceTypeFilterValues = selectedDataset$.map(dataset => {
-      if (!dataset || !dataset.genotypeBrowserConfig) {
-        return [];
-      }
-      return dataset.genotypeBrowserConfig.selectedInheritanceTypeFilterValues;
-    });
-    this.variantTypes = selectedDataset$.map(dataset => {
-      if (!dataset || !dataset.genotypeBrowserConfig) {
-        return new Set([]);
-      }
-      return dataset.genotypeBrowserConfig.variantTypes;
-    });
-    this.selectedVariantTypes = selectedDataset$.map(dataset => {
-      if (!dataset || !dataset.genotypeBrowserConfig) {
-        return new Set([]);
-      }
-      return dataset.genotypeBrowserConfig.selectedVariantTypes;
-    });
-  }
-
+  constructor() { }
 }
--- gpfjs/src/app/genotype-browser/genotype-browser.component.ts
+++ gpfjs/src/app/genotype-browser/genotype-browser.component.ts
@@ -2,25 +2,20 @@ import { Component, Input, OnInit, OnChanges, AfterViewInit, SimpleChanges } fro
 
 import { Observable } from 'rxjs';
 
-import { QueryStateCollector } from '../query/query-state-provider';
 import { QueryService } from '../query/query.service';
 import { FullscreenLoadingService } from '../fullscreen-loading/fullscreen-loading.service';
 import { ConfigService } from '../config/config.service';
 import { DatasetsService } from '../datasets/datasets.service';
 import { Dataset, SelectorValue } from '../datasets/datasets';
 import { GenotypePreviewVariantsArray } from 'app/genotype-preview-model/genotype-preview';
+import { Store } from '@ngxs/store';
 
 @Component({
   selector: 'gpf-genotype-browser',
   templateUrl: './genotype-browser.component.html',
   styleUrls: ['./genotype-browser.component.css'],
-  providers: [{
-    provide: QueryStateCollector,
-    useExisting: GenotypeBrowserComponent
-  }]
 })
-export class GenotypeBrowserComponent extends QueryStateCollector
-    implements OnInit, OnChanges, AfterViewInit {
+export class GenotypeBrowserComponent implements OnInit, OnChanges {
   genotypePreviewVariantsArray: GenotypePreviewVariantsArray;
   tablePreview: boolean;
   legend: Array<SelectorValue>;
@@ -34,40 +29,40 @@ export class GenotypeBrowserComponent extends QueryStateCollector
   private loadingFinished: boolean;
 
   constructor(
+    private store: Store,
     private queryService: QueryService,
     readonly configService: ConfigService,
     private loadingService: FullscreenLoadingService,
     private datasetsService: DatasetsService,
   ) {
-    super();
   }
 
-  getCurrentState() {
-    const state = super.getCurrentState();
+  // getCurrentState() {
+  //   const state = super.getCurrentState();
 
-    return state.map(current_state => {
-        const stateObject = Object.assign({ datasetId: this.selectedDatasetId }, current_state);
-        return stateObject;
-      });
-  }
+  //   return state.map(current_state => {
+  //       const stateObject = Object.assign({ datasetId: this.selectedDatasetId }, current_state);
+  //       return stateObject;
+  //     });
+  // }
 
-  ngAfterViewInit() {
-    this.detectNextStateChange(() => {
-        this.getCurrentState()
-        .take(1)
-        .subscribe(
-          state => {
-            this.genotypePreviewVariantsArray = null;
-            this.genotypeBrowserState = state;
-            this.disableQueryButtons = false;
-          },
-          error => {
-            this.genotypePreviewVariantsArray = null;
-            this.disableQueryButtons = true;
-            console.warn(error);
-          });
-      });
-  }
+  // ngAfterViewInit() {
+  //   this.detectNextStateChange(() => {
+  //       this.getCurrentState()
+  //       .take(1)
+  //       .subscribe(
+  //         state => {
+  //           this.genotypePreviewVariantsArray = null;
+  //           this.genotypeBrowserState = state;
+  //           this.disableQueryButtons = false;
+  //         },
+  //         error => {
+  //           this.genotypePreviewVariantsArray = null;
+  //           this.disableQueryButtons = true;
+  //           console.warn(error);
+  //         });
+  //     });
+  // }
 
   ngOnInit() {
     this.selectedDataset$ = this.datasetsService.getSelectedDataset();
@@ -78,47 +73,43 @@ export class GenotypeBrowserComponent extends QueryStateCollector
   }
 
   submitQuery() {
-    this.getCurrentState()
-      .subscribe(state => {
-        this.loadingFinished = false;
-        this.loadingService.setLoadingStart();
+    const state = {};
+    this.loadingFinished = false;
+    this.loadingService.setLoadingStart();
 
-        this.selectedDataset$.subscribe( selectedDataset => {
-          this.genotypePreviewVariantsArray = null;
-          this.genotypeBrowserState = state;
-          this.legend = selectedDataset.peopleGroupConfig.getLegend(state['peopleGroup']);
+    this.selectedDataset$.subscribe( selectedDataset => {
+      this.genotypePreviewVariantsArray = null;
+      this.genotypeBrowserState = state;
+      this.legend = selectedDataset.peopleGroupConfig.getLegend(state['peopleGroup']);
 
-          this.queryService.streamingFinishedSubject.subscribe(
-            _ => { this.loadingFinished = true; }
-          );
+      this.queryService.streamingFinishedSubject.subscribe(
+        _ => { this.loadingFinished = true; }
+      );
 
-          this.genotypePreviewVariantsArray =
-            this.queryService.getGenotypePreviewVariantsByFilter(
-              state, selectedDataset.genotypeBrowserConfig.columnIds, this.loadingService
-            );
+      this.genotypePreviewVariantsArray =
+        this.queryService.getGenotypePreviewVariantsByFilter(
+          state, selectedDataset.genotypeBrowserConfig.columnIds, this.loadingService
+        );
 
-        }, error => {
-          console.warn(error);
-        });
-      }, error => {
-        console.warn(error);
-      });
+    }, error => {
+      console.warn(error);
+    });
   }
 
   onSubmit(event) {
-    this.getCurrentState()
-    .subscribe(
-      state => {
-        this.selectedDataset$.subscribe( selectedDataset => {
-          const args: any = state
-          args.download = true;
-          event.target.queryData.value = JSON.stringify(args);
-          event.target.submit();
-        }, error => {
-            console.warn(error);
-        });
-      },
-      error => null
-    );
+    // this.getCurrentState()
+    // .subscribe(
+    //   state => {
+    //     this.selectedDataset$.subscribe( selectedDataset => {
+    //       const args: any = state
+    //       args.download = true;
+    //       event.target.queryData.value = JSON.stringify(args);
+    //       event.target.submit();
+    //     }, error => {
+    //         console.warn(error);
+    //     });
+    //   },
+    //   error => null
+    // );
   }
 }
