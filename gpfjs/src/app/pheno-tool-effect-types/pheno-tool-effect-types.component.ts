c356b7220c4d5f201f62d54f748de7e23920bbc4
iordanivanov <iiordan.ivanov@gmail.com>
2021-07-02 18:39:46 +0300

    GPF-863 Some fixes to PhenoToolEffectTypesComponent

--- gpfjs/src/app/pheno-tool-effect-types/pheno-tool-effect-types.component.ts
+++ gpfjs/src/app/pheno-tool-effect-types/pheno-tool-effect-types.component.ts
@@ -1,28 +1,28 @@
-import { Component, OnInit, Input, Output, EventEmitter, forwardRef } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { Observable } from 'rxjs';
 import { Store } from '@ngxs/store';
 
+import { EffecttypesComponent } from '../effecttypes/effecttypes.component';
 import {
-  PHENO_TOOL_ALL, PHENO_TOOL_OTHERS, PHENO_TOOL_CNV, PHENO_TOOL_LGDS,
+  PHENO_TOOL_ALL, PHENO_TOOL_OTHERS,
+  PHENO_TOOL_CNV, PHENO_TOOL_LGDS,
   PHENO_TOOL_INITIAL_VALUES
 } from './pheno-tool-effect-types';
-import { QueryStateProvider, QueryStateWithErrorsProvider } from '../query/query-state-provider';
-import { StateRestoreService } from '../store/state-restore.service';
-import { EffecttypesComponent } from '../effecttypes/effecttypes.component';
 
 @Component({
   selector: 'gpf-pheno-tool-effect-types',
   templateUrl: './pheno-tool-effect-types.component.html',
-  providers: [{
-    provide: Store,
-    useExisting: forwardRef(() => PhenoToolEffectTypesComponent)
-  }]
+  providers: [{ provide: Store, useClass: Store }]
 })
 export class PhenoToolEffectTypesComponent extends EffecttypesComponent  {
   phenoToolOthers: Set<string> = PHENO_TOOL_OTHERS;
   phenoToolCNV: Set<string> = PHENO_TOOL_CNV;
   phenoToolLGDs: Set<string> = PHENO_TOOL_LGDS;
+
+  constructor(store: Store) {
+    super(store);
+    this.effectTypesButtons.set('PHENO_TOOL_ALL', PHENO_TOOL_ALL);
+  }
 
   selectInitialValues() {
     this.setEffectTypes(PHENO_TOOL_INITIAL_VALUES);
