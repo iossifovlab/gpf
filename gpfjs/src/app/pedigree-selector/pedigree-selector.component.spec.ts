3bb27990afb216ab1f013a6514740022417e2dbc
iordanivanov <iiordan.ivanov@gmail.com>
2021-07-16 14:46:28 +0300

    GPF-863 GPF-915 Add PedigreeSelectorState to component's test bed

--- gpfjs/src/app/pedigree-selector/pedigree-selector.component.spec.ts
+++ gpfjs/src/app/pedigree-selector/pedigree-selector.component.spec.ts
@@ -2,6 +2,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NgxsModule } from '@ngxs/store';
 import { PedigreeSelectorComponent } from './pedigree-selector.component';
+import { PedigreeSelectorState } from './pedigree-selector.state';
 
 describe('PedigreeSelectorComponent', () => {
   let component: PedigreeSelectorComponent;
@@ -10,7 +11,7 @@ describe('PedigreeSelectorComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [PedigreeSelectorComponent],
-      imports: [NgxsModule.forRoot([])],
+      imports: [NgxsModule.forRoot([PedigreeSelectorState])],
     })
       .compileComponents();
   }));
