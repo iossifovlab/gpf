8075472e67421d73f09f8cc54b0464522f98b4d0
Hristo Stefanov <hrstefanov@yahoo.co.uk>
2021-08-05 17:12:16 +0300

    build_*.sh: update to latest version of build-scripts

--- build_cleanup.sh
+++ build_cleanup.sh
@@ -1,1 +1,1 @@
-././build.sh preset:"fast" clobber:"allow_if_matching_values" build_no:"0" stage:"Post Cleanup"
+././build.sh preset:"fast" clobber:"allow_if_matching_values" expose_ports:"yes" build_no:"0" stage:"Post Cleanup"
