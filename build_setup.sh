e2f686f765bb70671c0154b63bb21dd586d006c2
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2021-08-10 15:55:19 +0300

    GPF-767 allow clobber in build setup/tests scripts

--- build_setup.sh
+++ build_setup.sh
@@ -3,9 +3,9 @@
 set -e
 
 ./build.sh preset:"fast" expose_ports:"yes" stage:"Jenkinsfile.generated-stages.all"
-./build.sh preset:"fast" clobber:"allow_if_matching_values" expose_ports:"yes" build_no:"0" stage:"Cleanup"
-./build.sh preset:"fast" clobber:"allow_if_matching_values" expose_ports:"yes" build_no:"0" stage:"Create gpf-dev docker image"
-./build.sh preset:"fast" clobber:"allow_if_matching_values" expose_ports:"yes" build_no:"0" stage:"Prepare GPF data"
-./build.sh preset:"fast" clobber:"allow_if_matching_values" expose_ports:"yes" build_no:"0" stage:"Prepare GPF remote data"
-./build.sh preset:"fast" clobber:"allow_if_matching_values" expose_ports:"yes" build_no:"0" stage:"Run cluster"
-./build.sh preset:"fast" clobber:"allow_if_matching_values" expose_ports:"yes" build_no:"0" stage:"Import test data to impala"
+./build.sh preset:"fast" clobber:"allow" expose_ports:"yes" build_no:"0" stage:"Cleanup"
+./build.sh preset:"fast" clobber:"allow" expose_ports:"yes" build_no:"0" stage:"Create gpf-dev docker image"
+./build.sh preset:"fast" clobber:"allow" expose_ports:"yes" build_no:"0" stage:"Prepare GPF data"
+./build.sh preset:"fast" clobber:"allow" expose_ports:"yes" build_no:"0" stage:"Prepare GPF remote data"
+./build.sh preset:"fast" clobber:"allow" expose_ports:"yes" build_no:"0" stage:"Run cluster"
+./build.sh preset:"fast" clobber:"allow" expose_ports:"yes" build_no:"0" stage:"Import test data to impala"
