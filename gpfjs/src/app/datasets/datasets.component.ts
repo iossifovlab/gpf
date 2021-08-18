6a47019c0df52454a158d1983ec1e7aaa0bf56b1
IvoStefanov <ivostefanovtues@gmail.com>
2021-08-18 09:49:21 +0300

    GPF-1044 update datasets component dataset logic

--- gpfjs/src/app/datasets/datasets.component.ts
+++ gpfjs/src/app/datasets/datasets.component.ts
@@ -21,7 +21,8 @@ export class DatasetsComponent implements OnInit {
   registerAlertVisible = false;
   datasets$: Observable<Dataset[]>;
   datasetTrees: DatasetNode[];
-  selectedDataset$: Observable<Dataset>;
+  selectedDataset: Dataset;
+  selectedDataset$;
   permissionDeniedPrompt: string;
   public toolPageLinks = toolPageLinks;
 
@@ -36,6 +37,7 @@ export class DatasetsComponent implements OnInit {
   ngOnInit() {
     this.route.params.subscribe(
       (params: Params) => {
+        console.log('route');
         this.datasetsService.setSelectedDatasetById(params['dataset']);
       });
 
@@ -43,36 +45,37 @@ export class DatasetsComponent implements OnInit {
       this.datasetsService.getDatasetsObservable());
 
     this.createDatasetHierarchy();
-    this.selectedDataset$ = this.datasetsService.getSelectedDataset();
+    this.selectedDataset = this.datasetsService.getSelectedDataset();
+    this.selectedDataset$ = this.datasetsService.getSelectedDatasetObservable();
 
-    this.selectedDataset$
-    .subscribe(selectedDataset => {
-      if (!selectedDataset) {
+    this.datasetsService.getDatasetsLoadedObservable()
+    .subscribe(datasetsLoaded => {
+      this.selectedDataset = this.datasetsService.getSelectedDataset();
+      if (!this.selectedDataset) {
         return;
       }
-      this.registerAlertVisible = !selectedDataset.accessRights;
+      this.registerAlertVisible = !this.selectedDataset.accessRights;
+
+      if (!this.isToolSelected()) {
+        this.router.navigate(['/', 'datasets', this.selectedDataset.id, this.findFirstTool(this.selectedDataset)]);
+      } else {
+        const url = this.router.url.split('/');
+        const toolName = url[url.indexOf('datasets') + 2];
+
+        if (!this.isToolEnabled(this.selectedDataset, toolName)) {
+          this.router.navigate(['/', 'datasets', this.selectedDataset.id, this.findFirstTool(this.selectedDataset)]);
+        }
+      }
     });
 
     this.datasets$.pipe(take(1)).subscribe(datasets => {
       if (!this.datasetsService.hasSelectedDataset()) {
-        this.selectDataset(datasets[0]);
-      } else if (!this.isToolSelected()) {
-        this.selectedDataset$.pipe(take(1)).subscribe(dataset => {
-          this.router.navigate(['/', 'datasets', dataset.id, this.findFirstTool(dataset)]);
-        });
-      } else {
-        this.selectedDataset$.pipe(take(1)).subscribe(dataset => {
-          const url = this.router.url.split('/');
-          const toolName = url[url.indexOf('datasets') + 2];
-
-          if (!this.isToolEnabled(dataset, toolName)) {
-            this.router.navigate(['/', 'datasets', dataset.id, this.findFirstTool(dataset)]);
-          }
-        });
+        this.datasetsService.setSelectedDataset(datasets[0]);
       }
     });
 
     this.usersService.getUserInfoObservable().subscribe(() => {
+      console.log('users');
       this.datasetsService.reloadSelectedDataset();
     });
 
@@ -133,7 +136,7 @@ export class DatasetsComponent implements OnInit {
     } else if (selectedDataset.phenotypeTool) {
       firstTool = toolPageLinks.phenotypeTool;
     }
-
+    firstTool = toolPageLinks.datasetStatistics;
     return firstTool;
   }
 
@@ -156,12 +159,6 @@ export class DatasetsComponent implements OnInit {
           dataset.accessRights
       )
     ));
-  }
-
-  selectDataset(dataset: Dataset): void {
-    if (dataset !== undefined) {
-      this.router.navigate(['/', 'datasets', dataset.id, this.findFirstTool(dataset)]);
-    }
   }
 
   routeChange(): void {
