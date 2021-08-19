c7ae7f3c245870ca29189e8fb9d4444fca1df98e
Hristo Stefanov <hrstefanov@yahoo.co.uk>
2021-08-19 17:04:21 +0300

    build_*.sh: support overriding of options. expose ports is enabled by default

--- build_tests.sh
+++ build_tests.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 
+shopt -s extdebug
+shopt -s inherit_errexit
 set -e
 
-././build.sh preset:"fast" clobber:"allow" expose_ports:"yes" build_no:"0" stage:"Tests - dae"
-././build.sh preset:"fast" clobber:"allow" expose_ports:"yes" build_no:"0" stage:"Tests - wdae"
+. build-scripts/loader-extended.bash
+
+loader_addpath build-scripts/
+
+# shellcheck source=build-scripts/libopt.sh
+include libopt.sh
+
+declare -A options
+libopt_parse options \
+  stage:all preset:fast clobber:allow_if_matching_values build_no:0 generate_jenkins_init:no expose_ports:yes -- "$@"
+
+preset="${options["preset"]}"
+stage="${options["stage"]}"
+clobber="${options["clobber"]}"
+build_no="${options["build_no"]}"
+generate_jenkins_init="${options["generate_jenkins_init"]}"
+expose_ports="${options["expose_ports"]}"
+
+./build.sh preset:"$preset" clobber:"$clobber" expose_ports:"$expose_ports" stage:"Tests - dae"
+./build.sh preset:"$preset" clobber:"$clobber" expose_ports:"$expose_ports" stage:"Tests - wdae"
