6f2dc716b8aa23fad307ca0257f4a83072be8878
IvoTod <ivo.tod.todorov@gmail.com>
2021-08-24 12:59:18 +0300

    GPF-1061 Fix test_generate_common_report

--- dae/dae/tools/tests/test_generate_common_report.py
+++ dae/dae/tools/tests/test_generate_common_report.py
@@ -2,5 +2,5 @@ from dae.tools.generate_common_report import main
 
 
 def test_generate_common_report(gpf_instance_2013):
-    main(gpf_instance_2013, argv=["--studies", " "])
-    main(gpf_instance_2013, argv=["--show-studies"])
+    main(["--studies", " "], gpf_instance_2013)
+    main(["--show-studies"], gpf_instance_2013)
