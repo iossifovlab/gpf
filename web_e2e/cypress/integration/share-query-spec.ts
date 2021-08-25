f3a728eae54aaa3437efb4f5edd94504262cf77b
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2021-08-25 16:21:50 +0300

    GPF-1064 disable share-query-spec

--- web_e2e/cypress/integration/share-query-spec.ts
+++ web_e2e/cypress/integration/share-query-spec.ts
@@ -4,55 +4,55 @@ import { ShareQueryPage } from 'cypress/elements/share-query-page';
 import { datasetIds, toolPageLinks } from 'cypress/elements/utils';
 
 describe('Share query tests', () => {
-  const page = new ShareQueryPage();
+  // const page = new ShareQueryPage();
 
-  before(() => {
-    page.cleanup();
-    page.navigateToHome();
-    page.loginAdmin();
-  });
+  // before(() => {
+  //   page.cleanup();
+  //   page.navigateToHome();
+  //   page.loginAdmin();
+  // });
 
-  beforeEach(() => {
-    page.preserveLogin();
-    page.navigateToHome();
-    page.navigateToDatasetPage(datasetIds.compAll, toolPageLinks.genotypeBrowser);
-  });
+  // beforeEach(() => {
+  //   page.preserveLogin();
+  //   page.navigateToHome();
+  //   page.navigateToDatasetPage(datasetIds.compAll, toolPageLinks.genotypeBrowser);
+  // });
 
-  it('should open share query dropdown menu after share query button click', () => {
-    page.dropdownMenu.invoke('attr', 'class').should('contain', 'dropdown-menu');
+  // it('should open share query dropdown menu after share query button click', () => {
+  //   page.dropdownMenu.invoke('attr', 'class').should('contain', 'dropdown-menu');
 
-    page.button.click();
-    page.dropdownMenu.invoke('attr', 'class').should('contain', 'dropdown-menu');
-  });
+  //   page.button.click();
+  //   page.dropdownMenu.invoke('attr', 'class').should('contain', 'dropdown-menu');
+  // });
 
-  it('should share a query, load it and open all tools tabs', () => {
-    const datasetsPage = new DatasetsPage();
+  // it('should share a query, load it and open all tools tabs', () => {
+  //   const datasetsPage = new DatasetsPage();
 
-    page.button.click();
-    page.input.invoke('val').then((url) => {
-      cy.visit(String(url)).then(() => {
-        datasetsPage.datasetStatisticsButton.click();
-        datasetsPage.genotypeBrowserButton.click();
-        datasetsPage.phenotypeBrowserButton.click();
-        datasetsPage.phenotypeToolButton.click();
-        datasetsPage.geneBrowserButton.click();
-      });
-    })
-  });
+  //   page.button.click();
+  //   page.input.invoke('val').then((url) => {
+  //     cy.visit(String(url)).then(() => {
+  //       datasetsPage.datasetStatisticsButton.click();
+  //       datasetsPage.genotypeBrowserButton.click();
+  //       datasetsPage.phenotypeBrowserButton.click();
+  //       datasetsPage.phenotypeToolButton.click();
+  //       datasetsPage.geneBrowserButton.click();
+  //     });
+  //   })
+  // });
 
-  it('should navigate to genotype browser, check all effect types checkboxes, generate a share query, ' +
-     'load it and validate that all effect types checkboxes are checked', () => {
-    const genotypeBlockPage = new GenotypeBlockPage();
+  // it('should navigate to genotype browser, check all effect types checkboxes, generate a share query, ' +
+  //    'load it and validate that all effect types checkboxes are checked', () => {
+  //   const genotypeBlockPage = new GenotypeBlockPage();
 
-    genotypeBlockPage.findButtonInComponentContainingText('gpf-effecttypes', 'All').click();
+  //   genotypeBlockPage.findButtonInComponentContainingText('gpf-effecttypes', 'All').click();
 
-    page.button.click();
-    page.input.invoke('val').then((url) => {
-      cy.visit(String(url)).then(() => {
-        genotypeBlockPage.findAllCheckboxesInComponent('gpf-effecttypes').each((element) => {
-          cy.wrap(element).should('be.checked');
-        });
-      })
-    })
-  });
+  //   page.button.click();
+  //   page.input.invoke('val').then((url) => {
+  //     cy.visit(String(url)).then(() => {
+  //       genotypeBlockPage.findAllCheckboxesInComponent('gpf-effecttypes').each((element) => {
+  //         cy.wrap(element).should('be.checked');
+  //       });
+  //     })
+  //   })
+  // });
 });
