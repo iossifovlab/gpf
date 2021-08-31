fba86448cd4bba7260efba0cf3591f414d6df3fc
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2021-08-31 11:50:50 +0300

    GPF-1055 clean up annotation tests

--- dae/dae/annotation/tests/test_vcf_info_extractor.py
+++ dae/dae/annotation/tests/test_vcf_info_extractor.py
@@ -3,60 +3,60 @@ import pytest
 from .conftest import relative_to_this_test_folder
 
 from dae.configuration.gpf_config_parser import FrozenBox
-from dae.annotation.tools.annotator_config import AnnotationConfigParser
-from dae.annotation.tools.vcf_info_extractor import VCFInfoExtractor
-from dae.annotation.tools.file_io import IOManager, IOType
+# from dae.annotation.tools.annotator_config import AnnotationConfigParser
+# from dae.annotation.tools.vcf_info_extractor import VCFInfoExtractor
+# from dae.annotation.tools.file_io import IOManager, IOType
 
 
-@pytest.fixture
-def vcf_io(request):
-    io_config = {
-        "infile": relative_to_this_test_folder("fixtures/vcf_input.tsv"),
-        "outfile": "-",
-    }
-    io_config = FrozenBox(io_config)
-    io_manager = IOManager(io_config, IOType.TSV, IOType.TSV)
-    return io_manager
+# @pytest.fixture
+# def vcf_io(request):
+#     io_config = {
+#         "infile": relative_to_this_test_folder("fixtures/vcf_input.tsv"),
+#         "outfile": "-",
+#     }
+#     io_config = FrozenBox(io_config)
+#     io_manager = IOManager(io_config, IOType.TSV, IOType.TSV)
+#     return io_manager
 
 
-def test_vcf_info_extractor(capsys, vcf_io, genomes_db_2013):
+# def test_vcf_info_extractor(capsys, vcf_io, genomes_db_2013):
 
-    expected_output = (
-        "extracted-AC\textracted-AB\textracted-AT\textracted-AZ\n"
-        "0\t7.324234\t4.453e-10\ttest1\n"
-        "2\t\t6.453e+10\ttest2\n"
-        "4\t11.324234\t\ttest3\n"
-        "\t13.324234\t10.453e+10\t\n"
-    )
+#     expected_output = (
+#         "extracted-AC\textracted-AB\textracted-AT\textracted-AZ\n"
+#         "0\t7.324234\t4.453e-10\ttest1\n"
+#         "2\t\t6.453e+10\ttest2\n"
+#         "4\t11.324234\t\ttest3\n"
+#         "\t13.324234\t10.453e+10\t\n"
+#     )
 
-    opts = {"mode": "overwrite"}
+#     opts = {"mode": "overwrite"}
 
-    section_config = AnnotationConfigParser.parse_section({
-            "options": opts,
-            "columns": {
-                "AC": "extracted-AC",
-                "AB": "extracted-AB",
-                "AT": "extracted-AT",
-                "AZ": "extracted-AZ",
-            },
-            "annotator": "vcf_info_extractor.VCFInfoExtractor",
-            "virtual_columns": [],
-        }
-    )
+#     section_config = AnnotationConfigParser.parse_section({
+#             "options": opts,
+#             "columns": {
+#                 "AC": "extracted-AC",
+#                 "AB": "extracted-AB",
+#                 "AT": "extracted-AT",
+#                 "AZ": "extracted-AZ",
+#             },
+#             "annotator": "vcf_info_extractor.VCFInfoExtractor",
+#             "virtual_columns": [],
+#         }
+#     )
 
-    with vcf_io as io_manager:
-        annotator = VCFInfoExtractor(section_config, genomes_db_2013)
-        assert annotator is not None
-        capsys.readouterr()
-        annotator.annotate_file(io_manager)
+#     with vcf_io as io_manager:
+#         annotator = VCFInfoExtractor(section_config, genomes_db_2013)
+#         assert annotator is not None
+#         capsys.readouterr()
+#         annotator.annotate_file(io_manager)
 
-    # print(variants_input.output)
-    captured = capsys.readouterr()
+#     # print(variants_input.output)
+#     captured = capsys.readouterr()
 
-    print(captured.out)
-    print(captured.err)
-    print(expected_output)
-    assert captured.out == expected_output
+#     print(captured.out)
+#     print(captured.err)
+#     print(expected_output)
+#     assert captured.out == expected_output
 
 
-#     assert captured.err == 'Processed 4 lines.\n'
+# #     assert captured.err == 'Processed 4 lines.\n'
--- dae/dae/tools/tests/conftest.py
+++ dae/dae/tools/tests/conftest.py
@@ -4,7 +4,7 @@ import os
 import shutil
 from box import Box
 
-from dae.annotation.tools.file_io import IOManager, IOType
+# from dae.annotation.tools.file_io import IOManager, IOType
 from dae.utils.fixtures import change_environment
 
 
@@ -12,37 +12,37 @@ def relative_to_this_test_folder(path):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
 
 
-@pytest.fixture
-def vcf_variants_io(request):
-    def build(fixture_name, options=Box({})):
-        io_options = options.to_dict()
-        io_config = {
-            "infile": relative_to_this_test_folder(fixture_name),
-            "outfile": "-",
-        }
-        io_options.update(io_config)
+# @pytest.fixture
+# def vcf_variants_io(request):
+#     def build(fixture_name, options=Box({})):
+#         io_options = options.to_dict()
+#         io_config = {
+#             "infile": relative_to_this_test_folder(fixture_name),
+#             "outfile": "-",
+#         }
+#         io_options.update(io_config)
 
-        io_options = Box(io_options, default_box=True, default_box_attr=None)
-        io_manager = IOManager(io_options, IOType.TSV, IOType.TSV)
-        return io_manager
+#         io_options = Box(io_options, default_box=True, default_box_attr=None)
+#         io_manager = IOManager(io_options, IOType.TSV, IOType.TSV)
+#         return io_manager
 
-    return build
+#     return build
 
 
-@pytest.fixture(scope="module")
-def gene_info_cache_dir():
-    cache_dir = relative_to_this_test_folder("fixtures/geneInfo/cache")
-    assert not os.path.exists(
-        cache_dir
-    ), 'Cache dir "{}"already  exists..'.format(cache_dir)
-    os.makedirs(cache_dir)
+# @pytest.fixture(scope="module")
+# def gene_info_cache_dir():
+#     cache_dir = relative_to_this_test_folder("fixtures/geneInfo/cache")
+#     assert not os.path.exists(
+#         cache_dir
+#     ), 'Cache dir "{}"already  exists..'.format(cache_dir)
+#     os.makedirs(cache_dir)
 
-    new_envs = {
-        "DATA_STUDY_GROUPS_DENOVO_GENE_SETS_DIR": cache_dir,
-        "DAE_DB_DIR": relative_to_this_test_folder("fixtures"),
-    }
+#     new_envs = {
+#         "DATA_STUDY_GROUPS_DENOVO_GENE_SETS_DIR": cache_dir,
+#         "DAE_DB_DIR": relative_to_this_test_folder("fixtures"),
+#     }
 
-    for val in change_environment(new_envs):
-        yield val
+#     for val in change_environment(new_envs):
+#         yield val
 
-    shutil.rmtree(cache_dir)
+#     shutil.rmtree(cache_dir)
