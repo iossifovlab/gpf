85f066638e361a38c77b13c892cb5678f29c8ed1
iordanivanov <iiordan.ivanov@gmail.com>
2021-09-08 15:31:28 +0300

    GPF-1092 Wrap iterator_to_json with a try/except block

--- wdae/wdae/utils/streaming_response_util.py
+++ wdae/wdae/utils/streaming_response_util.py
@@ -14,17 +14,21 @@ def convert(obj):
 
 
 def iterator_to_json(variants):
-    yield "["
-    curr = next(variants, None)
-    post = next(variants, None)
-    while curr is not None:
-        yieldval = json.dumps(curr, default=convert)
-        if post is None:
-            yield yieldval
-            break
-        else:
-            yield yieldval + ","
-        curr = post
+    try:
+        yield "["
+        curr = next(variants, None)
         post = next(variants, None)
-    yield "]"
-    return 0
+        while curr is not None:
+            yieldval = json.dumps(curr, default=convert)
+            if post is None:
+                yield yieldval
+                break
+            else:
+                yield yieldval + ","
+            curr = post
+            post = next(variants, None)
+        yield "]"
+        return 0
+    except GeneratorExit:
+        # TODO
+        print("Iterator closed!")
