88edff289fe0a529e1b86d19c2f2241bffd262f3
livovachkov <livovachkov@gmail.com>
2021-09-20 17:03:30 +0300

    GPF-22 Added unit test for people counter

--- gpfjs/src/app/variant-reports/variant-reports.spec.ts
+++ gpfjs/src/app/variant-reports/variant-reports.spec.ts
@@ -1,27 +1,33 @@
-import { ChildrenCounter, GroupCounter } from './variant-reports';
+import {
+  ChildrenCounter,
+  GroupCounter,
+  PeopleCounter,
+} from './variant-reports';
 
-describe('ChildrenCounter', () => {
-  it('should create from json', () => {
+fdescribe('ChildrenCounter', () => {
+  fit('should create from json', () => {
     const childrenCounter = ChildrenCounter.fromJson(
       {
         column: 'fakeColumn',
-        row1: 7
+        row1: 7,
       },
       'row1'
     );
 
-    expect(childrenCounter).toEqual(new ChildrenCounter('row1', 'fakeColumn', 7));
+    expect(childrenCounter).toEqual(
+      new ChildrenCounter('row1', 'fakeColumn', 7)
+    );
   });
 });
 
-describe('GroupCounter', () => {
-  it('should create from json', () => {
+fdescribe('GroupCounter', () => {
+  fit('should create from json', () => {
     const groupCounter = GroupCounter.fromJson(
       {
         column: 'fakeColumn',
         row1: 7,
         row2: 13,
-        row3: 17
+        row3: 17,
       },
       ['row1', 'row2', 'row3']
     );
@@ -33,5 +39,37 @@ describe('GroupCounter', () => {
       new ChildrenCounter('row2', 'fakeColumn', 13),
       new ChildrenCounter('row3', 'fakeColumn', 17),
     ]);
+  });
+});
+
+fdescribe('PeopleCounter', () => {
+  fit('should create from json', () => {
+    const mockPeopleCounter = new PeopleCounter([
+      new GroupCounter('col1', [
+        new ChildrenCounter('row1', 'col1', 7),
+        new ChildrenCounter('row2', 'col1', 13),
+        new ChildrenCounter('row3', 'col1', 17),
+      ]),
+      new GroupCounter('col2', [
+        new ChildrenCounter('row1', 'col2', 15),
+        new ChildrenCounter('row2', 'col2', 666),
+        new ChildrenCounter('row3', 'col2', 42),
+      ]),
+    ],
+      'mock_group',
+      ['row1', 'row2', 'row3'],
+      ['col1', 'col2', 'col3']);
+
+    const peopleCounter = PeopleCounter.fromJson({
+      counters: [
+        { column: 'col1', row1: 7, row2: 13, row3: 17 },
+        { column: 'col2', row1: 15, row2: 666, row3: 42 },
+      ],
+      group_name: 'mock_group',
+      rows: ['row1', 'row2', 'row3'],
+      columns: ['col1', 'col2', 'col3'],
+    });
+
+    expect(peopleCounter).toEqual(mockPeopleCounter);
   });
 });
