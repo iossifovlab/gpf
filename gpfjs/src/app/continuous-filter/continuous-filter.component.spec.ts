7e3f20b85400728017d4b70fe59ba24aa314c7d6
livovachkov <livovachkov@gmail.com>
2021-09-20 13:44:54 +0300

    GPF-22 Added mock service

--- gpfjs/src/app/continuous-filter/continuous-filter.component.spec.ts
+++ gpfjs/src/app/continuous-filter/continuous-filter.component.spec.ts
@@ -1,18 +1,53 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Injectable } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ConfigService } from 'app/config/config.service';
+import { HistogramData } from 'app/measures/measures';
 import { MeasuresService } from 'app/measures/measures.service';
+import * as _ from 'lodash';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs/internal/observable/of';
 
 import { ContinuousFilterComponent } from './continuous-filter.component';
+
+export class MeasuresServiceMock {
+
+  public getContinuousMeasures(
+    datasetId: string
+  ) {
+
+  }
+
+  public getMeasureHistogram(
+    datasetId: string,
+    measureName: string
+  ): Observable<HistogramData> {
+    return of({datasetId: datasetId, measureName: measureName}  as any);
+  }
+
+  public getMeasurePartitions(
+    datasetId: string,
+    measureName: string,
+    rangeStart: number,
+    rangeEnd: number
+  ) {
+
+  }
+
+  public getRegressions(datasetId: string) {
+
+  }
+}
 
 describe('ContinuousFilterComponent', () => {
   let component: ContinuousFilterComponent;
   let fixture: ComponentFixture<ContinuousFilterComponent>;
+  const measuresServiceMock = new MeasuresServiceMock();
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ContinuousFilterComponent],
-      providers: [MeasuresService, ConfigService],
+      providers: [{provide: MeasuresService, useValue: measuresServiceMock}, ConfigService],
       imports: [HttpClientTestingModule]
     })
     .compileComponents();
@@ -27,4 +62,35 @@ describe('ContinuousFilterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  /*it('should set histogram data on changes', () => {
+    component.histogramData = undefined;
+    component.datasetId = undefined;
+    component.measureName = undefined;
+
+    component.ngOnChanges(undefined);
+
+    expect(component.histogramData).toEqual(undefined);
+
+    component.datasetId = 'datasetId';
+    component.measureName = undefined;
+
+    component.ngOnChanges(undefined);
+
+    expect(component.histogramData).toEqual(undefined);
+
+    component.datasetId = undefined;
+    component.measureName = 'measureName';
+
+    component.ngOnChanges(undefined);
+
+    expect(component.histogramData).toEqual(undefined);
+
+    component.datasetId = 'datasetId';
+    component.measureName = 'measureName';
+
+    component.ngOnChanges(undefined);
+
+    expect(component.histogramData).toEqual({datasetId: 'datasetId', measureName: 'measureName'}  as any);
+  });*/
 });
