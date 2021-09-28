bcc59f72723cf27260faaf53b488e1bd4d24559e
joankosev <joankosev@gmail.com>
2021-09-28 13:14:05 +0300

    Add commented logic to slow down cypress
    This logic adds delay between each cypress command,
    helps for debugging in some cases

--- web_e2e/cypress/support/commands.js
+++ web_e2e/cypress/support/commands.js
@@ -23,3 +23,20 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+// add delay between each command
+
+// const COMMAND_DELAY = 500;
+
+// for (const command of ['visit', 'click', 'trigger', 'type', 'clear', 'reload', 'contains']) {
+//     Cypress.Commands.overwrite(command, (originalFn, ...args) => {
+//         const origVal = originalFn(...args);
+
+//         return new Promise((resolve) => {
+//             setTimeout(() => {
+//                 resolve(origVal);
+//             }, COMMAND_DELAY);
+//         });
+//     });
+// } 
