98ccb789125401e55ce73b03ffa2bf14b68465cf
livovachkov <livovachkov@gmail.com>
2021-10-04 09:48:29 +0300

    GPF-1167 e2e tests for phenotype-browser

--- web_e2e/cypress/elements/pheno-browser-page.ts
+++ web_e2e/cypress/elements/pheno-browser-page.ts
@@ -16,4 +16,24 @@ export class PhenoBrowserPage extends BasePage {
   get tableDiv() {
     return cy.get('.col-lg-12');
   }
+
+  get instrumentsBox() {
+    return cy.get('select.form-control');
+  }
+
+  get headerCells() {
+    return this.tableDiv.get('gpf-table-view-header-cell');
+  }
+
+  get tableCells() {
+    return this.tableDiv.get('gpf-table-view-cell');
+  }
+
+  get tableRows() {
+    return this.tableDiv.get('gpf-pheno-browser-table > gpf-table > div > div.ng-star-inserted');
+  }
+
+  get downloadButton() {
+    return cy.get('.col-4 > a');
+  }
 }
--- web_e2e/cypress/integration/pheno-browser-spec.ts
+++ web_e2e/cypress/integration/pheno-browser-spec.ts
@@ -1,3 +1,4 @@
+import { should } from 'chai';
 import { PhenoBrowserPage } from 'cypress/elements/pheno-browser-page';
 import { datasetIds, toolPageLinks } from 'cypress/elements/utils';
 
@@ -23,20 +24,91 @@ describe('Pheno browser tests', () => {
       page.allTableRows.should('have.length', 8)
 
       page.searchInputBox.type(data.seachQuery);
-      page.allTableRows.should('have.length', 1)
+      page.allTableRows.should('have.length', 1);
     });
   });
 
   it('should filter the right rows when typing \'1\' in the sarchbox', () => {
     page.navigateToDatasetPage(datasetIds.compAll, toolPageLinks.phenotypeBrowser);
     page.searchInputBox.type('1');
-    page.allTableRows.should('have.length', 7)
+    page.allTableRows.should('have.length', 7);
   });
 
+
+  it('should test instruments dropdown', () => {
+    page.navigateToDatasetPage(datasetIds.compAll, toolPageLinks.phenotypeBrowser);
+
+    page.instrumentsBox.select('i1');
+    page.allTableRows.should('have.length', 7);
+
+    page.instrumentsBox.select('pheno_common');
+    page.allTableRows.should('have.length', 1);
+
+    page.instrumentsBox.select('All instruments');
+    page.allTableRows.should('have.length', 8);
+  });
+
+  it('should test sort', () => {
+    page.navigateToDatasetPage(datasetIds.compAll, toolPageLinks.phenotypeBrowser);
+    page.headerCells.eq(0).click();
+    page.tableCells.eq(0).should('have.text', 'i1');
+    page.headerCells.eq(0).click();
+    page.tableCells.eq(0).should('have.text', 'pheno_common');
+
+    page.headerCells.eq(1).click();
+    page.tableRows.eq(1).get('gpf-table-view-cell').eq(1).should('have.text', 'age');
+    page.headerCells.eq(1).click();
+    page.tableRows.eq(1).get('gpf-table-view-cell').eq(1).should('have.text', 'sample_id');
+
+    page.headerCells.eq(2).click();
+    page.tableRows.eq(2).get('gpf-table-view-cell').eq(2).should('have.text', '');
+    page.headerCells.eq(2).click();
+    page.tableRows.eq(2).get('gpf-table-view-cell').eq(2).should('have.text', 'The IQ of the individual');
+  });
+
+  it('should test row values', () => {
+    page.navigateToDatasetPage(datasetIds.compAll, toolPageLinks.phenotypeBrowser);
+    let itr = 0;
+    [
+      'i1', 'age', 'The age of the individual (in months)', '', 'continuous [68.001, 606.229] ', '', '    ', '' // The Age PV male female column makes holds empty spans with spaces when text is missing
+    ].forEach(data => {
+      page.tableRows.eq(0).get('gpf-table-view-cell').eq(itr++).should('have.text', data);
+    });
+/*
+    itr = 0;
+    [
+      'i1', 'iq', 'The age of the individual', '', 'continuous [68.001, 606.229] ', '', '    ', '' // The Age PV male female column makes holds empty spans with spaces when text is missing
+    ].forEach(data => {
+      page.tableRows.eq(1).get('gpf-table-view-cell').eq(itr++).should('have.text', data);
+    });
+*/
+  });
+
+  it('should test download', () => {
+    page.navigateToDatasetPage(datasetIds.compAll, toolPageLinks.phenotypeBrowser);
+    const downloadFilePath = Cypress.config('downloadsFolder');
+    const expectedFilePath = 'cypress/fixtures/pheno-browser/';
+
+    page.instrumentsBox.select('All instruments');
+
+    cy.window().document().then(function (doc) {
+      doc.addEventListener('click', () => {
+        setTimeout(function () { doc.location.reload() }, 5000)
+      })
+      page.downloadButton.click();
+    });
+
+    cy.readFile(downloadFilePath + '/instrument.csv', { timeout: 5000 }).then(downloadedFile => {
+      cy.readFile(expectedFilePath + 'instrument_all.csv', { timeout: 5000 }).then(expectedFile => {
+        const downloadedFileLines = downloadedFile.split(/\r\n|\r|\n/);
+        const expectedFileLines = expectedFile.split(/\r\n|\r|\n/);
+        expect(downloadedFileLines).to.deep.eq(expectedFileLines);
+      });
+    });
+
+  });
+
+
   // add tests for:
-  // instruments dropdown
   // icons
-  // download
-  // sort
-  // row values
 });
