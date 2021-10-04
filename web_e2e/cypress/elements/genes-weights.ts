6781cfcfdaca59fa78c40a7710eaff38fabe4972
joankosev <joankosev@gmail.com>
2021-10-04 12:04:34 +0300

    GPF-356 lint

--- web_e2e/cypress/elements/genes-weights.ts
+++ web_e2e/cypress/elements/genes-weights.ts
@@ -2,31 +2,31 @@ import { GenesBlockPage } from './genes-block-page';
 import { BasePage } from './utils';
 
 export class GenesWeights extends GenesBlockPage {
-    get buttonsLowRange() {
-        return cy.get('div.histogram-from > div.stepper > button');
-    }
+  get buttonsLowRange() {
+    return cy.get('div.histogram-from > div.stepper > button');
+  }
 
-    get buttonsHighRange() {
-        return cy.get('div.histogram-to > div.stepper > button');
-    }
+  get buttonsHighRange() {
+    return cy.get('div.histogram-to > div.stepper > button');
+  }
 
-    get fieldLowRange() {
-        return cy.get('div.histogram-from > div.stepper > input');
-    }
+  get fieldLowRange() {
+    return cy.get('div.histogram-from > div.stepper > input');
+  }
 
-    get fieldHighRange() {
-        return cy.get('div.histogram-to > div.stepper > input');
-    }
+  get fieldHighRange() {
+    return cy.get('div.histogram-to > div.stepper > input');
+  }
 
-    get selectField() {
-        return cy.get('gpf-gene-weights select');
-    }
+  get selectField() {
+    return cy.get('gpf-gene-weights select');
+  }
 
-    get sumOfBars() {
-        return cy.get('text#sumOfBarsLabel');
-    }
+  get sumOfBars() {
+    return cy.get('text#sumOfBarsLabel');
+  }
 
-    get graphicsTextArray() {
-        return cy.get('g > text');
-    }
+  get graphicsTextArray() {
+    return cy.get('g > text');
+  }
 }
--- web_e2e/cypress/integration/gene-weights-spec.ts
+++ web_e2e/cypress/integration/gene-weights-spec.ts
@@ -1,78 +1,76 @@
 import { GenesWeights } from 'cypress/elements/genes-weights';
 import { datasetIds, toolPageLinks } from 'cypress/elements/utils';
 
-
 describe('Genes block panel tests', () => {
-    const page = new GenesWeights();
+  const page = new GenesWeights();
+
+  before(() => {
+    page.cleanup();
+    page.navigateToHome();
+    page.loginAdmin();
+  });
   
-    before(() => {
-      page.cleanup();
-      page.navigateToHome();
-      page.loginAdmin();
-    });
+  beforeEach(() => {
+    page.preserveLogin();
+    page.navigateToHome();
+  });
+
+  it('head to the gene weights page', () => {
+    page.navigateToDatasetPage(datasetIds.compAll, toolPageLinks.genotypeBrowser);
+    page.genesWeightsPanel.should('not.exist');
   
-    beforeEach(() => {
-      page.preserveLogin();
-      page.navigateToHome();
+    page.geneWeightsButton.click();
+    page.genesWeightsPanel.should('be.visible');
+  });
+
+  it('should test selector buttons', () => {
+    page.navigateToDatasetPage(datasetIds.compAll, toolPageLinks.genotypeBrowser);
+    page.geneWeightsButton.click();
+
+    page.buttonsLowRange.should('not.exist');
+    page.buttonsHighRange.should('not.exist');
+    page.selectField.contains('ExAC pLI');
+    page.selectField.contains('ExAC pLI rank');
+    page.selectField.contains('RVIS');
+    page.selectField.contains('ExAC pRec');
+
+    page.selectField.select('ExAC pLI');
+
+    page.buttonsLowRange.eq(0).click();
+
+    cy.intercept('POST', '/gpf/api/v3/gene_weights/partitions*', (req) => {
+      expect(req.body).to.deep.equal({
+        "weight": "pLI",
+        "min": 1e-26,
+        "max": 1.01
+      });
+    }).as('getInputValueUp');
+
+    cy.wait('@getInputValueUp');
+
+    page.buttonsLowRange.eq(1).click();
+
+    cy.intercept('POST', '/gpf/api/v3/gene_weights/partitions*', (req) => {
+      expect(req.body).to.deep.equal({
+        "weight": "pLI",
+        "min": 0,
+        "max": 1.01
+      });
     });
-  
-    it('head to the gene weights page', () => {
-        page.navigateToDatasetPage(datasetIds.compAll, toolPageLinks.genotypeBrowser);
-        page.genesWeightsPanel.should('not.exist');
-  
-        page.geneWeightsButton.click();
-        page.genesWeightsPanel.should('be.visible');
-    });
+  });
 
-    it('should test selector buttons', () => {
-        page.navigateToDatasetPage(datasetIds.compAll, toolPageLinks.genotypeBrowser);
-        page.geneWeightsButton.click();
+  it('should test for changes in dropdown', () => {
+    page.navigateToDatasetPage(datasetIds.compAll, toolPageLinks.genotypeBrowser);
+    page.geneWeightsButton.click();
 
-        page.buttonsLowRange.should('not.exist');
-        page.buttonsHighRange.should('not.exist');
-        page.selectField.contains('ExAC pLI');
-        page.selectField.contains('ExAC pLI rank');
-        page.selectField.contains('RVIS');
-        page.selectField.contains('ExAC pRec');
+    page.selectField.select('ExAC pLI');
 
-        page.selectField.select('ExAC pLI');
+    page.sumOfBars.should('have.text', '17993 (100.00%)');
+    page.graphicsTextArray.eq(0).should('have.text', '1e-26');
 
-        page.buttonsLowRange.eq(0).click();
+    page.selectField.select('LGD score');
 
-        cy.intercept('POST', '/gpf/api/v3/gene_weights/partitions*', (req) => {
-            expect(req.body).to.deep.equal({
-                "weight": "pLI",
-                "min": 1e-26,
-                "max": 1.01
-            });
-        }).as('getInputValueUp');
-
-        cy.wait('@getInputValueUp');
-
-        page.buttonsLowRange.eq(1).click();
-
-        cy.intercept('POST', '/gpf/api/v3/gene_weights/partitions*', (req) => {
-            expect(req.body).to.deep.equal({
-                "weight": "pLI",
-                "min": 0,
-                "max": 1.01
-            });
-        });
-    });
-
-    it('should test for changes in dropdown', () => {
-        page.navigateToDatasetPage(datasetIds.compAll, toolPageLinks.genotypeBrowser);
-        page.geneWeightsButton.click();
-
-        page.selectField.select('ExAC pLI');
-
-        page.sumOfBars.should('have.text', '17993 (100.00%)');
-        page.graphicsTextArray.eq(0).should('have.text', '1e-26');
-
-        page.selectField.select('LGD score');
-
-        page.sumOfBars.should('have.text', '18459 (100.00%)');
-        page.graphicsTextArray.eq(0).should('have.text', '0');
-    });
-    
+    page.sumOfBars.should('have.text', '18459 (100.00%)');
+    page.graphicsTextArray.eq(0).should('have.text', '0');
+  });
 });
