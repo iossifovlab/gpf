40d2058d20052b5b9c04c4beaa55104d26348a54
livovachkov <livovachkov@gmail.com>
2021-10-07 16:21:11 +0300

    GPF-1188 Warnings are now surpassed

--- gpfjs/src/app/pheno-tool/pheno-tool.component.spec.ts
+++ gpfjs/src/app/pheno-tool/pheno-tool.component.spec.ts
@@ -7,10 +7,16 @@ import { ConfigService } from 'app/config/config.service';
 import { DatasetsService } from 'app/datasets/datasets.service';
 import { FullscreenLoadingService } from 'app/fullscreen-loading/fullscreen-loading.service';
 import { UsersService } from 'app/users/users.service';
-
 import { PhenoToolComponent } from './pheno-tool.component';
 import { PhenoToolService } from './pheno-tool.service';
 import { ErrorsState } from 'app/common/errors.state';
+import { GenesBlockComponent } from 'app/genes-block/genes-block.component';
+import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { PhenoToolMeasureComponent } from 'app/pheno-tool-measure/pheno-tool-measure.component';
+import { MeasuresService } from 'app/measures/measures.service';
+import { GeneSymbolsComponent } from 'app/gene-symbols/gene-symbols.component';
+import { GeneSymbolsState } from 'app/gene-symbols/gene-symbols.state';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('PhenoToolComponent', () => {
   let component: PhenoToolComponent;
@@ -18,16 +24,21 @@ describe('PhenoToolComponent', () => {
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [ PhenoToolComponent ],
-      providers: [
-        {provide: ActivatedRoute, useValue: new ActivatedRoute()},
-        DatasetsService,
-        ConfigService,
-        UsersService,
-        FullscreenLoadingService,
-        PhenoToolService
+      declarations: [
+        PhenoToolComponent,
+        GenesBlockComponent,
+        GeneSymbolsComponent,
+        PhenoToolMeasureComponent
       ],
-      imports: [HttpClientTestingModule, RouterTestingModule, NgxsModule.forRoot([ErrorsState])]
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: new ActivatedRoute()
+        }, DatasetsService, ConfigService, UsersService, FullscreenLoadingService, PhenoToolService, MeasuresService
+      ],
+      imports: [HttpClientTestingModule, RouterTestingModule,
+        NgxsModule.forRoot([ErrorsState, GeneSymbolsState]), NgbNavModule],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
