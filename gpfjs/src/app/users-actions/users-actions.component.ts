bbdb5ef6bda6b4166f0ac84cbff59456d03cec83
joankosev <joankosev@gmail.com>
2021-10-12 13:23:36 +0300

    GPF-1192 lint

--- gpfjs/src/app/users-actions/users-actions.component.ts
+++ gpfjs/src/app/users-actions/users-actions.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, NgZone, Input, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { take } from 'rxjs/operators';
-
 import { User } from '../users/users';
 import { UsersService } from '../users/users.service';
 
@@ -10,16 +9,16 @@ import { UsersService } from '../users/users.service';
   styleUrls: ['./users-actions.component.css']
 })
 export class UsersActionsComponent implements OnInit, OnDestroy {
-  @Input() user: User;
-  @ViewChild('ele') ele: ElementRef;
-  showDeleteButton = true;
+  @Input() public user: User;
+  @ViewChild('ele') public ele: ElementRef;
+  public showDeleteButton = true;
 
   constructor(
     private zone: NgZone,
     private usersService: UsersService,
   ) { }
 
-  ngOnInit() {
+  public ngOnInit() {
     this.usersService.getUserInfo().pipe(take(1)).subscribe((currentUser) => {
       if (currentUser.email === this.user.email) {
         this.showDeleteButton = false;
@@ -27,35 +26,33 @@ export class UsersActionsComponent implements OnInit, OnDestroy {
     });
   }
 
-  ngOnDestroy(): void {
+  public ngOnDestroy(): void {
     this.closeConfirmnationModal();
   }
 
-  closeConfirmnationModal() {
+  private closeConfirmnationModal(): void {
     this.ele.nativeElement.click();
   }
 
-  deleteUser(user: User) {
-    this.usersService.deleteUser(user).pipe(take(1))
-      .subscribe(() => {
-        this.reloadPage();
-      });
+  public deleteUser(user: User): void {
+    this.usersService.deleteUser(user).pipe(take(1)).subscribe(() => {
+      this.reloadPage();
+    });
   }
 
-  resetPassword(user: User) {
-    this.usersService.resetUserPassword(user).pipe(take(1))
-      .subscribe(() => {
-        this.reloadPage();
-      });
+  public resetPassword(user: User): void {
+    this.usersService.resetUserPassword(user).pipe(take(1)).subscribe(() => {
+      this.reloadPage();
+    });
   }
 
-  reloadPage() {
+  private reloadPage(): void {
     this.zone.runOutsideAngular(() => {
       window.location.reload();
     });
   }
 
-  getUserString(user: User) {
+  private getUserString(user: User): string {
     let user_string = `${user.name || user.email}`
     if (user.name) {
       user_string = `(${user.email}) ` + user_string
@@ -63,12 +60,11 @@ export class UsersActionsComponent implements OnInit, OnDestroy {
     return user_string;
   }
 
-  resetPasswordPopoverMessage(user: User) {
+  public resetPasswordPopoverMessage(user: User): string {
     return `${this.getUserString(user)}'s password will be reset. An email with reset instructions will be sent and they won't be able to login until they set a new password.`
   }
 
-  deleteUserPopoverMessage(user: User) {
+  public deleteUserPopoverMessage(user: User): string {
     return `${this.getUserString(user)} will be deleted. This action is irrevertible!`
   }
-
 }
