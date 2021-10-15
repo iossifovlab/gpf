6ec06b8361b6acf8b1ba9ee2c77cdec23b34eb6e
IvoStefanov <ivostefanovtues@gmail.com>
2021-10-15 10:56:06 +0300

    GPF-1209 refactor pheno tool results tests

--- gpfjs/src/app/pheno-tool/pheno-tool-results.spec.ts
+++ gpfjs/src/app/pheno-tool/pheno-tool-results.spec.ts
@@ -1,5 +1,118 @@
 import { PhenoToolResult, PhenoToolResults, PhenoToolResultsPerEffect, PhenoToolResultsPerGender } from './pheno-tool-results';
 
+const [
+  phenoToolResultsPerGenderJson1,
+  phenoToolResultsPerGenderJson2,
+  phenoToolResultsPerGenderJson3,
+  phenoToolResultsPerGenderJson4
+] = [
+  {
+    positive: {
+      count: 1,
+      deviation: 2,
+      mean: 3
+    }, negative: {
+      count: 4,
+      deviation: 5,
+      mean: 6
+    }, pValue: 7
+  },
+  {
+    positive: {
+      count: 8,
+      deviation: 9,
+      mean: 10
+    }, negative: {
+      count: 11,
+      deviation: 12,
+      mean: 13
+    }, pValue: 14
+  },
+  {
+    positive: {
+      count: 15,
+      deviation: 16,
+      mean: 17
+    }, negative: {
+      count: 18,
+      deviation: 19,
+      mean: 20
+      }, pValue: 21
+  },
+  {
+    positive: {
+      count: 22,
+      deviation: 23,
+      mean: 24
+    }, negative: {
+      count: 25,
+      deviation: 26,
+      mean: 27
+    }, pValue: 28
+  }
+];
+
+const [
+  phenoToolResultsPerGender1,
+  phenoToolResultsPerGender2,
+  phenoToolResultsPerGender3,
+  phenoToolResultsPerGender4
+] = [
+  new PhenoToolResultsPerGender(
+    new PhenoToolResult(1, 2, 3),
+    new PhenoToolResult(4, 5, 6), 7
+  ),
+  new PhenoToolResultsPerGender(
+    new PhenoToolResult(8, 9, 10),
+    new PhenoToolResult(11, 12, 13), 14
+  ),
+  new PhenoToolResultsPerGender(
+    new PhenoToolResult(15, 16, 17),
+    new PhenoToolResult(18, 19, 20), 21
+  ),
+  new PhenoToolResultsPerGender(
+    new PhenoToolResult(22, 23, 24),
+    new PhenoToolResult(25, 26, 27), 28
+  )
+];
+
+const phenoToolEffectArrayJson1 = [
+  {
+    effect: 'effectType1',
+    maleResults: phenoToolResultsPerGenderJson1,
+    femaleResults: phenoToolResultsPerGenderJson2
+  }, {
+    effect: 'effectType2',
+    maleResults: phenoToolResultsPerGenderJson3,
+    femaleResults: phenoToolResultsPerGenderJson4
+  }
+];
+
+const phenoToolEffectArray1 = [
+  new PhenoToolResultsPerEffect(
+    'effectType1',
+    phenoToolResultsPerGender1,
+    phenoToolResultsPerGender2
+  ),
+  new PhenoToolResultsPerEffect(
+    'effectType2',
+    phenoToolResultsPerGender3,
+    phenoToolResultsPerGender4
+  )
+];
+
+const phenoToolResultsPerEffectJson1 = {
+  effect: 'effectType1',
+  maleResults: phenoToolResultsPerGenderJson1,
+  femaleResults: phenoToolResultsPerGenderJson2
+};
+
+const phenoToolResultsPerEffect1 = new PhenoToolResultsPerEffect(
+  'effectType1',
+  phenoToolResultsPerGender1,
+  phenoToolResultsPerGender2
+);
+
 describe('PhenoToolResult', () => {
   it('should create from json', () => {
     const phenoToolFromJson = PhenoToolResult.fromJson({
@@ -11,7 +124,7 @@ describe('PhenoToolResult', () => {
   });
 
   it('should get range start', () => {
-    expect(new PhenoToolResult(5, 3, 4).rangeStart).toEqual(1);
+    expect(new PhenoToolResult(6, 3, 4).rangeStart).toEqual(1);
   });
 
   it('should get range end', () => {
@@ -21,202 +134,29 @@ describe('PhenoToolResult', () => {
 
 describe('PhenoToolResultsPerGender', () => {
   it('should create from json', () => {
-    const phenoToolMockPositive = new PhenoToolResult(3, 8, 5);
-
-    const phenoToolResultsPerGender = new PhenoToolResultsPerGender(
-      phenoToolMockPositive, new PhenoToolResult(5, 3, 4), 5
-    );
-
-    const phenoToolResultsPerGenderFromJson = PhenoToolResultsPerGender.fromJson({
-      positive: {
-        count: 3,
-        deviation: 8,
-        mean: 5
-      }, negative: {
-        count: 5,
-        deviation: 3,
-        mean: 4
-      }, pValue: 5
-    });
-
-    expect(phenoToolResultsPerGender).toEqual(phenoToolResultsPerGenderFromJson);
+    const phenoToolResultsPerGenderFromJson = PhenoToolResultsPerGender.fromJson(phenoToolResultsPerGenderJson1);
+    expect(phenoToolResultsPerGender1).toEqual(phenoToolResultsPerGenderFromJson);
   });
 });
 
 describe('PhenoToolResultsPerEffect', () => {
   it('should create from json', () => {
-    const phenoToolResultsPerEffect = new PhenoToolResultsPerEffect(
-      'effectType1',
-      new PhenoToolResultsPerGender(new PhenoToolResult(3, 8, 5), new PhenoToolResult(5, 3, 4), 5),
-      new PhenoToolResultsPerGender(new PhenoToolResult(7, 9, 1), new PhenoToolResult(0, -1, 2), 7)
-    );
-
-    const phenoToolResultsPerEffectFromJson = PhenoToolResultsPerEffect.fromJson({
-      effect: 'effectType1',
-      maleResults: {
-        positive: {
-          count: 3,
-          deviation: 8,
-          mean: 5
-        }, negative: {
-          count: 5,
-          deviation: 3,
-          mean: 4
-          }, pValue: 5
-      }, femaleResults: {
-        positive: {
-          count: 7,
-          deviation: 9,
-          mean: 1
-        }, negative: {
-          count: 0,
-          deviation: -1,
-          mean: 2
-        }, pValue: 7
-      }
-    });
-
-    expect(phenoToolResultsPerEffect).toEqual(phenoToolResultsPerEffectFromJson);
+    const phenoToolResultsPerEffectFromJson = PhenoToolResultsPerEffect.fromJson(phenoToolResultsPerEffectJson1);
+    expect(phenoToolResultsPerEffect1).toEqual(phenoToolResultsPerEffectFromJson);
   });
 
-    it('should create from json array', () => {
-      const phenoToolEffectArrayMock =
-      [
-        new PhenoToolResultsPerEffect(
-          'effectType1',
-          new PhenoToolResultsPerGender(new PhenoToolResult(3, 8, 5), new PhenoToolResult(5, 3, 4), 5),
-          new PhenoToolResultsPerGender(new PhenoToolResult(7, 9, 1), new PhenoToolResult(0, -1, 2), 7)
-        ),
-        new PhenoToolResultsPerEffect(
-          'effectType2',
-          new PhenoToolResultsPerGender(new PhenoToolResult(7, 9, 2), new PhenoToolResult(3, 7, 2), 3),
-          new PhenoToolResultsPerGender(new PhenoToolResult(1, 2, 3), new PhenoToolResult(9, 0, 4), 8)
-        )
-      ];
-
-      const phenoToolEffectFromJsonArrayMock = PhenoToolResultsPerEffect.fromJsonArray([
-        {
-          effect: 'effectType1',
-          maleResults: {
-            positive: {
-              count: 3,
-              deviation: 8,
-              mean: 5
-            }, negative: {
-              count: 5,
-              deviation: 3,
-              mean: 4
-              }, pValue: 5
-          }, femaleResults: {
-            positive: {
-              count: 7,
-              deviation: 9,
-              mean: 1
-            }, negative: {
-              count: 0,
-              deviation: -1,
-              mean: 2
-            }, pValue: 7
-          }
-        }, {
-          effect: 'effectType2',
-          maleResults: {
-            positive: {
-              count: 7,
-              deviation: 9,
-              mean: 2
-            }, negative: {
-              count: 3,
-              deviation: 7,
-              mean: 2
-              }, pValue: 3
-          }, femaleResults: {
-            positive: {
-              count: 1,
-              deviation: 2,
-              mean: 3
-            }, negative: {
-              count: 9,
-              deviation: 0,
-              mean: 4
-            }, pValue: 8
-          }
-        }
-      ]);
-
-      expect(phenoToolEffectArrayMock).toEqual(phenoToolEffectFromJsonArrayMock);
-    });
+  it('should create from json array', () => {
+    const phenoToolEffectFromJsonArrayMock = PhenoToolResultsPerEffect.fromJsonArray(phenoToolEffectArrayJson1);
+    expect(phenoToolEffectArray1).toEqual(phenoToolEffectFromJsonArrayMock);
+  });
 });
 
 describe('PhenoToolResults', () => {
   it('should create from json', () => {
-    const phenoResultsMock = new PhenoToolResults(
-      'desc1',
-      [
-        new PhenoToolResultsPerEffect(
-          'effectType1',
-          new PhenoToolResultsPerGender(new PhenoToolResult(3, 8, 5), new PhenoToolResult(5, 3, 4), 5),
-          new PhenoToolResultsPerGender(new PhenoToolResult(7, 9, 1), new PhenoToolResult(0, -1, 2), 7)
-        ),
-        new PhenoToolResultsPerEffect(
-          'effectType2',
-          new PhenoToolResultsPerGender(new PhenoToolResult(7, 9, 2), new PhenoToolResult(3, 7, 2), 3),
-          new PhenoToolResultsPerGender(new PhenoToolResult(1, 2, 3), new PhenoToolResult(9, 0, 4), 8)
-        )
-      ]
-    );
-
+    const phenoResultsMock = new PhenoToolResults('desc1', phenoToolEffectArray1);
     const phenoResultsMockFromJson = PhenoToolResults.fromJson({
       description: 'desc1',
-      results: [
-        {
-          effect: 'effectType1',
-          maleResults: {
-            positive: {
-              count: 3,
-              deviation: 8,
-              mean: 5
-            }, negative: {
-              count: 5,
-              deviation: 3,
-              mean: 4
-              }, pValue: 5
-          }, femaleResults: {
-            positive: {
-              count: 7,
-              deviation: 9,
-              mean: 1
-            }, negative: {
-              count: 0,
-              deviation: -1,
-              mean: 2
-            }, pValue: 7
-          }
-        }, {
-          effect: 'effectType2',
-          maleResults: {
-            positive: {
-              count: 7,
-              deviation: 9,
-              mean: 2
-            }, negative: {
-              count: 3,
-              deviation: 7,
-              mean: 2
-              }, pValue: 3
-          }, femaleResults: {
-            positive: {
-              count: 1,
-              deviation: 2,
-              mean: 3
-            }, negative: {
-              count: 9,
-              deviation: 0,
-              mean: 4
-            }, pValue: 8
-          }
-        }
-      ]
+      results: phenoToolEffectArrayJson1
     });
 
     expect(phenoResultsMock).toEqual(phenoResultsMockFromJson);
