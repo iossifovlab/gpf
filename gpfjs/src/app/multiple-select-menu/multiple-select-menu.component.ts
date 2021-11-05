8d24310f3098e19c7771ab79359ebbe6d2a2d6c7
joankosev <joankosev@gmail.com>
2021-11-05 13:34:45 +0200

    GPF-1273 lint multiple-select-menu

--- gpfjs/src/app/multiple-select-menu/multiple-select-menu.component.ts
+++ gpfjs/src/app/multiple-select-menu/multiple-select-menu.component.ts
@@ -1,34 +1,37 @@
 import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
-import { ChangeDetectionStrategy, Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, ViewChild } from '@angular/core';
-import {cloneDeep} from 'lodash';
+import { Component, ElementRef, EventEmitter, Input, OnChanges, OnInit, Output, ViewChild } from '@angular/core';
+import { cloneDeep } from 'lodash';
+
+interface MenuItem {
+  id: string;
+  isChecked: boolean;
+}
 
 @Component({
   selector: 'gpf-multiple-select-menu',
   templateUrl: './multiple-select-menu.component.html',
-  styleUrls: ['./multiple-select-menu.component.css'],
+  styleUrls: ['./multiple-select-menu.component.css']
 })
 export class MultipleSelectMenuComponent implements OnInit, OnChanges {
-  @Input() menuId: string;
-  @Input() selectedItems: string[];
-  @Input() allItems: string[];
-  @Input() readonly minSelectCount = 0;
-  @Output() applyEvent = new EventEmitter<{menuId: string, data: string[]}>();
-  @ViewChild('searchInput') searchInput: ElementRef;
+  @Input() public menuId: string;
+  @Input() public selectedItems: string[];
+  @Input() public allItems: string[];
+  @Input() public readonly minSelectCount = 0;
+  @Output() public applyEvent = new EventEmitter<{menuId: string, data: string[]}>();
+  @ViewChild('searchInput') public searchInput: ElementRef;
 
-  checkUncheckAllButtonName = 'Uncheck all';
-  searchText: String;
-  checkboxDataArray: {id: string; isChecked: boolean}[];
-  checkboxDataArraySavedState: {id: string; isChecked: boolean}[];
+  public checkUncheckAllButtonName = 'Uncheck all';
+  public searchText: string;
+  public checkboxDataArray: MenuItem[];
+  private checkboxDataArraySavedState: MenuItem[];
 
-  constructor() { }
-
-  ngOnChanges(): void {
+  public ngOnChanges(): void {
     this.checkboxDataArraySavedState = this.toCheckboxDataArray(this.allItems);
     this.applySavedState();
     this.searchText = '';
   }
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.checkboxDataArraySavedState = this.toCheckboxDataArray(this.allItems);
     this.applySavedState();
 
@@ -37,39 +40,25 @@ export class MultipleSelectMenuComponent implements OnInit, OnChanges {
     }
   }
 
-  toCheckboxDataArray(allItems: string[]) {
-    return allItems.map(
-      item => ({id: item, isChecked: this.selectedItems.includes(item)})
-    );
-  }
-
-  applySavedState() {
-    this.checkboxDataArray = cloneDeep(this.checkboxDataArraySavedState);
-  }
-
-  updateSavedState() {
-    this.checkboxDataArraySavedState = cloneDeep(this.checkboxDataArray);
-  }
-
-  areAllUnchecked(checkboxDataArray): boolean {
-    return !checkboxDataArray.map(item => item.isChecked).includes(true);
-  }
-
-  toggleCheckingAll() {
+  public toggleCheckingAll(): void {
     if (this.checkUncheckAllButtonName === 'Uncheck all') {
-      this.checkboxDataArray.forEach(item => item.isChecked = false);
+      this.checkboxDataArray.forEach(item => {
+        item.isChecked = false;
+      });
       this.checkUncheckAllButtonName = 'Check all';
     } else if (this.checkUncheckAllButtonName === 'Check all') {
-      this.checkboxDataArray.forEach(item => item.isChecked = true);
+      this.checkboxDataArray.forEach(item => {
+        item.isChecked = true;
+      });
       this.checkUncheckAllButtonName = 'Uncheck all';
     }
   }
 
-  isSelectionValid() {
+  public isSelectionValid(): boolean {
     return this.checkboxDataArray.filter(item => item.isChecked === true).length >= this.minSelectCount;
   }
 
-  apply() {
+  public apply(): void {
     this.updateSavedState();
 
     this.applyEvent.emit({
@@ -78,10 +67,32 @@ export class MultipleSelectMenuComponent implements OnInit, OnChanges {
     });
   }
 
-  /**
-  * Waits search input element to load.
-  * @returns promise
-  */
+  public focusSearchInput(): void {
+    this.waitForSearchInputToLoad().then(() => {
+      this.searchInput.nativeElement.focus();
+    });
+  }
+
+  public drop(event: CdkDragDrop<string[]>): void {
+    moveItemInArray(this.checkboxDataArray, event.previousIndex, event.currentIndex);
+  }
+
+  private toCheckboxDataArray(allItems: string[]): MenuItem[] {
+    return allItems.map(item => ({ id: item, isChecked: this.selectedItems.includes(item) }));
+  }
+
+  private applySavedState(): void {
+    this.checkboxDataArray = cloneDeep(this.checkboxDataArraySavedState);
+  }
+
+  private updateSavedState(): void {
+    this.checkboxDataArraySavedState = cloneDeep(this.checkboxDataArray);
+  }
+
+  private areAllUnchecked(checkboxDataArray: MenuItem[]): boolean {
+    return !checkboxDataArray.map(item => item.isChecked).includes(true);
+  }
+
   private async waitForSearchInputToLoad(): Promise<void> {
     return new Promise<void>(resolve => {
       const timer = setInterval(() => {
@@ -91,18 +102,5 @@ export class MultipleSelectMenuComponent implements OnInit, OnChanges {
         }
       }, 100);
     });
-  }
-
-  focusSearchInput() {
-    this.waitForSearchInputToLoad().then(() => {
-      this.searchInput.nativeElement.focus();
-    });
-  }
-
-  drop(event: CdkDragDrop<string[]>) {
-    console.log(this.checkboxDataArray);
-    console.log(event);
-    moveItemInArray(this.checkboxDataArray, event.previousIndex, event.currentIndex);
-    console.log(this.checkboxDataArray);
   }
 }
