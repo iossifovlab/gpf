6a4187e563aa7aa6196a11df9e9c24f4670f4431
IvoTod <ivo.tod.todorov@gmail.com>
2021-11-08 08:51:12 +0200

    GPF-1274 Add new aggregators and tests

--- dae/dae/genomic_resources/aggregators.py
+++ dae/dae/genomic_resources/aggregators.py
@@ -1,15 +1,37 @@
+import math
+
+
 class AbstractAggregator:
+    def __init__(self):
+        self.total_count = 0
+        self.used_count = 0
+
     def __call__(self):
         return self.get_final()
 
     def add(self, v):
+        self.total_count += 1
+        self._add_internal(v)
+
+    def _add_internal(self, v):
         raise NotImplementedError()
 
     def clear(self):
+        self.total_count = 0
+        self.used_count = 0
+        self._clear_internal()
+
+    def _clear_internal(self):
         raise NotImplementedError()
 
     def get_final(self):
         raise NotImplementedError()
+
+    def get_total_count(self):
+        return self.total_count
+
+    def get_used_count(self):
+        return self.used_count
 
     def __eq__(self, o: object) -> bool:
         return self.get_final() == o
@@ -17,15 +39,20 @@ class AbstractAggregator:
 
 class MaxAggregator(AbstractAggregator):
     def __init__(self):
+        super().__init__()
         self.current_max = None
 
-    def add(self, v):
-        if self.current_max is not None and v is not None:
+    def _add_internal(self, v):
+        if v is None:
+            return
+        if self.current_max is not None:
             self.current_max = max(self.current_max, v)
-        elif v is not None:
+        else:
             self.current_max = v
 
-    def clear(self):
+        self.used_count += 1
+
+    def _clear_internal(self):
         self.current_max = None
 
     def get_final(self):
@@ -34,15 +61,20 @@ class MaxAggregator(AbstractAggregator):
 
 class MinAggregator(AbstractAggregator):
     def __init__(self):
+        super().__init__()
         self.current_min = None
 
-    def add(self, v):
-        if self.current_min is not None and v is not None:
+    def _add_internal(self, v):
+        if v is None:
+            return
+        if self.current_min is not None:
             self.current_min = min(self.current_min, v)
-        elif v is not None:
+        else:
             self.current_min = v
 
-    def clear(self):
+        self.used_count += 1
+
+    def _clear_internal(self):
         self.current_min = None
 
     def get_final(self):
@@ -51,33 +83,36 @@ class MinAggregator(AbstractAggregator):
 
 class MeanAggregator(AbstractAggregator):
     def __init__(self):
+        super().__init__()
         self.sum = 0
-        self.count = 0
 
-    def add(self, v):
-        if v is not None:
-            self.sum += v
-            self.count += 1
+    def _add_internal(self, v):
+        if v is None:
+            return
 
-    def clear(self):
+        self.sum += v
+        self.used_count += 1
+
+    def _clear_internal(self):
         self.sum = 0
-        self.count = 0
 
     def get_final(self):
-        if self.count:
-            return self.sum / self.count
+        if self.used_count > 0:
+            return self.sum / self.used_count
         return None
 
 
 class ConcatAggregator(AbstractAggregator):
     def __init__(self):
+        super().__init__()
         self.out = ""
 
-    def add(self, v):
-        if v:
-            self.out += v
+    def _add_internal(self, v):
+        if v is not None:
+            self.out += str(v)
+            self.used_count += 1
 
-    def clear(self):
+    def _clear_internal(self):
         self.out = ""
 
     def get_final(self):
@@ -85,3 +120,56 @@ class ConcatAggregator(AbstractAggregator):
             return None
 
         return self.out
+
+
+class MedianAggregator(AbstractAggregator):
+    def __init__(self):
+        super().__init__()
+        self.values = list()
+
+    def _add_internal(self, v):
+        if v is not None:
+            self.values.append(v)
+            self.used_count += 1
+
+    def _clear_internal(self):
+        self.values.clear()
+
+    def get_final(self):
+        self.values.sort()
+        print(self.values)
+        if len(self.values) % 2 == 1:
+            return self.values[math.floor(len(self.values)/2)]
+        else:
+            first = self.values[int(len(self.values)/2)-1]
+            second = self.values[int(len(self.values)/2)]
+            if isinstance(first, str):
+                assert isinstance(second, str)
+                return first + second
+            else:
+                return (first + second) / 2
+
+
+class ModeAggregator(AbstractAggregator):
+    def __init__(self):
+        super().__init__()
+        self.value_counts = dict()
+
+    def _add_internal(self, v):
+        if v is not None:
+            if v not in self.value_counts:
+                self.value_counts[v] = 0
+            self.value_counts[v] += 1
+            self.used_count += 1
+
+    def _clear_internal(self):
+        self.value_counts.clear()
+
+    def get_final(self):
+        current_max = None
+        for value, count in self.value_counts.items():
+            if current_max is None:
+                current_max = value
+            elif self.value_counts[current_max] < self.value_counts[value]:
+                current_max = value
+        return current_max
---
+++ dae/dae/genomic_resources/tests/test_aggregators.py
@@ -0,0 +1,132 @@
+import numpy
+from dae.genomic_resources.aggregators import (
+    ConcatAggregator, MinAggregator,
+    MaxAggregator, MeanAggregator, ModeAggregator, MedianAggregator
+)
+
+
+def test_concat_aggregator():
+    values = ["a", "b", "c", "d"]
+    agg = ConcatAggregator()
+    for v in values:
+        agg.add(v)
+
+    assert agg.get_final() == "abcd"
+
+
+def test_min_aggregator():
+    values = [5, 6, 1, 2, 7]
+    agg = MinAggregator()
+    for v in values:
+        agg.add(v)
+
+    assert agg.get_final() == 1
+
+
+def test_min_aggregator_string():
+    values = ["asdf", "ghjk", "zab", "zob"]
+    agg = MinAggregator()
+    for v in values:
+        agg.add(v)
+
+    assert agg.get_final() == "asdf"
+
+
+def test_max_aggregator():
+    values = [5, 6, 1, 2, 7]
+    agg = MaxAggregator()
+    for v in values:
+        agg.add(v)
+
+    assert agg.get_final() == 7
+
+
+def test_max_aggregator_string():
+    values = ["asdf", "ghjk", "zab", "zob"]
+    agg = MaxAggregator()
+    for v in values:
+        agg.add(v)
+
+    assert agg.get_final() == "zob"
+
+
+def test_mean_aggregator():
+    values = [1, 2, 3, 4]
+    agg = MeanAggregator()
+    for v in values:
+        agg.add(v)
+
+    assert numpy.isclose(agg.get_final(), 2.5)
+
+
+def test_median_aggregator_even():
+    values = [2, 3, 4, 1]
+    agg = MedianAggregator()
+    for v in values:
+        agg.add(v)
+
+    assert numpy.isclose(agg.get_final(), 2.5)
+
+
+def test_median_aggregator_odd():
+    values = [2, 3, 4, 1, 5]
+    agg = MedianAggregator()
+    for v in values:
+        agg.add(v)
+
+    assert agg.get_final() == 3
+
+
+def test_median_aggregator_string_even():
+    values = ["a", "c", "b", "d"]
+    agg = MedianAggregator()
+    for v in values:
+        agg.add(v)
+
+    assert agg.get_final() == "bc"
+
+
+def test_median_aggregator_string_odd():
+    values = ["a", "c", "b", "d", "f"]
+    agg = MedianAggregator()
+    for v in values:
+        agg.add(v)
+
+    assert agg.get_final() == "c"
+
+
+def test_mode_aggregator():
+    values = [1, 2, 3, 1, 5, 6, 6, 1]
+    agg = ModeAggregator()
+    for v in values:
+        agg.add(v)
+
+    assert agg.get_final() == 1
+
+
+def test_mode_aggregator_multimode():
+    values = [6, 2, 3, 6, 5, 1, 1, 6, 1]
+    agg = ModeAggregator()
+    for v in values:
+        agg.add(v)
+
+    assert agg.get_final() == 6
+
+
+def test_aggregator_used_counts():
+    values = [1, 2, 3, None, None, 4, 5]
+    agg = MinAggregator()
+    for v in values:
+        agg.add(v)
+
+    assert agg.get_used_count() == 5
+    assert agg.get_total_count() == 7
+    agg.clear()
+    assert agg.get_used_count() == 0
+    assert agg.get_total_count() == 0
+
+    agg.add("asdf")
+    agg.add("ghjk")
+
+    assert agg.get_used_count() == 2
+    assert agg.get_total_count() == 2
