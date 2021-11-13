8a0c7a813d627e60f8e07dc36334710e0dde54a8
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2021-11-13 09:51:53 +0200

    Bump version: 3.5.5 → 3.5.6

--- dae/setup.py
+++ dae/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gpf_dae",
-    version="3.5.5",
+    version="3.5.6",
     author="Lubomir Chorbadjiev",
     author_email="lubomir.chorbadjiev@gmail.com",
     description="GPF: Genotypes and Phenotypes in Families",
