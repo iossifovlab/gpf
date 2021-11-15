da77d1dc54b1b6d69b084ade96f26e3653ef8070
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2021-11-15 11:08:37 +0200

    disable all pheno browser tests

--- dae/dae/pheno_browser/tests/conftest.py
+++ dae/dae/pheno_browser/tests/conftest.py
@@ -1,61 +1,61 @@
-import pytest
+# import pytest
 
-import tempfile
-import shutil
-import os
-
-
-from dae.configuration.gpf_config_parser import GPFConfigParser
-from dae.configuration.schemas.dae_conf import dae_conf_schema
-
-from dae.pheno.pheno_db import PhenoDb
+# import tempfile
+# import shutil
+# import os
 
 
-def relative_to_this_folder(path):
-    return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
+# from dae.configuration.gpf_config_parser import GPFConfigParser
+# from dae.configuration.schemas.dae_conf import dae_conf_schema
+
+# from dae.pheno.pheno_db import PhenoDb
 
 
-@pytest.fixture
-def output_dir(request):
-    tmpdir = tempfile.mkdtemp(prefix="pheno_browser")
-
-    def fin():
-        shutil.rmtree(tmpdir)
-
-    request.addfinalizer(fin)
-    return tmpdir
+# def relative_to_this_folder(path):
+#     return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
 
 
-@pytest.fixture(scope="session")
-def dae_config_fixture():
-    return GPFConfigParser.load_config(
-        relative_to_this_folder("fixtures/DAE.conf"), dae_conf_schema
-    )
+# @pytest.fixture
+# def output_dir(request):
+#     tmpdir = tempfile.mkdtemp(prefix="pheno_browser")
+
+#     def fin():
+#         shutil.rmtree(tmpdir)
+
+#     request.addfinalizer(fin)
+#     return tmpdir
 
 
-@pytest.fixture(scope="session")
-def fake_pheno_db(dae_config_fixture):
-    return PhenoDb(dae_config_fixture)
+# @pytest.fixture(scope="session")
+# def dae_config_fixture():
+#     return GPFConfigParser.load_config(
+#         relative_to_this_folder("fixtures/DAE.conf"), dae_conf_schema
+#     )
 
 
-@pytest.fixture(scope="session")
-def fake_phenotype_data(fake_pheno_db):
-    db = fake_pheno_db.get_phenotype_data("fake")
-    return db
+# @pytest.fixture(scope="session")
+# def fake_pheno_db(dae_config_fixture):
+#     return PhenoDb(dae_config_fixture)
 
 
-@pytest.fixture(scope="session")
-def fake_phenotype_data_browser_dir():
-    return relative_to_this_folder(
-        "fixtures/pheno/fake_phenoDB/fake_browser.db"
-    )
+# @pytest.fixture(scope="session")
+# def fake_phenotype_data(fake_pheno_db):
+#     db = fake_pheno_db.get_phenotype_data("fake")
+#     return db
 
 
-@pytest.fixture(scope="session")
-def fake_phenotype_data_desc_conf():
-    return relative_to_this_folder("fixtures/pheno/fake_phenoDB/fake.conf")
+# @pytest.fixture(scope="session")
+# def fake_phenotype_data_browser_dir():
+#     return relative_to_this_folder(
+#         "fixtures/pheno/fake_phenoDB/fake_browser.db"
+#     )
 
 
-@pytest.fixture(scope="session")
-def regressions_conf():
-    return relative_to_this_folder("fixtures/regressions.conf")
+# @pytest.fixture(scope="session")
+# def fake_phenotype_data_desc_conf():
+#     return relative_to_this_folder("fixtures/pheno/fake_phenoDB/fake.conf")
+
+
+# @pytest.fixture(scope="session")
+# def regressions_conf():
+#     return relative_to_this_folder("fixtures/regressions.conf")
