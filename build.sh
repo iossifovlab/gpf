8f00fc25b84526f0f44a0d57550435e822499b48
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2021-11-17 12:44:45 +0200

    GPF-1276 reorganize build.sh

--- build.sh
+++ build.sh
@@ -35,6 +35,7 @@ function main() {
   libmain_init_build_env \
     clobber:"$clobber" preset:"$preset" build_no:"$build_no" generate_jenkins_init:"$generate_jenkins_init" expose_ports:"$expose_ports" \
     seqpipe.data-hg19-startup
+
   libmain_save_build_env_on_exit
   libbuild_init stage:"$stage" registry.seqpipe.org
 
