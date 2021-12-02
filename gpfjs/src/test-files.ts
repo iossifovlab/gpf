d7fac558eb1d2e089839f0466ad9389d3dd1ab11
joankosev <joankosev@gmail.com>
2021-12-02 11:43:39 +0200

    GPF-1317 add string split pipe

--- gpfjs/src/test-files.ts
+++ gpfjs/src/test-files.ts
@@ -105,3 +105,4 @@ import './app/pheno-tool/pheno-tool-results.spec';
 import './app/pheno-tool/pheno-tool.service.spec';
 import './app/enrichment-query/enrichment-result.spec';
 import './app/genotype-browser/genotype-browser.spec';
+import './app/utils/split.pipe';
