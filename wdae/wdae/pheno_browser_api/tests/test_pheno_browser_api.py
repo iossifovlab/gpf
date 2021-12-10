0e6bb56251daf900f74fbd2e3271a693eb21b1e1
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2021-12-10 14:08:18 +0200

    GPF-1323 restore wdae pheno tool and pheno browser tests

--- wdae/wdae/pheno_browser_api/tests/test_pheno_browser_api.py
+++ wdae/wdae/pheno_browser_api/tests/test_pheno_browser_api.py
@@ -1,151 +1,151 @@
-# import pytest
-# import json
+import pytest
+import json
 
 
-# pytestmark = pytest.mark.usefixtures(
-#     "wdae_gpf_instance", "dae_calc_gene_sets")
+pytestmark = pytest.mark.usefixtures(
+    "wdae_gpf_instance", "dae_calc_gene_sets")
 
 
-# URL = "/api/v3/pheno_browser/instruments"
-# MEASURES_URL = "/api/v3/pheno_browser/measures"
-# MEASURES_INFO_URL = "/api/v3/pheno_browser/measures_info"
-# MEASURE_DESCRIPTION_URL = "/api/v3/pheno_browser/measure_description"
-# DOWNLOAD_URL = "/api/v3/pheno_browser/download"
+URL = "/api/v3/pheno_browser/instruments"
+MEASURES_URL = "/api/v3/pheno_browser/measures"
+MEASURES_INFO_URL = "/api/v3/pheno_browser/measures_info"
+MEASURE_DESCRIPTION_URL = "/api/v3/pheno_browser/measure_description"
+DOWNLOAD_URL = "/api/v3/pheno_browser/download"
 
 
-# def test_instruments_missing_dataset_id(admin_client):
-#     response = admin_client.get(URL)
+def test_instruments_missing_dataset_id(admin_client):
+    response = admin_client.get(URL)
 
-#     assert response.status_code == 400
-
-
-# def test_instruments_missing_dataset_id_forbidden(user_client):
-#     response = user_client.get(URL)
-
-#     assert response.status_code == 400
+    assert response.status_code == 400
 
 
-# def test_instruments(admin_client):
-#     url = "{}?dataset_id=quads_f1_ds".format(URL)
-#     response = admin_client.get(url)
+def test_instruments_missing_dataset_id_forbidden(user_client):
+    response = user_client.get(URL)
 
-#     assert response.status_code == 200
-#     assert "default" in response.data
-#     assert "instruments" in response.data
-#     assert len(response.data["instruments"]) == 1
+    assert response.status_code == 400
 
 
-# def test_instruments_forbidden(user_client):
-#     url = "{}?dataset_id=quads_f1_ds".format(URL)
-#     response = user_client.get(url)
+def test_instruments(admin_client):
+    url = "{}?dataset_id=quads_f1_ds".format(URL)
+    response = admin_client.get(url)
 
-#     assert response.status_code == 403
-
-#     header = response.data
-#     assert len(header.keys()) == 1
-#     assert (
-#         header["detail"]
-#         == "You do not have permission to perform this action."
-#     )
+    assert response.status_code == 200
+    assert "default" in response.data
+    assert "instruments" in response.data
+    assert len(response.data["instruments"]) == 1
 
 
-# def test_measures_info(admin_client):
-#     url = f"{MEASURES_INFO_URL}?dataset_id=quads_f1_ds"
-#     response = admin_client.get(url)
+def test_instruments_forbidden(user_client):
+    url = "{}?dataset_id=quads_f1_ds".format(URL)
+    response = user_client.get(url)
 
-#     assert response.status_code == 200
-#     assert "base_image_url" in response.data
-#     assert "has_descriptions" in response.data
+    assert response.status_code == 403
 
-
-# def test_measures(admin_client):
-#     url = "{}?dataset_id=quads_f1_ds&instrument=instrument1".format(
-#         MEASURES_URL
-#     )
-#     response = admin_client.get(url)
-#     assert response.status_code == 200
-
-#     res = response.streaming_content
-#     res = json.loads("".join(map(lambda x: x.decode("utf-8"), res)))
-#     assert len(res) == 4
+    header = response.data
+    assert len(header.keys()) == 1
+    assert (
+        header["detail"]
+        == "You do not have permission to perform this action."
+    )
 
 
-# def test_measures_forbidden(user_client, user):
-#     print(user.groups.all())
-#     url = "{}?dataset_id=quads_f1_ds&instrument=instrument1".format(
-#         MEASURES_URL
-#     )
-#     response = user_client.get(url)
+def test_measures_info(admin_client):
+    url = f"{MEASURES_INFO_URL}?dataset_id=quads_f1_ds"
+    response = admin_client.get(url)
 
-#     assert response.status_code == 403
-
-#     header = response.data
-#     assert len(header.keys()) == 1
-#     assert (
-#         header["detail"]
-#         == "You do not have permission to perform this action."
-#     )
+    assert response.status_code == 200
+    assert "base_image_url" in response.data
+    assert "has_descriptions" in response.data
 
 
-# def test_download(admin_client):
-#     url = "{}?dataset_id=quads_f1_ds&instrument=instrument1".format(
-#         DOWNLOAD_URL
-#     )
-#     response = admin_client.get(url)
+def test_measures(admin_client):
+    url = "{}?dataset_id=quads_f1_ds&instrument=instrument1".format(
+        MEASURES_URL
+    )
+    response = admin_client.get(url)
+    assert response.status_code == 200
 
-#     assert response.status_code == 200
-
-#     header = response.content.decode("utf-8").split()[0].split(",")
-#     assert header[0] == "person_id"
-
-
-# def test_download_forbidden(user_client):
-#     url = "{}?dataset_id=quads_f1_ds&instrument=instrument1".format(
-#         DOWNLOAD_URL
-#     )
-#     response = user_client.get(url)
-
-#     assert response.status_code == 403
-
-#     header = response.data
-#     assert len(header.keys()) == 1
-#     assert (
-#         header["detail"]
-#         == "You do not have permission to perform this action."
-#     )
+    res = response.streaming_content
+    res = json.loads("".join(map(lambda x: x.decode("utf-8"), res)))
+    assert len(res) == 4
 
 
-# def test_download_all_instruments(admin_client):
-#     url = "{}?dataset_id=quads_f1_ds&instrument=".format(DOWNLOAD_URL)
-#     response = admin_client.get(url)
+def test_measures_forbidden(user_client, user):
+    print(user.groups.all())
+    url = "{}?dataset_id=quads_f1_ds&instrument=instrument1".format(
+        MEASURES_URL
+    )
+    response = user_client.get(url)
 
-#     assert response.status_code == 200
+    assert response.status_code == 403
 
-#     content = list(response.streaming_content)[0].decode("utf-8")
-#     header = content.split()[0].split(",")
-
-#     print("header:\n", header)
-#     assert len(header) == 5
-#     assert set(header) == {
-#         "person_id",
-#         "instrument1.continuous",
-#         "instrument1.categorical",
-#         "instrument1.ordinal",
-#         "instrument1.raw",
-#     }
+    header = response.data
+    assert len(header.keys()) == 1
+    assert (
+        header["detail"]
+        == "You do not have permission to perform this action."
+    )
 
 
-# def test_measure_details(admin_client):
-#     url = (
-#         "{}?dataset_id=quads_f1_ds&measure_id=instrument1.categorical"
-#         .format(MEASURE_DESCRIPTION_URL)
-#     )
-#     response = admin_client.get(url)
+def test_download(admin_client):
+    url = "{}?dataset_id=quads_f1_ds&instrument=instrument1".format(
+        DOWNLOAD_URL
+    )
+    response = admin_client.get(url)
 
-#     assert response.status_code == 200
+    assert response.status_code == 200
 
-#     print(response.data)
-#     assert response.data["instrument_name"] == "instrument1"
-#     assert response.data["measure_name"] == "categorical"
-#     assert response.data["measure_type"] == "categorical"
-#     assert response.data["values_domain"] == ["option1", "option2"]
+    header = response.content.decode("utf-8").split()[0].split(",")
+    assert header[0] == "person_id"
+
+
+def test_download_forbidden(user_client):
+    url = "{}?dataset_id=quads_f1_ds&instrument=instrument1".format(
+        DOWNLOAD_URL
+    )
+    response = user_client.get(url)
+
+    assert response.status_code == 403
+
+    header = response.data
+    assert len(header.keys()) == 1
+    assert (
+        header["detail"]
+        == "You do not have permission to perform this action."
+    )
+
+
+def test_download_all_instruments(admin_client):
+    url = "{}?dataset_id=quads_f1_ds&instrument=".format(DOWNLOAD_URL)
+    response = admin_client.get(url)
+
+    assert response.status_code == 200
+
+    content = list(response.streaming_content)[0].decode("utf-8")
+    header = content.split()[0].split(",")
+
+    print("header:\n", header)
+    assert len(header) == 5
+    assert set(header) == {
+        "person_id",
+        "instrument1.continuous",
+        "instrument1.categorical",
+        "instrument1.ordinal",
+        "instrument1.raw",
+    }
+
+
+def test_measure_details(admin_client):
+    url = (
+        "{}?dataset_id=quads_f1_ds&measure_id=instrument1.categorical"
+        .format(MEASURE_DESCRIPTION_URL)
+    )
+    response = admin_client.get(url)
+
+    assert response.status_code == 200
+
+    print(response.data)
+    assert response.data["instrument_name"] == "instrument1"
+    assert response.data["measure_name"] == "categorical"
+    assert response.data["measure_type"] == "categorical"
+    assert response.data["values_domain"] == ["option1", "option2"]
--- wdae/wdae/pheno_tool_api/tests/test_pheno_tool_view_class_static_methods.py
+++ wdae/wdae/pheno_tool_api/tests/test_pheno_tool_view_class_static_methods.py
@@ -1,92 +1,92 @@
-# import pytest
+import pytest
 
-# from dae.variants.attributes import Sex
-# from dae.pheno_tool.tool import PhenoResult
+from dae.variants.attributes import Sex
+from dae.pheno_tool.tool import PhenoResult
 
-# from pheno_tool_api.views import PhenoToolView
-# from pheno_tool_api.pheno_tool_adapter import PhenoToolAdapter
-
-
-# pytestmark = pytest.mark.usefixtures(
-#     "wdae_gpf_instance", "dae_calc_gene_sets")
+from pheno_tool_api.views import PhenoToolView
+from pheno_tool_api.pheno_tool_adapter import PhenoToolAdapter
 
 
-# def test_pheno_tool_view_build_report_description():
-#     desc = PhenoToolView._build_report_description("measure", [])
-#     assert desc == "measure"
-#     desc = PhenoToolView._build_report_description("measure", ["norm1"])
-#     assert desc == "measure ~ norm1"
-#     desc = PhenoToolView._build_report_description(
-#         "measure", ["norm1", "norm2"]
-#     )
-#     assert desc == "measure ~ norm1 + norm2"
+pytestmark = pytest.mark.usefixtures(
+    "wdae_gpf_instance", "dae_calc_gene_sets")
 
 
-# def test_pheno_tool_view_align_NA_results_valid():
-#     results = {
-#         "maleResults": {
-#             "positive": {"count": 0, "mean": 0, "deviation": 0},
-#             "negative": {"count": 1, "mean": 1, "deviation": 0},
-#             "pValue": "NA",
-#         },
-#         "femaleResults": {
-#             "positive": {"count": 0, "mean": 0, "deviation": 0},
-#             "negative": {"count": 1, "mean": 1, "deviation": 0},
-#             "pValue": "NA",
-#         },
-#     }
-#     PhenoToolAdapter.align_NA_results([results])
-#     assert results["maleResults"]["positive"]["mean"] == 1
-#     assert results["femaleResults"]["positive"]["mean"] == 1
+def test_pheno_tool_view_build_report_description():
+    desc = PhenoToolView._build_report_description("measure", [])
+    assert desc == "measure"
+    desc = PhenoToolView._build_report_description("measure", ["norm1"])
+    assert desc == "measure ~ norm1"
+    desc = PhenoToolView._build_report_description(
+        "measure", ["norm1", "norm2"]
+    )
+    assert desc == "measure ~ norm1 + norm2"
 
 
-# def test_pheno_tool_view_align_NA_results_invalid():
-#     results = {
-#         "maleResults": {
-#             "positive": {"count": 0, "mean": 5, "deviation": 0.2},
-#             "negative": {"count": 0, "mean": 1, "deviation": 0.3},
-#             "pValue": 0.02,
-#         },
-#         "femaleResults": {
-#             "positive": {"count": 0, "mean": 5, "deviation": 0.2},
-#             "negative": {"count": 0, "mean": 1, "deviation": 0.3},
-#             "pValue": 0.02,
-#         },
-#     }
-#     with pytest.raises(AssertionError):
-#         PhenoToolAdapter.align_NA_results([results])
+def test_pheno_tool_view_align_NA_results_valid():
+    results = {
+        "maleResults": {
+            "positive": {"count": 0, "mean": 0, "deviation": 0},
+            "negative": {"count": 1, "mean": 1, "deviation": 0},
+            "pValue": "NA",
+        },
+        "femaleResults": {
+            "positive": {"count": 0, "mean": 0, "deviation": 0},
+            "negative": {"count": 1, "mean": 1, "deviation": 0},
+            "pValue": "NA",
+        },
+    }
+    PhenoToolAdapter.align_NA_results([results])
+    assert results["maleResults"]["positive"]["mean"] == 1
+    assert results["femaleResults"]["positive"]["mean"] == 1
 
 
-# def test_pheno_tool_view_get_result_by_sex():
-#     expected_result = {
-#         "positive": {"count": 1, "mean": 2, "deviation": 3},
-#         "negative": {"count": 4, "mean": 5, "deviation": 6},
-#         "pValue": 7,
-#     }
-#     result = PhenoResult()
-#     result._set_positive_stats(1, 2, 3)
-#     result._set_negative_stats(4, 5, 6)
-#     result.pvalue = 7
-#     results = {Sex.M.name: result, Sex.F.name: PhenoResult()}
-#     ret_val = PhenoToolView.get_result_by_sex(results, Sex.M.name)
-#     assert ret_val == expected_result
+def test_pheno_tool_view_align_NA_results_invalid():
+    results = {
+        "maleResults": {
+            "positive": {"count": 0, "mean": 5, "deviation": 0.2},
+            "negative": {"count": 0, "mean": 1, "deviation": 0.3},
+            "pValue": 0.02,
+        },
+        "femaleResults": {
+            "positive": {"count": 0, "mean": 5, "deviation": 0.2},
+            "negative": {"count": 0, "mean": 1, "deviation": 0.3},
+            "pValue": 0.02,
+        },
+    }
+    with pytest.raises(AssertionError):
+        PhenoToolAdapter.align_NA_results([results])
 
 
-# def test_pheno_tool_view_calc_by_effect():
-#     class MockPhenoTool:
-#         def calc(self, people_variants, sex_split):
-#             pr_m = PhenoResult()
-#             pr_m._set_positive_stats(1, 2, 3)
-#             pr_f = PhenoResult()
-#             pr_f._set_positive_stats(4, 5, 6)
-#             return {Sex.M.name: pr_m, Sex.F.name: pr_f}
+def test_pheno_tool_view_get_result_by_sex():
+    expected_result = {
+        "positive": {"count": 1, "mean": 2, "deviation": 3},
+        "negative": {"count": 4, "mean": 5, "deviation": 6},
+        "pValue": 7,
+    }
+    result = PhenoResult()
+    result._set_positive_stats(1, 2, 3)
+    result._set_negative_stats(4, 5, 6)
+    result.pvalue = 7
+    results = {Sex.M.name: result, Sex.F.name: PhenoResult()}
+    ret_val = PhenoToolView.get_result_by_sex(results, Sex.M.name)
+    assert ret_val == expected_result
 
-#     mocked_tool = MockPhenoTool()
-#     res = PhenoToolView.calc_by_effect("effectName", mocked_tool, dict())
-#     assert res["effect"] == "effectName"
-#     assert res["maleResults"]["positive"]["count"] == 1
-#     assert res["maleResults"]["positive"]["mean"] == 2
-#     assert res["maleResults"]["positive"]["deviation"] == 3
-#     assert res["femaleResults"]["positive"]["count"] == 4
-#     assert res["femaleResults"]["positive"]["mean"] == 5
-#     assert res["femaleResults"]["positive"]["deviation"] == 6
+
+def test_pheno_tool_view_calc_by_effect():
+    class MockPhenoTool:
+        def calc(self, people_variants, sex_split):
+            pr_m = PhenoResult()
+            pr_m._set_positive_stats(1, 2, 3)
+            pr_f = PhenoResult()
+            pr_f._set_positive_stats(4, 5, 6)
+            return {Sex.M.name: pr_m, Sex.F.name: pr_f}
+
+    mocked_tool = MockPhenoTool()
+    res = PhenoToolView.calc_by_effect("effectName", mocked_tool, dict())
+    assert res["effect"] == "effectName"
+    assert res["maleResults"]["positive"]["count"] == 1
+    assert res["maleResults"]["positive"]["mean"] == 2
+    assert res["maleResults"]["positive"]["deviation"] == 3
+    assert res["femaleResults"]["positive"]["count"] == 4
+    assert res["femaleResults"]["positive"]["mean"] == 5
+    assert res["femaleResults"]["positive"]["deviation"] == 6
