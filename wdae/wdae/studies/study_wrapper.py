4425d99fc00096c2b8fdfe28084d7074c72fef80
iordanivanov <iiordan.ivanov@gmail.com>
2021-12-21 14:28:09 +0200

    Revert "GPF-1368 Potential fix for the root dataset not sending the correct response"
    This reverts commit 4154d0f47a268906054955725cb8fa3724259057.

--- wdae/wdae/studies/study_wrapper.py
+++ wdae/wdae/studies/study_wrapper.py
@@ -60,21 +60,20 @@ class StudyWrapperBase:
     def build_genotype_data_all_datasets(config):
         keys = [
             "id",
-            "name"
+            "name",
+            "phenotype_browser",
+            "phenotype_tool"
         ]
         result = {
             key: config.get(key, None) for key in keys
         }
+        result["name"] = result["name"] or result["id"]
         result["genotype_browser"] = config.genotype_browser.enabled
-        result["genotype_browser_config"] = {
-        }
-        result["genotype_browser_config"]["table_columns"] = []
         result["common_report"] = {"enabled": config.common_report.enabled}
         result["enrichment_tool"] = config.enrichment.enabled
-        result["name"] = result["name"] or result["id"]
 
         return result
-    
+
     @staticmethod
     def build_genotype_data_group_description(
         gpf_instance, config, description, person_set_collection_configs
