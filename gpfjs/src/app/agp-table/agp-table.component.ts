dea4141acf48739a670c4fa9760cd5d53e846938
IvoStefanov <ivostefanovtues@gmail.com>
2022-01-04 12:43:08 +0200

    GPF-1371 add basic agp table header recursion logic

--- gpfjs/src/app/agp-table/agp-table.component.ts
+++ gpfjs/src/app/agp-table/agp-table.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { configMock } from './agp-table';
 
 @Component({
   selector: 'gpf-agp-table',
   templateUrl: './agp-table.component.html',
   styleUrls: ['./agp-table.component.css']
 })
-export class AgpTableComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class AgpTableComponent {
+  public config = configMock;
 }
