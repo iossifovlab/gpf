706c7338e45ad70e872d79ad649c2d78265a19a1
livovachkov <livovachkov@gmail.com>
2022-01-07 17:05:36 +0200

    GPF-1404 More assertions to insure share query tests

--- web_e2e/cypress/integration/share-query-spec.ts
+++ web_e2e/cypress/integration/share-query-spec.ts
@@ -29,6 +29,7 @@ describe('Share query tests', () => {
     const datasetsPage = new DatasetsPage();
 
     page.button.click();
+    page.datasetsDropdownMenuButton.should('have.text', 'comp_all');
     page.input.contains(/\.*/).invoke('val').then((url) => {
       cy.visit(String(url)).then(() => {
         datasetsPage.datasetStatisticsButton.click();
