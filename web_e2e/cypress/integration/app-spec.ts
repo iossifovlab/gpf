270d455e887b8e3d07e86086a611068549b0a04d
livovachkov <livovachkov@gmail.com>
2022-01-31 13:41:19 +0200

    GPF-1426 Fix for the app tests error

--- web_e2e/cypress/integration/app-spec.ts
+++ web_e2e/cypress/integration/app-spec.ts
@@ -178,7 +178,9 @@ describe('App user access rights tests', () => {
     page.loginAdmin();
     page.navigateToSidenavPage(sidenavPageLinks.management);
     userManagementPage.getUserEditorButtonByEmail(userData.normal.username).click();
+    userManagementPage.allUserEditGroupRemoveButtons.should('be.visible');
     userManagementPage.userWindowGroupDropDownMenuButton.click();
+    userManagementPage.userWindowGroupDropDownMenuButton.should('be.visible');
     userManagementPage.userWindowGroupDropdownSearch.type('comp_vcf');
     userManagementPage.userWindowGroupDropdownListCheckboxes.last().click();
     userManagementPage.userWindowGroupDropDownMenuButton.click();
@@ -201,7 +203,7 @@ describe('App user access rights tests', () => {
     page.loginAdmin();
     page.navigateToSidenavPage(sidenavPageLinks.management);
     userManagementPage.getUserEditorButtonByEmail(userData.normal.username).click();
-    userManagementPage.allUserEditGroupRemoveButtons.should('be.visible ');
+    userManagementPage.allUserEditGroupRemoveButtons.should('be.visible');
     userManagementPage.allUserEditGroupRemoveButtons.click({multiple: true});
     userManagementPage.userWindowGroupDropDownMenuButton.should('be.visible');
     userManagementPage.userWindowGroupDropDownMenuButton.click();
@@ -301,7 +303,7 @@ describe('App user access rights tests', () => {
     const genotypeBrowserPage = new GenotypeBrowserPage();
 
     page.loginAdmin();
-    page.navigateToSidenavPage(sidenavPageLinks.management);
+    page.navigateToSidenavPage(sidenavPageLinks.management); 
     userManagementPage.getUserEditorButtonByEmail(userData.normal.username).click();
     userManagementPage.allUserEditGroupRemoveButtons.click({multiple: true});
     userManagementPage.userWindowSubmitButton.click();
