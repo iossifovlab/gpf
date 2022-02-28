d37514375b215ffb3cdbf6ec7b337746c289ff8c
joankosev <joankosev@gmail.com>
2022-02-28 16:12:30 +0200

    GPF-1486 update header-cell component sorting
    now sorts by desc on first click instead of asc

--- gpfjs/src/app/table/view/header/header-cell.component.ts
+++ gpfjs/src/app/table/view/header/header-cell.component.ts
@@ -1,38 +1,38 @@
-import { Output, EventEmitter, Input, Component, ViewContainerRef } from '@angular/core';
+import { Output, EventEmitter, Input, Component, OnInit } from '@angular/core';
 import { GpfTableContentHeaderComponent } from '../../component/header.component';
 import { GpfTableSubheaderComponent } from '../../component/subheader.component';
 import { SortInfo } from '../../table.component';
 import { environment } from '../../../../environments/environment';
-
 
 @Component({
   selector: 'gpf-table-view-header-cell',
   templateUrl: './header-cell.component.html',
   styleUrls: ['./header-cell.component.css']
 })
-export class GpfTableHeaderCellComponent {
+export class GpfTableHeaderCellComponent implements OnInit {
   @Input() columnInfo: GpfTableContentHeaderComponent;
   @Output() sortingInfoChange = new EventEmitter();
   @Input() sortingInfo: SortInfo;
+  public imgPathPrefix: string;
 
-  constructor(private viewContainer: ViewContainerRef) {
+  public ngOnInit(): void {
+    this.imgPathPrefix = environment.imgPathPrefix;
   }
 
-  onSortClick(sortBySubcolumn: GpfTableSubheaderComponent) {
+  public onSortClick(sortBySubcolumn: GpfTableSubheaderComponent): boolean {
     if (!sortBySubcolumn.sortable) {
-      return true;
+      return;
     }
+
     let sortInfo: SortInfo;
     if (this.sortingInfo && this.sortingInfo.sortBySubcolumn === sortBySubcolumn) {
       sortInfo = new SortInfo(sortBySubcolumn, !this.sortingInfo.sortOrderAsc);
     } else {
-      sortInfo = new SortInfo(sortBySubcolumn, true);
+      sortInfo = new SortInfo(sortBySubcolumn, false);
     }
-    this.sortingInfoChange.emit(sortInfo);
-    return true;
-  }
 
-  get imgPathPrefix() {
-    return environment.imgPathPrefix;
+    this.sortingInfoChange.emit(sortInfo);
+
+    return true;
   }
 }
