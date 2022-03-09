704d9859f7a118e5195d7472828733f069d11c2b
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2022-03-09 18:48:02 +0200

    GPF-1541 fix a bug reported by sonarqube
    Add 1 missing arguments; 'deserialize_family_variant' expects at least 2
    positional arguments.

--- dae/dae/backends/impala/tests/test_impala_best_state_genetic_model.py
+++ dae/dae/backends/impala/tests/test_impala_best_state_genetic_model.py
@@ -5,9 +5,10 @@ from dae.variants.attributes import GeneticModel
 from dae.backends.impala.serializers import AlleleParquetSerializer
 
 
-@pytest.mark.skip("Incompatible API")
+# @pytest.mark.skip("Incompatible API")
 def test_best_state_genetic_model(variants_impala, impala_genotype_storage):
-    variants_impala("backends/quads_f1")
+    iv = variants_impala("backends/quads_f1")
+    families = iv.families
 
     impala = impala_genotype_storage.impala_helpers
     db = impala_genotype_storage.storage_config.impala.db
@@ -17,13 +18,20 @@ def test_best_state_genetic_model(variants_impala, impala_genotype_storage):
         np.array([[2, 1, 1, 2], [0, 1, 1, 0]], dtype=np.int8),
     ]
 
-    with impala.connection.cursor() as cursor:
-        cursor.execute(f"SELECT variant_data " f"FROM {db}.quads_f1_variants")
+    with impala.connection().cursor() as cursor:
+        cursor.execute(
+            f"SELECT variant_data, family_id FROM {db}.quads_f1_variants")
         rows = list(cursor)
         serializer = AlleleParquetSerializer(None)
-        variants = [
-            serializer.deserialize_family_variant(row[0]) for row in rows
-        ]
+        variants = []
+
+        for row in rows:
+            family = families[row[1]]
+            v = serializer.deserialize_family_variant(row[0], family)
+            print(v)
+
+            variants.append(v)
+
         assert np.array(
             [
                 variant.best_state == best_state_expecteds[0]
