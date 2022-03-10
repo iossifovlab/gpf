78201853b6702b43f5c4872ea1a2c7a1c96338ba
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2022-03-10 15:58:10 +0200

    skip checking for branch name in sonarqube stage

--- build.sh
+++ build.sh
@@ -415,7 +415,9 @@ EOT'
     build_run_local echo $gpf_git_branch
     build_run_local echo "SONARQUBE_DEFAULT_TOKEN=$SONARQUBE_DEFAULT_TOKEN"
 
-    if [ "$SONARQUBE_DEFAULT_TOKEN" != "" ] && [ "$gpf_git_branch" = "sonarqube-experiments" ]; then
+    # if [ "$SONARQUBE_DEFAULT_TOKEN" != "" ] && [ "$gpf_git_branch" = "sonarqube-experiments" ]; then
+
+    if [ "$SONARQUBE_DEFAULT_TOKEN" != "" ]; then
       build_run_local echo "Sonarqube stage started"
 
       build_run_local docker run --rm \
