3bb90b1428461399019c91c718ef25935eb19272
joankosev <joankosev@gmail.com>
2022-03-18 15:43:43 +0200

    GPF-1443 update build scripts cypress run command

--- web_e2e/scripts/internal_run_gpf_e2e.sh
+++ web_e2e/scripts/internal_run_gpf_e2e.sh
@@ -16,4 +16,4 @@ ${WD}/scripts/wait-for-it.sh ${CONTAINER_GPF_DEV}:80 --timeout=360
 
 
 # ng e2e --baseUrl http://${CONTAINER_GPF_DEV}/gpf/
-./node_modules/.bin/cypress run --browser chrome --config baseUrl=http://${CONTAINER_GPF_DEV}/gpf/ --spec cypress/integration/user-management-spec.ts, cypress/integration/app-spec.ts, cypress/integration/gene-weights-spec.ts, cypress/integration/share-query-spec.ts, cypress/integration/save-query-spec.ts, cypress/integration/datasets-spec.ts, cypress/integration/enrichment-tool-spec.ts, cypress/integration/genotype-browser-spec.ts
+./node_modules/.bin/cypress run --browser chrome --config baseUrl=http://${CONTAINER_GPF_DEV}/gpf/
