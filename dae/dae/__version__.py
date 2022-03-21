ab9afa31e32dca15c28c301f003372b45c771150
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2022-03-21 17:36:05 +0200

    Bump version: 3.6.dev4 → 3.6.dev5

--- dae/dae/__version__.py
+++ dae/dae/__version__.py
@@ -1,3 +1,3 @@
-RELEASE = "3.6.dev4"
+RELEASE = "3.6.dev5"
 
 VERSION = '.'.join(RELEASE.split('.')[:2])
--- wdae/setup.py
+++ wdae/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="gpf_wdae",
-    version="3.6.dev4",
+    version="3.6.dev5",
     author="Lubomir Chorbadjiev",
     author_email="lubomir.chorbadjiev@gmail.com",
     description="GPF: Genotypes and Phenotypes in Families",
