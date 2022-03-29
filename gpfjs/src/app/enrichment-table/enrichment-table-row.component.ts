3ebd4d2a6bc92b2cbd31e30adb4465d27a4d75da
livovachkov <livovachkov@gmail.com>
2022-03-29 09:22:03 +0300

    GPF-1411 Contrast adjustment based on blue cell background color

--- gpfjs/src/app/enrichment-table/enrichment-table-row.component.ts
+++ gpfjs/src/app/enrichment-table/enrichment-table-row.component.ts
@@ -57,11 +57,19 @@ export class EnrichmentTableRowComponent {
 
   public getBackgroundColor(testResult: EnrichmentTestResult): string {
     const intensity = this.pValueIntensityPipe.transform(testResult.pvalue) as string;
-
     if (testResult.overlapped > testResult.expected) {
       return `rgba(255, ${intensity}, ${intensity}, 0.8)`;
     } else {
       return `rgba(${intensity}, ${intensity}, 255, 0.5)`;
+    }
+  }
+
+  public contrastAdjust(rgba: string): string {
+    const res = rgba.match(/\d{1,3}, \d{1,3}, \d{1,3}, \d*/).toString().split(',');
+    if (Number(res[0]) <= 75 && Number(res[1]) <= 75) {
+      return '#FFFFFF';
+    } else {
+      return '#0000EE';
     }
   }
 }
