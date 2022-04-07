c911f13bf4df30c1623e0aaf3df384c5cd205184
IvoTod <ivo.tod.todorov@gmail.com>
2022-04-07 10:19:41 +0300

    GPF-1491 Migrate test data fixtures from conftest to tests

--- dae/dae/gene/tests/test_gene_scores_db.py
+++ dae/dae/gene/tests/test_gene_scores_db.py
@@ -1,5 +1,79 @@
-def test_scores_default(gene_scores_db):
-    assert gene_scores_db is not None
+import pytest
+
+from dae.gene.gene_scores import GeneScoresDb, GeneScore
+from dae.genomic_resources.embeded_repository import GenomicResourceEmbededRepo
+from dae.genomic_resources.repository import GR_CONF_FILE_NAME
+
+
+@pytest.fixture(scope="session")
+def scores_repo():
+    scores_repo = GenomicResourceEmbededRepo("scores", content={
+        "RVIS_rank": {
+            GR_CONF_FILE_NAME: (
+                "type: gene_score\n"
+                "id: RVIS_rank\n"
+                "filename: RVIS.csv\n"
+                "desc: RVIS rank\n"
+                "histogram:\n"
+                "  bins: 150\n"
+                "  xscale: linear\n"
+                "  yscale: linear\n"
+            ),
+            "RVIS.csv": (
+                "\"gene\",\"RVIS\",\"RVIS_rank\"\n"
+                "\"LRP1\",-7.28,3\n"
+                "\"TRRAP\",-6.14,6\n"
+                "\"ANKRD11\",-4.38,15\n"
+                "\"ZFHX3\",-4.26,19\n"
+                "\"HERC2\",-5.99,8\n"
+                "\"TRIO\",-4.04,29\n"
+                "\"MACF1\",-3.92,34\n"
+                "\"PLEC\",-6.57,5\n"
+                "\"SRRM2\",-4.51,13\n"
+                "\"SPTBN1\",-3.86,37\n"
+                "\"UBR4\",-7.5,2\n"
+                "\"EP400\",-3.86,36\n"
+                "\"NOTCH1\",-3.51,55\n"
+            )
+        },
+        "LGD_rank": {
+            GR_CONF_FILE_NAME: (
+                "type: gene_score\n"
+                "id: LGD_rank\n"
+                "filename: LGD.csv\n"
+                "desc: LGD rank\n"
+                "histogram:\n"
+                "  bins: 150\n"
+                "  xscale: linear\n"
+                "  yscale: linear\n"
+            ),
+            "LGD.csv": (
+                "\"gene\",\"LGD_score\",\"LGD_rank\"\n"
+                "\"LRP1\",0.000014,1\n"
+                "\"TRRAP\",0.00016,3\n"
+                "\"ANKRD11\",0.0004,5\n"
+                "\"ZFHX3\",0.000925,8\n"
+                "\"HERC2\",0.003682,25\n"
+                "\"TRIO\",0.001563,11\n"
+                "\"MACF1\",0.000442,6\n"
+                "\"PLEC\",0.004842,40\n"
+                "\"SRRM2\",0.004471,35\n"
+                "\"SPTBN1\",0.002715,19.5\n"
+                "\"UBR4\",0.007496,59\n"
+            )
+        }
+    })
+    return scores_repo
+
+
+@pytest.fixture(scope="session")
+def gene_scores_db(scores_repo):
+    resources = [
+        scores_repo.get_resource("LGD_rank"),
+        scores_repo.get_resource("RVIS_rank"),
+    ]
+    scores = [GeneScore.load_gene_score_from_resource(r) for r in resources]
+    return GeneScoresDb(scores)
 
 
 def test_scores_rvis_rank(gene_scores_db):
@@ -17,3 +91,52 @@ def test_scores_has_rvis_rank(gene_scores_db):
 
 def test_loaded_scores(gene_scores_db):
     assert len(gene_scores_db) == 2
+
+
+def test_create_score_from_repository(scores_repo):
+    resource = scores_repo.get_resource("RVIS_rank")
+    score = GeneScore.load_gene_score_from_resource(resource)
+    assert score
+    print(score)
+
+
+def test_scores_default(scores_repo):
+    resource = scores_repo.get_resource("RVIS_rank")
+    w = GeneScore.load_gene_score_from_resource(resource)
+
+    assert w.df is not None
+
+    assert "RVIS_rank" in w.df.columns
+
+
+def test_scores_min_max(scores_repo):
+    resource = scores_repo.get_resource("LGD_rank")
+    w = GeneScore.load_gene_score_from_resource(resource)
+
+    assert 1.0 == w.min()
+    assert 59.0 == w.max()
+
+
+def test_scores_get_genes(scores_repo):
+    resource = scores_repo.get_resource("LGD_rank")
+    w = GeneScore.load_gene_score_from_resource(resource)
+
+    genes = w.get_genes(1.5, 5.1)
+    assert len(genes) == 2
+
+    genes = w.get_genes(-1, 5.1)
+    assert len(genes) == 3
+
+    genes = w.get_genes(1, 5.1)
+    assert len(genes) == 3
+
+    genes = w.get_genes(1, 5.0)
+    assert len(genes) == 2
+
+
+def test_scores_to_tsv(scores_repo):
+    resource = scores_repo.get_resource("LGD_rank")
+    score = GeneScore.load_gene_score_from_resource(resource)
+    tsv = list(score.to_tsv())
+    assert len(tsv) == 12
+    assert tsv[0] == "gene\tLGD_rank\n"
--- dae/dae/gene/tests/test_gene_sets_db.py
+++ dae/dae/gene/tests/test_gene_sets_db.py
@@ -1,3 +1,110 @@
+import pytest
+
+from dae.gene.gene_sets_db import GeneSetCollection, GeneSetsDb
+from dae.genomic_resources.embeded_repository import GenomicResourceEmbededRepo
+from dae.genomic_resources.repository import GR_CONF_FILE_NAME
+
+
+@pytest.fixture(scope="session")
+def gene_sets_repo():
+    sets_repo = GenomicResourceEmbededRepo("gene_sets", content={
+        "main": {
+            GR_CONF_FILE_NAME: (
+                "type: gene_set\n"
+                "id: main\n"
+                "format: directory\n"
+                "directory: GeneSets\n"
+                "web_label: Main\n"
+                "web_format_str: \"key| (|count|): |desc\"\n"
+            ),
+            "GeneSets": {
+                "main_candidates.txt": (
+                    "Main Candidates\n"
+                    "POGZ\n"
+                    "CHD8\n"
+                    "ANK2\n"
+                    "FAT4\n"
+                    "NBEA\n"
+                    "CELSR1\n"
+                    "USP7\n"
+                    "GOLGA5\n"
+                    "PCSK2\n"
+                )
+            }
+        },
+        "test_mapping": {
+            GR_CONF_FILE_NAME: (
+                "type: gene_set\n"
+                "id: test_mapping\n"
+                "format: map\n"
+                "filename: test-map.txt\n"
+                "web_label: Test mapping\n"
+                "web_format_str: \"key| (|count|)\"\n"
+            ),
+            "test-map.txt": (
+                "#geneNS\tsym\n"
+                "POGZ\ttest:01 test:02\n"
+                "CHD8\ttest:02 test:03\n"
+            ),
+            "test-mapnames.txt": (
+                "test:01\ttest_first\n"
+                "test:02\ttest_second\n"
+                "test:03\ttest_third\n"
+            )
+        },
+        "test_gmt": {
+            GR_CONF_FILE_NAME: (
+                "type: gene_set\n"
+                "id: test_gmt\n"
+                "format: gmt\n"
+                "filename: test-gmt.gmt\n"
+                "web_label: Test GMT\n"
+                "web_format_str: \"key| (|count|)\"\n"
+            ),
+            "test-gmt.gmt": (
+                "TEST_GENE_SET1\tsomedescription\tPOGZ\tCHD8\n"
+                "TEST_GENE_SET2\tsomedescription\tANK2\tFAT4\n"
+                "TEST_GENE_SET3\tsomedescription\tPOGZ\n"
+            )
+        }
+    })
+    return sets_repo
+
+
+@pytest.fixture(scope="session")
+def gene_sets_db(gene_sets_repo):
+    resources = [
+        gene_sets_repo.get_resource("main"),
+        gene_sets_repo.get_resource("test_mapping"),
+        gene_sets_repo.get_resource("test_gmt"),
+    ]
+    gene_set_collections = [
+        GeneSetCollection.from_resource(r) for r in resources
+    ]
+    return GeneSetsDb(gene_set_collections)
+
+
+def test_gene_set_collection_main(gene_sets_repo):
+    resource = gene_sets_repo.get_resource("main")
+    gsc = GeneSetCollection.from_resource(resource)
+    gene_set = gsc.get_gene_set("main_candidates")
+    assert gene_set is not None
+    assert gene_set["name"] == "main_candidates"
+    assert gene_set["count"] == 9
+    assert set(gene_set["syms"]) == {
+        "POGZ",
+        "CHD8",
+        "ANK2",
+        "FAT4",
+        "NBEA",
+        "CELSR1",
+        "USP7",
+        "GOLGA5",
+        "PCSK2",
+    }
+    assert gene_set["desc"] == "Main Candidates"
+
+
 def test_get_gene_set_collection_ids(gene_sets_db):
     assert gene_sets_db.get_gene_set_collection_ids() == {
         "main",
