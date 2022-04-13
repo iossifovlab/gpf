997cfd1f91f4bca8db28022f36721957a270edcf
iordanivanov <iiordan.ivanov@gmail.com>
2022-04-13 16:31:27 +0300

    GPF-1638 Remove Karma, replace with Jest builder

--- gpfjs/jest.config.js
+++ gpfjs/jest.config.js
@@ -2,7 +2,6 @@ const esModules = ['d3', 'd3-array', 'internmap', 'delaunator', 'robust-predicat
 
 module.exports = {
   preset: 'jest-preset-angular',
-  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   globalSetup: 'jest-preset-angular/global-setup',
   modulePaths: ['<rootDir>/src'],
   transformIgnorePatterns: [`node_modules/(?!${esModules})`]
