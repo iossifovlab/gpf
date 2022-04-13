50db6865fb282ffab58dab225b146e268fe0ad1c
iordanivanov <iiordan.ivanov@gmail.com>
2022-04-13 15:00:55 +0300

    GPF-1638 Add ES modules to be transformed by Jest

--- gpfjs/jest.config.js
+++ gpfjs/jest.config.js
@@ -1,6 +1,9 @@
+const esModules = ['d3', 'd3-array', 'internmap', 'delaunator', 'robust-predicates'].join('|');
+
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   globalSetup: 'jest-preset-angular/global-setup',
-  modulePaths: ['<rootDir>/src']
+  modulePaths: ['<rootDir>/src'],
+  transformIgnorePatterns: [`node_modules/(?!${esModules})`]
 };
