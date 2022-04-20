6de23eb6473907693f3699ddbbad14780d9a4e4b
iordanivanov <iiordan.ivanov@gmail.com>
2022-04-20 17:09:11 +0300

    GPF-1655 Lint EnrichmentModelsBlockComponent

--- gpfjs/src/app/enrichment-models-block/enrichment-models-block.component.ts
+++ gpfjs/src/app/enrichment-models-block/enrichment-models-block.component.ts
@@ -1,30 +1,31 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
 import { NgbNav } from '@ng-bootstrap/ng-bootstrap';
 import { Store } from '@ngxs/store';
-import { EnrichmentModelsState } from 'app/enrichment-models/enrichment-models.state';
+import { EnrichmentModelsModel, EnrichmentModelsState } from 'app/enrichment-models/enrichment-models.state';
 import { StateReset } from 'ngxs-reset-plugin';
 
 @Component({
   selector: 'gpf-enrichment-models-block',
   templateUrl: './enrichment-models-block.component.html',
 })
-export class EnrichmentModelsBlockComponent {
-  @ViewChild('nav') ngbNav: NgbNav;
+export class EnrichmentModelsBlockComponent implements AfterViewInit {
+  @ViewChild('nav') public ngbNav: NgbNav;
 
   @Input()
   public selectedDatasetId: string;
 
-  constructor(private store: Store) { }
+  public constructor(private store: Store) { }
 
-  ngAfterViewInit() {
+  public ngAfterViewInit(): void {
     this.store.selectOnce(EnrichmentModelsState).subscribe(state => {
-      if (state.enrichmentBackgroundModel || state.enrichmentCountingModel) {
+      const enrichmentModelsState = state as EnrichmentModelsModel;
+      if (enrichmentModelsState.enrichmentBackgroundModel || enrichmentModelsState.enrichmentCountingModel) {
         setTimeout(() => this.ngbNav.select('models'));
       }
     });
   }
 
-  onNavChange() {
+  public onNavChange(): void {
     this.store.dispatch(new StateReset(EnrichmentModelsState));
   }
 }
