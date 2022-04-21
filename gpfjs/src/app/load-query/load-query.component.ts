237379f2bff3c6b0d0cefcd43e4fbc83c7f33cda
iordanivanov <iiordan.ivanov@gmail.com>
2022-04-21 13:20:48 +0300

    GPF-1655 Lint LoadQueryComponent

--- gpfjs/src/app/load-query/load-query.component.ts
+++ gpfjs/src/app/load-query/load-query.component.ts
@@ -2,17 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { QueryService } from '../query/query.service';
-import { DatasetsService } from '../datasets/datasets.service';
 import { Store } from '@ngxs/store';
 import { StateReset } from 'ngxs-reset-plugin';
 import { ErrorsState } from 'app/common/errors.state';
 import { take } from 'rxjs/operators';
 
 const PAGE_TYPE_TO_NAVIGATE = {
-    genotype: datasetId => ['datasets', datasetId, 'genotype-browser'],
-    phenotype: datasetId => ['datasets', datasetId, 'phenotype-browser'],
-    enrichment: datasetId => ['datasets', datasetId, 'enrichment-tool'],
-    phenotool: datasetId => ['datasets', datasetId, 'phenotype-tool']
+  genotype: (datasetId: string): string[] => ['datasets', datasetId, 'genotype-browser'],
+  phenotype: (datasetId: string): string[] => ['datasets', datasetId, 'phenotype-browser'],
+  enrichment: (datasetId: string): string[] => ['datasets', datasetId, 'enrichment-tool'],
+  phenotool: (datasetId: string): string[] => ['datasets', datasetId, 'phenotype-tool']
 };
 
 @Component({
@@ -21,42 +20,40 @@ const PAGE_TYPE_TO_NAVIGATE = {
   styleUrls: ['./load-query.component.css']
 })
 export class LoadQueryComponent implements OnInit {
-
-  constructor(
+  public constructor(
     private store: Store,
     private queryService: QueryService,
     private route: ActivatedRoute,
     private router: Router
   ) { }
 
-  ngOnInit() {
-      this.route.params.subscribe(
-          params => {
-              if (!params['uuid']) {
-                  this.router.navigate(['/']);
-              } else {
-                  this.loadQuery(params['uuid']);
-              }
-          });
+  public ngOnInit(): void {
+    this.route.params.subscribe(
+      params => {
+        if (!params['uuid']) {
+          this.router.navigate(['/']);
+        } else {
+          this.loadQuery(params['uuid'] as string);
+        }
+      });
   }
 
-  private loadQuery(uuid: string) {
-      this.queryService.loadQuery(uuid)
-          .pipe(take(1))
-          .subscribe(response => {
-              const queryData = response['data'];
-              const page = response['page'];
-              this.restoreQuery(queryData, page);
-          });
+  private loadQuery(uuid: string): void {
+    this.queryService.loadQuery(uuid)
+      .pipe(take(1))
+      .subscribe(response => {
+        const queryData = response['data'] as object;
+        const page = response['page'] as string;
+        this.restoreQuery(queryData, page);
+      });
   }
 
-  private restoreQuery(state: {}, page: string) {
-      if (page in PAGE_TYPE_TO_NAVIGATE) {
-          const navigationParams = PAGE_TYPE_TO_NAVIGATE[page](state['datasetId']);
-          this.store.reset(state);
-          this.store.dispatch(new StateReset(ErrorsState));
-          this.router.navigate(navigationParams);
-      }
+  private restoreQuery(state: object, page: string): void {
+    if (page in PAGE_TYPE_TO_NAVIGATE) {
+      const navigationParams: string[] = PAGE_TYPE_TO_NAVIGATE[page](state['datasetId']);
+      this.store.reset(state);
+      this.store.dispatch(new StateReset(ErrorsState));
+      this.router.navigate(navigationParams);
+    }
   }
-
 }
