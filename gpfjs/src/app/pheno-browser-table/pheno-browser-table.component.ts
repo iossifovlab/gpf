6c21e905d9534bd8feaa654a0f35d84fb1d8cc02
iordanivanov <iiordan.ivanov@gmail.com>
2022-04-25 13:01:45 +0300

    GPF-1655 Lint PhenoBrowserTableComponent

--- gpfjs/src/app/pheno-browser-table/pheno-browser-table.component.ts
+++ gpfjs/src/app/pheno-browser-table/pheno-browser-table.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { PhenoBrowserModalContentComponent } from '../pheno-browser-modal-content/pheno-browser-modal-content.component';
+import {
+  PhenoBrowserModalContentComponent
+} from '../pheno-browser-modal-content/pheno-browser-modal-content.component';
 import { PhenoMeasures } from '../pheno-browser/pheno-browser';
-
 import { PValueIntensityPipe } from '../utils/p-value-intensity.pipe';
-
 
 @Component({
   selector: 'gpf-pheno-browser-table',
@@ -13,33 +13,32 @@ import { PValueIntensityPipe } from '../utils/p-value-intensity.pipe';
   styleUrls: ['./pheno-browser-table.component.css']
 })
 export class PhenoBrowserTableComponent {
+  @Input() public measures: PhenoMeasures;
 
-  constructor(
+  public constructor(
     private modalService: NgbModal,
     private pValueIntensityPipe: PValueIntensityPipe
-) { }
+  ) { }
 
-  @Input('measures') measures: PhenoMeasures;
-
-  static compare(leftVal: any, rightVal: any): number {
-    if (leftVal == null && rightVal == null) {
+  public static compare(leftVal: any, rightVal: any): number {
+    if (leftVal === null && rightVal === null) {
       return 0;
     }
-    if (leftVal == null) {
+    if (leftVal === null) {
       return -1;
     }
-    if (rightVal == null) {
+    if (rightVal === null) {
       return 1;
     }
 
     if (!isNaN(leftVal) && !isNaN(rightVal)) {
-      return +leftVal - +rightVal;
+      return Number(leftVal) - Number(rightVal);
     }
 
     return leftVal.localeCompare(rightVal);
   }
 
-  regressionCompare(regressionId: string, field: string) {
+  public regressionCompare(regressionId: string, field: string) {
     return (a: any, b: any) => {
       let leftVal = a['regressions'][regressionId];
       let rightVal = b['regressions'][regressionId];
@@ -47,18 +46,24 @@ export class PhenoBrowserTableComponent {
       leftVal = !leftVal || isNaN(leftVal[field]) ? null : leftVal[field];
       rightVal = !rightVal || isNaN(rightVal[field]) ? null : rightVal[field];
 
-      if (leftVal == null && rightVal == null) { return 0; }
-      if (leftVal == null) { return -1; }
-      if (rightVal == null) { return 1; }
-      return +leftVal - +rightVal;
-    }
+      if (leftVal === null && rightVal === null) {
+        return 0;
+      }
+      if (leftVal === null) {
+        return -1;
+      }
+      if (rightVal === null) {
+        return 1;
+      }
+      return Number(leftVal) - Number(rightVal);
+    };
   }
 
-  getRegressionIds() {
+  public getRegressionIds(): string[] {
     return Object.getOwnPropertyNames(this.measures.regressionNames);
   }
 
-  openModal(imageUrl) {
+  public openModal(imageUrl: string): void {
     if (imageUrl) {
       const modalRef = this.modalService.open(PhenoBrowserModalContentComponent, {
         windowClass: 'modal-fullscreen'
@@ -67,10 +72,9 @@ export class PhenoBrowserTableComponent {
     }
   }
 
-  getBackgroundColor(pValue: string): string {
-    const intensity = this.pValueIntensityPipe.transform(pValue);
+  public getBackgroundColor(pValue: string): string {
+    const intensity = this.pValueIntensityPipe.transform(pValue) as number;
 
     return `rgba(255, ${intensity}, ${intensity}, 0.8)`;
   }
-
 }
