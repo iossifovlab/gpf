6f3ec72ffcb1ec2478bf52a8af0856d0b5e6a1a4
IvoTod <ivo.tod.todorov@gmail.com>
2022-05-05 09:34:22 +0300

    GPF-1652 Add family counter list and download views

--- wdae/wdae/common_reports_api/views.py
+++ wdae/wdae/common_reports_api/views.py
@@ -18,12 +18,67 @@ class VariantReportsView(QueryBaseView):
             common_report_id
         )
 
-        if common_report:
-            return Response(common_report)
+        if common_report is not None:
+            return Response(common_report.to_dict())
         return Response(
             {"error": "Common report {} not found".format(common_report_id)},
             status=status.HTTP_404_NOT_FOUND,
         )
+
+
+class FamilyCounterListView(QueryBaseView):
+    def __init__(self):
+        super().__init__()
+
+    def get(self, request, common_report_id, group_name, counter_id):
+        assert common_report_id
+
+        common_report = self.gpf_instance.get_common_report(
+            common_report_id
+        )
+
+        if common_report is None:
+            return Response(
+                {"error": f"Common report {common_report_id} not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        group = common_report.families_report.families_counters[group_name]
+
+        counter = group.counters[counter_id]
+
+        return Response(counter.families)
+
+
+class FamilyCounterDownloadView(QueryBaseView):
+    def __init__(self):
+        super().__init__()
+
+    def get(self, request, common_report_id, group_name, counter_id):
+        assert common_report_id
+
+        common_report = self.gpf_instance.get_common_report(
+            common_report_id
+        )
+
+        if common_report is None:
+            return Response(
+                {"error": f"Common report {common_report_id} not found"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        group = common_report.families_report.families_counters[group_name]
+
+        counter = group.counters[counter_id]
+
+        response = StreamingHttpResponse(
+            ",".join(counter.families),
+            content_type="text/csv"
+        )
+        response["Content-Disposition"] = "attachment; filename=families.csv"
+        response["Expires"] = "0"
+
+        return response
 
 
 class FamiliesDataDownloadView(QueryBaseView):
@@ -48,5 +103,5 @@ class FamiliesDataDownloadView(QueryBaseView):
 
         response["Content-Disposition"] = "attachment; filename=families.ped"
         response["Expires"] = "0"
-        
+
         return response
