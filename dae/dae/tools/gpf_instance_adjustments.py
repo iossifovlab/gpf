c6da2e1fb2803c73a9dc33621c25de2c3c782801
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2022-05-23 14:13:37 +0300

    GPF-1685 adjust impala genotype storage added to adjustments

--- dae/dae/tools/gpf_instance_adjustments.py
+++ dae/dae/tools/gpf_instance_adjustments.py
@@ -1,3 +1,4 @@
+from multiprocessing.sharedctypes import Value
 import os
 import sys
 import logging
@@ -15,7 +16,7 @@ logger = logging.getLogger("gpf_instance_adjustments")
 class VerbosityConfiguration:
     @staticmethod
     def set_argumnets(parser: argparse.ArgumentParser) -> None:
-        parser.add_argument('--verbose', '-v', '-V', action='count', default=0)
+        parser.add_argument("--verbose", "-v", "-V", action="count", default=0)
 
     @staticmethod
     def set(args):
@@ -28,7 +29,7 @@ class VerbosityConfiguration:
 
 
 class AdjustmentsCommand(abc.ABC):
-
+    """Abstract class for adjusting an GPF instance config."""
     def __init__(self, instance_dir):
         self.instance_dir = instance_dir
         self.filename = os.path.join(instance_dir, "gpf_instance.yaml")
@@ -44,9 +45,11 @@ class AdjustmentsCommand(abc.ABC):
 
     @abc.abstractmethod
     def execute(self):
+        """Executes adjustment command."""
         pass
 
     def close(self):
+        """Saves adjusted config."""
         with open(self.filename, "w", encoding="utf8") as outfile:
             outfile.write(yaml.safe_dump(self.config))
 
@@ -58,7 +61,7 @@ class AdjustmentsCommand(abc.ABC):
 
 
 class InstanceIdCommand(AdjustmentsCommand):
-
+    """Adjusts GPF instance ID."""
     def __init__(self, instance_dir, instance_id):
         super().__init__(instance_dir)
         self.instance_id = instance_id
@@ -68,6 +71,32 @@ class InstanceIdCommand(AdjustmentsCommand):
         variables["instance_id"] = self.instance_id
         logger.info(
             "replacing instance id with %s", self.instance_id)
+
+
+class AdjustImpalaStorageCommand(AdjustmentsCommand):
+    """Adjusts impala storage."""
+    def __init__(self, instance_dir, storage_id, hdfs_host, impala_hosts):
+        super().__init__(instance_dir)
+        self.storage_id = storage_id
+        self.hdfs_host = hdfs_host
+        self.impala_hosts = impala_hosts
+
+    def execute(self):
+        storages = self.config["storage"]
+        storage = storages.get(self.storage_id)
+        if storage is None:
+            logger.error(
+                "unable to find storage (%s) in instance at %s",
+                self.storage_id, self.instance_dir)
+            raise ValueError(f"unable to find storage {self.storage_id}")
+
+        if storage.get("storage_type") != "impala":
+            logger.error(
+                "storage %s is not Impala", self.storage_id)
+            raise ValueError(f"storage {self.storage_id} is not Impala")
+
+        storage["hdfs"]["host"] = self.hdfs_host
+        storage["impala"]["hosts"] = self.impala_hosts
 
 
 class StudyConfigsAdjustmentCommand(AdjustmentsCommand):
@@ -207,32 +236,44 @@ def cli(argv=sys.argv[1:]):
     VerbosityConfiguration.set_argumnets(parser)
     parser.add_argument("-i", "--instance", type=str, default=None)
 
-    subparsers = parser.add_subparsers(dest='command',
-                                       help='Command to execute')
+    subparsers = parser.add_subparsers(dest="command",
+                                       help="Command to execute")
 
     parser_instance_id = subparsers.add_parser(
-        'id', help='change the GPF instance ID')
+        "id", help="change the GPF instance ID")
     parser_instance_id.add_argument(
-        'instance_id', type=str,
-        help='new GPF instance ID')
+        "instance_id", type=str,
+        help="new GPF instance ID")
+
+    parser_impala_storage = subparsers.add_parser(
+        "impala-storage", help="adjust the GPF instance impala storage")
+    parser_impala_storage.add_argument(
+        "storage_id", type=str,
+        help="impala storage ID")
+    parser_impala_storage.add_argument(
+        "--impala-hosts", type=str, nargs="+",
+        help="list of impala hosts")
+    parser_impala_storage.add_argument(
+        "--hdfs-host", type=str,
+        help="HDFS host")
 
     parser_genotype_storage = subparsers.add_parser(
-        'storage', help='change the GPF default genotype storage')
+        "storage", help="change the GPF default genotype storage")
     parser_genotype_storage.add_argument(
-        'storage_id', type=str,
-        help='new GPF default genotype storage')
+        "storage_id", type=str,
+        help="new GPF default genotype storage")
 
     parser_disable_studies = subparsers.add_parser(
-        'disable-studies', help='disable studies from GPF instance')
+        "disable-studies", help="disable studies from GPF instance")
     parser_disable_studies.add_argument(
-        'study_id', type=str, nargs="+",
-        help='study IDs to disable')
+        "study_id", type=str, nargs="+",
+        help="study IDs to disable")
 
     parser_enable_studies = subparsers.add_parser(
-        'enable-studies', help='enable studies from GPF instance')
+        "enable-studies", help="enable studies from GPF instance")
     parser_enable_studies.add_argument(
-        'study_id', type=str, nargs="+",
-        help='study IDs to enable')
+        "study_id", type=str, nargs="+",
+        help="study IDs to enable")
 
     args = parser.parse_args(argv)
 
@@ -247,6 +288,12 @@ def cli(argv=sys.argv[1:]):
 
     if args.command == "id":
         with InstanceIdCommand(instance_dir, args.instance_id) as cmd:
+            cmd.execute()
+
+    elif args.command == "impala-storage":
+        with AdjustImpalaStorageCommand(
+                instance_dir, args.storage_id,
+                args.hdfs_host, args.impala_hosts) as cmd:
             cmd.execute()
 
     elif args.command == "storage":
