fbba3bfbe753e2b6937b9d112a928b8685d56420
iordanivanov <iiordan.ivanov@gmail.com>
2022-05-25 15:08:56 +0300

    GPF-1673 Update reset_dev.sh script

--- wdae/wdae/reset_dev.sh
+++ wdae/wdae/reset_dev.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-rm -rf wdae.sql
-rm -rf wdae_django_pre.cache
-rm -rf wdae_django_default.cache
+rm -rf $DAE_DB_DIR/wdae/wdae.sql
+rm -rf $DAE_DB_DIR/wdae/wdae_django_pre.cache
+rm -rf $DAE_DB_DIR/wdae/wdae_django_default.cache
 
 wdaemanage.py migrate
 ./wdae_create_dev_users.sh
