7d0e4b8807033748a7b4fa64f546a91120888efc
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2022-05-25 14:38:50 +0300

    GPF-1666 progress in fsspec protocols

--- dae/dae/genomic_resources/fsspec_protocol.py
+++ dae/dae/genomic_resources/fsspec_protocol.py
@@ -1,9 +1,10 @@
-from bz2 import compress
+import os
 import pathlib
-import gzip
+from urllib.parse import urlparse
 
-from typing import List, Generator, cast
+from typing import List, Generator, cast, Union, Optional
 
+import fsspec  # type: ignore
 import pysam
 
 from dae.genomic_resources.repository import ReadWriteRepositoryProtocol, \
@@ -16,52 +17,81 @@ from dae.genomic_resources.repository import ReadWriteRepositoryProtocol, \
 class FsspecReadWriteProtocol(ReadWriteRepositoryProtocol):
     """Provides filesystem genomic resources repository protocol."""
 
-    def __init__(self, proto_id: str, root_url, filesystem, **kwargs):
-        super().__init__(proto_id)
-        self.root_url = pathlib.Path(root_url)
-        self.filesystem = filesystem
-        self.filesystem.makedirs(self.root_url, exist_ok=True)
+    def __init__(
+            self, proto_id: str, 
+            root_url: Union[str, pathlib.Path],
+            filesystem: fsspec.AbstractFileSystem,
+            **kwargs):
 
-    def _path_to_dict(self, parent: pathlib.Path):
+        super().__init__(proto_id)
+
+        if isinstance(root_url, pathlib.Path):
+            root_url = str(root_url)
+
+        url = urlparse(root_url)
+        self.scheme = url.scheme
+        self.location = url.netloc
+        self.root_path = os.path.join(self.location, url.path)
+
+        self.filesystem = filesystem
+        self.filesystem.makedirs(self.root_path, exist_ok=True)
+
+    def _path_to_dict(self, parent: str, relative: Optional[str] = None):
+        if relative is None:
+            relative = parent
 
         if not self.filesystem.exists(parent):
             return {}
         elif self.filesystem.isdir(parent):
             result = {}
             for entry in self.filesystem.ls(parent, detail=True):
-                path = pathlib.Path(entry["name"]).relative_to(self.root_url)
-                if path.name in {".", ".."}:
+                path = entry["name"]
+                if path.startswith(parent):
+                    path = os.path.relpath(path, parent)
+
+                if path in {".", ".."}:
                     continue
-                if path.name.startswith("."):
+                if path.startswith("."):
                     continue
-                result[path.name] = self._path_to_dict(entry["name"])
+                result[path] = self._path_to_dict(entry["name"], parent)
             return result
 
-        return parent
+        return os.path.relpath(parent, relative)
 
     def collect_all_resources(self) -> Generator[GenomicResource, None, None]:
         """Returns generator for all resources managed by this protocol."""
-        dir_content = self._path_to_dict(self.root_url)
+
+        dir_content = self._path_to_dict(self.root_path, self.root_path)
         for res_id, res_ver in find_genomic_resources_helper(dir_content):
             yield self.build_genomic_resource(res_id, res_ver)
 
-    def get_resource_path(self, resource) -> pathlib.Path:
+    def get_resource_path(self, resource) -> str:
         """Returns directory pathlib.Path for specified resources."""
-        return self.root_url.joinpath(
+        return os.path.join(
+            self.root_path,
             resource.get_genomic_resource_id_version())
 
-    def get_resource_file_path(self, resource, filename: str) -> pathlib.Path:
+    def get_resource_file_path(self, resource, filename: str) -> str:
         """Returns pathlib.Path for a file in a resource."""
-        return self.get_resource_path(resource).joinpath(filename)
+        return os.path.join(
+            self.get_resource_path(resource), filename)
 
     def collect_resource_entries(self, resource) -> List[ManifestEntry]:
         """Returns a list of tuples for all files in a resources."""
-        content_dict = self._path_to_dict(
-            self.get_resource_path(resource))
+        resource_path = self.get_resource_path(resource)
+        content_dict = self._path_to_dict(resource_path)
 
         def my_leaf_to_size_and_time(filepath):
-            fileinfo = self.filesystem.info(filepath)
-            filetime = ManifestEntry.convert_timestamp(fileinfo["mtime"])
+            fileinfo = self.filesystem.info(
+                os.path.join(resource_path, filepath))
+            if self.scheme == "s3":
+                timestamp = fileinfo["LastModified"].timestamp()
+                filetime = ManifestEntry.convert_timestamp(timestamp)
+            elif self.scheme == "":
+                # local filesystem
+                filetime = ManifestEntry.convert_timestamp(fileinfo["mtime"])
+            else:
+                filetime = ManifestEntry.convert_timestamp(fileinfo["mtime"])
             return fileinfo["size"], filetime
 
         result = []
@@ -80,9 +110,10 @@ class FsspecReadWriteProtocol(ReadWriteRepositoryProtocol):
         filepath = self.get_resource_file_path(resource, filename)
         if "w" in mode:
             # Create the containing directory if it doesn't exists.
-            if not self.filesystem.exists(filepath.parent):
+            parent = os.path.dirname(filepath)
+            if not self.filesystem.exists(parent):
                 self.filesystem.mkdir(
-                    filepath.parent, create_parents=True, exist_ok=True)
+                    parent, create_parents=True, exist_ok=True)
 
         compression = None
         if kwargs.get("compression"):
@@ -92,7 +123,7 @@ class FsspecReadWriteProtocol(ReadWriteRepositoryProtocol):
 
     def open_tabix_file(self, resource,  filename,
                         index_filename=None):
-        file_path = str(self.get_resource_file_path(resource, filename))
+        file_path = self.get_resource_file_path(resource, filename)
         index_path = None
         if index_filename:
             index_path = str(self.get_resource_file_path(
--- dae/dae/genomic_resources/tests/test_fsspec_protocol.py
+++ dae/dae/genomic_resources/tests/test_fsspec_protocol.py
@@ -1,3 +1,4 @@
+import os
 import gzip
 import pytest
 
@@ -12,7 +13,7 @@ from dae.genomic_resources.fsspec_protocol import FsspecReadWriteProtocol
 # pylint: disable=redefined-outer-name
 
 @pytest.fixture
-def fsspec_proto(tmp_path):
+def fsspec_proto(tmp_path, s3):
     """Builds fsspec local filesystem protocol fixture."""
     demo_gtf_content = "TP53\tchr3\t300\t200"
     src_repo = GenomicResourceEmbededRepo("src", content={
@@ -21,7 +22,7 @@ def fsspec_proto(tmp_path):
             "data.txt": "alabala"  # NOSONAR
         },
         "sub": {
-            "two[1.0]": {
+            "two": {
                 GR_CONF_FILE_NAME: ["type: gene_models\nfile: genes.gtf",
                                     '2021-11-20T00:00:56'],
                 "genes.txt": demo_gtf_content
@@ -31,36 +32,71 @@ def fsspec_proto(tmp_path):
     dir_repo = GenomicResourceDirRepo('dir', directory=tmp_path)
     dir_repo.store_all_resources(src_repo)
 
-    return FsspecReadWriteProtocol("test", tmp_path, LocalFileSystem())
+    def builder(filesystem="local"):
+        if filesystem == "local":
+            return FsspecReadWriteProtocol(
+                "test", tmp_path, LocalFileSystem())
+
+        if filesystem == "s3":
+
+            s3_path = "s3://test-bucket"
+            for root, _, files in os.walk(dir_repo.directory):
+                for fname in files:
+                    root_rel = os.path.relpath(root, dir_repo.directory)
+                    if root_rel == '.':
+                        root_rel = ''
+                    s3.put(
+                        os.path.join(root, fname),
+                        os.path.join(s3_path, root_rel, fname))
+
+            return FsspecReadWriteProtocol("test", s3_path, s3)
+        return None
+
+    return builder
 
 
-def test_fsspec_proto_simple(fsspec_proto):
+@pytest.mark.parametrize("filesystem", [
+    "local",
+    "s3",
+])
+def test_fsspec_proto_simple(fsspec_proto, filesystem):
     """Simple test for fsspec local filesystem proto"""
-    resources = list(fsspec_proto.collect_all_resources())
+    proto = fsspec_proto(filesystem)
+    resources = list(proto.collect_all_resources())
     assert len(resources) == 2
 
 
-def test_fsspec_proto_resource_paths(fsspec_proto):
+@pytest.mark.parametrize("filesystem", [
+    "local",
+    "s3",
+])
+def test_fsspec_proto_resource_paths(fsspec_proto, filesystem):
     """Tests resource paths."""
+    proto = fsspec_proto(filesystem)
 
-    res = list(fsspec_proto.collect_all_resources())[0]
+    res = list(proto.collect_all_resources())[0]
 
-    res_path = fsspec_proto.get_resource_path(res)
-    assert res_path.relative_to(fsspec_proto.root_url).name == "one"
+    res_path = proto.get_resource_path(res)
+    assert os.path.relpath(res_path, proto.root_path) == "one"
 
-    config_path = fsspec_proto.get_resource_file_path(
+    config_path = proto.get_resource_file_path(
         res, "genomic_resource.yaml")
-    assert config_path.relative_to(fsspec_proto.root_url).name == \
-        "genomic_resource.yaml"
+    assert os.path.relpath(config_path, proto.root_path) == \
+        "one/genomic_resource.yaml"
 
 
-def test_collect_resource_entries(fsspec_proto):
+@pytest.mark.parametrize("filesystem", [
+    "local",
+    "s3",
+])
+def test_collect_resource_entries(fsspec_proto, filesystem):
     """Test collection of resource entries."""
+    proto = fsspec_proto(filesystem)
 
-    res = list(fsspec_proto.collect_all_resources())[0]
+    res = list(proto.collect_all_resources())[0]
     assert res.resource_id == "one"
 
-    entries = fsspec_proto.collect_resource_entries(res)
+    entries = proto.collect_resource_entries(res)
     assert len(entries) == 2
 
     entry = entries[0]
@@ -72,69 +108,96 @@ def test_collect_resource_entries(fsspec_proto):
     assert entry.size == 0
 
 
-def test_file_exists(fsspec_proto):
+@pytest.mark.parametrize("filesystem", [
+    "local",
+    "s3",
+])
+def test_file_exists(fsspec_proto, filesystem):
     """Tests file_exists method."""
+    proto = fsspec_proto(filesystem)
 
-    res = list(fsspec_proto.collect_all_resources())[0]
+    res = list(proto.collect_all_resources())[0]
     assert res.resource_id == "one"
 
-    assert fsspec_proto.file_exists(res, "genomic_resource.yaml")
-    assert not fsspec_proto.file_exists(res, "alabala.txt")
+    assert proto.file_exists(res, "genomic_resource.yaml")
+    assert not proto.file_exists(res, "alabala.txt")
 
 
-def test_open_raw_file_text_read(fsspec_proto):
+@pytest.mark.parametrize("filesystem", [
+    "local",
+    "s3",
+])
+def test_open_raw_file_text_read(fsspec_proto, filesystem):
     """Test simple open with mode='rt'."""
+    proto = fsspec_proto(filesystem)
 
-    res = list(fsspec_proto.collect_all_resources())[0]
+    res = list(proto.collect_all_resources())[0]
     assert res.resource_id == "one"
 
-    with fsspec_proto.open_raw_file(res, "data.txt", mode="rt") as infile:
+    with proto.open_raw_file(res, "data.txt", mode="rt") as infile:
         content = infile.read()
         assert content == "alabala"
 
-def test_open_raw_file_text_write(fsspec_proto):
+@pytest.mark.parametrize("filesystem", [
+    "local",
+    "s3",
+])
+def test_open_raw_file_text_write(fsspec_proto, filesystem):
     """Test simple open with mode='wt'."""
+    proto = fsspec_proto(filesystem)
 
-    res = list(fsspec_proto.collect_all_resources())[0]
+    res = list(proto.collect_all_resources())[0]
     assert res.resource_id == "one"
 
-    with fsspec_proto.open_raw_file(res, "new_data.txt", mode="wt") as infile:
+    with proto.open_raw_file(res, "new_data.txt", mode="wt") as infile:
         infile.write("new alabala")
 
-    assert fsspec_proto.file_exists(res, "new_data.txt")
+    assert proto.file_exists(res, "new_data.txt")
 
 
-def test_open_raw_file_text_write_compression(fsspec_proto):
+@pytest.mark.parametrize("filesystem", [
+    "local",
+    "s3",
+])
+def test_open_raw_file_text_write_compression(fsspec_proto, filesystem):
     """Test open with mode='wt' and compression."""
+    proto = fsspec_proto(filesystem)
 
-    res = list(fsspec_proto.collect_all_resources())[0]
+    res = list(proto.collect_all_resources())[0]
     assert res.resource_id == "one"
 
-    with fsspec_proto.open_raw_file(
+    with proto.open_raw_file(
             res, "new_data.txt.gz", mode="wt", compression=True) as outfile:
         outfile.write("new alabala")
 
-    assert fsspec_proto.file_exists(res, "new_data.txt.gz")
+    assert proto.file_exists(res, "new_data.txt.gz")
 
-    filepath = fsspec_proto.get_resource_file_path(res, "new_data.txt.gz")
-    with gzip.open(filepath, mode="rt") as infile:
+    filepath = proto.get_resource_file_path(res, "new_data.txt.gz")
+    with gzip.open(
+            proto.filesystem.open(filepath), mode="rt") as infile:
         content = infile.read()
         assert content == "new alabala"
 
 
-def test_open_raw_file_text_read_compression(fsspec_proto):
+@pytest.mark.parametrize("filesystem", [
+    "local",
+    "s3",
+])
+def test_open_raw_file_text_read_compression(fsspec_proto, filesystem):
     """Test open with mode='rt' and compression."""
+    proto = fsspec_proto(filesystem)
 
-    res = list(fsspec_proto.collect_all_resources())[0]
+    res = list(proto.collect_all_resources())[0]
     assert res.resource_id == "one"
 
-    filepath = fsspec_proto.get_resource_file_path(res, "new_data.txt.gz")
-    with gzip.open(filepath, mode="wt") as outfile:
+    filepath = proto.get_resource_file_path(res, "new_data.txt.gz")
+    with proto.filesystem.open(
+            filepath, mode="wt", compression="gzip") as outfile:
         outfile.write("new alabala")
 
-    assert fsspec_proto.file_exists(res, "new_data.txt.gz")
+    assert proto.file_exists(res, "new_data.txt.gz")
 
-    with fsspec_proto.open_raw_file(
+    with proto.open_raw_file(
             res, "new_data.txt.gz", mode="rt", compression=True) as infile:
         content = infile.read()
         assert content == "new alabala"
