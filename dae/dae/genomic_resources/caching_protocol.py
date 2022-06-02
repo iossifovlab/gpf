a32c2bd1f2f26c625747e81247100666bea575a7
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2022-06-02 10:20:14 +0300

    GPF-1666 caching protocol implementation and testing

--- dae/dae/genomic_resources/caching_protocol.py
+++ dae/dae/genomic_resources/caching_protocol.py
@@ -3,6 +3,7 @@
 from typing import Generator, Optional
 
 from dae.genomic_resources.repository import \
+    GR_CONF_FILE_NAME, \
     ReadOnlyRepositoryProtocol, \
     ReadWriteRepositoryProtocol, \
     GenomicResource, \
@@ -31,26 +32,53 @@ class CachingProtocol(ReadOnlyRepositoryProtocol):
 
         remote_resource = self.remote_protocol.get_resource(
             resource_id, version_constraint)
+        local_resource = self.local_protocol.get_or_create_resource(
+            remote_resource.resource_id, remote_resource.version)
 
-        local_resource = self.local_protocol.update_resource(remote_resource)
-        return local_resource
+        self.local_protocol.update_resource_file(
+            remote_resource, local_resource, GR_CONF_FILE_NAME)
+        self.local_protocol.invalidate()
+
+        return self.local_protocol.get_resource(
+            remote_resource.resource_id,
+            f"={remote_resource.get_version_str()}")
+
+    def _refresh_resource_file(
+            self, resource: GenomicResource, filename: str) -> None:
+
+        remote_resource = self.remote_protocol.get_resource(
+            resource.resource_id,
+            f"={resource.get_version_str()}")
+
+        self.local_protocol.update_resource_file(
+            remote_resource, resource, filename)
 
     def open_raw_file(self, resource, filename, mode="rt", **kwargs):
         assert resource.repo == self.local_protocol
 
+        if "w" in mode:
+            raise IOError(
+                f"Read-Only caching protocol {self.get_id()} trying to open "
+                f"{filename} for writing")
+
+        self._refresh_resource_file(resource, filename)
         return self.local_protocol.open_raw_file(
             resource, filename, mode, **kwargs)
 
     def open_tabix_file(self, resource, filename, index_filename=None):
         assert resource.repo == self.local_protocol
+        self._refresh_resource_file(resource, filename)
 
         return self.local_protocol.open_tabix_file(
             resource, filename, index_filename)
 
     def file_exists(self, resource, filename) -> bool:
         assert resource.repo == self.local_protocol
+        self._refresh_resource_file(resource, filename)
 
         return self.local_protocol.file_exists(resource, filename)
 
     def load_manifest(self, resource: GenomicResource) -> Manifest:
+        self._refresh_resource_file(resource, GR_CONF_FILE_NAME)
+
         return self.local_protocol.load_manifest(resource)
--- dae/dae/genomic_resources/tests/test_caching_protocol.py
+++ dae/dae/genomic_resources/tests/test_caching_protocol.py
@@ -1,4 +1,5 @@
 # pylint: disable=redefined-outer-name,C0114,C0116,protected-access
+import pytest
 
 from dae.genomic_resources.embedded_protocol import \
     build_embedded_protocol
@@ -21,3 +22,75 @@ def test_caching_repo_simple(embedded_proto, tmp_path):
     assert caching_proto is not None
 
     assert len(list(caching_proto.get_all_resources())) == 4
+
+
+@pytest.fixture
+def caching_proto(embedded_proto, tmp_path):
+
+    remote_proto = embedded_proto(tmp_path / "source")
+    local_proto = build_embedded_protocol(
+        "local", str(tmp_path / "cache"), {})
+
+    proto = CachingProtocol("test_cache", remote_proto, local_proto)
+    return proto
+
+
+def test_get_resource_three(caching_proto):
+
+    res = caching_proto.get_resource("three")
+
+    assert res.resource_id == "three"
+    assert res.version == (2, 0)
+
+
+def test_get_resource_two(caching_proto):
+
+    res = caching_proto.get_resource("sub/two")
+
+    assert res.resource_id == "sub/two"
+    assert res.version == (1, 0)
+
+
+def test_get_resource_copies_only_resource_config_three(caching_proto):
+
+    res = caching_proto.get_resource("three")
+
+    local_proto = caching_proto.local_protocol
+    assert local_proto.file_exists(res, "genomic_resource.yaml")
+    assert not local_proto.file_exists(res, "sub1/a.txt")
+    assert not local_proto.file_exists(res, "sub2/b.txt")
+
+
+def test_get_resource_copies_only_resource_config_two(caching_proto):
+
+    res = caching_proto.get_resource("sub/two")
+
+    local_proto = caching_proto.local_protocol
+    assert local_proto.file_exists(res, "genomic_resource.yaml")
+    assert not local_proto.file_exists(res, "genes.gtf")
+
+
+def test_open_raw_file_copies_the_file_three_a(caching_proto):
+
+    res = caching_proto.get_resource("three")
+    with caching_proto.open_raw_file(res, "sub1/a.txt") as infile:
+        content = infile.read()
+    assert content == "a"
+
+    local_proto = caching_proto.local_protocol
+    assert local_proto.file_exists(res, "genomic_resource.yaml")
+    assert local_proto.file_exists(res, "sub1/a.txt")
+    assert not local_proto.file_exists(res, "sub2/b.txt")
+
+
+def test_open_raw_file_copies_the_file_three_b(caching_proto):
+
+    res = caching_proto.get_resource("three")
+    with caching_proto.open_raw_file(res, "sub2/b.txt") as infile:
+        content = infile.read()
+    assert content == "b"
+
+    local_proto = caching_proto.local_protocol
+    assert local_proto.file_exists(res, "genomic_resource.yaml")
+    assert local_proto.file_exists(res, "sub2/b.txt")
+    assert not local_proto.file_exists(res, "sub1/a.txt")
