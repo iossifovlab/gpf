9b3cd50cdd44404d4019834bca4aab2b909ba663
Ivo Stefanov <ivostefanovtues@gmail.com>
2022-06-03 12:08:52 +0300

    GPF-1705 add split pipe unit test

--- gpfjs/src/app/utils/split.pipe.spec.ts
+++ gpfjs/src/app/utils/split.pipe.spec.ts
@@ -1,8 +1,16 @@
 import { SplitPipe } from './split.pipe';
 
 describe('SplitPipe', () => {
-  it('create an instance', () => {
+  it('should create an instance', () => {
     const pipe = new SplitPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should split strings', () => {
+    const pipe = new SplitPipe();
+
+    expect(pipe.transform('abc.de.f', '.')).toEqual(['abc', 'de', 'f']);
+    expect(pipe.transform('abc/def/g', '/')).toEqual(['abc', 'def', 'g']);
+    expect(pipe.transform('abcdbef', 'b')).toEqual(['a', 'cd', 'ef']);
   });
 });
--- gpfjs/src/app/utils/split.pipe.ts
+++ gpfjs/src/app/utils/split.pipe.ts
@@ -1,5 +1,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
-
+/**
+ * Simple string split pipe
+ */
 @Pipe({name: 'split'})
 export class SplitPipe implements PipeTransform {
   public transform(text: string, separator: string): string[] {
