b9e5471a1df5c3bb13a52a7458e039955d6a47c3
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2022-06-28 09:24:07 +0300

    GPF-1723 reorganization

--- dae/dae/pedigrees/family_tag_builder.py
+++ dae/dae/pedigrees/family_tag_builder.py
@@ -6,226 +6,249 @@ from dae.variants.attributes import Role, Status, Sex
 from dae.pedigrees.family import Person, Family
 
 
-class FamilyTagBuilder:  # pylint: disable=too-many-public-methods
-    """Tag families with a series of predefined tags."""
 
-    def __init__(self, family: Family):
-        self.family = family
+def get_mom(family: Family) -> Optional[Person]:
+    for person in family.members_in_order:
+        if person.role == Role.mom:
+            return person
+    return None
 
-    def get_mom(self) -> Optional[Person]:
-        for person in self.family.members_in_order:
-            if person.role == Role.mom:
-                return person
-        return None
+def get_dad(family: Family) -> Optional[Person]:
+    for person in family.members_in_order:
+        if person.role == Role.dad:
+            return person
+    return None
 
-    def get_dad(self) -> Optional[Person]:
-        for person in self.family.members_in_order:
-            if person.role == Role.dad:
-                return person
-        return None
+def get_prb(family: Family) -> Optional[Person]:
+    for person in family.members_in_order:
+        if person.role == Role.prb:
+            return person
+    return None
 
-    def get_prb(self) -> Optional[Person]:
-        for person in self.family.members_in_order:
-            if person.role == Role.prb:
-                return person
-        return None
+def get_sibs(family: Family) -> Iterable[Person]:
+    result = []
+    for person in family.members_in_order:
+        if person.role == Role.sib:
+            result.append(person)
+    return result
 
-    def get_sibs(self) -> Iterable[Person]:
-        result = []
-        for person in self.family.members_in_order:
-            if person.role == Role.sib:
-                result.append(person)
-        return result
+def tag(family: Family, label: str, value) -> None:
+    for person in family.persons.values():
+        person.set_attr(label, value)
 
-    def tag(self, label, value) -> None:
-        for person in self.family.persons.values():
-            person.set_attr(label, value)
-
-    def check_tag(self, label, value) -> bool:
-        for person in self.family.persons.values():
-            if not person.has_attr(label):
-                raise ValueError(f"preson has no attributes {label}")
-            if person.get_attr(label) != value:
-                return False
-        return True
-
-    def check_nuclear_family(self) -> bool:
-        """Check if the family is a nuclear family."""
-        mom = self.get_mom()
-        dad = self.get_dad()
-
-        if mom is None or dad is None:
+def check_tag(family: Family, label: str, value) -> bool:
+    for person in family.persons.values():
+        if not person.has_attr(label):
+            raise ValueError(f"preson has no attributes {label}")
+        if person.get_attr(label) != value:
             return False
+    return True
 
-        for person in self.family.persons.values():
-            if person.person_id in {mom.person_id, dad.person_id}:
-                continue
 
-            if not person.has_both_parents():
-                return False
-            if person.mom_id != mom.person_id \
-                    or person.dad_id != dad.person_id:
-                return False
+def check_nuclear_family(family: Family) -> bool:
+    """Check if the family is a nuclear family."""
+    mom = get_mom(family)
+    dad = get_dad(family)
 
-        return True
-
-    def tag_nuclear_family(self) -> bool:
-        """Set nuclear family tag to the family."""
-        value = self.check_nuclear_family()
-        self.tag("tag_nuclear_family", value)
-        return value
-
-    def check_quad_family(self) -> bool:
-        if len(self.family) != 4:
-            return False
-        if not self.check_nuclear_family():
-            return False
-
-        return True
-
-    def tag_quad_family(self) -> bool:
-        """Set quad family tag to the family."""
-        value = self.check_quad_family()
-        self.tag("tag_quad_family", value)
-        return value
-
-    def check_trio_family(self) -> bool:
-        if len(self.family) != 3:
-            return False
-        if not self.check_nuclear_family():
-            return False
-
-        return True
-
-    def tag_trio_family(self) -> bool:
-        """Set trio family tag to the family."""
-        value = self.check_trio_family()
-        self.tag("tag_trio_family", value)
-        return value
-
-    def check_simplex_family(self) -> bool:
-        return len(list(filter(
-            lambda p: p.status == Status.affected,
-            self.family.persons.values()))) == 1
-
-    def tag_simplex_family(self) -> bool:
-        """Set simplex family tag to the family."""
-        value = self.check_simplex_family()
-        self.tag("tag_simplex_family", value)
-        return value
-
-    def check_multiplex_family(self) -> bool:
-        return len(list(filter(
-            lambda p: p.status == Status.affected,
-            self.family.persons.values()))) > 1
-
-    def tag_multiplex_family(self) -> bool:
-        """Set multiplex family tag to the family."""
-        value = self.check_multiplex_family()
-        self.tag("tag_multiplex_family", value)
-        return value
-
-    def check_control_family(self) -> bool:
-        return len(list(filter(
-            lambda p: p.status == Status.affected,
-            self.family.persons.values()))) == 0
-
-    def tag_control_family(self) -> bool:
-        """Set control family tag to the family."""
-        value = self.check_control_family()
-        self.tag("tag_control_family", value)
-        return value
-
-    def check_affected_dad_family(self) -> bool:
-        dad = self.get_dad()
-        if dad is None:
-            return False
-        return bool(dad.status == Status.affected)
-
-    def tag_affected_dad_family(self) -> bool:
-        """Set affected dad family tag to the family."""
-        value = self.check_affected_dad_family()
-        self.tag("tag_affected_dad_family", value)
-        return value
-
-    def check_affected_mom_family(self) -> bool:
-        mom = self.get_mom()
-        if mom is None:
-            return False
-        return bool(mom.status == Status.affected)
-
-    def tag_affected_mom_family(self) -> bool:
-        """Set affected mom family tag to the family."""
-        value = self.check_affected_mom_family()
-        self.tag("tag_affected_mom_family", value)
-        return value
-
-    def check_affected_prb_family(self) -> bool:
-        prb = self.get_prb()
-        if prb is None:
-            return False
-        return bool(prb.status == Status.affected)
-
-    def tag_affected_prb_family(self) -> bool:
-        """Set affected proband family tag to the family."""
-        value = self.check_affected_prb_family()
-        self.tag("tag_affected_prb_family", value)
-        return value
-
-    def check_affected_sib_family(self) -> bool:
-        for sib in self.get_sibs():
-            if sib.status == Status.affected:
-                return True
+    if mom is None or dad is None:
         return False
 
-    def tag_affected_sib_family(self) -> bool:
-        """Set affected sibling family tag to the family."""
-        value = self.check_affected_sib_family()
-        self.tag("tag_affected_sib_family", value)
-        return value
+    for person in family.persons.values():
+        if person.person_id in {mom.person_id, dad.person_id}:
+            continue
 
-    def check_male_prb_family(self) -> bool:
-        prb = self.get_prb()
-        if prb is None:
+        if not person.has_both_parents():
             return False
-        return bool(prb.sex == Sex.male)
-
-    def tag_male_prb_family(self) -> bool:
-        """Set male proband family tag to the family."""
-        value = self.check_male_prb_family()
-        self.tag("tag_male_prb_family", value)
-        return value
-
-    def check_female_prb_family(self) -> bool:
-        prb = self.get_prb()
-        if prb is None:
+        if person.mom_id != mom.person_id \
+                or person.dad_id != dad.person_id:
             return False
-        return bool(prb.sex == Sex.female)
 
-    def tag_female_prb_family(self) -> bool:
-        """Set female proband family tag to the family."""
-        value = self.check_female_prb_family()
-        self.tag("tag_female_prb_family", value)
-        return value
+    return True
 
-    def check_missing_mom_family(self) -> bool:
-        if self.get_mom() is None:
+
+def tag_nuclear_family(family: Family) -> bool:
+    """Set nuclear family tag to the family."""
+    value = check_nuclear_family(family)
+    tag(family, "tag_nuclear_family", value)
+    return value
+
+
+def check_quad_family(family: Family) -> bool:
+    if len(family) != 4:
+        return False
+    if not check_nuclear_family(family):
+        return False
+
+    return True
+
+
+def tag_quad_family(family: Family) -> bool:
+    """Set quad family tag to the family."""
+    value = check_quad_family(family)
+    tag(family, "tag_quad_family", value)
+    return value
+
+
+def check_trio_family(family: Family) -> bool:
+    if len(family) != 3:
+        return False
+    if not check_nuclear_family(family):
+        return False
+
+    return True
+
+
+def tag_trio_family(family: Family) -> bool:
+    """Set trio family tag to the family."""
+    value = check_trio_family(family)
+    tag(family, "tag_trio_family", value)
+    return value
+
+
+def check_simplex_family(family: Family) -> bool:
+    return len(list(filter(
+        lambda p: p.status == Status.affected,
+        family.persons.values()))) == 1
+
+
+def tag_simplex_family(family: Family) -> bool:
+    """Set simplex family tag to the family."""
+    value = check_simplex_family(family)
+    tag(family, "tag_simplex_family", value)
+    return value
+
+
+def check_multiplex_family(family: Family) -> bool:
+    return len(list(filter(
+        lambda p: p.status == Status.affected,
+        family.persons.values()))) > 1
+
+
+def tag_multiplex_family(family: Family) -> bool:
+    """Set multiplex family tag to the family."""
+    value = check_multiplex_family(family)
+    tag(family, "tag_multiplex_family", value)
+    return value
+
+
+def check_control_family(family: Family) -> bool:
+    return len(list(filter(
+        lambda p: p.status == Status.affected,
+        family.persons.values()))) == 0
+
+
+def tag_control_family(family: Family) -> bool:
+    """Set control family tag to the family."""
+    value = check_control_family(family)
+    tag(family, "tag_control_family", value)
+    return value
+
+
+def check_affected_dad_family(family: Family) -> bool:
+    dad = get_dad(family)
+    if dad is None:
+        return False
+    return bool(dad.status == Status.affected)
+
+
+def tag_affected_dad_family(family: Family) -> bool:
+    """Set affected dad family tag to the family."""
+    value = check_affected_dad_family(family)
+    tag(family, "tag_affected_dad_family", value)
+    return value
+
+
+def check_affected_mom_family(family: Family) -> bool:
+    mom = get_mom(family)
+    if mom is None:
+        return False
+    return bool(mom.status == Status.affected)
+
+
+def tag_affected_mom_family(family: Family) -> bool:
+    """Set affected mom family tag to the family."""
+    value = check_affected_mom_family(family)
+    tag(family, "tag_affected_mom_family", value)
+    return value
+
+
+def check_affected_prb_family(family: Family) -> bool:
+    prb = get_prb(family)
+    if prb is None:
+        return False
+    return bool(prb.status == Status.affected)
+
+
+def tag_affected_prb_family(family: Family) -> bool:
+    """Set affected proband family tag to the family."""
+    value = check_affected_prb_family(family)
+    tag(family, "tag_affected_prb_family", value)
+    return value
+
+
+def check_affected_sib_family(family: Family) -> bool:
+    for sib in get_sibs(family):
+        if sib.status == Status.affected:
             return True
+    return False
+
+
+def tag_affected_sib_family(family: Family) -> bool:
+    """Set affected sibling family tag to the family."""
+    value = check_affected_sib_family(family)
+    tag(family, "tag_affected_sib_family", value)
+    return value
+
+
+def check_male_prb_family(family: Family) -> bool:
+    prb = get_prb(family)
+    if prb is None:
         return False
+    return bool(prb.sex == Sex.male)
 
-    def tag_missing_mom_family(self) -> bool:
-        """Set missing mom family tag to the family."""
-        value = self.check_missing_mom_family()
-        self.tag("tag_missing_mom_family", value)
-        return value
 
-    def check_missing_dad_family(self) -> bool:
-        if self.get_dad() is None:
-            return True
+def tag_male_prb_family(family: Family) -> bool:
+    """Set male proband family tag to the family."""
+    value = check_male_prb_family(family)
+    tag(family, "tag_male_prb_family", value)
+    return value
+
+
+def check_female_prb_family(family: Family) -> bool:
+    prb = get_prb(family)
+    if prb is None:
         return False
+    return bool(prb.sex == Sex.female)
 
-    def tag_missing_dad_family(self) -> bool:
-        """Set missing dad family tag to the family."""
-        value = self.check_missing_dad_family()
-        self.tag("tag_missing_dad_family", value)
-        return value
+
+def tag_female_prb_family(family: Family) -> bool:
+    """Set female proband family tag to the family."""
+    value = check_female_prb_family(family)
+    tag(family, "tag_female_prb_family", value)
+    return value
+
+
+def check_missing_mom_family(family: Family) -> bool:
+    if get_mom(family) is None:
+        return True
+    return False
+
+
+def tag_missing_mom_family(family: Family) -> bool:
+    """Set missing mom family tag to the family."""
+    value = check_missing_mom_family(family)
+    tag(family, "tag_missing_mom_family", value)
+    return value
+
+
+def check_missing_dad_family(family: Family) -> bool:
+    if get_dad(family) is None:
+        return True
+    return False
+
+
+def tag_missing_dad_family(family: Family) -> bool:
+    """Set missing dad family tag to the family."""
+    value = check_missing_dad_family(family)
+    tag(family, "tag_missing_dad_family", value)
+    return value
--- dae/dae/pedigrees/tests/test_family_tag_builder.py
+++ dae/dae/pedigrees/tests/test_family_tag_builder.py
@@ -2,7 +2,21 @@
 
 import pytest
 
-from dae.pedigrees.family_tag_builder import FamilyTagBuilder
+from dae.pedigrees.family_tag_builder import check_tag, \
+    tag_nuclear_family, \
+    tag_quad_family, \
+    tag_trio_family, \
+    tag_simplex_family, \
+    tag_multiplex_family, \
+    tag_control_family, \
+    tag_affected_dad_family, \
+    tag_affected_mom_family, \
+    tag_affected_prb_family, \
+    tag_affected_sib_family, \
+    tag_male_prb_family, \
+    tag_female_prb_family, \
+    tag_missing_mom_family, \
+    tag_missing_dad_family
 from dae.pedigrees.testing import build_family
 
 
@@ -21,42 +35,33 @@ def fam1_fixture():
 
 def test_tag_nuclear_family_simple(fam1_fixture):
 
-    tagger = FamilyTagBuilder(fam1_fixture)
-    assert tagger.tag_nuclear_family()
+    assert tag_nuclear_family(fam1_fixture)
 
-    assert tagger.check_tag("tag_nuclear_family", True)
+    assert check_tag(fam1_fixture, "tag_nuclear_family", True)
 
 
 def test_tag_quad_family_simple(fam1_fixture):
 
-    tagger = FamilyTagBuilder(fam1_fixture)
-    assert tagger.tag_quad_family()
-
-    assert tagger.check_tag("tag_quad_family", True)
+    assert tag_quad_family(fam1_fixture)
+    assert check_tag(fam1_fixture, "tag_quad_family", True)
 
 
 def test_tag_trio_family_simple(fam1_fixture):
 
-    tagger = FamilyTagBuilder(fam1_fixture)
-    assert not tagger.tag_trio_family()
-
-    assert tagger.check_tag("tag_trio_family", False)
+    assert not tag_trio_family(fam1_fixture)
+    assert check_tag(fam1_fixture, "tag_trio_family", False)
 
 
 def test_tag_simplex_family_simple(fam1_fixture):
 
-    tagger = FamilyTagBuilder(fam1_fixture)
-    assert tagger.tag_simplex_family()
-
-    assert tagger.check_tag("tag_simplex_family", True)
+    assert tag_simplex_family(fam1_fixture)
+    assert check_tag(fam1_fixture, "tag_simplex_family", True)
 
 
 def test_tag_multiplex_family_simple(fam1_fixture):
 
-    tagger = FamilyTagBuilder(fam1_fixture)
-    assert not tagger.tag_multiplex_family()
-
-    assert tagger.check_tag("tag_multiplex_family", False)
+    assert not tag_multiplex_family(fam1_fixture)
+    assert check_tag(fam1_fixture, "tag_multiplex_family", False)
 
 
 def test_tag_multiplex_family():
@@ -70,18 +75,14 @@ def test_tag_multiplex_family():
             f1       s1       d1     m1     1   2      sib
         """)
 
-    tagger = FamilyTagBuilder(fam)
-    assert tagger.tag_multiplex_family()
-
-    assert tagger.check_tag("tag_multiplex_family", True)
+    assert tag_multiplex_family(fam)
+    assert check_tag(fam, "tag_multiplex_family", True)
 
 
 def test_tag_control_family_simple(fam1_fixture):
 
-    tagger = FamilyTagBuilder(fam1_fixture)
-    assert not tagger.tag_control_family()
-
-    assert tagger.check_tag("tag_control_family", False)
+    assert not tag_control_family(fam1_fixture)
+    assert check_tag(fam1_fixture, "tag_control_family", False)
 
 
 def test_tag_control_family():
@@ -95,18 +96,14 @@ def test_tag_control_family():
             f1       s1       d1     m1     1   1      sib
         """)
 
-    tagger = FamilyTagBuilder(fam)
-    assert tagger.tag_control_family()
-
-    assert tagger.check_tag("tag_control_family", True)
+    assert tag_control_family(fam)
+    assert check_tag(fam, "tag_control_family", True)
 
 
 def test_tag_affected_dad_family_simple(fam1_fixture):
 
-    tagger = FamilyTagBuilder(fam1_fixture)
-    assert not tagger.tag_affected_dad_family()
-
-    assert tagger.check_tag("tag_affected_dad_family", False)
+    assert not tag_affected_dad_family(fam1_fixture)
+    assert check_tag(fam1_fixture, "tag_affected_dad_family", False)
 
 
 def test_tag_affected_dad_family():
@@ -120,18 +117,14 @@ def test_tag_affected_dad_family():
             f1       s1       d1     m1     1   1      sib
         """)
 
-    tagger = FamilyTagBuilder(fam)
-    assert tagger.tag_affected_dad_family()
-
-    assert tagger.check_tag("tag_affected_dad_family", True)
+    assert tag_affected_dad_family(fam)
+    assert check_tag(fam, "tag_affected_dad_family", True)
 
 
 def test_tag_affected_mom_family_simple(fam1_fixture):
 
-    tagger = FamilyTagBuilder(fam1_fixture)
-    assert not tagger.tag_affected_mom_family()
-
-    assert tagger.check_tag("tag_affected_mom_family", False)
+    assert not tag_affected_mom_family(fam1_fixture)
+    assert check_tag(fam1_fixture, "tag_affected_mom_family", False)
 
 
 def test_tag_affected_mom_family():
@@ -145,18 +138,14 @@ def test_tag_affected_mom_family():
             f1       s1       d1     m1     1   1      sib
         """)
 
-    tagger = FamilyTagBuilder(fam)
-    assert tagger.tag_affected_mom_family()
-
-    assert tagger.check_tag("tag_affected_mom_family", True)
+    assert tag_affected_mom_family(fam)
+    assert check_tag(fam, "tag_affected_mom_family", True)
 
 
 def test_tag_affected_prb_family_simple(fam1_fixture):
 
-    tagger = FamilyTagBuilder(fam1_fixture)
-    assert tagger.tag_affected_prb_family()
-
-    assert tagger.check_tag("tag_affected_prb_family", True)
+    assert tag_affected_prb_family(fam1_fixture)
+    assert check_tag(fam1_fixture, "tag_affected_prb_family", True)
 
 
 def test_tag_affected_prb_family():
@@ -170,18 +159,14 @@ def test_tag_affected_prb_family():
             f1       s1       d1     m1     1   1      sib
         """)
 
-    tagger = FamilyTagBuilder(fam)
-    assert not tagger.tag_affected_prb_family()
-
-    assert tagger.check_tag("tag_affected_prb_family", False)
+    assert not tag_affected_prb_family(fam)
+    assert check_tag(fam, "tag_affected_prb_family", False)
 
 
 def test_tag_affected_sib_family_simple(fam1_fixture):
 
-    tagger = FamilyTagBuilder(fam1_fixture)
-    assert not tagger.tag_affected_sib_family()
-
-    assert tagger.check_tag("tag_affected_sib_family", False)
+    assert not tag_affected_sib_family(fam1_fixture)
+    assert check_tag(fam1_fixture, "tag_affected_sib_family", False)
 
 
 def test_tag_affected_sib_family():
@@ -196,18 +181,14 @@ def test_tag_affected_sib_family():
             f1       s2       d1     m1     1   2      sib
         """)
 
-    tagger = FamilyTagBuilder(fam)
-    assert tagger.tag_affected_sib_family()
-
-    assert tagger.check_tag("tag_affected_sib_family", True)
+    assert tag_affected_sib_family(fam)
+    assert check_tag(fam,"tag_affected_sib_family", True)
 
 
 def test_tag_male_prb_family_simple(fam1_fixture):
 
-    tagger = FamilyTagBuilder(fam1_fixture)
-    assert not tagger.tag_male_prb_family()
-
-    assert tagger.check_tag("tag_male_prb_family", False)
+    assert not tag_male_prb_family(fam1_fixture)
+    assert check_tag(fam1_fixture, "tag_male_prb_family", False)
 
 
 def test_tag_male_prb_family():
@@ -222,18 +203,14 @@ def test_tag_male_prb_family():
             f1       s2       d1     m1     2   2      sib
         """)
 
-    tagger = FamilyTagBuilder(fam)
-    assert tagger.tag_male_prb_family()
-
-    assert tagger.check_tag("tag_male_prb_family", True)
+    assert tag_male_prb_family(fam)
+    assert check_tag(fam, "tag_male_prb_family", True)
 
 
 def test_tag_female_prb_family_simple(fam1_fixture):
 
-    tagger = FamilyTagBuilder(fam1_fixture)
-    assert tagger.tag_female_prb_family()
-
-    assert tagger.check_tag("tag_female_prb_family", True)
+    assert tag_female_prb_family(fam1_fixture)
+    assert check_tag(fam1_fixture, "tag_female_prb_family", True)
 
 
 def test_tag_female_prb_family():
@@ -248,18 +225,14 @@ def test_tag_female_prb_family():
             f1       s2       d1     m1     2   2      sib
         """)
 
-    tagger = FamilyTagBuilder(fam)
-    assert not tagger.tag_female_prb_family()
-
-    assert tagger.check_tag("tag_female_prb_family", False)
+    assert not tag_female_prb_family(fam)
+    assert check_tag(fam, "tag_female_prb_family", False)
 
 
 def test_tag_missing_mom_family_simple(fam1_fixture):
 
-    tagger = FamilyTagBuilder(fam1_fixture)
-    assert not tagger.tag_missing_mom_family()
-
-    assert tagger.check_tag("tag_missing_mom_family", False)
+    assert not tag_missing_mom_family(fam1_fixture)
+    assert check_tag(fam1_fixture, "tag_missing_mom_family", False)
 
 
 def test_tag_missing_mom_family():
@@ -272,10 +245,8 @@ def test_tag_missing_mom_family():
             f1       p1       d1     m1     1   2      prb  0
         """)
 
-    tagger = FamilyTagBuilder(fam)
-    assert tagger.tag_missing_mom_family()
-
-    assert tagger.check_tag("tag_missing_mom_family", True)
+    assert tag_missing_mom_family(fam)
+    assert check_tag(fam, "tag_missing_mom_family", True)
 
 
 def test_tag_missing_mom_family_again():
@@ -287,18 +258,14 @@ def test_tag_missing_mom_family_again():
             f1       p1       d1     0      1   2      prb
         """)
 
-    tagger = FamilyTagBuilder(fam)
-    assert tagger.tag_missing_mom_family()
-
-    assert tagger.check_tag("tag_missing_mom_family", True)
+    assert tag_missing_mom_family(fam)
+    assert check_tag(fam, "tag_missing_mom_family", True)
 
 
 def test_tag_missing_dad_family_simple(fam1_fixture):
 
-    tagger = FamilyTagBuilder(fam1_fixture)
-    assert not tagger.tag_missing_dad_family()
-
-    assert tagger.check_tag("tag_missing_dad_family", False)
+    assert not tag_missing_dad_family(fam1_fixture)
+    assert check_tag(fam1_fixture, "tag_missing_dad_family", False)
 
 
 def test_tag_missing_dad_family():
@@ -311,10 +278,8 @@ def test_tag_missing_dad_family():
             f1       p1       d1     m1     1   2      prb  0
         """)
 
-    tagger = FamilyTagBuilder(fam)
-    assert tagger.tag_missing_dad_family()
-
-    assert tagger.check_tag("tag_missing_dad_family", True)
+    assert tag_missing_dad_family(fam)
+    assert check_tag(fam, "tag_missing_dad_family", True)
 
 
 def test_tag_missing_dad_family_again():
@@ -326,7 +291,5 @@ def test_tag_missing_dad_family_again():
             f1       p1       0      m1     1   2      prb
         """)
 
-    tagger = FamilyTagBuilder(fam)
-    assert tagger.tag_missing_dad_family()
-
-    assert tagger.check_tag("tag_missing_dad_family", True)
+    assert tag_missing_dad_family(fam)
+    assert check_tag(fam, "tag_missing_dad_family", True)
