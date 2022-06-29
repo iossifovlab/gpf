07758fc3c08ed7346a07658fc0718118ba4552ee
Svetlin Mladenov <svetlinm@seqpipe.com>
2022-06-29 12:10:32 +0300

    Add a high level test for schema2

---
+++ dae/dae/backends/schema2/tests/test_schema2.py
@@ -0,0 +1,7 @@
+def test_import_and_query(tmpdir):
+    # run vcf2schema2 on the input vcf files
+    # copy resulting parquets in hdfs
+    # load parquets in impala
+    # query impala
+    # assert atleast the number of summary and family allies is the same
+    pass
