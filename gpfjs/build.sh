ba07395b494edc84b4938ff955d92b233e7885cd
joankosev <joankosev@gmail.com>
2022-07-08 11:52:13 +0300

    Change node/npm version to 14

--- gpfjs/build.sh
+++ gpfjs/build.sh
@@ -48,6 +48,9 @@ function main() {
   {
     build_run rm -rf dist
     build_run rm -rf node_modules package-lock.json
+    build_run apt-get remove nodejs
+    build_run bash -c 'curl -sL https://deb.nodesource.com/setup_14.x  | bash -'
+    build_run apt-get install -y nodejs
     build_run npm install
   }
 
