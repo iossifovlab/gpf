da397b370c83308b477b5b6a7214580fe537f6ae
IvoStefanov <ivostefanovtues@gmail.com>
2022-07-13 13:12:01 +0300

    GPF-1365 add agp single view wrapper tests

--- gpfjs/src/app/autism-gene-profiles-single-view-wrapper/autism-gene-profiles-single-view-wrapper.component.spec.ts
+++ gpfjs/src/app/autism-gene-profiles-single-view-wrapper/autism-gene-profiles-single-view-wrapper.component.spec.ts
@@ -2,14 +2,21 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ConfigService } from 'app/config/config.service';
 import { ActivatedRoute } from '@angular/router';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { AutismGeneProfileSingleViewWrapperComponent } from './autism-gene-profiles-single-view-wrapper.component';
+import { AutismGeneProfilesService } from 'app/autism-gene-profiles-block/autism-gene-profiles.service';
 
 class MockActivatedRoute {
   params = {dataset: 'testDatasetId', get: () => ''};
   parent = {params: of(this.params)};
   queryParamMap = of(this.params);
-  snapshot = {params: {genes: 'mockGeneSymbol'}};
+  snapshot = {params: {genes: 'abc1,def2,'}};
+}
+
+class MockAutismGeneProfilesService {
+  public getConfig(): Observable<object> {
+    return of({defaultDataset: 'mockDataset'});
+  }
 }
 
 describe('AutismGeneProfileSingleViewWrapperComponent', () => {
@@ -21,20 +28,33 @@ describe('AutismGeneProfileSingleViewWrapperComponent', () => {
       declarations: [AutismGeneProfileSingleViewWrapperComponent],
       providers: [
         ConfigService,
+        {provide: AutismGeneProfilesService, useValue: new MockAutismGeneProfilesService()},
         {provide: ActivatedRoute, useValue: new MockActivatedRoute()},
       ],
       imports: [HttpClientTestingModule]
-    })
-    .compileComponents();
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AutismGeneProfileSingleViewWrapperComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize', (done) => {
+    component.ngOnInit();
+    component.$autismGeneToolConfig.subscribe(config => {
+      expect(config.defaultDataset).toEqual('mockDataset');
+      done();
+    });
+  });
+
+  it('should set gene symbols after view initialization', () => {
+    expect(component.geneSymbols).toEqual(new Set<string>());
+    component.ngAfterViewInit();
+    expect(component.geneSymbols).toEqual(new Set<string>(['ABC1', 'DEF2']));
   });
 });
