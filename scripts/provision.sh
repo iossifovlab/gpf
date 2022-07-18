113a02862553a9a74f2c527f7f9483d80cf7d25e
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2022-07-18 07:21:39 +0300

    experiment with vagrant machine

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,71 +1,59 @@
 #!/bin/bash
 
 
-# UBUNTU 18.04
+# UBUNTU 20.04
 
-apt-get update --fix-missing && \ 
-	apt-get install -y build-essential default-libmysqlclient-dev gcc \
-        libgl1-mesa-glx openjdk-8-jdk-headless procps vim libsasl2-dev wget \
-        apt-transport-https ca-certificates curl software-properties-common && \
-	apt-get clean
-
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+DEBIAN_FRONTEND=noninteractive apt-get update --fix-missing && \ 
+	DEBIAN_FRONTEND=noninteractive apt-get dist-upgrade -y && \
+	DEBIAN_FRONTEND=noninteractive apt-get install -y \
+        build-essential gcc wget git python-lxml \
+        ca-certificates \
+        curl \
+        gnupg \
+        lsb-release && \
+	DEBIAN_FRONTEND=noninteractive apt-get clean
 
 
 # DOCKER
+sudo mkdir -p /etc/apt/keyrings
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
-apt update
-apt install  -y docker-ce
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" \
+  | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null 
+
+DEBIAN_FRONTEND=noninteractive apt update
+DEBIAN_FRONTEND=noninteractive apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
 usermod -aG docker vagrant
 
-# HADOOP
-export HADOOP_VER=3.0.2
 
-wget --quiet http://it.apache.contactlab.it/hadoop/core/hadoop-${HADOOP_VER}/hadoop-${HADOOP_VER}.tar.gz
-tar -xf hadoop-$HADOOP_VER.tar.gz -C ..; \
-    mv ../hadoop-${HADOOP_VER} /opt/hadoop
+# MINICONDA
+mkdir -p /root/.conda/
 
-export HADOOP_HOME=/opt/hadoop
-export HADOOP_CONF_DIR=$HADOOP_HOME/etc/hadoop
-
-cp /vagrant/scripts/configs/vagrant/etc/core-site.xml ${HADOOP_CONF_DIR}/core-site.xml
-
-
-# ANACONDA
-
-wget --quiet https://repo.anaconda.com/archive/Anaconda3-2019.07-Linux-x86_64.sh -O /anaconda.sh && \
-    /bin/bash /anaconda.sh -b -p /opt/conda && \
+wget --quiet https://repo.anaconda.com/miniconda/Miniconda3-py39_4.12.0-Linux-x86_64.sh -O /miniconda.sh && \
+    /bin/bash /miniconda.sh -b -p /opt/conda && \
     ln -s /opt/conda/etc/profile.d/conda.sh /etc/profile.d/ && \
-    rm /anaconda.sh
+    rm /miniconda.sh
 
 /opt/conda/bin/conda init bash
 
-/opt/conda/bin/conda create -c conda-forge -c bioconda \
-    --name gpf --file /vagrant/conda-environment.yml
+export PATH=/opt/conda/bin:$PATH
 
-# PIP
-/opt/conda/envs/gpf/bin/pip install reusables
+conda install -c defaults -c conda-forge \
+    python=3.9 mamba=0.24 \
+    bump2version conda-build conda-pack boa anaconda-client
 
-# cat <<EOT >> /etc/hosts
+ln -s /opt/conda/lib/libarchive.so.18 /opt/conda/lib/libarchive.so.13
 
-# 127.0.0.1       impala
+chown vagrant:vagrant -R /opt/conda
+
+sudo -u vagrant /opt/conda/bin/conda init bash
+
+sudo -u vagrant /opt/conda/bin/mamba env create --name gpf --file /vagrant/environment.yml
+sudo -u vagrant /opt/conda/bin/mamba env update --name gpf --file /vagrant/dev-environment.yml
+
+# cat <<EOT >> /home/vagrant/.bashrc
+
 
 # EOT
-
-cat <<EOT >> /home/vagrant/.bashrc
-
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
-
-export DAE_SOURCE_DIR="/vagrant/DAE"
-export DAE_DB_DIR="/data"
-
-export DAE_GENOMIC_SCORES_HG19=/genomic-scores-hg19
-export DAE_GENOMIC_SCORES_HG38=/genomic-scores-hg38
-
-export HADOOP_HOME=/opt/hadoop
-
-source /vagrant/scripts/setenv-vagrant.sh
-
-EOT
