62d2806cf6f7d61ce01013d9ea7866113a3144d0
IvoStefanov <ivostefanov.tues@gmail.com>
2022-08-03 12:04:16 +0300

    GPF-1774 add sorting buttons tests

--- gpfjs/src/app/sorting-buttons/sorting-buttons.component.spec.ts
+++ gpfjs/src/app/sorting-buttons/sorting-buttons.component.spec.ts
@@ -9,15 +9,53 @@ describe('SortingButtonsComponent', () => {
     TestBed.configureTestingModule({
       declarations: [SortingButtonsComponent]
     }).compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(SortingButtonsComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit sort event', () => {
+    const emitSpy = jest.spyOn(component.sortEvent, 'emit');
+
+    component.id = 'id1';
+    component.emitSortEvent('order1');
+    expect(emitSpy).toHaveBeenCalledWith({ id: 'id1', order: 'order1'});
+
+    component.id = 'id2';
+    component.emitSortEvent('order2');
+    expect(emitSpy).toHaveBeenCalledWith({ id: 'id2', order: 'order2'});
+  });
+
+  it('should emit sort', () => {
+    const emitSpy = jest.spyOn(component.sortEvent, 'emit');
+    component.hideState = 0;
+    component.id = 'id1';
+
+    component.emitSort();
+    expect(component.hideState).toBe(1);
+    expect(emitSpy).toHaveBeenCalledWith({id: 'id1', order: 'desc'});
+
+    component.emitSort();
+    expect(component.hideState).toBe(-1);
+    expect(emitSpy).toHaveBeenCalledWith({id: 'id1', order: 'asc'});
+  });
+
+  it('should reset hide state', () => {
+    component.hideState = 0;
+    component.resetHideState();
+    expect(component.hideState).toBe(0);
+
+    component.hideState = -1;
+    component.resetHideState();
+    expect(component.hideState).toBe(0);
+
+    component.hideState = 1;
+    component.resetHideState();
+    expect(component.hideState).toBe(0);
   });
 });
