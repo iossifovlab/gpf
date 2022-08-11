f3c3890d5cc7022988a655c6f740b397fbfc74b2
iordanivanov <iiordan.ivanov@gmail.com>
2022-08-11 13:33:02 +0300

    Update reset_dev.sh script to use absolute paths

--- wdae/wdae/reset_dev.sh
+++ wdae/wdae/reset_dev.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+
 rm -rf $DAE_DB_DIR/wdae/wdae.sql
 rm -rf $DAE_DB_DIR/wdae/wdae_django_pre.cache
 rm -rf $DAE_DB_DIR/wdae/wdae_django_default.cache
 
 wdaemanage.py migrate
-./wdae_create_dev_users.sh
-./wdae_create_dev_gpfjs_app.sh
+$SCRIPTPATH/wdae_create_dev_users.sh
+$SCRIPTPATH/wdae_create_dev_gpfjs_app.sh
