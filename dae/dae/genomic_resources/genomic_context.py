9075ee1ec39cf777b33f05fb4e6ac8837c62c613
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2022-08-12 13:00:23 +0300

    GPF-1777 simple genomic context provider added

--- dae/dae/genomic_resources/genomic_context.py
+++ dae/dae/genomic_resources/genomic_context.py
@@ -1,9 +1,17 @@
-from functools import cache
+from __future__ import annotations
+
+import argparse
 import logging
 from abc import ABC, abstractmethod
-from typing import List, Optional, Any, Tuple, Set
-from dae.genomic_resources.reference_genome import ReferenceGenome
-from dae.genomic_resources.gene_models import GeneModels
+from typing import List, Optional, Any, Tuple, Set, Callable, Dict
+from functools import cache
+
+from dae.genomic_resources.repository_factory import \
+    build_genomic_resource_repository
+from dae.genomic_resources.reference_genome import ReferenceGenome, \
+    build_reference_genome_from_resource
+from dae.genomic_resources.gene_models import GeneModels, \
+    build_gene_models_from_resource
 from dae.genomic_resources.repository import GenomicResourceRepo
 
 _REGISTERED_CONEXT_PROVIDERS: list = []
@@ -61,12 +69,13 @@ class GenomicContext(ABC):
 class GenomicContextProvider(ABC):
     """Abstract base class for genomic contexts provider."""
 
-    def get_context_generator_priority(self) -> int:
+    @abstractmethod
+    def get_context_provider_priority(self) -> int:
         # pylint: disable=no-self-use
-        return 0
+        pass
 
     @abstractmethod
-    def get_context_generator_type(self) -> str:
+    def get_context_provider_type(self) -> str:
         pass
 
     @abstractmethod
@@ -74,13 +83,65 @@ class GenomicContextProvider(ABC):
         pass
 
 
-def register_context_provider(context_generator: GenomicContextProvider):
+class SimpleGenomicContext(GenomicContext):
+    """Simple implementation of genomic context."""
+
+    def __init__(
+            self, context_objects: Dict[str, Any], source: Tuple[str, ...]):
+        self._context: Dict[str, Any] = context_objects
+        self._source = source
+
+    def get_context_object(self, key) -> Optional[Any]:
+        return self._context.get(key)
+
+    def get_context_keys(self):
+        return set(self._context.keys())
+
+    def get_source(self) -> Tuple[str, ...]:
+        return self._source
+
+
+class SimpleGenomicContextProvider(GenomicContextProvider):
+    """Simple implementation of genomic contexts provider."""
+
+    def __init__(
+            self,
+            context_builder: Callable[[], GenomicContext],
+            priority: int):
+        self._priority: int = priority
+        self._context_builder = context_builder
+        self._contexts: Optional[List[GenomicContext]] = None
+
+    def get_context_provider_priority(self) -> int:
+        return self._priority
+
+    def get_context_provider_type(self) -> str:
+        if self._contexts is None:
+            sources: List[Tuple[str, ...]] = [("not initialized", )]
+        else:
+            sources = [c.get_source() for c in self._contexts]
+        return f"SingleGenomicContextProvider{sources}"
+
+    def get_contexts(self) -> List[GenomicContext]:
+        if self._contexts is None:
+            try:
+                self._contexts = [self._context_builder()]
+            except Exception:  # pylint: disable=broad-except
+                logger.error(
+                    "problem while building genomic context",
+                    exc_info=True)
+                self._contexts = []
+
+        return self._contexts
+
+
+def register_context_provider(context_provider: GenomicContextProvider):
     logger.debug(
         "Registerfing the %s "
         "genomic context generator with priority %s",
-        context_generator.get_context_generator_type(),
-        context_generator.get_context_generator_type())
-    _REGISTERED_CONEXT_PROVIDERS.append(context_generator)
+        context_provider.get_context_provider_type(),
+        context_provider.get_context_provider_type())
+    _REGISTERED_CONEXT_PROVIDERS.append(context_provider)
 
 
 class PriorityGenomicContext(GenomicContext):
@@ -122,7 +183,105 @@ class PriorityGenomicContext(GenomicContext):
 def get_genomic_context() -> GenomicContext:
     contexts = []
     for provider in sorted(_REGISTERED_CONEXT_PROVIDERS,
-                           key=lambda g: (g.get_context_generator_priority(),
-                                          g.get_context_generator_type())):
-        contexts += provider.get_contexts()
+                           key=lambda g: (g.get_context_provider_priority(),
+                                          g.get_context_provider_type())):
+        contexts.extend(provider.get_contexts())
     return PriorityGenomicContext(contexts)
+
+
+class CLIGenomicContext(GenomicContext):
+    """Defines CLI genomics context."""
+
+    def __init__(self, grr, genome, gene_models):
+        self._grr: GenomicResourceRepo = grr
+        self._ref_genome: Optional[ReferenceGenome] = genome
+        self._gene_models: Optional[GeneModels] = gene_models
+
+    def get_context_object(self, key) -> Optional[Any]:
+        if key == GC_GRR_KEY and self._grr is not None:
+            return self._grr
+        if key == GC_REFERENCE_GENOME_KEY and self._ref_genome is not None:
+            return self._ref_genome
+        if key == GC_GENE_MODELS_KEY and self._gene_models is not None:
+            return self._gene_models
+        logger.info(
+            "genomic context object with key %s not found in %s",
+            key, self.get_source())
+        return None
+
+    @cache
+    def get_context_keys(self) -> Set[str]:
+        result = set()
+        if self._grr is not None:
+            result.add(GC_GRR_KEY)
+        if self._ref_genome is not None:
+            result.add(GC_REFERENCE_GENOME_KEY)
+        if self._gene_models is not None:
+            result.add(GC_GENE_MODELS_KEY)
+        return result
+
+    def get_source(self) -> Tuple[str, ...]:
+        return ("CLIGenomicContext",)
+
+
+class CLIGenomicContextProvider(SimpleGenomicContextProvider):
+    """Defines CLI genomic context provider."""
+
+    @staticmethod
+    def add_context_arguments(parser: argparse.ArgumentParser):
+        """Add command line arguments to the argument parser."""
+        parser.add_argument(
+            "-grr", "--grr-file-name", default=None,
+            help="The GRR configuration file. If the argument is absent, "
+            "the a GRR repository from the current genomic context "
+            "(i.e. gpf_instance) will be used or, if that fails, the "
+            "default GRR configuration will be used.")
+        parser.add_argument(
+            "-ref", "--reference-genome-resource-id", default=None,
+            help="The resource id for the reference genome. If the argument "
+                 "is absent the reference genome from the current genomic "
+                 "context will be used.")
+        parser.add_argument(
+            "-genes", "--gene-models-resource-id", default=None,
+            help="The resource is of the gene models resoruce. If the argument"
+                 " is absent the gene models from the current genomic "
+                 "context will be used.")
+
+    @staticmethod
+    def context_builder(args) -> CLIGenomicContext:
+        """Build a CLI genomic context."""
+        grr = None
+        if args.grr_filename is None:
+            grr = get_genomic_context().get_context_object(GC_GRR_KEY)
+        else:
+            logger.info(
+                "Using the GRR consigured in the file "
+                "%s as requested on the "
+                "command line.", args.grr_file_name)
+            grr = build_genomic_resource_repository(
+                file_name=args.grr_file_name)
+
+        if grr is None:
+            raise ValueError("Can't resolve genomic context GRR")
+
+        genome: Optional[ReferenceGenome] = None
+        if args.reference_genome_resource_id is not None:
+            logger.info(
+                "Using the reference genome from resoruce "
+                "%s provided on the command line.",
+                args.reference_genome_resource_id)
+            resource = grr.get_resource(args.reference_genome_resource_id)
+
+            genome = build_reference_genome_from_resource(resource)
+
+        gene_models: Optional[GeneModels] = None
+        if args.gene_models_resource_id is not None:
+            logger.info(
+                "Using the gene models from resoruce "
+                "%s provided on the command line.",
+                args.gene_models_resource_id)
+            resource = grr.get_resource(args.gene_models_resource_id)
+
+            gene_models = build_gene_models_from_resource(resource)
+
+        return CLIGenomicContext(grr, genome, gene_models)
--- dae/dae/gpf_instance_plugin/gpf_instance_context_plugin.py
+++ dae/dae/gpf_instance_plugin/gpf_instance_context_plugin.py
@@ -1,72 +1,61 @@
 import logging
-from typing import List, Optional, Any, Tuple, Set
+from typing import Optional, Any, Tuple, Set
 
 from dae.genomic_resources.genomic_context import GC_GRR_KEY
 from dae.genomic_resources.genomic_context import GC_REFERENCE_GENOME_KEY
 from dae.genomic_resources.genomic_context import GC_GENE_MODELS_KEY
 from dae.genomic_resources.genomic_context import GenomicContext
-from dae.genomic_resources.genomic_context import GenomicContextProvider
+from dae.genomic_resources.genomic_context import SimpleGenomicContextProvider
 from dae.genomic_resources.genomic_context import register_context_provider
 
 logger = logging.getLogger(__name__)
 
 
-class GPFInstanceGenomicContextProvider(GenomicContextProvider):
+class GPFInstanceGenomicContextProvider(SimpleGenomicContextProvider):
     """Defines GPFInstance genomic context provider."""
 
+    @staticmethod
+    def context_builder():
+        """Build GPF instance genomic context."""
+        # pylint: disable=import-outside-toplevel
+        from dae.gpf_instance.gpf_instance import GPFInstance
+
+        class GPFInstanceGenomicContext(GenomicContext):
+            """Defines GPFInstance genomic context."""
+
+            def __init__(self, gpf_instance: GPFInstance):
+                self.gpf_instance: GPFInstance = gpf_instance
+
+            def get_context_object(self, key) -> Optional[Any]:
+                if key == GC_GENE_MODELS_KEY:
+                    return self.gpf_instance.gene_models
+                if key == GC_REFERENCE_GENOME_KEY:
+                    return self.gpf_instance.reference_genome
+                if key == GC_GRR_KEY:
+                    return self.gpf_instance.grr
+                if key == "annotation_pipeline":
+                    return self.gpf_instance.get_annotation_pipeline()
+                if key == "gpf_instance":
+                    return self.gpf_instance
+                logger.info(
+                    "can't find %s in GPF instance genomic context", key)
+                return None
+
+            def get_context_keys(self) -> Set[str]:
+                return {
+                    GC_GENE_MODELS_KEY, GC_REFERENCE_GENOME_KEY,
+                    GC_GRR_KEY, "annotation_pipeline", "gpf_instance"
+                }
+
+            def get_source(self) -> Tuple[str, ...]:
+                return ("gpf_instance", self.gpf_instance.dae_db_dir)
+
+        return GPFInstanceGenomicContext(GPFInstance())
+
     def __init__(self):
-        self.contexts: Optional[List[GenomicContext]] = None
-
-    def get_context_generator_priority(self) -> int:
-        return 100
-
-    def get_context_generator_type(self) -> str:
-        return "gpf_instance"
-
-    def get_contexts(self) -> List[GenomicContext]:
-        if self.contexts is None:
-            # pylint: disable=import-outside-toplevel
-            from dae.gpf_instance.gpf_instance import GPFInstance
-
-            class GPFInstanceGenomicContext(GenomicContext):
-                """Defines GPFInstance genomic context."""
-
-                def __init__(self, gpf_instance: GPFInstance):
-                    self.gpf_instance: GPFInstance = gpf_instance
-
-                def get_context_object(self, key) -> Optional[Any]:
-                    if key == GC_GENE_MODELS_KEY:
-                        return self.gpf_instance.gene_models
-                    if key == GC_REFERENCE_GENOME_KEY:
-                        return self.gpf_instance.reference_genome
-                    if key == GC_GRR_KEY:
-                        return self.gpf_instance.grr
-                    if key == "annotation_pipeline":
-                        return self.gpf_instance.get_annotation_pipeline()
-                    if key == "gpf_instance":
-                        return self.gpf_instance
-                    logger.info(
-                        "can't find %s in GPF instance genomic context", key)
-                    return None
-
-                def get_context_keys(self) -> Set[str]:
-                    return {
-                        GC_GENE_MODELS_KEY, GC_REFERENCE_GENOME_KEY,
-                        GC_GRR_KEY, "annotation_pipeline", "gpf_instance"
-                    }
-
-                def get_source(self) -> Tuple[str, ...]:
-                    return ("gpf_instance", self.gpf_instance.dae_db_dir)
-
-            self.contexts = []
-            try:
-                self.contexts.append(GPFInstanceGenomicContext(GPFInstance()))
-            except Exception:  # pylint: disable=broad-except
-                logger.error(
-                    "problem while initializing default gpf instance",
-                    exc_info=True)
-
-        return self.contexts
+        super().__init__(
+            GPFInstanceGenomicContextProvider.context_builder,
+            100)
 
 
 def init_gpf_instance_genomic_context_plugin():
