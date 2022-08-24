f2a5c3f2df9558198c0ec6788c8a5423896ef9e2
Svetlin Mladenov <svetlinm@seqpipe.com>
2022-08-24 18:09:13 +0300

    Don't use global variables to test task graph

--- dae/dae/import_tools/tests/test_task_graph.py
+++ dae/dae/import_tools/tests/test_task_graph.py
@@ -1,4 +1,4 @@
-# pylint: disable=W0621,C0114,C0116,W0212,W0613
+# pylint: disable=W0621,C0114,C0115,C0116,W0212,W0613
 import time
 import pytest
 
@@ -23,55 +23,60 @@ def executor(threaded_client, request):
     return SequentialExecutor()
 
 
-# NOTE these tests should not run in parallel
-EXEC_ORDER = []
+class Worker:
+    # NOTE these tests should not run in parallel
+    EXEC_ORDER: list[str] = []
+
+    @classmethod
+    def do_work(cls, name, timeout):
+        if timeout != 0:
+            time.sleep(timeout)
+        cls.EXEC_ORDER.append(name)
+
+    @classmethod
+    def assign_to_execution_order(cls, list_obj):
+        cls.EXEC_ORDER.extend(list_obj)
 
 
-def do_work(name, timeout):
-    if timeout != 0:
-        time.sleep(timeout)
-    EXEC_ORDER.append(name)
+@pytest.fixture
+def worker():
+    Worker.EXEC_ORDER = []
+    return Worker()
 
 
-def test_dependancy_chain(executor):
-    global EXEC_ORDER  # pylint: disable=global-statement
-    EXEC_ORDER = []
+def test_dependancy_chain(executor, worker):
     graph = TaskGraph()
-    task_1 = graph.create_task("Task 1", do_work, ["task_1", 0.01], [])
-    graph.create_task("Task 1", do_work, ["task_2", 0], [task_1])
+    task_1 = graph.create_task("Task 1", worker.do_work, ["task_1", 0.01], [])
+    graph.create_task("Task 1", worker.do_work, ["task_2", 0], [task_1])
 
     executor.execute(graph)
 
-    assert EXEC_ORDER == ["task_1", "task_2"]
+    assert worker.EXEC_ORDER == ["task_1", "task_2"]
 
 
-def test_multiple_dependancies(executor):
-    global EXEC_ORDER  # pylint: disable=global-statement
-    EXEC_ORDER = []
+def test_multiple_dependancies(executor, worker):
     graph = TaskGraph()
 
     tasks = []
     for i in range(10):
-        task = graph.create_task(f"Task {i}", do_work, [f"task_{i}", 0], [])
+        task = graph.create_task(
+            f"Task {i}", worker.do_work, [f"task_{i}", 0], []
+        )
         tasks.append(task)
 
     for i in range(100, 105):
-        graph.create_task(f"Task {i}", do_work, [f"task_{i}", 0], tasks)
+        graph.create_task(f"Task {i}", worker.do_work, [f"task_{i}", 0], tasks)
 
     executor.execute(graph)
 
-    assert len(EXEC_ORDER) == 15
-    assert set(EXEC_ORDER[:10]) == set(f"task_{i}" for i in range(10))
-    assert set(EXEC_ORDER[10:]) == set(f"task_{i}" for i in range(100, 105))
+    assert len(worker.EXEC_ORDER) == 15
+    assert set(worker.EXEC_ORDER[:10]) == set(f"task_{i}" for i in range(10))
+    assert set(worker.EXEC_ORDER[10:]) == set(
+        f"task_{i}" for i in range(100, 105)
+    )
 
 
-def assign_to_execution_order(list_obj):
-    EXEC_ORDER.extend(list_obj)
-
-
-def test_implicit_dependancies(executor):
-    global EXEC_ORDER  # pylint: disable=global-statement
-    EXEC_ORDER = []
+def test_implicit_dependancies(executor, worker):
     graph = TaskGraph()
 
     def add_to_list(what, where):
@@ -82,8 +87,8 @@ def test_implicit_dependancies(executor):
     for i in range(1, 8):
         last_task = graph.create_task(f"{i}", add_to_list, [i, last_task], [])
 
-    graph.create_task("8", assign_to_execution_order, [last_task], [])
+    graph.create_task("8", worker.assign_to_execution_order, [last_task], [])
 
     executor.execute(graph)
 
-    assert EXEC_ORDER == list(range(8))
+    assert worker.EXEC_ORDER == list(range(8))
