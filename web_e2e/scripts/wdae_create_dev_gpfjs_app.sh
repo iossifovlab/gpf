9f425c8037e55cb03321e223856e0aaf1cc0228e
joankosev <joankosev@gmail.com>
2022-10-18 15:53:37 +0300

    GPF-1794 update e2e login for oauth

---
+++ web_e2e/scripts/wdae_create_dev_gpfjs_app.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT=$(readlink -f "$0")
+# Absolute path this script is in, thus /home/user/bin
+SCRIPTPATH=$(dirname "$SCRIPT")
+# Export for create_gpfjs_dev_app.py
+export GPFJS_URL="http://$(hostname -i)/gpf"
+
+wdaemanage.py shell < $SCRIPTPATH/create_gpfjs_dev_app.py
