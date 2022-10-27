d7ac0f39b03e88b22aa93d6f4851be802b72c4eb
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2022-10-27 12:35:00 +0300

    Mop up pylint warning in cli_manage.

--- dae/dae/genomic_resources/cli.py
+++ dae/dae/genomic_resources/cli.py
@@ -453,7 +453,7 @@ def _run_resource_repair_command(proto, repo_url, region_size, **kwargs):
 
 def cli_manage(cli_args=None):
     """Provide CLI for repository management."""
-    # pylint: disable=too-many-branches
+    # pylint: disable=too-many-branches,too-many-statements
     if cli_args is None:
         cli_args = sys.argv[1:]
     desc = "Genomic Resource Repository Management Tool"
