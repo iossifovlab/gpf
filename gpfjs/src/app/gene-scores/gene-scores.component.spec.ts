899d43d8337080aa0106113a5c126ead83362766
livovachkov <livovachkov@gmail.com>
2022-11-30 14:10:24 +0200

    Minor lint

--- gpfjs/src/app/gene-scores/gene-scores.component.spec.ts
+++ gpfjs/src/app/gene-scores/gene-scores.component.spec.ts
@@ -1,6 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
-import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgxsModule } from '@ngxs/store';
