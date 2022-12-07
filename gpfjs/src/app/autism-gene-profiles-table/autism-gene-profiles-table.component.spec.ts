b23be337817be4d299e5343b0152ade2b85ce6d4
IlinaGergova <ili.gergova@gmail.com>
2022-12-07 16:50:30 +0200

    lint autism-gene-profiles-table  test

--- gpfjs/src/app/autism-gene-profiles-table/autism-gene-profiles-table.component.spec.ts
+++ gpfjs/src/app/autism-gene-profiles-table/autism-gene-profiles-table.component.spec.ts
@@ -3,6 +3,8 @@ import { AgpTableConfig } from './autism-gene-profiles-table';
 import { plainToClass } from 'class-transformer';
 import { Observable, of } from 'rxjs';
 import { cloneDeep } from 'lodash';
+import { AgpTableService } from './autism-gene-profiles-table.service';
+import { TestBed } from '@angular/core/testing';
 
 const column1 = {
   clickable: 'createTab',
@@ -186,6 +188,7 @@ const gridData = [
   }
 ];
 
+/* eslint-disable */
 const genesMock = [
   {
     'column1': 'value10',
@@ -224,6 +227,7 @@ const genesMock = [
     'column3.column33.column332': 'value39'
   }
 ];
+/* eslint-enable */
 
 class AgpTableServiceMock {
   public getGenes(pageIndex: number, geneInput: string): Observable<Record<string, any>> {
@@ -242,8 +246,16 @@ describe('AgpTableComponent', () => {
   const agpTableServiceMock = new AgpTableServiceMock();
 
   beforeEach(() => {
-    component = new AgpTableComponent(agpTableServiceMock as any);
+    TestBed.configureTestingModule({
+      declarations: [AgpTableComponent],
+      providers: [{provide: AgpTableService, useValue: agpTableServiceMock}],
+    }).compileComponents();
+
+    const fixture = TestBed.createComponent(AgpTableComponent);
+    component = fixture.componentInstance;
+
     component.sortingButtonsComponents = [];
+    fixture.detectChanges();
   });
 
   it('should create', () => {
@@ -255,29 +267,29 @@ describe('AgpTableComponent', () => {
 
     component.ngOnInit();
 
-    expect(component.defaultSortBy).toEqual('mockSort');
+    expect(component.defaultSortBy).toBe('mockSort');
   });
 
   it('should update when change happens', () => {
     component.ngOnChanges();
-    expect(component.leaves).toEqual(undefined);
+    expect(component.leaves).toBeUndefined();
     expect(component.pageIndex).toBe(0);
-    expect(component.genes).toEqual([]);
+    expect(component.genes).toStrictEqual([]);
 
     component.config = cloneDeep(configMock);
     component.ngOnChanges();
 
     component.leaves.forEach((leaf, index) => {
-      expect(leaf.id).toEqual(gridData[index].id);
-      expect(leaf.gridColumn).toEqual(gridData[index].gridColumn);
-      expect(leaf.gridRow).toEqual(gridData[index].gridRow);
-      expect(leaf.parent?.id).toEqual(gridData[index].parent);
-      expect(leaf.depth).toEqual(gridData[index].depth);
+      expect(leaf.id).toStrictEqual(gridData[index].id);
+      expect(leaf.gridColumn).toStrictEqual(gridData[index].gridColumn);
+      expect(leaf.gridRow).toStrictEqual(gridData[index].gridRow);
+      expect(leaf.parent?.id).toStrictEqual(gridData[index].parent);
+      expect(leaf.depth).toStrictEqual(gridData[index].depth);
     });
     expect(component.pageIndex > 1).toBeTruthy();
-    expect(component.genes[0]).toEqual(genesMock[0]);
-    expect(component.genes[1]).toEqual(genesMock[1]);
-    expect(component.genes[2]).toEqual(genesMock[2]);
+    expect(component.genes[0]).toStrictEqual(genesMock[0]);
+    expect(component.genes[1]).toStrictEqual(genesMock[1]);
+    expect(component.genes[2]).toStrictEqual(genesMock[2]);
   });
 
   it('should search', () => {
@@ -285,27 +297,27 @@ describe('AgpTableComponent', () => {
     component.ngOnChanges();
     component.search('mockSearch');
 
-    expect(component.geneInput).toEqual('mockSearch');
-    expect(component.genes[0]).toEqual({search: 'mockSearch', ...genesMock[0]});
-    expect(component.genes[1]).toEqual({search: 'mockSearch', ...genesMock[1]});
-    expect(component.genes[2]).toEqual({search: 'mockSearch', ...genesMock[2]});
+    expect(component.geneInput).toBe('mockSearch');
+    expect(component.genes[0]).toStrictEqual({search: 'mockSearch', ...genesMock[0]});
+    expect(component.genes[1]).toStrictEqual({search: 'mockSearch', ...genesMock[1]});
+    expect(component.genes[2]).toStrictEqual({search: 'mockSearch', ...genesMock[2]});
   });
 
   it('should update genes', () => {
-    expect(component.genes).toEqual([]);
-    expect(component.pageIndex).toEqual(0);
+    expect(component.genes).toStrictEqual([]);
+    expect(component.pageIndex).toBe(0);
     component.updateGenes();
-    expect(component.genes).toEqual(genesMock);
-    expect(component.pageIndex).toEqual(1);
+    expect(component.genes).toStrictEqual(genesMock);
+    expect(component.pageIndex).toBe(1);
   });
 
   it('should reorder header', () => {
     component.config = cloneDeep(configMock);
 
     component.reorderHeader(['column3', 'column1', 'column2']);
-    expect(component.config.columns[0].id).toEqual('column3');
-    expect(component.config.columns[1].id).toEqual('column1');
-    expect(component.config.columns[2].id).toEqual('column2');
+    expect(component.config.columns[0].id).toBe('column3');
+    expect(component.config.columns[1].id).toBe('column1');
+    expect(component.config.columns[2].id).toBe('column2');
 
     const sortedLeaves = ['column311', 'column312', 'column321', 'column322', 'column1', 'column21', 'column22'];
     const sortedGridData = cloneDeep(gridData);
@@ -316,11 +328,11 @@ describe('AgpTableComponent', () => {
     });
 
     component.leaves.forEach((leaf, index) => {
-      expect(leaf.id).toEqual(sortedGridData[index].id);
-      expect(leaf.gridColumn).toEqual(sortedGridData[index].gridColumn);
-      expect(leaf.gridRow).toEqual(sortedGridData[index].gridRow);
-      expect(leaf.parent?.id).toEqual(sortedGridData[index].parent);
-      expect(leaf.depth).toEqual(sortedGridData[index].depth);
+      expect(leaf.id).toBe(sortedGridData[index].id);
+      expect(leaf.gridColumn).toBe(sortedGridData[index].gridColumn);
+      expect(leaf.gridRow).toBe(sortedGridData[index].gridRow);
+      expect(leaf.parent?.id).toBe(sortedGridData[index].parent);
+      expect(leaf.depth).toBe(sortedGridData[index].depth);
     });
   });
 
@@ -330,11 +342,11 @@ describe('AgpTableComponent', () => {
     component.genes = undefined;
 
     component.sort('mockSortBy', 'mockOrderBy');
-    expect(component.sortBy).toEqual('mockSortBy');
-    expect(component.orderBy).toEqual('mockOrderBy');
-    expect(component.genes[0]).toEqual(genesMock[0]);
-    expect(component.genes[1]).toEqual(genesMock[1]);
-    expect(component.genes[2]).toEqual(genesMock[2]);
+    expect(component.sortBy).toBe('mockSortBy');
+    expect(component.orderBy).toBe('mockOrderBy');
+    expect(component.genes[0]).toStrictEqual(genesMock[0]);
+    expect(component.genes[1]).toStrictEqual(genesMock[1]);
+    expect(component.genes[2]).toStrictEqual(genesMock[2]);
   });
 
   it('should toggle highlighted genes', () => {
