198f43722d6810132368738c0e15a482dc9f1a57
IvoStefanov <ivostefanov.tues@gmail.com>
2022-12-07 14:25:21 +0200

    lint svg drawing

--- gpfjs/src/app/utils/svg-drawing.ts
+++ gpfjs/src/app/utils/svg-drawing.ts
@@ -6,7 +6,7 @@ export const affectedStatusColors: Record<string, string> = {
   'Affected and unaffected': '#8a8a8a',
 };
 
-function getTrianglePoints(plotX: number, plotY: number, size: number) {
+const getTrianglePoints = (plotX: number, plotY: number, size: number): string => {
   const height = Math.sqrt(Math.pow(size, 2) - Math.pow((size / 2.0), 2));
   const x1 = plotX - (size / 2.0);
   const y1 = plotY + (height / 2.0);
@@ -15,9 +15,16 @@ function getTrianglePoints(plotX: number, plotY: number, size: number) {
   const x3 = plotX;
   const y3 = plotY - (height / 2.0);
   return `${x1},${y1} ${x2},${y2} ${x3},${y3}`;
-}
+};
 
-export function hoverText(element: Selection<BaseType, unknown, HTMLElement, any>, x: number, y: number, text: string, svgTitle: string, fontSize: number) {
+export const hoverText = (
+  element: Selection<BaseType, unknown, HTMLElement, unknown>,
+  x: number,
+  y: number,
+  text: string,
+  svgTitle: string,
+  fontSize: number
+): void => {
   element.append('text')
     .attr('x', x)
     .attr('y', y)
@@ -26,12 +33,17 @@ export function hoverText(element: Selection<BaseType, unknown, HTMLElement, any
     .attr('cursor', 'default')
     .attr('text-anchor', 'end')
     .append('svg:title').text(`${svgTitle}`);
-}
+};
 
-export function surroundingRectangle(
-    element: Selection<BaseType, unknown, HTMLElement, any>, x: number, y: number, color: string, title: string,
-    strokeOpacity: number = 1, w: number = 16
-  ) {
+export const surroundingRectangle = (
+  element: Selection<BaseType, unknown, HTMLElement, unknown>,
+  x: number,
+  y: number,
+  color: string,
+  title: string,
+  strokeOpacity: number = 1,
+  w: number = 16
+): void => {
   const h = 16;
   element.append('g')
     .append('rect')
@@ -45,24 +57,38 @@ export function surroundingRectangle(
     .style('stroke-width', 2)
     .style('stroke-opacity', strokeOpacity)
     .append('svg:title').text(title);
-}
+};
 
-export function star(element: Selection<BaseType, unknown, HTMLElement, any>, x: number, y: number, color: string, title: string) {
+export const star = (
+  element: Selection<BaseType, unknown, HTMLElement, unknown>,
+  x: number,
+  y: number,
+  color: string,
+  title: string
+): void => {
   element.append('svg')
     .attr('x', x - 8.5)
     .attr('y', y - 8.5)
     .append('g')
     .append('path')
-    .attr('d', 'M12 .587l3.668 7.568 8.332 1.151-6.064 5.828 1.48 8.279-7.416-3.967-7.417 3.967 1.481-8.279-6.064-5.828 8.332-1.151z')
+    .attr('d', 'M12 .587l3.668 7.568 8.332 1.151-6.064 '
+      + '5.828 1.48 8.279-7.416-3.967-7.417 3.967 '
+      + '1.481-8.279-6.064-5.828 8.332-1.151z')
     .attr('transform', 'scale(0.7)')
     .attr('fill', color)
     .attr('fill-opacity', '0.6')
     .style('stroke-width', 1)
     .style('stroke', color)
     .append('svg:title').text(title);
-}
+};
 
-export function triangle(element: Selection<BaseType, unknown, HTMLElement, any>, x: number, y: number, color: string, title: string) {
+export const triangle = (
+  element: Selection<BaseType, unknown, HTMLElement, unknown>,
+  x: number,
+  y: number,
+  color: string,
+  title: string
+): void => {
   element.append('g')
     .append('polygon')
     .attr('points', getTrianglePoints(x, y, 14))
@@ -71,9 +97,16 @@ export function triangle(element: Selection<BaseType, unknown, HTMLElement, any>
     .style('stroke-width', 1)
     .style('stroke', color)
     .append('svg:title').text(title);
-}
+};
 
-export function circle(element: Selection<BaseType, unknown, HTMLElement, any>, x: number, y: number, color: string, title: string, radius: number = 7) {
+export const circle = (
+  element: Selection<BaseType, unknown, HTMLElement, unknown>,
+  x: number,
+  y: number,
+  color: string,
+  title: string,
+  radius: number = 7
+): void => {
   element.append('g')
     .append('circle')
     .attr('cx', x)
@@ -84,13 +117,27 @@ export function circle(element: Selection<BaseType, unknown, HTMLElement, any>, 
     .style('stroke-width', 1)
     .style('stroke', color)
     .append('svg:title').text(title);
-}
+};
 
-export function dot(element: Selection<BaseType, unknown, HTMLElement, any>, x: number, y: number, color: string, title: string) {
+export const dot = (
+  element: Selection<BaseType, unknown, HTMLElement, unknown>,
+  x: number,
+  y: number,
+  color: string,
+  title: string): void => {
   circle(element, x, y, color, title, 3);
-}
+};
 
-export function rect(element: Selection<BaseType, unknown, HTMLElement, any>, xStart: number, xEnd: number, y: number, height: number, color: string, opacity: number, title: string) {
+export const rect = (
+  element: Selection<BaseType, unknown, HTMLElement, unknown>,
+  xStart: number,
+  xEnd: number,
+  y: number,
+  height: number,
+  color: string,
+  opacity: number,
+  title: string
+): void => {
   element.append('rect')
     .attr('height', height)
     .attr('width', xEnd - xStart)
@@ -101,9 +148,15 @@ export function rect(element: Selection<BaseType, unknown, HTMLElement, any>, xS
     .attr('stroke', color)
     .style('stroke-opacity', opacity)
     .append('svg:title').text(title);
-}
+};
 
-export function line(element: Selection<BaseType, unknown, HTMLElement, any>, xStart: number, xEnd: number, y: number, svgTitle: string) {
+export const line = (
+  element: Selection<BaseType, unknown, HTMLElement, unknown>,
+  xStart: number,
+  xEnd: number,
+  y: number,
+  svgTitle: string
+): void => {
   element.append('line')
     .attr('x1', xStart)
     .attr('y1', y)
@@ -111,4 +164,4 @@ export function line(element: Selection<BaseType, unknown, HTMLElement, any>, xS
     .attr('y2', y)
     .attr('stroke', 'black')
     .append('svg:title').text(svgTitle);
-}
+};
