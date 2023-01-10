27dcdeef27704095b20185abd310292c954ae131
IlinaGergova <ili.gergova@gmail.com>
2023-01-10 13:07:36 +0200

    add download buttons tests gene browser #20

--- web_e2e/cypress/e2e/gene-browser.cy.ts
+++ web_e2e/cypress/e2e/gene-browser.cy.ts
@@ -107,3 +107,72 @@ describe('Gene browser basic display tests after query', () => {
     page.downloadButton.should('be.visible');
   });
 });
+
+describe('Gene browser tests for download', () => {
+  const page = new GeneBrowserPage();
+
+  before(() => {
+    page.cleanup();
+    page.navigateToHome(false);
+    page.loginAdmin();
+  });
+
+  beforeEach(() => {
+    page.navigateToDatasetPage(datasetIds.iossifov2014, toolPageLinks.geneBrowser);
+    cy.deleteDownloadsFolder();
+  });
+
+  [
+    {
+      gene: 'chd8',
+      filters: ['Other', 'ins'],
+      expectedPaths: [
+        'summary_variants1.tsv',
+        'variants1.tsv'
+      ]
+    },
+    {
+      gene: 'chd8',
+      filters: ['Affected only'],
+      expectedPaths: [
+        'summary_variants2.tsv',
+        'variants2.tsv'
+      ]
+    },
+    {
+      gene: 'pogz',
+      filters: ['missense', 'synonymous'],
+      expectedPaths: [
+        'summary_variants3.tsv',
+        'variants3.tsv'
+      ]
+    }
+  ].forEach(data => {
+    it(`should test download summary and download button for gene ${data.gene} 
+    with filters ${data.filters.toString()}`, () => {
+      page.searchInputBox.type(data.gene);
+      page.pressGoButton();
+
+      data.filters.forEach(filter => {
+        page.getAffectedStatusCheckbox(filter).click();
+      });
+
+      page.downloadSummaryButton.click();
+      page.downloadButton.click();
+
+      const paths = ['/summary_variants.tsv', '/variants.tsv'];
+      paths.forEach((path, i) => {
+        const downloadedSummaryVariantsPath = Cypress.config('downloadsFolder') + path;
+        const expectedVariantsPath = 'cypress/fixtures/gene-browser/' + data.expectedPaths[i];
+
+        cy.readFile(downloadedSummaryVariantsPath, { timeout: 5000 }).then((downloadedFile: string) => {
+          cy.readFile(expectedVariantsPath, { timeout: 5000 }).then((expectedFile: string) => {
+            const downloadedFileLines = downloadedFile.split(/\r\n|\r|\n/);
+            const expectedFileLines = expectedFile.split(/\r\n|\r|\n/);
+            expect(downloadedFileLines).to.deep.eq(expectedFileLines);
+          });
+        });
+      });
+    });
+  });
+});
