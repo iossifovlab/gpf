cfa4d54b99571ace63e6d11c752e995f495136fc
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2023-01-11 17:06:17 +0200

    Some test for handling summary variants queries.

--- gcp_genotype_storage/gcp_genotype_storage/tests/test_big_query_backend_queries.py
+++ gcp_genotype_storage/gcp_genotype_storage/tests/test_big_query_backend_queries.py
@@ -20,3 +20,18 @@ def bq_backend(imported_study):
 def test_family_queries(bq_backend, index, query, ecount):
     vs = list(bq_backend.query_variants(**query))
     assert len(vs) == ecount
+
+
+@pytest.mark.parametrize("index,query,ecount", [
+    (1, {}, 2),
+    (2, {"genes": ["g1"]}, 2),
+    (3, {"genes": ["g2"]}, 0),
+    (4, {"effect_types": ["missense"]}, 1),
+    (5, {"effect_types": ["splice-site"]}, 1),
+    (6, {"regions": [Region("foo", 10, 10)]}, 1),
+])
+def test_summary_queries(bq_backend, index, query, ecount):
+
+    vs = list(bq_backend.query_summary_variants(**query))
+
+    assert len(vs) == ecount
