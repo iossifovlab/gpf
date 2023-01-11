62ec20eed55a8dee00c240e04cff1b03aa1cf041
IlinaGergova <ili.gergova@gmail.com>
2023-01-11 12:33:25 +0200

    test checkboxes pheno tool measure #21

--- web_e2e/cypress/e2e/pheno-tool-measure.cy.ts
+++ web_e2e/cypress/e2e/pheno-tool-measure.cy.ts
@@ -13,14 +13,26 @@ describe('Pheno tool measure tests', () => {
   beforeEach(() => {
     page.preserveLogin();
     page.navigateToHome();
+    page.navigateToDatasetPage(datasetIds.compAll, toolPageLinks.phenotypeTool);
   });
 
   it('should display error alert when measure searchbox is empty', () => {
-    page.navigateToDatasetPage(datasetIds.compAll, toolPageLinks.phenotypeTool);
     page.findErrorAlertInComponent('gpf-pheno-tool-measure').should('be.visible');
 
     page.searchbox.click();
     page.findButtonInComponentContainingText('gpf-pheno-measure-selector', 'i1.age').click();
     page.findErrorAlertInComponent('gpf-pheno-tool-measure').should('not.exist');
+  });
+
+  it('should check if Age checkbox is disabled', () => {
+    page.searchbox.click();
+    page.findButtonInComponentContainingText('gpf-pheno-measure-selector', 'i1.age').click();
+    page.block.contains('Age').find('input[type="checkbox"]').should('be.disabled');
+  });
+
+  it('should check if Non verbal IQ checkbox is disabled', () => {
+    page.searchbox.click();
+    page.findButtonInComponentContainingText('gpf-pheno-measure-selector', 'i1.iq').click();
+    page.block.contains('Non verbal IQ').find('input[type="checkbox"]').should('be.disabled');
   });
 });
