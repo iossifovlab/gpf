cd557ed7e07872db93bece56143804a5f9dd178d
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2023-01-26 16:00:12 +0200

    Fix tests by reimplementing cli.run_with_project
    TaskGraphCli was reordered a little bit to allow for the implementation
    of a run_with_project function.

--- dae/dae/import_tools/cli.py
+++ dae/dae/import_tools/cli.py
@@ -5,11 +5,8 @@ from typing import Union
 
 from dae.utils.verbosity_configuration import VerbosityConfiguration
 from dae.import_tools.import_tools import ImportProject
-from dae.task_graph.cache import CacheRecordType, FileTaskCache
 
-from dae.task_graph.executor import \
-    DaskExecutor, SequentialExecutor
-from dae.dask.client_factory import DaskClient
+from dae.task_graph.executor import SequentialExecutor
 from dae.utils import fs_utils
 
 from dae.task_graph import TaskGraphCli
@@ -41,139 +38,9 @@ def main(argv=None):
     return TaskGraphCli.process_graph(args, task_graph)
 
 
-def _cmd_run(args, project, task_cache):
-    ids_to_run = args.task_ids or []
-    executor: Union[SequentialExecutor, DaskExecutor]
-    if args.jobs == 1:
-        executor = SequentialExecutor(task_cache=task_cache)
-        return run_with_project(
-            project, executor, ids_to_run=ids_to_run,
-            keep_going=args.keep_going
-        )
-
-    dask_client = DaskClient.from_arguments(args)
-    if dask_client is None:
-        sys.exit(1)
-    with dask_client as client:
-        executor = DaskExecutor(client, task_cache=task_cache)
-        return run_with_project(
-            project, executor, ids_to_run=ids_to_run,
-            keep_going=args.keep_going
-        )
-
-
-def _cmd_list(args, project, task_cache):
-    storage = project.get_import_storage()
-    print("Generating Task Graph ...", end="")
-    task_graph = storage.generate_import_task_graph(project)
-    print("Done")
-    task_graph.input_files.extend(project.config_filenames)
-    task_graph = _prune_tasks(task_graph, args.task_ids or [])
-
-    task_cache.set_task_graph(task_graph)
-    id_col_len = max(len(t.task_id) for t in task_graph.tasks)
-    id_col_len = min(120, max(50, id_col_len))
-    if not args.verbose:
-        columns = ["TaskID", "Status"]
-        print(f"{columns[0]:{id_col_len}s} {columns[1]}")
-    for task in task_graph.tasks:
-        record = task_cache.get_record(task)
-        status = record.type.name
-        is_error = record.type == CacheRecordType.ERROR
-
-        if args.verbose:
-            print(f"Task-Id={task.task_id}")
-            print(f"Status={status}")
-        else:
-            id_and_status = f"{task.task_id:{id_col_len}s} {status}"
-            if is_error:
-                id_and_status += " (-v to see exception)"
-            print(id_and_status)
-
-        if args.verbose:
-            task_dep_ids_str = ",".join(dep.task_id for dep in task.deps)
-            print(f"Deps=[{task_dep_ids_str}]")
-            inputs = task.input_files if task.deps else task_graph.input_files
-            inputs_str = ",".join(inputs)
-            print(f"Input-files=[{inputs_str}]")
-        if is_error and args.verbose:
-            print("Error=", end="")
-            traceback.print_exception(
-                etype=None, value=record.error,
-                tb=record.error.__traceback__,
-                file=sys.stdout
-            )
-        if args.verbose:
-            print()
-
-
-def _prune_tasks(graph, ids_to_run):
-    if not ids_to_run:
-        return graph
-
-    try:
-        return graph.prune(ids_to_keep=ids_to_run)
-    except KeyError as exp:
-        print(f"Task IDs {exp.args[0]} not found", file=sys.stderr)
-        sys.exit(1)
-
-
-def _add_task_deps(task, task_set):
-    for dep in task.deps:
-        if dep not in task_set:
-            task_set.add(dep)
-            _add_task_deps(dep, task_set)
-
-
-def run_with_project(project, executor=SequentialExecutor(), ids_to_run=None,
-                     keep_going=False):
-    """Import the project using the provided executor."""
+def run_with_project(project, executor=SequentialExecutor()):
     storage = project.get_import_storage()
     task_graph = storage.generate_import_task_graph(project)
     task_graph.input_files.extend(project.config_filenames)
-    task_graph = _prune_tasks(task_graph, ids_to_run)
 
-    any_errors = False
-
-    tasks_iter = executor.execute(task_graph)
-    last_stagenames = _get_current_stagenames(executor)
-    _print_stagenames(last_stagenames)
-    for task, result_or_error in tasks_iter:
-        if isinstance(result_or_error, Exception):
-            if keep_going:
-                print(f"Task {task.task_id} failed with:", file=sys.stderr)
-                traceback.print_exception(
-                    etype=None, value=result_or_error,
-                    tb=result_or_error.__traceback__
-                )
-                any_errors = True
-            else:
-                raise result_or_error
-        current_stagenames = _get_current_stagenames(executor)
-        if current_stagenames != last_stagenames:
-            last_stagenames = current_stagenames
-            _print_stagenames(last_stagenames)
-
-    return 1 if any_errors else 0
-
-
-def _get_current_stagenames(executor):
-    # we use the task name is also the stagename
-    return {
-        t.task_id
-        for t in executor.get_active_tasks()
-        if t.task_id is not None
-    }
-
-
-def _print_stagenames(stagenames):
-    if stagenames:
-        stagenames_str = ", ".join(stagenames)
-        print(f"Executing stage: {stagenames_str}")
-
-
-def _create_task_cache(args, project):
-    cache_dir = fs_utils.join(
-        project.work_dir, ".task-progress", project.study_id
-    )
-    return FileTaskCache(force=args.force, cache_dir=cache_dir)
+    return TaskGraphCli.run(task_graph, executor, keep_going=False)
--- dae/dae/task_graph/cli_tools.py
+++ dae/dae/task_graph/cli_tools.py
@@ -5,8 +5,8 @@ import traceback
 import yaml
 from dae.task_graph.cache import CacheRecordType, TaskCache, \
     FileTaskCache, NoTaskCache
-from dae.task_graph.executor import DaskExecutor
-from dae.task_graph.executor import SequentialExecutor
+from dae.task_graph.executor import DaskExecutor, TaskGraphExecutor, \
+    SequentialExecutor
 
 from dae.task_graph.graph import TaskGraph
 from dae.dask.named_cluster import setup_client
@@ -14,10 +14,13 @@ from dae.dask.named_cluster import setup_client_from_config
 
 
 class TaskGraphCli:
+    """Takes care of creating a task graph executor and executing a graph."""
+
     @staticmethod
     def add_arguments(parser: argparse.ArgumentParser,
                       force_mode="optional",
                       default_task_status_dir="."):
+        """Add arguments needed to execute a task graph."""
 
         executor_group = parser.add_argument_group(title="Task Graph Executor")
         # cluster_name
@@ -69,6 +72,7 @@ class TaskGraphCli:
 
     @staticmethod
     def process_graph(args: argparse.Namespace, task_graph: TaskGraph) -> bool:
+        """Process task_graph in according with the arguments in args."""
         task_cache: TaskCache
         if "force" not in vars(args):
             # if the force_mode is set no 'always'
@@ -80,45 +84,16 @@ class TaskGraphCli:
         if args.task_ids:
             task_graph = task_graph.prune(ids_to_keep=args.task_ids)
 
+        executor = TaskGraphCli.create_executor(args)
         if args.command is None or args.command == "run":
-            if args.jobs == 1:
-                assert args.dask_cluster_name is None
-                assert args.dask_cluster_config_file is None
-                executor = SequentialExecutor(task_cache=task_cache)
-            else:
-                assert args.dask_cluster_name is None or \
-                    args.dask_cluster_config_file is None
-                if args.dask_cluster_config_file is not None:
-                    with open(args.dask_cluster_config_file) as F:
-                        dask_cluster_config = yaml.safe_load(F)
-                    print("THE CLUSTER CONFIG IS:", dask_cluster_config,
-                          "loaded from", args.dask_cluster_config_file)
-                    client, _ = setup_client_from_config(
-                        dask_cluster_config,
-                        number_of_threads=args.jobs)
-                else:
-                    client, _ = setup_client(args.dask_cluster_name,
-                                             number_of_threads=args.jobs)
-                print("Working with client:", client)
-                executor = DaskExecutor(client, task_cache=task_cache)
+            return TaskGraphCli.run(task_graph, executor, args.keep_going)
 
-            no_errors = True
-            tasks_iter = executor.execute(task_graph)
-            for task, result_or_error in tasks_iter:
-                if isinstance(result_or_error, Exception):
-                    if args.keep_going:
-                        print(f"Task {task.task_id} failed with:",
-                              file=sys.stderr)
-                        no_errors = False
-                    else:
-                        raise result_or_error
-            return no_errors
-        elif args.command == "list":
-            task_cache.set_task_graph(task_graph)
+        if args.command == "list":
             id_col_len = max(len(t.task_id) for t in task_graph.tasks)
             id_col_len = min(120, max(50, id_col_len))
             columns = ["TaskID", "Status"]
             print(f"{columns[0]:{id_col_len}s} {columns[1]}")
+            task_cache = getattr(executor, "_task_cache")
             for task in task_graph.tasks:
                 record = task_cache.get_record(task)
                 status = record.type.name
@@ -134,9 +109,54 @@ class TaskGraphCli:
                         file=sys.stdout
                     )
             return True
-        else:
-            raise Exception("Unknown command")
 
-        # if args.command in {"list", "status"}:
-        #     return _cmd_list(args, project, task_cache)
-        # return 0
+        raise Exception("Unknown command")
+
+    @staticmethod
+    def run(
+        task_graph: TaskGraph, executor: TaskGraphExecutor, keep_going: bool
+    ) -> bool:
+        """Execute (runs) the task_graph with the given executor."""
+        tasks_iter = executor.execute(task_graph)
+        no_errors = True
+        for task, result_or_error in tasks_iter:
+            if isinstance(result_or_error, Exception):
+                if keep_going:
+                    print(f"Task {task.task_id} failed with:",
+                          file=sys.stderr)
+                    traceback.print_exception(
+                        etype=None, value=result_or_error,
+                        tb=result_or_error.__traceback__
+                    )
+                    no_errors = False
+                else:
+                    raise result_or_error
+        return no_errors
+
+    @staticmethod
+    def create_executor(args) -> TaskGraphExecutor:
+        """Create a task graph executor according to the args specified."""
+        task_cache = FileTaskCache(force=args.force,
+                                   cache_dir=args.task_status_dir)
+
+        if args.jobs == 1:
+            assert args.dask_cluster_name is None
+            assert args.dask_cluster_config_file is None
+            return SequentialExecutor(task_cache=task_cache)
+
+        assert args.dask_cluster_name is None or \
+            args.dask_cluster_config_file is None
+        if args.dask_cluster_config_file is not None:
+            with open(args.dask_cluster_config_file) as conf_file:
+                dask_cluster_config = yaml.safe_load(conf_file)
+            print("THE CLUSTER CONFIG IS:", dask_cluster_config,
+                  "loaded from", args.dask_cluster_config_file)
+            client, _ = setup_client_from_config(
+                dask_cluster_config,
+                number_of_threads_param=args.jobs
+            )
+        else:
+            client, _ = setup_client(args.dask_cluster_name,
+                                     number_of_threads=args.jobs)
+        print("Working with client:", client)
+        return DaskExecutor(client, task_cache=task_cache)
