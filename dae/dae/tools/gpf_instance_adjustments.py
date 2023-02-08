238b3b7ee5fd5f2a629b20aa7cd9f0958ec8420d
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2023-02-08 12:08:39 +0200

    Fix enable study command.

--- dae/dae/tools/gpf_instance_adjustments.py
+++ dae/dae/tools/gpf_instance_adjustments.py
@@ -209,11 +209,18 @@ class EnableDisableStudies(StudyConfigsAdjustmentCommand):
         if gpfjs is not None:
             selected_genotype_data = gpfjs.get("selected_genotype_data")
             if selected_genotype_data:
-                result = []
-                for study_id in selected_genotype_data:
-                    if study_id in self.study_ids:
-                        continue
-                    result.append(study_id)
+                if self.enabled:
+                    result = selected_genotype_data
+                    for study_id in self.study_ids:
+                        if study_id not in result:
+                            result.append(study_id)
+                else:
+                    result = []
+                    for study_id in selected_genotype_data:
+                        if study_id in self.study_ids:
+                            continue
+                        result.append(study_id)
+
                 gpfjs["selected_genotype_data"] = result
 
     def adjust_study(self, study_id, study_config):
