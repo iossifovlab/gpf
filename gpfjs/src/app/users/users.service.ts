7560fe6ee3d9f4d3ece32b490064c13fc52f242e
IlinaGergova <ili.gergova@gmail.com>
2023-02-21 20:04:04 +0200

    users service

--- gpfjs/src/app/users/users.service.ts
+++ gpfjs/src/app/users/users.service.ts
@@ -76,9 +76,9 @@ export class UsersService {
 
   public isEmailValid(email: string): boolean {
     const re = new RegExp(
-      "[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{" +
-      "|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?" +
-      ":[a-z0-9-]*[a-z0-9])?"
+      '[a-z0-9!#$%&\'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&\'*+/=?^_`{' +
+      '|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?' +
+      ':[a-z0-9-]*[a-z0-9])?'
     );
     return re.test(String(email).toLowerCase());
   }
