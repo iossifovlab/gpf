a0c9e0f648fc30ed0e933ee75ffdc88bc21d35d3
IlinaGergova <ili.gergova@gmail.com>
2023-02-21 19:50:24 +0200

    pheno-browser service

--- gpfjs/src/app/pheno-browser/pheno-browser.service.ts
+++ gpfjs/src/app/pheno-browser/pheno-browser.service.ts
@@ -82,13 +82,12 @@ export class PhenoBrowserService {
       console.warn('oboejs encountered a fail event while streaming');
     });
 
-    return measuresSubject.pipe(map(data => { 
+    return measuresSubject.pipe(map(data => {
       if (data === null) {
         return null;
       }
-      return PhenoMeasure.fromJson(data['measure'] as object)
+      return PhenoMeasure.fromJson(data['measure'] as object);
     }));
-
   }
 
   public getMeasuresInfo(datasetId: string): Observable<PhenoMeasures> {
@@ -115,7 +114,7 @@ export class PhenoBrowserService {
     return this.http.post(
       this.config.baseUrl + this.downloadUrl,
       { dataset_id: datasetId, instrument: instrument, measure_ids: measureIds },
-      { 
+      {
         responseType: 'blob',
         headers: new HttpHeaders().append('Content-Type', 'application/json'),
         withCredentials: true
