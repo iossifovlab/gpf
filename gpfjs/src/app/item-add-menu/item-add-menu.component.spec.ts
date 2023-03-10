4abd15e00860f4ffd555db60def23298ec209e6c
IvoStefanov <ivostefanovtues@gmail.com>
2023-03-10 14:14:40 +0200

    GPF-1672 fix item add menu unit tests
    remove async in before each because it bugs
    the recreation of component templates

--- gpfjs/src/app/item-add-menu/item-add-menu.component.spec.ts
+++ gpfjs/src/app/item-add-menu/item-add-menu.component.spec.ts
@@ -17,7 +17,7 @@ describe('ItemAddMenuComponent', () => {
   const item5 = new Item('5', 'item5');
   const item6 = new Item('6', 'item6');
 
-  beforeEach(waitForAsync(() => {
+  beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [
         ItemAddMenuComponent,
@@ -38,7 +38,7 @@ describe('ItemAddMenuComponent', () => {
       }
       return of([] as Item[]);
     };
-  }));
+  });
 
   it('should create', () => {
     expect(component).toBeTruthy();
@@ -59,14 +59,18 @@ describe('ItemAddMenuComponent', () => {
 
   it('should search', () => {
     const getItemsSpy = jest.spyOn(component, 'getItems');
+    fixture.debugElement.query(By.css('.add-button')).triggerEventHandler('click', {});
+
+    expect(getItemsSpy).toHaveBeenCalledTimes(1);
+    expect(getItemsSpy).toHaveBeenCalledWith(1, '');
 
     component.search('item3');
-    expect(getItemsSpy).toHaveBeenCalledTimes(1);
+    expect(getItemsSpy).toHaveBeenCalledTimes(2);
     expect(getItemsSpy).toHaveBeenCalledWith(1, 'item3');
     expect(component.items).toStrictEqual([item1, item2, item3]);
 
     component.search('item1');
-    expect(getItemsSpy).toHaveBeenCalledTimes(2);
+    expect(getItemsSpy).toHaveBeenCalledTimes(3);
     expect(getItemsSpy).toHaveBeenCalledWith(1, 'item1');
     expect(component.items).toStrictEqual([item1, item2, item3]);
   });
@@ -74,9 +78,8 @@ describe('ItemAddMenuComponent', () => {
   it('should request item list update when list is scrolled', () => {
     const searchSpy = jest.spyOn(component, 'search');
     const getItemsSpy = jest.spyOn(component, 'getItems');
-    const button = fixture.debugElement.query(By.css('.add-button'));
 
-    button.triggerEventHandler('click', {});
+    fixture.debugElement.query(By.css('.add-button')).triggerEventHandler('click', {});
     fixture.detectChanges();
     const tableContainer = fixture.debugElement.query(By.css('.table-container'));
 
@@ -95,7 +98,7 @@ describe('ItemAddMenuComponent', () => {
 
   it('should close menu when clicking outside', () => {
     // Simulate opening the menu
-    component.showMenu = true;
+    fixture.debugElement.query(By.css('.add-button')).triggerEventHandler('click', {});
 
     // Simulate click inside the menu
     component.clickInside();
