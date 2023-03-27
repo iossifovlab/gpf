ef6aaf0648943e9c5c4f02ccb4017cd1510a8653
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2023-03-27 12:40:57 +0300

    Clean up of setup for wdae package.
    Empty tests file removed.

--- wdae/setup.py
+++ wdae/setup.py
@@ -1,35 +1,36 @@
-from pathlib import Path
-from typing import Dict, List
+# from pathlib import Path
+# from typing import Dict, List
 
 import setuptools
 
 
-def _expand_recursive_globs(
-        root_dir: str,
-        package_data: Dict[str, List[str]]) -> Dict[str, List[str]]:
-    root = (Path(__file__).parent / root_dir).resolve()
-    for module, patterns in package_data.items():
-        new_patterns = []
-        module_root = root / module
-        for p in patterns:
-            if "**" in p:
-                pattern_prefix = p.split("**")[0]
-                path_to_glob = module_root / pattern_prefix
-                for f in path_to_glob.glob("**"):  # all subdirectories
-                    if f.name == "__pycache__":
-                        continue
-                    subdir_pattern = p.replace(
-                        "**", str(f.relative_to(path_to_glob))
-                    )
-                    new_patterns.append(subdir_pattern)
-            else:
-                new_patterns.append(p)
-        package_data[module] = new_patterns
-    return package_data
+# def _expand_recursive_globs(
+#         root_dir: str,
+#         package_data: Dict[str, List[str]]) -> Dict[str, List[str]]:
+#     root = (Path(__file__).parent / root_dir).resolve()
+#     for module, patterns in package_data.items():
+#         new_patterns = []
+#         module_root = root / module
+#         for p in patterns:
+#             if "**" in p:
+#                 pattern_prefix = p.split("**")[0]
+#                 path_to_glob = module_root / pattern_prefix
+#                 for f in path_to_glob.glob("**"):  # all subdirectories
+#                     if f.name == "__pycache__":
+#                         continue
+#                     subdir_pattern = p.replace(
+#                         "**", str(f.relative_to(path_to_glob))
+#                     )
+#                     new_patterns.append(subdir_pattern)
+#             else:
+#                 new_patterns.append(p)
+#         package_data[module] = new_patterns
+#     return package_data
 
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
+
 
 setuptools.setup(
     name="gpf_wdae",
@@ -40,21 +41,21 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/IossifovLab/gpf",
-    packages=setuptools.find_packages(
-        where="wdae/", exclude=["docs", "*.tests.*", "tests", ]
+    packages=setuptools.find_namespace_packages(
+        where="wdae/", exclude=["*.tests", "*.tests.*", "docs"]
     ),
     include_package_data=True,
     package_dir={"": "wdae"},
-    package_data=_expand_recursive_globs("wdae", {
-        "gpfjs": [
-            "static/**/*",
-            "templates/**/*",
-        ],
-        "users_api": [
-            "static/**/*",
-            "templates/**/*",
-        ]
-    }),
+    # package_data=_expand_recursive_globs("wdae", {
+    #     "gpfjs": [
+    #         "static/**/*",
+    #         "templates/**/*",
+    #     ],
+    #     "users_api": [
+    #         "static/**/*",
+    #         "templates/**/*",
+    #     ]
+    # }),
     scripts=[
         "wdae/wdaemanage.py",
         "wdae/wdae_create_dev_users.sh",
@@ -71,5 +72,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.6",
+    python_requires=">=3.9",
 )
