28ee18fbe634e6af314bdc3d6cdd6f033a930610
joankosev <joankosev@gmail.com>
2023-04-06 17:27:18 +0300

    sentry: update tunneling logic

--- gpfjs/src/environments/environment.prod.ts
+++ gpfjs/src/environments/environment.prod.ts
@@ -5,5 +5,6 @@ export const environment = {
   basePath: basePath,
   apiPath: basePath + 'api/v3/',
   imgPathPrefix: 'assets/',
+  sentryTunnel: 'sentry/',
   oauthClientId: 'gpfjs'
 };
--- gpfjs/src/main.ts
+++ gpfjs/src/main.ts
@@ -4,14 +4,14 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 import * as Sentry from '@sentry/angular-ivy';
 
-Sentry.init({
-  dsn: 'https://0@0.ingest.sentry.io/0', // wdae/sentry/views.py
-  tunnel: 'http://localhost:8000/api/v3/sentry/',
-  tracesSampleRate: 1.0,
-});
-
 if (environment.production) {
   enableProdMode();
+
+  Sentry.init({
+    dsn: 'https://0@0.ingest.sentry.io/0', // wdae/sentry/views.py
+    tunnel: environment.apiPath + environment.sentryTunnel,
+    tracesSampleRate: 1.0,
+  });
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.log(err));
