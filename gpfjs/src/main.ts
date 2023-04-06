c261bdfb637a63273a2640ed1c64485833f7b1e0
joankosev <joankosev@gmail.com>
2023-04-06 15:24:20 +0300

    add sentry support

--- gpfjs/src/main.ts
+++ gpfjs/src/main.ts
@@ -1,13 +1,17 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import * as Sentry from '@sentry/angular-ivy';
 
+Sentry.init({
+  dsn: 'https://0@0.ingest.sentry.io/0', // wdae/sentry/views.py
+  tunnel: 'http://localhost:8000/api/v3/sentry/',
+  tracesSampleRate: 1.0,
+});
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.log(err));
