51de458fab41e629078e335fdbe1e6fea5ef12fc
IlinaGergova <ili.gergova@gmail.com>
2023-04-12 14:57:16 +0300

    jenkins test user reset password

--- web_e2e/cypress/e2e/user-management.cy.ts
+++ web_e2e/cypress/e2e/user-management.cy.ts
@@ -21,9 +21,8 @@ describe('User management tests for reset password in Users', () => {
     cy.request('GET', 'http://mailhog:8025/api/v2/search?kind=to&query=user_reset_password@email.com').then(
       (response) => {
         const lines: string[] = (response.body.items[0].Content.Body as string).split('\r\n');
-        // const url = lines[1].replace(' http://gpf/gpf/', ''); // remove when commit ??
-        // cy.visit(url);
-        cy.visit(lines[1]);
+        const url = lines[1].replace(' http://gpf/gpf/', ''); // remove when commit ??
+        cy.visit(url);
         page.newPasswordInput.type('XC^ZF*TZXuUChFsv');
         page.repeatNewPasswordInput.type('XC^ZF*TZXuUChFsv');
         page.newPasswordButton.click();
@@ -66,9 +65,8 @@ describe('User management tests for reset password in Users', () => {
     cy.request('GET', 'http://mailhog:8025/api/v2/search?kind=to&query=forgotten_password@email.com').then(
       (response) => {
         const lines: string[] = (response.body.items[0].Content.Body as string).split('\r\n');
-        // const url = lines[1].replace(' http://gpf/gpf/', 'http://172.20.0.6/gpf/'); // remove when commit
-        // cy.visit(url);
-        cy.visit(lines[1]);
+        const url = lines[1].replace(' http://gpf/gpf/', 'http://172.20.0.6/gpf/'); // remove when commit
+        cy.visit(url);
         page.newPasswordInput.type('XC^ZF*TZXuUChFsv');
         page.repeatNewPasswordInput.type('XC^ZF*TZXuUChFsv');
         page.newPasswordButton.click();
