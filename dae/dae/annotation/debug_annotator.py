83b48e55136cd2104942b9f7d1e49ffd3c3fe3bf
Ivan Iossifov <iossifov@cshl.edu>
2023-05-26 16:26:17 -0400

    checkpoint before starting the genomic scores refactoring.

---
+++ dae/dae/annotation/debug_annotator.py
@@ -0,0 +1,37 @@
+
+from typing import Any
+from .annotatable import Annotatable
+from .annotation_pipeline import AnnotationPipeline
+from .annotation_pipeline import Annotator
+from .annotation_pipeline import AnnotatorInfo
+from .annotation_factory import AnnotationConfigParser
+
+
+class HelloWorldAnnotator(Annotator):
+    def __init__(self, pipeline: AnnotationPipeline, info: AnnotatorInfo):
+        super().__init__(pipeline, info)
+
+    def annotate(self, _annotatable: Annotatable, _context: dict[str, Any]) \
+            -> dict[str, Any]:
+        r = {}
+        for attribute_config in self._info.attributes:
+            assert attribute_config.source == "hi"
+            r[attribute_config.name] = "hello world"
+        return r
+
+
+def build_annotator(pipeline: AnnotationPipeline,
+                    info: AnnotatorInfo) -> Annotator:
+
+    if not info.attributes:
+        info.attributes = AnnotationConfigParser.parse_raw_attributes(["hi"])
+
+    for attribute_info in info.attributes:
+        if attribute_info.source != "hi":
+            raise ValueError(f"The {info.type} does not provide the source "
+                             f"{attribute_info.source}. The only source "
+                             "provided is 'hi'")
+        attribute_info.type = "str"
+        attribute_info.description = \
+            "The attribute 'hi' has as constant value of 'hello world.'"
+    return HelloWorldAnnotator(pipeline, info)
---
+++ dae/dae/annotation/tests/test_basiscs_with_debug.py
@@ -0,0 +1,87 @@
+# pylint: disable=W0621,C0114,C0116,W0212,W0613
+
+from dae.genomic_resources.testing import build_inmemory_test_repository
+from dae.annotation.annotation_factory import build_annotation_pipeline
+from dae.annotation.annotatable import Position
+
+
+def test_default_attributes():
+    empty_grr = build_inmemory_test_repository({})
+    annotation_cofiguration = """
+    - debug_annotator
+    """
+    pipeline = build_annotation_pipeline(
+        pipeline_config_str=annotation_cofiguration,
+        grr_repository=empty_grr)
+
+    info = pipeline.get_info()
+    assert len(info) == 1
+    assert info[0].type == "debug_annotator"
+    assert len(info[0].attributes) == 1
+    assert info[0].attributes[0].name == "hi"
+    assert info[0].attributes[0].source == "hi"
+    assert info[0].attributes[0].type == "str"
+
+    annotation = pipeline.annotate(Position("1", 3))
+    assert annotation["hi"] == "hello world"
+
+
+def test_default_remapping():
+    empty_grr = build_inmemory_test_repository({})
+    annotation_cofiguration = """
+    - debug_annotator:
+        attributes:
+          - hi
+          - destination: hi2
+            source: hi
+          - destination: hi3
+            source: hi
+    """
+    pipeline = build_annotation_pipeline(
+        pipeline_config_str=annotation_cofiguration,
+        grr_repository=empty_grr)
+
+    info = pipeline.get_info()
+    assert len(info) == 1
+    assert info[0].type == "debug_annotator"
+    assert len(info[0].attributes) == 3
+    assert info[0].attributes[1].name == "hi2"
+    assert info[0].attributes[1].source == "hi"
+    assert info[0].attributes[1].type == "str"
+
+    annotation = pipeline.annotate(Position("1", 3))
+    assert annotation["hi"] == "hello world"
+    assert annotation["hi2"] == "hello world"
+    assert annotation["hi3"] == "hello world"
+
+
+def test_str_value_trasform():
+    empty_grr = build_inmemory_test_repository({})
+    annotation_cofiguration = """
+    - debug_annotator:
+        attributes:
+          - hi
+          - destination: hi_gosho
+            source: hi
+            value_transform: value + ' gosho'
+          - destination: hi_len
+            source: hi
+            value_transform: len(value)
+    """
+    pipeline = build_annotation_pipeline(
+        pipeline_config_str=annotation_cofiguration,
+        grr_repository=empty_grr)
+
+    info = pipeline.get_info()
+    assert len(info) == 1
+    assert info[0].type == "debug_annotator"
+    assert len(info[0].attributes) == 3
+    assert info[0].attributes[1].name == "hi_gosho"
+    assert info[0].attributes[1].parameters["value_transform"] == \
+        "value + ' gosho'"
+    assert info[0].attributes[1].type == "str"
+
+    annotation = pipeline.annotate(Position("1", 3))
+    assert annotation["hi"] == "hello world"
+    assert annotation["hi_gosho"] == "hello world gosho"
+    assert annotation["hi_len"] == 11
