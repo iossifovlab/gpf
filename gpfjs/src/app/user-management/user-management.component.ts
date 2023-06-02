d1e03db6365f64da4fcfa1bfbeb7d6bb4c0a34a5
IlinaGergova <ili.gergova@gmail.com>
2023-06-02 18:37:32 +0300

    fix creation user error message management

--- gpfjs/src/app/user-management/user-management.component.ts
+++ gpfjs/src/app/user-management/user-management.component.ts
@@ -93,8 +93,8 @@ export class UserManagementComponent implements OnInit {
     const newUser = new User(null, name, email, ['any_user', email], false, []);
 
     this.usersService.createUser(newUser)
-      .pipe(catchError(err => {
-        this.creationError = err as string;
+      .pipe(catchError((err: {error: { email: string[] } }) => {
+        this.creationError = `Error: ${err.error.email[0]}`;
         return of(null);
       }))
       .subscribe((user: User) => {
