3974bf706e6ff3c0580d823f80aefcba3d5ad2cd
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2023-06-29 11:11:58 +0000

    Bump version: 2023.6.0rc9 → 2023.6.0rc10

--- dae/dae/__version__.py
+++ dae/dae/__version__.py
@@ -1,5 +1,5 @@
 """GPF version."""
 
-RELEASE = "2023.6.0rc9"
+RELEASE = "2023.6.0rc10"
 
 VERSION = ".".join(RELEASE.split(".")[:2])
--- wdae/wdae/__version__.py
+++ wdae/wdae/__version__.py
@@ -1,3 +1,3 @@
-RELEASE = "2023.6.0rc9"
+RELEASE = "2023.6.0rc10"
 
 VERSION = ".".join(RELEASE.split(".")[:2])
