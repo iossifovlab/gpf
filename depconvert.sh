e813fd3ecb93393ef091fb4c7b0d886c9f23835f
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2023-07-07 11:40:26 +0300

    Clean up dependencies.

--- depconvert.sh
+++ depconvert.sh
@@ -4,5 +4,5 @@
 grep "=" environment.yml | \
     sed -E "s/\s+-\s+(.+)=(.+)$/\1==\2/g" > requirements.txt
 
-grep "=" dev-environment.yml | \
-    sed -E "s/\s+-\s+(.+)=(.+)$/\1==\2/g" >> requirements.txt
+# grep "=" dev-environment.yml | \
+#     sed -E "s/\s+-\s+(.+)=(.+)$/\1==\2/g" >> requirements.txt
