d5e4c3f4c72f3e114b4b42293e04e8b2353100c1
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2023-07-07 11:55:17 +0300

    Bump version of scipy to 1.10.0

--- depconvert.sh
+++ depconvert.sh
@@ -3,6 +3,3 @@
 
 grep "=" environment.yml | \
     sed -E "s/\s+-\s+(.+)=(.+)$/\1==\2/g" > requirements.txt
-
-# grep "=" dev-environment.yml | \
-#     sed -E "s/\s+-\s+(.+)=(.+)$/\1==\2/g" >> requirements.txt
