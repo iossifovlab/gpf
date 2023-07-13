e9d2911c5239acf6bd825bf0f85421542c97988f
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2023-07-13 09:29:49 +0300

    Inheritance tests for genotype storage transfered to integration.

--- dae/dae/variants/tests/test_inheritance_checks.py
+++ dae/dae/variants/tests/test_inheritance_checks.py
@@ -3,7 +3,7 @@ import numpy as np
 from dae.variants.family_variant import FamilyAllele as FV
 
 
-def test_denovo_check_1():
+def test_denovo_check_1() -> None:
     # AA, AA -> AC
     assert FV.check_denovo_trio(
         np.array([0, 0]), np.array([0, 0]), np.array([0, 1]), 1
@@ -13,7 +13,7 @@ def test_denovo_check_1():
     )
 
 
-def test_denovo_check_2():
+def test_denovo_check_2() -> None:
     # AA, AA -> CA
     assert FV.check_denovo_trio(
         np.array([0, 0]), np.array([0, 0]), np.array([1, 0]), 1
@@ -23,7 +23,7 @@ def test_denovo_check_2():
     )
 
 
-def test_denovo_check_3():
+def test_denovo_check_3() -> None:
     # CC, CC -> CA
     assert FV.check_denovo_trio(
         np.array([1, 1]), np.array([1, 1]), np.array([1, 0]), 0
@@ -33,7 +33,7 @@ def test_denovo_check_3():
     )
 
 
-def test_denovo_check_4():
+def test_denovo_check_4() -> None:
     # CC, CC -> AA
     assert FV.check_denovo_trio(
         np.array([1, 1]), np.array([1, 1]), np.array([0, 0]), 0
@@ -43,7 +43,7 @@ def test_denovo_check_4():
     )
 
 
-def test_denovo_check_5():
+def test_denovo_check_5() -> None:
     # GG, GG -> AC
     assert FV.check_denovo_trio(
         np.array([2, 2]), np.array([2, 2]), np.array([0, 1]), 0
@@ -56,7 +56,7 @@ def test_denovo_check_5():
     )
 
 
-def test_denovo_check_6():
+def test_denovo_check_6() -> None:
     # CC,CC -> GC
     assert FV.check_denovo_trio(
         np.array([1, 1]), np.array([1, 1]), np.array([2, 1]), 2
@@ -69,7 +69,7 @@ def test_denovo_check_6():
     )
 
 
-def test_omission_check_1():
+def test_omission_check_1() -> None:
     # CC, AA -> AA
     assert FV.check_omission_trio(
         np.array([1, 1]), np.array([0, 0]), np.array([0, 0]), 1
@@ -85,7 +85,7 @@ def test_omission_check_1():
     )
 
 
-def test_omission_check_2():
+def test_omission_check_2() -> None:
     # AA, CC -> AA
     assert FV.check_omission_trio(
         np.array([0, 0]), np.array([1, 1]), np.array([0, 0]), 1
@@ -95,7 +95,7 @@ def test_omission_check_2():
     )
 
 
-def test_omission_check_3():
+def test_omission_check_3() -> None:
     # AA,CC -> CC
     assert FV.check_omission_trio(
         np.array([0, 0]), np.array([1, 1]), np.array([1, 1]), 0
@@ -105,7 +105,7 @@ def test_omission_check_3():
     )
 
 
-def test_omission_check_4():
+def test_omission_check_4() -> None:
     # AA,CC -> CC
     assert FV.check_omission_trio(
         np.array([1, 1]), np.array([0, 0]), np.array([1, 1]), 0
@@ -115,24 +115,30 @@ def test_omission_check_4():
     )
 
 
-def test_omission_not_denovo():
+def test_omission_not_denovo() -> None:
     # AA,CC -> AA
     trio = [
         np.array([0, 0]),  # p1
         np.array([1, 1]),  # p2
         np.array([0, 0]),  # ch
     ]
-    assert FV.check_omission_trio(*trio, allele_index=1)
-    assert not FV.check_denovo_trio(*trio, allele_index=1)
-    assert not FV.check_mendelian_trio(*trio, allele_index=1)
+    assert FV.check_omission_trio(
+        trio[0], trio[1], trio[2], allele_index=1)
+    assert not FV.check_denovo_trio(
+        trio[0], trio[1], trio[2], allele_index=1)
+    assert not FV.check_mendelian_trio(
+        trio[0], trio[1], trio[2], allele_index=1)
 
     # FIXME:
-    assert not FV.check_omission_trio(*trio, allele_index=0)
-    assert not FV.check_denovo_trio(*trio, allele_index=0)
-    assert FV.check_mendelian_trio(*trio, allele_index=0)
+    assert not FV.check_omission_trio(
+        trio[0], trio[1], trio[2], allele_index=0)
+    assert not FV.check_denovo_trio(
+        trio[0], trio[1], trio[2], allele_index=0)
+    assert FV.check_mendelian_trio(
+        trio[0], trio[1], trio[2], allele_index=0)
 
 
-def test_mendelian_from_ivan_1():
+def test_mendelian_from_ivan_1() -> None:
     # AC AA AA mendel
     assert FV.check_mendelian_trio(
         np.array([0, 1]), np.array([0, 0]), np.array([0, 0]), 0
@@ -143,7 +149,7 @@ def test_mendelian_from_ivan_1():
     )
 
 
-def test_mendelian_from_ivan_2():
+def test_mendelian_from_ivan_2() -> None:
     # AC AA AC mendel
     assert FV.check_mendelian_trio(
         np.array([0, 1]), np.array([0, 0]), np.array([0, 1]), 0
@@ -154,7 +160,7 @@ def test_mendelian_from_ivan_2():
     )
 
 
-def test_mendelian_from_ivan_3():
+def test_mendelian_from_ivan_3() -> None:
     # AC AC AA mendel
     assert FV.check_mendelian_trio(
         np.array([0, 1]), np.array([0, 1]), np.array([0, 0]), 0
@@ -164,7 +170,7 @@ def test_mendelian_from_ivan_3():
     )
 
 
-def test_mendelian_from_ivan_4():
+def test_mendelian_from_ivan_4() -> None:
     # AC AC AC mendel
     assert FV.check_mendelian_trio(
         np.array([0, 1]), np.array([0, 1]), np.array([0, 1]), 0
@@ -174,7 +180,7 @@ def test_mendelian_from_ivan_4():
     )
 
 
-def test_mendelian_from_ivan_5():
+def test_mendelian_from_ivan_5() -> None:
     # AC AC CC mendel
     assert FV.check_mendelian_trio(
         np.array([0, 1]), np.array([0, 1]), np.array([1, 1]), 1
@@ -184,7 +190,7 @@ def test_mendelian_from_ivan_5():
     )
 
 
-def test_denovo_and_omission_from_ivan_1():
+def test_denovo_and_omission_from_ivan_1() -> None:
     # AA AA AC denovo
     assert FV.check_denovo_trio(
         np.array([0, 0]), np.array([0, 0]), np.array([0, 1]), 1
@@ -194,7 +200,7 @@ def test_denovo_and_omission_from_ivan_1():
     )
 
 
-def test_denovo_and_omission_from_ivan_2():
+def test_denovo_and_omission_from_ivan_2() -> None:
     # AC AA CC omission
     assert FV.check_omission_trio(
         np.array([0, 1]), np.array([0, 0]), np.array([1, 1]), 0
@@ -204,7 +210,7 @@ def test_denovo_and_omission_from_ivan_2():
     )
 
 
-def test_denovo_and_omission_from_ivan_3():
+def test_denovo_and_omission_from_ivan_3() -> None:
     # AA CC AA omission
     assert FV.check_omission_trio(
         np.array([0, 0]), np.array([1, 1]), np.array([0, 0]), 1
@@ -214,7 +220,7 @@ def test_denovo_and_omission_from_ivan_3():
     )
 
 
-def test_denovo_and_omission_from_ivan_4():
+def test_denovo_and_omission_from_ivan_4() -> None:
     # AA CC CC omission
     assert FV.check_omission_trio(
         np.array([0, 0]), np.array([1, 1]), np.array([1, 1]), 0
@@ -224,7 +230,7 @@ def test_denovo_and_omission_from_ivan_4():
     )
 
 
-def test_weird_mendel_from_ivan_1():
+def test_weird_mendel_from_ivan_1() -> None:
     # AC AT AA mendel
     assert FV.check_mendelian_trio(
         np.array([0, 1]), np.array([0, 2]), np.array([0, 0]), 0
@@ -237,7 +243,7 @@ def test_weird_mendel_from_ivan_1():
     )
 
 
-def test_weird_mendel_from_ivan_2():
+def test_weird_mendel_from_ivan_2() -> None:
     # AC AT AT mendel
     assert FV.check_mendelian_trio(
         np.array([0, 1]), np.array([0, 2]), np.array([0, 2]), 0
@@ -250,7 +256,7 @@ def test_weird_mendel_from_ivan_2():
     )
 
 
-def test_weird_mendel_from_ivan_3():
+def test_weird_mendel_from_ivan_3() -> None:
     # AC AT AC mendel
     assert FV.check_mendelian_trio(
         np.array([0, 1]), np.array([0, 2]), np.array([0, 1]), 0
@@ -263,7 +269,7 @@ def test_weird_mendel_from_ivan_3():
     )
 
 
-def test_weird_mendel_from_ivan_4():
+def test_weird_mendel_from_ivan_4() -> None:
     # AC AT CT mendel
     assert not FV.check_mendelian_trio(
         np.array([0, 1]), np.array([0, 2]), np.array([1, 2]), 0
@@ -276,7 +282,7 @@ def test_weird_mendel_from_ivan_4():
     )
 
 
-def test_weird_denovo_from_ivan_1_1():
+def test_weird_denovo_from_ivan_1_1() -> None:
     # AA AC AG denovo
     assert FV.check_denovo_trio(
         np.array([0, 0]), np.array([0, 1]), np.array([0, 2]), 2
@@ -298,7 +304,7 @@ def test_weird_denovo_from_ivan_1_1():
     )
 
 
-def test_weird_denovo_from_ivan_1_2():
+def test_weird_denovo_from_ivan_1_2() -> None:
     # AC AA AG denovo
     assert FV.check_denovo_trio(
         np.array([0, 1]), np.array([0, 0]), np.array([0, 2]), 2
@@ -320,7 +326,7 @@ def test_weird_denovo_from_ivan_1_2():
     )
 
 
-def test_weird_denovo_from_ivan_1_3():
+def test_weird_denovo_from_ivan_1_3() -> None:
     # AA AC GA denovo
     assert FV.check_denovo_trio(
         np.array([0, 0]), np.array([0, 1]), np.array([2, 0]), 2
@@ -342,7 +348,7 @@ def test_weird_denovo_from_ivan_1_3():
     )
 
 
-def test_weird_denovo_from_ivan_1_4():
+def test_weird_denovo_from_ivan_1_4() -> None:
     # AC AA GA denovo
     assert FV.check_denovo_trio(
         np.array([0, 1]), np.array([0, 0]), np.array([2, 0]), 2
@@ -364,7 +370,7 @@ def test_weird_denovo_from_ivan_1_4():
     )
 
 
-def test_weird_denovo_from_ivan_3():
+def test_weird_denovo_from_ivan_3() -> None:
     # AC AC AG denovo
     assert FV.check_denovo_trio(
         np.array([0, 1]), np.array([0, 1]), np.array([0, 2]), 2
@@ -386,7 +392,7 @@ def test_weird_denovo_from_ivan_3():
     )
 
 
-def test_weird_denovo_from_ivan_4():
+def test_weird_denovo_from_ivan_4() -> None:
     # AC AC CG denovo
     assert FV.check_denovo_trio(
         np.array([0, 1]), np.array([0, 1]), np.array([1, 2]), 2
@@ -408,99 +414,156 @@ def test_weird_denovo_from_ivan_4():
     )
 
 
-def test_super_weird_denovo_omission_mix_from_ivan_1():
+def test_super_weird_denovo_omission_mix_from_ivan_1() -> None:
     # AC AA TG denovo
     trio = [
         np.array([0, 1]),
         np.array([0, 0]),
         np.array([2, 3]),
     ]
-    assert FV.check_omission_trio(*trio, allele_index=0)
-    assert not FV.check_omission_trio(*trio, allele_index=1)
-    assert not FV.check_omission_trio(*trio, allele_index=2)
-    assert not FV.check_omission_trio(*trio, allele_index=3)
+    assert FV.check_omission_trio(
+        trio[0], trio[1], trio[2], allele_index=0)
+    assert not FV.check_omission_trio(
+        trio[0], trio[1], trio[2], allele_index=1)
+    assert not FV.check_omission_trio(
+        trio[0], trio[1], trio[2], allele_index=2)
+    assert not FV.check_omission_trio(
+        trio[0], trio[1], trio[2], allele_index=3)
 
-    assert not FV.check_denovo_trio(*trio, allele_index=0)
-    assert not FV.check_denovo_trio(*trio, allele_index=1)
-    assert FV.check_denovo_trio(*trio, allele_index=2)
-    assert FV.check_denovo_trio(*trio, allele_index=3)
-    # assert FV.calc_inheritance_trio(*trio) == Inheritance.denovo
+    assert not FV.check_denovo_trio(
+        trio[0], trio[1], trio[2], allele_index=0)
+    assert not FV.check_denovo_trio(
+        trio[0], trio[1], trio[2], allele_index=1)
+    assert FV.check_denovo_trio(
+        trio[0], trio[1], trio[2], allele_index=2)
+    assert FV.check_denovo_trio(
+        trio[0], trio[1], trio[2], allele_index=3)
 
 
-def test_super_weird_denovo_omission_mix_from_ivan_2():
+def test_super_weird_denovo_omission_mix_from_ivan_2() -> None:
     # AA AC TG denovo
     trio = [
         np.array([0, 0]),
         np.array([0, 1]),
         np.array([2, 3]),
     ]
-    assert FV.check_omission_trio(*trio, allele_index=0)
-    assert not FV.check_omission_trio(*trio, allele_index=1)
-    assert not FV.check_omission_trio(*trio, allele_index=2)
-    assert not FV.check_omission_trio(*trio, allele_index=3)
+    assert FV.check_omission_trio(
+        trio[0], trio[1], trio[2], allele_index=0)
+    assert not FV.check_omission_trio(
+        trio[0], trio[1], trio[2], allele_index=1)
+    assert not FV.check_omission_trio(
+        trio[0], trio[1], trio[2], allele_index=2)
+    assert not FV.check_omission_trio(
+        trio[0], trio[1], trio[2], allele_index=3)
 
-    assert not FV.check_denovo_trio(*trio, allele_index=0)
-    assert not FV.check_denovo_trio(*trio, allele_index=1)
-    assert FV.check_denovo_trio(*trio, allele_index=2)
-    assert FV.check_denovo_trio(*trio, allele_index=3)
+    assert not FV.check_denovo_trio(
+        trio[0], trio[1], trio[2], allele_index=0)
+    assert not FV.check_denovo_trio(
+        trio[0], trio[1], trio[2], allele_index=1)
+    assert FV.check_denovo_trio(
+        trio[0], trio[1], trio[2], allele_index=2)
+    assert FV.check_denovo_trio(
+        trio[0], trio[1], trio[2], allele_index=3)
     # assert FV.calc_inheritance_trio(*trio) == Inheritance.denovo
 
 
-def test_mixed_check_1():
+def test_mixed_check_1() -> None:
     # AA,CC -> GG
     trio = [
         np.array([1, 1]),
         np.array([0, 0]),
         np.array([2, 2]),
     ]
-    assert not FV.check_denovo_trio(*trio, allele_index=0)
-    assert not FV.check_denovo_trio(*trio, allele_index=1)
-    assert FV.check_denovo_trio(*trio, allele_index=2)
+    assert not FV.check_denovo_trio(
+        trio[0], trio[1], trio[2], allele_index=0)
+    assert not FV.check_denovo_trio(
+        trio[0], trio[1], trio[2], allele_index=1)
+    assert FV.check_denovo_trio(
+        trio[0], trio[1], trio[2], allele_index=2)
 
-    assert FV.check_omission_trio(*trio, allele_index=0)
-    assert FV.check_omission_trio(*trio, allele_index=1)
-    assert not FV.check_omission_trio(*trio, allele_index=2)
+    assert FV.check_omission_trio(
+        trio[0], trio[1], trio[2], allele_index=0)
+    assert FV.check_omission_trio(
+        trio[0], trio[1], trio[2], allele_index=1)
+    assert not FV.check_omission_trio(
+        trio[0], trio[1], trio[2], allele_index=2)
 
-    assert not FV.check_mendelian_trio(*trio, allele_index=0)
-    assert not FV.check_mendelian_trio(*trio, allele_index=1)
-    assert not FV.check_mendelian_trio(*trio, allele_index=2)
+    assert not FV.check_mendelian_trio(
+        trio[0], trio[1], trio[2], allele_index=0)
+    assert not FV.check_mendelian_trio(
+        trio[0], trio[1], trio[2], allele_index=1)
+    assert not FV.check_mendelian_trio(
+        trio[0], trio[1], trio[2], allele_index=2)
 
 
-def test_mixed_check_2():
+def test_mixed_check_2() -> None:
     # AA,CC -> AG
     trio = [
         np.array([1, 1]),
         np.array([0, 0]),
         np.array([2, 1]),
     ]
-    assert not FV.check_denovo_trio(*trio, allele_index=0)
-    assert not FV.check_denovo_trio(*trio, allele_index=1)
-    assert FV.check_denovo_trio(*trio, allele_index=2)
+    assert not FV.check_denovo_trio(
+        trio[0], trio[1], trio[2], allele_index=0)
+    assert not FV.check_denovo_trio(
+        trio[0], trio[1], trio[2], allele_index=1)
+    assert FV.check_denovo_trio(
+        trio[0], trio[1], trio[2], allele_index=2)
 
-    assert FV.check_omission_trio(*trio, allele_index=0)
-    assert not FV.check_omission_trio(*trio, allele_index=1)
-    assert not FV.check_omission_trio(*trio, allele_index=2)
+    assert FV.check_omission_trio(
+        trio[0], trio[1], trio[2], allele_index=0)
+    assert not FV.check_omission_trio(
+        trio[0], trio[1], trio[2], allele_index=1)
+    assert not FV.check_omission_trio(
+        trio[0], trio[1], trio[2], allele_index=2)
 
-    assert not FV.check_mendelian_trio(*trio, allele_index=0)
-    assert FV.check_mendelian_trio(*trio, allele_index=1)
-    assert not FV.check_mendelian_trio(*trio, allele_index=2)
+    assert not FV.check_mendelian_trio(
+        trio[0], trio[1], trio[2], allele_index=0)
+    assert FV.check_mendelian_trio(
+        trio[0], trio[1], trio[2], allele_index=1)
+    assert not FV.check_mendelian_trio(
+        trio[0], trio[1], trio[2], allele_index=2)
 
 
-def test_mixed_check_4():
+def test_mixed_check_4() -> None:
     # AA,CC -> AG
     trio = [
         np.array([1, 1]),  # p1
         np.array([0, 0]),  # p2
         np.array([0, 2]),  # ch
     ]
-    assert not FV.check_denovo_trio(*trio, allele_index=0)
-    assert not FV.check_denovo_trio(*trio, allele_index=1)
-    assert FV.check_denovo_trio(*trio, allele_index=2)
+    assert not FV.check_denovo_trio(
+        trio[0], trio[1], trio[2], allele_index=0)
+    assert not FV.check_denovo_trio(
+        trio[0], trio[1], trio[2], allele_index=1)
+    assert FV.check_denovo_trio(
+        trio[0], trio[1], trio[2], allele_index=2)
 
-    assert not FV.check_omission_trio(*trio, allele_index=0)
-    assert FV.check_omission_trio(*trio, allele_index=1)
-    assert not FV.check_omission_trio(*trio, allele_index=2)
+    assert not FV.check_omission_trio(
+        trio[0], trio[1], trio[2], allele_index=0)
+    assert FV.check_omission_trio(
+        trio[0], trio[1], trio[2], allele_index=1)
+    assert not FV.check_omission_trio(
+        trio[0], trio[1], trio[2], allele_index=2)
 
-    assert FV.check_mendelian_trio(*trio, allele_index=0)
-    assert not FV.check_mendelian_trio(*trio, allele_index=1)
-    assert not FV.check_mendelian_trio(*trio, allele_index=2)
+    assert FV.check_mendelian_trio(
+        trio[0], trio[1], trio[2], allele_index=0)
+    assert not FV.check_mendelian_trio(
+        trio[0], trio[1], trio[2], allele_index=1)
+    assert not FV.check_mendelian_trio(
+        trio[0], trio[1], trio[2], allele_index=2)
+
+
+def test_omission_and_denovo() -> None:
+    # AA,CC -> AA
+    trio = [
+        np.array([1, 1]),  # p1
+        np.array([1, 1]),  # p2
+        np.array([0, 1]),  # ch
+    ]
+
+    assert not FV.check_omission_trio(
+        trio[0], trio[1], trio[2], allele_index=1)
+
+    assert FV.check_denovo_trio(
+        trio[0], trio[1], trio[2], allele_index=0)
