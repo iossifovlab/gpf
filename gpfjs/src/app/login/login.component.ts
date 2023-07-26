bf84c9c5f69f72c41942a4c94d4427bd2713613d
iordanivanov <iiordan.ivanov@gmail.com>
2023-07-26 13:00:57 +0300

    Fix redirect logic in LoginComponent

--- gpfjs/src/app/login/login.component.ts
+++ gpfjs/src/app/login/login.component.ts
@@ -14,12 +14,13 @@ export class LoginComponent implements OnInit {
   public ngOnInit() {
     const url = new URL(window.location.href);
     let state = url.searchParams.get('state');
+    let redirectTo = ['datasets'];
     if (state) {
       state = JSON.parse(atob(state));
       if (state['came_from']) {
-        this.router.navigate([state['came_from']]);
+        redirectTo = [state['came_from']];
       }
     }
-    this.router.navigate(['datasets']);
+    this.router.navigate(redirectTo);
   }
 }
