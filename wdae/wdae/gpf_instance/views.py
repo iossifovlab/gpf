be910c1772343ec1604b8c2994a891840fcae4b7
IvoStefanov <ivostefanovtues@gmail.com>
2023-07-26 15:48:05 +0300

    refactor version api
    add error handling

--- wdae/wdae/gpf_instance/views.py
+++ wdae/wdae/gpf_instance/views.py
@@ -1,5 +1,6 @@
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.request import Request 
 from rest_framework.decorators import api_view
 from utils.logger import LOGGER, request_logging_function_view
 from dae import __version__ as VERSION
@@ -7,9 +8,17 @@ from dae import __version__ as VERSION
 
 @request_logging_function_view(LOGGER)
 @api_view(["GET"])
-def version(self) -> Response:
+def version(request: Request) -> Response:
     """Get GPF version."""
+    if request.method == "GET":
+        return Response(
+            {"version": VERSION},
+            status=status.HTTP_200_OK
+        )
     return Response(
-        {"version": VERSION},
-        status=status.HTTP_200_OK
+        {
+            "error":
+            "You have no permission to edit, update or delete the gpf version."
+        },
+        status=status.HTTP_403_FORBIDDEN
     )
