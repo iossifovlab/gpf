3ba6ce28f27096f3c3bdc282be7892612f81b695
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2023-09-12 18:32:14 +0300

    Generated persons added to the person set collection tests.

--- dae/dae/tests/test_person_set_collection.py
+++ dae/dae/tests/test_person_set_collection.py
@@ -18,18 +18,20 @@ def families_fixture(tmp_path: pathlib.Path) -> FamiliesData:
     ped_path = setup_pedigree(
         tmp_path / "test_pedigree" / "ped.ped",
         textwrap.dedent("""
-            familyId personId dadId	 momId	sex status role
-            f1       mom1     0      0      2   1      mom
-            f1       dad1     0      0      1   1      dad
-            f1       prb1     dad1   mom1   1   2      prb
-            f1       sib1     dad1   mom1   2   2      sib
-            f1       sib2     dad1   mom1   2   2      sib
-            f2       grmom2   0      0      2   1      maternal_grandmother
-            f2       grdad2   0      0      1   1      maternal_grandfather
-            f2       mom2     grdad2 grmom2 2   1      mom
-            f2       dad2     0      0      1   1      dad
-            f2       prb2     dad2   mom2   1   2      prb
-            f2       sib2_3   dad2   mom2   2   2      sib
+familyId personId dadId	 momId	sex status role                 generated
+f1       grmom1   0      0      2   0      maternal_grandmother True
+f1       grdad1   0      0      1   0      maternal_grandfather True
+f1       mom1     0      0      2   1      mom                  False
+f1       dad1     0      0      1   1      dad                  False
+f1       prb1     dad1   mom1   1   2      prb                  False
+f1       sib1     dad1   mom1   2   2      sib                  False
+f1       sib2     dad1   mom1   2   2      sib                  False
+f2       grmom2   0      0      2   0      maternal_grandmother True
+f2       grdad2   0      0      1   1      maternal_grandfather False
+f2       mom2     grdad2 grmom2 2   1      mom                  False
+f2       dad2     0      0      1   1      dad                  False
+f2       prb2     dad2   mom2   1   2      prb                  False
+f2       sib2_3   dad2   mom2   2   2      sib                  False
         """))
     families = FamiliesLoader(ped_path).load()
     assert families is not None
