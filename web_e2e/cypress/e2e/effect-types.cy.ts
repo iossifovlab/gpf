286660042551eea6f6d12fec9a27daf8534bb802
IlinaGergova <ili.gergova@gmail.com>
2023-09-19 17:26:13 +0300

    update effect types checkbox indexes

--- web_e2e/cypress/e2e/effect-types.cy.ts
+++ web_e2e/cypress/e2e/effect-types.cy.ts
@@ -42,12 +42,12 @@ describe('Effect types tests', () => {
   it('should test checkboxes using "UTRs" button', () => {
     page.navigateToDatasetPage(datasetIds.compAll, toolPageLinks.genotypeBrowser);
     genotypeBlockPage.findButtonInComponentContainingText('gpf-effect-types', 'UTRs').click();
+    cy.get('input').eq(14).should('be.checked');
+    cy.get('input').eq(14).siblings().first().should('have.text', '3\'UTR');
     cy.get('input').eq(15).should('be.checked');
-    cy.get('input').eq(15).siblings().first().should('have.text', '3\'UTR');
-    cy.get('input').eq(16).should('be.checked');
-    cy.get('input').eq(16).siblings().first().should('have.text', '5\'UTR');
+    cy.get('input').eq(15).siblings().first().should('have.text', '5\'UTR');
 
-    [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14].forEach(index => {
+    [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].forEach(index => {
       cy.get('input').eq(index).should('not.be.checked');
     });
   });
@@ -55,14 +55,14 @@ describe('Effect types tests', () => {
   it('should test checkboxes using "LGDs" button', () => {
     page.navigateToDatasetPage(datasetIds.compAll, toolPageLinks.genotypeBrowser);
     genotypeBlockPage.findButtonInComponentContainingText('gpf-effect-types', 'LGDs').click();
-    let i = 3;
+    let i = 2;
     genotypeBlockPage.effectTypesGroups.get('LGDs').forEach(element => {
       cy.get('input').eq(i).should('be.checked');
       cy.get('input').eq(i).siblings().first().should('have.text', element);
       i++;
     });
 
-    [7, 8, 9, 10, 11, 12, 13, 14, 15, 16].forEach(index => {
+    [6, 7, 8, 9, 10, 11, 12, 13, 14, 15].forEach(index => {
       cy.get('input').eq(index).should('not.be.checked');
     });
   });
@@ -70,14 +70,14 @@ describe('Effect types tests', () => {
   it('should test checkboxes using "Nonsynonymous" button', () => {
     page.navigateToDatasetPage(datasetIds.compAll, toolPageLinks.genotypeBrowser);
     genotypeBlockPage.findButtonInComponentContainingText('gpf-effect-types', 'Nonsynonymous').click();
-    let i = 3;
+    let i = 2;
     genotypeBlockPage.effectTypesGroups.get('Nonsynonymous').forEach(element => {
       cy.get('input').eq(i).should('be.checked');
       cy.get('input').eq(i).siblings().first().should('have.text', element);
       i++;
     });
 
-    [11, 12, 13, 14, 15, 16].forEach(index => {
+    [10, 11, 12, 13, 14, 15].forEach(index => {
       cy.get('input').eq(index).should('not.be.checked');
     });
   });
@@ -85,14 +85,14 @@ describe('Effect types tests', () => {
   it('should test checkboxes using "Coding" button', () => {
     page.navigateToDatasetPage(datasetIds.compAll, toolPageLinks.genotypeBrowser);
     genotypeBlockPage.findButtonInComponentContainingText('gpf-effect-types', 'Coding').click();
-    let i = 3;
+    let i = 2;
     genotypeBlockPage.effectTypesGroups.get('Coding').forEach(element => {
       cy.get('input').eq(i).should('be.checked');
       cy.get('input').eq(i).siblings().first().should('have.text', element);
       i++;
     });
 
-    [12, 13, 14, 15, 16].forEach(index => {
+    [11, 12, 13, 14, 15].forEach(index => {
       cy.get('input').eq(index).should('not.be.checked');
     });
   });
