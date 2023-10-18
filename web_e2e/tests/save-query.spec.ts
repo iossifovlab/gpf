2849df35d10bd8920fce2aa0e5d18e1dfa027c08
livovachkov <livovachkov@gmail.com>
2023-10-18 11:38:27 +0300

    Added more save query playwright tests

--- web_e2e/tests/save-query.spec.ts
+++ web_e2e/tests/save-query.spec.ts
@@ -6,7 +6,7 @@ test.describe('Save query common tests', () => {
     await page.goto(utils.instanceUrl, {waitUntil: 'load'});
     await utils.navigateToHome(page);
     await utils.loginAdmin(page);
-    await utils.navigateToDatasetPage(page, utils.datasetIds.compAll, utils.toolPageLinks.genotypeBrowser);
+    await utils.navigateToDatasetPage(page, utils.datasetIds.compAll, 'Genotype browser');
     await expect(page.locator('#save-query-dropdown')).toHaveAttribute('class', 'dropdown-menu');
     await page.locator('#save-query-dropdown-button').click();
   });
@@ -40,7 +40,7 @@ test.describe('Save query tests', () => {
   });
 
   test('should open save query dropdown menu after pressing "Share/save query" button', async({ page }) => {
-    await utils.navigateToDatasetPage(page, utils.datasetIds.compAll, utils.toolPageLinks.genotypeBrowser);
+    await utils.navigateToDatasetPage(page, utils.datasetIds.compAll, 'Genotype browser');
     await expect(page.locator('#save-query-dropdown')).toHaveAttribute('class', /dropdown-menu/);
 
     await page.locator('#save-query-dropdown-button').click();
@@ -49,7 +49,7 @@ test.describe('Save query tests', () => {
 
   test('should open save query dropdown menu, click on the copy link button ' +
      'and check whether the "Copied!" tooltip appears', async({ page }) => {
-    await utils.navigateToDatasetPage(page, utils.datasetIds.compAll, utils.toolPageLinks.genotypeBrowser);
+    await utils.navigateToDatasetPage(page, utils.datasetIds.compAll, 'Genotype browser');
     await page.locator('#save-query-dropdown-button').click();
     await expect(page.locator('#copy-link-button')).toBeVisible();
 
@@ -57,4 +57,67 @@ test.describe('Save query tests', () => {
     await page.locator('#copy-link-button').click();
     await expect(page.locator('ngb-tooltip-window')).toBeVisible();
   });
+
+  test('should save a query, load it, open all tools tabs and delete the query', async ({ page }) => {
+    await utils.navigateToDatasetPage(page, utils.datasetIds.compAll, 'Genotype browser');
+    await page.locator('#save-query-dropdown-button').click();
+    await page.locator('gpf-save-query #name').fill('TestLoadAndDeleteQuery');
+    await page.locator('gpf-save-query #save-button').click();
+    await page.waitForResponse(utils.instanceUrl + '/api/v3/user_queries/save');
+    await page.goto(utils.instanceUrl + '/user-profile', { waitUntil: 'load'});
+    const cell = page.locator('div[id="query-TestLoadAndDeleteQuery-actions-cell"]').first();
+    await expect(cell).toBeVisible();
+    await cell.locator('#load-button').click();
+
+    await page.locator('li.nav-item').filter({ hasText: 'Dataset Statistics'}).click();
+    await expect(page.locator('gpf-variant-reports')).toBeVisible();
+    await page.locator('li.nav-item').filter({ hasText: 'Genotype browser'}).click();
+    await expect(page.locator('gpf-genotype-browser')).toBeVisible();
+    await page.locator('li.nav-item').filter({ hasText: 'Phenotype browser'}).click();
+    await expect(page.locator('gpf-pheno-browser-table')).toBeVisible();
+    await page.locator('li.nav-item').filter({ hasText: 'Phenotype tool'}).click();
+    await expect(page.locator('gpf-pheno-tool')).toBeVisible();
+    await page.locator('li.nav-item').filter({ hasText: 'Gene browser'}).click();
+    await expect(page.locator('gpf-gene-browser')).toBeVisible();
+
+    await page.goto(utils.instanceUrl + '/user-profile', { waitUntil: 'load'});
+
+    await cell.locator('#delete-button').click();
+  });
+
+  test('should navigate to genotype browser, check all effect types checkboxes, save a query, ' +
+  'load it and validate that all effect types checkboxes are checked', async({ page }) => {
+    await utils.navigateToDatasetPage(page, utils.datasetIds.compAll, 'Genotype browser');
+    await page.locator('gpf-effect-types').getByText('All').click();
+    await page.locator('#save-query-dropdown-button').click();
+    await expect(page.getByText('ShareSaveNameDescriptionSave')).toBeVisible();
+    await page.locator('gpf-save-query #name').fill('CheckedSavedQuery');
+    await expect(page.locator('gpf-save-query #save-button')).toBeVisible();
+    await page.locator('gpf-save-query #save-button').click({force: true});
+    await page.waitForResponse(utils.instanceUrl + '/api/v3/user_queries/save');
+
+    await page.goto(utils.instanceUrl + '/user-profile', { waitUntil: 'load'});
+    await page.waitForSelector('div#query-CheckedSavedQuery-actions-cell');
+    const cell = page.locator('div#query-CheckedSavedQuery-actions-cell').first();
+    await expect(cell).toBeVisible();
+    await cell.locator('#load-button').click();
+
+    const radios = page.locator('gpf-effect-types input');
+    await expect(page.getByText('nonsense')).toBeChecked();
+    const radiosPromises = [];
+    const count = await radios.count();
+    for (let i = 0; i < count; i++) {
+      radiosPromises.push(radios.nth(i).isChecked());
+    }
+
+    await Promise.all(radiosPromises).then(results => {
+      for (let i = 0; i < results.length; i++) {
+        expect(results[i]).toBe(true);
+      }
+    });
+
+    await page.goto(utils.instanceUrl + '/user-profile', { waitUntil: 'load'});
+    await cell.locator('#delete-button').click();
+  });
 });
+
