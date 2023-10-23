9ecc38883e35f61eef70ec3ea89dcdb460a68e44
livovachkov <livovachkov@gmail.com>
2023-10-23 11:48:06 +0300

    Pheno tool download tests

--- web_e2e/tests/pheno-tool.spec.ts
+++ web_e2e/tests/pheno-tool.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from '@playwright/test';
 import * as utils from './utils';
+import * as fs from 'fs';
+import * as path from 'path';
 
 test.describe('Pheno tool tests', () => {
   test.beforeEach(async({ page }) => {
@@ -123,5 +125,282 @@ test.describe('Pheno tool tests', () => {
     await expect(page.locator('#save-query-dropdown-button')).toBeEnabled();
     await expect(page.getByText('Report')).toBeEnabled();
     await expect(page.getByRole('button', {name: 'Download'})).toBeEnabled();
+  });
+});
+
+test.describe('Pheno tool download tests', () => {
+  test.beforeEach(async({ page }) => {
+    await page.goto(utils.instanceUrl, {waitUntil: 'load'});
+    await utils.loginAdmin(page);
+    await utils.navigateToDatasetPage(page, utils.datasetIds.compAll, 'Phenotype tool');
+  });
+  test('should download i1.m1 and check if it equals the reference data', async({ page }) => {
+    await page.locator('input#search-box').click();
+    await page.getByText('i1.m1').first().click();
+
+    const expectedVariantsPath = path.join(__dirname + '/../fixtures/pheno-tool/pheno_report1.csv');
+
+    const [download] = await Promise.all([
+      page.waitForEvent('download'),
+      page.getByText('Download').click(),
+    ]);
+
+    const downloadedFile = await download.path().then(filePath => fs.readFileSync(filePath, 'utf8'));
+    const expectedFile = fs.readFileSync(expectedVariantsPath, 'utf8');
+    const downloadedFileLines = downloadedFile.split(/\r\n|\r|\n/);
+    const expectedFileLines = expectedFile.split(/\r\n|\r|\n/);
+    expect(downloadedFileLines).toEqual(expectedFileLines);
+  });
+
+  [
+    {id: '2', measure: 'i1.m2', normalizedBy: 'Age'},
+    {id: '3', measure: 'i1.age', normalizedBy: 'Non verbal IQ'}
+  ].forEach(data => {
+    test(`should normalize by "
+      ${data.normalizedBy}
+    ", download report and compare it to the reference data`, async({ page }) => {
+      await page.locator('input#search-box').click();
+      await page.getByText(data.measure).first().click();
+      await page.getByLabel(data.normalizedBy).click();
+
+      const expectedVariantsPath = path.join(__dirname + `/../fixtures/pheno-tool/pheno_report${data.id}.csv`);
+
+      const [download] = await Promise.all([
+        page.waitForEvent('download'),
+        page.getByText('Download').click(),
+      ]);
+
+      const downloadedFile = await download.path().then(filePath => fs.readFileSync(filePath, 'utf8'));
+      const expectedFile = fs.readFileSync(expectedVariantsPath, 'utf8');
+      const downloadedFileLines = downloadedFile.split(/\r\n|\r|\n/);
+      const expectedFileLines = expectedFile.split(/\r\n|\r|\n/);
+      expect(downloadedFileLines).toEqual(expectedFileLines);
+    });
+  });
+
+  [
+    {id: '4', filters: ['father only', 'mother and father', 'Nonsense', 'Nonsynonymous', 'Synonymous'] },
+    {id: '5', filters: ['mother only', 'mother and father', 'neither', 'LGDs', 'Splice-site', 'Frame-shift'] }
+  ].forEach(data => {
+    test(`should apply ${
+      data.filters.toString()
+    } filters, download report and compare it to the reference data`, async({ page }) => {
+      await page.locator('input#search-box').click();
+      await page.getByText('i1.m1').first().click();
+
+
+      await page.locator('gpf-checkbox-list').getByRole('button', { name: 'None' }).click();
+      await page.locator('gpf-pheno-tool-effect-types').getByRole('button', { name: 'None' }).click();
+
+      for (const filter of data.filters) {
+        await page.getByText(filter, {exact: true}).click();
+      }
+
+      const expectedVariantsPath = path.join(__dirname + `/../fixtures/pheno-tool/pheno_report${data.id}.csv`);
+
+      const [download] = await Promise.all([
+        page.waitForEvent('download'),
+        page.getByText('Download').click(),
+      ]);
+
+
+      const downloadedFile = await download.path().then(filePath => fs.readFileSync(filePath, 'utf8'));
+      const expectedFile = fs.readFileSync(expectedVariantsPath, 'utf8');
+      const downloadedFileLines = downloadedFile.split(/\r\n|\r|\n/);
+      const expectedFileLines = expectedFile.split(/\r\n|\r|\n/);
+      expect(downloadedFileLines).toEqual(expectedFileLines);
+    });
+  });
+
+  [
+    {id: '6', familyId: 'f1'},
+    {id: '7', familyId: 'f3'}
+  ].forEach(data => {
+    test(`should check downloaded report with family id ${data.familyId}`, async({ page }) => {
+      await page.locator('input#search-box').click();
+      await page.getByText('i1.age').first().click();
+
+      await page.locator('#family-ids').click();
+      await page.locator('gpf-family-ids textarea').fill(data.familyId);
+
+      const expectedVariantsPath = path.join(__dirname + `/../fixtures/pheno-tool/pheno_report${data.id}.csv`);
+
+      const [download] = await Promise.all([
+        page.waitForEvent('download'),
+        page.getByText('Download').click(),
+      ]);
+
+      const downloadedFile = await download.path().then(filePath => fs.readFileSync(filePath, 'utf8'));
+      const expectedFile = fs.readFileSync(expectedVariantsPath, 'utf8');
+      const downloadedFileLines = downloadedFile.split(/\r\n|\r|\n/);
+      const expectedFileLines = expectedFile.split(/\r\n|\r|\n/);
+      expect(downloadedFileLines).toEqual(expectedFileLines);
+    });
+  });
+
+  [
+    {id: '8', familyFilter: 'i1.iq', familyHistogramfromTo: ['33', '130']},
+    {id: '9', familyFilter: 'i1.m1', familyHistogramfromTo: ['84', '107']}
+  ].forEach(data => {
+    test('should test advanced family filters', async({ page }) => {
+      await page.locator('input#search-box').click();
+      await page.getByText('i1.age').first().click();
+
+      await page.getByRole('tab', { name: 'Advanced' }).click();
+      await page.getByLabel('Advanced').getByPlaceholder('Select or start typing to search').click();
+      await page.getByRole('button', { name: 'i1.iq' }).click();
+
+      await expect(page.locator('gpf-histogram')).toBeVisible();
+      await page.locator('#from-input-field').clear();
+      await page.locator('#from-input-field').fill(data.familyHistogramfromTo[0]);
+      await page.locator('#to-input-field').clear();
+      await page.locator('#to-input-field').fill(data.familyHistogramfromTo[1]);
+
+      const expectedVariantsPath = path.join(__dirname + `/../fixtures/pheno-tool/pheno_report${data.id}.csv`);
+
+      const [download] = await Promise.all([
+        page.waitForEvent('download'),
+        page.getByText('Download').click(),
+      ]);
+
+      const downloadedFile = await download.path().then(filePath => fs.readFileSync(filePath, 'utf8'));
+      const expectedFile = fs.readFileSync(expectedVariantsPath, 'utf8');
+      const downloadedFileLines = downloadedFile.split(/\r\n|\r|\n/);
+      const expectedFileLines = expectedFile.split(/\r\n|\r|\n/);
+      expect(downloadedFileLines).toEqual(expectedFileLines);
+    });
+  });
+
+  [
+    {id: '10', geneSymbol: 'CAMSAP1'},
+    {id: '11', geneSymbol: 'SAMD11'}
+  ].forEach(data => {
+    test(`should check downloaded report with gene symbol ${data.geneSymbol}`, async({ page }) => {
+      await page.locator('li#gene-symbols').click();
+      await page.locator('gpf-gene-symbols textarea').fill(data.geneSymbol);
+
+      await page.locator('input#search-box').click();
+      await page.getByText('i1.age').first().click();
+
+      const expectedVariantsPath = path.join(__dirname + `/../fixtures/pheno-tool/pheno_report${data.id}.csv`);
+
+      const [download] = await Promise.all([
+        page.waitForEvent('download'),
+        page.getByText('Download').click(),
+      ]);
+
+      const downloadedFile = await download.path().then(filePath => fs.readFileSync(filePath, 'utf8'));
+      const expectedFile = fs.readFileSync(expectedVariantsPath, 'utf8');
+      const downloadedFileLines = downloadedFile.split(/\r\n|\r|\n/);
+      const expectedFileLines = expectedFile.split(/\r\n|\r|\n/);
+      expect(downloadedFileLines).toEqual(expectedFileLines);
+    });
+  });
+
+  [
+    {id: '12', collection: 'SFARI Genes', set: 'SFARI ALL (910): SFARI Genes (2017-09): All genes', measure: 'i1.age'},
+    {id: '13', collection: 'Protein domains', set: 'AMOP (3)', measure: 'i1.iq'}
+  ].forEach(data => {
+    test('should check downloaded report with gene sets', async({ page }) => {
+      await page.getByRole('tab', { name: 'Gene Sets' }).click();
+
+      await page.locator('gpf-gene-sets select.form-control').selectOption(data.collection);
+      await page.getByLabel('Gene Sets').getByPlaceholder('Select or start typing to search').click();
+      await page.getByLabel('Gene Sets')
+        .getByPlaceholder('Select or start typing to search').fill(data.set.substring(0, data.set.indexOf(' (')));
+      await page.locator('button.dropdown-item span').filter({ hasText: data.set }).click();
+
+      await page.getByRole('textbox', { name: 'Select or start typing to search' }).click();
+      await page.getByText(data.measure).first().click();
+
+      const expectedVariantsPath = path.join(__dirname + `/../fixtures/pheno-tool/pheno_report${data.id}.csv`);
+
+      const [download] = await Promise.all([
+        page.waitForEvent('download'),
+        page.getByRole('button', { name: 'Download' }).click(),
+      ]);
+
+      const downloadedFile = await download.path().then(filePath => fs.readFileSync(filePath, 'utf8'));
+      const expectedFile = fs.readFileSync(expectedVariantsPath, 'utf8');
+      const downloadedFileLines = downloadedFile.split(/\r\n|\r|\n/);
+      const expectedFileLines = expectedFile.split(/\r\n|\r|\n/);
+      expect(downloadedFileLines).toEqual(expectedFileLines);
+    });
+  });
+
+  [
+    {
+      id: '14',
+      genotype: 'comp_vcf',
+      affectedStatus: 'affected',
+      set: 'Missense.Male (1): Missense.Male (comp_all:status:affected;comp_vcf:status:affected)',
+      measure: 'i1.age'
+    },
+    {
+      id: '15',
+      genotype: 'iossifov_2014',
+      affectedStatus: 'unaffected',
+      set: 'LGDs.Recurrent (3): LGDs.Recurrent (comp_all:status:affected;iossifov_2014:status:unaffected)',
+      measure: 'i1.iq'
+    }
+  ].forEach(data => {
+    test('should check downloaded report with gene set Denovo', async({ page }) => {
+      await page.getByRole('tab', { name: 'Gene Sets' }).click();
+      await page.locator('gpf-gene-sets select.form-control').selectOption('Denovo');
+
+      await page.locator('ngb-accordion').filter({ hasText: data.genotype + ': Affected Status' }).click();
+      await page.getByRole('button', { name: 'comp_vcf: Affected Status' }).click();
+      await page.locator(`#${data.genotype}-checkbox-${data.affectedStatus}`).click();
+
+      await page.getByLabel('Gene Sets').getByPlaceholder('Select or start typing to search').click();
+      await page.locator('button.dropdown-item span').filter({ hasText: data.set }).click();
+
+      await page.getByRole('textbox', { name: 'Select or start typing to search' }).click();
+      await page.locator('button.dropdown-item span').filter({ hasText: data.measure }).click();
+
+      const expectedVariantsPath = path.join(__dirname + `/../fixtures/pheno-tool/pheno_report${data.id}.csv`);
+
+      const [download] = await Promise.all([
+        page.waitForEvent('download'),
+        page.getByRole('button', { name: 'Download' }).click(),
+      ]);
+
+      const downloadedFile = await download.path().then(filePath => fs.readFileSync(filePath, 'utf8'));
+      const expectedFile = fs.readFileSync(expectedVariantsPath, 'utf8');
+      const downloadedFileLines = downloadedFile.split(/\r\n|\r|\n/);
+      const expectedFileLines = expectedFile.split(/\r\n|\r|\n/);
+      expect(downloadedFileLines).toEqual(expectedFileLines);
+    });
+  });
+
+  [
+    {id: '16', measure: 'i1.age', geneScore: 'LGD rank', geneScoresHistogramFromTo: ['2944', '14716']},
+    {id: '17', measure: 'i1.m1', geneScore: 'ExAC pRec', geneScoresHistogramFromTo: ['0.000012', '0.832']}
+  ].forEach(data => {
+    test(`should check downloaded report with gene score ${data.geneScore}`, async({ page }) => {
+      await page.getByRole('tab', { name: 'Gene Scores' }).click();
+      await page.locator('#gene-scores-panel select.form-control').selectOption(data.geneScore);
+
+      await page.locator('input#search-box').click();
+      await page.getByText(data.measure).first().click();
+
+      await page.locator('#from-input-field').clear();
+      await page.locator('#from-input-field').fill(data.geneScoresHistogramFromTo[0]);
+      await page.locator('#to-input-field').clear();
+      await page.locator('#to-input-field').fill(data.geneScoresHistogramFromTo[1]);
+
+      const expectedVariantsPath = path.join(__dirname + `/../fixtures/pheno-tool/pheno_report${data.id}.csv`);
+
+      const [download] = await Promise.all([
+        page.waitForEvent('download'),
+        page.getByRole('button', { name: 'Download' }).click(),
+      ]);
+
+      const downloadedFile = await download.path().then(filePath => fs.readFileSync(filePath, 'utf8'));
+      const expectedFile = fs.readFileSync(expectedVariantsPath, 'utf8');
+      const downloadedFileLines = downloadedFile.split(/\r\n|\r|\n/);
+      const expectedFileLines = expectedFile.split(/\r\n|\r|\n/);
+      expect(downloadedFileLines).toEqual(expectedFileLines);
+    });
   });
 });
