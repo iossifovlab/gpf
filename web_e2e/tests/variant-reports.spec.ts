0a50742db23fc49938096732060617389a982d6b
IlinaGergova <ili.gergova@gmail.com>
2023-10-23 10:52:01 +0300

    variant reports test migration

--- web_e2e/tests/variant-reports.spec.ts
+++ web_e2e/tests/variant-reports.spec.ts
@@ -11,6 +11,66 @@ test.describe('Variant reports tests', () => {
     await page.getByText('Dataset Statistics').click();
   });
 
+  test('should display the correct elements in the families by number tab', async({ page }) => {
+    await page.getByText('Families by number').click();
+    await expect(page.locator('#download-total-families')).toBeVisible();
+    await expect(page.locator('#families-by-number-select')).toBeVisible();
+    await expect(page.locator('#families-by-number-div')).toBeVisible();
+
+    await page.getByText('De Novo variants').click();
+    await expect(page.locator('#download-total-families')).not.toBeVisible();
+    await expect(page.locator('#families-by-number-select')).not.toBeVisible();
+    await expect(page.locator('#families-by-number-div')).not.toBeVisible();
+  });
+
+  test('should display the correct elements in the families by pedigree tab', async({ page }) => {
+    await page.getByText('Families by pedigree').click();
+    await expect(page.locator('#families-by-pedigree-select')).toBeVisible();
+    await expect(page.locator('#download-button')).toBeVisible();
+    await expect(page.locator('#families-by-pedigree-div')).toBeVisible();
+    await expect(page.locator('#selected-tags-list')).toBeVisible();
+
+    await page.getByText('De Novo variants').click();
+    await expect(page.locator('#families-by-pedigree-select')).not.toBeVisible();
+    await expect(page.locator('#download-button')).not.toBeVisible();
+    await expect(page.locator('#families-by-pedigree-div')).not.toBeVisible();
+    await expect(page.locator('#selected-tags-list')).not.toBeVisible();
+  });
+
+  test('should display the correct elements in the families by pedigree tab', async({ page }) => {
+    await page.getByText('De Novo variants').click();
+    await expect(page.locator('#denovo-variants-select')).toBeVisible();
+    await expect(page.locator('#de-novo-variants-legend-report')).toBeVisible();
+    await expect(page.locator('#denovo-variants-div')).toBeVisible();
+
+    await page.getByText('Families by number').click();
+    await expect(page.locator('#denovo-variants-select')).not.toBeVisible();
+    await expect(page.locator('#de-novo-variants-legend-report')).not.toBeVisible();
+    await expect(page.locator('#denovo-variants-div')).not.toBeVisible();
+  });
+
+  [
+    {option: 'Affected Status', legendElements: ['affected', 'unaffected']},
+    {option: 'Role', legendElements: ['mom', 'dad', 'proband', 'sibling']},
+    {option: 'Phenotype', legendElements: ['autism', 'unaffected']}
+  ].forEach(data => {
+    test.skip('should check content of the legend for ' + data.option + ' option in the families by pedigree tab', async({ page }) => {
+      await page.getByText('Families by pedigree').click();
+      await expect(page.locator('#expand-legend-button')).toBeVisible();
+      await expect(page.locator('#legend')).not.toBeVisible();
+      await page.locator('#families-by-pedigree-select').selectOption(data.option);
+
+      await page.locator('#expand-legend-button').click();
+      await expect(page.locator('#legend')).toBeVisible();
+
+      await expect(page.locator('.legend-item')).toHaveCount(data.legendElements.length);
+
+      data.legendElements.forEach(el => {
+        await expect(page.locator('#legend')).toContainText(el);
+      });
+    });
+  });
+
   [
     {index: 0, name: 'first', columnsToCheck: ['family_id', 'person_id', 'mom_id', 'dad_id', 'sex', 'status', 'role']},
     {index: 1, name: 'second', columnsToCheck: ['family_id', 'person_id', 'mom_id', 'dad_id', 'sex', 'status', 'role']},
