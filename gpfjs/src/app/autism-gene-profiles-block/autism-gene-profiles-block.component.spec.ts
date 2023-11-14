ee85347caa66a5e6bd0df1bd5d57dac1dbc8184b
IlinaGergova <ili.gergova@gmail.com>
2023-11-14 14:46:34 +0200

    unit test for agp get config in agp block

--- gpfjs/src/app/autism-gene-profiles-block/autism-gene-profiles-block.component.spec.ts
+++ gpfjs/src/app/autism-gene-profiles-block/autism-gene-profiles-block.component.spec.ts
@@ -12,16 +12,116 @@ import { NgxsModule } from '@ngxs/store';
 import { MultipleSelectMenuComponent } from 'app/multiple-select-menu/multiple-select-menu.component';
 import { FormsModule } from '@angular/forms';
 import { APP_BASE_HREF } from '@angular/common';
+import { AutismGeneProfilesService } from './autism-gene-profiles.service';
+import { Observable, of } from 'rxjs';
+import { AgpSingleViewConfig } from 'app/autism-gene-profiles-single-view/autism-gene-profile-single-view';
+import { TruncatePipe } from '../utils/truncate.pipe';
+import { AgpColumn, AgpTableConfig } from 'app/autism-gene-profiles-table/autism-gene-profiles-table';
+
+const config = {
+  shown: [],
+  defaultDataset: 'mockDataset',
+  geneSets: [
+    {
+      category: 'autism_gene_sets',
+      displayName: 'Autism Gene Sets',
+      defaultVisible: true,
+      sets: [
+        { setId: 'SFARI ALL', defaultVisible: true, collectionId: 'sfari', meta: null }
+      ]
+    }
+  ],
+  genomicScores: [
+    {
+      category: 'autism_scores',
+      defaultVisible: true,
+      displayName: 'Autism Scores',
+      scores: [
+        { format: '%s', defaultVisible: true, scoreName: 'SFARI gene score', meta: null }
+      ]
+    }
+  ],
+  datasets: [
+    {
+      defaultVisible: true,
+      displayName: 'Sequencing de Novo',
+      id: 'sequencing_de_novo',
+      meta: null,
+      shown: [],
+      personSets: [
+        {
+          id: 'autism',
+          shown: [],
+          defaultVisible: true,
+          displayName: 'autism',
+          collectionId: 'phenotype',
+          description: '',
+          parentsCount: 9836,
+          childrenCount: 21775,
+          statistics: [
+            {
+              id: 'denovo_lgds',
+              description: 'de Novo LGDs',
+              displayName: 'dn LGDs',
+              effects: [
+                'LGDs'
+              ],
+              category: 'denovo',
+              defaultVisible: true,
+              scores: [],
+              variantTypes: []
+            }
+          ]
+        }
+      ],
+      statistics: []
+    }
+  ],
+  order: [
+    { section: null, id: 'autism_gene_sets_rank' },
+    { section: 'genomicScores', id: 'autism_scores' },
+    { section: 'datasets', id: 'sequencing_de_novo' }
+  ],
+  pageSize: 5
+};
+
+class AutismGeneProfilesServiceMock {
+  public getConfig(): Observable<AgpSingleViewConfig> {
+    return of(config);
+  }
+}
+
+/* eslint-disable max-len */
+const geneColumn = new AgpColumn('createTab', [], 'Gene', false, 'geneSymbol', null, false, true);
+const geneSetSetsCol = new AgpColumn(null, [], 'SFARI ALL', true, 'autism_gene_sets_rank.SFARI ALL', null, true, true);
+const geneSetCol = new AgpColumn(null, [geneSetSetsCol], 'Autism Gene Sets', false, 'autism_gene_sets_rank', null, true, true);
+const geneScoreScoresCol = new AgpColumn(null, [], 'SFARI gene score', true, 'autism_scores.SFARI gene score', null, true, true);
+const geneScoreCol = new AgpColumn(null, [geneScoreScoresCol], 'Autism Scores', false, 'autism_scores', null, false, true);
+const datasetPersonSetsStatisticsCol = new AgpColumn('goToQuery', [], 'dn LGDs', false, 'sequencing_de_novo.autism.denovo_lgds', null, true, true);
+const datasetPersonSetsCol = new AgpColumn(null, [datasetPersonSetsStatisticsCol], 'autism (21775)', false, 'sequencing_de_novo.autism', null, false, true);
+const datasetCol = new AgpColumn(null, [datasetPersonSetsCol], 'Sequencing de Novo', false, 'sequencing_de_novo', null, false, true);
+/* eslint-enable max-len */
+
+const agpTableConfigMock= new AgpTableConfig();
+agpTableConfigMock.columns = [geneColumn, geneSetCol, geneScoreCol, datasetCol];
+agpTableConfigMock.defaultDataset = 'mockDataset';
+agpTableConfigMock.pageSize = 5;
 
 describe('AutismGeneProfilesBlockComponent', () => {
   let component: AutismGeneProfilesBlockComponent;
   let fixture: ComponentFixture<AutismGeneProfilesBlockComponent>;
+  const autismGeneProfilesServiceMock = new AutismGeneProfilesServiceMock();
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [AutismGeneProfilesBlockComponent, AgpTableComponent, MultipleSelectMenuComponent],
+      declarations: [AutismGeneProfilesBlockComponent, AgpTableComponent, MultipleSelectMenuComponent, TruncatePipe],
       providers: [
-        ConfigService, QueryService, DatasetsService, UsersService, { provide: APP_BASE_HREF, useValue: '' }
+        ConfigService,
+        QueryService,
+        DatasetsService,
+        UsersService,
+        { provide: APP_BASE_HREF, useValue: '' },
+        { provide: AutismGeneProfilesService, useValue: autismGeneProfilesServiceMock }
       ],
       imports: [
         HttpClientTestingModule, NgbNavModule, RouterTestingModule, FormsModule,
@@ -36,5 +136,12 @@ describe('AutismGeneProfilesBlockComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should create agp table configuration', () => {
+    component.ngOnInit();
+    expect(component.agpTableConfig).toStrictEqual(agpTableConfigMock);
+    expect(component.agpTableSortBy).toBe('autism_gene_sets_rank');
+    expect(component.agpSingleViewConfig).toStrictEqual(config);
   });
 });
