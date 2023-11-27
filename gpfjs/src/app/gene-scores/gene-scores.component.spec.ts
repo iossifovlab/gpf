cb1e47fe3209ceafbdd6a58bcf84ce75536f5ad6
IlinaGergova <ili.gergova@gmail.com>
2023-11-27 15:48:47 +0200

    add test for modal in gene scores

--- gpfjs/src/app/gene-scores/gene-scores.component.spec.ts
+++ gpfjs/src/app/gene-scores/gene-scores.component.spec.ts
@@ -2,7 +2,7 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
-import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbModal, NgbModalRef, NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgxsModule } from '@ngxs/store';
 import { ConfigService } from 'app/config/config.service';
 import { Observable } from 'rxjs';
@@ -11,13 +11,19 @@ import { GeneScores } from './gene-scores';
 import { GeneScoresComponent } from './gene-scores.component';
 import { GeneScoresService } from './gene-scores.service';
 import { GeneScoresState } from './gene-scores.state';
+import { PopupComponent } from 'app/popup/popup.component';
 
 class MockGeneScoresService {
   public provide = true;
   public getGeneScores(): Observable<GeneScores[]> {
     if (this.provide) {
-      return of([new GeneScores([1, 2], 'score3', [4, 5], 'desc6', [7, 8], 'xScale9', 'yScale10'),
-        new GeneScores([11, 12], 'score13', [14, 15], 'desc16', [17, 18], 'xScale19', 'yScale20')
+      return of([
+        new GeneScores(
+          [1, 2], [4, 5], 'desc1', 'help1', 'larger1', 'smaller1', 'score31', [7, 8], 'xScale1', 'yScale1'
+        ),
+        new GeneScores(
+          [11, 12], [14, 15], 'desc2', 'help2', 'larger2', 'smaller2', 'score2', [17, 18], 'xScale2', 'yScale2'
+        )
       ]);
     } else {
       return of([] as GeneScores[]);
@@ -27,9 +33,13 @@ class MockGeneScoresService {
 describe('GeneScoresComponent', () => {
   let component: GeneScoresComponent;
   let fixture: ComponentFixture<GeneScoresComponent>;
-  const mockGeneScoresService: MockGeneScoresService = new MockGeneScoresService();
+  let mockGeneScoresService: MockGeneScoresService;
+  let modalService: NgbModal;
+  let modalRef: NgbModalRef;
 
   beforeEach(() => {
+    mockGeneScoresService = new MockGeneScoresService();
+
     TestBed.configureTestingModule({
       imports: [
         NgxsModule.forRoot([GeneScoresState], {developmentMode: true}), HttpClientTestingModule, NgbNavModule
@@ -38,8 +48,13 @@ describe('GeneScoresComponent', () => {
       providers: [{provide: GeneScoresService, useValue: mockGeneScoresService}, ConfigService],
       schemas: [NO_ERRORS_SCHEMA]
     }).compileComponents();
+
+    modalService = TestBed.inject(NgbModal);
+    modalRef = modalService.open(PopupComponent);
+
     fixture = TestBed.createComponent(GeneScoresComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
@@ -55,6 +70,22 @@ describe('GeneScoresComponent', () => {
     fixture.detectChanges();
     expect(fixture.debugElement.query(By.css('div > div.form-block > div.card > ul > li > span'))).toBeTruthy();
     expect(fixture.debugElement.query(By.css('div > div#gene-scores-panel'))).not.toBeTruthy();
+  });
+
+  it('should show help', () => {
+    jest.spyOn(modalService, 'open').mockReturnValue(modalRef);
+    component.showHelp();
+    expect(modalService.open).toHaveBeenCalledWith(PopupComponent, {
+      size: 'lg',
+      centered: true
+    });
+    expect(modalService.open).toHaveBeenCalledWith(PopupComponent, {
+      size: 'lg',
+      centered: true
+    });
+    expect((modalRef.componentInstance as PopupComponent).data).toBe('help1');
+
+    modalRef.close();
   });
 });
 
