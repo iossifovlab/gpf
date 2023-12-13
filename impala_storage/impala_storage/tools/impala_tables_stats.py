0e06cd9825db5d317bd435a913c2fcf70d089c9a
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2023-12-13 12:04:27 +0200

    Mop-up lint and mypy errors.

--- impala_storage/impala_storage/tools/impala_tables_stats.py
+++ impala_storage/impala_storage/tools/impala_tables_stats.py
@@ -3,11 +3,13 @@ import sys
 import argparse
 import logging
 import time
+from typing import Optional
 
 from contextlib import closing
 
 from dae.utils.verbosity_configuration import VerbosityConfiguration
 from dae.gpf_instance.gpf_instance import GPFInstance
+from dae.studies.study import GenotypeDataStudy
 
 from impala_storage.schema1.impala_variants import ImpalaVariants
 
@@ -15,7 +17,7 @@ from impala_storage.schema1.impala_variants import ImpalaVariants
 logger = logging.getLogger("impala_tables_stats")
 
 
-def parse_cli_arguments(argv):
+def parse_cli_arguments(argv: list[str]) -> argparse.Namespace:
     """Construct parser and parse CLI arguments."""
     parser = argparse.ArgumentParser(
         description="loading study parquet files in impala db",
@@ -31,32 +33,34 @@ def parse_cli_arguments(argv):
         help="comma separated list of study IDs",
     )
 
-    argv = parser.parse_args(argv)
-    return argv
+    return parser.parse_args(argv)
 
 
-def variants_region_bins(study_backend):
+def variants_region_bins(study_backend: ImpalaVariants) -> list[str]:
     """Collect region bins for a study."""
     impala = study_backend._impala_helpers  # pylint: disable=protected-access
 
     region_bins = []
     with closing(impala.connection()) as connection:
-        with connection.cursor() as cursor:
+        with closing(connection.cursor()) as cursor:
             query = f"SELECT DISTINCT(region_bin) FROM " \
                 f"{study_backend.db}.{study_backend.variants_table}"
             logger.info("running %s", query)
             cursor.execute(query)
-            for row in cursor:
+            for row in cursor.fetchall():
                 region_bins.append(row[0])
     logger.info("collected region bins: %s", region_bins)
     return region_bins
 
 
-def variants_compute_stats(study_backend, region_bin=None):
+def variants_compute_stats(
+    study_backend: ImpalaVariants,
+    region_bin: Optional[str] = None
+) -> None:
     """Compute family variants tables statisticsfor specified region."""
     impala = study_backend._impala_helpers  # pylint: disable=protected-access
     with closing(impala.connection()) as connection:
-        with connection.cursor() as cursor:
+        with closing(connection.cursor()) as cursor:
             if region_bin is not None:
                 query = f"COMPUTE INCREMENTAL STATS " \
                     f"{study_backend.db}.{study_backend.variants_table} " \
@@ -68,11 +72,14 @@ def variants_compute_stats(study_backend, region_bin=None):
             cursor.execute(query)
 
 
-def summary_variants_compute_stats(study_backend, region_bin=None):
+def summary_variants_compute_stats(
+    study_backend: ImpalaVariants,
+    region_bin: Optional[str] = None
+) -> None:
     """Compute summary variants table statistics."""
     impala = study_backend._impala_helpers  # pylint: disable=protected-access
     with closing(impala.connection()) as connection:
-        with connection.cursor() as cursor:
+        with closing(connection.cursor()) as cursor:
             if region_bin is not None:
                 query = f"COMPUTE INCREMENTAL STATS " \
                     f"{study_backend.db}." \
@@ -86,55 +93,60 @@ def summary_variants_compute_stats(study_backend, region_bin=None):
             cursor.execute(query)
 
 
-def pedigree_compute_stats(study_backend):
+def pedigree_compute_stats(study_backend: ImpalaVariants) -> None:
     """Compute pedigree table statistics."""
     impala = study_backend._impala_helpers  # pylint: disable=protected-access
     with closing(impala.connection()) as connection:
-        with connection.cursor() as cursor:
+        with closing(connection.cursor()) as cursor:
             query = f"COMPUTE STATS " \
                 f"{study_backend.db}.{study_backend.pedigree_table}"
             logger.info("compute stats for pedigree table: %s", query)
             cursor.execute(query)
 
 
-def main(argv=None, gpf_instance=None):
+def main(
+    argv: Optional[list[str]] = None,
+    gpf_instance: Optional[GPFInstance] = None
+) -> None:
     """Run CLI for impala_table_stats.py tool."""
     if argv is None:
         argv = sys.argv[1:]
     if gpf_instance is None:
         gpf_instance = GPFInstance.build()
 
-    argv = parse_cli_arguments(argv)
-    VerbosityConfiguration.set(argv)
+    args = parse_cli_arguments(argv)
+    VerbosityConfiguration.set(args)
 
-    if argv.studies is None:
+    if args.studies is None:
         study_ids = [
             gd.study_id
             for gd in gpf_instance.get_all_genotype_data() if not gd.is_group]
     else:
-        study_ids = [sid.strip() for sid in argv.studies.split(",")]
+        study_ids = [sid.strip() for sid in args.studies.split(",")]
 
     logger.info("computing table stats for studies: %s", study_ids)
 
     for study_id in study_ids:
         study = gpf_instance.get_genotype_data(study_id)
         assert study.study_id == study_id
+        assert isinstance(study, GenotypeDataStudy)
 
         study_backend = study._backend  # pylint: disable=protected-access
         if not isinstance(study_backend, ImpalaVariants):
             logger.info("not an impala study: %s; skipping...", study_id)
             continue
+        assert study_backend.schema is not None
 
         pedigree_compute_stats(study_backend)
         if study_backend.variants_table is None:
             continue
 
-        if "region_bin" not in study_backend.schema:
+        if "region_bin" not in {a.name for a in study_backend.schema}:
             variants_compute_stats(study_backend, region_bin=None)
             if study_backend.has_summary_variants_table:
                 summary_variants_compute_stats(study_backend, region_bin=None)
         else:
-            assert "region_bin" in study_backend.schema
+            assert "region_bin" in {a.name for a in study_backend.schema}
             region_bins = variants_region_bins(study_backend)
             logger.info(
                 "processing  %s region bins; %s",
--- impala_storage/impala_storage/tools/impala_tables_summary_variants.py
+++ impala_storage/impala_storage/tools/impala_tables_summary_variants.py
@@ -5,12 +5,14 @@ import logging
 import time
 import itertools
 import math
-from typing import Any
+from typing import Any, Optional
 from contextlib import closing
 
 from dae.utils.regions import Region
+from dae.utils.verbosity_configuration import VerbosityConfiguration
+from dae.genomic_resources.reference_genome import ReferenceGenome
 from dae.gpf_instance.gpf_instance import GPFInstance
-
+from dae.studies.study import GenotypeDataStudy
 from impala_storage.schema1.impala_variants import ImpalaVariants
 
 
@@ -27,8 +29,7 @@ def parse_cli_arguments(
         formatter_class=argparse.RawDescriptionHelpFormatter,
     )
 
-    parser.add_argument("--verbose", "-V", action="count", default=0)
-
+    VerbosityConfiguration.set_argumnets(parser)
     parser.add_argument(
         "--studies",
         type=str,
@@ -55,12 +56,12 @@ def variants_parition_bins(
 
     partition_bins = []
     with closing(impala.connection()) as connection:
-        with connection.cursor() as cursor:
+        with closing(connection.cursor()) as cursor:
             query = f"SELECT DISTINCT({partition}) FROM " \
                 f"{study_backend.db}.{study_backend.variants_table}"
             logger.info("collecting patitions: %s", query)
             cursor.execute(query)
-            for row in cursor:
+            for row in cursor.fetchall():
                 partition_bins.append(row[0])
     logger.info("partitions found: %s", partition_bins)
     return partition_bins
@@ -68,8 +69,8 @@ def variants_parition_bins(
 
 def collect_summary_schema(impala_variants: ImpalaVariants) -> dict[str, str]:
     """Collect summary schema."""
-    # pylint: disable=invalid-name
-    FAMILY_FIELDS = set([
+    assert impala_variants.schema is not None
+    family_fields = set([
         "family_index",
         "family_id",
         "variant_in_sexes",
@@ -79,7 +80,7 @@ def collect_summary_schema(impala_variants: ImpalaVariants) -> dict[str, str]:
         "family_bin",
         "extra_attributes",
     ])
-    TYPE_MAP: dict[Any, str] = {
+    type_map: dict[Any, str] = {
         "int": "int",
         "float": "float",
         "str": "string",
@@ -88,10 +89,10 @@ def collect_summary_schema(impala_variants: ImpalaVariants) -> dict[str, str]:
     schema = {}
     for attr_info in impala_variants.schema:
         field_name = attr_info.name
-        if field_name in FAMILY_FIELDS:
+        if field_name in family_fields:
             continue
         field_type = attr_info.type
-        schema[field_name] = TYPE_MAP[field_type]
+        schema[field_name] = type_map[field_type]
 
     schema["seen_in_status"] = "tinyint"
     schema["seen_as_denovo"] = "boolean"
@@ -130,7 +131,7 @@ def drop_summary_table(
     # pylint: disable=protected-access
     impala = impala_variants._impala_helpers
     with closing(impala.connection()) as connection:
-        with connection.cursor() as cursor:
+        with closing(connection.cursor()) as cursor:
             query = f"DROP TABLE IF EXISTS " \
                 f"{summary_table_name(study_id, impala_variants)}"
             logger.info("drop summary table: %s", query)
@@ -152,7 +153,7 @@ def rename_summary_table(
     qry = f"ALTER TABLE {summary_table_name_temp(study_id, impala_variants)} " \
         f"RENAME TO {summary_table_name(study_id, impala_variants)}"
     with closing(impala.connection()) as connection:
-        with connection.cursor() as cursor:
+        with closing(connection.cursor()) as cursor:
             logger.info("drop summary table: %s", qry)
             cursor.execute(qry)
 
@@ -183,7 +184,7 @@ def create_summary_table(
     # pylint: disable=protected-access
     impala = impala_variants._impala_helpers
     with closing(impala.connection()) as connection:
-        with connection.cursor() as cursor:
+        with closing(connection.cursor()) as cursor:
 
             qry = f"CREATE TABLE IF NOT EXISTS " \
                 f"{summary_table_name_temp(study_id, impala_variants)} " \
@@ -201,17 +202,17 @@ class RegionBinsHelper:
     """Encapsulates data related to region bins."""
     # pylint: disable=too-few-public-methods
 
-    def __init__(self, table_properties, genome):
+    def __init__(self, table_properties: dict, genome: ReferenceGenome):
         self.table_properties = table_properties
         self.chromosomes = self.table_properties["chromosomes"]
         self.region_length = self.table_properties["region_length"]
         self.chromosome_lengths = dict(genome.get_all_chrom_lengths())
-        self.region_bins = {}
+        self.region_bins: dict[str, Region] = {}
 
     # pylint: disable=inconsistent-return-statements
-    def _build_region_bins(self):
+    def _build_region_bins(self) -> None:
         if not self.chromosomes or self.region_length == 0:
-            return []
+            return
 
         for chrom in self.chromosome_lengths:
             target_chrom = chrom
@@ -234,14 +235,22 @@ class RegionBinsHelper:
                     prev_region = self.region_bins[region_bin]
                     assert prev_region.chrom == region_bin
                     assert prev_region.begin == region.begin
+                    assert region.end is not None
+                    assert prev_region.end is not None
                     region = Region(
                         region_bin, 1, max(region.end, prev_region.end))
                     self.region_bins[region_bin] = region
 
 
 def insert_into_summary_table(
-        pedigree_table, variants_table, summary_table,
-        summary_schema, parition, region_bins, region_split=0):
+    pedigree_table: str,
+    variants_table: str,
+    summary_table: str,
+    summary_schema: dict,
+    partition: dict,
+    region_bins: dict[str, Region],
+    region_split: int = 0
+) -> list[str]:
     """Insert into a summary table."""
     # pylint: disable=too-many-locals
 
@@ -264,7 +273,7 @@ def insert_into_summary_table(
     other_fields = []
 
     for field_name, _field_type in summary_schema.items():
-        if field_name in parition:
+        if field_name in partition:
             # handle partition
             pass
         elif field_name in family_summary_fields:
@@ -291,7 +300,7 @@ def insert_into_summary_table(
 
     insert_partition_parts = []
     select_partition_parts = []
-    for bin_name, bin_value in parition.items():
+    for bin_name, bin_value in partition.items():
         if bin_name == "region_bin":
             insert_partition_parts.append(
                 f"`{bin_name}` = '{bin_value}'")
@@ -304,7 +313,7 @@ def insert_into_summary_table(
                 f"variants.`{bin_name}` = {bin_value}")
     insert_partition_statement = ", ".join(insert_partition_parts)
 
-    region_bin = parition["region_bin"]
+    region_bin = partition["region_bin"]
     region = region_bins[region_bin]
     select_partition_statement = " AND ".join(select_partition_parts)
 
@@ -312,6 +321,8 @@ def insert_into_summary_table(
     if region_split == 0:
         region_statements.append("")
     else:
+        assert region.begin is not None
+        assert region.end is not None
         for region_begin in range(region.begin, region.end, region_split):
             region_statement = f"variants.`position` >= {region_begin} AND " \
                 f"variants.`position` <= {region_begin + region_split} AND"
@@ -343,39 +354,35 @@ def insert_into_summary_table(
     return queries
 
 
-def main(argv=None, gpf_instance=None):
+def main(
+    argv: Optional[list[str]] = None,
+    gpf_instance: Optional[GPFInstance] = None
+) -> None:
     """Entry point for the script."""
     # flake8: noqa: C901
     # pylint: disable=too-many-branches, too-many-statements, too-many-locals
-    argv = sys.argv[1:] if argv is None else argv
     if gpf_instance is None:
         gpf_instance = GPFInstance.build()
+    if argv is None:
+        argv = sys.argv[1:]
 
-    argv = parse_cli_arguments(argv, gpf_instance)
+    args = parse_cli_arguments(argv, gpf_instance)
 
-    if argv.verbose == 1:
-        logging.basicConfig(level=logging.WARNING)
-    elif argv.verbose == 2:
-        logging.basicConfig(level=logging.INFO)
-    elif argv.verbose >= 3:
-        logging.basicConfig(level=logging.DEBUG)
-    else:
-        logging.basicConfig(level=logging.ERROR)
+    VerbosityConfiguration.set(args)
 
-    logging.getLogger("impala").setLevel(logging.WARNING)
-
-    if argv.studies is None:
+    if args.studies is None:
         study_ids = [
             gd.study_id
             for gd in gpf_instance.get_all_genotype_data() if not gd.is_group]
     else:
-        study_ids = [sid.strip() for sid in argv.studies.split(",")]
+        study_ids = [sid.strip() for sid in args.studies.split(",")]
 
     logger.info("building summary variants tables for studies: %s", study_ids)
 
     for study_id in study_ids:
         study = gpf_instance.get_genotype_data(study_id)
         assert study.study_id == study_id
+        assert isinstance(study, GenotypeDataStudy)
 
         # pylint: disable=protected-access
         study_backend = study._backend
@@ -402,9 +409,9 @@ def main(argv=None, gpf_instance=None):
             "collecting partitions %s from "
             "variants table %s", partitions, variants_table)
 
-        for partition in partitions:
-            partition_bins[partition] = variants_parition_bins(
-                study_backend, partition)
+        for partition_id in partitions:
+            partition_bins[partition_id] = variants_parition_bins(
+                study_backend, partition_id)
 
         logger.info("variant table partitions: %s", partition_bins)
 
@@ -425,8 +432,9 @@ def main(argv=None, gpf_instance=None):
             set(region_bin_helpers.region_bins.keys()))
 
         all_partitions = list(itertools.product(*partition_bins.values()))
-        for index, partition in enumerate(all_partitions):
-            partition = dict(zip(partition_bins.keys(), partition))
+
+        for index, partition_values in enumerate(all_partitions):
+            partition = dict(zip(partition_bins.keys(), partition_values))
             logger.info(
                 "building summary table for partition: %d/%d; %s of %s",
                 index, len(all_partitions), partition, study_id)
@@ -436,15 +444,16 @@ def main(argv=None, gpf_instance=None):
                 pedigree_table, variants_table, summary_table,
                 summary_schema, partition,
                 region_bin_helpers.region_bins,
-                argv.split_size
+                args.split_size
             ):
                 repeat = 10
                 while repeat > 0:
                     try:
                         with closing(impala.connection()) as connection:
-                            with connection.cursor() as cursor:
+                            with closing(connection.cursor()) as cursor:
                                 logger.debug(
-                                    "going to run summary query: %s", qry)
+                                    "going to run partition %s summary query: "
+                                    "%s", partition, qry)
                                 cursor.execute(qry)
                                 break
                     except Exception as ex:  # pylint: disable=broad-except
