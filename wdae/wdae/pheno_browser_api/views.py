74b12aa2cdf2f5ef6ef9387fa3df76f3434f5798
IvoTod <ivo.tod.todorov@gmail.com>
2023-12-14 12:05:43 +0200

    Restore measures download limit

--- wdae/wdae/pheno_browser_api/views.py
+++ wdae/wdae/pheno_browser_api/views.py
@@ -195,6 +195,9 @@ class PhenoMeasuresDownload(QueryDatasetView):
             if not set(measure_ids).issubset(set(instrument_measures)):
                 return Response(status=status.HTTP_400_BAD_REQUEST)
 
+        if len(measure_ids) > 1900:
+            measure_ids = measure_ids[0:1900]
+
         values_iterator = self.csv_value_iterator(
             dataset, measure_ids
         )
