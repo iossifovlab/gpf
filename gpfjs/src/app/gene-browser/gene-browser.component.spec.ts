09ba44ac6694f6b23ca806129e5da7ccb8e9c6c7
joankosev <joankosev@gmail.com>
2024-01-03 15:12:33 +0200

    gene browser: update variant type unit tests

--- gpfjs/src/app/gene-browser/gene-browser.component.spec.ts
+++ gpfjs/src/app/gene-browser/gene-browser.component.spec.ts
@@ -238,7 +238,7 @@ describe('GeneBrowserComponent', () => {
       ],
       inheritanceTypeFilter: ['denovo', 'mendelian', 'omission', 'missing'],
       affectedStatus: ['Affected only', 'Unaffected only', 'Affected and unaffected'],
-      variantTypes: ['sub', 'ins', 'del', 'CNV+', 'CNV-']
+      variantTypes: ['sub', 'ins', 'del', 'CNV+', 'CNV-', 'complex']
     });
   });
 
@@ -279,7 +279,7 @@ describe('GeneBrowserComponent', () => {
       ],
       inheritanceTypeFilter: ['denovo', 'mendelian', 'omission', 'missing'],
       affectedStatus: ['Affected only', 'Unaffected only', 'Affected and unaffected'],
-      variantTypes: ['sub', 'ins', 'del', 'CNV+', 'CNV-'],
+      variantTypes: ['sub', 'ins', 'del', 'CNV+', 'CNV-', 'complex'],
       geneSymbols: ['POGZ'],
       datasetId: 'testDatasetId',
       regions: '',
