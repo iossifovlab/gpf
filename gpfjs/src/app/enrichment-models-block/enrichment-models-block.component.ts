6476c5651c6544edf835e5aa8af24509be96d9f8
joankosev <joankosev@gmail.com>
2024-01-15 16:20:55 +0200

    enrichment models: remove tabs

--- gpfjs/src/app/enrichment-models-block/enrichment-models-block.component.ts
+++ gpfjs/src/app/enrichment-models-block/enrichment-models-block.component.ts
@@ -1,31 +1,9 @@
-import { AfterViewInit, Component, Input, ViewChild } from '@angular/core';
-import { NgbNav } from '@ng-bootstrap/ng-bootstrap';
-import { Store } from '@ngxs/store';
-import { EnrichmentModelsModel, EnrichmentModelsState } from 'app/enrichment-models/enrichment-models.state';
-import { StateReset } from 'ngxs-reset-plugin';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'gpf-enrichment-models-block',
   templateUrl: './enrichment-models-block.component.html',
 })
-export class EnrichmentModelsBlockComponent implements AfterViewInit {
-  @ViewChild('nav') public ngbNav: NgbNav;
-
-  @Input()
-  public selectedDatasetId: string;
-
-  public constructor(private store: Store) { }
-
-  public ngAfterViewInit(): void {
-    this.store.selectOnce(EnrichmentModelsState).subscribe(state => {
-      const enrichmentModelsState = state as EnrichmentModelsModel;
-      if (enrichmentModelsState.enrichmentBackgroundModel || enrichmentModelsState.enrichmentCountingModel) {
-        setTimeout(() => this.ngbNav.select('models'));
-      }
-    });
-  }
-
-  public onNavChange(): void {
-    this.store.dispatch(new StateReset(EnrichmentModelsState));
-  }
+export class EnrichmentModelsBlockComponent {
+  @Input() public selectedDatasetId: string;
 }
