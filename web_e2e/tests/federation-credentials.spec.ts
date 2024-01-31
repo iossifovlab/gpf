be4a3c2975f548ab6f3b78fcfea7d83a0e56ad60
IlinaGergova <ili.gergova@gmail.com>
2024-01-31 17:42:39 +0200

    migrate federation credentials and genomic scores

---
+++ web_e2e/tests/federation-credentials.spec.ts
@@ -0,0 +1,120 @@
+import { test, expect } from '@playwright/test';
+import * as utils from './utils';
+
+test.describe('Federation token tests', () => {
+  test.beforeEach(async({ page }) => {
+    await page.goto(utils.instanceUrl, {waitUntil: 'load'});
+    await utils.loginAdmin(page);
+    await utils.navigateToSidenavPage(page, 'user-profile');
+    await page.getByText('Federation tokens').click();
+  });
+
+  test('should check federation tokens tab elements', async({ page }) => {
+    await expect(page.locator('gpf-federation-credentials')).toBeVisible();
+    await expect(page.locator('#new-credential-name-box')).toBeVisible();
+    await expect(page.locator('#create-credential-button')).toBeVisible();
+    await expect(page.getByText('Name', {exact: true})).toBeVisible();
+    await expect(page.getByText('Actions', {exact: true})).toBeVisible();
+  });
+
+  test('should create new credential', async({ page }) => {
+    await page.waitForSelector('.grid-container');
+    await page.locator('#new-credential-name-box').fill('TestCredentials');
+    await page.locator('#create-credential-button').click();
+    await expect(page.locator('.credential-modal')).toBeVisible();
+    await expect(page.locator('.credential-modal')).toContainText('Credentials: shown only once, copy before closing.');
+    await page.locator('#copy-credentials-button').click();
+    await expect(page.locator('#credential-modal-content')).not.toBeEmpty();
+
+    await page.mouse.click(0, 0); // close modal
+    await expect(page.locator('.credential-modal')).not.toBeVisible();
+
+    await expect(page.locator('div[id="TestCredentials-name-cell"]')).toBeVisible();
+    await expect(page.locator('div[id="TestCredentials-actions-cell"]')).toBeVisible();
+
+    await page.locator('#TestCredentials-delete-credential-button').click();
+    await page.getByRole('button', {name: 'Delete'}).click();
+    await page.waitForSelector('div[id="TestCredentials-name-cell"]', {state: 'detached'});
+  });
+
+  test('should not create credential with invalid name', async({ page }) => {
+    await page.waitForSelector('.grid-container');
+    await page.locator('#new-credential-name-box').fill('a');
+    await page.locator('#create-credential-button').click();
+    await expect(page.getByText('Credential names must be at least 3 symbols long.')).toBeVisible();
+    await expect(page.locator('#new-credential-name-box')).toBeFocused();
+    await expect(page.locator('div[id="a-name-cell"]')).not.toBeVisible();
+
+    await page.locator('#new-credential-name-box').fill('b');
+    await page.locator('#create-credential-button').click();
+    await expect(page.getByText('Credential names must be at least 3 symbols long.')).toBeVisible();
+    await expect(page.locator('#new-credential-name-box')).toBeFocused();
+    await expect(page.locator('div[id="ab-name-cell"]')).not.toBeVisible();
+  });
+
+  test('should not create credential with existing name', async({ page }) => {
+    await page.waitForSelector('.grid-container');
+    await page.locator('#new-credential-name-box').fill('ExistingCredential');
+    await page.locator('#create-credential-button').click();
+
+    await page.waitForSelector('.credential-modal');
+    await page.mouse.click(0, 0); // close modal
+    await expect(page.locator('div[id="ExistingCredential-name-cell"]')).toBeVisible();
+
+    await page.locator('#new-credential-name-box').fill('ExistingCredential');
+    await page.locator('#create-credential-button').click();
+    await expect(page.getByText('Credential with such name already exists!')).toBeVisible();
+    await expect(page.locator('#new-credential-name-box')).toBeFocused();
+
+    await page.locator('#ExistingCredential-delete-credential-button').click();
+    await page.getByRole('button', {name: 'Delete'}).click();
+    await expect(page.locator('div[id="ExistingCredential-name-cell"]')).not.toBeVisible();
+  });
+
+  test('should rename credential', async({ page }) => {
+    await page.waitForSelector('.grid-container');
+    await page.locator('#new-credential-name-box').fill('RenameCredential');
+    await page.locator('#create-credential-button').click();
+
+    await page.waitForSelector('.credential-modal');
+    await page.mouse.click(0, 0); // close modal
+    await expect(page.locator('div[id="RenameCredential-name-cell"]')).toBeVisible();
+
+    await page.locator('div[id="RenameCredential-name-cell"]').locator('#edit-icon').click();
+    await expect(page.locator('#RenameCredential-new-name-input')).toBeVisible();
+    await expect(page.locator('#cancel-button')).toBeVisible();
+
+    await page.locator('#RenameCredential-new-name-input').clear();
+    await page.locator('#RenameCredential-new-name-input').fill('NewNameCredential');
+    await page.keyboard.press('Enter');
+
+    await expect(page.locator('div[id="RenameCredential-name-cell"]')).not.toBeVisible();
+    await expect(page.locator('div[id="NewNameCredential-name-cell"]')).toBeVisible();
+
+    await page.locator('#NewNameCredential-delete-credential-button').click();
+    await page.getByRole('button', {name: 'Delete'}).click();
+    await expect(page.locator('div[id="NewNameCredential-name-cell"]')).not.toBeVisible();
+  });
+
+  test('should cancel renaming credential', async({ page }) => {
+    await page.waitForSelector('.grid-container');
+    await page.locator('#new-credential-name-box').fill('Credential');
+    await page.locator('#create-credential-button').click();
+
+    await page.waitForSelector('.credential-modal');
+    await page.mouse.click(0, 0); // close modal
+    await expect(page.locator('div[id="Credential-name-cell"]')).toBeVisible();
+
+    await page.locator('div[id="Credential-name-cell"]').locator('#edit-icon').click();
+    await page.locator('#Credential-new-name-input').clear();
+    await page.locator('#Credential-new-name-input').fill('Credentialllll');
+    await page.locator('#cancel-button').click();
+
+    await expect(page.locator('div[id="Credentialllll-name-cell"]')).not.toBeVisible();
+    await expect(page.locator('div[id="Credential-name-cell"]')).toBeVisible();
+
+    await page.locator('#Credential-delete-credential-button').click();
+    await page.getByRole('button', {name: 'Delete'}).click();
+    await expect(page.locator('div[id="Credential-name-cell"]')).not.toBeVisible();
+  });
+});
---
+++ web_e2e/tests/genomic-scores-block.spec.ts
@@ -0,0 +1,72 @@
+import { test, expect } from '@playwright/test';
+import * as utils from './utils';
+import { scanCSV } from 'nodejs-polars';
+
+test.describe('Genomic scores tests', () => {
+  test.beforeEach(async({ page }) => {
+    await page.goto(utils.instanceUrl, {waitUntil: 'load'});
+    await utils.loginAdmin(page);
+    await utils.navigateToDatasetPage(page, utils.datasetIds.compAll, 'Genotype browser');
+  });
+
+  test('should display genomic scores panel after "add filter" button click ' +
+  'and remove it after "remove filter" button click', async({ page }) => {
+    await expect(page.locator('gpf-genomic-scores')).toBeHidden();
+
+    await page.locator('#add-filter').click();
+    await expect(page.locator('gpf-genomic-scores')).toBeVisible();
+
+    await page.locator('#remove-button').click();
+    await expect(page.locator('gpf-genomic-scores')).toBeHidden();
+  });
+
+  test('should enter filter data and check how it affects the histogram', async({ page }) => {
+    await page.locator('#add-filter').click();
+    await page.locator('input#from-input-field').clear();
+    await page.locator('input#from-input-field').fill('0.3');
+    await expect(page.locator('text.partitions-text').nth(0)).toHaveText('~21727789 (32.46%)');
+
+    await page.locator('input#to-input-field').clear();
+    await page.locator('input#to-input-field').fill('1.45');
+    await expect(page.locator('text.partitions-text').nth(1)).toHaveText('~9402408 (14.05%)');
+    await expect(page.locator('text.partitions-text').nth(2)).toHaveText('~35798106 (53.49%)');
+
+    await page.locator('#add-filter').click();
+    await page.locator('gpf-genomic-scores select').nth(1).selectOption('exome_gnomad_af_percent - ' +
+    'Alternative allele frequency in the whole gnomAD exome samples v2.1.1 as percent');
+    await page.locator('input#from-input-field').nth(1).clear();
+    await page.locator('input#from-input-field').nth(1).fill('9.7944');
+    await expect(page.locator('text.partitions-text').nth(3)).toHaveText('~17113188 (99.46%)');
+
+    await page.locator('input#to-input-field').nth(1).clear();
+    await page.locator('input#to-input-field').nth(1).fill('13.6935');
+    await expect(page.locator('text.partitions-text').nth(4)).toHaveText('~82289 (0.48%)');
+    await expect(page.locator('text.partitions-text').nth(5)).toHaveText('~10162 (0.06%)');
+
+    await page.locator('#remove-button').nth(0).click();
+    await expect(page.locator('text.partitions-text').nth(0)).toHaveText('~17113188 (99.46%)');
+    await expect(page.locator('text.partitions-text').nth(1)).toHaveText('~82289 (0.48%)');
+    await expect(page.locator('text.partitions-text').nth(2)).toHaveText('~10162 (0.06%)');
+  });
+
+  test('should test download', async({ page }) => {
+    await utils.navigateToDatasetPage(page, utils.datasetIds.allGenotypes, 'Genotype browser');
+
+    await page.locator('#add-filter').click();
+    await page.locator('input#from-input-field').clear();
+    await page.locator('input#from-input-field').fill('1.25');
+
+    await page.locator('input#to-input-field').clear();
+    await page.locator('input#to-input-field').fill('1.8');
+
+    const downloadPromise = page.waitForEvent('download');
+    await page.getByRole('button', { name: 'Download' }).click();
+    const download = await downloadPromise;
+
+    const fixtureData = scanCSV(await download.path(), {sep: '\t'});
+    const downloadData = scanCSV('playwright/fixtures/genomic-scores/variants.tsv', {sep: '\t'});
+    const fixtureFrame = (await fixtureData.collect()).sort('family id');
+    const downloadFrame = (await downloadData.collect()).sort('family id');
+    expect(fixtureFrame.toString()).toEqual(downloadFrame.toString());
+  });
+});
