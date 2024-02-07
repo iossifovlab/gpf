c0fdabed0f2fa2eadc10ac1dbd698a70af6f5545
IlinaGergova <ili.gergova@gmail.com>
2024-02-07 12:29:07 +0200

    update regions filter regex to validate numbers having digits separated by commas, update es to 2021 and update unit tests

--- gpfjs/src/app/regions-filter/regions-filter.validator.spec.ts
+++ gpfjs/src/app/regions-filter/regions-filter.validator.spec.ts
@@ -30,17 +30,26 @@ describe('RegionsFilterValidator', () => {
     expect(component.validate('  9:76710815 ', args)).toBe(true);
     expect(component.validate('  9:76710815-76710830 ', args)).toBe(true);
     expect(component.validate('  9:76710815-76710830 \n11:10169163-10169314  ' +
-          '\n\n\n12:50344524-50352664', args)).toBe(true);
+      '\n\n\n12:50344524-50352664', args)).toBe(true);
     expect(component.validate('  9:76710815-76710830, X:10169163-10169314, Y:50344524-50352664', args)).toBe(true);
     expect(component.validate('  9:76710815-76710830, 11:10169163-10169314, 12:50344524-50352664', args)).toBe(true);
+    expect(component.validate('  9:76710815-76710830, 11:10169,163-10169,314,12:50344524-50352664', args)).toBe(true);
+    expect(component.validate('  9:76710,815-76710830, 11:10,169,163-10169,314,12:50344524-50352664', args)).toBe(true);
+
 
     expect(component.validate('  9:76710833-76710830 ', args)).toBe(false);
-    expect(component.validate('  9:76710833-76710830:420-345 ', args)).toBe(false);
+    expect(component.validate('  9:76710833-76710830:320-345 ', args)).toBe(false);
+    expect(component.validate('  9:76710833-76710\n830:320-345 ', args)).toBe(false);
+    expect(component.validate('  9:76710833-76710830+12:420-345 ', args)).toBe(false);
+
     expect(component.validate('23', args)).toBe(false);
     expect(component.validate('9:', args)).toBe(false);
     expect(component.validate('X12', args)).toBe(false);
     expect(component.validate('Y1', args)).toBe(false);
     expect(component.validate('chr9:76710815-76710830 ', args)).toBe(false);
+    expect(component.validate('  9:76710815-76710830, 11:10169,163-101693,14,12:50344524-50352664', args)).toBe(false);
+    expect(component.validate('  9:76710815-76710830, 11:10169,163-101,69314,12:50344524-50352664', args)).toBe(false);
+    expect(component.validate('  9:76710815-76710830, 11:10169,163-101,69314,,12:50344524-50352664', args)).toBe(false);
   });
 
   it('should check if regions filter input is valid for hg38', () => {
@@ -61,17 +70,28 @@ describe('RegionsFilterValidator', () => {
     expect(component.validate('  chr9:76710815-76710830 ', args)).toBe(true);
     expect(component.validate('chr9', args)).toBe(true);
     expect(component.validate('  chr9:76710815-76710830 \nchr11:10169163-10169314  ' +
-        '\nchr12:50344524-50352664', args)).toBe(true);
+      '\nchr12:50344524-50352664', args)).toBe(true);
     expect(component.validate('  chr9:76710815-76710830 , chrX:10169163-10169314,  ' +
-        'chrY:50344524-50352664', args)).toBe(true);
+      'chrY:50344524-50352664', args)).toBe(true);
+    expect(component.validate('  chr9:76710815-76710830, ' +
+      'chr11:10169,163-10169,314,chr12:50344524-50352664', args)).toBe(true);
+    expect(component.validate('  chr9:76710,815-76710830, ' +
+      'chr11:10,169,163-10169,314,chr12:50344524-50352664', args)).toBe(true);
 
     expect(component.validate('  chr9:76710815-76710830 \n11:10169163-10169314  ' +
-        '\nchr12:50344524-50352664', args)).toBe(false);
+      '\nchr12:50344524-50352664', args)).toBe(false);
     expect(component.validate('  chr9:76710844-76710830 ', args)).toBe(false);
     expect(component.validate('chr23', args)).toBe(false);
     expect(component.validate('chr9:', args)).toBe(false);
     expect(component.validate('chrX12', args)).toBe(false);
     expect(component.validate('chrY1', args)).toBe(false);
     expect(component.validate('9:76710815-76710830 ', args)).toBe(false);
+
+    expect(component.validate('chr9:76710815-76710830, ' +
+      'chr11:10169,163-101693,14,chr12:50344524-50352664', args)).toBe(false);
+    expect(component.validate('chr9:76710815-76710830, ' +
+      '11:10169,163-101,69314,12:50344524-50352664', args)).toBe(false);
+    expect(component.validate('  chr9:76710815-76710830, ' +
+      'chr11:10169,163-101,69314,,chr12:50344524-50352664', args)).toBe(false);
   });
 });
