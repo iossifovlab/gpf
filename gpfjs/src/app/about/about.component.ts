1817b50fe5554e6a7bbf8c52b04775df49d141e6
joankosev <joankosev@gmail.com>
2024-02-08 13:52:12 +0200

    about: add editor WIP

--- gpfjs/src/app/about/about.component.ts
+++ gpfjs/src/app/about/about.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./about.component.css']
 })
 export class AboutComponent {
-
+  public write(markdown: string): void {
+    // to do
+  }
 }
