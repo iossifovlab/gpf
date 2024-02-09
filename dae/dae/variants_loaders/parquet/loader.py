9178f39e7593ca793a86b3426b010c8b6b128089
iordanivanov <iiordan.ivanov@gmail.com>
2024-02-09 11:03:21 +0200

    Initial work on parquet variants loader

---
+++ dae/dae/variants_loaders/parquet/loader.py
@@ -0,0 +1,42 @@
+import json
+import glob
+from typing import Generator, Optional
+from pyarrow import parquet as pq
+from dae.schema2_storage.schema2_import_storage import Schema2DatasetLayout, schema2_dataset_layout
+from dae.pedigrees.loader import FamiliesLoader
+from dae.pedigrees.families_data import FamiliesData
+from dae.variants.attributes import Role, Status, Sex
+from dae.variants.variant import SummaryVariant, SummaryVariantFactory
+
+
+class ParquetLoader:
+    RECORD_COLUMN = "summary_variant_data"
+
+    def __init__(self, data_dir: str):
+        self.data_dir: str = data_dir
+        self.layout: Schema2DatasetLayout = schema2_dataset_layout(data_dir)
+        self.families: Optional[FamiliesData] = None
+
+    def _deserialize_summary_variant(self, record: str) -> SummaryVariant:
+        return SummaryVariantFactory.summary_variant_from_records(
+            json.loads(record)
+        )
+
+    def load_families(self) -> None:
+        parquet_file = pq.ParquetFile(self.layout.pedigree)
+        ped_df = parquet_file.read().to_pandas()
+        ped_df.role = ped_df.role.apply(Role.from_value)
+        ped_df.sex = ped_df.sex.apply(Sex.from_value)
+        ped_df.status = ped_df.status.apply(Status.from_value)
+        ped_df.loc[ped_df.layout.isna(), "layout"] = None
+        self.families = FamiliesLoader.build_families_data_from_pedigree(ped_df)
+        parquet_file.close()
+        
+    def fetch_variants(self) -> Generator[SummaryVariant, None, None]:
+        assert self.families is not None
+        for path in glob.glob(f"{self.layout.summary}/*.parquet"):
+            parquet_file = pq.ParquetFile(path)
+            for batch in parquet_file.iter_batches(columns=[self.RECORD_COLUMN]):
+                for rec in batch.column(self.RECORD_COLUMN):
+                    yield self._deserialize_summary_variant(rec.as_py())
+            parquet_file.close()
