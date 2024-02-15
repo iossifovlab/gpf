43eda46f814b5a7b4e741b2d59d832e6faa52aa7
iordanivanov <iiordan.ivanov@gmail.com>
2024-02-15 11:49:38 +0200

    Basic variant count tests for ParquetLoader

--- dae/dae/variants_loaders/parquet/tests/test_parquet_loader.py
+++ dae/dae/variants_loaders/parquet/tests/test_parquet_loader.py
@@ -2,49 +2,20 @@ import pathlib
 import pytest
 
 from dae.gpf_instance import GPFInstance
-from dae.studies.study import GenotypeData
-
+from dae.variants_loaders.parquet.loader import ParquetLoader
 from dae.testing import setup_pedigree, setup_vcf, vcf_study
 from dae.testing.t4c8_import import t4c8_gpf
 
-from dae.genotype_storage.genotype_storage_registry import \
-    get_genotype_storage_factory
-from dae.duckdb_storage.duckdb_genotype_storage import \
-    DuckDbGenotypeStorage
-
-from dae.variants_loaders.parquet.loader import ParquetLoader
-
 
 @pytest.fixture(scope="module")
-def duckdb_storage_fixture(tmp_path_factory: pytest.TempPathFactory) -> DuckDbGenotypeStorage:
-    storage_path = tmp_path_factory.mktemp("duckdb_storage")
-    storage_config = {
-        "id": "dev_duckdb_storage",
-        "storage_type": "duckdb",
-        "db": "duckdb_storage/dev_storage.db",
-        "base_dir": str(storage_path)
-    }
-    storage_factory = get_genotype_storage_factory("duckdb")
-    assert storage_factory is not None
-    storage = storage_factory(storage_config)
-    assert storage is not None
-    assert isinstance(storage, DuckDbGenotypeStorage)
-    return storage
-
-
-@pytest.fixture(scope="module")
-def t4c8_instance(tmp_path_factory: pytest.TempPathFactory,
-                  duckdb_storage_fixture: DuckDbGenotypeStorage) -> GPFInstance:
+def t4c8_instance(tmp_path_factory: pytest.TempPathFactory) -> GPFInstance:
     root_path = tmp_path_factory.mktemp("t4c8_instance")
-    gpf_instance = t4c8_gpf(root_path, duckdb_storage_fixture)
+    gpf_instance = t4c8_gpf(root_path)
     return gpf_instance
 
 
 @pytest.fixture(scope="module")
-def t4c8_study_1(
-    t4c8_instance: GPFInstance,
-    duckdb_storage_fixture: DuckDbGenotypeStorage
-) -> str:
+def t4c8_study_1(t4c8_instance: GPFInstance) -> str:
     root_path = pathlib.Path(t4c8_instance.dae_dir)
     ped_path = setup_pedigree(
         root_path / "study_1" / "pedigree" / "in.ped",
@@ -75,16 +46,13 @@ chr1   122  .  A   C   .    .      .    GT     0/0  1/0  0/0 0/0  0/0  0/0
         root_path,
         "study_1", ped_path, [vcf_path1],
         t4c8_instance,
+        project_config_overwrite={"destination": {"storage_type": "schema2"}}
     )
-    duckdb_storage_fixture.shutdown()
     return f"{root_path}/work_dir/study_1"
 
 
 @pytest.fixture(scope="module")
-def t4c8_study_2(
-    t4c8_instance: GPFInstance,
-    duckdb_storage_fixture: DuckDbGenotypeStorage
-) -> GenotypeData:
+def t4c8_study_2(t4c8_instance: GPFInstance) -> str:
     root_path = pathlib.Path(t4c8_instance.dae_dir)
     ped_path = setup_pedigree(
         root_path / "study_2" / "pedigree" / "in.ped",
@@ -136,30 +104,31 @@ chr1   122  .  A   C,AC .    .      .    GT     0/1  0/1  0/1 0/2  0/2  0/2
             }
         },
     }
-
-    study = vcf_study(
+    vcf_study(
         root_path,
         "study_2", ped_path, [vcf_path1],
         t4c8_instance,
         project_config_update=project_config_update,
+        project_config_overwrite={"destination": {"storage_type": "schema2"}}
     )
-    duckdb_storage_fixture.shutdown()
     return f"{root_path}/work_dir/study_2"
 
-# def test_sample(t4c8_study_1):
-#     loader = ParquetLoader(t4c8_study_1)
-#     loader.load_families()
-#     import pdb; pdb.set_trace()
-#     vs = list(loader.fetch_variants())
-#     for v in vs:
-#         pass
-#     assert False
 
-def test_sample2(t4c8_study_2):
+def test_fetch_variants_count_nonpartitioned(t4c8_study_1):
+    loader = ParquetLoader(t4c8_study_1)
+    loader.load_families()
+    vs = list(loader.fetch_variants())
+    # summary variants
+    assert len(vs) == 3
+    # family variants
+    assert sum(len(fvs) for _, fvs in vs) == 4
+
+
+def test_fetch_variants_count_partitioned(t4c8_study_2):
     loader = ParquetLoader(t4c8_study_2, partitioned=True)
     loader.load_families()
-    import pdb; pdb.set_trace()
-    vs = list(loader.fetch_variants_partitioned())
-    for v in vs:
-        pass
-    assert True
+    vs = list(loader.fetch_variants())
+    # summary variants
+    assert len(vs) == 6
+    # family variants
+    assert sum(len(fvs) for _, fvs in vs) == 12
