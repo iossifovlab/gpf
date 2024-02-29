92179e0be32cfe3566ed1e7ed88a8c73ca4164f4
IlinaGergova <ili.gergova@gmail.com>
2024-02-29 15:40:27 +0200

    add family tags bottom line

--- gpfjs/src/app/family-tags/family-tags.ts
+++ gpfjs/src/app/family-tags/family-tags.ts
@@ -1,7 +1,4 @@
-import { IsEmpty } from 'class-validator';
-
 export class FamilyTags {
-  // @IsEmpty({message: 'Please select at least one family tag.'})
   public selectedTags: string[] = [];
   public deselectedTags: string[] = [];
   public tagIntersection = true; // mode "And"
