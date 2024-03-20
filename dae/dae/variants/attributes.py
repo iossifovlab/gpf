1caf125bde84f1d004336dd2298230b61ede104b
IvoTod <ivo.tod.todorov@gmail.com>
2024-03-20 17:02:12 +0200

    Add utilities for getting enum values from names

--- dae/dae/variants/attributes.py
+++ dae/dae/variants/attributes.py
@@ -134,6 +134,11 @@ class Role(enum.Enum):
         return Role.unknown
 
     @staticmethod
+    def to_value(name: Optional[Union[str, int]]) -> int:
+        role = Role.from_name(name)
+        return role.value
+
+    @staticmethod
     def from_value(val: int) -> Role:
         return Role(int(val))
 
@@ -172,6 +177,11 @@ class Sex(enum.Enum):
         if name in set(["unspecified", "u", "0", "unknown"]):
             return Sex.unspecified
         raise ValueError(f"unexpected sex name: {name}")
+
+    @staticmethod
+    def to_value(name: Optional[Union[int, str]]) -> int:
+        sex = Sex.from_name(name)
+        return sex.value
 
     @staticmethod
     def from_value(val: int) -> Sex:
@@ -224,6 +234,11 @@ class Status(enum.Enum):
         if name in set(["unspecified", "-", "0", "unknown"]):
             return Status.unspecified
         raise ValueError("unexpected status type: " + name)
+
+    @staticmethod
+    def to_value(name: Optional[Union[int, str]]) -> int:
+        status = Status.from_name(name)
+        return status.value
 
     @staticmethod
     def from_value(val: int) -> Status:
