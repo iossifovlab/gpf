743ca53fb39e07de8c02f0caed423ea3b0edac34
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2024-04-23 13:08:41 +0300

    Clean up version of vcf_liftover tool.

--- dae/dae/tools/vcf_liftover.py
+++ dae/dae/tools/vcf_liftover.py
@@ -1,23 +1,23 @@
 import argparse
 import logging
-import pathlib
 import sys
+import tempfile
 import textwrap
-from collections import Counter, defaultdict
 from contextlib import closing
-from typing import Any, Optional
+from typing import Optional
 
 import pysam
 
 from dae.annotation.annotatable import VCFAllele
 from dae.annotation.annotation_factory import build_annotation_pipeline
+from dae.annotation.annotation_pipeline import AnnotationPipeline
 from dae.annotation.context import CLIAnnotationContext
 from dae.genomic_resources.genomic_context import get_genomic_context
 from dae.genomic_resources.reference_genome import (
     ReferenceGenome,
     build_reference_genome_from_resource,
 )
-from dae.tools.stats_liftover import save_liftover_stats
+from dae.genomic_resources.repository import GenomicResourceRepo
 from dae.utils.regions import Region
 from dae.utils.verbosity_configuration import VerbosityConfiguration
 
@@ -33,7 +33,7 @@ def parse_cli_arguments() -> argparse.ArgumentParser:
     VerbosityConfiguration.set_arguments(parser)
 
     parser.add_argument(
-        "--vcf", help="vcf file to liftover",
+        "vcffile", help="vcf file to liftover",
     )
 
     parser.add_argument(
@@ -43,21 +43,10 @@ def parse_cli_arguments() -> argparse.ArgumentParser:
     parser.add_argument(
         "-t", "--target-genome", help="target genome",
         default="hg38/genomes/GRCh38-hg38")
-    parser.add_argument(
-        "--target-gene-models", "--tgm", help="target gene models",
-        default="hg38/gene_models/refGene_v20170601")
 
     parser.add_argument(
         "-s", "--source-genome", help="source genome",
         default="hg19/genomes/GATK_ResourceBundle_5777_b37_phiX174")
-    parser.add_argument(
-        "--source-gene-models", "--sgm", help="source gene models",
-        default="hg19/gene_models/refGene_v20190211")
-
-    parser.add_argument(
-        "--stats", help="filename to store liftover statistics",
-        default="stats.txt",
-    )
 
     parser.add_argument(
         "-o", "--output-prefix", help="output filename prefix",
@@ -76,6 +65,42 @@ def parse_cli_arguments() -> argparse.ArgumentParser:
     return parser
 
 
+def _construct_reference_genome(
+    grr: GenomicResourceRepo,
+    resource_id: str,
+) -> ReferenceGenome:
+    res = grr.get_resource(resource_id)
+    if res is None:
+        raise ValueError(f"reference genome {resource_id} not found")
+
+    genome = build_reference_genome_from_resource(res)
+    if genome is None:
+        raise ValueError(f"reference genome {resource_id} not found")
+    return genome.open()
+
+
+def _construct_liftover_pipeline(
+    grr: GenomicResourceRepo,
+    **kwargs: dict[str, str],
+) -> AnnotationPipeline:
+    pipeline_config = textwrap.dedent(
+        f"""
+        - liftover_annotator:
+            chain: {kwargs.get('chain')}
+            target_genome: {kwargs.get('target_genome')}
+            attributes:
+            - source: liftover_annotatable
+              name: target_annotatable
+        """,
+    )
+
+    pipeline = build_annotation_pipeline(
+        pipeline_config_str=pipeline_config,
+        grr_repository=grr)
+    pipeline.open()
+    return pipeline
+
+
 def main(
         argv: Optional[list[str]] = None,
 ) -> None:
@@ -90,25 +115,12 @@ def main(
     VerbosityConfiguration.set(args)
 
     context = get_genomic_context()
-
     grr = context.get_genomic_resources_repository()
     if grr is None:
         raise ValueError("genomic resources repository not found")
 
-    res = grr.get_resource(args.source_genome)
-    if res is None:
-        raise ValueError(f"source genome {args.source_genome} not found")
-
-    source_genome = build_reference_genome_from_resource(res)
-    if source_genome is None:
-        raise ValueError(f"source genome {args.source_genome} not found")
-
-    res = grr.get_resource(args.target_genome)
-    if res is None:
-        raise ValueError(f"target genome {args.target_genome} not found")
-    target_genome = build_reference_genome_from_resource(res)
-    if target_genome is None:
-        raise ValueError(f"target genome {args.target_genome} not found")
+    source_genome = _construct_reference_genome(grr, args.source_genome)
+    target_genome = _construct_reference_genome(grr, args.target_genome)
 
     output_filename = f"{args.output_prefix}.vcf"
     region = None
@@ -116,55 +128,15 @@ def main(
         region = str(Region.from_str(args.region))
         output_filename = f"{args.output_prefix}-{region}.vcf"
 
-    pipeline_config = textwrap.dedent(
-        f"""
-        - effect_annotator:
-            gene_models: {args.source_gene_models}
-            genome: {args.source_genome}
-            attributes:
-            - source: "worst_effect"
-              name: "source_worst_effect"
-            - source: "gene_effects"
-              name: "source_gene_effects"
-            - source: "effect_details"
-              name: "source_effect_details"
+    pipeline = _construct_liftover_pipeline(grr, **vars(args))
 
-        - liftover_annotator:
-            chain: {args.chain}
-            target_genome: {args.target_genome}
-            attributes:
-            - source: liftover_annotatable
-              name: target_annotatable
-
-        - effect_annotator:
-            gene_models: {args.target_gene_models}
-            genome: {args.target_genome}
-            input_annotatable: target_annotatable
-            attributes:
-            - source: "worst_effect"
-              name: "target_worst_effect"
-            - source: "gene_effects"
-              name: "target_gene_effects"
-            - source: "effect_details"
-              name: "target_effect_details"
-
-        """,
-    )
-
-    pipeline = build_annotation_pipeline(
-        pipeline_config_str=pipeline_config,
-        grr_repository=grr)
-    pipeline.open()
-
-    target_stats: dict[str, Any] = defaultdict(Counter)
-
-    with closing(pysam.VariantFile(args.vcf)) as infile:
+    with closing(pysam.VariantFile(args.vcffile)) as infile:
         output_header = liftover_header(
-            pathlib.Path(args.vcf),
+            args.vcffile,
             source_genome,
             target_genome)
 
-    with closing(pysam.VariantFile(args.vcf)) as infile, \
+    with closing(pysam.VariantFile(args.vcffile)) as infile, \
             closing(pysam.VariantFile(
                 output_filename, "w", header=output_header)) as outfile:
         for vcf_variant in infile.fetch(region=region):
@@ -207,14 +179,18 @@ def main(
                     report_vcf_variant(vcf_variant), report_alleles(alleles))
                 continue
             vcf_variant.translate(output_header)
-            vcf_variant.contig = alleles[0].chrom
-            vcf_variant.pos = alleles[0].pos
-            vcf_variant.ref = alleles[0].ref
-            vcf_variant.alts = tuple(a.alt for a in alleles)
+            try:
+                vcf_variant.contig = alleles[0].chrom
+                vcf_variant.pos = alleles[0].pos
+                vcf_variant.ref = alleles[0].ref
+                vcf_variant.alts = tuple(a.alt for a in alleles)
 
-            outfile.write(vcf_variant)
-
-    save_liftover_stats(target_stats, args.stats)
+                outfile.write(vcf_variant)
+            except ValueError:
+                logger.exception(
+                    "skipping variant with invalid liftover: %s liftover to %s",
+                    report_vcf_variant(vcf_variant), report_alleles(alleles))
+                raise
 
 
 def report_vcf_variant(vcf_variant: pysam.VariantRecord) -> str:
@@ -236,49 +212,48 @@ def report_alleles(alleles: list[VCFAllele]) -> str:
 
 
 def liftover_header(
-    infile: pathlib.Path,
+    vcffile: str,
     source_genome: ReferenceGenome,
     target_genome: ReferenceGenome,
 ) -> pysam.VariantHeader:
     """Create liftover VCF header."""
-    with closing(pysam.TabixFile(str(infile))) as tabix_file:
-        contigs = tabix_file.contigs
     target_contigs = []
-    for chrom in contigs:
-        name = f"chr{chrom}"
-        target_contigs.append(name)
+    target_contigs = target_genome.chromosomes[:]
 
-    with closing(pysam.VariantFile(str(infile))) as vcf, \
-        closing(pysam.VariantFile(
-                "tmp.vcf", "w", header=vcf.header)) as tmp:
-        pass
+    with tempfile.NamedTemporaryFile(
+        mode="wt", suffix=".vcf") as temp_vcf:
 
-    with open("tmp.vcf", "rt") as tmp:
-        lines = tmp.readlines()
-        outheader = []
-        contig_index = None
-        for index, line in enumerate(lines):
-            if line.startswith("##contig"):
-                if contig_index is None:
-                    contig_index = index
-                continue
-            outheader.append(line)
-    outcontigs = [f"##contig=<ID={contig}>\n" for contig in target_contigs]
-    if contig_index is None:
-        raise ValueError("No contig lines found in header")
+        with closing(pysam.VariantFile(vcffile)) as vcf, \
+            closing(pysam.VariantFile(
+                    temp_vcf.name, "w", header=vcf.header)) as tmp:
+            pass
 
-    with open("tmp.vcf", "wt") as tmp:
-        for index, line in enumerate(outheader):
-            if index == contig_index:
-                for contig in outcontigs:
-                    tmp.write(contig)
-            tmp.write(line)
-        tmp.write(
-            f"##source_reference={source_genome.resource_id}\n")
-        tmp.write(
-            f"##target_reference={target_genome.resource_id}\n")
-        tmp.write(
-            f"##command=vcf_liftover {' '.join(sys.argv[1:])}\n")
+        with open(temp_vcf.name, "rt") as tmp:
+            lines = tmp.readlines()
+            outheader = []
+            contig_index = None
+            for index, line in enumerate(lines):
+                if line.startswith("##contig"):
+                    if contig_index is None:
+                        contig_index = index
+                    continue
+                outheader.append(line)
+        outcontigs = [f"##contig=<ID={contig}>\n" for contig in target_contigs]
+        if contig_index is None:
+            raise ValueError("No contig lines found in header")
 
-    with closing(pysam.VariantFile("tmp.vcf")) as tmp:
-        return tmp.header
+        with open(temp_vcf.name, "wt") as tmp:
+            for index, line in enumerate(outheader):
+                if index == contig_index:
+                    for contig in outcontigs:
+                        tmp.write(contig)
+                tmp.write(line)
+            tmp.write(
+                f"##source_genome={source_genome.resource_id}\n")
+            tmp.write(
+                f"##target_genome={target_genome.resource_id}\n")
+            tmp.write(
+                f"##command=vcf_liftover {' '.join(sys.argv[1:])}\n")
+
+        with closing(pysam.VariantFile(temp_vcf.name)) as tmp:
+            return tmp.header
