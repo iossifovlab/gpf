4647d78d8c363da6934b574d685ab99719640adb
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2024-04-26 13:00:29 +0300

    Liftover chain fixture expanded. Chain on reverse strand added.

--- dae/dae/tools/tests/test_liftover_tools.py
+++ dae/dae/tools/tests/test_liftover_tools.py
@@ -10,6 +10,9 @@ import pytest
 from dae.genomic_resources.liftover_chain import (
     build_liftover_chain_from_resource,
 )
+from dae.genomic_resources.reference_genome import (
+    build_reference_genome_from_resource,
+)
 from dae.genomic_resources.repository import GenomicResourceRepo
 from dae.genomic_resources.testing import (
     build_filesystem_test_repository,
@@ -89,6 +92,16 @@ def grr_fixture(
         48 4 0
         48 0 0
         0
+
+        chain 4800 bar 112 + 4 108 chrBar 100 + 0 96 2
+        48 8 0
+        48 0 0
+        0
+
+        chain 4700 baz 108 + 4 104 chrBaz 100 - 4 100 3
+        48 4 0
+        48 0 0
+        0
         """),
     )
     setup_genome(
@@ -108,9 +121,9 @@ def grr_fixture(
             >foo
             NNNN{12 * 'ACGT'}NNNN{12 * 'ACGT'}
             >bar
-            NNNN{12 * 'ACGT'}NNNNNN{12 * 'ACGT'}
+            NNNN{12 * 'ACGT'}NNNNNNNN{12 * 'ACGT'}NNNN
             >baz
-            NNNN{12 * 'TGCA'}NNNNNNNN{12 * 'TGCA'}
+            NNNN{12 * 'TGCA'}NNNN{12 * 'TGCA'}NNNN
             """),
     )
 
@@ -136,7 +149,23 @@ def grr_fixture(
     ("foo", 103, ("chrFoo", 95, "+", 4900)),
     ("foo", 104, ("chrFoo", 96, "+", 4900)),
     ("foo", 105, None),
-    ("bar", 5, None),
+    ("bar", 5, ("chrBar", 1, "+", 4800)),
+    ("bar", 52, ("chrBar", 48, "+", 4800)),
+    ("bar", 53, None),
+    ("bar", 60, None),
+    ("bar", 61, ("chrBar", 49, "+", 4800)),
+    ("bar", 108, ("chrBar", 96, "+", 4800)),
+    ("bar", 109, None),
+    ("bar", 112, None),
+    ("baz", 5, ("chrBaz", 96, "-", 4700)),
+    ("baz", 6, ("chrBaz", 95, "-", 4700)),
+    ("baz", 51, ("chrBaz", 50, "-", 4700)),
+    ("baz", 52, ("chrBaz", 49, "-", 4700)),
+    ("baz", 53, None),
+    ("baz", 56, None),
+    ("baz", 57, ("chrBaz", 48, "-", 4700)),
+    ("baz", 104, ("chrBaz", 1, "-", 4700)),
+    ("baz", 105, None),
 ])
 def test_liftover_chain_fixture(
         schrom: str,
@@ -146,10 +175,21 @@ def test_liftover_chain_fixture(
     res = grr_fixture.get_resource("liftover_chain")
     liftover_chain = build_liftover_chain_from_resource(res)
 
+    res = grr_fixture.get_resource("target_genome")
+    target_genome = build_reference_genome_from_resource(res).open()
+    res = grr_fixture.get_resource("source_genome")
+    source_genome = build_reference_genome_from_resource(res).open()
+
     assert liftover_chain is not None
     liftover_chain.open()
     lo_coordinates = liftover_chain.convert_coordinate(schrom, spos)
     assert lo_coordinates == expected
+
+    if expected is not None:
+        sseq = source_genome.get_sequence(schrom, spos, spos)
+        chrom, pos, _, _ = expected
+        tseq = target_genome.get_sequence(chrom, pos, pos)
+        assert sseq == tseq
 
 
 def test_vcf_liftover_simple(
