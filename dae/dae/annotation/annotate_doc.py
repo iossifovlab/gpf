78cbc279f32b15f4ecf3fe6df7c9fdcb21a31e41
iordanivanov <iiordan.ivanov@gmail.com>
2024-04-30 17:34:48 +0300

    annotate_doc: fix mistaken autoescape parameter from earlier lint

--- dae/dae/annotation/annotate_doc.py
+++ dae/dae/annotation/annotate_doc.py
@@ -48,8 +48,7 @@ def cli(raw_args: Optional[list[str]] = None) -> None:
 
     annotation_info = pipeline.get_info()
 
-    env = Environment(loader=PackageLoader("dae.annotation", "templates"),
-                      autoescape=True)
+    env = Environment(loader=PackageLoader("dae.annotation", "templates"))
     template = env.get_template("annotate_doc_pipeline_template.jinja")
     html_doc = template.render(annotation_pipeline_info=annotation_info,
                                markdown=markdown)
