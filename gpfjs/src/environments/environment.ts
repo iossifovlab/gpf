d9c5c4c9f8e8a41f010230c21744b6b94177f2bf
joankosev <joankosev@gmail.com>
2024-06-05 15:21:57 +0300

    environment.ts: update apiPath

--- gpfjs/src/environments/environment.ts
+++ gpfjs/src/environments/environment.ts
@@ -3,12 +3,12 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `angular-cli.json`.
 import versionInfo from '../../version.json';
-const basePath = 'http://localhost:8000';
+const basePath = 'http://localhost:8000/';
 
 export const environment = {
   production: false,
   basePath: basePath,
-  apiPath: basePath + '/api/v3/',
+  apiPath: basePath + 'api/v3/',
   imgPathPrefix: 'assets/',
   sentryTunnel: '',
   oauthClientId: 'gpfjs',
