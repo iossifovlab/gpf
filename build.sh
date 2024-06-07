dd46686f8c4b4ee6517e336f2f7147c7cd08feae
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2024-06-07 11:08:01 +0300

    Forgottend deployment step restored.

--- build.sh
+++ build.sh
@@ -183,30 +183,30 @@ EOT
 
   }
 
-#   build_stage "Publish documentation"
-#   {
+  build_stage "Publish documentation"
+  {
 
-#     local branch=$(e gpf_documentation_git_branch)
+    local branch=$(e gpf_documentation_git_branch)
 
-#     if [ "$branch" == "master" ]; then
-#         local iossifovlab_infra_ref;
-#         iossifovlab_infra_ref=$(e docker_img_iossifovlab_infra)
+    if [ "$branch" == "master" ]; then
+        local iossifovlab_infra_ref;
+        iossifovlab_infra_ref=$(e docker_img_iossifovlab_infra)
 
-#         build_run_ctx_init "container" "${iossifovlab_infra_ref}"
-#         defer_ret build_run_ctx_reset
+        build_run_ctx_init "container" "${iossifovlab_infra_ref}"
+        defer_ret build_run_ctx_reset
 
-#         # copy host's .ssh dir as the root .ssh in the container
-#         build_run_container_cp_to /root/ $HOME/.ssh
-#         build_run_container chown -R root:root /root/.ssh
+        # copy host's .ssh dir as the root .ssh in the container
+        build_run_container_cp_to /root/ $HOME/.ssh
+        build_run_container chown -R root:root /root/.ssh
 
-#         build_run_container bash -c "
-#             /opt/conda/bin/conda run --no-capture-output -n infra \
-#             ansible-playbook -i doc_inventory doc_publish.yml"
-#     else
-#         build_run_local echo "Skip publish documentation because the branch is not master"
-#     fi
+        build_run_container bash -c "
+            /opt/conda/bin/conda run --no-capture-output -n infra \
+            ansible-playbook -i doc_inventory doc_publish.yml"
+    else
+        build_run_local echo "Skip publish documentation because the branch is not master"
+    fi
 
-#   }
+  }
 
 }
 
