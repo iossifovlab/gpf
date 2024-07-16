b3a6e062a7b178dc34249f08d1f324e993bf7ea4
joankosev <joankosev@gmail.com>
2024-07-16 17:20:23 +0300

    pheno measure selector: further refactor

--- gpfjs/src/app/pheno-measure-selector/pheno-measure-selector.component.ts
+++ gpfjs/src/app/pheno-measure-selector/pheno-measure-selector.component.ts
@@ -1,8 +1,12 @@
-import { Component, OnChanges, Input, Output, EventEmitter } from '@angular/core';
-
+import {
+  Component, OnChanges, Input, Output,
+  EventEmitter, ElementRef, ViewChild,
+  HostListener
+} from '@angular/core';
 import { MeasuresService } from '../measures/measures.service';
 import { ContinuousMeasure } from '../measures/measures';
 import { first } from 'rxjs/operators';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
 
 @Component({
   selector: 'gpf-pheno-measure-selector',
@@ -14,14 +18,21 @@ export class PhenoMeasureSelectorComponent implements OnChanges {
   @Output() public selectedMeasureChange = new EventEmitter(true);
   @Output() public measuresChange = new EventEmitter(true);
 
+  @ViewChild('measuresSearchBox') private searchBox: ElementRef;
+  @ViewChild(CdkVirtualScrollViewport) private viewport: CdkVirtualScrollViewport;
+
   public measures: Array<ContinuousMeasure> = [];
   public filteredMeasures: Array<ContinuousMeasure> = [];
   public searchString = '';
-  public selectedMeasure: ContinuousMeasure;
+  public selectedMeasure: ContinuousMeasure = null;
   public loadingMeasures = false;
   public loadingDropdown = false;
   public isSelected = false;
   public showDropdown = false;
+
+  public topVisibleIdx = 0;
+  public selectedIdx = -1;
+  public idxSubscription = null;
 
   public constructor(
     private measuresService: MeasuresService,
@@ -39,6 +50,22 @@ export class PhenoMeasureSelectorComponent implements OnChanges {
     }
   }
 
+  @HostListener('window:keydown.tab', ['$event'])
+  public handleTabKey(event): void {
+    if (!this.showDropdown) {
+      return;
+    }
+    event.preventDefault();
+    if (event.code !== 'Tab') {
+      return;
+    }
+    if (event.shiftKey) {
+      this.prevItem();
+    } else {
+      this.nextItem();
+    }
+  }
+
   public selectMeasure(measure: ContinuousMeasure, sendEvent: boolean = true): void {
     this.selectedMeasure = measure;
     this.searchString = measure ? measure.name : '';
@@ -47,18 +74,60 @@ export class PhenoMeasureSelectorComponent implements OnChanges {
     }
   }
 
+  private initIdxSubscription(): void {
+    this.idxSubscription = this.viewport.scrolledIndexChange.subscribe((idx) => {
+      this.topVisibleIdx = idx;
+    });
+  }
+
+  public prevItem(): void {
+    if (this.idxSubscription === null) {
+      this.initIdxSubscription();
+    }
+
+    if (this.selectedIdx > 0) {
+      this.selectedIdx -= 1;
+    }
+    if (this.selectedIdx < this.topVisibleIdx) {
+      this.viewport.scrollToIndex(this.topVisibleIdx - 1);
+    }
+  }
+
+  public nextItem(): void {
+    if (this.idxSubscription === null) {
+      this.initIdxSubscription();
+    }
+
+    if (this.selectedIdx + 1 < this.filteredMeasures.length) {
+      this.selectedIdx += 1;
+    }
+    if (this.selectedIdx >= this.topVisibleIdx + 7) {
+      this.viewport.scrollToIndex(this.topVisibleIdx + 1);
+    }
+  }
+
+  public closeDropdown(): void {
+    this.showDropdown = false;
+    (this.searchBox.nativeElement as HTMLInputElement).blur();
+  }
+
   public clear(): void {
+    if (this.selectedMeasure === null) {
+      return;
+    }
     this.selectMeasure(null);
     this.isSelected = false;
     this.loadDropdownData();
   }
 
-  public loadDropdownData(): void {
+  public loadDropdownData($event = null): void {
+    if ($event && ($event.key === 'ArrowUp' || $event.key === 'ArrowDown')) {
+      return;
+    }
     if (!this.loadingMeasures) {
       this.filterData();
     } else {
       this.loadingDropdown = true;
-
       const intervalId = setInterval(() => {
         if (!this.loadingMeasures) {
           this.filterData();
@@ -70,6 +139,9 @@ export class PhenoMeasureSelectorComponent implements OnChanges {
   }
 
   private filterData(): void {
+    this.selectedIdx = -1;
+    this.topVisibleIdx = 0;
+    this.viewport?.scrollToIndex(0);
     this.filteredMeasures = this.measures;
     if (this.searchString.length) {
       this.filteredMeasures = this.filteredMeasures.filter(measure =>
