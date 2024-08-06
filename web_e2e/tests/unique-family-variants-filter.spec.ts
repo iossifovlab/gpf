8accfa73b94b407779297fc449cad7ffa80884f3
IlinaGergova <ili.gergova@gmail.com>
2024-08-06 15:25:58 +0300

    add and update unique family variants tests

--- web_e2e/tests/unique-family-variants-filter.spec.ts
+++ web_e2e/tests/unique-family-variants-filter.spec.ts
@@ -7,22 +7,26 @@ test.describe('Unique family variants filter tests', () => {
     await utils.loginAdmin(page);
   });
 
-  test('should make unique family variants filter on a single study disabled', async({ page }) => {
-    await utils.navigateToDatasetPage(page, utils.datasetIds.compAll, 'Genotype Browser');
+  test('should disable unique family variants filter on a study', async({ page }) => {
+    await utils.navigateToDatasetPage(page, utils.datasetIds.compDenovo, 'Genotype Browser');
     await expect(page.locator('#unique-family-variants-block')).not.toBeVisible();
 
-    await utils.navigateToDatasetPage(page, utils.datasetIds.compAll, 'Gene Browser');
+    await utils.navigateToDatasetPage(page, utils.datasetIds.compDenovo, 'Gene Browser');
     await page.locator('gpf-gene-browser input#search-box').fill('SAMD11');
     await page.getByText('Go').click();
     await expect(page.locator('gpf-gene-plot')).toBeVisible();
     await expect(page.locator('#unique-family-variants-checkbox')).toBeDisabled();
   });
 
-  test.skip('should make the unique family variants filter on a dataset with only a single study disabled', async ({ page }) => {
-    // ...
+  test('should disable unique family variants when no variants are loaded', async({ page }) => {
+    await utils.navigateToDatasetPage(page, utils.datasetIds.multi, 'Gene Browser');
+    await page.locator('gpf-gene-browser input#search-box').fill('CHD8');
+    await page.getByText('Go').click();
+    await expect(page.locator('gpf-gene-plot')).toBeVisible();
+    await expect(page.locator('#unique-family-variants-checkbox')).toBeDisabled();
   });
 
-  test('should enable the unique family variants filter on a dataset with at least 2 studies inside', async({ 
+  test('should enable the unique family variants filter on a dataset with at least 2 studies inside', async({
     page
   }) => {
     await utils.navigateToDatasetPage(page, utils.datasetIds.compGenotypes, 'Genotype Browser');
@@ -35,7 +39,17 @@ test.describe('Unique family variants filter tests', () => {
     await expect(page.locator('#unique-family-variants-checkbox')).toBeEnabled();
   });
 
-  test.skip('should use the unique family variants filter to hide variants', async({ page }) => {
-    // ...
+  test('should use the unique family variants filter to hide variants', async({ page }) => {
+    await utils.navigateToDatasetPage(page, utils.datasetIds.allGenotypes, 'Genotype Browser');
+    await expect(page.locator('#unique-family-variants-block')).toBeVisible();
+
+    await utils.navigateToDatasetPage(page, utils.datasetIds.allGenotypes, 'Gene Browser');
+    await page.locator('gpf-gene-browser input#search-box').fill('SAMD11');
+    await page.getByText('Go').click();
+    await expect(page.locator('gpf-gene-plot')).toBeVisible();
+
+    await expect(page.locator('#variants-count-span')).toHaveText('60 variants selected');
+    await page.locator('#unique-family-variants-checkbox').click();
+    await expect(page.locator('#variants-count-span')).toHaveText('30 variants selected');
   });
 });
