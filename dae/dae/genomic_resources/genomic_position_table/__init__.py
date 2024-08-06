f636927337094f86f0af98b90f047541d89d7fed
iordanivanov <iiordan.ivanov@gmail.com>
2024-08-06 16:06:49 +0300

    genomic position table / score: minor fixes

--- dae/dae/genomic_resources/genomic_position_table/__init__.py
+++ dae/dae/genomic_resources/genomic_position_table/__init__.py
@@ -1,9 +1,12 @@
-from .line import Line, LineBuffer, VCFLine
+from .line import BigWigLine, Line, LineBuffer, VCFLine
+from .table_bigwig import BigWigTable
 from .table_tabix import TabixGenomicPositionTable
 from .table_vcf import VCFGenomicPositionTable
 from .utils import build_genomic_position_table
 
 __all__ = [
+    "BigWigLine",
+    "BigWigTable",
     "Line",
     "LineBuffer",
     "TabixGenomicPositionTable",
