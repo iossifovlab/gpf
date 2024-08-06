92384fd6565aac3a27d105243c682ee2d5556de7
iordanivanov <iiordan.ivanov@gmail.com>
2024-08-06 13:06:50 +0300

    WIP various improvements and work on buffered table

--- dae/dae/genomic_resources/genomic_position_table/table_buffered.py
+++ dae/dae/genomic_resources/genomic_position_table/table_buffered.py
@@ -1,19 +1,23 @@
 from collections.abc import Generator
 
-from dae.genomic_resources.genomic_position_table.line import LineBase
+from dae.genomic_resources.genomic_position_table.line import Line, LineBase
 from dae.genomic_resources.genomic_position_table.table import (
     GenomicPositionTable,
 )
+from dae.genomic_resources.genomic_position_table.table_bigwig import BigWigTable
 from dae.utils.regions import Region
 
+
+import cProfile
+pr = cProfile.Profile()
 
 class BufferedTable(GenomicPositionTable):
     """Wrapper class to provide buffering for GenomicPositionTable."""
 
-    def __init__(self, table: GenomicPositionTable, fetch_size: int = 2000):
+    def __init__(self, table: BigWigTable, fetch_size: int = 2000):
         super().__init__(table.genomic_resource, table.definition)
         self._table = table
-        self._buffer: list[LineBase] = []
+        self._buffer: list[tuple[int, int, float]] = []
         self._buffer_region: Region = Region("?", -1, -1)
         self.fetch_size = fetch_size
 
@@ -39,25 +43,28 @@ class BufferedTable(GenomicPositionTable):
         Will fetch in ranges of length ``fetch_size`` starting from ``start``
         until either results are found or ``stop`` is reached.
         """
-        self._buffer.clear()
+        self._buffer = tuple()
         self._buffer_region = Region("?", -1, -1)
 
-        stop = min(self.get_chromosome_length(chrom), stop)
-        range_start = start
-        range_stop = min(stop, range_start + self.fetch_size)
-
-        while not (res := list(
-            self._table.get_records_in_region(chrom, range_start, range_stop))
-        ) and range_stop <= stop:
-            range_start = range_stop + 1
+        range_start = max(0, start - 1)
+        chromlen = self._table.chroms[chrom]
+        range_stop = min(chromlen, range_start + self.fetch_size)
+        stop = min(stop, chromlen)
+        
+        res = self._table.bw_file.intervals(chrom, range_start, range_stop)
+        while not res and range_stop < stop:
+            range_start = range_stop
             range_stop = range_start + self.fetch_size
+            range_stop = min(chromlen, range_stop)
+            print(range_start, range_stop)
+            res = self._table.bw_file.intervals(chrom, range_start, range_stop)
 
-        self._buffer = res
+        self._buffer = res or []
         if res:
             self._buffer_region = Region(
                 chrom,
-                self._buffer[0].pos_begin,
-                self._buffer[-1].pos_end)
+                self._buffer[0][0] + 1,
+                self._buffer[-1][1])
 
     @staticmethod
     def _binary_search(q_start: int, q_stop: int, start: int, stop: int) -> int:
@@ -79,7 +86,7 @@ class BufferedTable(GenomicPositionTable):
             idx = (r_bound + l_bound) // 2
             line = self._buffer[idx]
             res = BufferedTable._binary_search(
-                pos_begin, pos_end, line.pos_begin, line.pos_end)
+                pos_begin, pos_end, line[0], line[1])
             if res == 1:
                 l_bound = idx + 1
             elif res == -1 or res == 0:
@@ -104,16 +111,16 @@ class BufferedTable(GenomicPositionTable):
 
         pos_current = pos_begin
 
-        idx = self._find(chrom, pos_begin, pos_end)
+        idx = self._find(fchrom, pos_begin, pos_end)
         if idx == -1:
-            self._fill(chrom, pos_begin, pos_end)
-            idx = self._find(chrom, pos_begin, pos_end)
+            self._fill(fchrom, pos_begin, pos_end)
+            idx = self._find(fchrom, pos_begin, pos_end)
 
         while pos_current <= pos_end and self._buffer:
             line = self._buffer[idx]
-            yield line
-            pos_current = line.pos_end + 1
+            yield Line((chrom, *line))
+            pos_current = line[1] + 1
             idx += 1
             if idx == len(self._buffer):
-                self._fill(chrom, pos_current, pos_end)
+                self._fill(fchrom, pos_current, pos_end)
                 idx = 0
