06baedd1e9b2793dd2389ecb80d5369ce48af48e
iordanivanov <iiordan.ivanov@gmail.com>
2024-08-07 14:50:49 +0300

    bigwig: merged direct/buffered bigwig table impls

--- dae/dae/genomic_resources/genomic_position_table/table_bigwig.py
+++ dae/dae/genomic_resources/genomic_position_table/table_bigwig.py
@@ -7,12 +7,15 @@ from dae.genomic_resources.genomic_position_table.table import (
     GenomicPositionTable,
 )
 from dae.genomic_resources.repository import GenomicResource
+from dae.utils.regions import Region
 
 
 class BigWigTable(GenomicPositionTable):
     """bigWig format implementation of the genomic position table."""
 
-    BATCH_SIZE = 2000
+    DIRECT_FETCH_SIZE = 50
+    BUFFER_FETCH_SIZE = 500
+    USE_BUFFERED_THRESHOLD = 500
 
     def __init__(
         self,
@@ -20,43 +23,123 @@ class BigWigTable(GenomicPositionTable):
         table_definition: dict,
     ):
         super().__init__(genomic_resource, table_definition)
-        self.bw_file = None
+        self._bw_file = None
         self.chroms: dict[str, int] = {}
+        self._buffer: list[tuple[int, int, float]] = []
+        self._buffer_region: Region = Region("?", -1, -1)
+        self._last_pos = -1
 
     def open(self) -> BigWigTable:
-        self.bw_file = self.genomic_resource.open_bigwig_file(
+        self._bw_file = self.genomic_resource.open_bigwig_file(
             self.definition.filename)
-        if self.bw_file is None:
+        if self._bw_file is None:
             raise OSError
-        self.chroms = self.bw_file.chroms()
+        self.chroms = self._bw_file.chroms()
         self._set_core_column_keys()
         self._build_chrom_mapping()
         return self
 
     def close(self) -> None:
-        if self.bw_file is not None:
-            self.bw_file.close()
-        self.bw_file = None
+        if self._bw_file is not None:
+            self._bw_file.close()
+        self._bw_file = None
 
-    def _intervals(
+    def _fill(self, chrom: str, start: int, stop: int) -> None:
+        """
+        Attempts to fill the buffer with records for the given range.
+
+        Will fetch in ranges of length ``BUFFER_FETCH_SIZE`` starting from
+        ``start`` until either results are found or ``stop`` is reached.
+        """
+        assert self._bw_file is not None
+        self._buffer = ()
+        self._buffer_region = Region("?", -1, -1)
+
+        range_start = max(0, start - 1)
+        chromlen = self.chroms[chrom]
+        range_stop = min(chromlen, range_start + self.BUFFER_FETCH_SIZE)
+        stop = min(stop, chromlen)
+
+        res = self._bw_file.intervals(chrom, range_start, range_stop)
+        while not res and range_stop < stop:
+            range_start = range_stop
+            range_stop = range_start + self.BUFFER_FETCH_SIZE
+            range_stop = min(chromlen, range_stop)
+            res = self._bw_file.intervals(chrom, range_start, range_stop)
+
+        self._buffer = res or []
+        if res:
+            self._buffer_region = Region(
+                chrom,
+                self._buffer[0][0] + 1,
+                self._buffer[-1][1])
+
+    @staticmethod
+    def _binary_search(q_start: int, q_stop: int, start: int, stop: int) -> int:
+        if q_stop < start:
+            return -1
+        if q_start > stop:
+            return 1
+        return 0
+
+    def _find(self, chrom: str, pos_begin: int, pos_end: int) -> int:
+        query = Region(chrom, pos_begin, pos_end)
+        if not query.intersects(self._buffer_region):
+            return -1
+        idx: int = len(self._buffer) // 2
+        # do binary search on buffer, get idx
+        l_bound = 0
+        r_bound = len(self._buffer) - 1
+        while l_bound <= r_bound:
+            idx = (r_bound + l_bound) // 2
+            line = self._buffer[idx]
+            res = BigWigTable._binary_search(
+                pos_begin, pos_end, line[0], line[1])
+            if res == 1:
+                l_bound = idx + 1
+            elif res == -1 or res == 0:
+                r_bound = idx - 1
+        return idx
+
+    def _fetch_buffered(
         self, chrom: str, pos_begin: int, pos_end: int,
     ) -> Generator[tuple[int, int, float], None, None]:
-        assert self.bw_file is not None
+        pos_current = pos_begin
+
+        idx = self._find(chrom, pos_begin, pos_end)
+        if idx == -1:
+            self._fill(chrom, pos_begin, pos_end)
+            idx = self._find(chrom, pos_begin, pos_end)
+
+        while pos_current <= pos_end and self._buffer:
+            line = self._buffer[idx]
+            yield (line[0] + 1, line[1], line[2])
+            pos_current = line[1] + 1
+            idx += 1
+            if idx == len(self._buffer):
+                self._fill(chrom, pos_current, pos_end)
+                idx = 0
+
+    def _fetch_direct(
+        self, chrom: str, pos_begin: int, pos_end: int,
+    ) -> Generator[tuple[int, int, float], None, None]:
+        assert self._bw_file is not None
         chrom_len = self.chroms[chrom]
         pos_end = min(pos_end, chrom_len)
 
         start = max(0, pos_begin - 1)
-        stop = min(start + self.BATCH_SIZE, pos_end)
+        stop = min(start + self.DIRECT_FETCH_SIZE, pos_end)
         while start < pos_end:
-            intervals = self.bw_file.intervals(chrom, start, stop)
+            intervals = self._bw_file.intervals(chrom, start, stop)
             while intervals is None:
                 start = stop
-                stop = min(start + self.BATCH_SIZE, pos_end)
+                stop = min(start + self.DIRECT_FETCH_SIZE, pos_end)
                 if start >= pos_end:
                     return
-                intervals = self.bw_file.intervals(chrom, start, stop)
+                intervals = self._bw_file.intervals(chrom, start, stop)
             start = intervals[-1][1]
-            stop = min(start + self.BATCH_SIZE, pos_end)
+            stop = min(start + self.DIRECT_FETCH_SIZE, pos_end)
+
             for interval in intervals:
                 yield (interval[0] + 1, interval[1], interval[2])
 
@@ -66,9 +149,7 @@ class BigWigTable(GenomicPositionTable):
         pos_begin: int | None = None,
         pos_end: int | None = None,
     ) -> Generator[BigWigLine, None, None]:
-        assert self.bw_file is not None
         fchrom = self._map_file_chrom(chrom)
-
         if fchrom not in self.chroms:
             raise KeyError
         if pos_begin is None:
@@ -76,18 +157,24 @@ class BigWigTable(GenomicPositionTable):
         if pos_end is None:
             pos_end = self.chroms[fchrom]
 
-        for interval in self._intervals(fchrom, pos_begin, pos_end):
+        fetch_method = self._fetch_buffered \
+            if pos_begin - self._last_pos <= self.USE_BUFFERED_THRESHOLD \
+            else self._fetch_direct
+
+        self._last_pos = pos_begin
+
+        for interval in fetch_method(fchrom, pos_begin, pos_end):
             yield BigWigLine((chrom, *interval))
 
     def get_all_records(self) -> Generator[BigWigLine, None, None]:
-        assert self.bw_file is not None
+        assert self._bw_file is not None
         for chrom in self.get_chromosomes():
             yield from self.get_records_in_region(chrom)
 
     def get_chromosome_length(
         self, chrom: str, _step: int = 100_000_000,
     ) -> int:
-        assert self.bw_file is not None
+        assert self._bw_file is not None
         if chrom not in self.get_chromosomes():
             raise ValueError(
                 f"contig {chrom} not present in the table's contigs: "
@@ -106,5 +193,5 @@ class BigWigTable(GenomicPositionTable):
         return self.chroms[fchrom]
 
     def get_file_chromosomes(self) -> list[str]:
-        assert self.bw_file is not None
+        assert self._bw_file is not None
         return list(self.chroms.keys())
