352109fd681199f2bb441f17574dd7c0773ba19f
iordanivanov <iiordan.ivanov@gmail.com>
2024-08-13 16:39:37 +0300

    bigwig: another buffering duplication bugfix

--- dae/dae/genomic_resources/genomic_position_table/tests/test_bigwig.py
+++ dae/dae/genomic_resources/genomic_position_table/tests/test_bigwig.py
@@ -338,6 +338,65 @@ def test_no_repeating_in_buffered(
         assert vs[0].pos_end == 1003
 
 
+def test_no_repeating_in_buffered_alt_case(
+    tmp_path: pathlib.Path,
+) -> None:
+    root_path = tmp_path
+    setup_directories(
+        root_path,
+        {
+            "grr.yaml": textwrap.dedent(f"""
+                id: test_grr
+                type: directory
+                directory: {root_path!s}
+            """),
+            "test_score": {
+                "genomic_resource.yaml": textwrap.dedent("""
+                        type: position_score
+                        table:
+                            filename: data.bw
+                            format: bigWig
+                        scores:
+                        - id: score_one
+                          type: float
+                          index: 3
+                """),
+            },
+        },
+    )
+    data = textwrap.dedent("""
+        chr1   0         1000       0.01
+        chr1   1000      1001       0.02
+        chr1   1001      1002       0.03
+        chr1   1002      1003       0.04
+    """)
+    setup_bigwig(
+        root_path / "test_score" / "data.bw", data,
+        {"chr1": 5000},
+    )
+    grr = build_filesystem_test_repository(root_path)
+    assert grr is not None
+    res = grr.get_resource("test_score")
+    table_definition = res.get_config()["table"]
+    table_definition["buffer_fetch_size"] = 1  # important for bug reproduction!
+    with BigWigTable(res, table_definition) as bigwig_table:
+        vs = list(bigwig_table.get_records_in_region("chr1", 1, 1000))
+        assert len(vs) == 1
+        assert vs[0].chrom == "chr1"
+        assert vs[0].pos_begin == 1
+        assert vs[0].pos_end == 1000
+        vs = list(bigwig_table.get_records_in_region("chr1", 1001, 1001))
+        assert len(vs) == 1
+        assert vs[0].chrom == "chr1"
+        assert vs[0].pos_begin == 1001
+        assert vs[0].pos_end == 1001
+        vs = list(bigwig_table.get_records_in_region("chr1", 1002, 1002))
+        assert len(vs) == 1
+        assert vs[0].chrom == "chr1"
+        assert vs[0].pos_begin == 1002
+        assert vs[0].pos_end == 1002
+
+
 def test_bigwig_buffering_switching(
     tmp_path: pathlib.Path, mocker: pytest_mock.MockerFixture,
 ) -> None:
