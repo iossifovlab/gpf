b80f39dd19adbe4c1d9aa05ab9d57f1001eb0e6a
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2024-08-22 17:29:07 +0300

    Support for testing both integer and string region bins

--- dae/tests/integration/study_query_variants/test_queries_on_partitioning.py
+++ dae/tests/integration/study_query_variants/test_queries_on_partitioning.py
@@ -4,24 +4,26 @@ from typing import Any
 import pytest
 
 from dae.genotype_storage.genotype_storage import GenotypeStorage
-from dae.query_variants.sql.schema2.sql_query_builder import (
-    SqlQueryBuilder,
-)
 from dae.studies.study import GenotypeData
 from dae.testing import setup_pedigree, setup_vcf, vcf_study
 from dae.testing.t4c8_import import t4c8_gpf
 from dae.utils.regions import Region
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="module", params=["srb", "irb"])
 def study_2p(
-        tmp_path_factory: pytest.TempPathFactory,
-        genotype_storage: GenotypeStorage) -> GenotypeData:
-    root_path = tmp_path_factory.mktemp(
-        f"study_2p_{genotype_storage.storage_id}")
+    request: pytest.FixtureRequest,
+    tmp_path_factory: pytest.TempPathFactory,
+    genotype_storage: GenotypeStorage,
+) -> GenotypeData:
+    param = request.param
+    study_id = f"study_2p_{genotype_storage.storage_id}_{param}"
+
+    root_path = tmp_path_factory.mktemp(study_id)
+
     t4c8_instance = t4c8_gpf(root_path, genotype_storage)
     ped_path = setup_pedigree(
-        root_path / "study_2p" / "pedigree" / "in.ped",
+        root_path / study_id / "pedigree" / "in.ped",
         """
 familyId personId dadId momId sex status role
 f1.1     mom1     0     0     2   1      mom
@@ -32,7 +34,7 @@ f1.3     dad3     0     0     1   1      dad
 f1.3     ch3      dad3  mom3  2   2      prb
         """)
     vcf_path1 = setup_vcf(
-        root_path / "study_2p" / "vcf" / "in.vcf.gz",
+        root_path / study_id / "vcf" / "in.vcf.gz",
         """
 ##fileformat=VCFv4.2
 ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
@@ -48,11 +50,19 @@ chr1   119  .  A   G,C  .    .      .    GT     0/0  0/2  0/2 0/1  0/2  0/1
 chr1   122  .  A   C,AC .    .      .    GT     0/1  0/1  0/1 0/2  0/2  0/2
         """)
 
+    if request.param == "srb":
+        integer_region_bins = False
+    elif request.param == "irb":
+        integer_region_bins = True
+    else:
+        raise ValueError(f"unexpected param: {request.param}")
+
     project_config_update = {
         "partition_description": {
             "region_bin": {
                 "chromosomes": ["chr1"],
                 "region_length": 100,
+                "integer_region_bins": integer_region_bins,
             },
             "frequency_bin": {
                 "rare_boundary": 25.0,
@@ -73,184 +83,10 @@ chr1   122  .  A   C,AC .    .      .    GT     0/1  0/1  0/1 0/2  0/2  0/2
 
     return vcf_study(
         root_path,
-        "study_2p", ped_path, [vcf_path1],
+        study_id, ped_path, [vcf_path1],
         t4c8_instance,
         project_config_update=project_config_update,
     )
-
-
-@pytest.mark.skip("we have no abstract query builder interace")
-@pytest.mark.parametrize("params, coding_bin", [
-    ({"effect_types": ["missense"]}, 1),
-    ({"effect_types": ["synonymous"]}, 1),
-    ({"effect_types": ["intergenic"]}, None),
-    ({"effect_types": ["intergenic", "synonymous"]}, None),
-    ({}, None),
-])
-def test_coding_bin_heuristics_query(
-    params: dict[str, Any],
-    coding_bin: int | None,
-    query_builder: SqlQueryBuilder,
-) -> None:
-    assert query_builder.GENE_REGIONS_HEURISTIC_EXTEND == 0
-    queries = query_builder.build_summary_variants_query(**params)
-    assert queries is not None
-    assert len(queries) > 0
-    query = queries[0]
-
-    if coding_bin is None:
-        assert "coding_bin" not in query
-    else:
-        assert f"sa.coding_bin = {coding_bin}" in query
-
-
-@pytest.mark.skip("we have no abstract query builder interace")
-@pytest.mark.parametrize("params, region_bins", [
-    ({"regions": [Region("chr1", 2, 20)]}, ["chr1_0"]),
-    ({"regions": [Region("chr1", 2, 120)]}, ["chr1_0", "chr1_1"]),
-    ({"regions": [Region("chr1")]}, ["chr1_0", "chr1_1", "chr1_2"]),
-    ({"regions": [Region("chr1", 105)]}, ["chr1_1", "chr1_2"]),
-    ({"regions": [Region("chr1", None, 105)]}, ["chr1_0", "chr1_1"]),
-])
-def test_region_bin_heuristics_query(
-    params: dict[str, Any],
-    region_bins: list[str] | None,
-    query_builder: SqlQueryBuilder,
-) -> None:
-    assert query_builder.GENE_REGIONS_HEURISTIC_EXTEND == 0
-    queries = query_builder.build_summary_variants_query(**params)
-    assert queries is not None
-    assert len(queries) > 0
-    query = queries[0]
-
-    if region_bins is None:
-        assert "region_bin" not in query
-    else:
-        assert "sa.region_bin" in query
-        for region_bin in region_bins:
-            assert f"'{region_bin}'" in query
-
-
-@pytest.mark.skip("we have no abstract query builder interace")
-@pytest.mark.parametrize("params, frequency_bins", [
-    ({"ultra_rare": True}, "(0, 1)"),
-    ({"ultra_rare": False}, None),
-    ({}, None),
-    ({"frequency_filter": [("af_allele_freq", (None, 15.0))]}, "(0, 1, 2)"),
-    ({"frequency_filter": [("af_allele_freq", (None, 25.0))]}, "(0, 1, 2)"),
-    ({"frequency_filter": [("af_allele_freq", (None, 25.1))]}, None),
-])
-def test_frequency_bin_heuristics_query(
-    params: dict[str, Any],
-    frequency_bins: str | None,
-    query_builder: SqlQueryBuilder,
-) -> None:
-    assert query_builder.GENE_REGIONS_HEURISTIC_EXTEND == 0
-    queries = query_builder.build_summary_variants_query(**params)
-    assert queries is not None
-    assert len(queries) > 0
-    query = queries[0]
-
-    if frequency_bins is None:
-        assert "frequency_bin" not in query
-    else:
-        assert "sa.frequency_bin" in query
-        assert f"sa.frequency_bin IN {frequency_bins}" in query
-
-
-@pytest.mark.skip("we have no abstract query builder interace")
-@pytest.mark.parametrize("params, coding_bin", [
-    ({"effect_types": ["missense"]}, 1),
-    ({"effect_types": ["synonymous"]}, 1),
-    ({"effect_types": ["intergenic"]}, None),
-    ({"effect_types": ["intergenic", "synonymous"]}, None),
-    ({}, None),
-])
-def test_coding_bin_heuristics_family_query(
-    params: dict[str, Any],
-    coding_bin: int | None,
-    query_builder: SqlQueryBuilder,
-) -> None:
-    assert query_builder.GENE_REGIONS_HEURISTIC_EXTEND == 0
-    queries = query_builder.build_family_variants_query(**params)
-    assert queries is not None
-    assert len(queries) > 0
-    query = queries[0]
-
-    if coding_bin is None:
-        assert "coding_bin" not in query
-    else:
-        assert f"sa.coding_bin = {coding_bin}" in query
-        assert f"fa.coding_bin = {coding_bin}" in query
-
-
-@pytest.mark.skip("we have no abstract query builder interace")
-@pytest.mark.parametrize("params, count, region_bins", [
-    ({"regions": [Region("chr1", 2, 20)]}, 1, ["chr1_0"]),
-    ({"regions": [Region("chr1", 2, 120)]}, 1, ["chr1_0", "chr1_1"]),
-    ({"regions": [Region("chr1")]}, 1, ["chr1_0", "chr1_1", "chr1_2"]),
-    ({"regions": [Region("chr1", 105)]}, 1, ["chr1_1", "chr1_2"]),
-    ({"regions": [Region("chr1", None, 105)]}, 1, ["chr1_0", "chr1_1"]),
-    ({"regions": None}, 3, ["chr1_0"]),
-])
-def test_region_bin_heuristics_family_query(
-    params: dict[str, Any],
-    count: int,
-    region_bins: list[str],
-    query_builder: SqlQueryBuilder,
-) -> None:
-    assert query_builder.GENE_REGIONS_HEURISTIC_EXTEND == 0
-    queries = query_builder.build_family_variants_query(**params)
-    assert queries is not None
-    assert len(queries) == count
-    query = queries[0]
-
-    assert "sa.region_bin" in query
-    assert "fa.region_bin" in query
-    for region_bin in region_bins:
-        assert f"'{region_bin}'" in query
-
-
-@pytest.mark.skip("we have no abstract query builder interace")
-def test_region_bin_heuristics_batched_query(
-    query_builder: SqlQueryBuilder,
-) -> None:
-
-    queries = query_builder.build_family_variants_query()
-
-    assert queries is not None
-    assert len(queries) == 3
-    for query in queries:
-        assert "sa.region_bin" in query
-        assert "fa.region_bin" in query
-
-
-@pytest.mark.skip("we have no abstract query builder interace")
-@pytest.mark.parametrize("params, frequency_bins", [
-    ({"ultra_rare": True}, "(0, 1)"),
-    ({"ultra_rare": False}, None),
-    ({}, None),
-    ({"frequency_filter": [("af_allele_freq", (None, 15.0))]}, "(0, 1, 2)"),
-    ({"frequency_filter": [("af_allele_freq", (None, 25.0))]}, "(0, 1, 2)"),
-    ({"frequency_filter": [("af_allele_freq", (None, 25.1))]}, None),
-])
-def test_frequency_bin_heuristics_family_query(
-    params: dict[str, Any],
-    frequency_bins: str | None,
-    query_builder: SqlQueryBuilder,
-) -> None:
-    assert query_builder.GENE_REGIONS_HEURISTIC_EXTEND == 0
-    queries = query_builder.build_family_variants_query(**params)
-    assert queries is not None
-    assert len(queries) > 0
-    query = queries[0]
-
-    if frequency_bins is None:
-        assert "frequency_bin" not in query
-    else:
-        assert "sa.frequency_bin" in query
-        assert "fa.frequency_bin" in query
-        assert f"fa.frequency_bin IN {frequency_bins}" in query
 
 
 def test_variants_simple(study_2p: GenotypeData) -> None:
@@ -375,43 +211,6 @@ def test_query_family_variants_by_inheritance(
         **params,
     ))
     assert len(fvs) == count
-
-
-@pytest.mark.skip("we have no abstract query builder interace")
-@pytest.mark.parametrize(
-    "params,expected", [
-        (
-            {
-                "roles": "( prb and not sib ) or ( prb and sib )",
-                "inheritance": [
-                    "not possible_denovo and not possible_omission",
-                    "any(denovo,mendelian,missing,omission)",
-                ],
-                "ultra_rare": True,
-            },
-            ["0", "1"],
-        ),
-        (
-            {
-                "roles": "( prb and not sib ) or ( prb and sib )",
-                "inheritance": [
-                    "not possible_denovo and not possible_omission",
-                    "any(denovo,mendelian,missing,omission)",
-                ],
-                "ultra_rare": False,
-                "frequency_filter": [("af_allele_freq", (None, 1.0))],
-            },
-            ["0", "1", "2"],
-        ),
-    ],
-)
-def test_calc_frequency_bin_heuristics(
-    params: dict[str, Any],
-    expected: list[str],
-    query_builder: SqlQueryBuilder,
-) -> None:
-    frequency_bins = query_builder.calc_frequency_bins(**params)
-    assert frequency_bins == expected
 
 
 @pytest.mark.parametrize("params, count", [
