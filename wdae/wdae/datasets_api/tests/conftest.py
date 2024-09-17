7f25a8e2b031261b5f1cae0ccc9d91a77247e0e4
iordanivanov <iiordan.ivanov@gmail.com>
2024-09-17 15:16:47 +0300

    wdae tests: update dataset_api permissions tests

---
+++ wdae/wdae/datasets_api/tests/conftest.py
@@ -0,0 +1,42 @@
+# pylint: disable=W0621,C0114,C0116,W0212,W0613
+import pytest
+import pytest_mock
+from conftest import setup_wgpf_intance
+from gpf_instance.gpf_instance import WGPFInstance, reload_datasets
+
+from dae.testing.import_helpers import setup_dataset_config
+
+
+@pytest.fixture(scope="module")
+def custom_wgpf_module(
+    tmp_path_factory: pytest.TempPathFactory,
+) -> WGPFInstance:
+    root_path = tmp_path_factory.mktemp("t4c8_wgpf_module")
+    gpf_instance = setup_wgpf_intance(root_path)
+    setup_dataset_config(gpf_instance, "dataset_1", ["t4c8_study_1"])
+    setup_dataset_config(gpf_instance, "dataset_2", ["t4c8_study_2"])
+    setup_dataset_config(gpf_instance, "omni_dataset", ["dataset_1",
+                                                        "dataset_2"])
+    return gpf_instance
+
+
+@pytest.fixture()
+def custom_wgpf(
+    custom_wgpf_module: WGPFInstance,
+    db: None,  # noqa: ARG001
+    mocker: pytest_mock.MockFixture,
+) -> WGPFInstance:
+    reload_datasets(custom_wgpf_module)
+    mocker.patch(
+        "gpf_instance.gpf_instance.get_wgpf_instance",
+        return_value=custom_wgpf_module,
+    )
+    mocker.patch(
+        "datasets_api.permissions.get_wgpf_instance",
+        return_value=custom_wgpf_module,
+    )
+    mocker.patch(
+        "query_base.query_base.get_wgpf_instance",
+        return_value=custom_wgpf_module,
+    )
+    return custom_wgpf_module
