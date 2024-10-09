ccb17e8f1f53a1b9616e02bfe2aeb4dcfad144c2
iordanivanov <iiordan.ivanov@gmail.com>
2024-10-09 17:04:27 +0300

    annotation: refactor annotate_schema2_parquet tool

--- dae/dae/annotation/annotate_schema2_parquet.py
+++ dae/dae/annotation/annotate_schema2_parquet.py
@@ -1,35 +1,21 @@
 import argparse
 import os
-import pathlib
-from datetime import datetime
-from glob import glob
-
-import yaml
 
 from dae.annotation.annotate_utils import AnnotationTool
-from dae.annotation.annotation_config import RawAnnotatorsConfig
-from dae.annotation.annotation_pipeline import (
-    AnnotationPipeline,
-    ReannotationPipeline,
-)
 from dae.annotation.context import CLIAnnotationContext
-from dae.genomic_resources.cli import VerbosityConfiguration
-from dae.genomic_resources.reference_genome import ReferenceGenome
-from dae.gpf_instance.gpf_instance import GPFInstance
-from dae.parquet.helpers import merge_parquets
-from dae.parquet.parquet_writer import (
-    append_meta_to_parquet,
-    merge_variants_parquets,
-    serialize_summary_schema,
+from dae.annotation.parquet import (
+    backup_schema2_study,
+    produce_schema2_annotation_tasks,
+    produce_schema2_merging_tasks,
+    symlink_pedigree_and_family_variants,
+    write_new_meta,
 )
-from dae.parquet.partition_descriptor import PartitionDescriptor
-from dae.parquet.schema2.parquet_io import VariantsParquetWriter
+from dae.genomic_resources.cli import VerbosityConfiguration
+from dae.gpf_instance.gpf_instance import GPFInstance
 from dae.schema2_storage.schema2_import_storage import (
-    Schema2DatasetLayout,
     create_schema2_dataset_layout,
 )
 from dae.task_graph.cli_tools import TaskGraphCli
-from dae.utils.regions import Region
 from dae.variants_loaders.parquet.loader import ParquetLoader
 
 
@@ -70,246 +56,50 @@ class AnnotateSchema2ParquetTool(AnnotationTool):
         VerbosityConfiguration.set_arguments(parser)
         return parser
 
-    @staticmethod
-    def annotate(
-        input_layout: Schema2DatasetLayout,
-        output_dir: str,
-        pipeline_config: RawAnnotatorsConfig,
-        region: str,
-        grr_definition: dict,
-        bucket_idx: int,
-        allow_repeated_attributes: bool,  # noqa: FBT001
-    ) -> None:
-        """Run annotation over a given directory of Parquet files."""
-        loader = ParquetLoader(input_layout)
-        pipeline = AnnotateSchema2ParquetTool._produce_annotation_pipeline(
-            pipeline_config,
-            (loader.meta["annotation_pipeline"]
-             if loader.has_annotation else None),
-            grr_definition,
-            allow_repeated_attributes=allow_repeated_attributes,
-        )
-
-        writer = VariantsParquetWriter(
-            output_dir, pipeline.get_attributes(),
-            loader.partition_descriptor,
-            bucket_index=bucket_idx,
-        )
-
-        if isinstance(pipeline, ReannotationPipeline):
-            internal_attributes = [
-                attribute.name
-                for annotator in (pipeline.annotators_new
-                                  | pipeline.annotators_rerun)
-                for attribute in annotator.attributes
-                if attribute.internal
-            ]
-        else:
-            internal_attributes = [
-                attribute.name
-                for attribute in pipeline.get_attributes()
-                if attribute.internal
-            ]
-
-        region_obj = Region.from_str(region)
-        for variant in loader.fetch_summary_variants(region=region_obj):
-            for allele in variant.alt_alleles:
-                if isinstance(pipeline, ReannotationPipeline):
-                    result = pipeline.annotate_summary_allele(allele)
-                    for attr in pipeline.attributes_deleted:
-                        del allele.attributes[attr]
-                else:
-                    result = pipeline.annotate(allele.get_annotatable())
-                for attr in internal_attributes:
-                    del result[attr]
-                allele.update_attributes(result)
-            writer.write_summary_variant(variant)
-
-        writer.close()
-
-    @staticmethod
-    def _write_meta(
-        layout: Schema2DatasetLayout,
-        loader: ParquetLoader,
-        pipeline_raw_config: RawAnnotatorsConfig,
-        pipeline: AnnotationPipeline,
-    ) -> None:
-        """Write metadata to the output Parquet dataset."""
-
-        # Write metadata
-        meta_keys = [
-            "annotation_pipeline",
-            "summary_schema",
-        ]
-        meta_values = [
-            yaml.dump(pipeline_raw_config, sort_keys=False),
-            serialize_summary_schema(
-                pipeline.get_attributes(),
-                loader.partition_descriptor),
-        ]
-        for k, v in loader.meta.items():
-            if k in {
-                    "annotation_pipeline",
-                    "summary_schema"}:
-                continue  # ignore old annotation
-            meta_keys.append(k)
-            meta_values.append(str(v))
-        append_meta_to_parquet(layout.meta, meta_keys, meta_values)
-
-    def relabel_old_data(self, directory: str) -> tuple[str, str]:
-        """
-        Backup old meta and summary data, create new directories and link them.
-        """
-        loader = ParquetLoader.load_from_dir(directory)
-        assert loader.layout.summary is not None
-        meta_path = pathlib.Path(loader.layout.meta)
-        summary_path = pathlib.Path(loader.layout.summary)
-
-        date = datetime.today().strftime("%Y%m%d")
-
-        bak_meta_name = f"{meta_path.stem}_{date}"
-        bak_summary_name = f"summary_{date}"
-
-        if meta_path.with_stem(bak_meta_name).exists():
-            counts = len(list(meta_path.parent.glob(f"{bak_meta_name}*")))
-            bak_meta_name = f"{bak_meta_name}-{counts}"
-
-        if summary_path.with_name(bak_summary_name).exists():
-            counts = len(list(summary_path.parent.glob(f"{bak_summary_name}*")))
-            bak_summary_name = f"{bak_summary_name}-{counts}"
-
-        return str(meta_path.rename(meta_path.with_stem(bak_meta_name))), \
-               str(summary_path.rename(summary_path.with_name(bak_summary_name)))
-
     def work(self) -> None:
         input_dir = os.path.abspath(self.args.input)
 
         if self.args.in_place:
             output_dir = input_dir
+            input_layout = backup_schema2_study(input_dir)
+            loader = ParquetLoader(input_layout)
         else:
             output_dir = os.path.abspath(self.args.output)
-
-        loader = ParquetLoader.load_from_dir(input_dir)
-
-        if self.args.in_place:
-            input_meta, input_summary = self.relabel_old_data(input_dir)
-            input_layout = Schema2DatasetLayout(
-                loader.layout.study,
-                loader.layout.pedigree,
-                input_summary,
-                loader.layout.family,
-                input_meta,
-                loader.layout.base_dir,
-            )
-            loader = ParquetLoader(input_layout)
+            loader = ParquetLoader.load_from_dir(input_dir)
 
         if not os.path.exists(output_dir):
             os.mkdir(output_dir)
-        layout = create_schema2_dataset_layout(output_dir)
+        output_layout = create_schema2_dataset_layout(output_dir)
 
         if loader.layout.summary is None:
             raise ValueError("Invalid summary dir in input layout!")
         if loader.layout.family is None:
             raise ValueError("Invalid family dir in input layout!")
-        if layout.summary is None:
+        if output_layout.summary is None:
             raise ValueError("Invalid summary dir in output layout!")
-        if layout.family is None:
+        if output_layout.family is None:
             raise ValueError("Invalid family dir in output layout!")
 
-        pipeline = AnnotateSchema2ParquetTool._produce_annotation_pipeline(
-            self.pipeline.raw,  # type: ignore
-            (loader.meta["annotation_pipeline"]
-             if loader.has_annotation else None),
-            self.grr.definition,
-            allow_repeated_attributes=self.args.allow_repeated_attributes,
-        )
-
-        self._write_meta(
-            layout, loader,
-            self.pipeline.raw,  # type: ignore
-            pipeline,
-        )
+        write_new_meta(loader, self.pipeline, output_layout)
 
         if not self.args.in_place:
-            # Symlink pedigree and family variants directories
-            os.symlink(
-                os.path.split(loader.layout.pedigree)[0],
-                os.path.split(layout.pedigree)[0],
-                target_is_directory=True,
-            )
-            os.symlink(
-                loader.layout.family,
-                layout.family,
-                target_is_directory=True,
-            )
+            symlink_pedigree_and_family_variants(loader.layout, output_layout)
 
-        if "reference_genome" not in loader.meta:
-            raise ValueError("No reference genome found in study metadata!")
-        genome = ReferenceGenome(
-            self.grr.get_resource(loader.meta["reference_genome"]))
-
-        contig_lens = {}
-        contigs = loader.contigs or genome.chromosomes
-        for contig in contigs:
-            contig_lens[contig] = genome.get_chrom_length(contig)
-
-        regions = [
-            f"{contig}:{start}-{start + self.args.region_size}"
-            for contig, length in contig_lens.items()
-            for start in range(1, length, self.args.region_size)
-        ]
-
-        annotation_tasks = []
-        for idx, region in enumerate(regions):
-            annotation_tasks.append(self.task_graph.create_task(
-                f"part_{region}",
-                AnnotateSchema2ParquetTool.annotate,
-                [loader.layout, output_dir,
-                 self.pipeline.raw, region, self.grr.definition,
-                 idx, self.args.allow_repeated_attributes],
-                [],
-            ))
-
-        if loader.partitioned:
-            def merge_partitioned(
-                summary_dir: str, partition_dir: str,
-                partition_descriptor: PartitionDescriptor,
-            ) -> None:
-                partitions = []
-                for partition in partition_dir.split("/"):
-                    key, value = partition.split("=", maxsplit=1)
-                    partitions.append((key, value))
-                output_dir = os.path.join(summary_dir, partition_dir)
-                merge_variants_parquets(
-                    partition_descriptor, output_dir, partitions)
-
-            to_join = [
-                os.path.relpath(dirpath, loader.layout.summary)
-                for dirpath, subdirs, _ in os.walk(loader.layout.summary)
-                if not subdirs
-            ]
-
-            for path in to_join:
-                self.task_graph.create_task(
-                    f"merge_{path}",
-                    merge_partitioned,
-                    [layout.summary, path, loader.partition_descriptor],
-                    annotation_tasks,
-                )
-        else:
-            def merge(output_dir: str, output_filename: str) -> None:
-                to_merge = glob(os.path.join(output_dir, "*.parquet"))
-                output_path = os.path.join(output_dir, output_filename)
-                merge_parquets(to_merge, output_path)
-
-            out_path = next(loader.get_summary_pq_filepaths()).pop()
-
-            self.task_graph.create_task(
-                "merge", merge,
-                [os.path.join(output_dir, "summary"),
-                 os.path.basename(out_path)],
-                annotation_tasks,
-            )
+        annotation_tasks = produce_schema2_annotation_tasks(
+            self.task_graph,
+            loader,
+            output_dir,
+            self.pipeline.raw,
+            self.grr,
+            self.args.region_size,
+            self.args.allow_repeated_attributes,
+        )
+        produce_schema2_merging_tasks(
+            self.task_graph,
+            annotation_tasks,
+            loader,
+            output_layout,
+        )
 
 
 def cli(
---
+++ dae/dae/annotation/parquet.py
@@ -0,0 +1,265 @@
+import os
+import pathlib
+from datetime import datetime
+from glob import glob
+
+import yaml
+
+from dae.annotation.annotate_utils import AnnotationTool
+from dae.annotation.annotation_config import (
+    RawAnnotatorsConfig,
+    RawPipelineConfig,
+)
+from dae.annotation.annotation_pipeline import (
+    AnnotationPipeline,
+    ReannotationPipeline,
+)
+from dae.genomic_resources.reference_genome import ReferenceGenome
+from dae.genomic_resources.repository import GenomicResourceRepo
+from dae.parquet.helpers import merge_parquets
+from dae.parquet.parquet_writer import (
+    append_meta_to_parquet,
+    merge_variants_parquets,
+    serialize_summary_schema,
+)
+from dae.parquet.partition_descriptor import PartitionDescriptor
+from dae.parquet.schema2.parquet_io import VariantsParquetWriter
+from dae.schema2_storage.schema2_layout import Schema2DatasetLayout
+from dae.task_graph.graph import Task, TaskGraph
+from dae.utils.regions import Region
+from dae.variants_loaders.parquet.loader import ParquetLoader
+
+
+def backup_schema2_study(directory: str) -> Schema2DatasetLayout:
+    """
+    Backup current meta and summary data for a parquet study.
+
+    Renames the meta Parquet file and summary variants directory by
+    attaching a suffix with the current date, then returns a corrected
+    layout using the newly-renamed paths. This clears the way for then new
+    'meta' and 'summary' that will be produced when reannotating a Parquet
+    study in place.
+    """
+    loader = ParquetLoader.load_from_dir(directory)
+    assert loader.layout.summary is not None
+    meta_path = pathlib.Path(loader.layout.meta)
+    summary_path = pathlib.Path(loader.layout.summary)
+
+    date = datetime.today().strftime("%Y%m%d")
+
+    bak_meta_name = f"{meta_path.stem}_{date}"
+    bak_summary_name = f"summary_{date}"
+
+    if meta_path.with_stem(bak_meta_name).exists():
+        counts = len(list(meta_path.parent.glob(f"{bak_meta_name}*")))
+        bak_meta_name = f"{bak_meta_name}-{counts}"
+
+    if summary_path.with_name(bak_summary_name).exists():
+        counts = len(list(summary_path.parent.glob(f"{bak_summary_name}*")))
+        bak_summary_name = f"{bak_summary_name}-{counts}"
+
+    return Schema2DatasetLayout(
+        loader.layout.study,
+        loader.layout.pedigree,
+        str(summary_path.rename(summary_path.with_name(bak_summary_name))),
+        loader.layout.family,
+        str(meta_path.rename(meta_path.with_stem(bak_meta_name))),
+        loader.layout.base_dir,
+    )
+
+
+def annotate_parquet(
+    input_layout: Schema2DatasetLayout,
+    output_dir: str,
+    pipeline_config: RawAnnotatorsConfig,
+    region: str,
+    grr_definition: dict,
+    bucket_idx: int,
+    allow_repeated_attributes: bool,  # noqa: FBT001
+) -> None:
+    """Run annotation over a given directory of Parquet files."""
+    loader = ParquetLoader(input_layout)
+    pipeline = AnnotationTool.produce_annotation_pipeline(
+        pipeline_config,
+        (loader.meta["annotation_pipeline"] if loader.has_annotation else None),
+        grr_definition,
+        allow_repeated_attributes=allow_repeated_attributes,
+    )
+
+    writer = VariantsParquetWriter(
+        output_dir, pipeline.get_attributes(),
+        loader.partition_descriptor,
+        bucket_index=bucket_idx,
+    )
+
+    if isinstance(pipeline, ReannotationPipeline):
+        internal_attributes = [
+            attribute.name
+            for annotator in (pipeline.annotators_new
+                                | pipeline.annotators_rerun)
+            for attribute in annotator.attributes
+            if attribute.internal
+        ]
+    else:
+        internal_attributes = [
+            attribute.name
+            for attribute in pipeline.get_attributes()
+            if attribute.internal
+        ]
+
+    region_obj = Region.from_str(region)
+    for variant in loader.fetch_summary_variants(region=region_obj):
+        for allele in variant.alt_alleles:
+            if isinstance(pipeline, ReannotationPipeline):
+                result = pipeline.annotate_summary_allele(allele)
+                for attr in pipeline.attributes_deleted:
+                    del allele.attributes[attr]
+            else:
+                result = pipeline.annotate(allele.get_annotatable())
+            for attr in internal_attributes:
+                del result[attr]
+            allele.update_attributes(result)
+        writer.write_summary_variant(variant)
+
+    writer.close()
+
+
+def produce_schema2_annotation_tasks(
+    task_graph: TaskGraph,
+    loader: ParquetLoader,
+    output_dir: str,
+    raw_pipeline: RawPipelineConfig,
+    grr: GenomicResourceRepo,
+    region_size: int,
+    allow_repeated_attributes: bool,  # noqa: FBT001
+) -> list[Task]:
+    """Produce TaskGraph tasks for Parquet file annotation."""
+
+    if "reference_genome" not in loader.meta:
+        raise ValueError("No reference genome found in study metadata!")
+    genome = ReferenceGenome(
+        grr.get_resource(loader.meta["reference_genome"]))
+
+    contig_lens = {}
+    contigs = loader.contigs or genome.chromosomes
+    for contig in contigs:
+        contig_lens[contig] = genome.get_chrom_length(contig)
+
+    regions = [
+        f"{contig}:{start}-{start + region_size}"
+        for contig, length in contig_lens.items()
+        for start in range(1, length, region_size)
+    ]
+
+    tasks = []
+    for idx, region in enumerate(regions):
+        tasks.append(task_graph.create_task(
+            f"part_{region}",
+            annotate_parquet,
+            [loader.layout, output_dir,
+             raw_pipeline, region, grr.definition,
+             idx, allow_repeated_attributes],
+            [],
+        ))
+    return tasks
+
+
+def produce_schema2_merging_tasks(
+    task_graph: TaskGraph,
+    annotation_tasks: list[Task],
+    loader: ParquetLoader,
+    output_layout: Schema2DatasetLayout,
+) -> list[Task]:
+    """Produce TaskGraph tasks for Parquet file merging."""
+
+    if loader.layout.summary is None:
+        raise ValueError("No summary variants to merge!")
+
+    if loader.partitioned:
+        to_join = [
+            os.path.relpath(dirpath, loader.layout.summary)
+            for dirpath, subdirs, _ in os.walk(loader.layout.summary)
+            if not subdirs
+        ]
+        tasks = [
+            task_graph.create_task(
+                f"merge_{path}",
+                merge_partitioned,
+                [output_layout.summary,
+                 path,
+                 loader.partition_descriptor],
+                annotation_tasks,
+            ) for path in to_join
+        ]
+    else:
+        out_path = next(loader.get_summary_pq_filepaths()).pop()
+        tasks = [
+            task_graph.create_task(
+                "merge",
+                merge_non_partitioned,
+                [output_layout.summary,
+                 os.path.basename(out_path)],
+                annotation_tasks,
+            ),
+        ]
+    return tasks
+
+
+def symlink_pedigree_and_family_variants(
+    src_layout: Schema2DatasetLayout,
+    dest_layout: Schema2DatasetLayout,
+) -> None:
+    """
+    Mirror pedigree and family variants data using symlinks.
+    """
+    os.symlink(
+        pathlib.Path(src_layout.pedigree).parent,
+        pathlib.Path(dest_layout.pedigree).parent,
+        target_is_directory=True,
+    )
+    if src_layout.family is not None and dest_layout.family is not None:
+        os.symlink(
+            src_layout.family,
+            dest_layout.family,
+            target_is_directory=True,
+        )
+
+
+def write_new_meta(
+    loader: ParquetLoader,
+    pipeline: AnnotationPipeline,
+    output_layout: Schema2DatasetLayout,
+) -> None:
+    """Produce and write new metadata to the output Parquet dataset."""
+    meta_keys = ["annotation_pipeline", "summary_schema"]
+    meta_values = [
+        yaml.dump(pipeline.raw, sort_keys=False),
+        serialize_summary_schema(pipeline.get_attributes(),
+                                 loader.partition_descriptor),
+    ]
+    for k, v in loader.meta.items():
+        if k in {"annotation_pipeline", "summary_schema"}:
+            continue  # ignore old annotation
+        meta_keys.append(k)
+        meta_values.append(str(v))
+    append_meta_to_parquet(output_layout.meta, meta_keys, meta_values)
+
+
+def merge_partitioned(
+    summary_dir: str,
+    partition_dir: str,
+    partition_descriptor: PartitionDescriptor,
+) -> None:
+    """Helper method to merge Parquet files in partitioned studies."""
+    partitions = []
+    for partition in partition_dir.split("/"):
+        key, value = partition.split("=", maxsplit=1)
+        partitions.append((key, value))
+    output_dir = os.path.join(summary_dir, partition_dir)
+    merge_variants_parquets(partition_descriptor, output_dir, partitions)
+
+
+def merge_non_partitioned(output_dir: str, output_filename: str) -> None:
+    to_merge = glob(os.path.join(output_dir, "*.parquet"))
+    output_path = os.path.join(output_dir, output_filename)
+    merge_parquets(to_merge, output_path)
