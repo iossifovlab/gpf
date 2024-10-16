12fd812afc651c0264782ef9ca6d2938a0f54ae6
IlinaGergova <ili.gergova@gmail.com>
2024-10-16 14:26:44 +0300

    add about component unit tests
    * test writing and getting description for About page

--- gpfjs/src/app/about/about.component.spec.ts
+++ gpfjs/src/app/about/about.component.spec.ts
@@ -1,14 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { AboutComponent } from './about.component';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { ConfigService } from 'app/config/config.service';
+import { Observable, of } from 'rxjs';
+import { InstanceService } from 'app/instance.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
+
+class InstanceServiceMock {
+  public writeAboutDescription(description: string): Observable<object> {
+    return of({
+      'content': description
+    });
+  }
+
+  public getAboutDescription(): Observable<object> {
+   return of({
+    'content': '# Introduction\n[GPF](https://iossifovlab.com/gpf/) (Genotypes and Phenotypes in Families) is an open-source platform that manages genotypes and phenotypes derived from collections of families.'
+    });
+  }
+}
 describe('AboutComponent', () => {
   let component: AboutComponent;
   let fixture: ComponentFixture<AboutComponent>;
+  let instanceServiceMock = new InstanceServiceMock();
 
   beforeEach(async() => {
     await TestBed.configureTestingModule({
-      declarations: [AboutComponent]
+      declarations: [AboutComponent],
+      providers: [
+        HttpClient,
+        HttpHandler,
+        ConfigService,
+        { provide: InstanceService, useValue: instanceServiceMock},
+      ],
+      schemas: [NO_ERRORS_SCHEMA],
     }).compileComponents();
 
     fixture = TestBed.createComponent(AboutComponent);
@@ -16,7 +42,18 @@ describe('AboutComponent', () => {
     fixture.detectChanges();
   });
 
-  it.skip('should create', () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get about description', () => {
+    expect(component.aboutDescription).toBe('# Introduction\n[GPF](https://iossifovlab.com/gpf/) (Genotypes and Phenotypes in Families) is an open-source platform that manages genotypes and phenotypes derived from collections of families.');
+  });
+
+  it('should write about description', () => {
+    jest.spyOn(instanceServiceMock, 'getAboutDescription').mockReturnValueOnce(of({'content': 'new description'}));
+
+    component.writeDescription('new description');
+    expect(component.aboutDescription).toBe('new description');
   });
 });
