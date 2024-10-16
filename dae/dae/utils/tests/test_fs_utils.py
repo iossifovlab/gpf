d8fb94b8321c99fc74aa52cd79bdcf57a601e0b2
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2024-10-16 14:09:06 +0300

    Mop-up ruff and mypy errors

--- dae/dae/utils/tests/test_fs_utils.py
+++ dae/dae/utils/tests/test_fs_utils.py
@@ -2,6 +2,7 @@
 import os
 
 import pytest
+import pytest_mock
 
 from dae.utils import fs_utils
 
@@ -13,7 +14,10 @@ from dae.utils import fs_utils
     (["/abc/", "s3://server/", "path"], "s3://server/path"),
     (["s3://server/", "/abc/", "path"], "/abc/path"),
 ])
-def test_join(segments, expected):
+def test_join(
+    segments: str,
+    expected: str,
+) -> None:
     assert fs_utils.join(*segments) == expected
 
 
@@ -28,7 +32,10 @@ def test_join(segments, expected):
     ("/", "/"),
     ("s3://", "s3://"),
 ])
-def test_containing_path(url, expected):
+def test_containing_path(
+    url: str,
+    expected: str | None,
+) -> None:
     expected = expected if expected is not None else os.getcwd()
     assert fs_utils.containing_path(url) == expected
 
@@ -38,17 +45,24 @@ def test_containing_path(url, expected):
     ("test", {"test": True, "test.csi": True}, "test.csi"),
     ("test", {"test": True}, None),
 ])
-def test_tabix_index_filename(mocker, filename, exists_mockery, expected):
+def test_tabix_index_filename(
+    mocker: pytest_mock.MockFixture,
+    filename: str,
+    exists_mockery: dict[str, bool],
+    expected: str | None,
+) -> None:
     mocker.patch(
         "dae.utils.fs_utils.exists",
         lambda tf: exists_mockery.get(tf, False))
     assert fs_utils.tabix_index_filename(filename) == expected
 
 
-def test_tabix_index_filename_file_not_found(mocker):
+def test_tabix_index_filename_file_not_found(
+    mocker: pytest_mock.MockFixture,
+) -> None:
     mocker.patch(
         "dae.utils.fs_utils.exists",
-        lambda tf: False)
+        return_value=False)
     with pytest.raises(IOError, match="tabix file 'test' not found"):
         fs_utils.tabix_index_filename("test")
 
@@ -64,13 +78,19 @@ def test_tabix_index_filename_file_not_found(mocker):
     ("/", "/"),
     ("s3://", "s3://"),
 ])
-def test_abspath(url, expected, mocker):
+def test_abspath(
+    url: str,
+    expected: str,
+    mocker: pytest_mock.MockFixture,
+) -> None:
     mocker.patch("os.getcwd", return_value="/abs/path")
     res = fs_utils.abspath(url)
     assert res == expected
 
 
-def test_copy_folder(tmp_path_factory):
+def test_copy_folder(
+    tmp_path_factory: pytest.TempPathFactory,
+) -> None:
     dest_path = tmp_path_factory.mktemp("dest")
 
     src_path = tmp_path_factory.mktemp("src")
