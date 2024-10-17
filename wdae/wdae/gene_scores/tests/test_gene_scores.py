0cda7f684c3de417c8c3f7645b4c03d6d25f0b85
joankosev <joankosev@gmail.com>
2024-10-17 13:40:39 +0300

    wdae: refactor gene scores api unit tests

--- wdae/wdae/gene_scores/tests/test_gene_scores.py
+++ wdae/wdae/gene_scores/tests/test_gene_scores.py
@@ -1,43 +1,49 @@
 # pylint: disable=W0621,C0114,C0116,W0212,W0613
 import pytest
-
-from dae.gene_scores.gene_scores import GeneScoresDb
-
-pytestmark = pytest.mark.usefixtures(
-    "wdae_gpf_instance", "dae_calc_gene_sets")
+from gpf_instance.gpf_instance import WGPFInstance
 
 
-def test_scores_created(gene_scores_db: GeneScoresDb) -> None:
-    assert gene_scores_db is not None
+def test_scores_created(
+    t4c8_wgpf_instance: WGPFInstance,  # setup WGPF instance
+) -> None:
+    assert t4c8_wgpf_instance.gene_scores_db is not None
 
 
-def test_lgd_rank_available(gene_scores_db: GeneScoresDb) -> None:
-    assert "LGD_rank" in gene_scores_db
+def test_t4c8_score_available(
+    t4c8_wgpf_instance: WGPFInstance,  # setup WGPF instance
+) -> None:
+    assert "t4c8_score" in t4c8_wgpf_instance.gene_scores_db
 
 
-def test_get_lgd_rank(gene_scores_db: GeneScoresDb) -> None:
-    score = gene_scores_db.get_gene_score("gene_scores/LGD")
+def test_get_t4c8_score(
+    t4c8_wgpf_instance: WGPFInstance,  # setup WGPF instance
+) -> None:
+    score = t4c8_wgpf_instance.gene_scores_db.get_gene_score(
+        "gene_scores/t4c8_score")
 
     assert score is not None
-    assert score.get_min("LGD_rank") == pytest.approx(1.0, 0.01)
-    assert score.get_max("LGD_rank") == pytest.approx(18394.5, 0.01)
+    assert score.get_min("t4c8_score") == pytest.approx(10.123456789)
+    assert score.get_max("t4c8_score") == pytest.approx(20.0)
 
 
-def test_get_genes_by_score(gene_scores_db: GeneScoresDb) -> None:
-    gene_score = gene_scores_db.get_gene_score("gene_scores/LGD")
+def test_get_genes_by_score(
+    t4c8_wgpf_instance: WGPFInstance,  # setup WGPF instance
+) -> None:
+    gene_score = t4c8_wgpf_instance.gene_scores_db.get_gene_score(
+        "gene_scores/t4c8_score")
     assert gene_score is not None
 
     genes = gene_score.get_genes(
-        "LGD_rank", 1.5, 5.0,
+        "t4c8_score", 1.0, 4.0,
     )
-    assert len(genes) == 4
+    assert len(genes) == 0
 
     genes = gene_score.get_genes(
-        "LGD_rank", -1, 5.0,
+        "t4c8_score", 5.0, 15.0,
     )
-    assert len(genes) == 5
+    assert len(genes) == 1
 
     genes = gene_score.get_genes(
-        "LGD_rank", 1.0, 5.0,
+        "t4c8_score", 0, 205,
     )
-    assert len(genes) == 5
+    assert len(genes) == 2
