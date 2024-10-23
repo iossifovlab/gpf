bd0ce7cbaff911181d671fccc91feae75544c5ea
IlinaGergova <ili.gergova@gmail.com>
2024-10-23 13:27:03 +0300

    add genotype browser tests
    * test if the state of genotype browser is constructed correctly using
    the state of each component in genotype
    browser. Make one mock with all states and one
    mock with expected genotype browser state
    * test submitting query
    * test getting selected dataset by getting dataset id from state first

--- gpfjs/src/app/genotype-browser/genotype-browser.component.spec.ts
+++ gpfjs/src/app/genotype-browser/genotype-browser.component.spec.ts
@@ -35,33 +35,36 @@ import { HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/internal/Observable';
 import { NavigationStart, Router, Event } from '@angular/router';
 import { Subject } from 'rxjs/internal/Subject';
-import { Dataset, GenotypeBrowser } from 'app/datasets/datasets';
+import {
+  Column, Dataset, GenotypeBrowser, PersonFilter,
+  PersonSet, PersonSetCollection, PersonSetCollections } from 'app/datasets/datasets';
 import { VariantReportsService } from 'app/variant-reports/variant-reports.service';
 import { Store, StoreModule } from '@ngrx/store';
-
-
-const genotypeBrowserConfigMock = {
-  personFilters: [],
-  familyFilters: [],
-  inheritanceTypeFilter: {},
-  selectedVariantTypes: {}
-};
-
-
-class MockDatasetsService {
-  public getSelectedDataset(): object {
-    return { id: 'testDataset', genotypeBrowserConfig: genotypeBrowserConfigMock };
-  }
-}
-class MockQueryService {
-  public downloadVariants(): Observable<HttpResponse<Blob>> {
-    return of([] as any) as Observable<HttpResponse<Blob>>;
-  }
-
-  public cancelStreamPost(): void {
-    return null;
-  }
-}
+import { PresentInParent, presentInParentReducer } from 'app/present-in-parent/present-in-parent.state';
+import { PedigreeSelector, pedigreeSelectorReducer } from 'app/pedigree-selector/pedigree-selector.state';
+import { FamilyTags } from 'app/family-tags/family-tags';
+import { GeneSet, GeneSetsCollection } from 'app/gene-sets/gene-sets';
+import { GenotypePreviewVariantsArray } from 'app/genotype-preview-model/genotype-preview';
+import { familyIdsReducer } from 'app/family-ids/family-ids.state';
+import { familyTagsReducer } from 'app/family-tags/family-tags.state';
+import { personFiltersReducer } from 'app/person-filters/person-filters.state';
+import { geneSymbolsReducer } from 'app/gene-symbols/gene-symbols.state';
+import { geneSetsReducer } from 'app/gene-sets/gene-sets.state';
+import { geneScoresReducer } from 'app/gene-scores/gene-scores.state';
+import { regionsFiltersReducer } from 'app/regions-filter/regions-filter.state';
+import { genomicScoresReducer } from 'app/genomic-scores-block/genomic-scores-block.state';
+import { personIdsReducer } from 'app/person-ids/person-ids.state';
+import { uniqueFamilyVariantsFilterReducer }
+  from 'app/unique-family-variants-filter/unique-family-variants-filter.state';
+import { studyFiltersReducer } from 'app/study-filters/study-filters.state';
+import { errorsReducer } from 'app/common/errors.state';
+import { studyTypesReducer } from 'app/study-types/study-types.state';
+import { presentInChildReducer } from 'app/present-in-child/present-in-child.state';
+import { inheritanceTypesReducer } from 'app/inheritancetypes/inheritancetypes.state';
+import { gendersReducer } from 'app/gender/gender.state';
+import { effectTypesReducer } from 'app/effect-types/effect-types.state';
+import { variantTypesReducer } from 'app/variant-types/variant-types.state';
+import { datasetIdReducer } from 'app/datasets/datasets.state';
 
 /* eslint-disable */
 const genotypeMock = GenotypeBrowser.fromJson({
@@ -130,10 +133,211 @@ const genotypeMock = GenotypeBrowser.fromJson({
 });
 /* eslint-enable */
 
+const genotypeBrowserStateResult = {
+  variantTypes: ['ins'],
+  effectTypes: ['missense'],
+  genders: ['male'],
+  inheritanceTypeFilter: [],
+  presentInChild: ['sibling only'],
+  studyTypes: ['studyType'],
+  familyIds: ['familyId1'],
+  geneSymbols: ['chd8'],
+  regions: ['regionFrom-regionTo'],
+  genomicScores: [{
+    metric: 'phylop100way',
+    rangeStart: -14.9,
+    rangeEnd: 4.899999999999999
+  }],
+  personIds: ['personId1'],
+  studyFilters: ['SD_Chung2015CHD_liftover'],
+  uniqueFamilyVariants: false,
+  personSetCollection: {
+    id: 'personSetCollection',
+    checkedValues: ['checkedValue1']
+  },
+  tagIntersection: false,
+  selectedFamilyTags: ['tag1'],
+  deselectedFamilyTags: ['tag2'],
+  familyFilters: [{
+    id: 'Mother Race',
+    sourceType: 'categorical',
+    role: 'mom',
+    from: 'phenodb',
+    selection: {
+      selection: [
+        'african-amer'
+      ]
+    },
+    source: 'ssc_commonly_used.race_parents'
+  }],
+  personFilters: [{
+    id: 'Biospecimen wb_dna',
+    sourceType: 'categorical',
+    from: 'phenodb',
+    selection: {
+      selection: [
+        0
+      ]
+    },
+    source: 'ssc_biospecimen.wb_dna'
+  }],
+  geneSet: {
+    geneSet: 'LGDs',
+    geneSetsCollection: 'denovo',
+    geneSetsTypes: [
+      {
+        datasetId: 'sequencing_de_novo',
+        collections: [
+          {
+            personSetId: 'phenotype',
+            types: [
+              'developmental_disorder'
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  geneScores: {
+    score: 'LGD_rank',
+    rangeStart: 3434.5466666666666,
+    rangeEnd: 16923.48
+  },
+  presentInParent: {
+    presentInParent: ['neither'],
+    rarity: {
+      ultraRare: false,
+      minFreq: 0,
+      maxFreq: 1,
+    }
+  },
+};
+
+const allStatesMock = [
+  ['ins'],
+  ['missense'],
+  ['male'],
+  [],
+  ['sibling only'],
+  { presentInParent: ['neither'],
+    rarity: {
+      rarityType: 'rare',
+      rarityIntervalStart: 0,
+      rarityIntervalEnd: 1,
+    }},
+  ['studyType'],
+  {
+    id: 'personSetCollection',
+    checkedValues: ['checkedValue1']
+  } as PedigreeSelector,
+  ['familyId1'],
+  {
+    selectedFamilyTags: ['tag1'],
+    deselectedFamilyTags: ['tag2'],
+    tagIntersection: false,
+  } as FamilyTags,
+  {
+    personFilters: [{
+      id: 'Biospecimen wb_dna',
+      sourceType: 'categorical',
+      from: 'phenodb',
+      selection: {
+        selection: [
+          0
+        ]
+      },
+      source: 'ssc_biospecimen.wb_dna'
+    }],
+    familyFilters: [{
+      id: 'Mother Race',
+      sourceType: 'categorical',
+      role: 'mom',
+      from: 'phenodb',
+      selection: {
+        selection: [
+          'african-amer'
+        ]
+      },
+      source: 'ssc_commonly_used.race_parents'
+    }]
+  },
+  ['chd8'],
+  {
+    geneSet: new GeneSet('LGDs', 0, '', ''),
+    geneSetsCollection: new GeneSetsCollection('denovo', '', []),
+    geneSetsTypes: [
+      {
+        datasetId: 'sequencing_de_novo',
+        collections: [
+          {
+            personSetId: 'phenotype',
+            types: [
+              'developmental_disorder'
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  {
+    score: 'LGD_rank',
+    rangeStart: 3434.5466666666666,
+    rangeEnd: 16923.48
+  },
+  ['regionFrom-regionTo'],
+  [
+    {
+      metric: 'phylop100way',
+      rangeStart: -14.9,
+      rangeEnd: 4.899999999999999
+    }
+  ],
+  ['personId1'],
+  false,
+  ['SD_Chung2015CHD_liftover']
+];
+
+const mockDataset = new Dataset(
+  'testDatasetId',
+  'testDataset',
+  [], true, [], [], [], '', true, true, true, true, null,
+  genotypeMock, null, [], null, null, '', null
+);
+class MockDatasetsService {
+  public getDataset(datasetId: string): Observable<Dataset> {
+    if (datasetId === 'testDatasetId') {
+      return of(mockDataset);
+    }
+    return of(null);
+  }
+}
+class MockQueryService {
+  public streamingSubject = new Subject();
+  public streamingFinishedSubject = new Subject();
+  public downloadVariants(): Observable<HttpResponse<Blob>> {
+    return of([] as any) as Observable<HttpResponse<Blob>>;
+  }
+
+  public cancelStreamPost(): void {
+    return null;
+  }
+
+  public getGenotypePreviewVariantsByFilter(
+    dataset: Dataset,
+    filter,
+    maxVariantsCount: number = 1001,
+    callback?: () => void
+  ): GenotypePreviewVariantsArray {
+    return new GenotypePreviewVariantsArray();
+  }
+}
+
+
 describe('GenotypeBrowserComponent', () => {
   let component: GenotypeBrowserComponent;
   let fixture: ComponentFixture<GenotypeBrowserComponent>;
   const queryService = new MockQueryService();
+  const mockDatasetsService = new MockDatasetsService();
   let loadingService: FullscreenLoadingService;
   let store: Store;
 
@@ -160,7 +364,7 @@ describe('GenotypeBrowserComponent', () => {
         UsersService,
         VariantReportsService,
         GenomicScoresBlockService,
-        { provide: DatasetsService, useValue: new MockDatasetsService() },
+        { provide: DatasetsService, useValue: mockDatasetsService },
         UniqueFamilyVariantsFilterComponent,
         EffectTypesComponent,
         GenderComponent,
@@ -171,7 +375,29 @@ describe('GenotypeBrowserComponent', () => {
       ],
       imports: [
         HttpClientTestingModule, RouterTestingModule, NgbNavModule, FormsModule,
-        StoreModule.forRoot({}),
+        StoreModule.forRoot({
+          familyIds: familyIdsReducer,
+          familyTags: familyTagsReducer,
+          personFilters: personFiltersReducer,
+          geneSymbols: geneSymbolsReducer,
+          geneSets: geneSetsReducer,
+          geneScores: geneScoresReducer,
+          regionsFilter: regionsFiltersReducer,
+          genomicScores: genomicScoresReducer,
+          personIds: personIdsReducer,
+          uniqueFamilyVariantsFilter: uniqueFamilyVariantsFilterReducer,
+          studyFilters: studyFiltersReducer,
+          errors: errorsReducer,
+          pedigreeSelector: pedigreeSelectorReducer,
+          studyTypes: studyTypesReducer,
+          presentInParent: presentInParentReducer,
+          presentInChild: presentInChildReducer,
+          inheritanceTypes: inheritanceTypesReducer,
+          genders: gendersReducer,
+          effectTypes: effectTypesReducer,
+          variantTypes: variantTypesReducer,
+          datasetId: datasetIdReducer,
+        }),
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA]
     }).compileComponents();
@@ -179,10 +405,7 @@ describe('GenotypeBrowserComponent', () => {
     component = fixture.componentInstance;
     loadingService = TestBed.inject(FullscreenLoadingService);
 
-    const selectedDatasetMockModel = {selectedDatasetId: 'testId'};
-
     store = TestBed.inject(Store);
-    jest.spyOn(store, 'select').mockReturnValue(of(selectedDatasetMockModel));
 
     fixture.detectChanges();
   });
@@ -190,26 +413,6 @@ describe('GenotypeBrowserComponent', () => {
   it('should create', () => {
     fixture.detectChanges();
     expect(component).toBeTruthy();
-  });
-
-  it('should test download', () => {
-    const mockEvent = {
-      target: {
-        queryData: {
-          value: ''
-        },
-        submit: jest.fn()
-      }
-    };
-    // eslint-disable-next-line @stylistic/max-len
-    component.selectedDataset = new Dataset('datasetId', 'testDataset', [], true, [], [], [], '', true, true, true, true, null, genotypeMock, null, [], null, null, '', null);
-
-    component.onSubmit(mockEvent);
-    expect(mockEvent.target.queryData.value).toStrictEqual(JSON.stringify({
-      datasetId: component.selectedDataset.id,
-      download: true
-    }));
-    expect(mockEvent.target.submit).toHaveBeenCalledTimes(1);
   });
 
   it('should cancel queries on router change', () => {
@@ -221,5 +424,212 @@ describe('GenotypeBrowserComponent', () => {
 
     expect(stopSpy).toHaveBeenCalledTimes(1);
     expect(cancelSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should create selected dataset by getting the id from state', () => {
+    const mockDatasetsServiceSpy = jest.spyOn(mockDatasetsService, 'getDataset');
+    jest.spyOn(store, 'select').mockReturnValueOnce(of('testDatasetId'));
+
+    component.ngOnInit();
+    expect(mockDatasetsServiceSpy).toHaveBeenCalledWith('testDatasetId');
+    expect(component.selectedDataset).toStrictEqual(mockDataset);
+
+    component.selectedDataset = undefined;
+
+    jest.spyOn(store, 'select').mockReturnValueOnce(of(null));
+
+    component.ngOnInit();
+    expect(mockDatasetsServiceSpy).toHaveBeenCalledWith(null);
+    expect(component.selectedDataset).toBeUndefined();
+  });
+
+  it('should create genotype browser state with rare rarity', () => {
+    jest.clearAllMocks();
+    const rxjs = jest.requireActual('rxjs');
+    jest.spyOn(rxjs, 'combineLatest').mockReturnValueOnce(of(allStatesMock));
+    component.ngOnInit();
+
+    expect(component.genotypeBrowserState).toStrictEqual(genotypeBrowserStateResult);
+    expect(component.genotypePreviewVariantsArray).toBeNull();
+  });
+
+  it('should create genotype browser state with interval rarity', () => {
+    jest.clearAllMocks();
+    const rxjs = jest.requireActual('rxjs');
+    jest.spyOn(rxjs, 'combineLatest').mockReturnValueOnce(of(allStatesMock));
+
+    allStatesMock[5] = {
+      presentInParent: ['neither'],
+      rarity: {
+        rarityType: 'interval',
+        rarityIntervalStart: 0,
+        rarityIntervalEnd: 1,
+      }} as PresentInParent;
+
+    jest.spyOn(rxjs, 'combineLatest').mockReturnValueOnce(of(allStatesMock));
+    component.ngOnInit();
+    expect(component.genotypeBrowserState).toStrictEqual(genotypeBrowserStateResult);
+  });
+
+  it('should get genotype browser state with ultra rare rarity', () => {
+    jest.clearAllMocks();
+    const rxjs = jest.requireActual('rxjs');
+
+    allStatesMock[5] = {
+      presentInParent: ['neither'],
+      rarity: {
+        rarityType: 'ultraRare'
+      }} as PresentInParent;
+
+    jest.spyOn(rxjs, 'combineLatest').mockReturnValueOnce(of(allStatesMock));
+    component.ngOnInit();
+
+    delete genotypeBrowserStateResult.presentInParent.rarity.maxFreq;
+    delete genotypeBrowserStateResult.presentInParent.rarity.minFreq;
+
+    genotypeBrowserStateResult.presentInParent.rarity.ultraRare = true;
+    expect(component.genotypeBrowserState).toStrictEqual(genotypeBrowserStateResult);
+    expect(component.genotypePreviewVariantsArray).toBeNull();
+  });
+
+  it('should create genotype browser empty state when all component states are empty', () => {
+    jest.clearAllMocks();
+    const rxjs = jest.requireActual('rxjs');
+
+    const emptyStatesMock = [
+      [],
+      [],
+      [],
+      [],
+      [],
+      { presentInParent: [],
+        rarity: {}},
+      [],
+      {},
+      [],
+      {
+        selectedFamilyTags: [],
+        deselectedFamilyTags: [],
+        tagIntersection: true,
+      } as FamilyTags,
+      {
+        personFilters: [],
+        familyFilters: []
+      },
+      [],
+      {
+        geneSet: null,
+        geneSetsCollection: null,
+        geneSetsTypes: []
+      },
+      {
+        score: null,
+        rangeStart: 3434.5466666666666,
+        rangeEnd: 16923.48
+      },
+      [],
+      [],
+      [],
+      false,
+      []
+    ];
+
+    jest.spyOn(rxjs, 'combineLatest').mockReturnValueOnce(of(emptyStatesMock));
+    component.ngOnInit();
+    expect(component.genotypeBrowserState).toStrictEqual(
+      {
+        inheritanceTypeFilter: [],
+        genomicScores: [],
+        studyFilters: [],
+        uniqueFamilyVariants: false,
+        personSetCollection: { }
+      }
+    );
+  });
+
+  it('should test submitting query', () => {
+    component.selectedDataset = new Dataset(
+      'testDatasetId',
+      null, ['id1', 'parent2'], null, null, null, null, null,
+      null, null, null, null, null,
+      new GenotypeBrowser(
+        false,
+        false,
+        false,
+        false,
+        true,
+        false,
+        true,
+        false,
+        false,
+        [
+          new Column('name1', 'source1', 'format1'),
+          new Column('name2', 'source2', 'format2')
+        ],
+        [],
+        [
+          new PersonFilter('familyFilter3', 'string3', 'source3', 'sourceType3', 'filterType3', 'role3'),
+          new PersonFilter('familyFilter4', 'string4', 'source4', 'sourceType4', 'filterType4', 'role4')
+        ],
+        ['inheritance', 'string1'],
+        ['selectedInheritance', 'string2'],
+        ['variant', 'string3'],
+        ['selectedVariant', 'string1'],
+        5
+      ), new PersonSetCollections(
+        [
+          new PersonSetCollection(
+            'id1',
+            'name1',
+            [
+              new PersonSet('id1', 'name1', 'color1'),
+              new PersonSet('id1', 'name2', 'color3'),
+              new PersonSet('id2', 'name3', 'color4')
+            ]
+          ),
+          new PersonSetCollection(
+            'id2',
+            'name2',
+            [
+              new PersonSet('id2', 'name2', 'color2'),
+              new PersonSet('id2', 'name3', 'color5'),
+              new PersonSet('id3', 'name4', 'color6')
+            ]
+          )
+        ]
+      ), null, null, null, null, null
+    );
+
+    component.loadingFinished = true;
+    component.showTable = true;
+    component.variantsCountDisplay = '';
+    // const setLoadingStartSpy = jest.spyOn(component, 'setLoadingStart');
+    component.submitQuery();
+
+    expect(component.loadingFinished).toBe(false);
+    expect(component.showTable).toBe(false);
+    expect(component.variantsCountDisplay).toBe('Loading variants...');
+    expect(component.genotypePreviewVariantsArray).toStrictEqual(new GenotypePreviewVariantsArray());
+    expect(component.genotypeBrowserState['datasetId']).toBe('testDatasetId');
+  });
+
+  it('should test download', () => {
+    const mockEvent = {
+      target: {
+        queryData: {
+          value: ''
+        },
+        submit: jest.fn()
+      }
+    };
+    component.genotypeBrowserState = {};
+    component.selectedDataset = mockDataset;
+
+    component.onSubmit(mockEvent);
+    expect(mockEvent.target.queryData.value).toStrictEqual(JSON.stringify({
+      datasetId: component.selectedDataset.id,
+      download: true,
+    }));
+    expect(mockEvent.target.submit).toHaveBeenCalledTimes(1);
   });
 });
