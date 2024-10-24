7b64166277b60c3d17690a96fba261e653b78ab4
IvoStefanov <ivostefanovtues@gmail.com>
2024-10-24 12:50:00 +0300

    Create gene scores api for more histogram types
    Previous gene scores list api supported only number type histograms.
    Support a wider range of histogram types by creating api route that
    returns a more raw histogram data.

--- wdae/wdae/gene_scores/urls.py
+++ wdae/wdae/gene_scores/urls.py
@@ -5,6 +5,11 @@ from gene_scores import views
 urlpatterns = [
     re_path(r"^/?$", views.GeneScoresListView.as_view(), name="gene_scores"),
     re_path(
+        r"^/histograms/?$",
+        views.HistogramView.as_view(),
+        name="gene_scores_histograms",
+    ),
+    re_path(
         r"^/download/(?P<score>.+)$",
         views.GeneScoresDownloadView.as_view(),
         name="gene_scores_download",
