30c475859cb02232503060b12a71e037048124a5
IlinaGergova <ili.gergova@gmail.com>
2024-11-06 10:27:18 +0200

    update pheno browser tests and fixtures
    * not to check for pheno common
    * update download test

--- web_e2e/tests/pheno-browser.spec.ts
+++ web_e2e/tests/pheno-browser.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import * as utils from './utils';
 import * as fs from 'fs';
 import * as path from 'path';
-import pl from 'nodejs-polars';
+import { scanCSV } from 'nodejs-polars';
 
 test.describe('Pheno browser tests', () => {
   test.beforeEach(async({ page }) => {
@@ -19,7 +19,7 @@ test.describe('Pheno browser tests', () => {
     {seachQuery: 'measure 1'}
   ].forEach(data => {
     test(`should filter the right rows when typing "${data.seachQuery}" in the searchbox`, async({ page }) => {
-      await expect(page.locator('.instrument-cell')).toHaveCount(8);
+      await expect(page.locator('.instrument-cell')).toHaveCount(7);
 
       await page.locator('input.form-control').fill(data.seachQuery);
       await expect(page.locator('.instrument-cell')).toHaveCount(1);
@@ -27,40 +27,32 @@ test.describe('Pheno browser tests', () => {
   });
 
   test('should filter the right rows when typing "1" in the sarchbox', async({ page }) => {
-    await expect(page.locator('.instrument-cell')).toHaveCount(8);
-
-    await page.locator('input.form-control').fill('1');
     await expect(page.locator('.instrument-cell')).toHaveCount(7);
+
+    await page.locator('input.form-control').fill('m');
+    await expect(page.locator('.instrument-cell')).toHaveCount(6);
   });
 
   test('should filter the right rows using the instruments dropdown', async({ page }) => {
-    await expect(page.locator('.instrument-cell')).toHaveCount(8);
-
     await page.locator('select.form-control').selectOption('i1');
     await expect(page.locator('.instrument-cell')).toHaveCount(7);
 
-    await page.locator('select.form-control').selectOption('pheno_common');
-    await expect(page.locator('.instrument-cell')).toHaveCount(1);
-
     await page.locator('select.form-control').selectOption('All instruments');
-    await expect(page.locator('.instrument-cell')).toHaveCount(8);
+    await expect(page.locator('.instrument-cell')).toHaveCount(7);
   });
 
   test('should have working table header sorting buttons', async({ page }) => {
     await page.getByText('Instrument', { exact: true }).click();
-    await expect(page.locator('.instrument-cell').nth(0)).toHaveText('pheno_common');
-
-    await page.getByText('Instrument', { exact: true }).click();
     await expect(page.locator('.instrument-cell').nth(0)).toHaveText('i1');
 
     await page.getByText('Proband Pheno Measure', { exact: true }).click();
-    await expect(page.locator('.measure-name-cell').nth(0)).toHaveText('sample_id');
+    await expect(page.locator('.measure-name-cell').nth(0)).toHaveText('m5');
 
     await page.getByText('Proband Pheno Measure', { exact: true }).click();
     await expect(page.locator('.measure-name-cell').nth(0)).toHaveText('age');
 
     await page.getByText('Proband Pheno Measure', { exact: true }).click();
-    await expect(page.locator('.measure-name-cell').nth(0)).toHaveText('sample_id');
+    await expect(page.locator('.measure-name-cell').nth(0)).toHaveText('m5');
 
     await page.getByText('male', { exact: true }).first().click();
     await expect(page.locator('.age').nth(0)).toHaveText('0.97110.2512');
@@ -96,43 +88,10 @@ test.describe('Pheno browser tests', () => {
     await page.locator('#download-measures').click();
     const download = await downloadPromise;
 
-    const columnsToCheck = [
-      'person_id',
-      'i1.age',
-      'i1.iq',
-      'i1.m1',
-      'i1.m2',
-      'i1.m3',
-      'i1.m4',
-      'i1.m5',
-      'pheno_common.sample_id'
-    ];
-
-    const fixtureFrame = (
-      await pl
-        .scanCSV(
-          'playwright/fixtures/pheno-browser/measures_comp_all.csv',
-          {
-            sep: ',',
-            nullValues: '-'
-          }
-        )
-        .select(columnsToCheck)
-        .collect()
-    ).sort('person_id');
-
-    const downloadFrame = (
-      await pl
-        .scanCSV(
-          await download.path(),
-          {
-            sep: ',',
-            nullValues: '-'
-          }
-        )
-        .select(columnsToCheck)
-        .collect()
-    ).sort('person_id');
+    const downloadData = scanCSV(await download.path(), {sep: ',', nullValues: '-'});
+    const fixtureData = scanCSV('playwright/fixtures/pheno-browser/measures_comp_all.csv', {sep: ',', nullValues: '-'});
+    const downloadFrame = (await downloadData.select(downloadData.columns.sort()).collect()).sort('person_id');
+    const fixtureFrame = (await fixtureData.select(fixtureData.columns.sort()).collect()).sort('person_id');
     expect(fixtureFrame.frameEqual(downloadFrame));
   });
 });
