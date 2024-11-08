6f76a285e63dd7733ad02349a0fe0dc929c23a92
iordanivanov <iiordan.ivanov@gmail.com>
2024-11-08 16:07:32 +0200

    Refactor enrichment_api unit tests
    This refactor removes the tests' dependency on the wdae_gpf_instance
    fixture, and replaces it with the t4c8_wgpf_instance fixture.
    Additionally, some of the asserts in the unit tests have been slimmed
    down to test only wdae-relevant code (e.g. not testing values produced
    by the enrichment classes from dae).

--- wdae/wdae/enrichment_api/tests/test_enrichment_api.py
+++ wdae/wdae/enrichment_api/tests/test_enrichment_api.py
@@ -7,30 +7,25 @@ from django.test.client import Client
 from gpf_instance.gpf_instance import WGPFInstance
 from rest_framework import status
 
-pytestmark = pytest.mark.usefixtures(
-    "wdae_gpf_instance", "dae_calc_gene_sets")
-
 
 @pytest.mark.parametrize("url,method,body", [
-    ("/api/v3/enrichment/models/f1_trio", "get", None),
-    (
-        "/api/v3/enrichment/test",
-        "post",
-        {
-            "datasetId": "f1_trio",
-            "enrichmentBackgroundModel": "enrichment/coding_len_testing",
-            "enrichmentCountingModel": "enrichment_gene_counting",
-            "geneSymbols": ["SAMD11", "PLEKHN1", "POGZ"],
-        },
-    ),
+    ("/api/v3/enrichment/models/t4c8_study_1", "GET", None),
+    ("/api/v3/enrichment/test", "POST",
+     {
+         "datasetId": "t4c8_study_1",
+         "enrichmentBackgroundModel": "coding_len_background",
+         "enrichmentCountingModel": "enrichment_gene_counting",
+         "geneSymbols": ["T4", "C8"],
+     }),
 ])
 def test_enrichment_api_permissions(
     anonymous_client: Client,
+    t4c8_wgpf_instance: WGPFInstance,  # noqa: ARG001 ; setup WGPF instance
     url: str,
     method: str,
     body: dict[str, str | list[str]] | None,
 ) -> None:
-    if method == "get":
+    if method == "GET":
         response = anonymous_client.get(url)
     else:
         response = anonymous_client.post(
@@ -41,8 +36,11 @@ def test_enrichment_api_permissions(
     assert response.status_code == status.HTTP_200_OK
 
 
-def test_enrichment_models(admin_client: Client) -> None:
-    response = admin_client.get("/api/v3/enrichment/models/f1_trio")
+def test_enrichment_models(
+    admin_client: Client,
+    t4c8_wgpf_instance: WGPFInstance,  # noqa: ARG001 ; setup WGPF instance
+) -> None:
+    response = admin_client.get("/api/v3/enrichment/models/t4c8_study_1")
 
     assert response
     assert response.status_code == 200
@@ -50,44 +48,43 @@ def test_enrichment_models(admin_client: Client) -> None:
     result = response.data  # type: ignore
     assert result
 
-    assert len(result["background"]) == 2
+    assert len(result["background"]) == 1
     background = result["background"]
-    background.sort(key=operator.itemgetter("id"))
-    assert background[0]["id"] == "enrichment/coding_len_testing"
-    assert background[0]["name"] == "CodingLenBackground"
-    assert background[0]["desc"] == ""
-
-    assert background[1]["id"] == "enrichment/samocha_testing"
-    assert background[1]["name"] == "Samocha's enrichment background model"
-    assert background[1]["desc"] == ""
+    assert background[0]["id"] == "coding_len_background"
+    assert background[0]["name"] == "t4c8CodingLenBackground"
+    assert background[0]["desc"] == "T4C8 gene coding length enrichment background model"  # noqa: E501
 
     assert len(result["counting"]) == 2
-
     counting = result["counting"]
     counting.sort(key=operator.itemgetter("name"))
 
-    assert counting[0]["id"] == "enrichment_events_counting"
-    assert counting[0]["name"] == "enrichment_events_counting name"
-    assert counting[0]["desc"] == "Enrichment Events Counting"
+    assert counting[0]["id"] == "enrichment_gene_counting"
+    assert counting[0]["name"] == "Counting affected genes"
+    assert counting[0]["desc"] == "Counting affected genes"
 
-    assert counting[1]["id"] == "enrichment_gene_counting"
-    assert counting[1]["name"] == "enrichment_gene_counting name"
-    assert counting[1]["desc"] == "Enrichment Gene Counting"
+    assert counting[1]["id"] == "enrichment_events_counting"
+    assert counting[1]["name"] == "Counting events"
+    assert counting[1]["desc"] == "Counting events"
 
 
-def test_enrichment_models_missing_study(admin_client: Client) -> None:
+def test_enrichment_models_missing_study(
+    admin_client: Client,
+    t4c8_wgpf_instance: WGPFInstance,  # noqa: ARG001 ; setup WGPF instance
+) -> None:
     response = admin_client.get("/api/v3/enrichment/models/f1")
-
     assert response
     assert response.status_code == 404
 
 
-def test_enrichment_test_missing_dataset_id(admin_client: Client) -> None:
+def test_enrichment_test_missing_dataset_id(
+    admin_client: Client,
+    t4c8_wgpf_instance: WGPFInstance,  # noqa: ARG001 ; setup WGPF instance
+) -> None:
     url = "/api/v3/enrichment/test"
     query = {
-        "enrichmentBackgroundModel": "enrichment/coding_len_testing",
+        "enrichmentBackgroundModel": "coding_len_background",
         "enrichmentCountingModel": "enrichment_gene_counting",
-        "geneSymbols": ["POGZ"],
+        "geneSymbols": ["T4"],
     }
     response = admin_client.post(
         url, json.dumps(query), content_type="application/json", format="json",
@@ -97,13 +94,16 @@ def test_enrichment_test_missing_dataset_id(admin_client: Client) -> None:
     assert response.status_code == 400
 
 
-def test_enrichment_test_missing_study(admin_client: Client) -> None:
+def test_enrichment_test_missing_study(
+    admin_client: Client,
+    t4c8_wgpf_instance: WGPFInstance,  # noqa: ARG001 ; setup WGPF instance
+) -> None:
     url = "/api/v3/enrichment/test"
     query = {
-        "datasetId": "f1",
-        "enrichmentBackgroundModel": "enrichment/coding_len_testing",
+        "datasetId": "t4c8_study_9001",
+        "enrichmentBackgroundModel": "coding_len_background",
         "enrichmentCountingModel": "enrichment_gene_counting",
-        "geneSymbols": ["POGZ"],
+        "geneSymbols": ["T4"],
     }
     response = admin_client.post(
         url, json.dumps(query), content_type="application/json", format="json",
@@ -113,11 +113,14 @@ def test_enrichment_test_missing_study(admin_client: Client) -> None:
     assert response.status_code == 404
 
 
-def test_enrichment_test_missing_gene_symbols(admin_client: Client) -> None:
+def test_enrichment_test_missing_gene_symbols(
+    admin_client: Client,
+    t4c8_wgpf_instance: WGPFInstance,  # noqa: ARG001 ; setup WGPF instance
+) -> None:
     url = "/api/v3/enrichment/test"
     query = {
-        "datasetId": "f1_trio",
-        "enrichmentBackgroundModel": "enrichment/coding_len_testing",
+        "datasetId": "t4c8_study_1",
+        "enrichmentBackgroundModel": "coding_len_background",
         "enrichmentCountingModel": "enrichment_gene_counting",
     }
     response = admin_client.post(
@@ -128,14 +131,16 @@ def test_enrichment_test_missing_gene_symbols(admin_client: Client) -> None:
     assert response.status_code == 400
 
 
-# @pytest.mark.xfail(reason="[gene models] wrong annotation")
-def test_enrichment_test_gene_symbols(admin_client: Client) -> None:
+def test_enrichment_test_with_gene_symbols(
+    admin_client: Client,
+    t4c8_wgpf_instance: WGPFInstance,  # noqa: ARG001 ; setup WGPF instance
+) -> None:
     url = "/api/v3/enrichment/test"
     query = {
-        "datasetId": "f1_trio",
-        "enrichmentBackgroundModel": "enrichment/coding_len_testing",
+        "datasetId": "t4c8_study_1",
+        "enrichmentBackgroundModel": "coding_len_background",
         "enrichmentCountingModel": "enrichment_gene_counting",
-        "geneSymbols": ["SAMD11", "PLEKHN1", "POGZ"],
+        "geneSymbols": ["T4", "C8"],
     }
     response = admin_client.post(
         url, json.dumps(query), content_type="application/json", format="json",
@@ -145,92 +150,27 @@ def test_enrichment_test_gene_symbols(admin_client: Client) -> None:
     assert response.status_code == 200
 
     result = response.data  # type: ignore
-    assert len(result) == 2
 
-    assert result["desc"][:14] == "Gene Symbols: "
-    assert result["desc"][:14] == "Gene Symbols: "
-    assert result["desc"][-4:] == " (3)"
-    assert sorted(result["desc"][14:-4].split(",")) == sorted(
-        ["SAMD11", "PLEKHN1", "POGZ"],
-    )
-
+    assert set(result.keys()) == {"desc", "result"}
+    assert result["desc"] == "Gene Symbols: C8,T4 (2)"
     assert len(result["result"]) == 2
-    assert len(result["result"][0]) == 6
-    assert result["result"][0]["selector"] == "phenotype 1"
-    assert result["result"][0]["peopleGroupId"] == "phenotype"
-    assert len(result["result"][0]["childrenStats"]) == 3
-    assert result["result"][0]["childrenStats"]["M"] == 1
-    assert result["result"][0]["childrenStats"]["F"] == 1
-    assert result["result"][0]["childrenStats"]["U"] == 0
-    assert result["result"][0]["LGDs"]["all"]["count"] == 0
-    assert result["result"][0]["LGDs"]["rec"]["count"] == 0
-    assert result["result"][0]["LGDs"]["male"]["count"] == 0
-    assert result["result"][0]["LGDs"]["female"]["count"] == 0
-    assert result["result"][0]["missense"]["all"]["count"] == 1
-    assert result["result"][0]["missense"]["rec"]["count"] == 1
-    assert result["result"][0]["missense"]["male"]["count"] == 1
-    assert result["result"][0]["missense"]["female"]["count"] == 1
-    assert result["result"][0]["synonymous"]["all"]["count"] == 1
-    assert result["result"][0]["synonymous"]["rec"]["count"] == 1
-    assert result["result"][0]["synonymous"]["male"]["count"] == 1
-    assert result["result"][0]["synonymous"]["female"]["count"] == 1
-
-    assert len(result["result"][1]) == 6
-    assert result["result"][1]["selector"] == "unaffected"
-    assert result["result"][1]["peopleGroupId"] == "phenotype"
-    assert len(result["result"][1]["childrenStats"]) == 3
-    assert result["result"][1]["childrenStats"]["F"] == 1
-    assert result["result"][1]["childrenStats"]["M"] == 0
-    assert result["result"][1]["childrenStats"]["U"] == 0
-    assert result["result"][1]["LGDs"]["all"]["count"] == 0
-    assert result["result"][1]["LGDs"]["rec"]["count"] == 0
-    assert result["result"][1]["LGDs"]["male"]["count"] == 0
-    assert result["result"][1]["LGDs"]["female"]["count"] == 0
-    assert result["result"][1]["missense"]["all"]["count"] == 0
-    assert result["result"][1]["missense"]["rec"]["count"] == 0
-    assert result["result"][1]["missense"]["male"]["count"] == 0
-    assert result["result"][1]["missense"]["female"]["count"] == 0
-    assert result["result"][1]["synonymous"]["all"]["count"] == 1
-    assert result["result"][1]["synonymous"]["rec"]["count"] == 0
-    assert result["result"][1]["synonymous"]["male"]["count"] == 0
-    assert result["result"][1]["synonymous"]["female"]["count"] == 1
+    assert len(result["result"][0]) == 18
+    assert len(result["result"][1]) == 18
 
 
-def test_enrichment_test_gene_scores(
+def test_enrichment_test_with_gene_score(
     admin_client: Client,
-    wdae_gpf_instance: WGPFInstance,  # noqa: ARG001
+    t4c8_wgpf_instance: WGPFInstance,  # noqa: ARG001 ; setup WGPF instance
 ) -> None:
     url = "/api/v3/enrichment/test"
     query = {
-        "datasetId": "f1_trio",
-        "enrichmentBackgroundModel": "enrichment/coding_len_testing",
+        "datasetId": "t4c8_study_1",
+        "enrichmentBackgroundModel": "coding_len_background",
         "enrichmentCountingModel": "enrichment_gene_counting",
         "geneScores": {
-            "score": "LGD_rank",
-            "rangeStart": 1,
-            "rangeEnd": 1000,
-        },
-    }
-    response = admin_client.post(
-        url, json.dumps(query), content_type="application/json", format="json",
-    )
-
-    assert response
-    assert response.status_code == 200
-
-
-def test_enrichment_test_gene_scores_with_zero_range(
-        admin_client: Client, wdae_gpf_instance: WGPFInstance,  # noqa: ARG001
-) -> None:
-    url = "/api/v3/enrichment/test"
-    query = {
-        "datasetId": "f1_trio",
-        "enrichmentBackgroundModel": "enrichment/coding_len_testing",
-        "enrichmentCountingModel": "enrichment_gene_counting",
-        "geneScores": {
-            "score": "LGD_rank",
-            "rangeStart": 0,
-            "rangeEnd": 1000,
+            "score": "t4c8_score",
+            "rangeStart": 15,
+            "rangeEnd": 20,
         },
     }
     response = admin_client.post(
@@ -241,34 +181,51 @@ def test_enrichment_test_gene_scores_with_zero_range(
     assert response.status_code == 200
 
     result = response.data  # type: ignore
-    assert result["desc"] == "Gene Scores: LGD_rank from 0 up to 1000 (1024)"
+
+    assert set(result.keys()) == {"desc", "result"}
+    assert result["desc"] == "Gene Scores: t4c8_score from 15 up to 20 (1)"
 
 
-def test_enrichment_test_gene_set(
+def test_enrichment_test_with_gene_score_with_zero_range(
     admin_client: Client,
-    wdae_gpf_instance: WGPFInstance,  # noqa: ARG001
+    t4c8_wgpf_instance: WGPFInstance,  # noqa: ARG001 ; setup WGPF instance
 ) -> None:
     url = "/api/v3/enrichment/test"
     query = {
-        "datasetId": "f1_trio",
-        "enrichmentBackgroundModel": "enrichment/coding_len_testing",
+        "datasetId": "t4c8_study_1",
+        "enrichmentBackgroundModel": "coding_len_background",
+        "enrichmentCountingModel": "enrichment_gene_counting",
+        "geneScores": {
+            "score": "t4c8_score",
+            "rangeStart": 0,
+            "rangeEnd": 20,
+        },
+    }
+    response = admin_client.post(
+        url, json.dumps(query), content_type="application/json", format="json",
+    )
+
+    assert response
+    assert response.status_code == 200
+
+    result = response.data  # type: ignore
+
+    assert set(result.keys()) == {"desc", "result"}
+    assert result["desc"] == "Gene Scores: t4c8_score from 0 up to 20 (2)"
+
+
+def test_enrichment_test_with_gene_set(
+    admin_client: Client,
+    t4c8_wgpf_instance: WGPFInstance,  # noqa: ARG001 ; setup WGPF instance
+) -> None:
+    url = "/api/v3/enrichment/test"
+    query = {
+        "datasetId": "t4c8_study_1",
+        "enrichmentBackgroundModel": "coding_len_background",
         "enrichmentCountingModel": "enrichment_gene_counting",
         "geneSet": {
-            "geneSetsCollection": "denovo",
-            "geneSet": "Missense",
-            "geneSetsTypes": [
-            {
-                "datasetId": "f1_trio",
-                "collections": [
-                    {
-                        "personSetId": "phenotype",
-                        "types": [
-                            "phenotype1",
-                        ],
-                    },
-                ],
-            },
-        ],
+            "geneSetsCollection": "main",
+            "geneSet": "t4_candidates",
         },
     }
     response = admin_client.post(
@@ -279,7 +236,6 @@ def test_enrichment_test_gene_set(
     assert response.status_code == 200
 
     result = response.data  # type: ignore
-    assert len(result) == 2
 
-    assert result["desc"] == \
-        "Gene Set: Missense (f1_trio:phenotype:phenotype1) (1)"
+    assert set(result.keys()) == {"desc", "result"}
+    assert result["desc"] == "Gene Set: T4 Candidates (1)"
