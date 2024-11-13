907f82a13c944da41a103ae7b411f80aa5b7765e
IlinaGergova <ili.gergova@gmail.com>
2024-11-13 15:30:55 +0200

    add categorical histogram uni tests
    * test when loading from state values from a range
    * test if bars color change when loading values from
    state with clickable view
    * test transforming sliders position to index valid for the
    array of histogram values

--- gpfjs/src/app/categorical-histogram/categorical-histogram.component.spec.ts
+++ gpfjs/src/app/categorical-histogram/categorical-histogram.component.spec.ts
@@ -2,6 +2,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CategoricalHistogramComponent } from './categorical-histogram.component';
 import { CategoricalHistogram } from 'app/gene-scores/gene-scores';
+import { By } from '@angular/platform-browser';
 
 describe('CategoricalHistogramComponent', () => {
   let component: CategoricalHistogramComponent;
@@ -100,5 +101,89 @@ describe('CategoricalHistogramComponent', () => {
     expect(component.histogram.values[2].name).toBe('name3');
     expect(component.histogram.values[3].name).toBe('other');
     expect(component.histogram.values[3].value).toBe(90);
+  });
+
+  it('should sort range values from state for categorical histogram', () => {
+    component.stateCategoricalNames = ['name5', 'name1', 'name3', 'name2', 'name4'];
+    component.histogram = new CategoricalHistogram(
+      [
+        {name: 'name1', value: 10},
+        {name: 'name2', value: 20},
+        {name: 'name3', value: 30},
+        {name: 'name4', value: 40},
+        {name: 'name5', value: 50},
+      ],
+      ['name1', 'name2', 'name3', 'name4', 'name5'],
+      'large value descriptions',
+      'small value descriptions',
+      true,
+      2,
+    );
+    component.interactType = 'range selector';
+
+    component.ngOnInit();
+    expect(component.stateCategoricalNames).toStrictEqual(['name1', 'name2', 'name3', 'name4', 'name5']);
+  });
+
+  it('should color bars in coral with click selector view of categorial historam', () => {
+    component.stateCategoricalNames = ['name3', 'name1'];
+    component.interactType = 'click selector';
+
+    component.ngOnInit();
+    expect(component.stateCategoricalNames).toStrictEqual(['name3', 'name1']);
+
+    const bar1: HTMLElement = (fixture.nativeElement as HTMLElement).querySelector('rect[id=name3]');
+    expect(bar1.style.fill).toBe('coral');
+
+    const bar2: HTMLElement = (fixture.nativeElement as HTMLElement).querySelector('rect[id=name1]');
+    expect(bar2.style.fill).toBe('coral');
+  });
+
+  it('should check if single score value is valid', () => {
+    component.singleScoreValue = 'scoreValue';
+    expect(component.singleScoreValueIsValid()).toBe(true);
+
+    component.singleScoreValue = '';
+    expect(component.singleScoreValueIsValid()).toBe(false);
+
+    component.singleScoreValue = undefined;
+    expect(component.singleScoreValueIsValid()).toBe(false);
+
+    component.singleScoreValue = null;
+    expect(component.singleScoreValueIsValid()).toBe(false);
+  });
+
+  it('should transform range end slider position into index', () => {
+    component.endX = 215;
+    expect(component.sliderEndIndex).toBe(1);
+
+    component.endX = 50;
+    expect(component.sliderEndIndex).toBe(0);
+
+    component.sliderStartIndex = 2;
+    component.endX = 215;
+    expect(component.sliderEndIndex).toBe(0);
+
+    component.sliderStartIndex = 0;
+    component.sliderEndIndex = 1;
+    component.endX = 215;
+    expect(component.sliderEndIndex).toBe(1);
+  });
+
+  it('should transform range start slider position into index', () => {
+    component.startX = 215;
+    expect(component.sliderStartIndex).toBe(2);
+
+    component.startX = 50;
+    expect(component.sliderStartIndex).toBe(0);
+
+    component.sliderEndIndex = 1;
+    component.startX = 215;
+    expect(component.sliderStartIndex).toBe(0);
+
+    component.sliderStartIndex = 0;
+    component.sliderEndIndex = 1;
+    component.startX = 215;
+    expect(component.sliderStartIndex).toBe(0);
   });
 });
