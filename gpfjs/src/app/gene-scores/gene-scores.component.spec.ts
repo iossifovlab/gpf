2396251da6ef4c9e976c81fa5b4e16d75cbbc743
IlinaGergova <ili.gergova@gmail.com>
2024-11-13 15:25:00 +0200

    add gene scores unit tests
    * test loading categorical and continuous histograms from state
    * test changing categorical histogram selected values
    and saving to state
    * test clearing data when switching categorical histogram views
    * test setting default categorical histogram default view
    based on having value order
    * test setting domain of continuous histogram based on
    histogram defualt domain min and max
    * test setting and getting range start and range end

--- gpfjs/src/app/gene-scores/gene-scores.component.spec.ts
+++ gpfjs/src/app/gene-scores/gene-scores.component.spec.ts
@@ -6,10 +6,10 @@ import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { ConfigService } from 'app/config/config.service';
 import { Observable } from 'rxjs';
 import { of } from 'rxjs/internal/observable/of';
-import { GeneScores, NumberHistogram } from './gene-scores';
+import { CategoricalHistogram, CategoricalHistogramView, GeneScores, NumberHistogram } from './gene-scores';
 import { GeneScoresComponent } from './gene-scores.component';
 import { GeneScoresService } from './gene-scores.service';
-import { geneScoresReducer } from './gene-scores.state';
+import { geneScoresReducer, setGeneScoreCategorical, setGeneScoreContinuous } from './gene-scores.state';
 import { Store, StoreModule } from '@ngrx/store';
 
 class MockGeneScoresService {
@@ -55,7 +55,6 @@ describe('GeneScoresComponent', () => {
 
     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
     store = TestBed.inject(Store);
-    jest.spyOn(store, 'select').mockReturnValue(of({score: 'score1'}));
 
     fixture = TestBed.createComponent(GeneScoresComponent);
     component = fixture.componentInstance;
@@ -77,6 +76,182 @@ describe('GeneScoresComponent', () => {
 
     expect(fixture.debugElement.query(By.css('div > div.form-block > div.card > ul > li > span'))).toBeTruthy();
     expect(fixture.debugElement.query(By.css('div > div#gene-scores-panel'))).not.toBeTruthy();
+  });
+
+  it('should load default score', () => {
+    component.ngOnInit();
+    expect(component.selectedGeneScores.score).toBe('score1');
+  });
+
+  it('should continuous histogram load from state', () => {
+    jest.spyOn(store, 'select').mockReturnValue(of({
+      histogramType: 'continuous',
+      score: 'score2',
+      rangeStart: 10,
+      rangeEnd: 20,
+      values: null,
+      categoricalView: null,
+    }));
+
+    component.ngOnInit();
+
+    expect(component.selectedGeneScores.score).toBe('score2');
+    expect(component.rangeStart).toBe(10);
+    expect(component.rangeEnd).toBe(20);
+  });
+
+  it('should categorical histogram load from state', () => {
+    jest.spyOn(store, 'select').mockReturnValue(of({
+      histogramType: 'categorical',
+      score: 'score1',
+      rangeStart: 10,
+      rangeEnd: 20,
+      values: ['name2', 'name5'],
+      categoricalView: 'click selector',
+    }));
+
+    component.ngOnInit();
+
+    expect(component.selectedGeneScores.score).toBe('score1');
+    expect(component.categoricalValues).toStrictEqual(['name2', 'name5']);
+    expect(component.selectedCategoricalHistogramView).toBe('click selector');
+  });
+
+  it('should toggle categorical histogram values and save to state', () => {
+    const dispatchSpy = jest.spyOn(component['store'], 'dispatch');
+    component.geneScoresLocalState.score = new GeneScores('desc', 'help', 'score1', null);
+
+    const values = ['name5', 'name2'];
+    component.categoricalValues = ['name1', 'name2', 'name3'];
+    component.toggleCategoricalValues(values);
+    expect(dispatchSpy).toHaveBeenCalledWith(setGeneScoreCategorical({
+      score: 'score1',
+      values: ['name1', 'name3', 'name5'],
+      categoricalView: 'range selector',
+    }));
+  });
+
+  it('should switch categorical histogram view', () => {
+    const dispatchSpy = jest.spyOn(component['store'], 'dispatch');
+    component.geneScoresLocalState.score = new GeneScores('desc', 'help', 'score1', null);
+    component.selectedCategoricalHistogramView = 'range selector';
+    component.categoricalValues = ['name1', 'name2', 'name3'];
+
+    component.switchCategoricalHistogramView('click selector' as CategoricalHistogramView);
+
+    expect(component.selectedCategoricalHistogramView).toBe('click selector');
+    expect(component.categoricalValues).toStrictEqual([]);
+
+    expect(dispatchSpy).toHaveBeenCalledWith(setGeneScoreCategorical({
+      score: 'score1',
+      values: [],
+      categoricalView: 'click selector',
+    }));
+  });
+
+  it('should set default categorical histogram view', () => {
+    const scoreWithValuesOrder = new GeneScores('desc', 'help', 'score1', new CategoricalHistogram(
+      [
+        {name: 'name1', value: 10},
+        {name: 'name2', value: 20},
+        {name: 'name3', value: 30},
+      ],
+      ['name3', 'name1', 'name2'],
+      'large value descriptions',
+      'small value descriptions',
+      true,
+    ));
+    component.selectedCategoricalHistogramView = 'range selector';
+    component.selectedGeneScores = scoreWithValuesOrder;
+    expect(component.selectedCategoricalHistogramView).toBe('range selector');
+
+    const scoreWithoutValuesOrder = new GeneScores('desc', 'help', 'score1', new CategoricalHistogram(
+      [
+        {name: 'name1', value: 10},
+        {name: 'name2', value: 20},
+        {name: 'name3', value: 30},
+      ],
+      null,
+      'large value descriptions',
+      'small value descriptions',
+      true,
+    ));
+    component.selectedGeneScores = scoreWithoutValuesOrder;
+    expect(component.selectedCategoricalHistogramView).toBe('click selector');
+  });
+
+  it('should set default ranges for continuous histogram as domain', () => {
+    const score = new GeneScores(
+      'desc',
+      'help',
+      'score1',
+      new NumberHistogram([1, 2], [4, 5], 'larger1', 'smaller1', null, null, true, true),
+    );
+
+    component.selectedGeneScores = score;
+    expect(component.geneScoresLocalState.domainMin).toBe(4);
+    expect(component.geneScoresLocalState.domainMax).toBe(5);
+  });
+
+  it('should set domain using number histogram min and max ranges', () => {
+    const score = new GeneScores(
+      'desc',
+      'help',
+      'score1',
+      new NumberHistogram([1, 2], [4, 5], 'larger1', 'smaller1', 2, 500, true, true),
+    );
+
+    component.selectedGeneScores = score;
+    expect(component.geneScoresLocalState.domainMin).toBe(2);
+    expect(component.geneScoresLocalState.domainMax).toBe(500);
+  });
+
+  it('should set and get range start', () => {
+    const dispatchSpy = jest.spyOn(component['store'], 'dispatch');
+    const score = new GeneScores(
+      'desc',
+      'help',
+      'score1',
+      new NumberHistogram([1, 2], [4, 5], 'larger1', 'smaller1', 2, 500, true, true),
+    );
+
+    component.geneScoresLocalState.score = score;
+    component.geneScoresLocalState.rangeStart = 0;
+    component.geneScoresLocalState.rangeEnd = 10;
+    component.rangeStart = 3;
+
+    expect(component.geneScoresLocalState.rangeStart).toBe(3);
+
+    expect(dispatchSpy).toHaveBeenCalledWith(setGeneScoreContinuous({
+      score: 'score1',
+      rangeStart: 3,
+      rangeEnd: 10,
+    }));
+    expect(component.rangeStart).toBe(3);
+  });
+
+  it('should set and get range end', () => {
+    const dispatchSpy = jest.spyOn(component['store'], 'dispatch');
+    const score = new GeneScores(
+      'desc',
+      'help',
+      'score1',
+      new NumberHistogram([1, 2], [4, 5], 'larger1', 'smaller1', 2, 500, true, true),
+    );
+
+    component.geneScoresLocalState.score = score;
+    component.geneScoresLocalState.rangeStart = 0;
+    component.geneScoresLocalState.rangeEnd = 10;
+    component.rangeEnd = 3;
+
+    expect(component.geneScoresLocalState.rangeEnd).toBe(3);
+
+    expect(dispatchSpy).toHaveBeenCalledWith(setGeneScoreContinuous({
+      score: 'score1',
+      rangeStart: 0,
+      rangeEnd: 3,
+    }));
+    expect(component.rangeEnd).toBe(3);
   });
 });
 
