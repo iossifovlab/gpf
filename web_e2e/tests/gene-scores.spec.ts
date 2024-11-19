17f69bb87efa99a273af733ae49716dda757022f
IlinaGergova <ili.gergova@gmail.com>
2024-11-19 18:02:35 +0200

    add gene scores categorical histogram tests
    * test switching between click selection and range selection
    * test histogram bars color change when selecting and deselecting in both selection methods
    * test switching selection mode button visibility when selecting score with other histogram
    * test resetting selection when switching the views and when selecting new score
    * test download, share query and table preview with both selection methods

--- web_e2e/tests/gene-scores.spec.ts
+++ web_e2e/tests/gene-scores.spec.ts
@@ -2,6 +2,7 @@
 import { test, expect } from '@playwright/test';
 import * as utils from './utils';
 import * as path from 'path';
+import { scanCSV } from 'nodejs-polars';
 
 const geneScoresData = [
   {
@@ -60,9 +61,7 @@ test.describe('Gene scores tests', () => {
     await expect(page.locator('input#to-input-field')).toHaveValue('16640');
 
     await page.locator('.histogram-from .step.up').click();
-    await page.waitForRequest(utils.backendUrl + '/api/v3/gene_scores/partitions');
     await page.locator('.histogram-to .step.down').click();
-    await page.waitForRequest(utils.backendUrl + '/api/v3/gene_scores/partitions');
     await expect(page.locator('input#from-input-field')).toHaveValue('111.927');
     await expect(page.locator('input#to-input-field')).toHaveValue('16529.073');
 
@@ -112,8 +111,6 @@ test.describe('Gene scores tests', () => {
         await page.click('#gene-scores');
         await page.locator('gpf-gene-scores select').selectOption(geneScore.desc);
 
-        await expect(page.locator('text#sumOfBarsLabel')).not.toContainText('~');
-
         await page.locator('gpf-effect-types').getByRole('button', { name: 'All' }).click();
         await page.getByRole('button', { name: 'Table Preview' }).click();
 
@@ -141,5 +138,267 @@ test.describe('Gene scores tests', () => {
     const expectedFileLines = await utils.readFile(expectedVariantsPath);
     const downloadedData = Buffer.concat(data);
     expect(downloadedData.toString()).toEqual(expectedFileLines.toString());
+  });
+});
+
+test.describe('Gene scores categorical histogram tests', () => {
+  test.beforeEach(async({ page }) => {
+    await page.goto(utils.frontendUrl, {waitUntil: 'load'});
+    await utils.navigateToHome(page);
+    await utils.loginAdmin(page);
+
+    await utils.navigateToDatasetPage(page, utils.datasetIds.allGenotypes, 'Genotype browser');
+    await page.locator('#gene-scores').click();
+    await page.locator('gpf-gene-scores select')
+      .selectOption('gene-score - Evidence strength supporting a gene\'s association with autism');
+  });
+
+  test('should switch historgam selection modes', async({ page }) => {
+    await page.getByRole('button', {name: 'Mode'}).click();
+    await expect(page.locator('.mat-mdc-menu-content').getByRole('menuitem')).toHaveCount(2);
+    await expect(page.getByRole('menuitem', {name: 'range selector'})).toBeVisible();
+    await expect(page.getByRole('menuitem', {name: 'click selector'})).toBeVisible();
+
+    await expect(page.getByRole('menuitem', {name: 'range selector'}))
+      .toHaveCSS('background-color', 'rgb(42, 99, 149)');
+    await expect(page.locator('[gpf-histogram-range-selector-line]')).toHaveCount(2);
+
+    await page.getByRole('menuitem', {name: 'click selector'}).click();
+    await expect(page.locator('[gpf-histogram-range-selector-line]')).toHaveCount(0);
+
+    await page.getByRole('button', {name: 'Mode'}).click();
+    await expect(page.getByRole('menuitem', {name: 'click selector'}))
+      .toHaveCSS('background-color', 'rgb(42, 99, 149)');
+  });
+
+  test('should check switch selection mode button is not visible for other histograms', async({ page }) => {
+    await expect(page.locator('gpf-categorical-histogram')).toBeVisible();
+    await expect(page.getByRole('button', {name: 'Mode'})).toBeVisible();
+    await expect(page.locator('input#from-input-field')).not.toBeVisible();
+    await expect(page.locator('input#to-input-field')).not.toBeVisible();
+
+    await page.locator('gpf-gene-scores select').selectOption('RVIS score - Intolerance of a gene to genetic variants');
+    await expect(page.getByRole('button', {name: 'Mode'})).not.toBeVisible();
+    await expect(page.locator('gpf-histogram')).toBeVisible();
+  });
+
+  test('should check historgam colors change in range selector mode', async({ page }) => {
+    await expect(page.locator('rect[id="1"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+    await expect(page.locator('rect[id="2"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+    await expect(page.locator('rect[id="3"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+
+    await page.locator('[gpf-histogram-range-selector-line]').nth(0).hover();
+    await page.mouse.down();
+    await page.mouse.move(900, 400, {steps: 3});
+    await page.mouse.up();
+
+    await page.locator('[gpf-histogram-range-selector-line]').nth(1).hover();
+    await page.mouse.down();
+    await page.mouse.move(1000, 400, {steps: 3});
+    await page.mouse.up();
+
+    await expect(page.locator('rect[id="1"]')).toHaveCSS('fill', 'rgb(176, 196, 222)');
+    await expect(page.locator('rect[id="2"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+    await expect(page.locator('rect[id="3"]')).toHaveCSS('fill', 'rgb(176, 196, 222)');
+
+
+    await page.locator('[gpf-histogram-range-selector-line]').nth(0).hover();
+    await page.mouse.down();
+    await page.mouse.move(100, 400, {steps: 3});
+    await page.mouse.up();
+
+    await page.locator('[gpf-histogram-range-selector-line]').nth(1).hover();
+    await page.mouse.down();
+    await page.mouse.move(1200, 400, {steps: 3});
+    await page.mouse.up();
+
+    await expect(page.locator('rect[id="1"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+    await expect(page.locator('rect[id="2"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+    await expect(page.locator('rect[id="3"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+  });
+
+  test('should check historgam colors change in click selector mode', async({ page }) => {
+    await page.getByRole('button', {name: 'Mode'}).click();
+    await page.getByRole('menuitem', {name: 'click selector'}).click();
+    await expect(page.getByText('Please select at least one bar.')).toBeVisible();
+
+    await expect(page.locator('rect[id="1"]')).toHaveCSS('fill', 'rgb(176, 196, 222)');
+    await expect(page.locator('rect[id="2"]')).toHaveCSS('fill', 'rgb(176, 196, 222)');
+    await expect(page.locator('rect[id="3"]')).toHaveCSS('fill', 'rgb(176, 196, 222)');
+
+    await page.locator('rect[id="1"]').click();
+    await expect(page.getByText('Please select at least one bar.')).not.toBeVisible();
+    await page.locator('rect[id="2"]').click();
+
+    await expect(page.locator('rect[id="1"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+    await expect(page.locator('rect[id="2"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+    await expect(page.locator('rect[id="3"]')).toHaveCSS('fill', 'rgb(176, 196, 222)');
+
+    await page.locator('rect[id="1"]').click();
+    await page.locator('rect[id="2"]').click();
+
+    await expect(page.getByText('Please select at least one bar.')).toBeVisible();
+    await expect(page.locator('rect[id="1"]')).toHaveCSS('fill', 'rgb(176, 196, 222)');
+    await expect(page.locator('rect[id="2"]')).toHaveCSS('fill', 'rgb(176, 196, 222)');
+    await expect(page.locator('rect[id="3"]')).toHaveCSS('fill', 'rgb(176, 196, 222)');
+  });
+
+  test('should check if range selection is reset when switching to click selection', async({ page }) => {
+    await page.locator('[gpf-histogram-range-selector-line]').nth(0).hover();
+    await page.mouse.down();
+    await page.mouse.move(900, 400, {steps: 3});
+    await page.mouse.up();
+
+    await expect(page.locator('rect[id="1"]')).toHaveCSS('fill', 'rgb(176, 196, 222)');
+
+    await page.getByRole('button', {name: 'Mode'}).click();
+    await page.getByRole('menuitem', {name: 'click selector'}).click();
+
+    await page.getByRole('button', {name: 'Mode'}).click();
+    await page.getByRole('menuitem', {name: 'range selector'}).click();
+
+    await expect(page.locator('rect[id="1"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+    await expect(page.locator('rect[id="2"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+    await expect(page.locator('rect[id="3"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+  });
+
+  test('should check if click selection is reset when switching to range selection', async({ page }) => {
+    await page.getByRole('button', {name: 'Mode'}).click();
+    await page.getByRole('menuitem', {name: 'click selector'}).click();
+
+    await page.locator('rect[id="3"]').click();
+    await page.locator('rect[id="2"]').click();
+
+    await expect(page.locator('rect[id="3"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+
+    await page.getByRole('button', {name: 'Mode'}).click();
+    await page.getByRole('menuitem', {name: 'range selector'}).click();
+
+    await page.getByRole('button', {name: 'Mode'}).click();
+    await page.getByRole('menuitem', {name: 'click selector'}).click();
+
+    await expect(page.locator('rect[id="1"]')).toHaveCSS('fill', 'rgb(176, 196, 222)');
+    await expect(page.locator('rect[id="2"]')).toHaveCSS('fill', 'rgb(176, 196, 222)');
+    await expect(page.locator('rect[id="3"]')).toHaveCSS('fill', 'rgb(176, 196, 222)');
+  });
+
+  test('should check if range selection is reset when selecting other histogram', async({ page }) => {
+    await page.locator('[gpf-histogram-range-selector-line]').nth(0).hover();
+    await page.mouse.down();
+    await page.mouse.move(900, 400, {steps: 3});
+    await page.mouse.up();
+
+    await expect(page.locator('rect[id="1"]')).toHaveCSS('fill', 'rgb(176, 196, 222)');
+
+    await page.locator('gpf-gene-scores select').selectOption('RVIS score - Intolerance of a gene to genetic variants');
+    await page.locator('gpf-gene-scores select')
+      .selectOption('gene-score - Evidence strength supporting a gene\'s association with autism');
+
+    await expect(page.locator('rect[id="1"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+    await expect(page.locator('rect[id="2"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+    await expect(page.locator('rect[id="3"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+  });
+
+  test('should check if click selection is reset when selecting other histogram', async({ page }) => {
+    await page.getByRole('button', {name: 'Mode'}).click();
+    await page.getByRole('menuitem', {name: 'click selector'}).click();
+
+    await page.locator('rect[id="1"]').click();
+
+    await expect(page.locator('rect[id="1"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+
+    await page.locator('gpf-gene-scores select').selectOption('RVIS score - Intolerance of a gene to genetic variants');
+    await page.locator('gpf-gene-scores select')
+      .selectOption('gene-score - Evidence strength supporting a gene\'s association with autism');
+
+    await page.getByRole('button', {name: 'Mode'}).click();
+    await page.getByRole('menuitem', {name: 'click selector'}).click();
+
+    await expect(page.locator('rect[id="1"]')).toHaveCSS('fill', 'rgb(176, 196, 222)');
+    await expect(page.locator('rect[id="2"]')).toHaveCSS('fill', 'rgb(176, 196, 222)');
+    await expect(page.locator('rect[id="3"]')).toHaveCSS('fill', 'rgb(176, 196, 222)');
+  });
+
+  test('should set ranges and share query', async({ page }) => {
+    await page.locator('[gpf-histogram-range-selector-line]').nth(0).hover();
+    await page.mouse.down();
+    await page.mouse.move(900, 400, {steps: 3});
+    await page.mouse.up();
+
+    await page.getByRole('button', {name: 'Share/save query'}).click();
+    await expect(page.locator('#save-query-dropdown')).toBeVisible();
+    const shareLinkUrl = await page.locator('#link-input').inputValue();
+    await page.goto(shareLinkUrl, {waitUntil: 'load'});
+
+    await expect(page.locator('rect[id="1"]')).toHaveCSS('fill', 'rgb(176, 196, 222)');
+    await expect(page.locator('rect[id="2"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+    await expect(page.locator('rect[id="3"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+  });
+
+  test('should set ranges and download', async({ page }) => {
+    await page.locator('[gpf-histogram-range-selector-line]').nth(0).hover();
+    await page.mouse.down();
+    await page.mouse.move(900, 400, {steps: 3});
+    await page.mouse.up();
+
+    const downloadPromise = page.waitForEvent('download', { timeout: 180000 });
+    await page.getByRole('button', {name: 'Download'}).click();
+
+    const download = await downloadPromise;
+    const fixtureData = scanCSV('playwright/fixtures/gene-scores/variants1.tsv', {sep: '\t'});
+    const downloadData = scanCSV(await download.path(), {sep: '\t'});
+    const fixtureFrame = (await fixtureData.select(fixtureData.columns.sort()).collect()).sort('family id');
+    const downloadFrame = (await downloadData.select(downloadData.columns.sort()).collect()).sort('family id');
+    expect(fixtureFrame.toString()).toEqual(downloadFrame.toString());
+  });
+
+  test('should set ranges and check table results', async({ page }) => {
+    await page.locator('[gpf-histogram-range-selector-line]').nth(0).hover();
+    await page.mouse.down();
+    await page.mouse.move(900, 400, {steps: 3});
+    await page.mouse.up();
+
+    await page.getByRole('button', {name: 'Table Preview'}).click();
+    await expect(page.locator('#variants-count-span')).toHaveText('87 variants selected');
+  });
+
+  test('should click histogram bars and share query', async({ page }) => {
+    await page.getByRole('button', {name: 'Mode'}).click();
+    await page.getByRole('menuitem', {name: 'click selector'}).click();
+    await page.locator('rect[id="2"]').click();
+
+    await page.getByRole('button', {name: 'Share/save query'}).click();
+    await expect(page.locator('#save-query-dropdown')).toBeVisible();
+    const shareLinkUrl = await page.locator('#link-input').inputValue();
+    await page.goto(shareLinkUrl, {waitUntil: 'load'});
+
+    await expect(page.locator('rect[id="1"]')).toHaveCSS('fill', 'rgb(176, 196, 222)');
+    await expect(page.locator('rect[id="2"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+    await expect(page.locator('rect[id="3"]')).toHaveCSS('fill', 'rgb(176, 196, 222)');
+  });
+
+  test('should click histogram bars and download', async({ page }) => {
+    await page.getByRole('button', {name: 'Mode'}).click();
+    await page.getByRole('menuitem', {name: 'click selector'}).click();
+    await page.locator('rect[id="3"]').click();
+
+    const downloadPromise = page.waitForEvent('download', { timeout: 180000 });
+    await page.getByRole('button', {name: 'Download'}).click();
+
+    const download = await downloadPromise;
+    const fixtureData = scanCSV('playwright/fixtures/gene-scores/variants2.tsv', {sep: '\t'});
+    const downloadData = scanCSV(await download.path(), {sep: '\t'});
+    const fixtureFrame = (await fixtureData.select(fixtureData.columns.sort()).collect()).sort('family id');
+    const downloadFrame = (await downloadData.select(downloadData.columns.sort()).collect()).sort('family id');
+    expect(fixtureFrame.toString()).toEqual(downloadFrame.toString());
+  });
+
+  test('should click histogram bars and check table results', async({ page }) => {
+    await page.getByRole('button', {name: 'Mode'}).click();
+    await page.getByRole('menuitem', {name: 'click selector'}).click();
+    await page.locator('rect[id="3"]').click();
+
+    await page.getByRole('button', {name: 'Table Preview'}).click();
+    await expect(page.locator('#variants-count-span')).toHaveText('4 variants selected');
   });
 });
