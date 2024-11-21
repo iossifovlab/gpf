2addf961e6f9c5c9195b58c6eec037e5387b5c28
iordanivanov <iiordan.ivanov@gmail.com>
2024-11-21 15:15:15 +0200

    Refactor test_effect_annotator to use t4c8-style fixtures
    Part of removing the dependency on dae_conftests, as well as setup for
    future unit tests with more specific configuration

--- dae/dae/annotation/tests/test_effect_annotator.py
+++ dae/dae/annotation/tests/test_effect_annotator.py
@@ -1,22 +1,43 @@
 # pylint: disable=W0621,C0114,C0116,W0212,W0613,too-many-lines
+import pathlib
 import textwrap
+
+import pytest
 
 from dae.annotation.annotation_factory import load_pipeline_from_yaml
 from dae.genomic_resources.repository import GenomicResourceRepo
+from dae.genomic_resources.testing import (
+    build_filesystem_test_repository,
+    setup_gene_models,
+    setup_genome,
+)
+from dae.testing.t4c8_import import GENOME_CONTENT, GMM_CONTENT
 
 
-def test_effect_annotator_resources(
-        grr_fixture: GenomicResourceRepo) -> None:
-    genome = "hg19/GATK_ResourceBundle_5777_b37_phiX174_short/genome"
-    gene_models = "hg19/GATK_ResourceBundle_5777_b37_phiX174_short/" \
-        "gene_models/refGene_201309"
+@pytest.fixture()
+def grr(tmp_path: pathlib.Path) -> GenomicResourceRepo:
+    setup_genome(tmp_path / "t4c8_genome" / "chrAll.fa", GENOME_CONTENT)
+    setup_gene_models(
+        tmp_path / "t4c8_genes" / "genes.txt",
+        GMM_CONTENT,
+        config=textwrap.dedent("""
+            type: gene_models
+            filename: genes.txt
+            format: refflat
+        """))
+    return build_filesystem_test_repository(tmp_path)
+
+
+def test_effect_annotator_resources(grr: GenomicResourceRepo) -> None:
+    genome = "t4c8_genome"
+    gene_models = "t4c8_genes"
     config = textwrap.dedent(f"""
         - effect_annotator:
             genome: {genome}
             gene_models: {gene_models}
         """)
 
-    annotation_pipeline = load_pipeline_from_yaml(config, grr_fixture)
+    annotation_pipeline = load_pipeline_from_yaml(config, grr)
 
     with annotation_pipeline.open() as pipeline:
         annotator = pipeline.annotators[0]
@@ -26,17 +47,13 @@ def test_effect_annotator_resources(
         }
 
 
-def test_effect_annotator_documentation(
-        grr_fixture: GenomicResourceRepo) -> None:
-
-    pipeline = load_pipeline_from_yaml(textwrap.dedent(
-        """
+def test_effect_annotator_documentation(grr: GenomicResourceRepo) -> None:
+    pipeline_config = textwrap.dedent("""
         - effect_annotator:
-            genome: hg19/GATK_ResourceBundle_5777_b37_phiX174_short/genome
-            gene_models: hg19/GATK_ResourceBundle_5777_b37_phiX174_short/gene_models/refGene_201309
-        """),  # noqa
-        grr_fixture)
-
+            genome: t4c8_genome
+            gene_models: t4c8_genes
+        """)
+    pipeline = load_pipeline_from_yaml(pipeline_config, grr)
     att = pipeline.get_attribute_info("worst_effect")
     assert att is not None
     assert "Worst" in att.documentation
