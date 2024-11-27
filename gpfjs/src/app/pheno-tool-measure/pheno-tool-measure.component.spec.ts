2df2e01e16f24dc7221cdfa6e693b5ffc085ffc9
IlinaGergova <ili.gergova@gmail.com>
2024-11-27 13:57:09 +0200

    add pheno tool measure tests
    * test setting and getting selected measure
    * mock datasets service and measures service
    * test getting dataset id from state, then getting
    the whole dataset and regressions in ngOnInit
    * test selecting and deselecting normalization
    * test if a measure is normalized

--- gpfjs/src/app/pheno-tool-measure/pheno-tool-measure.component.spec.ts
+++ gpfjs/src/app/pheno-tool-measure/pheno-tool-measure.component.spec.ts
@@ -12,11 +12,51 @@ import { Observable, of } from 'rxjs';
 import { DatasetsService } from 'app/datasets/datasets.service';
 import { Store, StoreModule } from '@ngrx/store';
 import { phenoToolMeasureReducer } from './pheno-tool-measure.state';
-import { Dataset } from 'app/datasets/datasets';
+import { ContinuousMeasure } from 'app/measures/measures';
+import { Dataset, GeneBrowser } from 'app/datasets/datasets';
 
-class MockDatasetService {
-  public getDatasets(): Observable<Dataset[]> {
-    return of([] as Dataset[]);
+const datasetMock = new Dataset(
+  'id1',
+  'name1',
+  ['parent1', 'parent2'],
+  false,
+  ['study1', 'study2'],
+  ['studyName1', 'studyName2'],
+  ['studyType1', 'studyType2'],
+  'phenotypeData1',
+  false,
+  true,
+  true,
+  false,
+  {enabled: true},
+  null,
+  null,
+  null,
+  new GeneBrowser(true, 'frequencyCol1', 'frequencyName1', 'effectCol1', 'locationCol1', 5, 6, true),
+  false,
+  'genome1',
+  true
+);
+
+const regressionMock = {
+  age: {
+    display_name: 'age at assessment',
+    instrument_name: 'pheno_common',
+    measure_name: 'age_at_assessment'
+  }
+};
+
+class DatasetsServiceMock {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public getDataset(datasetId: string): Observable<Dataset> {
+    return of(datasetMock);
+  }
+}
+
+class MeasuresServiceMock {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public getRegressions(datasetId: string): Observable<object> {
+    return of(regressionMock);
   }
 }
 
@@ -24,6 +64,8 @@ describe('PhenoToolMeasureComponent', () => {
   let component: PhenoToolMeasureComponent;
   let fixture: ComponentFixture<PhenoToolMeasureComponent>;
   let store: Store;
+  const measuresServiceMock = new MeasuresServiceMock();
+  const datasetsServiceMock = new DatasetsServiceMock();
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -36,9 +78,9 @@ describe('PhenoToolMeasureComponent', () => {
         HttpHandler,
         ConfigService,
         UsersService,
-        { provide: APP_BASE_HREF, useValue: '' },
-        { provide: DatasetsService, useValue: new MockDatasetService() },
-
+        { provide: DatasetsService, useValue: datasetsServiceMock },
+        { provide: MeasuresService, useValue: measuresServiceMock },
+        { provide: APP_BASE_HREF, useValue: '' }
       ],
       imports: [RouterTestingModule, StoreModule.forRoot({phenoToolMeasure: phenoToolMeasureReducer})],
       schemas: [NO_ERRORS_SCHEMA]
@@ -59,5 +101,159 @@ describe('PhenoToolMeasureComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get regressions and selected dataset', () => {
+    component.ngOnInit();
+    expect(component.dataset).toStrictEqual(datasetMock);
+    expect(component.regressions).toStrictEqual(regressionMock);
+    expect(component.regressionNames).toStrictEqual(['age']);
+  });
+
+  it('should set measure', () => {
+    const updateStateSpy = jest.spyOn(component, 'updateState');
+    component.normalizeBy = [
+      {
+        display_name: 'd1',
+        instrument_name: 'i1',
+        measure_name: 'm1'
+      },
+      {
+        display_name: 'd2',
+        instrument_name: 'i2',
+        measure_name: 'm2'
+      }
+    ];
+    component.measure = new ContinuousMeasure('i1.m1', 0, 10);
+    expect(component.normalizeBy).toStrictEqual([{
+      display_name: 'd2',
+      instrument_name: 'i2',
+      measure_name: 'm2'
+    }]);
+    expect(component.selectedMeasure).toStrictEqual(new ContinuousMeasure('i1.m1', 0, 10));
+    expect(updateStateSpy).toHaveBeenCalledWith();
+  });
+
+  it('should get measure', () => {
+    component.selectedMeasure = new ContinuousMeasure('i1.m1', 0, 10);
+    expect(component.measure).toStrictEqual(new ContinuousMeasure('i1.m1', 0, 10));
+  });
+
+  it('should check normalize by', () => {
+    const updateStateSpy = jest.spyOn(component, 'updateState');
+    component.normalizeBy = [
+      {
+        display_name: 'd1',
+        instrument_name: 'i1',
+        measure_name: 'm1'
+      },
+      {
+        display_name: 'd2',
+        instrument_name: 'i2',
+        measure_name: 'm2'
+      }
+    ];
+    const mockCheckEvent = {
+      target: {
+        checked: true
+      }
+    };
+
+    component.onNormalizeByChange({
+      display_name: 'd1',
+      instrument_name: 'i1',
+      measure_name: 'm1'
+    }, mockCheckEvent);
+    expect(component.normalizeBy).toStrictEqual([
+      {
+        display_name: 'd1',
+        instrument_name: 'i1',
+        measure_name: 'm1'
+      },
+      {
+        display_name: 'd2',
+        instrument_name: 'i2',
+        measure_name: 'm2'
+      }
+    ]);
+
+    expect(updateStateSpy).toHaveBeenCalledWith();
+
+    component.onNormalizeByChange({
+      display_name: 'd3',
+      instrument_name: 'i3',
+      measure_name: 'm3'
+    }, mockCheckEvent);
+    expect(component.normalizeBy).toStrictEqual([
+      {
+        display_name: 'd1',
+        instrument_name: 'i1',
+        measure_name: 'm1'
+      },
+      {
+        display_name: 'd2',
+        instrument_name: 'i2',
+        measure_name: 'm2'
+      },
+      {
+        display_name: 'd3',
+        instrument_name: 'i3',
+        measure_name: 'm3'
+      }
+    ]);
+  });
+
+  it('should uncheck normalize by', () => {
+    const updateStateSpy = jest.spyOn(component, 'updateState');
+    component.normalizeBy = [
+      {
+        display_name: 'd1',
+        instrument_name: 'i1',
+        measure_name: 'm1'
+      },
+      {
+        display_name: 'd2',
+        instrument_name: 'i2',
+        measure_name: 'm2'
+      }
+    ];
+    const mockCheckEvent = {
+      target: {
+        checked: false
+      }
+    };
+
+    component.onNormalizeByChange({
+      display_name: 'd1',
+      instrument_name: 'i1',
+      measure_name: 'm1'
+    }, mockCheckEvent);
+    expect(component.normalizeBy).toStrictEqual([
+      {
+        display_name: 'd2',
+        instrument_name: 'i2',
+        measure_name: 'm2'
+      }
+    ]);
+
+    expect(updateStateSpy).toHaveBeenCalledWith();
+  });
+
+  it('should check if measure is normalized by', () => {
+    component.normalizeBy = [
+      {
+        display_name: 'd1',
+        instrument_name: 'i1',
+        measure_name: 'm1'
+      },
+      {
+        display_name: 'd2',
+        instrument_name: 'i2',
+        measure_name: 'm2'
+      }
+    ];
+
+    expect(component.isNormalizedBy('m1')).toBe(true);
+    expect(component.isNormalizedBy('m111')).toBe(false);
   });
 });
