9a38b653492e277b9195891bb304a4196220fe58
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2024-11-27 11:11:19 +0200

    Support for read-only directory repository
    We started supporting read-only filesystem genomic resources
    repository. When creating a filesystem repository by default it
    is a read-write repository.
    
    One can add a `read_only: true` configuration parameter to the
    repository config:
    
    ```
    id: local
    type: directory
    directory: /path/to/repository/directory
    read_only: true
    ```

--- dae/dae/genomic_resources/tests/test_repository_factory.py
+++ dae/dae/genomic_resources/tests/test_repository_factory.py
@@ -6,12 +6,20 @@ import yaml
 from dae.genomic_resources import build_genomic_resource_repository
 from dae.genomic_resources.cached_repository import GenomicResourceCachedRepo
 from dae.genomic_resources.fsspec_protocol import (
+    FsspecReadOnlyProtocol,
+    FsspecReadWriteProtocol,
     FsspecRepositoryProtocol,
 )
 from dae.genomic_resources.genomic_scores import PositionScore
 from dae.genomic_resources.group_repository import GenomicResourceGroupRepo
 from dae.genomic_resources.repository import (
+    GR_CONF_FILE_NAME,
     GenomicResourceProtocolRepo,
+)
+from dae.genomic_resources.testing import (
+    build_filesystem_test_repository,
+    convert_to_tab_separated,
+    setup_directories,
 )
 
 
@@ -215,3 +223,51 @@ def test_build_a_configuration_with_embedded() -> None:
     score = PositionScore(res)
     score.open()
     assert score.fetch_scores("chr1", 23) == [0.01]
+
+
+def test_read_only_filesystem_repo(
+    tmp_path: pathlib.Path,
+) -> None:
+    setup_directories(
+        tmp_path / "grr", {
+            "allele_score": {
+                GR_CONF_FILE_NAME: """
+                    type: allele_score
+                    table:
+                        filename: data.txt
+                        reference:
+                          name: reference
+                        alternative:
+                          name: alternative
+                    scores:
+                        - id: freq
+                          type: float
+                          desc: ""
+                          name: freq
+                    default_annotation:
+                    - source: freq
+                      name: allele_freq
+
+                """,
+                "data.txt": convert_to_tab_separated("""
+                    chrom  pos_begin  reference  alternative  freq
+                    1      10         A          G            0.02
+                """),
+            },
+        })
+    repo = build_filesystem_test_repository(tmp_path / "grr")
+    assert repo is not None
+    assert isinstance(repo, GenomicResourceProtocolRepo)
+    assert isinstance(repo.proto, FsspecRepositoryProtocol)
+    assert isinstance(repo.proto, FsspecReadWriteProtocol)
+
+    repo_ro = build_genomic_resource_repository({
+        "id": "allele_score_local",
+        "type": "directory",
+        "directory": str(tmp_path / "grr"),
+        "read_only": True,
+    })
+    assert repo_ro is not None
+    assert isinstance(repo_ro, GenomicResourceProtocolRepo)
+    assert isinstance(repo_ro.proto, FsspecRepositoryProtocol)
+    assert isinstance(repo_ro.proto, FsspecReadOnlyProtocol)
