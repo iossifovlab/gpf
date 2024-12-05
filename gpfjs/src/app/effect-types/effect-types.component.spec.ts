cf002d1e5d89ec45b46293ce1a2c571ade0c75ed
IlinaGergova <ili.gergova@gmail.com>
2024-12-05 13:51:41 +0200

    add effect types unit tests
    * test setting default types when state is empty
    * test setting all types in pheno tool except cnv types

--- gpfjs/src/app/effect-types/effect-types.component.spec.ts
+++ gpfjs/src/app/effect-types/effect-types.component.spec.ts
@@ -3,10 +3,11 @@ import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { EffectTypesComponent } from './effect-types.component';
 import { EffecttypesColumnComponent } from './effect-types-column.component';
-import { ALL, CODING, EffectTypes, LGDS, NONSYNONYMOUS, UTRS } from './effect-types';
+import { ALL, CODING, EffectTypes, GENOTYPE_BROWSER_INITIAL_VALUES, LGDS, NONSYNONYMOUS, UTRS } from './effect-types';
 import { of } from 'rxjs';
 import { addEffectType, effectTypesReducer, removeEffectType, setEffectTypes } from './effect-types.state';
 import { Store, StoreModule } from '@ngrx/store';
+import { PHENO_TOOL_ALL } from 'app/pheno-tool-effect-types/pheno-tool-effect-types';
 
 describe('EffectTypesComponent', () => {
   let component: EffectTypesComponent;
@@ -48,6 +49,16 @@ describe('EffectTypesComponent', () => {
     component.ngOnInit();
   });
 
+  it('should set default default effect types when state is empty', () => {
+    jest.spyOn(store, 'select').mockReturnValueOnce(of(null));
+    const dispatchSpy = jest.spyOn(store, 'dispatch');
+    component.ngOnInit();
+
+    expect(component.effectTypes.selected).toStrictEqual(GENOTYPE_BROWSER_INITIAL_VALUES);
+
+    expect(dispatchSpy).toHaveBeenCalledWith(setEffectTypes({ effectTypes: [...GENOTYPE_BROWSER_INITIAL_VALUES]}));
+  });
+
   it('should initialize button groups', () => {
     component.effectTypesButtons = undefined;
     component['initButtonGroups']();
@@ -59,7 +70,7 @@ describe('EffectTypesComponent', () => {
       .set('CODING', CODING)
       .set('NONSYNONYMOUS', NONSYNONYMOUS)
       .set('UTRS', UTRS);
-    expect(component.effectTypesButtons).toEqual(expectedMap);
+    expect(component.effectTypesButtons).toStrictEqual(expectedMap);
   });
 
   it('should update variant types', () => {
@@ -120,5 +131,36 @@ describe('EffectTypesComponent', () => {
     expect(component.effectTypes.selected).toStrictEqual(new Set(['testEffect', 'newTestEffect']));
     // but the original set is not modified
     expect(initial).toStrictEqual(new Set(['testEffect']));
+  });
+
+  it('should select LGDs button group', () => {
+    const setEffectTypesSpy = jest.spyOn(component, 'setEffectTypes');
+
+    component.selectButtonGroup('LGDS');
+
+    expect(setEffectTypesSpy).toHaveBeenCalledWith(new Set([
+      'frame-shift',
+      'nonsense',
+      'splice-site',
+      'no-frame-shift-newStop',
+    ]));
+  });
+
+  it('should select All button group in pheno tool', () => {
+    const setEffectTypesSpy = jest.spyOn(component, 'setEffectTypes');
+    component.variantTypes = new Set<string>(['PHENO_TOOL_ALL']);
+    component.effectTypesButtons.set('PHENO_TOOL_ALL', PHENO_TOOL_ALL);
+
+    component.selectButtonGroup('PHENO_TOOL_ALL');
+
+    expect(setEffectTypesSpy).toHaveBeenCalledWith(new Set([
+      'Missense',
+      'Nonsynonymous',
+      'Synonymous',
+      'LGDs',
+      'Nonsense',
+      'Frame-shift',
+      'Splice-site'
+    ]));
   });
 });
