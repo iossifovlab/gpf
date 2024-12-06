8c5a6d675887b98e75e93725f3b32caba7a24646
IvoStefanov <ivostefanovtues@gmail.com>
2024-12-06 12:58:22 +0200

    Update gene profiles api statistics test
    Expand test fixture to make the test better.

--- wdae/wdae/gene_profiles_api/tests/test_agp_api.py
+++ wdae/wdae/gene_profiles_api/tests/test_agp_api.py
@@ -44,13 +44,106 @@ def test_get_statistic(
     t4c8_wgpf_instance: WGPFInstance,
     mocker: MockerFixture,
 ) -> None:
+    mock_gp_statistic = GPStatistic(
+        "CHD8",
+        ["gene_set_1", "gene_set_2"],
+        {
+            "category_1": {
+                "genomic_score_1": {
+                    "score_1": {
+                        "value_1": 1,
+                        "value_2": 2,
+                    },
+                },
+            },
+        },
+        {
+            "study_1": {
+                "autism": {
+                    "lgds": {
+                        "count": 1.0,
+                        "rate": 1000.0,
+                    },
+                    "denovo_missense": {
+                        "count": 1.0,
+                        "rate": 1000.0,
+                    },
+                },
+                "unaffected": {
+                    "lgds": {
+                        "count": 0.0,
+                        "rate": 0.0,
+                    },
+                    "denovo_missense": {
+                        "count": 2.0,
+                        "rate": 2000.0,
+                    },
+                },
+            },
+        },
+    )
     mocker.patch.object(
         t4c8_wgpf_instance, "get_gp_statistic",
-        return_value=GPStatistic("CHD8", [], {}, {}),
+        return_value=mock_gp_statistic,
     )
 
     response = admin_client.get(f"{ROUTE_PREFIX}/single-view/gene/CHD8")
     assert response.data["geneSymbol"] == "CHD8"  # type: ignore
+    assert response.data["geneSets"] == ["gene_set_1", "gene_set_2"]  # type: ignore
+    assert response.data["genomicScores"] == [{  # type: ignore
+        "id": "category_1",
+        "scores": [{
+            "id": "genomic_score_1",
+            "score_1": {
+                "value_1": 1,
+                "value_2": 2,
+            },
+        }],
+    }]
+    assert response.data["studies"] == [{  # type: ignore
+        "id": "study_1",
+        "personSets": [
+            {
+                "id": "autism",
+                "effectTypes": [
+                    {
+                        "id": "lgds",
+                        "value": {
+                            "count": 1.0,
+                            "rate": 1000.0,
+                        },
+                    },
+                    {
+                        "id": "denovo_missense",
+                        "value": {
+                            "count": 1.0,
+                            "rate": 1000.0,
+                        },
+                    },
+                ],
+            },
+            {
+                "id": "unaffected",
+                "effectTypes": [
+                    {
+                        "id": "lgds",
+                        "value": {
+                            "count": 0.0,
+                            "rate": 0.0,
+                        },
+                    },
+                    {
+                        "id": "denovo_missense",
+                        "value": {
+                            "count": 2.0,
+                            "rate": 2000.0,
+                        },
+                    },
+                ],
+            },
+        ],
+    }]
+
     assert response.status_code == 200
     print(response.data)  # type: ignore
 
