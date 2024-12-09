803b063a38c958fee78f5a905b79b904cd447905
IlinaGergova <ili.gergova@gmail.com>
2024-12-09 13:59:13 +0200

    update datasets service test
    * add dataset mock
    * clear mock calls in test

--- gpfjs/src/app/datasets/datasets.service.spec.ts
+++ gpfjs/src/app/datasets/datasets.service.spec.ts
@@ -6,10 +6,58 @@ import { DatasetsService } from 'app/datasets/datasets.service';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { UsersService } from 'app/users/users.service';
 import { RouterTestingModule } from '@angular/router/testing';
-import { Dataset } from './datasets';
+import { Dataset, GeneBrowser, PersonSet, PersonSetCollection, PersonSetCollections } from './datasets';
 import { lastValueFrom, of, take } from 'rxjs';
 import { APP_BASE_HREF } from '@angular/common';
 import { StoreModule } from '@ngrx/store';
+import { UserGroup } from 'app/users-groups/users-groups';
+
+const datasetMock = new Dataset(
+  'id1',
+  'name1',
+  ['parent1', 'parent2'],
+  false,
+  ['study1', 'study2'],
+  ['studyName1', 'studyName2'],
+  ['studyType1', 'studyType2'],
+  'phenotypeData1',
+  false,
+  true,
+  true,
+  false,
+  {enabled: true},
+  null,
+  new PersonSetCollections(
+    [
+      new PersonSetCollection(
+        'id1',
+        'name1',
+        [
+          new PersonSet('id1', 'name1', 'color1'),
+          new PersonSet('id1', 'name2', 'color3'),
+          new PersonSet('id2', 'name3', 'color4')
+        ]
+      ),
+      new PersonSetCollection(
+        'id2',
+        'name2',
+        [
+          new PersonSet('id2', 'name2', 'color2'),
+          new PersonSet('id2', 'name3', 'color5'),
+          new PersonSet('id3', 'name4', 'color6')
+        ]
+      )
+    ]
+  ),
+  [
+    new UserGroup(3, 'name1', ['user1', 'user2'], [{datasetName: 'dataset2', datasetId: 'dataset3'}]),
+    new UserGroup(5, 'name2', ['user12', 'user5'], [{datasetName: 'dataset1', datasetId: 'dataset2'}])
+  ],
+  new GeneBrowser(true, 'frequencyCol1', 'frequencyName1', 'effectCol1', 'locationCol1', 5, 6, true),
+  false,
+  'genome1',
+  true
+);
 
 describe('DatasetService', () => {
   let service: DatasetsService;
@@ -25,7 +73,6 @@ describe('DatasetService', () => {
       ],
       declarations: [],
     }).compileComponents();
-    jest.clearAllMocks();
 
     service = TestBed.inject(DatasetsService);
   }));
@@ -45,15 +92,17 @@ describe('DatasetService', () => {
 
   it('should get dataset', async() => {
     const datasetFromJsonSpy = jest.spyOn(Dataset, 'fromDatasetAndDetailsJson');
-    datasetFromJsonSpy.mockReturnValue('fakeDataset' as any);
+    datasetFromJsonSpy.mockReturnValue(datasetMock);
     const httpGetSpy = jest.spyOn(HttpClient.prototype, 'get');
-    httpGetSpy.mockReturnValue(of('fakeResponse'));
+    httpGetSpy.mockReturnValue(of(datasetMock));
+
+    jest.clearAllMocks();
 
     const response = await lastValueFrom(service.getDataset('geneSymbol').pipe(take(1)));
-    expect(response).toBe('fakeDataset' as any);
+    expect(response).toBe(datasetMock);
     expect(httpGetSpy.mock.calls[0][0]).toBe('testUrl/datasets/geneSymbol');
     expect(httpGetSpy.mock.calls[1][0]).toBe('testUrl/datasets/details/geneSymbol');
     // eslint-disable-next-line jest/prefer-strict-equal
-    expect(datasetFromJsonSpy.mock.calls).toEqual([[undefined, 'fakeResponse' as any]]);
+    expect(datasetFromJsonSpy.mock.calls).toEqual([[undefined, datasetMock]]);
   });
 });
