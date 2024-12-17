251738cbbd19932bd2504a59fe3284c08a31e4cc
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2024-12-17 10:53:03 +0200

    Fix the name of VEP annotator plugin package

--- external_vep_annotator/setup.py
+++ external_vep_annotator/setup.py
@@ -4,7 +4,7 @@ import setuptools
 import versioneer
 
 setuptools.setup(
-    name="gpf_external_demo_annotator",
+    name="gpf_vep_annotator",
     version=versioneer.get_version(),  # type: ignore
     cmdclass=versioneer.get_cmdclass(),  # type: ignore
     author="Lubomir Chorbadjiev",
