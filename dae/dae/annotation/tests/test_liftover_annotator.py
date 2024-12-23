c00f371b6f2f3f0ada728f8ea253b2fdb4dde8df
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2024-12-23 17:22:49 +0200

    Clean up usage of gpf_instance_2013 fixture

--- dae/dae/annotation/tests/test_liftover_annotator.py
+++ dae/dae/annotation/tests/test_liftover_annotator.py
@@ -1,10 +1,8 @@
 # pylint: disable=W0621,C0114,C0116,W0212,W0613
 import textwrap
-from typing import cast
 
 import pytest
 
-from dae.annotation.annotatable import VCFAllele
 from dae.annotation.annotation_factory import (
     load_pipeline_from_file,
     load_pipeline_from_yaml,
@@ -14,7 +12,6 @@ from dae.genomic_resources.testing import (
     build_filesystem_test_repository,
     setup_directories,
 )
-from dae.gpf_instance import GPFInstance
 from dae.variants.core import Allele
 
 
@@ -59,35 +56,6 @@ def test_pipeline_liftover(
         allele = Allele.build_vcf_allele("chr1", 69094, "G", "A")
         attributes = work_pipeline.annotate(allele.get_annotatable())
         assert attributes.get("mpc") is not None
-
-
-@pytest.mark.parametrize("chrom,pos,ref,alt,expected", [
-    ("1", 13199996, "C", "T", VCFAllele("chr1", 13132543, "T", "C")),
-    ("1", 13047220, "C", "G", None),
-    ("1", 12892619, "C", "T", VCFAllele("chr1", 12832765, "T", "C")),
-    ("1", 2690489, "C", "G", VCFAllele("chr1", 2773943, "G", "C")),
-])
-def test_liftover_annotator_denovo_db_examples(
-    gpf_instance_2013: GPFInstance,
-    chrom: str, pos: int, ref: str, alt: str,
-    expected: VCFAllele | None,
-) -> None:
-
-    pipeline_config = textwrap.dedent("""
-        - liftover_annotator:
-            chain: liftover/hg19ToHg38
-            source_genome: hg19/genomes/GATK_ResourceBundle_5777_b37_phiX174
-            target_genome: hg38/genomes/GRCh38-hg38
-        """)
-
-    pipeline = load_pipeline_from_yaml(pipeline_config, gpf_instance_2013.grr)
-
-    allele = VCFAllele(chrom, pos, ref, alt)
-
-    result = pipeline.annotate(allele)
-
-    liftover_allele = cast(VCFAllele, result.get("liftover_annotatable"))
-    assert liftover_allele == expected
 
 
 def test_liftover_annotator_resources(
