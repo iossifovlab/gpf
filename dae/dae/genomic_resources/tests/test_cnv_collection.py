9f1fd5b320826fba02484aaefeda850f1550ff52
IvoStefanov <ivostefanovtues@gmail.com>
2025-01-30 14:04:46 +0200

    Test cnv collection impl do_histogram

--- dae/dae/genomic_resources/tests/test_cnv_collection.py
+++ dae/dae/genomic_resources/tests/test_cnv_collection.py
@@ -1,17 +1,31 @@
 # pylint: disable=W0621,C0114,C0116,W0212,W0613
+import pathlib
+import textwrap
 from typing import Any, cast
 
 import pytest
 import pytest_mock
 
+from dae.genomic_resources.cli import cli_manage
+from dae.genomic_resources.fsspec_protocol import FsspecReadWriteProtocol
 from dae.genomic_resources.genomic_scores import (
     CnvCollection,
+)
+from dae.genomic_resources.histogram import (
+    HistogramConfig,
+    NumberHistogram,
+    build_histogram_config,
 )
 from dae.genomic_resources.implementations.genomic_scores_impl import (
     CnvCollectionImplementation,
 )
 from dae.genomic_resources.repository import GR_CONF_FILE_NAME, GenomicResource
-from dae.genomic_resources.testing import build_inmemory_test_resource
+from dae.genomic_resources.testing import (
+    build_filesystem_test_protocol,
+    build_inmemory_test_resource,
+    setup_directories,
+    setup_tabix,
+)
 from dae.task_graph.executor import SequentialExecutor, task_graph_run
 from dae.task_graph.graph import TaskGraph
 
@@ -51,6 +65,44 @@ def cnvs_resource() -> GenomicResource:
     })
     assert res.get_type() == "cnv_collection"
     return res
+
+
+@pytest.fixture
+def proto_fixture(
+    tmp_path_factory: pytest.TempPathFactory,
+) -> tuple[pathlib.Path, FsspecReadWriteProtocol]:
+    path = tmp_path_factory.mktemp("cli_info_repo_fixture")
+    setup_directories(
+        path,
+        {
+            "one": {
+                GR_CONF_FILE_NAME: textwrap.dedent("""
+                    type: cnv_collection
+                    table:
+                        filename: data.txt.gz
+                        format: tabix
+                    scores:
+                        - id: phastCons100way
+                          type: float
+                          name: s1
+                          histogram:
+                            type: number
+                            number_of_bins: 5
+                    """),
+            },
+        })
+    setup_tabix(
+        path / "one" / "data.txt.gz",
+        """
+        #chrom  pos_begin  pos_end  s1    s2    frequency
+        1       10         20       0.2   1.2   1
+        1       30         50       0.3   1.3   2
+        1       100        120      0.4   1.4   3
+        2       10         50       0.1   2.1   4
+        2       70         200      0.2   2.2   2
+        """, seq_col=0, start_col=1, end_col=2)
+    proto = build_filesystem_test_protocol(path)
+    return path, proto
 
 
 @pytest.fixture
@@ -150,3 +202,45 @@ def test_cnv_collection_implementation(
 
     info = cnvs_impl.get_statistics_info()
     assert "Filename" in info
+
+
+def test_cnv_collection_implementation_histogram(
+    cnvs_resource: GenomicResource,
+) -> None:
+    hist_conf = build_histogram_config({
+        "histogram": {
+            "type": "number",
+            "view_range": {"min": 0, "max": 0.3},
+            "number_of_bins": 2,
+        },
+    })
+    assert isinstance(hist_conf, HistogramConfig)
+
+    hist_confs = {"freq": hist_conf}
+
+    histograms = CnvCollectionImplementation._do_histogram(
+        cnvs_resource, hist_confs, "2", 0, 300,
+    )
+
+    assert isinstance(histograms["freq"], NumberHistogram)
+    assert histograms["freq"].min_value == 1e-05
+    assert histograms["freq"].max_value == 0.3
+    assert histograms["freq"].bars.tolist() == [2, 1]
+    assert histograms["freq"].bins.tolist() == [0, 0.15, 0.3]
+
+
+def test_cli_manage_cnv_collection_histograms(
+     proto_fixture: tuple[pathlib.Path, FsspecReadWriteProtocol],
+) -> None:
+    path, _proto = proto_fixture
+    assert not (path / "one/index.html").exists()
+
+    cli_manage(["resource-repair", "-R", str(path), "-r", "one", "-j", "1"])
+
+    assert (path / "one/statistics").exists()
+
+    hist_file = (
+        path / "one/statistics/histogram_phastCons100way.json"
+    ).read_text().replace(" ", "").replace("\n", "")
+
+    assert hist_file.find('"bars":[1,2,0,1,1]') != -1
