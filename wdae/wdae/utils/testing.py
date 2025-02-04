9b1a116b8ea19eaed435f00cf8009ab00ade190e
IvoTod <ivo.tod.todorov@gmail.com>
2025-02-04 16:00:57 +0200

    Fix WDAE pheno fixture

--- wdae/wdae/utils/testing.py
+++ wdae/wdae/utils/testing.py
@@ -862,8 +862,8 @@ s4,121.0199895975403,39.74107684421966,77.32212831797972,51.37116746952451,36.55
         "--regression", str(pheno_path / "regressions.yaml"),
     ])
     build_browser([
-        str(instance_path / "pheno"),
-        "--phenotype-data-id", "study_1_pheno",
+        "study_1_pheno",
+        "--gpf-instance", str(instance_path / "gpf_instance.yaml"),
         "-j", "1",
         "--force",
         "--task-status-dir", str(pheno_path / "status"),
