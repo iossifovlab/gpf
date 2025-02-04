340942c8de4145f72b2d30f23af59c837a49e69e
IvoStefanov <ivostefanovtues@gmail.com>
2025-02-04 15:16:49 +0200

    Refactor test cnv collection fixtures

--- dae/dae/genomic_resources/tests/test_cnv_collection.py
+++ dae/dae/genomic_resources/tests/test_cnv_collection.py
@@ -7,7 +7,6 @@ import pytest
 import pytest_mock
 
 from dae.genomic_resources.cli import cli_manage
-from dae.genomic_resources.fsspec_protocol import FsspecReadWriteProtocol
 from dae.genomic_resources.genomic_scores import (
     CnvCollection,
 )
@@ -19,95 +18,87 @@ from dae.genomic_resources.histogram import (
 from dae.genomic_resources.implementations.genomic_scores_impl import (
     CnvCollectionImplementation,
 )
-from dae.genomic_resources.repository import GR_CONF_FILE_NAME, GenomicResource
+from dae.genomic_resources.repository import (
+    GenomicResource,
+    GenomicResourceRepo,
+)
+from dae.genomic_resources.repository_factory import (
+    build_genomic_resource_repository,
+)
 from dae.genomic_resources.testing import (
-    build_filesystem_test_protocol,
-    build_inmemory_test_resource,
+    convert_to_tab_separated,
     setup_directories,
-    setup_tabix,
 )
 from dae.task_graph.executor import SequentialExecutor, task_graph_run
 from dae.task_graph.graph import TaskGraph
 
 
 @pytest.fixture
-def cnvs_resource() -> GenomicResource:
-    res = build_inmemory_test_resource({
-        GR_CONF_FILE_NAME: """
-            type: cnv_collection
-            table:
-                filename: data.mem
-            scores:
-                - id: freq
-                  name: frequency
-                  type: float
-                  desc: some populaton frequency
-                - id: collection
-                  name: collection
-                  type: str
-                  desc: SSC or AGRE
-                - id: status
-                  name: affected_status
-                  type: str
-                  desc: |
-                    shows if the child that has the de novo
-                    is affected or unaffected
-        """,
-        "data.mem": """
-            chrom  pos_begin  pos_end  frequency  collection affected_status
-            1      10         20       0.02       SSC        affected
-            1      50         100      0.1        SSC        affected
-            2      1          8        0.00001    AGRE       unaffected
-            2      16         20       0.3        SSC        affected
-            2      200        203      0.0002     AGRE       unaffected
-            15     16         20       0.2        AGRE       affected
-        """,
-    })
-    assert res.get_type() == "cnv_collection"
-    return res
-
-
-@pytest.fixture
-def proto_fixture(
-    tmp_path_factory: pytest.TempPathFactory,
-) -> tuple[pathlib.Path, FsspecReadWriteProtocol]:
-    path = tmp_path_factory.mktemp("cli_info_repo_fixture")
+def test_grr(tmp_path: pathlib.Path) -> GenomicResourceRepo:
+    root_path = tmp_path
     setup_directories(
-        path,
-        {
-            "one": {
-                GR_CONF_FILE_NAME: textwrap.dedent("""
-                    type: cnv_collection
-                    table:
-                        filename: data.txt.gz
-                        format: tabix
-                    scores:
-                        - id: phastCons100way
-                          type: float
-                          name: s1
-                          histogram:
-                            type: number
-                            number_of_bins: 5
+        root_path, {
+            "grr.yaml": textwrap.dedent(f"""
+                id: reannotation_repo
+                type: dir
+                directory: "{root_path}/grr"
+            """),
+            "grr": {
+                "score_one": {
+                    "genomic_resource.yaml": textwrap.dedent("""
+                        type: cnv_collection
+                        table:
+                            filename: data.txt
+                        scores:
+                            - id: freq
+                              name: frequency
+                              type: float
+                              histogram:
+                                type: number
+                                number_of_bins: 3
+                                view_range:
+                                  min: 0
+                                  max: 1
+                                x_log_scale: false
+                                y_log_scale: true
+                              desc: some populaton frequency
+                            - id: collection
+                              name: collection
+                              type: str
+                              desc: SSC or AGRE
+                            - id: status
+                              name: affected_status
+                              type: str
+                              desc: |
+                                shows if the child that has the de novo
+                                is affected or unaffected
                     """),
+                    "data.txt": convert_to_tab_separated(textwrap.dedent("""
+                chrom  pos_begin  pos_end  frequency  collection affected_status
+                1      10         20       0.02       SSC        affected
+                1      50         100      0.1        SSC        affected
+                2      1          8        0.00001    AGRE       unaffected
+                2      16         20       0.3        SSC        affected
+                2      200        203      0.0002     AGRE       unaffected
+                15     16         20       0.2        AGRE       affected
+                    """)),
+                },
             },
-        })
-    setup_tabix(
-        path / "one" / "data.txt.gz",
-        """
-        #chrom  pos_begin  pos_end  s1    s2    frequency
-        1       10         20       0.2   1.2   1
-        1       30         50       0.3   1.3   2
-        1       100        120      0.4   1.4   3
-        2       10         50       0.1   2.1   4
-        2       70         200      0.2   2.2   2
-        """, seq_col=0, start_col=1, end_col=2)
-    proto = build_filesystem_test_protocol(path)
-    return path, proto
+        },
+    )
+    return build_genomic_resource_repository(file_name=str(
+        root_path / "grr.yaml",
+    ))
 
 
 @pytest.fixture
-def cnvs(cnvs_resource: GenomicResource) -> CnvCollection:
-    return CnvCollection(cnvs_resource)
+def cnvs(test_grr: GenomicResourceRepo) -> CnvCollection:
+    return CnvCollection(test_grr.get_resource("score_one"))
+
+
+@pytest.fixture
+def cnvs_resource(test_grr: GenomicResourceRepo) -> GenomicResource:
+    return test_grr.get_resource("score_one")
 
 
 @pytest.mark.parametrize("chrom,beg,end,count,attributes", [
@@ -164,7 +155,10 @@ def test_cnv_collection_wrong_resource_types(
 
 
 def test_cnv_collection_no_open(cnvs: CnvCollection) -> None:
-    with pytest.raises(ValueError, match="The resource <> is not open"):
+    with pytest.raises(
+        ValueError,
+        match="The resource <score_one> is not open",
+    ):
         cnvs.fetch_cnvs("1", 5, 15)
 
 
@@ -230,17 +224,23 @@ def test_cnv_collection_implementation_histogram(
 
 
 def test_cli_manage_cnv_collection_histograms(
-     proto_fixture: tuple[pathlib.Path, FsspecReadWriteProtocol],
+    tmp_path: pathlib.Path,
+    test_grr: GenomicResourceRepo,  # noqa: ARG001
 ) -> None:
-    path, _proto = proto_fixture
-    assert not (path / "one/index.html").exists()
+    grr_path = tmp_path / "grr"
+    assert not (grr_path / "/score_one/statistics").exists()
 
-    cli_manage(["resource-repair", "-R", str(path), "-r", "one", "-j", "1"])
+    cli_manage([
+        "resource-repair",
+        "-R", str(grr_path),
+        "-r", "score_one",
+        "-j", "1",
+    ])
 
-    assert (path / "one/statistics").exists()
+    assert (grr_path / "score_one/statistics").exists()
 
+    assert (grr_path / "score_one/statistics/histogram_freq.json").exists()
     hist_file = (
-        path / "one/statistics/histogram_phastCons100way.json"
+        grr_path / "score_one/statistics/histogram_freq.json"
     ).read_text().replace(" ", "").replace("\n", "")
-
-    assert hist_file.find('"bars":[1,2,0,1,1]') != -1
+    assert hist_file.find('"bars":[6,0,0]') != -1
