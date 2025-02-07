194c9e16f092222460a220a9548b6086983cb9c5
IvoTod <ivo.tod.todorov@gmail.com>
2025-02-07 13:03:45 +0200

    Update old code

--- wdae/wdae/gpfjs/views.py
+++ wdae/wdae/gpfjs/views.py
@@ -1,4 +1,3 @@
-from django.conf import settings
 from django.contrib.staticfiles import finders
 from django.contrib.staticfiles.views import serve
 from django.shortcuts import render
@@ -6,11 +5,11 @@ from django.shortcuts import render
 
 def index(request):
     if finders.find("gpfjs/gpfjs/index.html"):
-        return serve(request, "gpfjs/gpfjs/index.html", settings.STATIC_ROOT)
+        return serve(request, "gpfjs/gpfjs/index.html")
     return render(request, "gpfjs/empty/empty.html")
 
 
 def favicon(request):
     if finders.find("gpfjs/gpfjs/favicon.ico"):
-        return serve(request, "gpfjs/gpfjs/favicon.ico", settings.STATIC_ROOT)
+        return serve(request, "gpfjs/gpfjs/favicon.ico")
     return render(request, "gpfjs/empty/favicon.ico")
