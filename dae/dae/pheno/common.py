5e99fa3c8ad2e521376c7c86ac12d14b19623dd7
IvoTod <ivo.tod.todorov@gmail.com>
2025-02-12 12:55:59 +0200

    Fix manifest recreation

--- dae/dae/pheno/common.py
+++ dae/dae/pheno/common.py
@@ -180,12 +180,15 @@ class ImportManifest(BaseModel):
     @staticmethod
     def create_table(connection: duckdb.DuckDBPyConnection, table: Table):
         """Create table for recording import manifests."""
-        query = sqlglot.parse_one(
+        drop = sqlglot.parse_one(
+            f"DROP TABLE IF EXISTS {table.alias_or_name}").sql()
+        create = sqlglot.parse_one(
             f"CREATE TABLE {table.alias_or_name} "
             "(unix_timestamp DOUBLE, import_config VARCHAR)",
         ).sql()
         with connection.cursor() as cursor:
-            cursor.execute(query)
+            cursor.execute(drop)
+            cursor.execute(create)
 
     @staticmethod
     def write_to_db(
