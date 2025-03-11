f0269a5712e28bb419331cc5b0f1bf64be786fcb
IlinaGergova <ili.gergova@gmail.com>
2025-03-11 13:21:44 +0200

    add enrichment models component and service tests
    * test getting background models from query and from state
    and check if class properties are set correctly

--- gpfjs/src/app/enrichment-models/enrichment-models.component.spec.ts
+++ gpfjs/src/app/enrichment-models/enrichment-models.component.spec.ts
@@ -1,22 +1,73 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EnrichmentModelsComponent } from './enrichment-models.component';
-import { EnrichmentModelsService } from './enrichment-models.service';
+import { EnrichmentModels, EnrichmentModelsService } from './enrichment-models.service';
 import { of } from 'rxjs/internal/observable/of';
 import { ConfigService } from 'app/config/config.service';
 import { Store, StoreModule } from '@ngrx/store';
 import { provideHttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { EnrichmentModelsState, setEnrichmentModels } from './enrichment-models.state';
+
+
+const backgroundsMock = [
+  {
+    id: 'hg38/enrichment/ur_synonymous_iWGS_v1_1',
+    name: 'UR synonymous background from SPARK iWGS v1.1',
+    description: 'Ultra rare synonymous enrichment background build from\nSPARK iWGS v1.1 on Jan 8th, 2024.\n'
+  },
+  {
+    id: 'hg38/enrichment/ur_synonymous_AGRE_WG38_859',
+    name: 'UR synonymous background from AGRE WGS CSHL',
+    description: 'Ultra rare synonymous enrichment background build from\n AGRE WGS CSHL on Jan 16th, 2024.\n'
+  }
+];
+
+const countingMock = [
+  {
+    id: 'enrichment_events_counting',
+    name: 'Counting events',
+    description: 'Counting events'
+  },
+  {
+    id: 'enrichment_gene_counting',
+    name: 'Counting affected genes',
+    description: 'Counting affected genes'
+  }
+];
+
+const enrichmentModelsMock: EnrichmentModels = {
+  countings: countingMock,
+  backgrounds: backgroundsMock,
+  defaultBackground: 'hg38/enrichment/ur_synonymous_iWGS_v1_1',
+  defaultCounting: 'enrichment_events_counting'
+};
+
+const enrichmentModelsStateMock: EnrichmentModelsState = {
+  enrichmentBackgroundModel: 'hg38/enrichment/ur_synonymous_AGRE_WG38_859',
+  enrichmentCountingModel: 'enrichment_gene_counting'
+};
+
+class EnrichmentModelsServiceMock {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  public getBackgroundModels(datasetId: string): Observable<EnrichmentModels> {
+    return of(enrichmentModelsMock);
+  }
+}
 
 describe('EnrichmentModelsComponent', () => {
   let component: EnrichmentModelsComponent;
   let fixture: ComponentFixture<EnrichmentModelsComponent>;
-  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
-  let enrichmentModelsService: EnrichmentModelsService;
+  const enrichmentModelsServiceMock = new EnrichmentModelsServiceMock();
   let store: Store;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [EnrichmentModelsComponent],
-      providers: [EnrichmentModelsService, ConfigService, provideHttpClient()],
+      providers: [
+        { provide: EnrichmentModelsService, useValue: enrichmentModelsServiceMock },
+        ConfigService,
+        provideHttpClient()
+      ],
       imports: [StoreModule.forRoot({})]
     });
     fixture = TestBed.createComponent(EnrichmentModelsComponent);
@@ -26,12 +77,37 @@ describe('EnrichmentModelsComponent', () => {
     store = TestBed.inject(Store);
     jest.spyOn(store, 'select').mockReturnValue(of(['value1', 'value2', 'value3']));
     jest.spyOn(store, 'dispatch').mockReturnValue();
-
-    enrichmentModelsService = TestBed.inject(EnrichmentModelsService);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should make query for background models and set background and counting', () => {
+    const dispatchSpy = jest.spyOn(store, 'dispatch').mockImplementation();
+    component.ngOnInit();
+    expect(component.backgrounds).toStrictEqual(backgroundsMock);
+    expect(component.countings).toStrictEqual(countingMock);
+    expect(component.background).toStrictEqual(backgroundsMock[0]);
+    expect(component.counting).toStrictEqual(countingMock[0]);
+    expect(dispatchSpy).toHaveBeenCalledWith(setEnrichmentModels({
+      enrichmentBackgroundModel: 'hg38/enrichment/ur_synonymous_iWGS_v1_1',
+      enrichmentCountingModel: 'enrichment_events_counting'
+    }));
+  });
+
+  it('should get background models from state and set background and counting', () => {
+    jest.spyOn(store, 'select').mockReturnValue(of(enrichmentModelsStateMock));
+    const dispatchSpy = jest.spyOn(store, 'dispatch').mockImplementation();
+    component.ngOnInit();
+    expect(component.backgrounds).toStrictEqual(backgroundsMock);
+    expect(component.countings).toStrictEqual(countingMock);
+    expect(component.background).toStrictEqual(backgroundsMock[1]);
+    expect(component.counting).toStrictEqual(countingMock[1]);
+    expect(dispatchSpy).toHaveBeenCalledWith(setEnrichmentModels({
+      enrichmentBackgroundModel: enrichmentModelsStateMock.enrichmentBackgroundModel,
+      enrichmentCountingModel: enrichmentModelsStateMock.enrichmentCountingModel
+    }));
   });
 
   it('should change background', () => {
--- gpfjs/src/app/enrichment-models/enrichment-models.service.spec.ts
+++ gpfjs/src/app/enrichment-models/enrichment-models.service.spec.ts
@@ -3,7 +3,9 @@ import { TestBed } from '@angular/core/testing';
 import { provideHttpClientTesting, HttpTestingController } from '@angular/common/http/testing';
 import { ConfigService } from '../config/config.service';
 import { EnrichmentModelsService, EnrichmentModels } from './enrichment-models.service';
-import { provideHttpClient } from '@angular/common/http';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
+import { of, lastValueFrom, take } from 'rxjs';
+import { IdDescriptionName } from './iddescription';
 
 describe('EnrichmentModelsService', () => {
   let service: EnrichmentModelsService;
@@ -24,7 +26,7 @@ describe('EnrichmentModelsService', () => {
     httpMock.verify();
   });
 
-  it('should get the background models for a given dataset id', () => {
+  it('should get the background models for a given dataset id', async() => {
     const mockData = {
       counting: [
         { id: '1', name: 'Counting 1', desc: 'Description 1' },
@@ -37,14 +39,15 @@ describe('EnrichmentModelsService', () => {
       defaultCounting: '2',
       defaultBackground: '3'
     };
+
     const expectedResult: EnrichmentModels = {
       countings: [
-        { id: '1', name: 'Counting 1', description: 'Description 1' },
-        { id: '2', name: 'Counting 2', description: 'Description 2' }
+        new IdDescriptionName('1', 'Description 1', 'Counting 1'),
+        new IdDescriptionName('2', 'Description 2', 'Counting 2')
       ],
       backgrounds: [
-        { id: '3', name: 'Background 1', description: 'Description 3' },
-        { id: '4', name: 'Background 2', description: 'Description 4' }
+        new IdDescriptionName('3', 'Description 3', 'Background 1'),
+        new IdDescriptionName('4', 'Description 4', 'Background 2')
       ],
       defaultCounting: '2',
       defaultBackground: '3'
@@ -53,13 +56,13 @@ describe('EnrichmentModelsService', () => {
     const mockDatasetId = 'test';
     const mockUrl = `${configService.baseUrl}enrichment/models/${mockDatasetId}`;
 
-    service.getBackgroundModels(mockDatasetId).subscribe((result: EnrichmentModels) => {
-      expect(result).toStrictEqual(expectedResult);
-    });
+    const httpGetSpy = jest.spyOn(HttpClient.prototype, 'get');
+    httpGetSpy.mockReturnValue(of(mockData));
 
-    const req = httpMock.expectOne(mockUrl);
-    expect(req.request.method).toBe('GET');
+    const getResponse = service.getBackgroundModels(mockDatasetId);
 
-    req.flush(mockData);
+    expect(httpGetSpy).toHaveBeenCalledWith(mockUrl);
+    const res = await lastValueFrom(getResponse.pipe(take(1)));
+    expect(res).toStrictEqual(expectedResult);
   });
 });
