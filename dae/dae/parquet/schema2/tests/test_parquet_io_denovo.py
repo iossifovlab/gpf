645b41d1833481c3be5e57561e2e57622e7c4ecd
iordanivanov <iiordan.ivanov@gmail.com>
2025-03-13 17:23:58 +0200

    Add unit test for batch annotation in genotype data import

--- dae/dae/parquet/schema2/tests/test_parquet_io_denovo.py
+++ dae/dae/parquet/schema2/tests/test_parquet_io_denovo.py
@@ -1,7 +1,8 @@
-# pylint: disable=W0621,C0114,C0116,W0212,W0613
+# pylint: disable=W0621,C0114,C0116,W0212,W0613,R0917
 import pathlib
 
 import pytest
+import pytest_mock
 
 from dae.annotation.annotation_pipeline import AnnotationPipeline
 from dae.genomic_resources.reference_genome import ReferenceGenome
@@ -76,3 +77,39 @@ def test_vcf_denovo_reference(
 
     assert (tmp_path / "out").exists()
     assert (tmp_path / "out" / "family" / "frequency_bin=0").exists()
+
+
+@pytest.mark.parametrize("batch_size", [0, 500])
+def test_batch_annotation(
+    batch_size: int,
+    mocker: pytest_mock.MockerFixture,
+    vcf_variants: tuple[str, str],
+    acgt_genome_38: ReferenceGenome,
+    tmp_path: pathlib.Path,
+    t4c8_grr: GenomicResourceRepo,
+) -> None:
+    ped_path, vcf_path = vcf_variants
+    families = FamiliesLoader(ped_path).load()
+    vcf_loader = VcfLoader(families, [vcf_path], acgt_genome_38)
+    annotation_pipeline = AnnotationPipeline(t4c8_grr)
+    partition_descriptor = PartitionDescriptor()
+    parquet_writer = VariantsParquetWriter(
+        str(tmp_path / "out"),
+        annotation_pipeline,
+        partition_descriptor,
+    )
+
+    mocker.spy(AnnotationPipeline, "annotate")
+    mocker.spy(AnnotationPipeline, "batch_annotate")
+
+    parquet_writer.write_dataset(
+        vcf_loader.full_variants_iterator(),
+        annotation_batch_size=batch_size,
+    )
+
+    assert AnnotationPipeline.annotate.call_count == (  # type: ignore
+        0 if batch_size else 1
+    )
+    assert AnnotationPipeline.batch_annotate.call_count == (  # type: ignore
+        1 if batch_size else 0
+    )
