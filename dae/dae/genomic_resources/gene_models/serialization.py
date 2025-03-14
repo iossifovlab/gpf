078b9d462f7a96eca3a6dc81f0a0cda7b8396eb9
IvoTod <ivo.tod.todorov@gmail.com>
2025-03-14 14:38:11 +0200

    Use new chrom_gene_models in GTF conversion

--- dae/dae/genomic_resources/gene_models/serialization.py
+++ dae/dae/genomic_resources/gene_models/serialization.py
@@ -48,9 +48,9 @@ def gene_models_to_gtf(
 
     record_buffer: list[GTFRecord] = []
 
-    for gene_name, transcripts in gene_models.gene_models.items():
+    for (chrom, gene_name), transcripts in \
+            gene_models.chrom_gene_models.items():
         t = transcripts[0]
-        chrom = t.chrom
         start = min(t.tx[0] for t in transcripts)
         stop = max(t.tx[1] for t in transcripts)
         strand = t.strand
