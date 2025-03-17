f09ec9b1fd13d6e54b3e147e95c13bd752a5975a
IvoStefanov <ivostefanovtues@gmail.com>
2025-03-17 15:27:56 +0200

    Test aggregators on cnv collection annotator

--- dae/dae/annotation/tests/test_cnv_collection_annotator.py
+++ dae/dae/annotation/tests/test_cnv_collection_annotator.py
@@ -52,6 +52,50 @@ def grr() -> GenomicResourceRepo:
     })
 
 
+@pytest.fixture(scope="module")
+def larger_grr() -> GenomicResourceRepo:
+    return build_inmemory_test_repository({
+        "cnvs": {
+            "genomic_resource.yaml": textwrap.dedent("""
+                type: cnv_collection
+                table:
+                  filename: data.mem
+                scores:
+                - id: frequency
+                  name: frequency
+                  type: int
+                  desc: some populaton frequency
+                - id: collection
+                  name: collection
+                  type: str
+                  desc: SSC or AGRE
+                - id: affected_status
+                  name: "affected_status"
+                  type: str
+                  desc: |
+                        shows if the child that has the de novo is
+                        affected or unaffected
+                - id: size
+                  name: size
+                  type: int
+                  desc: size
+            """),
+            "data.mem": convert_to_tab_separated("""
+chrom  pos_begin  pos_end  frequency  collection  affected_status  size
+chr1   10         20       1          SSC         affected         100
+chr1   50         100      1          AGRE        affected         130
+chr1   1          2        1          AGRE        unaffected       250
+chr1   16         20       3          SSC         affected         360
+chr1   32         65       2          AGRE        unaffected       560
+chr1   15         60       2          AGRE        affected         670
+chr1   12         78       3          SSC        unaffected       550
+chr1   16         35       4          AGRE        unaffected       300
+chr1   25         67       5          SSc        affected         50
+chr1   24         35       2          AGRE        affected         900
+            """)},
+    })
+
+
 @pytest.mark.parametrize("annotatable, cnv_count", [
     (Position("1", 15), 1),
     (Region("1", 15, 60), 2),
@@ -143,3 +187,58 @@ def test_cnv_filter_and_attribute(
     assert "aggregator: join(,)" in status_info.documentation
     assert "aggregator: max" in status2_info.documentation
     assert "aggregator: concatenate" in collection_info.documentation
+
+
+def test_cnv_aggregators(
+    larger_grr: GenomicResourceRepo,
+) -> None:
+    pipeline = load_pipeline_from_yaml(
+        textwrap.dedent("""
+            - cnv_collection:
+                resource_id: cnvs
+                attributes:
+                - count
+                - name: size_max
+                  source: "attribute.size"
+                  aggregator: max
+                - name: affected_status_count
+                  source: "attribute.affected_status"
+                  aggregator: count
+                - name: size_median
+                  source: "attribute.size"
+                  aggregator: median
+                - name: frequency_median
+                  source: "attribute.frequency"
+                  aggregator: median
+                - name: frequency_list
+                  source: "attribute.frequency"
+                  aggregator: list
+                - name: size_mode
+                  source: "attribute.size"
+                  aggregator: mode
+                - name: collection_join
+                  source: "attribute.collection"
+                  aggregator: join(;)
+            """),
+        larger_grr)
+    annotatable = Region("chr1", 1, 100)
+    atts = pipeline.annotate(annotatable)
+
+    assert "count" in atts
+    assert "size_max" in atts
+    assert "affected_status_count" in atts
+    assert "size_median" in atts
+    assert "frequency_median" in atts
+    assert "frequency_list" in atts
+    assert "size_mode" in atts
+    assert "collection_join" in atts
+
+    assert atts["count"] == 10
+    assert atts["size_max"] == 900
+    assert atts["affected_status_count"] == 10
+    assert atts["size_median"] == 330.0
+    assert atts["frequency_median"] == 2.0
+    assert atts["frequency_list"] == [1, 1, 3, 2, 3, 4, 2, 5, 2, 1]
+    assert atts["size_mode"] == 50
+    assert atts["collection_join"] == \
+        "AGRE;SSC;SSC;AGRE;SSC;AGRE;AGRE;SSc;AGRE;AGRE"
