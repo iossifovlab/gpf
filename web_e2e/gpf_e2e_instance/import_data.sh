cacd6132e949e2faa034ac2b9c2269444625efcd
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2025-03-17 12:10:27 +0200

    Study configurations added

--- web_e2e/gpf_e2e_instance/import_data.sh
+++ web_e2e/gpf_e2e_instance/import_data.sh
@@ -2,55 +2,48 @@
 
 set -e
 
-# # cleanup
-# rm -rf ${WD}/gpf_e2e_instance/wdae/*
-# rm -rf ${WD}/gpf_e2e_instance/phenotype_storage/*
-# rm -rf ${WD}/gpf_e2e_instance/genotype_storage/*
-# rm -rf ${WD}/gpdb.duckdb
+# cleanup
+rm -rf ${WD}/gpf_e2e_instance/wdae/*
+rm -rf ${WD}/gpf_e2e_instance/phenotype_storage/*
+rm -rf ${WD}/gpf_e2e_instance/genotype_storage/*
+rm -rf ${WD}/gpdb.duckdb
 
-# # import comp data
-# cd ${WD}/gpf_e2e_instance/input_data/genotypes/comp
-# rm -rf OUTPUT
-# import_tools import_project_comp_vcf.yaml -f -vv -j 1
-# import_tools import_project_comp_denovo.yaml -f -vv -j 1
-# import_tools import_project_comp_all.yaml -f -vv -j 1
+# import comp data
+cd ${WD}/gpf_e2e_instance/input_data/genotypes/comp
+rm -rf OUTPUT
+import_tools import_project_comp_vcf.yaml -f -vv -j 1
+import_tools import_project_comp_denovo.yaml -f -vv -j 1
+import_tools import_project_comp_all.yaml -f -vv -j 1
 
-# cp comp_vcf.yaml ${WD}/gpf_e2e_instance/studies/comp_vcf_liftover
-# cp comp_denovo.yaml ${WD}/gpf_e2e_instance/studies/comp_denovo_liftover
-# cp comp_all.yaml ${WD}/gpf_e2e_instance/studies/comp_all_liftover
-# cd -
+cd -
 
-# # import iossifov 2014
-# cd ${WD}/gpf_e2e_instance/input_data/genotypes/iossifov_2014
-# rm -rf OUTPUT
-# import_tools import_project.yaml -f -vv -j 1
-# cp iossifov_2014.yaml ${WD}/gpf_e2e_instance/studies/iossifov_2014_liftover/
-# cd -
+# import iossifov 2014
+cd ${WD}/gpf_e2e_instance/input_data/genotypes/iossifov_2014
+rm -rf OUTPUT
+import_tools import_project.yaml -f -vv -j 1
+cd -
 
-# # import multi
-# cd ${WD}/gpf_e2e_instance/input_data/genotypes/multi
-# rm -rf OUTPUT
-# import_tools import_project.yaml -f -vv -j 1
-# cp multi.yaml ${WD}/gpf_e2e_instance/studies/multi_liftover/
-# cd -
+# import multi
+cd ${WD}/gpf_e2e_instance/input_data/genotypes/multi
+rm -rf OUTPUT
+import_tools import_project.yaml -f -vv -j 1
+cd -
 
-# # import phenotype comp data
-# cd ${WD}/gpf_e2e_instance/input_data/phenotypes/comp-data
-# rm -rf .task-progress .task-log work
+# import phenotype comp data
+cd ${WD}/gpf_e2e_instance/input_data/phenotypes/comp-data
+rm -rf .task-progress .task-log work
 
-# pheno_import --force -j 1 -v -p comp_pheno.ped \
-#     -i instruments/ --data-dictionary comp_pheno_data_dictionary.tsv \
-#     -o ${WD}/gpf_e2e_instance/phenotype_storage/comp_pheno \
-#     --person-column personId \
-#     --pheno-id comp_pheno \
-#     --regression comp_pheno_regressions.yaml
+pheno_import --force -j 1 -v -p comp_pheno.ped \
+    -i instruments/ --data-dictionary comp_pheno_data_dictionary.tsv \
+    -o ${WD}/gpf_e2e_instance/phenotype_storage/comp_pheno \
+    --person-column personId \
+    --pheno-id comp_pheno \
+    --regression comp_pheno_regressions.yaml
 
-# build_pheno_browser -j 1 --force comp_pheno \
-#     --gpf-instance ${WD}/gpf_e2e_instance/gpf_instance.yaml --no-cache
+build_pheno_browser -j 1 --force comp_pheno \
+    --gpf-instance ${WD}/gpf_e2e_instance/gpf_instance.yaml --no-cache
 
-# cp comp_pheno.yaml ${WD}/gpf_e2e_instance/pheno/comp_pheno/comp_pheno.yaml
-
-# cd -
+cd -
 
 # import helloworld data
 cd ${WD}/gpf_e2e_instance/input_data/genotypes/helloworld
@@ -69,10 +62,10 @@ build_pheno_browser -j 1 --force comp_pheno \
 
 cd -
 
-# # gene profiles generation
-# generate_gene_profile \
-#     --genes CHD8,GRIN2B,SHANK2,FLG,CMIP,TBCD,RAPGEF4,RAPGEFL1,RAPGEF1,RAPGEF2,SENP2,SENP3,SENP1,SENP6,SENP8,SENP3-EIF4A1
+# gene profiles generation
+generate_gene_profile \
+    --genes CHD8,GRIN2B,SHANK2,FLG,CMIP,TBCD,RAPGEF4,RAPGEFL1,RAPGEF1,RAPGEF2,SENP2,SENP3,SENP1,SENP6,SENP8,SENP3-EIF4A1
 
-# # generate common reports
-# generate_common_report
-# generate_denovo_gene_sets
+# generate common reports
+generate_common_report
+generate_denovo_gene_sets
