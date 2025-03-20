feb8106f6e4d8fbe0cd08ea83c3999fdaa5285d4
iordanivanov <iiordan.ivanov@gmail.com>
2025-03-20 12:33:54 +0200

    Remove unnecessary eager init test from dae/gpf_instance

--- dae/dae/gpf_instance/tests/test_gpf_instance.py
+++ dae/dae/gpf_instance/tests/test_gpf_instance.py
@@ -1,10 +1,7 @@
 # pylint: disable=redefined-outer-name,C0114,C0116,protected-access
-import textwrap
 
 import pytest
-from utils.testing import setup_t4c8_grr
 
-from dae.genomic_resources.testing import setup_directories
 from dae.gpf_instance import GPFInstance
 from dae.testing.setup_helpers import setup_gpf_instance
 
@@ -21,50 +18,6 @@ def test_init(t4c8_instance: GPFInstance) -> None:
     assert t4c8_instance._variants_db
     assert t4c8_instance.gene_sets_db is not None
     assert t4c8_instance.denovo_gene_sets_db is not None
-
-
-def test_eager_init(
-    tmp_path_factory: pytest.TempPathFactory,
-) -> None:
-    root_path = tmp_path_factory.mktemp("sample_t4c8_instance_for_test")
-    instance_path = root_path / "gpf_instance"
-    t4c8_grr = setup_t4c8_grr(root_path)
-    setup_directories(
-        instance_path, {
-            "gpf_instance.yaml": textwrap.dedent("""
-                instance_id: t4c8_instance
-                annotation:
-                  conf_file: annotation.yaml
-                reference_genome:
-                  resource_id: t4c8_genome
-                gene_models:
-                  resource_id: t4c8_genes
-                gene_scores_db:
-                  gene_scores:
-                  - "gene_scores/t4c8_score"
-                gene_sets_db:
-                  gene_set_collections:
-                  - gene_sets/main
-            """),
-            "annotation.yaml": textwrap.dedent("""
-               - position_score: genomic_scores/score_one
-            """),
-        },
-    )
-    instance = setup_gpf_instance(instance_path, grr=t4c8_grr)
-    instance.load()
-
-    assert instance
-
-    assert instance.dae_config
-    assert instance.reference_genome
-    assert instance.gene_models
-    assert instance._pheno_registry
-    assert instance.gene_scores_db is not None
-    assert instance.genomic_scores
-    assert instance._variants_db
-    assert instance.gene_sets_db
-    assert instance.denovo_gene_sets_db is not None
 
 
 def test_dae_config(tmp_path_factory: pytest.TempPathFactory) -> None:
