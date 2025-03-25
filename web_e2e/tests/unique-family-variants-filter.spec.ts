259e4935bfbd862d38d2c7482d672f0fdcb4f371
IlinaGergova <ili.gergova@gmail.com>
2025-03-25 16:10:03 +0200

    update unique family variants tests
    * update genes and datasets

--- web_e2e/tests/unique-family-variants-filter.spec.ts
+++ web_e2e/tests/unique-family-variants-filter.spec.ts
@@ -8,19 +8,19 @@ test.describe('Unique family variants filter tests', () => {
   });
 
   test('should disable unique family variants filter on a study', async({ page }) => {
-    await utils.navigateToDatasetPage(page, utils.datasetIds.compDenovoLiftover, 'Genotype Browser');
+    await utils.navigateToDatasetPage(page, utils.datasetIds.denovoHelloWorld, 'Genotype Browser');
     await expect(page.locator('#unique-family-variants-block')).not.toBeVisible();
 
-    await utils.navigateToDatasetPage(page, utils.datasetIds.compDenovoLiftover, 'Gene Browser');
-    await page.locator('gpf-gene-browser input#search-box').fill('SAMD11');
+    await utils.navigateToDatasetPage(page, utils.datasetIds.vcfHelloWorld, 'Gene Browser');
+    await page.locator('gpf-gene-browser input#search-box').fill('CHD8');
     await page.getByText('Go').click();
     await expect(page.locator('gpf-gene-plot')).toBeVisible();
     await expect(page.locator('#unique-family-variants-checkbox')).toBeDisabled();
   });
 
   test('should disable unique family variants when no variants are loaded', async({ page }) => {
-    await utils.navigateToDatasetPage(page, utils.datasetIds.multiLiftover, 'Gene Browser');
-    await page.locator('gpf-gene-browser input#search-box').fill('CHD8');
+    await utils.navigateToDatasetPage(page, utils.datasetIds.helloWorldGenotypes, 'Gene Browser');
+    await page.locator('gpf-gene-browser input#search-box').fill('CSDC2');
     await page.getByText('Go').click();
     await expect(page.locator('gpf-gene-plot')).toBeVisible();
     await expect(page.locator('#unique-family-variants-checkbox')).toBeDisabled();
@@ -29,27 +29,27 @@ test.describe('Unique family variants filter tests', () => {
   test('should enable the unique family variants filter on a dataset with at least 2 studies inside', async({
     page
   }) => {
-    await utils.navigateToDatasetPage(page, utils.datasetIds.compGenotypes, 'Genotype Browser');
+    await utils.navigateToDatasetPage(page, utils.datasetIds.helloWorldGenotypes, 'Genotype Browser');
     await expect(page.locator('#unique-family-variants-block')).toBeVisible();
 
-    await utils.navigateToDatasetPage(page, utils.datasetIds.compGenotypes, 'Gene Browser');
-    await page.locator('gpf-gene-browser input#search-box').fill('SAMD11');
+    await utils.navigateToDatasetPage(page, utils.datasetIds.helloWorldGenotypes, 'Gene Browser');
+    await page.locator('gpf-gene-browser input#search-box').fill('CHD8');
     await page.getByText('Go').click();
     await expect(page.locator('gpf-gene-plot')).toBeVisible();
     await expect(page.locator('#unique-family-variants-checkbox')).toBeEnabled();
   });
 
   test('should use the unique family variants filter to hide variants', async({ page }) => {
-    await utils.navigateToDatasetPage(page, utils.datasetIds.allGenotypes, 'Genotype Browser');
+    await utils.navigateToDatasetPage(page, utils.datasetIds.helloWorldGenotypes, 'Genotype Browser');
     await expect(page.locator('#unique-family-variants-block')).toBeVisible();
 
-    await utils.navigateToDatasetPage(page, utils.datasetIds.allGenotypes, 'Gene Browser');
-    await page.locator('gpf-gene-browser input#search-box').fill('SAMD11');
+    await utils.navigateToDatasetPage(page, utils.datasetIds.helloWorldGenotypes, 'Gene Browser');
+    await page.locator('gpf-gene-browser input#search-box').fill('CHD8');
     await page.getByText('Go').click();
     await expect(page.locator('gpf-gene-plot')).toBeVisible();
 
-    await expect(page.locator('#variants-count-span')).toHaveText('20 variants selected');
+    await expect(page.locator('#variants-count-span')).toHaveText('26 variants selected');
     await page.locator('#unique-family-variants-checkbox').click();
-    await expect(page.locator('#variants-count-span')).toHaveText('10 variants selected');
+    await expect(page.locator('#variants-count-span')).toHaveText('22 variants selected');
   });
 });
