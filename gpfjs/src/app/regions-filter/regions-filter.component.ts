3dea01be5cea07afa0472a333f9195fd2064a144
IvoStefanov <ivostefanovtues@gmail.com>
2025-03-28 15:52:55 +0200

    Refactor regions filter validation

--- gpfjs/src/app/regions-filter/regions-filter.component.ts
+++ gpfjs/src/app/regions-filter/regions-filter.component.ts
@@ -1,19 +1,21 @@
 import { RegionsFilter } from './regions-filter';
-import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, Input, OnInit, ViewChild, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { selectRegionsFilters, setRegionsFilters } from './regions-filter.state';
-import { ValidateNested } from 'class-validator';
 import { ComponentValidator } from 'app/common/component-validator';
 import { take } from 'rxjs';
 import { cloneDeep } from 'lodash';
+import { resetErrors, setErrors } from 'app/common/errors.state';
 
 @Component({
   selector: 'gpf-regions-filter',
   templateUrl: './regions-filter.component.html',
 })
-export class RegionsFilterComponent extends ComponentValidator implements OnInit {
+export class RegionsFilterComponent extends ComponentValidator implements OnInit, OnDestroy {
   @Input() public genome: string;
-  @ValidateNested() public regionsFilter = new RegionsFilter();
+  public regionsFilter = new RegionsFilter();
+  public errors: string[] = [];
+
   @ViewChild('textArea') private textArea: ElementRef;
 
   public constructor(
@@ -27,16 +29,19 @@ export class RegionsFilterComponent extends ComponentValidator implements OnInit
       .subscribe((regionsFilters: string[]) => {
         this.focusTextInputArea();
         this.regionsFilter.genome = this.genome;
-        super.ngOnInit();
+        this.validateState(regionsFilters);
         this.setRegionsFilter(regionsFilters.join('\n'));
       });
   }
 
   public setRegionsFilter(regionsFilter: string): void {
-    const result = regionsFilter
-      .split(/[\s]/)
-      .map(s => s.replace(/[,]/g, ''))
+    let formattedFilter = regionsFilter + '\n';
+    formattedFilter = formattedFilter.replace(/,(?![0-9]{3}\D{1})/g, '\n');
+    const result = formattedFilter
+      .split('\n')
+      .map(t => t.trim())
       .filter(s => s !== '');
+    this.validateState(result);
     this.regionsFilter.regionsFilter = regionsFilter;
     this.store.dispatch(setRegionsFilters({regionsFilter: cloneDeep(result)}));
   }
@@ -56,5 +61,57 @@ export class RegionsFilterComponent extends ComponentValidator implements OnInit
     this.waitForTextInputAreaToLoad().then(() => {
       this.textArea.nativeElement.focus();
     });
+  }
+
+  private validateState(state: string[]): void {
+    this.errors = [];
+
+    if (state.length === 0) {
+      this.errors.push('Add at least one region filter.');
+    } else {
+      for (const region of state) {
+        if (!this.isRegionValid(region, this.genome)) {
+          this.errors.push(`Invalid region: ${region}`);
+        }
+      }
+    }
+
+    if (this.errors.length) {
+      this.store.dispatch(setErrors({
+        errors: {
+          componentId: 'regionsFilter', errors: cloneDeep(this.errors)
+        }
+      }));
+    } else {
+      this.store.dispatch(resetErrors({componentId: 'regionsFilter'}));
+    }
+  }
+
+  private isRegionValid(region: string, genome: string): boolean {
+    region = region.replaceAll(',', '');
+    let chromRegex = '(2[0-2]|1[0-9]|[0-9]|X|Y)';
+    if (genome === 'hg38') {
+      chromRegex = 'chr' + chromRegex;
+    }
+    const lineRegex = `${chromRegex}:([0-9]+)(?:-([0-9]+))?|${chromRegex}`;
+    const match = region.match(new RegExp(lineRegex, 'i'));
+    if (match === null || match[0] !== region) {
+      return false;
+    }
+
+    if (
+      match.length >= 3
+      && match[2]
+      && match[3]
+      && Number(match[2].replace(',', '')) > Number(match[3].replace(',', ''))
+    ) {
+      return false;
+    }
+
+    return true;
+  }
+
+  public ngOnDestroy(): void {
+    this.store.dispatch(resetErrors({componentId: 'regionsFilter'}));
   }
 }
