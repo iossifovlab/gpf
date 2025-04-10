e75c86103693b7a12f98da36f2f24042024838df
IlinaGergova <ili.gergova@gmail.com>
2025-04-10 12:50:01 +0300

    create zygosity filter component

---
+++ gpfjs/src/app/zygosity-filters/zygosity-filter.component.ts
@@ -0,0 +1,56 @@
+import { Component, OnInit } from '@angular/core';
+import { resetZygosityFilter, selectZygosityFilter, setZygosityFilter } from './zygosity-filter.state';
+import { Store } from '@ngrx/store';
+import { take } from 'rxjs';
+import { resetErrors, setErrors } from 'app/common/errors.state';
+
+@Component({
+  selector: 'gpf-zygosity-filter',
+  templateUrl: './zygosity-filter.component.html',
+  styleUrl: './zygosity-filter.component.css'
+})
+export class ZygosityFilterComponent implements OnInit {
+  public zygosityTypes: string[] = [];
+  public selectedZygosityTypes: Set<string> = new Set<string>();
+
+  public constructor(private store: Store) {}
+
+  public ngOnInit(): void {
+    this.zygosityTypes = ['homozygous', 'heterozygous'];
+
+    this.store.select(selectZygosityFilter).pipe(take(1)).subscribe(zygosityFilter => {
+      if (zygosityFilter) {
+        this.selectedZygosityTypes.add(zygosityFilter);
+      } else {
+        this.selectedZygosityTypes.add(this.zygosityTypes[0]);
+        this.selectedZygosityTypes.add(this.zygosityTypes[1]);
+      }
+    });
+  }
+
+  public checkZygosityType(zygosityType: string, value: boolean): void {
+    if (value) {
+      this.selectedZygosityTypes.add(zygosityType);
+    } else {
+      this.selectedZygosityTypes.delete(zygosityType);
+    }
+    if (this.selectedZygosityTypes.size === this.zygosityTypes.length) {
+      this.store.dispatch(resetZygosityFilter());
+    } else {
+      this.store.dispatch(setZygosityFilter({zygosityFilter: [...this.selectedZygosityTypes][0]}));
+    }
+    this.validateState();
+  }
+
+  public validateState(): void {
+    if (!this.selectedZygosityTypes.size) {
+      this.store.dispatch(setErrors({
+        errors: {
+          componentId: 'zygosityFilter', errors: ['Select at least one zygosity.']
+        }
+      }));
+    } else {
+      this.store.dispatch(resetErrors({componentId: 'zygosityFilter'}));
+    }
+  }
+}
