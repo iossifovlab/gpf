28cb319b2eac03fc0d5f42262727abe7b2e498c3
IvoTod <ivo.tod.todorov@gmail.com>
2025-04-17 12:15:46 +0300

    Fix roles queries for old storages and add test cases

--- dae/dae/query_variants/base_query_variants.py
+++ dae/dae/query_variants/base_query_variants.py
@@ -140,7 +140,7 @@ class QueryVariantsBase(QueryVariants):
         Helper method for supporting legacy backends.
         """
         if roles_in_parent and roles_in_child:
-            roles = f"{roles_in_parent} and {roles_in_child}"
+            roles = f"({roles_in_parent}) and ({roles_in_child})"
         elif roles_in_child or roles_in_parent:
             roles = roles_in_child or roles_in_parent
         else:
