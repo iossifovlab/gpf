260c3d6320f499b6a9c609f37dc4586481f6cce6
IlinaGergova <ili.gergova@gmail.com>
2025-04-29 12:25:19 +0300

    add categorical histogram tests in genomic score

--- web_e2e/tests/genomic-scores-block.spec.ts
+++ web_e2e/tests/genomic-scores-block.spec.ts
@@ -58,6 +58,139 @@ test.describe('Genomic scores tests', () => {
     await expect(page.locator('text.partitions-text').nth(2)).toHaveText('~1090009 (1.43%)');
   });
 
+  test('should select categorical histogram', async({ page }) => {
+    const clinsig = 'CLNSIG - Aggregate germline classification for this single variant;' +
+    ' multiple values are separated by a vertical bar';
+
+    await page.locator('gpf-genomic-scores-block >> mat-form-field').click();
+    await page.locator(`mat-option:has-text("${clinsig}")`).click();
+    await expect(page.locator('gpf-genomic-scores')).toBeVisible();
+    await expect(page.locator('gpf-helper-modal')).toBeVisible();
+    await expect(page.getByRole('button', {name: 'Mode'})).toBeVisible();
+    await expect(page.locator('gpf-categorical-histogram')).toBeVisible();
+    await expect(page.locator('gpf-remove-button')).toBeVisible();
+  });
+
+  test('should check histograms order when adding new histogram', async({ page }) => {
+    const clndn = 'CLNDN - ClinVar\'s preferred disease name for' +
+    ' the concept specified by disease identifiers in CLNDISDB';
+
+    const mpc = 'mpc - Missense badness, PolyPhen-2, and Constraint. ' +
+    'A deleteriousness prediction score for missense variants';
+
+    const gnomad_exomes = 'gnomad_4_1_exomes_af_percent - Alternate allele frequency as percent';
+
+    await page.locator('gpf-genomic-scores-block >> mat-form-field').click();
+    await page.locator(`mat-option:has-text("${clndn}")`).click();
+    await page.locator('gpf-genomic-scores-block >> mat-form-field').click();
+    await page.locator(`mat-option:has-text("${mpc}")`).click();
+    await page.locator('gpf-genomic-scores-block >> mat-form-field').click();
+    await page.locator(`mat-option:has-text("${gnomad_exomes}")`).click();
+
+    await expect(page.locator('gpf-genomic-scores')).toHaveCount(3);
+    await expect(page.locator('gpf-genomic-scores').nth(0)).toContainText(gnomad_exomes);
+    await expect(page.locator('gpf-genomic-scores').nth(1)).toContainText(mpc);
+    await expect(page.locator('gpf-genomic-scores').nth(2)).toContainText(clndn);
+  });
+
+  test('should remove histogram from histograms list', async({ page }) => {
+    const clndn = 'CLNDN - ClinVar\'s preferred disease name for' +
+    ' the concept specified by disease identifiers in CLNDISDB';
+
+    const dbSNP = 'dbSNP_RS - dbSNP ID (i.e. rs number)';
+
+    const gnomad_genomes = 'gnomad_4_1_genomes_af_percent - Alternate allele frequency as percent';
+
+    await page.locator('gpf-genomic-scores-block >> mat-form-field').click();
+    await page.locator(`mat-option:has-text("${clndn}")`).click();
+
+    await page.locator('gpf-genomic-scores-block >> mat-form-field').click();
+    await page.locator(`mat-option:has-text("${dbSNP}")`).click();
+
+    await page.locator('gpf-genomic-scores-block >> mat-form-field').click();
+    await page.locator(`mat-option:has-text("${gnomad_genomes}")`).click();
+
+
+    await expect(page.locator('gpf-genomic-scores')).toHaveCount(3);
+
+    await page.locator('gpf-remove-button').nth(2).click();
+    await expect(page.locator('gpf-genomic-scores')).toHaveCount(2);
+    await expect(page.locator('gpf-genomic-scores').nth(0)).toContainText(gnomad_genomes);
+    await expect(page.locator('gpf-genomic-scores').nth(1)).toContainText(dbSNP);
+
+    await page.locator('gpf-remove-button').nth(0).click();
+    await expect(page.locator('gpf-genomic-scores')).toHaveCount(1);
+    await expect(page.locator('gpf-genomic-scores').nth(0)).toContainText(dbSNP);
+
+    await page.locator('gpf-remove-button').nth(0).click();
+    await expect(page.locator('gpf-genomic-scores')).toHaveCount(0);
+  });
+
+  test('should check if dropdown options are filtered when selecting histogram', async({ page }) => {
+    const clndn = 'CLNDN - ClinVar\'s preferred disease name for' +
+    ' the concept specified by disease identifiers in CLNDISDB';
+
+    const dbSNP = 'dbSNP_RS - dbSNP ID (i.e. rs number)';
+
+    await page.locator('gpf-genomic-scores-block >> mat-form-field').click();
+    await expect(page.locator('mat-option')).toHaveCount(6);
+    await page.locator(`mat-option:has-text("${clndn}")`).click();
+
+    await page.locator('gpf-genomic-scores-block >> mat-form-field').click();
+    await expect(page.locator('mat-option')).toHaveCount(5);
+    await expect(page.locator(`mat-option:has-text("${clndn}")`)).not.toBeVisible();
+    await page.locator(`mat-option:has-text("${dbSNP}")`).click();
+
+    await page.locator('gpf-genomic-scores-block >> mat-form-field').click();
+    await expect(page.locator('mat-option')).toHaveCount(4);
+    await expect(page.locator(`mat-option:has-text("${dbSNP}")`)).not.toBeVisible();
+  });
+
+  test('should check if error message is shown when too many categorical values are selected', async({ page }) => {
+    const clndn = 'CLNDN - ClinVar\'s preferred disease name for' +
+    ' the concept specified by disease identifiers in CLNDISDB';
+
+    await page.locator('gpf-genomic-scores-block >> mat-form-field').click();
+    await page.locator(`mat-option:has-text("${clndn}")`).click();
+
+    await expect(page.locator('gpf-categorical-histogram')).toBeVisible();
+    await expect(page.getByText(clndn)).toBeVisible();
+
+    await expect(page.locator('gpf-categorical-histogram svg')).toBeVisible();
+    // await expect(page.locator('gpf-categorical-histogram svg')).toContainText('166066 values');
+    await expect(page.locator('gpf-genomic-scores gpf-errors-alert')).toBeVisible();
+    await expect(page.locator('gpf-genomic-scores gpf-errors-alert')).toHaveText('Please select less than 1000 values.');
+    // expect buttons to be disabled
+  });
+
+  test('should share and load query with categorical histogram', async({ page }) => {
+    const clinsig = 'CLNSIG - Aggregate germline classification for this single variant;' +
+    ' multiple values are separated by a vertical bar';
+
+    await page.locator('gpf-genomic-scores-block >> mat-form-field').click();
+    await page.locator(`mat-option:has-text("${clinsig}")`).click();
+
+    await page.getByRole('button', {name: 'Mode'}).click();
+    await page.getByRole('menuitem', {name: 'click selector'}).click();
+
+    await page.locator('rect[id="Conflicting_classifications_of_pathogenicity"]').click();
+    await page.locator('rect[id="Likely_pathogenic"]').click();
+    await page.locator('rect[id="Benign/Likely_benign"]').click();
+
+    // await page.waitForSelector('gpf-errors-alert', {state: 'detached'});
+    await expect(page.locator('gpf-genomic-scores-block').getByText('3 values')).toBeVisible();
+    await expect(page.locator('gpf-errors-alert').filter({ hasText: 'Please select at least one' })).not.toBeVisible();
+
+    await page.getByRole('button', {name: 'Share/save query'}).click();
+    await expect(page.locator('#save-query-dropdown')).toBeVisible();
+    const shareLinkUrl = await page.locator('#link-input').inputValue();
+    await page.goto(shareLinkUrl, {waitUntil: 'load'});
+
+    await expect(page.locator('rect[id="Conflicting_classifications_of_pathogenicity"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+    await expect(page.locator('rect[id="Likely_pathogenic"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+    await expect(page.locator('rect[id="Benign/Likely_benign"]')).toHaveCSS('fill', 'rgb(70, 130, 180)');
+  });
+
   test('should test download', async({ page }) => {
     await utils.navigateToDatasetPage(page, utils.datasetIds.allGenotypes, 'Genotype browser');
 
