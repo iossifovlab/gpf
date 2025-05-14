de3fb06d7c074108196320b4fb8077e61f9970f4
IlinaGergova <ili.gergova@gmail.com>
2025-05-14 15:31:51 +0300

    update pheno browser table content tests

--- web_e2e/tests/pheno-browser.spec.ts
+++ web_e2e/tests/pheno-browser.spec.ts
@@ -39,13 +39,13 @@ test.describe('Pheno browser tests', () => {
     await expect(page.locator('.instrument-cell').nth(0)).toHaveText('instrument_2');
 
     await page.getByText('Pheno Measure', { exact: true }).click();
-    await expect(page.locator('.measure-name-cell').nth(0)).toHaveText('measure_7');
+    await expect(page.locator('.measure-name-cell').nth(0)).toHaveText('measure_7 info');
 
     await page.getByText('Pheno Measure', { exact: true }).click();
-    await expect(page.locator('.measure-name-cell').nth(0)).toHaveText('age');
+    await expect(page.locator('.measure-name-cell').nth(0)).toHaveText('age info');
 
     await page.getByText('Pheno Measure', { exact: true }).click();
-    await expect(page.locator('.measure-name-cell').nth(0)).toHaveText('measure_7');
+    await expect(page.locator('.measure-name-cell').nth(0)).toHaveText('measure_7 info');
 
     await page.getByText('male', { exact: true }).first().click();
     await expect(page.locator('[class="age.pvalueRegressionMale"]').nth(0)).toHaveText('0.8406');
@@ -62,92 +62,83 @@ test.describe('Pheno browser tests', () => {
 
     const instrument1 = await page.locator('.instrument-cell').nth(0).textContent();
     const measureName1 = await page.locator('.measure-name-cell').nth(0).textContent();
-    const description1 = await page.locator('.description-cell').nth(0).textContent();
     const measureType1 = await page.locator('.measure-type-cell ').nth(0).textContent();
     const age1 = await page.locator('.age').nth(0).textContent();
     const iq1 = await page.locator('.iq').nth(0).textContent();
 
-    const row1 = instrument1 + measureName1 + description1 + measureType1 + age1 + iq1;
+    const row1 = instrument1 + measureName1 + measureType1 + age1 + iq1;
     expect(row1).toBe(rowValues[0]);
 
     const instrument2 = await page.locator('.instrument-cell').nth(1).textContent();
     const measureName2 = await page.locator('.measure-name-cell').nth(1).textContent();
-    const description2 = await page.locator('.description-cell').nth(1).textContent();
     const measureType2 = await page.locator('.measure-type-cell ').nth(1).textContent();
     const age2 = await page.locator('.age').nth(1).textContent();
     const iq2 = await page.locator('.iq').nth(1).textContent();
 
-    const row2 = instrument2 + measureName2 + description2 + measureType2 + age2 + iq2;
+    const row2 = instrument2 + measureName2 + measureType2 + age2 + iq2;
     expect(row2).toBe(rowValues[1]);
 
     const instrument3 = await page.locator('.instrument-cell').nth(2).textContent();
     const measureName3 = await page.locator('.measure-name-cell').nth(2).textContent();
-    const description3 = await page.locator('.description-cell').nth(2).textContent();
     const measureType3 = await page.locator('.measure-type-cell ').nth(2).textContent();
     const age3 = await page.locator('.age').nth(2).textContent();
     const iq3 = await page.locator('.iq').nth(2).textContent();
 
-    const row3 = instrument3 + measureName3 + description3 + measureType3 + age3 + iq3;
+    const row3 = instrument3 + measureName3 + measureType3 + age3 + iq3;
     expect(row3).toBe(rowValues[2]);
 
     const instrument4 = await page.locator('.instrument-cell').nth(3).textContent();
     const measureName4 = await page.locator('.measure-name-cell').nth(3).textContent();
-    const description4 = await page.locator('.description-cell').nth(3).textContent();
     const measureType4 = await page.locator('.measure-type-cell ').nth(3).textContent();
     const age4 = await page.locator('.age').nth(3).textContent();
     const iq4 = await page.locator('.iq').nth(3).textContent();
 
-    const row4 = instrument4 + measureName4 + description4 + measureType4 + age4 + iq4;
+    const row4 = instrument4 + measureName4 + measureType4 + age4 + iq4;
     expect(row4).toBe(rowValues[3]);
 
     const instrument5 = await page.locator('.instrument-cell').nth(4).textContent();
     const measureName5 = await page.locator('.measure-name-cell').nth(4).textContent();
-    const description5 = await page.locator('.description-cell').nth(4).textContent();
     const measureType5 = await page.locator('.measure-type-cell ').nth(4).textContent();
     const age5 = await page.locator('.age').nth(4).textContent();
     const iq5 = await page.locator('.iq').nth(4).textContent();
 
-    const row5 = instrument5 + measureName5 + description5 + measureType5 + age5 + iq5;
+    const row5 = instrument5 + measureName5 + measureType5 + age5 + iq5;
     expect(row5).toBe(rowValues[4]);
 
     const instrument6 = await page.locator('.instrument-cell').nth(5).textContent();
     const measureName6 = await page.locator('.measure-name-cell').nth(5).textContent();
-    const description6 = await page.locator('.description-cell').nth(5).textContent();
     const measureType6 = await page.locator('.measure-type-cell ').nth(5).textContent();
     const age6 = await page.locator('.age').nth(5).textContent();
     const iq6 = await page.locator('.iq').nth(5).textContent();
 
-    const row6 = instrument6 + measureName6 + description6 + measureType6 + age6 + iq6;
+    const row6 = instrument6 + measureName6 + measureType6 + age6 + iq6;
     expect(row6).toBe(rowValues[5]);
 
     const instrument7 = await page.locator('.instrument-cell').nth(6).textContent();
     const measureName7 = await page.locator('.measure-name-cell').nth(6).textContent();
-    const description7 = await page.locator('.description-cell').nth(6).textContent();
     const measureType7 = await page.locator('.measure-type-cell ').nth(6).textContent();
     const age7 = await page.locator('.age').nth(6).textContent();
     const iq7 = await page.locator('.iq').nth(6).textContent();
 
-    const row7 = instrument7 + measureName7 + description7 + measureType7 + age7 + iq7;
+    const row7 = instrument7 + measureName7 + measureType7 + age7 + iq7;
     expect(row7).toBe(rowValues[6]);
 
     const instrument8 = await page.locator('.instrument-cell').nth(7).textContent();
     const measureName8 = await page.locator('.measure-name-cell').nth(7).textContent();
-    const description8 = await page.locator('.description-cell').nth(7).textContent();
     const measureType8 = await page.locator('.measure-type-cell ').nth(7).textContent();
     const age8 = await page.locator('.age').nth(7).textContent();
     const iq8 = await page.locator('.iq').nth(7).textContent();
 
-    const row8 = instrument8 + measureName8 + description8 + measureType8 + age8 + iq8;
+    const row8 = instrument8 + measureName8 + measureType8 + age8 + iq8;
     expect(row8).toBe(rowValues[7]);
 
     const instrument9 = await page.locator('.instrument-cell').nth(8).textContent();
     const measureName9 = await page.locator('.measure-name-cell').nth(8).textContent();
-    const description9 = await page.locator('.description-cell').nth(8).textContent();
     const measureType9 = await page.locator('.measure-type-cell ').nth(8).textContent();
     const age9 = await page.locator('.age').nth(8).textContent();
     const iq9 = await page.locator('.iq').nth(8).textContent();
 
-    const row9 = instrument9 + measureName9 + description9 + measureType9 + age9 + iq9;
+    const row9 = instrument9 + measureName9 + measureType9 + age9 + iq9;
     expect(row9).toBe(rowValues[8]);
   });
 
