c735a7fffb4984c7f18b35090b895a68fecec500
iordanivanov <iiordan.ivanov@gmail.com>
2025-05-16 12:14:01 +0300

    Further work on federation testing infrastructure

--- federation/setup_testing_remote.py
+++ federation/setup_testing_remote.py
@@ -1,5 +1,14 @@
+import pathlib  # noqa: INP001
+
 from dae.conftest import setup_t4c8_instance
 
-ROOT_PATH = "/wd/federation/tmp"
+ROOT_PATH = pathlib.Path("/wd/federation/tmp")
 
 setup_t4c8_instance(ROOT_PATH)
+
+GRR_PATH = ROOT_PATH / "grr_definition.yaml"
+GRR_PATH.write_text(
+"""id: "remote"
+type: "directory"
+directory: "/wd/federation/tmp/t4c8_grr"
+""")
--- federation/testing_entrypoint.sh
+++ federation/testing_entrypoint.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/bash
 
-mkdir /wd/federation/tmp
-mkdir /wd/federation/tmp/instance
-mkdir /wd/federation/tmp/grrCache
+for d in /wd/dae /wd/wdae; do
+    cd ${d};
+    /opt/conda/bin/conda run --no-capture-output -n gpf pip install -e .
+done
 
-cat >> /wd/federation/tmp/grr_definition.yaml << EOT
-id: "remote"
-type: "directory"
-directory: "/wd/federation/tmp/t4c8_grr"
-EOT
+rm -rf /wd/federation/tmp
+mkdir /wd/federation/tmp
 
 /opt/conda/bin/conda run --no-capture-output -n gpf \
-    /wd/federation/setup_testing_remote.py
+    python /wd/federation/setup_testing_remote.py
+
+export GRR_DEFINITION_FILE="/wd/federation/tmp/grr_definiton.yaml"
 
 /opt/conda/bin/conda run --no-capture-output -n gpf \
     /wd/wdae/wdae/wdaemanage.py migrate
@@ -25,5 +25,3 @@ while true; do
         /wd/wdae/wdae/wdaemanage.py runserver 0.0.0.0:21010
     sleep 10
 done
-
-rm -rf /wd/federation/tmp
