6e1c6e72b43d02518cdcfb9e815c75c5f7102e0d
IlinaGergova <ili.gergova@gmail.com>
2025-05-23 16:22:24 +0300

    update script for creating users
    * add any_user group to each user

--- web_e2e/scripts/wdae_create_dev_users.sh
+++ web_e2e/scripts/wdae_create_dev_users.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wdaemanage.py user_create admin@iossifovlab.com -p secret -g any_dataset:admin || true
-wdaemanage.py user_create research@iossifovlab.com -p secret || true
-wdaemanage.py user_create user_comp_vcf@iossifovlab.com -p secret || true
-wdaemanage.py user_create user_comp_genotypes@iossifovlab.com -p secret || true
-wdaemanage.py user_create user_all_genotypes@iossifovlab.com -p secret || true
-wdaemanage.py user_create user_iossifov_2014@iossifovlab.com -p secret || true
+wdaemanage.py user_create admin@iossifovlab.com -p secret -g any_dataset:any_user:admin || true
+wdaemanage.py user_create research@iossifovlab.com -p secret -g any_user || true
+wdaemanage.py user_create user_comp_vcf@iossifovlab.com -p secret -g any_user || true
+wdaemanage.py user_create user_comp_genotypes@iossifovlab.com -p secret -g any_user || true
+wdaemanage.py user_create user_all_genotypes@iossifovlab.com -p secret -g any_user || true
+wdaemanage.py user_create user_iossifov_2014@iossifovlab.com -p secret -g any_user || true
