eaed36a549998276dd854e357288f5f217a4e8a4
iordanivanov <iiordan.ivanov@gmail.com>
2025-07-17 14:31:37 +0300

    Extend VcfSerializer to add INFO writing capabilities

--- dae/dae/variants_loaders/vcf/serializer.py
+++ dae/dae/variants_loaders/vcf/serializer.py
@@ -7,6 +7,8 @@ from types import TracebackType
 
 import pysam
 
+from dae.annotation.annotation_config import AttributeInfo
+from dae.annotation.format_handlers import stringify
 from dae.genomic_resources.reference_genome import ReferenceGenome
 from dae.pedigrees.families_data import FamiliesData
 from dae.variants.family_variant import FamilyVariant
@@ -20,17 +22,19 @@ class VcfSerializer:
     """Stores a sequence of alleles in a VCF file."""
 
     def __init__(
-            self,
-            families: FamiliesData,
-            genome: ReferenceGenome,
-            output_path: pathlib.Path | str | None,
-            header: list[str] | None = None,
+        self,
+        families: FamiliesData,
+        genome: ReferenceGenome,
+        output_path: pathlib.Path | str | None,
+        header: list[str] | None = None,
+        annotations: list[AttributeInfo] | None = None,
     ):
         self.families = families
         self.genome = genome
         self.output_path = output_path
         self._vcf_header = self._build_vcf_header(header)
         self.vcf_file: pysam.VariantFile | None = None
+        self.annotations = annotations
 
     def serialize(
         self,
@@ -72,23 +76,25 @@ class VcfSerializer:
         )
 
     def _build_vcf_record(
-            self,
-            sv: SummaryVariant,
-            fvs: Sequence[FamilyVariant],
+        self,
+        sv: SummaryVariant,
+        fvs: Sequence[FamilyVariant],
     ) -> pysam.VariantRecord:
         assert self.vcf_file is not None
-        record = self.vcf_file.new_record()
+        record: pysam.VariantRecord = self.vcf_file.new_record()
         record.contig = sv.chrom
         record.pos = sv.position
         record.ref = sv.reference
-
-        alts = [aa.alternative for aa in sv.alt_alleles]
-        record.alts = alts
+        record.alts = tuple(aa.alternative for aa in sv.alt_alleles
+                            if aa.alternative is not None)
 
         svid = [
             f"{aa.chrom}_{aa.position}_{aa.reference}_{aa.alternative}"
             for aa in sv.alt_alleles]
         record.id = ";".join(svid)
+
+        if self.annotations is not None:
+            self._update_info(record, sv, self.annotations)
 
         for fv in fvs:
             fam = self.families[fv.family_id]
@@ -96,6 +102,45 @@ class VcfSerializer:
                 gt = [g for g in fv.genotype[mem_index] if g > -2]
                 record.samples[mem.sample_id]["GT"] = gt
         return record
+
+    def _update_info(
+        self,
+        vcf_var: pysam.VariantRecord,
+        summary_variant: SummaryVariant,
+        attributes: list[AttributeInfo],
+    ) -> None:
+        buffers: list[list] = []
+        for attribute in attributes:
+            values = summary_variant.get_attribute(attribute.name)
+            buff = []
+            for value in values:
+                if isinstance(value, list):
+                    transformed = ";".join(stringify(a, vcf=True)
+                                           for a in value)
+                elif isinstance(value, dict):
+                    transformed = ";".join(
+                        f"{k}:{v}"
+                        for k, v in value.items()
+                    )
+                else:
+                    transformed = stringify(value, vcf=True)
+                transformed = stringify(value, vcf=True) \
+                    .replace(";", "|")\
+                    .replace(",", "|")\
+                    .replace(" ", "_")
+                buff.append(transformed)
+            buffers.append(buff)
+        # If the all values for a given attribute are
+        # empty (i.e. - "."), then that attribute has no
+        # values to be written and will be skipped in the output
+        has_value = {
+            attr.name: any(filter(lambda x: x != ".", buffers[idx]))
+            for idx, attr in enumerate(attributes)
+        }
+        for buff, attribute in zip(buffers, attributes, strict=True):
+            if attribute.internal or not has_value[attribute.name]:
+                continue
+            vcf_var.info[attribute.name] = buff
 
     def __enter__(self) -> VcfSerializer:
         return self.open()
