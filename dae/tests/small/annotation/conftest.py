cea924641343dedc2b58efa3eafb0de39a1ee16d
iordanivanov <iiordan.ivanov@gmail.com>
2025-07-21 00:07:14 +0300

    Refactor and cleanup of annotation tool unit tests

--- dae/tests/small/annotation/conftest.py
+++ dae/tests/small/annotation/conftest.py
@@ -1,8 +1,15 @@
 # pylint: disable=W0621,C0114,C0116,W0212,W0613
 
 import os
+import pathlib
+import textwrap
 
 import pytest
+from dae.genomic_resources.testing import (
+    setup_genome,
+    setup_pedigree,
+)
+from dae.testing import setup_denovo, setup_directories
 
 
 def relative_to_this_test_folder(path: str) -> str:
@@ -12,3 +19,188 @@ def relative_to_this_test_folder(path: str) -> str:
 @pytest.fixture(scope="session")
 def work_dir() -> str:
     return relative_to_this_test_folder("fixtures")
+
+
+@pytest.fixture(scope="module")
+def annotate_directory_fixture(
+    tmp_path_factory: pytest.TempPathFactory,
+) -> pathlib.Path:
+    root_path = tmp_path_factory.mktemp("annotate_tests_fixtures")
+    setup_directories(
+        root_path,
+        {
+            "annotation.yaml": """
+                - position_score: one
+            """,
+            "annotation_duplicate.yaml": """
+                - position_score: one
+                - position_score: one
+            """,
+            "annotation_multiallelic.yaml": """
+                - allele_score: two
+            """,
+            "annotation_forbidden_symbols.yaml": """
+                - allele_score: three
+            """,
+            "annotation_quotes_in_description.yaml": """
+                - position_score: four
+            """,
+            "annotation_repeated_attributes.yaml": """
+                - position_score: one
+                - position_score: four
+            """,
+            "annotation_internal_attributes.yaml": """
+                - position_score:
+                    resource_id: one
+                    attributes:
+                    - source: score
+                      name: score_1
+                - position_score:
+                    resource_id: four
+                    attributes:
+                    - source: score
+                      name: score_4
+                      internal: true
+            """,
+            "grr.yaml": f"""
+                id: mm
+                type: dir
+                directory: "{root_path}/grr"
+            """,
+            "grr": {
+                "one": {
+                    "genomic_resource.yaml": """
+                        type: position_score
+                        table:
+                            filename: data.txt
+                        scores:
+                        - id: score
+                          type: float
+                          desc: |
+                                The phastCons computed over the tree of 100
+                                verterbrate species
+                          name: s1
+                    """,
+                },
+                "two": {
+                    "genomic_resource.yaml": """
+                        type: allele_score
+                        table:
+                            filename: data.txt
+                            reference:
+                              name: reference
+                            alternative:
+                              name: alternative
+                        scores:
+                        - id: score
+                          type: float
+                          name: s1
+                    """,
+                },
+                "three": {
+                    "genomic_resource.yaml": """
+                        type: allele_score
+                        table:
+                            filename: data.txt
+                            reference:
+                              name: reference
+                            alternative:
+                              name: alternative
+                        scores:
+                        - id: score
+                          type: str
+                          name: s1
+                    """,
+                },
+                "four": {
+                    "genomic_resource.yaml": """
+                        type: position_score
+                        table:
+                            filename: data.txt
+                        scores:
+                        - id: score
+                          type: float
+                          desc: |
+                                The "phastCons" computed over the tree of 100
+                                verterbrate species
+                          name: s1
+                    """,
+                },
+                "res_pipeline": {
+                    "annotation.yaml": """
+                        - position_score: one
+                    """,
+                    "genomic_resource.yaml": """
+                        type: annotation_pipeline
+                        filename: annotation.yaml
+                    """,
+                },
+                "test_genome": {
+                    "genomic_resource.yaml": """
+                        type: genome
+                        filename: genome.fa
+                    """,
+                },
+            },
+        },
+    )
+    one_content = textwrap.dedent("""
+        chrom  pos_begin  s1
+        chr1   23         0.1
+        chr1   24         0.2
+        chr2   33         0.3
+        chr2   34         0.4
+        chr3   43         0.5
+        chr3   44         0.6
+        chr4   53         0.1234567890123456789
+    """)
+    two_content = textwrap.dedent("""
+        chrom  pos_begin  reference  alternative  s1
+        chr1   23         C          T            0.1
+        chr1   23         C          A            0.2
+        chr1   24         C          A            0.3
+        chr1   24         C          G            0.4
+        chr1   25         C          G            0.4
+    """)
+    three_content = textwrap.dedent("""
+        chrom  pos_begin  reference  alternative  s1
+        chr1   23         C          A            a;b
+        chr1   24         C          A            c,d
+        chr1   25         C          A            e||f
+    """)
+    four_content = textwrap.dedent("""
+        chrom  pos_begin  s1
+        chr1   23         0.101
+        chr1   24         0.201
+        chr2   33         0.301
+        chr2   34         0.401
+        chr3   43         0.501
+        chr3   44         0.601
+    """)
+    setup_denovo(root_path / "grr" / "one" / "data.txt", one_content)
+    setup_denovo(root_path / "grr" / "two" / "data.txt", two_content)
+    setup_denovo(root_path / "grr" / "three" / "data.txt", three_content)
+    setup_denovo(root_path / "grr" / "four" / "data.txt", four_content)
+    setup_genome(root_path / "grr" / "test_genome" / "genome.fa",
+                 textwrap.dedent(f"""
+                     >chr1
+                     {25 * 'ACGT'}
+                     >chr2
+                     {25 * 'ACGT'}
+                     >chr3
+                     {25 * 'ACGT'}
+                     >chr4
+                     {25 * 'ACGT'}
+                 """))
+    setup_pedigree(root_path / "sample.ped", textwrap.dedent("""
+        familyId personId dadId momId sex status role
+        f1       m1       0     0     2   1      mom
+        f1       d1       0     0     1   1      dad
+        f1       c1       dad   mom   1   2      prb
+    """))
+    return root_path
+
+
+@pytest.fixture(scope="module")
+def sample_ped(annotate_directory_fixture: pathlib.Path) -> pathlib.Path:
+    return annotate_directory_fixture / "sample.ped"
--- dae/tests/small/annotation/test_annotate_vcf.py
+++ dae/tests/small/annotation/test_annotate_vcf.py
@@ -1,11 +1,17 @@
 # pylint: disable=W0621,C0114,C0116,W0212,W0613
+import os
 import pathlib
 import textwrap
 
 import numpy as np
 import pysam
 import pytest
-from dae.annotation.annotate_vcf import VCFWriter, process_vcf
+from dae.annotation.annotate_vcf import (
+    VCFWriter,
+    cli,
+    process_vcf,
+    produce_partfile_paths,
+)
 from dae.genomic_resources.testing import (
     setup_denovo,
     setup_pedigree,
@@ -14,6 +20,7 @@ from dae.genomic_resources.testing import (
 from dae.gpf_instance.gpf_instance import GPFInstance
 from dae.pedigrees.loader import FamiliesLoader
 from dae.testing.acgt_import import acgt_gpf
+from dae.utils.regions import Region
 from dae.variants.family_variant import FamilyVariant
 from dae.variants.variant import SummaryVariantFactory
 from dae.variants_loaders.raw.loader import FullVariant
@@ -180,3 +187,420 @@ def test_process_vcf_simple_batch(
     with pysam.VariantFile(str(out_path)) as vcf_file:
         result = [vcf.info["score"][0] for vcf in vcf_file.fetch()]
     assert result == ["0.1", "0.2", "0.3"]
+
+
+def test_basic_vcf(
+    annotate_directory_fixture: pathlib.Path,
+    sample_ped: pathlib.Path,
+    tmp_path: pathlib.Path,
+) -> None:
+    in_content = textwrap.dedent("""
+        ##fileformat=VCFv4.2
+        ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+        ##contig=<ID=chr1>
+        #CHROM POS ID REF ALT QUAL FILTER INFO FORMAT m1  d1  c1
+        chr1   23  .  C   T   .    .      .    GT     0/1 0/0 0/0
+        chr1   24  .  C   A   .    .      .    GT     0/0 0/1 0/0
+    """)
+    root_path = annotate_directory_fixture
+    in_file = tmp_path / "in.vcf"
+    out_file = root_path / "out.vcf"
+    work_dir = tmp_path / "output"
+    annotation_file = root_path / "annotation.yaml"
+    grr_file = root_path / "grr.yaml"
+
+    setup_vcf(in_file, in_content)
+
+    cli([
+        str(a) for a in [
+            in_file,
+            sample_ped,
+            annotation_file,
+            "--grr", grr_file,
+            "-o", out_file,
+            "-w", work_dir,
+            "-j", 1,
+            "-R", "test_genome",
+        ]
+    ])
+
+    # pylint: disable=no-member
+    with pysam.VariantFile(str(out_file)) as vcf_file:
+        result = [vcf.info["score"][0] for vcf in vcf_file.fetch()]
+    assert result == ["0.1", "0.2"]
+
+
+def test_multiallelic_vcf(
+    annotate_directory_fixture: pathlib.Path,
+    sample_ped: pathlib.Path,
+    tmp_path: pathlib.Path,
+) -> None:
+    in_content = textwrap.dedent("""
+        ##fileformat=VCFv4.2
+        ##contig=<ID=chr1>
+        #CHROM POS ID REF ALT QUAL FILTER INFO
+        chr1   23  .  C   T,A   .    .      .
+        chr1   24  .  C   A,G   .    .      .
+    """)
+    root_path = annotate_directory_fixture
+    in_file = tmp_path / "in.vcf"
+    out_file = root_path / "out.vcf"
+    work_dir = tmp_path / "output"
+    annotation_file = root_path / "annotation_multiallelic.yaml"
+    grr_file = root_path / "grr.yaml"
+
+    setup_vcf(in_file, in_content)
+
+    cli([
+        str(a) for a in [
+            in_file,
+            sample_ped,
+            annotation_file,
+            "--grr", grr_file,
+            "-o", out_file,
+            "-w", work_dir,
+            "-j", 1,
+            "-R", "test_genome",
+        ]
+    ])
+
+    result = []
+    # pylint: disable=no-member
+    with pysam.VariantFile(str(out_file)) as vcf_file:
+        result = [vcf.info["score"] for vcf in vcf_file.fetch()]
+    assert result == [("0.1", "0.2"), ("0.3", "0.4")]
+
+
+def test_vcf_multiple_chroms(
+    annotate_directory_fixture: pathlib.Path,
+    sample_ped: pathlib.Path,
+    tmp_path: pathlib.Path,
+) -> None:
+    in_content = textwrap.dedent("""
+        ##fileformat=VCFv4.2
+        ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+        ##contig=<ID=chr1>
+        ##contig=<ID=chr2>
+        ##contig=<ID=chr3>
+        #CHROM POS ID REF ALT QUAL FILTER INFO FORMAT m1  d1  c1
+        chr1   23  .  C   T   .    .      .    GT     0/1 0/0 0/0
+        chr1   24  .  C   A   .    .      .    GT     0/0 0/1 0/0
+        chr2   33  .  C   T   .    .      .    GT     0/1 0/0 0/0
+        chr2   34  .  C   A   .    .      .    GT     0/0 0/1 0/0
+        chr3   43  .  C   T   .    .      .    GT     0/1 0/0 0/0
+        chr3   44  .  C   A   .    .      .    GT     0/0 0/1 0/0
+    """)
+    root_path = annotate_directory_fixture
+    in_file = tmp_path / "in.vcf.gz"
+    out_file = root_path / "out.vcf.gz"
+    out_file_tbi = root_path / "out.vcf.gz.tbi"
+    work_dir = tmp_path / "output"
+    annotation_file = root_path / "annotation.yaml"
+    grr_file = root_path / "grr.yaml"
+
+    setup_vcf(in_file, in_content)
+
+    cli([
+        str(a) for a in [
+            in_file,
+            sample_ped,
+            annotation_file,
+            "--grr", grr_file,
+            "-o", out_file,
+            "-w", work_dir,
+            "-j", 1,
+            "-R", "test_genome",
+        ]
+    ])
+
+    result = []
+    # pylint: disable=no-member
+    with pysam.VariantFile(str(out_file)) as vcf_file:
+        result = [vcf.info["score"][0] for vcf in vcf_file.fetch()]
+    assert result == ["0.1", "0.2",
+                      "0.3", "0.4",
+                      "0.5", "0.6"]
+    assert os.path.exists(out_file_tbi)
+    assert set(os.listdir(work_dir)) == {
+        ".task-log",     # default task logs dir
+        ".task-status",  # default task status dir
+        # part files must be cleaned up
+    }
+
+
+def test_annotate_vcf_float_precision(
+    annotate_directory_fixture: pathlib.Path,
+    sample_ped: pathlib.Path,
+    tmp_path: pathlib.Path,
+) -> None:
+    in_content = textwrap.dedent("""
+        ##fileformat=VCFv4.2
+        ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+        ##contig=<ID=chr4>
+        #CHROM POS ID REF ALT QUAL FILTER INFO FORMAT m1  d1  c1
+        chr4   53  .  C   T   .    .      .    GT     0/1 0/0 0/0
+    """)
+    root_path = annotate_directory_fixture
+    in_file = tmp_path / "in.vcf"
+    out_file = root_path / "out.vcf"
+    work_dir = tmp_path / "output"
+    annotation_file = root_path / "annotation.yaml"
+    grr_file = root_path / "grr.yaml"
+
+    setup_vcf(in_file, in_content)
+
+    cli([
+        str(a) for a in [
+            in_file,
+            sample_ped,
+            annotation_file,
+            "--grr", grr_file,
+            "-o", out_file,
+            "-w", work_dir,
+            "-j", 1,
+            "-R", "test_genome",
+        ]
+    ])
+
+    # pylint: disable=no-member
+    with pysam.VariantFile(str(out_file)) as vcf_file:
+        result = [vcf.info["score"][0] for vcf in vcf_file.fetch()]
+    assert result == ["0.123"]
+
+
+def test_annotate_vcf_internal_attributes(
+    annotate_directory_fixture: pathlib.Path,
+    sample_ped: pathlib.Path,
+    tmp_path: pathlib.Path,
+) -> None:
+    in_content = textwrap.dedent("""
+        ##fileformat=VCFv4.2
+        ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+        ##contig=<ID=chr1>
+        #CHROM POS ID REF ALT QUAL FILTER INFO FORMAT m1  d1  c1
+        chr1   23  .  C   T   .    .      .    GT     0/1 0/0 0/0
+        chr1   24  .  C   A   .    .      .    GT     0/0 0/1 0/0
+    """)
+    root_path = annotate_directory_fixture
+    in_file = tmp_path / "in.vcf"
+    out_file = root_path / "out.vcf"
+    work_dir = tmp_path / "output"
+    annotation_file = root_path / "annotation_internal_attributes.yaml"
+    grr_file = root_path / "grr.yaml"
+
+    setup_vcf(in_file, in_content)
+
+    cli([
+        str(a) for a in [
+            in_file,
+            sample_ped,
+            annotation_file,
+            "--grr", grr_file,
+            "-o", out_file,
+            "-w", work_dir,
+            "-j", 1,
+            "-R", "test_genome",
+        ]
+    ])
+
+    # pylint: disable=no-member
+    with pysam.VariantFile(str(out_file)) as vcf_file:
+        for rec in vcf_file.fetch():
+            assert "score_1" in rec.info
+            assert "score_4" not in rec.info
+
+
+def test_annotate_vcf_forbidden_symbol_replacement(
+    annotate_directory_fixture: pathlib.Path,
+    sample_ped: pathlib.Path,
+    tmp_path: pathlib.Path,
+) -> None:
+    in_content = textwrap.dedent("""
+        ##fileformat=VCFv4.2
+        ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+        ##contig=<ID=chr1>
+        #CHROM POS ID REF ALT QUAL FILTER INFO FORMAT m1  d1  c1
+        chr1   23  .  C   A   .    .      .    GT     0/1 0/0 0/0
+        chr1   24  .  C   A   .    .      .    GT     0/0 0/1 0/0
+        chr1   25  .  C   A   .    .      .    GT     0/0 0/1 0/0
+    """)
+    root_path = annotate_directory_fixture
+    in_file = tmp_path / "in.vcf"
+    out_file = root_path / "out.vcf"
+    work_dir = tmp_path / "output"
+    annotation_file = root_path / "annotation_forbidden_symbols.yaml"
+    grr_file = root_path / "grr.yaml"
+
+    setup_vcf(in_file, in_content)
+
+    cli([
+        str(a) for a in [
+            in_file,
+            sample_ped,
+            annotation_file,
+            "--grr", grr_file,
+            "-o", out_file,
+            "-w", work_dir,
+            "-j", 1,
+            "-R", "test_genome",
+        ]
+    ])
+
+    # pylint: disable=no-member
+    with pysam.VariantFile(str(out_file)) as vcf_file:
+        result = [vcf.info["score"][0] for vcf in vcf_file.fetch()]
+    assert result == ["a|b", "c|d", "e_f"]
+
+
+def test_annotate_vcf_none_values(
+    annotate_directory_fixture: pathlib.Path,
+    sample_ped: pathlib.Path,
+    tmp_path: pathlib.Path,
+) -> None:
+    in_content = textwrap.dedent("""
+        ##fileformat=VCFv4.2
+        ##contig=<ID=chr1>
+        #CHROM POS ID REF ALT QUAL FILTER INFO
+        chr1   23  .  C   T   .    .      .
+        chr1   24  .  C   A,G,T   .    .      .
+        chr1   25  .  C   C,T   .    .      .
+        chr1   26  .  C   G   .    .      .
+    """)
+    root_path = annotate_directory_fixture
+    in_file = tmp_path / "in.vcf"
+    out_file = root_path / "out.vcf"
+    work_dir = tmp_path / "output"
+    annotation_file = root_path / "annotation_multiallelic.yaml"
+    grr_file = root_path / "grr.yaml"
+
+    setup_vcf(in_file, in_content)
+
+    cli([
+        str(a) for a in [
+            in_file,
+            sample_ped,
+            annotation_file,
+            "--grr", grr_file,
+            "-o", out_file,
+            "-w", work_dir,
+            "-j", 1,
+            "-R", "test_genome",
+        ]
+    ])
+
+    # pylint: disable=no-member
+    with pysam.VariantFile(str(out_file)) as vcf_file:
+        variants = [*vcf_file.fetch()]
+    assert variants[0].info["score"] == ("0.1",)
+    assert variants[1].info["score"] == ("0.3", "0.4", ".")
+    assert "score" not in variants[2].info
+    assert "score" not in variants[3].info
+
+
+def test_vcf_description_with_quotes(
+    annotate_directory_fixture: pathlib.Path,
+    sample_ped: pathlib.Path,
+    tmp_path: pathlib.Path,
+) -> None:
+    in_content = textwrap.dedent("""
+        ##fileformat=VCFv4.2
+        ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+        ##contig=<ID=chr1>
+        #CHROM POS ID REF ALT QUAL FILTER INFO FORMAT m1  d1  c1
+        chr1   23  .  C   A   .    .      .    GT     0/1 0/0 0/0
+        chr1   24  .  C   A   .    .      .    GT     0/0 0/1 0/0
+        chr1   25  .  C   A   .    .      .    GT     0/0 0/1 0/0
+    """)
+    root_path = annotate_directory_fixture
+    in_file = tmp_path / "in.vcf"
+    out_file = root_path / "out.vcf"
+    work_dir = tmp_path / "output"
+    annotation_file = root_path / "annotation_quotes_in_description.yaml"
+    grr_file = root_path / "grr.yaml"
+
+    setup_vcf(in_file, in_content)
+
+    cli([
+        str(a) for a in [
+            in_file,
+            sample_ped,
+            annotation_file,
+            "--grr", grr_file,
+            "-o", out_file,
+            "-w", work_dir,
+            "-j", 1,
+            "-R", "test_genome",
+        ]
+    ])
+
+    # pylint: disable=no-member
+    with pysam.VariantFile(str(out_file)) as vcf_file:
+        info = vcf_file.header.info
+    assert info["score"].description == \
+        'The \\"phastCons\\" computed over the tree of 100 verterbrate species'
+
+
+def test_annotate_vcf_repeated_attributes(
+    annotate_directory_fixture: pathlib.Path,
+    sample_ped: pathlib.Path,
+    tmp_path: pathlib.Path,
+) -> None:
+    in_content = textwrap.dedent("""
+        ##fileformat=VCFv4.2
+        ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+        ##contig=<ID=chr1>
+        #CHROM POS ID REF ALT QUAL FILTER INFO FORMAT m1  d1  c1
+        chr1   23  .  C   T   .    .      .    GT     0/1 0/0 0/0
+        chr1   24  .  C   A   .    .      .    GT     0/0 0/1 0/0
+    """)
+    root_path = annotate_directory_fixture
+    in_file = tmp_path / "in.vcf"
+    out_file = root_path / "out.vcf"
+    work_dir = tmp_path / "output"
+    annotation_file = root_path / "annotation_repeated_attributes.yaml"
+    grr_file = root_path / "grr.yaml"
+
+    setup_vcf(in_file, in_content)
+
+    cli([
+        str(a) for a in [
+            in_file,
+            sample_ped,
+            annotation_file,
+            "--grr", grr_file,
+            "-o", out_file,
+            "-w", work_dir,
+            "-j", 1,
+            "--allow-repeated-attributes",
+            "-R", "test_genome",
+        ]
+    ])
+
+    result = []
+    # pylint: disable=no-member
+    with pysam.VariantFile(str(out_file)) as vcf_file:
+        for vcf in vcf_file.fetch():
+            result.extend([
+                vcf.info["score_A0"][0],
+                vcf.info["score_A1"][0],
+            ])
+    assert result == ["0.1", "0.101", "0.2", "0.201"]
+
+
+def test_produce_partfile_paths() -> None:
+    regions = [Region("chr1", 0, 1000),
+               Region("chr1", 1000, 2000),
+               Region("chr1", 2000, 3000)]
+    expected_output = [
+        "work_dir/output/input.vcf_annotation_chr1_0_1000",
+        "work_dir/output/input.vcf_annotation_chr1_1000_2000",
+        "work_dir/output/input.vcf_annotation_chr1_2000_3000",
+    ]
+    # relative input file path
+    assert produce_partfile_paths(
+        "src/input.vcf", regions, "work_dir/output",
+    ) == expected_output
+    # absolute input file path
+    assert produce_partfile_paths(
+        "/home/user/src/input.vcf", regions, "work_dir/output",
+    ) == expected_output
