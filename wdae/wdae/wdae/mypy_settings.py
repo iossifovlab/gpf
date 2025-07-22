fb9fe1330ee8f6f5e3d84dc5f36d568a72b3027d
iordanivanov <iiordan.ivanov@gmail.com>
2025-07-22 12:51:48 +0300

    Mypy: don't run GPF instance when typechecking
    + Replace deprecated `strict_concatenate` option with `extra_checks`

---
+++ wdae/wdae/wdae/mypy_settings.py
@@ -0,0 +1,9 @@
+# pylint: disable=W0621,C0114,C0116,W0212,W0613
+# flake8: noqa
+
+# pylint: disable=wildcard-import,unused-wildcard-import
+from .default_settings import *
+
+# This stops mypy from setting up running a GPF instance when typechecking
+# which is slow and unnecessary
+INSTALLED_APPS.remove("gpf_instance.apps.WDAEConfig")
