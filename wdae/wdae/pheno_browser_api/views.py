e0fa1d7f79d3a1a3865e49a9da7f9897278b9e8d
IvoStefanov <ivostefanovtues@gmail.com>
2025-08-01 14:18:25 +0300

    Refactor pheno measures download
    * Move helper methods to helper class.
    * Prepare for remote helper class implementation.

--- wdae/wdae/pheno_browser_api/views.py
+++ wdae/wdae/pheno_browser_api/views.py
@@ -1,7 +1,4 @@
-import csv
 import logging
-from collections.abc import Generator
-from io import StringIO
 
 from datasets_api.permissions import (
     get_instance_timestamp_etag,
@@ -10,12 +7,18 @@ from datasets_api.permissions import (
 from django.http.response import HttpResponse, StreamingHttpResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.http import etag
+from gpf_instance.gpf_instance import WGPFInstance
 from query_base.query_base import DatasetAccessRightsView, QueryBaseView
 from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
-from studies.study_wrapper import WDAEStudy
-from utils.streaming_response_util import iterator_to_json
+from studies.study_wrapper import WDAEAbstractStudy, WDAEStudy
+
+from pheno_browser_api.pheno_browser_helper import (
+    BasePhenoBrowserHelper,
+    CountError,
+    PhenoBrowserHelper,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -156,112 +159,32 @@ class PhenoMeasuresView(QueryBaseView):
         return Response(measures_page)
 
 
-class CountError(Exception):
-    pass
-
-
 class PhenoMeasuresDownload(QueryBaseView, DatasetAccessRightsView):
     """Phenotype measure downloads view."""
 
-    def csv_value_iterator(
-        self,
-        dataset: WDAEStudy,
-        measure_ids: list[str],
-    ) -> Generator[str, None, None]:
-        """Create CSV content for people measures data."""
-        header = ["person_id", *measure_ids]
-        buffer = StringIO()
-        writer = csv.writer(buffer, delimiter=",")
-        writer.writerow(header)
-        yield buffer.getvalue()
-        buffer.seek(0)
-        buffer.truncate(0)
-
-        values_iterator = dataset.phenotype_data.get_people_measure_values(
-            measure_ids)
-
-        for values_dict in values_iterator:
-            output = [values_dict[header[0]]]
-            all_null = True
-            for col in header[1:]:
-                value = values_dict[col]
-                if value is not None:
-                    all_null = False
-                output.append(value)
-            if all_null:
-                continue
-            writer.writerow(output)
-            yield buffer.getvalue()
-            buffer.seek(0)
-            buffer.truncate(0)
-
-        buffer.close()
-
-    def get_measure_ids(self, request: Request) -> Generator[str, None, None]:
-        """Get measure ids."""
-        data = request.query_params
-        data = {k: str(v) for k, v in data.items()}
-
-        if "dataset_id" not in data:
-            raise ValueError
-        dataset_id = data["dataset_id"]
-
-        dataset = self.gpf_instance.get_wdae_wrapper(dataset_id)
-        if not dataset or not dataset.has_pheno_data:
-            raise KeyError
-
-        search_term = data.get("search_term", None)
-        instrument = data.get("instrument", None)
-
-        if (instrument is not None
-                and instrument != ""
-                and instrument not in dataset.phenotype_data.instruments):
-            raise KeyError
-
-        measures = dataset.phenotype_data.search_measures(
-            instrument, search_term,
-        )
-        measure_ids = [
-            measure["measure"]["measure_id"] for measure in measures
-        ]
-
-        if len(measure_ids) > 1900:
-            raise CountError
-
-        return self.csv_value_iterator(
-            dataset, measure_ids,
-        )
-
-    def count_measure_ids(self, request: Request) -> int:
-        """Get measure ids."""
-        data = request.query_params
-        data = {k: str(v) for k, v in data.items()}
-
-        if "dataset_id" not in data:
-            raise ValueError
-        dataset_id = data["dataset_id"]
-
-        dataset = self.gpf_instance.get_wdae_wrapper(dataset_id)
-        if not dataset or not dataset.has_pheno_data:
-            raise KeyError
-
-        search_term = data.get("search_term", None)
-        instrument = data.get("instrument", None)
-
-        if (instrument is not None
-                and instrument != ""
-                and instrument not in dataset.phenotype_data.instruments):
-            raise KeyError
-
-        return dataset.phenotype_data.count_measures(
-            instrument, search_term,
-        )
 
     @method_decorator(etag(get_instance_timestamp_etag))
     def get(self, request: Request) -> Response:
         """Return a CSV file stream for measures."""
+        data = request.query_params
+
+        if "dataset_id" not in data:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        dataset_id = data["dataset_id"]
+
+        study = self.gpf_instance.get_wdae_wrapper(dataset_id)
+
+        if not study or not study.has_pheno_data:
+            logger.info("Measures not found")
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        pheno_browser_helper = create_pheno_browser_helper(
+            self.gpf_instance,
+            study,
+        )
+
         try:
-            values_iterator = self.get_measure_ids(request)
+            values_iterator = pheno_browser_helper.get_measure_ids(data)
             response = StreamingHttpResponse(
                 values_iterator, content_type="text/csv")
         except ValueError:
@@ -283,8 +206,25 @@ class PhenoMeasuresDownload(QueryBaseView, DatasetAccessRightsView):
     #  pylint:disable=method-hidden
     def head(self, request: Request) -> Response:
         """Return a status code validating if measures can be downloaded."""
+
+        data = request.query_params
+
+        if "dataset_id" not in data:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        dataset_id = data["dataset_id"]
+
+        study = self.gpf_instance.get_wdae_wrapper(dataset_id)
+
+        if not study or not study.has_pheno_data:
+            logger.info("Measures not found")
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        pheno_browser_helper = create_pheno_browser_helper(
+            self.gpf_instance,
+            study,
+        )
         try:
-            measure_ids_count = self.count_measure_ids(request)
+            measure_ids_count = pheno_browser_helper.count_measure_ids(data)
         except ValueError:
             logger.exception("Error")
             return Response(status=status.HTTP_400_BAD_REQUEST)
@@ -375,3 +315,22 @@ class PhenoImagesView(QueryBaseView, DatasetAccessRightsView):
             return Response(status=status.HTTP_404_NOT_FOUND)
 
         return HttpResponse(image, content_type=mimetype)
+
+
+def create_pheno_browser_helper(
+    gpf_instance: WGPFInstance, study: WDAEAbstractStudy,
+) -> BasePhenoBrowserHelper:
+    """Create an pheno browser helper for the given dataset."""
+    for ext_name, extension in gpf_instance.extensions.items():
+        pheno_browser_helper = extension.get_tool(study, "pheno_browser_helper")
+        if pheno_browser_helper is not None:
+            if not isinstance(pheno_browser_helper, BasePhenoBrowserHelper):
+                raise ValueError(
+                    f"{ext_name} returned an invalid pheno browser helper!")
+            return pheno_browser_helper
+
+    if not isinstance(study, WDAEStudy):
+        raise ValueError(  # noqa: TRY004
+            f"Pheno browser helper for {study.study_id} is missing!")
+
+    return PhenoBrowserHelper(study)
