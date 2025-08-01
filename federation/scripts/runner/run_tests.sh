55a10fbfd09316fe7731e18933b35cf1b2b4a96f
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2025-08-01 14:06:04 +0300

    Collect coverage from federation tests

--- federation/scripts/runner/run_tests.sh
+++ federation/scripts/runner/run_tests.sh
@@ -9,5 +9,15 @@ done
 cd /wd/federation/
 /opt/conda/bin/conda run --no-capture-output -n gpf \
     py.test -vv -s --log-level=DEBUG \
+        --cov-config /wd/coveragerc \
+        --cov federation \
         --junitxml=/wd/results/federation-tests-junit.xml \
-        tests/
+        /wd/federation/tests
+
+/opt/conda/bin/conda run -n gpf \
+    coverage xml
+
+cp /wd/federation/coverage.xml /wd/test-results/
+
+/opt/conda/bin/conda run -n gpf \
+    coverage html --title gpf_federation -d /wd/test-results/coverage-html
