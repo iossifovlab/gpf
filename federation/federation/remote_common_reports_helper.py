10aaa5485679f393c1420cb4d78f8695dd2fff8c
IvoStefanov <ivostefanovtues@gmail.com>
2025-08-14 14:15:02 +0300

    Implement remote common reports helper

--- federation/federation/remote_common_reports_helper.py
+++ federation/federation/remote_common_reports_helper.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Any
 
 from common_reports_api.common_reports_helper import BaseCommonReportsHelper
 from gpf_instance.extension import GPFTool
@@ -26,3 +27,47 @@ class RemoteCommonReportsHelper(BaseCommonReportsHelper):
             study.rest_client,
             study.remote_study_id,
         )
+
+    def get_common_report(self) -> dict[str, Any] | None:
+        return self.rest_client.get_common_report(self.dataset_id)
+
+    def get_full_common_report(self) -> dict[str, Any] | None:
+        return self.rest_client.get_common_report(self.dataset_id, full=True)
+
+    def get_family_counter_list(
+        self,
+        group_name: str,
+        counter_id: int,
+    ) -> Any:
+        return self.rest_client.get_family_counter_list(
+            self.dataset_id,
+            group_name,
+            counter_id,
+        )
+
+    def get_family_counter_tsv(
+        self,
+        group_name: str,
+        counter_id: int,
+    ) -> list[str]:
+        return list(self.rest_client.download_family_counter_tsv(
+            self.dataset_id,
+            group_name,
+            counter_id,
+        ))
+
+    def get_family_data_tsv(
+        self,
+    ) -> list[str]:
+        return list(self.rest_client.get_family_data_pedigree_file(
+            self.dataset_id,
+        ))
+
+    def get_filtered_family_data_tsv(
+        self,
+        data: dict,
+    ) -> list[str]:
+        return list(self.rest_client.get_filtered_family_data_pedigree_file(
+            self.dataset_id,
+            data,
+        ))
