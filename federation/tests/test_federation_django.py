5319f7f3e4f49708bef077abd2d29427cd9ffd11
IvoStefanov <ivostefanovtues@gmail.com>
2025-08-28 11:04:55 +0300

    Add federation test for histograms beta api

--- federation/tests/test_federation_django.py
+++ federation/tests/test_federation_django.py
@@ -847,6 +847,66 @@ def test_histograms(
     assert response.status_code == 400
 
 
+def test_histograms_beta(
+    admin_client: Client,
+    t4c8_wgpf_instance: WGPFInstance,  # noqa: ARG001 ; setup WGPF instance
+) -> None:
+    response = admin_client.post("/api/v3/measures/histogram-beta", {
+        "datasetId": "TEST_REMOTE_t4c8_study_1", "measure": "i1.age",
+    })
+    assert response.status_code == 200
+
+    result = response.json()
+    assert result["measure"] == "i1.age"
+    assert result["description"] is None
+    assert result["histogram"]["min_value"] == pytest.approx(68, 0.1)
+    assert result["histogram"]["max_value"] == pytest.approx(565.91, 0.1)
+    assert result["histogram"]["config"] == {
+        "number_of_bins": 100,
+        "type": "number",
+        "view_range": {
+            "max": pytest.approx(565.91, 0.1),
+            "min": pytest.approx(68, 0.1),
+        },
+        "x_log_scale": False,
+        "x_min_log": None,
+        "y_log_scale": False,
+    }
+    assert result["histogram"]["out_of_range_bins"] == [0, 0]
+    assert result["histogram"]["bars"] == [
+        2, 0, 3, 0, 2, 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 2, 1,
+    ]
+    assert result["histogram"]["bins"] == [
+        pytest.approx(68.00, 0.1),
+        pytest.approx(87.91, 0.1),
+        pytest.approx(107.83, 0.1),
+        pytest.approx(127.75, 0.1),
+        pytest.approx(147.66, 0.1),
+        pytest.approx(167.58, 0.1),
+        pytest.approx(187.49, 0.1),
+        pytest.approx(207.41, 0.1),
+        pytest.approx(227.33, 0.1),
+        pytest.approx(247.24, 0.1),
+        pytest.approx(267.16, 0.1),
+        pytest.approx(287.08, 0.1),
+        pytest.approx(306.99, 0.1),
+        pytest.approx(326.91, 0.1),
+        pytest.approx(346.83, 0.1),
+        pytest.approx(366.74, 0.1),
+        pytest.approx(386.66, 0.1),
+        pytest.approx(406.57, 0.1),
+        pytest.approx(426.49, 0.1),
+        pytest.approx(446.41, 0.1),
+        pytest.approx(466.32, 0.1),
+        pytest.approx(486.24, 0.1),
+        pytest.approx(506.16, 0.1),
+        pytest.approx(526.07, 0.1),
+        pytest.approx(545.99, 0.1),
+        pytest.approx(565.91, 0.1),
+    ]
+
+
 def test_regressions(
     admin_client: Client,
     t4c8_wgpf_instance: WGPFInstance,  # noqa: ARG001 ; setup WGPF instance
