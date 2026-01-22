988c9060446e713559e4b58236ec0829c9c2032f
Lubomir Chorbadjiev <lubomir.chorbadjiev@gmail.com>
2026-01-22 17:02:32 +0200

    Reorganization of task graph to support dynamic ordering

--- dae/dae/task_graph/graph.py
+++ dae/dae/task_graph/graph.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
 import logging
-from collections.abc import Callable, Iterable
+import multiprocessing as mp
+from collections import defaultdict
+from collections.abc import Callable, Generator, Iterable, Sequence
 from copy import copy
 from dataclasses import dataclass
 from typing import Any
@@ -42,10 +44,11 @@ class TaskGraph:
         self._task_ids: set[str] = set()
 
     def create_task(
-            self, task_id: str, func: Callable[..., Any], *,
-            args: list,
-            deps: list[Task],
-            input_files: list[str] | None = None) -> Task:
+        self, task_id: str, func: Callable[..., Any], *,
+        args: list,
+        deps: list[Task],
+        input_files: list[str] | None = None,
+    ) -> Task:
         """Create a new task and add it to the graph.
 
         :param name: Name of the task (used for debugging purposes)
@@ -108,3 +111,209 @@ class TaskGraph:
 
 def sync_tasks() -> None:
     return None
+
+
+class TaskGraph2:
+    """An object representing a graph of tasks."""
+
+    def __init__(self) -> None:
+        self._lock = mp.Lock()
+        self._tasks: dict[str, Task] = {}
+        self._task_dependants: dict[str, set[str]] = defaultdict(set)
+
+        self.input_files: list[str] = []
+
+    def add_task(self, task: Task) -> None:
+        """Add a new task to the graph.
+
+        :param task: Task to add
+        """
+        with self._lock:
+            self._add_task(task)
+
+    def _add_task(self, task: Task) -> None:
+        if task.task_id in self._tasks:
+            raise ValueError(
+                f"Task with id='{task.task_id}' already in graph")
+
+        self._tasks[task.task_id] = task
+        for dep in task.deps:
+            self._task_dependants[dep.task_id].add(task.task_id)
+
+    def create_task(
+        self, task_id: str, func: Callable[..., Any], *,
+        args: list,
+        deps: list[Task],
+        input_files: list[str] | None = None,
+    ) -> Task:
+        """Create a new task and add it to the graph.
+
+        :param name: Name of the task (used for debugging purposes)
+        :param func: Function to execute
+        :param args: Arguments to that function
+        :param deps: List of TaskNodes on which the current task depends
+        :param input_files: Files that were used to build the graph itself
+        :return TaskNode: The newly created task node in the graph
+        """
+        if len(task_id) > 200:
+            logger.warning("task id is too long %s: %s", len(task_id), task_id)
+            logger.warning("truncating task id to 200 symbols")
+            task_id = task_id[:200]
+
+        with self._lock:
+            if task_id in self._tasks:
+                raise ValueError(f"Task with id='{task_id}' already in graph")
+
+        # tasks that use the output of other tasks as input should
+        # have those other tasks as dependancies
+        deps = copy(deps)
+        for arg in args:
+            if isinstance(arg, Task):
+                deps.append(arg)
+        task = Task(task_id, func, args, deps, input_files or [])
+        self.add_task(task)
+        return task
+
+    def prune(self, ids_to_keep: Iterable[str]) -> TaskGraph2:
+        """Prune tasks which are not in ids_to_keep or in their deps.
+
+        tasks ids which are in ids_to_keep but not in the graph are simply
+        assumed to have already been removed and no error is raised.
+        """
+        ids_to_keep = set(ids_to_keep)
+        with self._lock:
+            ids_not_found = ids_to_keep - self._tasks.keys()
+            if ids_not_found:
+                raise KeyError(ids_not_found)
+
+            tasks_to_keep: set[str] = set()
+            for task_id, task in self._tasks.items():
+                if task_id in ids_to_keep:
+                    tasks_to_keep.add(task.task_id)
+                    self._collect_task_deps(task, tasks_to_keep)
+
+            new_tasks = {
+                task_id: task
+                for task_id, task in self._tasks.items()
+                if task_id in tasks_to_keep}
+
+            res = TaskGraph2()
+            res._tasks = new_tasks
+            res.input_files = self.input_files
+            return res
+
+    @staticmethod
+    def _collect_task_deps(task: Task, task_set: set[str]) -> None:
+        for dep in task.deps:
+            if dep.task_id not in task_set:
+                task_set.add(dep.task_id)
+                TaskGraph2._collect_task_deps(dep, task_set)
+
+    @staticmethod
+    def from_task_graph(task_graph: TaskGraph) -> TaskGraph2:
+        """Create TaskGraph2 from TaskGraph."""
+        res = TaskGraph2()
+        res.input_files = task_graph.input_files
+        for task in task_graph.tasks:
+            res.add_task(task)
+        return res
+
+    def ready_tasks(self) -> Sequence[Task]:
+        """Return tasks which have no dependencies."""
+        result = []
+        with self._lock:
+            for task in self._topological_order():
+                if task.deps:
+                    break
+                result.append(task)
+            for task in result:
+                del self._tasks[task.task_id]
+
+        return result
+
+    def _topological_order(self) -> Generator[Task, None, None]:
+        """Return tasks in topological order."""
+        # pylint: disable=import-outside-toplevel
+        import networkx
+        di_graph = networkx.DiGraph()
+        nodes: list[str] = list(self._tasks.keys())
+        di_graph.add_nodes_from(sorted(nodes))
+
+        for task in self._tasks.values():
+            for dep in task.deps:
+                di_graph.add_edge(dep.task_id, task.task_id)
+
+        for task_id in networkx.topological_sort(di_graph):
+            if task_id not in self._tasks:
+                continue
+            yield self._tasks[task_id]
+
+    def topological_order(self) -> Sequence[Task]:
+        """Return tasks in topological order."""
+        with self._lock:
+            return list(self._topological_order())
+
+    def _prune_dependants(self, task_id: str) -> None:
+        dependents = self._task_dependants.get(task_id, set())
+        for dep_id in dependents:
+            logger.debug("pruning dependent task %s", dep_id)
+            if dep_id not in self._tasks:
+                continue
+            del self._tasks[dep_id]
+
+    def _reconfigure_task_deps(
+        self, task: Task,
+        completed_task_id: str,
+        completed_result: Any,
+    ) -> Task:
+        args = []
+        for arg in task.args:
+            if isinstance(arg, Task) and arg.task_id == completed_task_id:
+                arg = completed_result
+            args.append(arg)
+
+        deps = []
+        for dep in task.deps:
+            if dep.task_id == completed_task_id:
+                continue
+            deps.append(dep)
+
+        return Task(
+            task.task_id,
+            task.func,
+            args,
+            deps,
+            task.input_files,
+        )
+
+    def get_task(self, task_id: str) -> Task:
+        """Get task by its id.
+
+        :param task_id: Id of the task to get
+        :return: Task with the given id
+        """
+        with self._lock:
+            return self._tasks[task_id]
+
+    def process_completed_tasks(
+        self, task_result: Sequence[tuple[str, Any]],
+    ) -> None:
+        """Process a completed task.
+
+        :param task: Completed task
+        """
+        with self._lock:
+            for task_id, result in task_result:
+                if isinstance(result, BaseException):
+                    self._prune_dependants(task_id)
+                    return
+                task_dependants = self._task_dependants.get(task_id, set())
+                for dep_id in task_dependants:
+                    if dep_id not in self._tasks:
+                        continue
+                    dep_task = self._tasks[dep_id]
+                    dep_task = self._reconfigure_task_deps(
+                        dep_task, task_id, result)
+                    self._tasks[dep_id] = dep_task
+                if task_id in self._task_dependants:
+                    del self._task_dependants[task_id]
---
+++ dae/tests/small/task_graph/test_dynamic_task_graph.py
@@ -0,0 +1,177 @@
+# pylint: disable=W0621,C0114,C0115,C0116,W0212,W0613
+import pytest
+from dae.task_graph.graph import TaskGraph2
+
+
+@pytest.mark.parametrize(
+    "tasks,expected_order", [
+        (  # simple chain
+            [
+                ("A", []),
+                ("B", ["A"]),
+                ("C", ["B"]),
+            ],
+            ["A", "B", "C"],
+        ),
+        (  # diamond
+            [
+                ("A", []),
+                ("B", ["A"]),
+                ("C", ["A"]),
+                ("D", ["B", "C"]),
+            ],
+            ["A", "B", "C", "D"],
+        ),
+        (  # wide graph
+            [
+                ("A", []),
+                ("B", []),
+                ("C", []),
+                ("D", []),
+                ("E", ["A", "B", "C", "D"]),
+            ],
+            ["A", "B", "C", "D", "E"],
+        ),
+        (  # complex graph
+            [
+                ("A", []),
+                ("B", ["A"]),
+                ("C", ["A"]),
+                ("D", ["B"]),
+                ("E", ["B", "C"]),
+                ("F", ["D", "E"]),
+            ],
+            ["A", "B", "C", "D", "E", "F"],
+        ),
+        (
+            [  # multiple roots
+                ("A", []),
+                ("B", []),
+                ("C", ["A"]),
+                ("D", ["B"]),
+                ("E", ["C", "D"]),
+            ],
+            ["A", "B", "C", "D", "E"],
+        ),
+        (
+            [  # multiple independent chains
+                ("A1", []),
+                ("B1", ["A1"]),
+                ("C1", ["B1"]),
+                ("A2", []),
+                ("B2", ["A2"]),
+                ("C2", ["B2"]),
+            ],
+            ["A1", "A2", "B1", "B2", "C1", "C2"],
+        ),
+        (
+            [
+                ("3", []),
+                ("2", []),
+                ("1", ["2", "3"]),
+            ],
+            ["2", "3", "1"],
+        ),
+    ],
+)
+def test_graph_order(
+    tasks: list[tuple[str, list[str]]],
+    expected_order: list[str],
+) -> None:
+    graph = TaskGraph2()
+    for task_id, dep_ids in tasks:
+        deps = [graph.get_task(dep_id) for dep_id in dep_ids]
+        graph.create_task(task_id, lambda: None, args=[], deps=deps)
+
+    tasks_in_order = graph.topological_order()
+    ids_in_order = [task.task_id for task in tasks_in_order]
+
+    assert ids_in_order == expected_order
+
+
+@pytest.mark.parametrize(
+    "tasks,ready_seq", [
+        (  # 0: simple chain
+            [
+                ("A", []),
+                ("B", ["A"]),
+                ("C", ["B"]),
+            ],
+            [["A"], ["B"], ["C"]],
+        ),
+        (  # 1: diamond
+            [
+                ("A", []),
+                ("B", ["A"]),
+                ("C", ["A"]),
+                ("D", ["B", "C"]),
+            ],
+            [["A"], ["B", "C"], ["D"]],
+        ),
+        (  # 2: wide graph
+            [
+                ("A", []),
+                ("B", []),
+                ("C", []),
+                ("D", []),
+                ("E", ["A", "B", "C", "D"]),
+            ],
+            [["A", "B", "C", "D"], ["E"]],
+        ),
+        (  # 3: complex graph
+            [
+                ("A", []),
+                ("B", ["A"]),
+                ("C", ["A"]),
+                ("D", ["B"]),
+                ("E", ["B", "C"]),
+                ("F", ["D", "E"]),
+            ],
+            [["A"], ["B", "C"], ["D", "E"], ["F"]],
+        ),
+        (
+            [  # 4: multiple roots
+                ("A", []),
+                ("B", []),
+                ("C", ["A"]),
+                ("D", ["B"]),
+                ("E", ["C", "D"]),
+            ],
+            [["A", "B"], ["C", "D"], ["E"]],
+        ),
+        (
+            [  # 5: multiple independent chains
+                ("A1", []),
+                ("B1", ["A1"]),
+                ("C1", ["B1"]),
+                ("A2", []),
+                ("B2", ["A2"]),
+                ("C2", ["B2"]),
+            ],
+            [["A1", "A2"], ["B1", "B2"], ["C1", "C2"]],
+        ),
+        (
+            [  # 6: simple graph with numeric ids
+                ("3", []),
+                ("2", []),
+                ("1", ["2", "3"]),
+            ],
+            [["2", "3"], ["1"]],
+        ),
+    ],
+)
+def test_graph_ready_tasks(
+    tasks: list[tuple[str, list[str]]],
+    ready_seq: list[list[str]],
+) -> None:
+    graph = TaskGraph2()
+    for task_id, dep_ids in tasks:
+        deps = [graph.get_task(dep_id) for dep_id in dep_ids]
+        graph.create_task(task_id, lambda: None, args=[], deps=deps)
+
+    for ready in ready_seq:
+        ready_tasks = graph.ready_tasks()
+        ids_in_order = {task.task_id for task in ready_tasks}
+        assert ids_in_order == set(ready)
+        graph.process_completed_tasks(
+            [(task.task_id, "done") for task in ready_tasks])
