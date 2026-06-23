2841395787348600b12c4c65dad6d91940414473
IlinaGergova <ili.gergova@gmail.com>
2026-06-23 15:58:59 +0300

    upgrade Angular to v20

--- web_ui/jest.config.js
+++ web_ui/jest.config.js
@@ -2,7 +2,6 @@ const esModules = ['d3', 'd3-array', 'internmap', 'delaunator', 'robust-predicat
 
 module.exports = {
   preset: 'jest-preset-angular',
-  globalSetup: 'jest-preset-angular/global-setup',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   modulePaths: ['<rootDir>/src'],
   transformIgnorePatterns: [`node_modules/(?!${esModules}|.*.mjs$)`],
